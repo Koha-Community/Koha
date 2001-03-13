003f583b43af8cb939025b63d3d04e249fb41655
rangi <rangi>
2001-03-13 22:41:21 +0000

    Shifted catmaintain.pl to a dir that can be easily passowrd protected

--- maint/catmaintain.pl
+++ maint/catmaintain.pl
@@ -24,11 +24,11 @@ if ($type eq 'allsub'){
   for (my $i=0;$i<$count;$i++){
     my $sub2=$results->[$i]->{'subject'};
     $sub2=~ s/ /%20/g;
-    print "\"<a href=\"/cgi-bin/koha/catmaintain.pl?type=allsub&sub=$sub\" onclick=\'messenger(\"/cgi-bin/koha/catmaintain.pl?type=modsub&sub=$sub2\");window1.focus()\'>$results->[$i]->{'subject'}\"</a><br>\n";
+    print "\"<a href=\"/cgi-bin/koha/maint/catmaintain.pl?type=allsub&sub=$sub\" onclick=\'messenger(\"/cgi-bin/koha/maint/catmaintain.pl?type=modsub&sub=$sub2\");window1.focus()\'>$results->[$i]->{'subject'}\"</a><br>\n";
   }
 } elsif ($type eq 'modsub'){
   my $sub=$input->param('sub');
-  print "<form action=/cgi-bin/koha/catmaintain.pl>";
+  print "<form action=/cgi-bin/koha/maint/catmaintain.pl>";
   print "Subject:<input type=text value=\"$sub\" name=sub size=40><br>\n";
   print "<input type=hidden name=type value=upsub>";
   print "<input type=hidden name=oldsub value=\"$sub\">";
@@ -41,7 +41,7 @@ if ($type eq 'allsub'){
   my $oldsub=$input->param('oldsub');
   updatesub($sub,$oldsub);
   print "Successfully modified $oldsub is now $sub";
-  print "<p><a href=/cgi-bin/koha/catmaintain.pl target=window0 onclick=\"window0.focus()\">Back to catalogue maintenance</a><br>";
+  print "<p><a href=/cgi-bin/koha/maint/catmaintain.pl target=window0 onclick=\"window0.focus()\">Back to catalogue maintenance</a><br>";
   print "<a href=nowhere onclick=\"self.close()\">Close this window</a>";
 } elsif ($type eq 'undel'){
   my $title=$input->param('title');
@@ -49,17 +49,17 @@ if ($type eq 'allsub'){
   print "<table border=0>";
   print "<tr><td><b>Title</b></td><td><b>Author</b></td><td><b>Undelete</b></td></tr>";
   for (my $i=0;$i<$count;$i++){
-    print "<tr><td>$results->[$i]->{'title'}</td><td>$results->[$i]->{'author'}</td><td><a href=/cgi-bin/koha/catmaintain.pl?type=finun&bib=$results->[$i]->{'biblionumber'}>Undelete</a></td>\n";
+    print "<tr><td>$results->[$i]->{'title'}</td><td>$results->[$i]->{'author'}</td><td><a href=/cgi-bin/koha/maint/catmaintain.pl?type=finun&bib=$results->[$i]->{'biblionumber'}>Undelete</a></td>\n";
   }
   print "</table>";
 } elsif ($type eq 'finun'){
   my $bib=$input->param('bib');
   undeletebib($bib);
   print "Succesfully undeleted";
-  print "<p><a href=/cgi-bin/koha/catmaintain.pl>Back to Catalogue Maintenance</a>";
+  print "<p><a href=/cgi-bin/koha/maint/catmaintain.pl>Back to Catalogue Maintenance</a>";
 } else {
   print "<B>Subject Maintenance</b><br>";
-  print "<form action=/cgi-bin/koha/catmaintain.pl method=post>";
+  print "<form action=/cgi-bin/koha/maint/catmaintain.pl method=post>";
   print "<input type=hidden name=type value=allsub>";
   print "Show all subjects beginning with <input type=text name=sub><br>";
   print "<input type=submit value=Show>";
@@ -73,7 +73,7 @@ if ($type eq 'allsub'){
   print "</form>";
   print "<p>";
   print "<B>Undelete Biblio</b></br>";
-  print "<form action=/cgi-bin/koha/catmaintain.pl method=post>";
+  print "<form action=/cgi-bin/koha/maint/catmaintain.pl method=post>";
   print "<input type=hidden name=type value=undel>";
   print "Show all Titles beginning with <input type=text name=title><br>";
   print "<input type=submit value=Show>";
