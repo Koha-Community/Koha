f257b8ae0eb8a0647536cd571571e60470c240bf
tonnesen <tonnesen>
2002-05-10 21:22:18 +0000

    Still playing.  Added a "next record" link

--- html-template/search.pl
+++ html-template/search.pl
@@ -7,17 +7,20 @@ use CGI;
  
 my $query=new CGI;
 
-my $template=$query->param('template');
-($template) || ($template='searchresults.tmpl');
+my $templatename=$query->param('template');
+my $startfrom=$query->param('startfrom');
+($startfrom) || ($startfrom=0);
+($templatename) || ($templatename='searchresults.tmpl');
 
 
 my $dbh=&C4Connect;  
 
 
-my $template = HTML::Template->new(filename => $template, die_on_bad_params => 0);
+print STDERR "SF: $startfrom\n";
+my $template = HTML::Template->new(filename => $templatename, die_on_bad_params => 0);
 
 my @results;
-my $sth=$dbh->prepare("select * from biblio where author like 's%' limit 20");
+my $sth=$dbh->prepare("select * from biblio where author like 's%' order by author limit $startfrom,20");
 $sth->execute;
 while (my $data=$sth->fetchrow_hashref){    
     push @results, $data;
@@ -25,9 +28,9 @@ while (my $data=$sth->fetchrow_hashref){
 
 
 
-
-$template->param(SEARCH_RESULTS => \@results
-		);
-
+$startfrom+=20;
+$template->param(startfrom => $startfrom);
+$template->param(template => $templatename);
+$template->param(SEARCH_RESULTS => \@results);
 
 print "Content-Type: text/html\n\n", $template->output;
