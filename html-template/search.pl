7821d507a30975b69825617b5b6e96aa4843dbd1
tonnesen <tonnesen>
2002-05-10 17:41:05 +0000

    Added ability to switch between two templates.  One template shows biblionumber, the other template shows notes, in the third column.
    My thinking is that for every template, the scripts can search for a local
    version of the template before using the default template.

--- html-template/search.pl
+++ html-template/search.pl
@@ -3,12 +3,18 @@ use HTML::Template;
 use strict;
 require Exporter;
 use C4::Database;
+use CGI;
+ 
+my $query=new CGI;
+
+my $template=$query->param('template');
+($template) || ($template='searchresults.tmpl');
 
 
 my $dbh=&C4Connect;  
 
 
-my $template = HTML::Template->new(filename => 'searchresults.tmpl', die_on_bad_params => 0);
+my $template = HTML::Template->new(filename => $template, die_on_bad_params => 0);
 
 my @results;
 my $sth=$dbh->prepare("select * from biblio where author like 's%' limit 20");
