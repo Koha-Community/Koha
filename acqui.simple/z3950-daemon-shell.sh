219e006412be8ffbf6bd27773f6a49311ec689c8
tonnesen <tonnesen>
2002-07-02 22:08:50 +0000

    merging changes from rel-1-2

--- acqui.simple/z3950-daemon-shell.sh
+++ acqui.simple/z3950-daemon-shell.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
+# $Id$
+
 # Script to start Koha background Z39.50 search daemon
 
 # Part of the Koha Library Mgmt System -  www.koha.org
 # Licensed under the GPL
 
 #----------------------------
-# Do NOT run this script directly from system startup-
+# Do NOT run this script directly from system startup-- this should not run as root
 #    Call  z3950-daemon-launch.sh  instead
 
 #----------------------------
 
-KohaDir=/usr/local/www/koha/htdocs/cgi-bin/koha/acqui.simple
+KohaZ3950Dir=/usr/local/www/koha/htdocs/cgi-bin/koha/acqui.simple
 KohaModuleDir=/usr/local/koha/modules
 LogDir=/var/log/koha
 
@@ -25,7 +27,7 @@ then
 	exit 1
 fi
 
-KohaZ3950Script=$KohaDir/processz3950queue
+KohaZ3950Script=$KohaZ3950Dir/processz3950queue
 if [ ! -x $KohaZ3950Script ]
 then
 	echo ERROR: Cannot find Koha Z39.50 daemon script $KohaZ3950Script
@@ -35,4 +37,17 @@ fi
 PERL5LIB=$KohaModuleDir
 export PERL5LIB
 
-exec $KohaDir/processz3950queue >>$LOGFILE 2>&1
+exec $KohaZ3950Script $LogDir >>$LOGFILE 2>&1
+
+#-------------------
+# $Log$
+# Revision 1.3  2002/07/02 22:08:50  tonnesen
+# merging changes from rel-1-2
+#
+# Revision 1.1.2.3  2002/06/28 17:45:39  tonnesen
+# z3950queue now listens for a -HUP signal before processing the queue.  Z3950.pm
+# sends the -HUP signal when queries are added to the queue.
+#
+# Revision 1.1.2.2  2002/06/26 16:25:51  amillar
+# Make directory variable name more explanatory
+#
