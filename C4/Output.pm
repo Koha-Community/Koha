cb1dd812e135188c8ea897ee255b191d3b2f3d33
tonnesen <tonnesen>
2002-10-30 23:00:21 +0000

    Removed warn line

--- C4/Output.pm
+++ C4/Output.pm
@@ -201,7 +201,6 @@ sub themelanguage {
   THEME: 
   foreach my $th (@themes) {
     foreach my $la (@languages) {
-	warn "File = $htdocs/$th/$la/$tmpl\n";
 	if (-e "$htdocs/$th/$la/$tmpl") {
 	    $theme = $th;
 	    $lang = $la;
