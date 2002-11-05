a845bd24895eccba89def66bc4fe8138247c3cca
tonnesen <tonnesen>
2002-11-05 19:54:43 +0000

    Modified opac search script to use itemtypesearchgroups table to generate class list.

--- opac/opac-search.pl
+++ opac/opac-search.pl
@@ -5,6 +5,16 @@ require Exporter;
 use C4::Output;
 use CGI;
 use C4::Auth;
+use C4::Context;
+
+my $classlist='';
+my $dbh=C4::Context->dbh();
+my $sth=$dbh->prepare("select groupname,itemtypes from itemtypesearchgroups order by groupname");
+$sth->execute;
+while (my ($groupname,$itemtypes) = $sth->fetchrow) {
+    $classlist.="<option value=\"$itemtypes\">$groupname\n";
+}
+
 
 my $query = new CGI;
 
@@ -15,6 +25,7 @@ my ($loggedinuser, $cookie, $sessionID) = checkauth($query ,1, $flagsrequired);
 
 my $template = gettemplate("opac-search.tmpl", "opac");
 
-$template->param(loggedinuser => $loggedinuser);
+$template->param(loggedinuser => $loggedinuser,
+		classlist => $classlist);
 
 print "Content-Type: text/html\n\n", $template->output;
