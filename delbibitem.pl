d2efdbaa3aea185e3a186fe961568f593fae9a32
tonnesen <tonnesen>
2002-12-09 17:29:07 +0000

    Adding missing script for deleting biblioitems.  This script refuses to delete biblioitems which have items attached to it, but doesn't provide any notification to the user that the biblioitem was not deleted.  Otherwise, it is fully functional.  We still need a way to actually display  biblioitems that do not have items attached to them.  Right now, the only way is to add an item to the biblioitem, then delete the item and biblioitem one after another (because there is no way to get back to the page that shows the "delete biblioitem" button after the item has been deleted).

---
+++ delbibitem.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl
+
+#script to delete items
+#written 2/5/00
+#by chris@katipo.co.nz
+
+
+# Copyright 2000-2002 Katipo Communications
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Koha; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+# Suite 330, Boston, MA  02111-1307 USA
+
+use strict;
+
+use C4::Search;
+use CGI;
+use C4::Output;
+use C4::Acquisitions;
+use C4::Biblio;
+use C4::Auth;
+
+my $input = new CGI;
+my $flagsrequired;
+$flagsrequired->{editcatalogue}=1;
+my ($loggedinuser, $cookie, $sessionID) = checkauth($input, 0, $flagsrequired);
+
+#print $input->header;
+my $bibitem=$input->param('bibitemnum');
+my ($count) = getitemsbybiblioitem($bibitem);
+if ($count ==0) {
+    delbibitem($bibitem);
+}
+print $input->redirect("/cgi-bin/koha/catalogue-home.pl");
