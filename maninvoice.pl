ee40f753d44d11d6e553dc3ed79fc2373b0ffa75
acli <acli>
2003-01-25 20:15:11 +0000

    Handle non-latin1 charsets

--- maninvoice.pl
+++ maninvoice.pl
@@ -24,6 +24,7 @@
 use strict;
 use C4::Auth;
 use C4::Output;
+use C4::Charset;
 use CGI;
 use C4::Search;
 use C4::Accounts2;
@@ -53,6 +54,9 @@ if ($add){
 					debug => 1,
 					});
 	$template->param(bornum => $bornum);
-	print $input->header(-cookie => $cookie),$template->output;
+	print $input->header(
+	    -type => guesstype($template->output),
+	    -cookie => $cookie
+	),$template->output;
 
 }
