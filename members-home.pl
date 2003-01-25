5acde630643800cce5e1b2b106c8ba3352299b27
acli <acli>
2003-01-25 19:16:33 +0000

    Handle non-latin1 charsets

--- members-home.pl
+++ members-home.pl
@@ -4,6 +4,7 @@ use strict;
 use CGI;
 use C4::Auth;
 use C4::Output;
+use C4::Charset;
 use C4::Context;
 use HTML::Template;
 
@@ -17,4 +18,7 @@ my ($template, $loggedinuser, $cookie)
 			     debug => 1,
 			     });
 
-print $query->header(-cookie => $cookie),$template->output;
+print $query->header(
+    -type => guesstype($template->output),
+    -cookie => $cookie
+),$template->output;
