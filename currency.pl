01c317d7b0b1d8e173defeaaa507f34b53cef24d
tipaul <tipaul>
2003-02-13 18:03:36 +0000

    script now used only when currencies rates modified in acqui/acqui-home.pl page => removed unused code

--- currency.pl
+++ currency.pl
@@ -24,55 +24,18 @@
 # Koha; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
 # Suite 330, Boston, MA  02111-1307 USA
 
-# FIXME - There's an "admin/currency.pl", and this script never seems
-# to be used. Is it obsolete?
-
 use CGI;
 use C4::Catalogue;
 use C4::Biblio;
 
 my $input=new CGI;
 
-my $type=$input->param('type');
-#find out what the script is being called for
-#print $input->header();
-if ($type ne 'change'){
-  #display, we must fetch the exchange rate data and output it
-  print $input->header();
-  print <<printend
-  <TABLE width="40%" cellspacing=0 cellpadding=5 border=1 >
-  <FORM ACTION="/cgi-bin/koha/currency.pl">
-  <input type=hidden name=type value=change>
-  <TR VALIGN=TOP>
-  <TD  bgcolor="99cc33" background="/images/background-mem.gif" colspan=2 ><b>EXCHANGE RATES </b></TD></TR>
-  <TR VALIGN=TOP>
-  <TD>
-printend
-;
-  my ($count,$rates)=getcurrencies();
-  for (my $i=0;$i<$count;$i++){
-    if ($rates->[$i]->{'currency'} ne 'NZD'){
-      print "$rates->[$i]->{'currency'}<INPUT TYPE=\"text\"  SIZE=\"5\"   NAME=\"$rates->[$i]->{'currency'}\" value=$rates->[$i]->{'rate'}>";
-    }
-#    print $rates->[$i]->{'currency'};
-  }
-  print <<printend
-    <p>
-  <input type=image  name=submit src=/images/save-changes.gif border=0 width=187 height=42>
-
-  </TD></TR>
-  </form>
-  </table>
-printend
-;
-} else {
-#  print $input->Dump;
-  my @params=$input->param;
-  foreach my $param (@params){
-    if ($param ne 'type' && $param !~ /submit/){
-      my $data=$input->param($param);
-      updatecurrencies($param,$data);
-    }
-  }
-  print $input->redirect('/acquisitions/');
+my @params=$input->param;
+foreach my $param (@params){
+	if ($param ne 'type' && $param !~ /submit/){
+		my $data=$input->param($param);
+		warn "$data / $param";
+		updatecurrencies($param,$data);
 }
+}
+print $input->redirect('/cgi-bin/koha/acqui/acqui-home.pl');
