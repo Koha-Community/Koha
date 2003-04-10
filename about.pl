fa0d959c2a08a53dad6ee8891594356dfe0c2fb2
pate <pate>
2003-04-10 10:56:42 +0000

    first cut at perl script to create the about page.

---
+++ about.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+use HTML::Template;
+use strict;
+require Exporter;
+use C4::Database;
+use C4::Output;  # contains gettemplate
+use C4::Interface::CGI::Output;
+use C4::Auth;
+use C4::Context;
+use CGI;
+
+my $query = new CGI;
+my ($template, $loggedinuser, $cookie)
+    = get_template_and_user({template_name => "intranet-main.tmpl",
+			     query => $query,
+			     type => "intranet",
+			     authnotrequired => 0,
+			     flagsrequired => {parameters => 1},
+			     debug => 1,
+			     });
+
+my $kohaVersion = C4::Context->config("kohaversion");
+my $osVersion = `/bin/uname -a`;
+my $perlVersion = `/usr/bin/perl -v`;
+my $mysqlVersion = "unknown";
+my $apacheVersion =  "unknown";
+
+$template>param(
+                        osVersion          => $osVersion,
+                        perlVersion        => $perlVersion,
+                        mysqlVersion       => $mysqlVersion,
+                        apacheVersion      => $apacheVersion,
+		);
+
+output_html_with_http_headers $query, $cookie, $template->output;
