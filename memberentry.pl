3583bd69ff77c8fc066f358d4e56a6622edd6d8d
tipaul <tipaul>
2003-05-21 14:36:21 +0000

    "tab-ing" script

--- memberentry.pl
+++ memberentry.pl
@@ -51,94 +51,88 @@ my ($template, $loggedinuser, $cookie)
 
 my $member=$input->param('bornum');
 if ($member eq ''){
-  $member=NewBorrowerNumber();
+	$member=NewBorrowerNumber();
 }
 my $type=$input->param('type') || '';
 my $modify=$input->param('modify.x');
 my $delete=$input->param('delete.x');
 if ($delete){
-  print $input->redirect("/cgi-bin/koha/deletemem.pl?member=$member");
-
+	print $input->redirect("/cgi-bin/koha/deletemem.pl?member=$member");
 } else {  # this else goes down the whole script
-  if ($type ne 'Add'){
-    $template->param( header => 'Update Member Details'); # bad templating style
-  } else {
-    $template->param( header => 'Add New Member');
-  }
+	if ($type ne 'Add'){
+		$template->param( header => 'Update Member Details'); # bad templating style
+	} else {
+		$template->param( header => 'Add New Member');
+	}
 
-  my $data=borrdata('',$member);
+	my $data=borrdata('',$member);
+	warn "data: ".$data->{'surname'};
+	if ($type eq 'Add'){
+		$template->param( updtype => 'I');
+	} else {
+		$template->param( updtype => 'M');
+	}
+	my $cardnumber=C4::Members::fixup_cardnumber($data->{'cardnumber'});
+	if ($data->{'sex'} eq 'F'){
+		$template->param(female => 1);
+	}
+	my ($categories,$labels)=ethnicitycategories();
+	my $ethnicitycategoriescount=$#{$categories};
+	my $ethcatpopup;
+	if ($ethnicitycategoriescount>=0) {
+		$ethcatpopup = CGI::popup_menu(-name=>'ethnicity',
+					-values=>$categories,
+					-default=>$data->{'ethnicity'},
+					-labels=>$labels);
+		$template->param(ethcatpopup => $ethcatpopup); # bad style, has to be fixed
+	}
 
-  if ($type eq 'Add'){
-    $template->param( updtype => 'I');
-  } else {
-    $template->param( updtype => 'M');
-  }
+	($categories,$labels)=borrowercategories();
+	my $catcodepopup = CGI::popup_menu(-name=>'categorycode',
+					-values=>$categories,
+					-default=>$data->{'categorycode'},
+					-labels=>$labels);
 
-  my $cardnumber=C4::Members::fixup_cardnumber($data->{'cardnumber'});
+	my @relationships = ('workplace', 'relative','friend', 'neighbour');
+	my @relshipdata;
+	while (@relationships) {
+		my $relship = shift @relationships;
+		my %row = ('relationship' => $relship);
+		if ($data->{'altrelationship'} eq $relship) {
+			$row{'selected'}=' selected';
+		} else {
+			$row{'selected'}='';
+		}
+		push(@relshipdata, \%row);
+	}
 
-  if ($data->{'sex'} eq 'F'){
-    $template->param(female => 1);
-  }
+	# %flags: keys=$data-keys, datas=[formname, HTML-explanation]
+	my %flags = ('gonenoaddress' => ['gna', 'Gone no address'],
+				'lost'          => ['lost', 'Lost'],
+				'debarred'      => ['debarred', 'Debarred']);
 
-  my ($categories,$labels)=ethnicitycategories();
-  my $ethnicitycategoriescount=$#{$categories};
-  my $ethcatpopup;
-  if ($ethnicitycategoriescount>=0) {
-  	$ethcatpopup = CGI::popup_menu(-name=>'ethnicity',
-  			        -values=>$categories,
-  			        -default=>$data->{'ethnicity'},
-  			        -labels=>$labels);
-  	$template->param(ethcatpopup => $ethcatpopup); # bad style, has to be fixed
-  }
+	my @flagdata;
+	foreach (keys(%flags)) {
+	my $key = $_;
+	my %row =  ('key'   => $key,
+			'name'  => $flags{$key}[0],
+			'html'  => $flags{$key}[1]);
+	if ($data->{$key}) {
+		$row{'yes'}=' checked';
+		$row{'no'}='';
+	} else {
+		$row{'yes'}='';
+		$row{'no'}=' checked';
+	}
+	push(@flagdata, \%row);
+	}
 
-  ($categories,$labels)=borrowercategories();
-  my $catcodepopup = CGI::popup_menu(-name=>'categorycode',
-  			        -values=>$categories,
-  			        -default=>$data->{'categorycode'},
-  			        -labels=>$labels);
+	if ($modify){
+	$template->param( modify => 1 );
+	}
 
-
-
-  my @relationships = ('workplace', 'relative','friend', 'neighbour');
-  my @relshipdata;
-  while (@relationships) {
-    my $relship = shift @relationships;
-    my %row = ('relationship' => $relship);
-    if ($data->{'altrelationship'} eq $relship) {
-      $row{'selected'}=' selected';
-    } else {
-      $row{'selected'}='';
-    }
-    push(@relshipdata, \%row);
-  }
-
-  # %flags: keys=$data-keys, datas=[formname, HTML-explanation]
-  my %flags = ('gonenoaddress' => ['gna', 'Gone no address'],
-               'lost'          => ['lost', 'Lost'],
-               'debarred'      => ['debarred', 'Debarred']);
-
-  my @flagdata;
-  foreach (keys(%flags)) {
-    my $key = $_;
-    my %row =  ('key'   => $key,
-		'name'  => $flags{$key}[0],
-		'html'  => $flags{$key}[1]);
-    if ($data->{$key}) {
-      $row{'yes'}=' checked';
-      $row{'no'}='';
-    } else {
-      $row{'yes'}='';
-      $row{'no'}=' checked';
-    }
-    push(@flagdata, \%row);
-  }
-
-  if ($modify){
-    $template->param( modify => 1 );
-  }
-
-  #Convert dateofbirth to correct format
-  $data->{'dateofbirth'} = format_date($data->{'dateofbirth'});
+	#Convert dateofbirth to correct format
+	$data->{'dateofbirth'} = format_date($data->{'dateofbirth'});
 
 	my @branches;
 	my @select_branch;
@@ -155,39 +149,39 @@ if ($delete){
 				-size     => 1,
 				-multiple => 0 );
 
-  $template->param(
-			member          => $member,
-			address         => $data->{'streetaddress'},
-			firstname       => $data->{'firstname'},
-			surname         => $data->{'surname'},
-			othernames	=> $data->{'othernames'},
-			initials	=> $data->{'initials'},
-			ethcatpopup	=> $ethcatpopup,
-			catcodepopup	=> $catcodepopup,
-			streetaddress   => $data->{'physstreet'},
-			zipcode => $data->{'zipcode'},
-			streetcity      => $data->{'streetcity'},
-			homezipcode => $data->{'homezipcode'},
-			city		=> $data->{'city'},
-  			phone           => $data->{'phone'},
-  			phoneday        => $data->{'phoneday'},
-  			faxnumber       => $data->{'faxnumber'},
-  			emailaddress    => $data->{'emailaddress'},
-			textmessaging   => $data->{'textmessaging'},
-  			contactname     => $data->{'contactname'},
-  			altphone        => $data->{'altphone'},
-  			altnotes	=> $data->{'altnotes'},
-  			borrowernotes	=> $data->{'borrowernotes'},
-  			flagloop	=> \@flagdata,
-  			relshiploop	=> \@relshipdata,
-			"title_".$data->{'title'} => " SELECTED ",
-  			dateenrolled	=> $data->{'dateenrolled'},
-  			expiry		=> $data->{'expiry'},
-  			cardnumber	=> $cardnumber,
-  			dateofbirth	=> $data->{'dateofbirth'},
-			dateformat      => display_date_format(),
-			CGIbranch => $CGIbranch);
-output_html_with_http_headers $input, $cookie, $template->output;
+	$template->param(
+				member          => $member,
+				address         => $data->{'streetaddress'},
+				firstname       => $data->{'firstname'},
+				surname         => $data->{'surname'},
+				othernames	=> $data->{'othernames'},
+				initials	=> $data->{'initials'},
+				ethcatpopup	=> $ethcatpopup,
+				catcodepopup	=> $catcodepopup,
+				streetaddress   => $data->{'physstreet'},
+				zipcode => $data->{'zipcode'},
+				streetcity      => $data->{'streetcity'},
+				homezipcode => $data->{'homezipcode'},
+				city		=> $data->{'city'},
+				phone           => $data->{'phone'},
+				phoneday        => $data->{'phoneday'},
+				faxnumber       => $data->{'faxnumber'},
+				emailaddress    => $data->{'emailaddress'},
+				textmessaging   => $data->{'textmessaging'},
+				contactname     => $data->{'contactname'},
+				altphone        => $data->{'altphone'},
+				altnotes	=> $data->{'altnotes'},
+				borrowernotes	=> $data->{'borrowernotes'},
+				flagloop	=> \@flagdata,
+				relshiploop	=> \@relshipdata,
+				"title_".$data->{'title'} => " SELECTED ",
+				dateenrolled	=> $data->{'dateenrolled'},
+				expiry		=> $data->{'expiry'},
+				cardnumber	=> $cardnumber,
+				dateofbirth	=> $data->{'dateofbirth'},
+				dateformat      => display_date_format(),
+				CGIbranch => $CGIbranch);
+	output_html_with_http_headers $input, $cookie, $template->output;
 
 
 }
