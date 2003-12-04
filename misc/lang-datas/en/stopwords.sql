8e1d61da867512cc7fc4b2716483b11ba50583e6
tipaul <tipaul>
2003-12-04 12:29:58 +0000

    empty english stopwords file

---
+++ misc/lang-datas/en/stopwords.sql
@@ -0,0 +1,16 @@
+# phpMyAdmin MySQL-Dump
+# version 2.4.0
+# http://www.phpmyadmin.net/ (download page)
+#
+# Serveur: localhost
+# Généré le : Jeudi 04 Décembre 2003 à 13:02
+# Version du serveur: 4.0.15
+# Version de PHP: 4.3.3
+# Base de données: `koha_200RC1`
+
+#
+# Contenu de la table `stopwords`
+#
+
+INSERT INTO stopwords VALUES ('THE');
+
