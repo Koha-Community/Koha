7c2c39dea5e6c5a884016169b1e72ef63c59e2d7
tipaul <tipaul>
2004-03-30 16:09:04 +0000

    NEW : template for report writing. with 2 nice reports. pls test. documentation of the template syntax coming soon

---
+++ reports/manager.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/perl
+
+# Copyright 2000-2002 Katipo Communications
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Koha; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+# Suite 330, Boston, MA  02111-1307 USA
+
+use strict;
+use CGI;
+use C4::Auth;
+use C4::Context;
+use C4::Output;
+use C4::Interface::CGI::Output;
+use C4::Circulation::Circ2;
+use HTML::Template;
+
+my $input = new CGI;
+my $report_name=$input->param("report_name");
+my $do_it=$input->param('do_it');
+my $fullreportname = "reports/".$report_name.".tmpl";
+my @values = $input->param("value");
+my ($template, $borrowernumber, $cookie)
+	= get_template_and_user({template_name => $fullreportname,
+				query => $input,
+				type => "intranet",
+				authnotrequired => 0,
+				flagsrequired => {editcatalogue => 1},
+				debug => 1,
+				});
+$template->param(do_it => $do_it,
+		report_name => $report_name);
+my $plugin = "./".$report_name.".plugin";
+require $plugin;
+if ($do_it) {
+	my $results = calculate(\@values);
+	$template->param(mainloop => $results);
+} else {
+	$template = set_parameters($template);
+}
+output_html_with_http_headers $input, $cookie, $template->output;
