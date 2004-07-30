049ce2ed3720ef9dd9529619d960e028e0a729ff
doxulting <doxulting>
2004-07-30 14:13:31 +0000

    for error managing in .htaccess

---
+++ koha-tmpl/errors/400.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+use strict;
+use CGI;
+use C4::Auth;
+use C4::Output;
+use C4::Interface::CGI::Output;
+use C4::Context;
+use HTML::Template;
+
+my $query = new CGI;
+my ($template, $loggedinuser, $cookie)
+= get_template_and_user({template_name => "errors/400.tmpl",
+				query => $query,
+				type => "intranet",
+				authnotrequired => 0,
+				flagsrequired => {catalogue => 1},
+				debug => 1,
+				});
+output_html_with_http_headers $query, $cookie, $template->output;
