8664309215ed28729dfa9148001e8988fbbd0a19
tipaul <tipaul>
2005-01-03 16:30:13 +0000

    updating doc & adding a script to rebuild officially supported languages

---
+++ misc/translator/rebuild_lang.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+./tmpl_process3.pl install -r -s po/css_opac_fr_FR.po -i ../../koha-tmpl/opac-tmpl/css/en -o ../../koha-tmpl/opac-tmpl/css/fr
+./tmpl_process3.pl install -r -s po/default_intranet_fr_FR.po -i ../../koha-tmpl/intranet-tmpl/default/en -o ../../koha-tmpl/intranet-tmpl/default/fr
+
+./tmpl_process3.pl install -r -s po/css_opac_zh_TW.po -i ../../koha-tmpl/opac-tmpl/css/zh_TW -o ../../koha-tmpl/opac-tmpl/css/zh_TW
+./tmpl_process3.pl install -r -s po/default_intranet_zh_TW.po -i ../../koha-tmpl/intranet-tmpl/default/en -o ../../koha-tmpl/intranet-tmpl/default/zh_TW
+
+./tmpl_process3.pl install -r -s po/css_opac_es_AR.po -i ../../koha-tmpl/opac-tmpl/css/zh_TW -o ../../koha-tmpl/opac-tmpl/css/es_AR
+./tmpl_process3.pl install -r -s po/default_intranet_es_AR.po -i ../../koha-tmpl/intranet-tmpl/default/en -o ../../koha-tmpl/intranet-tmpl/default/es_AR
