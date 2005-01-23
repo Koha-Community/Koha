9f1213711f9f60300233275ba14b6b91afb169a7
acli <acli>
2005-01-23 06:22:26 +0000

    create fails if the pot file already exists

--- misc/translator/po/update.pl
+++ misc/translator/po/update.pl
@@ -47,6 +47,7 @@ for my $spec (
       # but this is not Koha's convention.
       #
       my $target = "po/${theme}_${module}" . ($pot_p? ".pot": "$lang.po");
+      rename($target, "$target~") if $pot_p;
       exec('./tmpl_process3.pl', ($pot_p? 'create': 'update'),
 	    '-i', "../../koha-tmpl/$module-tmpl/$theme/en/",
 	    '-s', $target, '-r');
