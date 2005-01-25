f72e8b7de56e010a8d26cdac6abc726414204d2e
oleonard <oleonard>
2005-01-25 22:53:46 +0000

    Three new functions: CheckAll, which toggles checkboxes in results lists; confirmDelete, which offers a warning before proceding with an action; Dopop, for popping up virtual shelves window

--- koha-tmpl/opac-tmpl/npl/en/includes/script.js
+++ koha-tmpl/opac-tmpl/npl/en/includes/script.js
@@ -38,3 +38,29 @@ function ValidateCode()
      }
   }
 }
+
+function CheckAll()
+{
+count = document.mainform.elements.length;
+    for (i=0; i < count; i++) 
+	{
+	    if(document.mainform.elements[i].checked == 1){
+			document.mainform.elements[i].checked = 0;
+		} else {
+			document.mainform.elements[i].checked = 1;
+		}
+	}
+}
+
+function confirmDelete(message) {
+	var agree = confirm(message);
+	if(agree) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function Dopop(link) {
+	newin=window.open(link,'popup','width=500,height=400,toolbar=false,scrollbars=yes');
+	}
