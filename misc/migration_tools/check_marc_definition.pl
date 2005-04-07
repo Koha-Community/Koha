50047399754873c79013fbe967e878b6ddcba949
tipaul <tipaul>
2005-04-07 12:00:57 +0000

    updating to deal with frameworkcodes.

--- misc/migration_tools/check_marc_definition.pl
+++ misc/migration_tools/check_marc_definition.pl
@@ -35,13 +35,21 @@ my $dbh = C4::Context->dbh;
 print "Checking\n";
 my $sth = $dbh->prepare("SELECT count(*), tag, subfieldcode, frameworkcode FROM marc_subfield_table, marc_biblio WHERE marc_biblio.bibid = marc_subfield_table.bibid group by frameworkcode,tag,subfieldcode");
 $sth->execute;
+my %tags;
 my $sth2 = $dbh->prepare("select tab,liblibrarian,kohafield from marc_subfield_structure where tagfield=? and tagsubfield=? and frameworkcode=?");
 while (my ($total,$tag,$subfield,$frameworkcode) = $sth->fetchrow) {
 	$sth2->execute($tag,$subfield,$frameworkcode);
+	$tags{$frameworkcode." / ".$tag." / ".$subfield} ++;
 	my ($tab,$liblibrarian,$kohafield) = $sth2->fetchrow;
 	if ($tab eq -1 && $kohafield ne "biblio.biblionumber" && $kohafield ne "biblioitems.biblioitemnumber" && $kohafield ne "items.itemnumber") {
 		print "Tab ignore for framework $frameworkcode, $tag\$$subfield - $liblibrarian (used $total times)\n";
 	}
 }
 
+$sth = $dbh->prepare("select frameworkcode,tagfield,tagsubfield from marc_subfield_structure where tab<>-1 order by frameworkcode,tagfield,tagsubfield");
+$sth->execute;
+print "===================\n";
+while (my ($frameworkcode,$tag,$subfield) = $sth->fetchrow) {
+	print "$tag, $subfield in framework $frameworkcode is active, but never filled\n" unless $tags{$frameworkcode." / ".$tag." / ".$subfield};
+}
 print "Done\n";
