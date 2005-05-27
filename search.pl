6f7bf22490225ae85e23aebcaaafb239a3b7d7a0
hdl <hdl>
2005-05-27 10:02:49 +0000

    Bug Fixing : Using old search.pl with subjectitems In normal mode display. Problem : Changing Page doesn't work.

--- search.pl
+++ search.pl
@@ -19,6 +19,10 @@
 # Suite 330, Boston, MA  02111-1307 USA
 
 # $Log$
+# Revision 1.35.2.1  2005/05/27 10:02:50  hdl
+# Bug Fixing : Using old search.pl with subjectitems In normal mode display.
+# Problem : Changing Page doesn't work.
+#
 # Revision 1.35  2004/04/07 22:43:04  rangi
 # Fix for bug 217
 #
@@ -99,7 +103,7 @@ my @forminputs;			# This is used in the form template.
 
 foreach my $term (qw(keyword subject author illustrator itemnumber
 		     isbn date-before class dewey branch title abstract
-		     publisher ttype))
+		     publisher ttype subjectitems))
 {
 	my $value = $query->param($term);
 	next unless defined $value && $value ne "";
