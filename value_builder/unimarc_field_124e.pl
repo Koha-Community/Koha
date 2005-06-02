495eae771de18544f4f765f4b2e7c4eb7c86e8df
doxulting <doxulting>
2005-06-02 15:46:43 +0000

    Plugin for field 124e

--- value_builder/unimarc_field_124e.pl
+++ value_builder/unimarc_field_124e.pl
@@ -52,7 +52,7 @@ function Blur$function_name(subfield_managed) {
 
 function Clic$function_name(i) {
 	defaultvalue=document.forms[0].field_value[i].value;
-	newin=window.open(\"../plugin_launcher.pl?plugin_name=unimarc_field_124e.pl&index=\"+i+\"&result=\"+defaultvalue,\"unimarc field 124e\",'width=1000,height=375,toolbar=false,scrollbars=yes');
+	newin=window.open(\"../plugin_launcher.pl?plugin_name=unimarc_field_124e.pl&index=\"+i+\"&result=\"+defaultvalue,\"unimarc field 124e\",'width=800,height=400,toolbar=false,scrollbars=yes');
 
 }
 </script>
