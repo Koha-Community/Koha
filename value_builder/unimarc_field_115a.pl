65153d32081deaacfe123e76a25bbf2d3bf2ee6a
doxulting <doxulting>
2005-06-02 15:38:51 +0000

    Plugin for field 115a

--- value_builder/unimarc_field_115a.pl
+++ value_builder/unimarc_field_115a.pl
@@ -52,7 +52,7 @@ function Blur$function_name(subfield_managed) {
 
 function Clic$function_name(i) {
 	defaultvalue=document.forms[0].field_value[i].value;
-	newin=window.open(\"../plugin_launcher.pl?plugin_name=unimarc_field_115a.pl&index=\"+i+\"&result=\"+defaultvalue,\"unimarc field 115a\",'width=1200,height=600,toolbar=false,scrollbars=yes');
+	newin=window.open(\"../plugin_launcher.pl?plugin_name=unimarc_field_115a.pl&index=\"+i+\"&result=\"+defaultvalue,\"unimarc field 115a\",'width=800,height=400,toolbar=false,scrollbars=yes');
 
 }
 </script>
