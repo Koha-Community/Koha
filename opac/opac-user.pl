9b4b71dceeb59d0d7dfe488fc10739887b0526ff
tipaul <tipaul>
2005-06-16 15:49:05 +0000

    adding opac text messaging to login page.

--- opac/opac-user.pl
+++ opac/opac-user.pl
@@ -136,9 +136,10 @@ foreach my $res (@$reserves) {
 
 $template->param(WAITING => \@waiting);
 $template->param(waiting_count => $wcount,
-			     LibraryName => C4::Context->preference("LibraryName"),
-					suggestion => C4::Context->preference("suggestion"),
-					virtualshelves => C4::Context->preference("virtualshelves"),
+				LibraryName => C4::Context->preference("LibraryName"),
+				suggestion => C4::Context->preference("suggestion"),
+				virtualshelves => C4::Context->preference("virtualshelves"),
+				textmessaging => $borr->{textmessaging},
 );
 
 output_html_with_http_headers $query, $cookie, $template->output;
