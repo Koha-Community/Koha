a4d86bfa6c0c2b60b3bf2cbc41d804793d193b15
tipaul <tipaul>
2006-02-13 16:36:48 +0000

    cleaning code (useless use C4::)

--- catalogue/ISBDdetail.pl
+++ catalogue/ISBDdetail.pl
@@ -43,11 +43,8 @@ use C4::Context;
 use C4::Output;
 use C4::Interface::CGI::Output;
 use CGI;
-use C4::Search;
-use MARC::Record;
+use C4::Koha;
 use C4::Biblio;
-use C4::Acquisition;
-use HTML::Template;
 
 my $query=new CGI;
 
