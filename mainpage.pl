3389e743a1c2bebf1294b29f6bc8cdd90b46d9b4
tipaul <tipaul>
2006-03-03 17:02:22 +0000

    commit for holidays and news management. (some forgotten files)

--- mainpage.pl
+++ mainpage.pl
@@ -9,7 +9,7 @@ use CGI;
 use C4::Auth;
 use C4::AuthoritiesMarc;
 use C4::Koha;
-
+use C4::NewsChannels;
 my $query = new CGI;
 my $authtypes = getauthtypes;
 my @authtypesloop;
@@ -36,5 +36,9 @@ my ($template, $loggedinuser, $cookie)
 my $marc_p = C4::Context->boolean_preference("marc");
 $template->param(NOTMARC => !$marc_p);
 $template->param(authtypesloop => \@authtypesloop);
+
+my ($koha_news_count, $all_koha_news) = &get_opac_news(undef, 'koha');
+$template->param(koha_news        => $all_koha_news);
+$template->param(koha_news_count  => $koha_news_count);
 
 output_html_with_http_headers $query, $cookie, $template->output;
