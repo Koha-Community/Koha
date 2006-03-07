ae0b569eacce3169f55a62df70f2d579f0c1f0a2
oleonard <oleonard>
2006-03-07 16:57:47 +0000

    One section still missing IntranetNav and stylesheet system prefs

--- acqui/acquire.pl
+++ acqui/acquire.pl
@@ -183,6 +183,9 @@ if ($count == 1){
 						name => $booksellers[0]->{'name'},
 						supplierid => $supplierid,
 						invoice => $invoice,
+		intranetcolorstylesheet => C4::Context->preference("intranetcolorstylesheet"),
+		intranetstylesheet => C4::Context->preference("intranetstylesheet"),
+		IntranetNav => C4::Context->preference("IntranetNav"),
 );
 
 }
