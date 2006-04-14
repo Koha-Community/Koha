924dc4d4ee3b2eaa04ad9c73a03157a5d2de02c5
plg <plg>
2006-04-14 13:18:25 +0000

    bug fixed: missing090field.pl is a rel_2_2 script, ie using rel_2_2 modules functions, not a HEAD script. It doesn't work in HEAD.

---
+++ misc/missing090field.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/perl
+# This script finds and fixes missing 090 fields in Koha for MARC21
+#  Written by TG on 01/10/2005
+#  Revised by Joshua Ferraro on 03/31/2006
+use strict;
+
+# Koha modules used
+
+use C4::Context;
+use C4::Biblio;
+use MARC::Record;
+use MARC::File::USMARC;
+
+
+my $dbh = C4::Context->dbh;
+
+my $sth=$dbh->prepare("select m.biblionumber,b.biblioitemnumber from marc_biblio m left join biblioitems b on b.biblionumber=m.biblionumber ");
+	$sth->execute();
+
+while (my ($biblionumber,$biblioitemnumber)=$sth->fetchrow ){
+ my $record = MARCgetbiblio($dbh,$biblionumber);
+		
+		MARCmodbiblionumber($biblionumber,$biblioitemnumber,$record);
+		
+}
+
+sub MARCmodbiblionumber{
+my ($biblionumber,$biblioitemnumber,$record)=@_;
+
+my ($tagfield,$biblionumtagsubfield) = &MARCfind_marc_from_kohafield($dbh,"biblio.biblionumber","");
+my ($tagfield2,$biblioitemtagsubfield) = &MARCfind_marc_from_kohafield($dbh,"biblio.biblioitemnumber","");
+	
+my $update=0;
+      my @tags = $record->field($tagfield);
+
+if (!@tags){
+         
+my $newrec = MARC::Field->new( $tagfield,'','', $biblionumtagsubfield => $biblionumber,$biblioitemtagsubfield=>$biblioitemnumber);
+    $record->append_fields($newrec);
+ $update=1;
+	} 	
+
+ 
+if ($update){	
+&MARCmodbiblio($dbh,$biblionumber,$record,'',0);
+	print "$biblionumber \n";	
+	}
+
+}
+END;
