ef2ff09c8bb7b23b4cc4dc56b8efc64a2178e19c
rangi <rangi>
2006-06-07 02:02:00 +0000

    Merging Katipo changes
    Workaround to deal with bug with IE

--- help.pl
+++ help.pl
@@ -1,4 +1,22 @@
 #!/usr/bin/perl
+
+# Copyright 2006 Katipo Communications
+#
+# This file is part of Koha.                                                    
+#                                                                               
+# Koha is free software; you can redistribute it and/or modify it under the     
+# terms of the GNU General Public License as published by the Free Software     
+# Foundation; either version 2 of the License, or (at your option) any later    
+# version.                                                                      
+#                                                                               
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY       
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR 
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.   
+#                                                                               
+# You should have received a copy of the GNU General Public License along with  
+# Koha; if not, write to the Free Software Foundation, Inc., 59 Temple Place,   
+# Suite 330, Boston, MA  02111-1307 USA
+
 use HTML::Template;
 use strict;
 require Exporter;
@@ -13,6 +31,12 @@ my $query = new CGI;
 # find the script that called the online help using the CGI referer()
 
 my $refer  = $query->referer();
+# workaround for popup not functioning correctly in IE
+my $referurl = $query->param('url');
+if($referurl){
+    $refer = $query->param('url');
+}
+
 $refer =~ /.*koha\/(.*)\.pl.*/;
 my $from = "help/$1.tmpl";
 
