3fd0bb0ed9195ee55074f77b82dee2045109c650
toins <toins>
2006-07-26 10:27:47 +0000

    Code cleaned. GPL & POD added. Sub renamed.

--- acqui/histsearch.pl
+++ acqui/histsearch.pl
@@ -1,4 +1,54 @@
 #!/usr/bin/perl
+
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Koha; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+# Suite 330, Boston, MA  02111-1307 USA
+
+# $Id$
+
+
+=head1 NAME
+
+histsearch.pl
+
+=head1 DESCRIPTION
+this script offer a interface to search among order.
+
+
+=head1 CGI PARAMETERS
+
+=over 4
+
+=item title
+if the script has to filter the results on title.
+
+=item author
+if the script has to filter the results on author.
+
+=item name
+if the script has to filter the results on supplier.
+
+=item fromplacedon
+to filter on started date.
+
+=item toplacedon
+to filter on ended date.
+
+=back
+
+=cut
+
 use strict;
 require Exporter;
 use CGI;
@@ -24,7 +74,7 @@ my ($template, $loggedinuser, $cookie)
 			     flagsrequired => {acquisition => 1},
 			     debug => 1,
 			     });
-my ($order_loop,$total_qty,$total_price,$total_qtyreceived)= &histsearch($title,$author,$name,$from_placed_on,$to_placed_on);
+my ($order_loop,$total_qty,$total_price,$total_qtyreceived)= &GetHistory($title,$author,$name,$from_placed_on,$to_placed_on);
 $template->param(suggestions_loop => $order_loop,
 				total_qty => $total_qty,
 				total_qtyreceived => $total_qtyreceived,
