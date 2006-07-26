bd85aad550b90cc22cb63764b85efe8647c65d87
toins <toins>
2006-07-26 09:57:33 +0000

    Code cleaned. POD added. Sub renamed.

--- acqui/lateorders.pl
+++ acqui/lateorders.pl
@@ -17,41 +17,68 @@
 
 # $Id$
 
+=head1 NAME
+
+lateorders.pl
+
+=head1 DESCRIPTION
+
+this script shows late orders for a specific supplier, branch and delay
+given on input arg.
+
+=head1 CGI PARAMETERS
+
+=over 4
+
+=item supplierid
+To know on which supplier this script have to display late order.
+
+=item delay
+To know the time boundary. Default value is 30 days.
+
+=item branch
+To know on which branch this script have to display late order.
+
+=back
+
+=cut
+
 use strict;
 use CGI;
-use C4::Acquisition;
+use C4::Bookseller;
 use C4::Auth;
 use C4::Koha;
 use C4::Output;
 use C4::Interface::CGI::Output;
 use C4::Context;
 use HTML::Template;
+use C4::Acquisition;
 
 my $query = new CGI;
 my ($template, $loggedinuser, $cookie)
-= get_template_and_user({template_name => "acqui/lateorders.tmpl",
+= get_template_and_user(
+                {template_name => "acqui/lateorders.tmpl",
 				query => $query,
 				type => "intranet",
 				authnotrequired => 0,
 				flagsrequired => {acquisition => 1},
 				debug => 1,
 				});
-# my $title = $query->param('title');
-# my $ISSN = $query->param('ISSN');
-# my @subscriptions = GetSubscriptions($title,$ISSN);
 
 my $supplierid = $query->param('supplierid');
 my $delay = $query->param('delay');
 my $branch = $query->param('branch');
 
-$delay =($delay?$delay:30);
+#default value for delay
+$delay = 30 unless $delay;
 
-my %supplierlist = getsupplierlistwithlateorders($delay,$branch);
+my %supplierlist = GetBooksellersWithLateOrders($delay,$branch);
 my @select_supplier;
 push @select_supplier,"";
 foreach my $supplierid (keys %supplierlist){
 	push @select_supplier, $supplierid;
 }
+
 my $CGIsupplier=CGI::scrolling_list( -name     => 'supplierid',
 			-values   => \@select_supplier,
 			-default  => $supplierid,
@@ -62,7 +89,8 @@ my $CGIsupplier=CGI::scrolling_list( -name     => 'supplierid',
 
 $template->param(Supplier=>$supplierlist{$supplierid}) if ($supplierid);
 
-my $branches = getbranches;
+my $branches = GetBranches;
+
 my @branchloop;
 foreach my $thisbranch (sort keys %$branches) {
 	my %row =(value => $thisbranch,
@@ -71,13 +99,15 @@ foreach my $thisbranch (sort keys %$branches) {
 	push @branchloop, \%row;
 }
 my $CGIbranch=CGI::scrolling_list( -name     => 'branch',
-				-values   => \@select_branches,
-				-labels   => \%select_branches,
+				-values   => \@branchloop,
+				-labels   => $branches,
 				-size     => 1,
  				-tabindex=>'',
 				-multiple => 0 );
 
-my ($count, @lateorders) = getlateorders($delay,$supplierid,$branch);
+my @lateorders = GetLateOrders($delay,$supplierid,$branch);
+my $count = scalar @lateorders;
+
 my $total;
 foreach my $lateorder (@lateorders){
 	$total+=$lateorder->{subtotal};
