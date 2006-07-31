a30329db0be59a062189abd3879ca455504642bd
toins <toins>
2006-07-31 14:30:45 +0000

    GPL & POD added,

--- acqui/suggestion-select.pl
+++ acqui/suggestion-select.pl
@@ -1,9 +1,83 @@
 #!/usr/bin/perl
+
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Koha; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+# Suite 330, Boston, MA  02111-1307 USA
+
+# $Id$
+
+=head1 NAME
+suggestion-select.pl
+
+=head1 DESCRIPTION
+this script allow to add an order from a existing suggestion.
+
+=head1 CGI PARAMETERS
+
+=over 4
+
+=item basketno
+the number of this basket.
+
+=item booksellerid
+the bookseller who sells this record.
+
+=item title
+the title of this record suggested.
+
+=item author
+the author of this suggestion.
+
+=item note
+this param allow to enter a note with this suggestion.
+
+=item copyrightdate
+the copyright date for this suggestion.
+
+=item publishercode
+
+=item volumedesc
+
+=item publicationyear
+the publication year of this record.
+
+=item place
+
+=item isbn
+the isbn of this suggestion.
+
+=item duplicateNumber
+is the biblionumber to put to the new suggestion.
+
+=item suggestionid
+the id of the suggestion to select.
+
+=item op
+can be equal to
+    * connectDuplicate :
+        then call to the function : ConnectSuggestionAndBiblio.
+        i.e set the biblionumber of this suggestion.
+    * else :
+        is the default value.
+=back
+
+=cut
+
 use strict;
 require Exporter;
 use CGI;
 use HTML::Template;
-
 use C4::Auth;       # get_template_and_user
 use C4::Interface::CGI::Output;
 use C4::Suggestions;
@@ -14,7 +88,6 @@ my $input = new CGI;
 
 my $basketno = $input->param('basketno');
 my $supplierid = $input->param('booksellerid');
-
 my $title = $input->param('title');
 my $author = $input->param('author');
 my $note = $input->param('note');
