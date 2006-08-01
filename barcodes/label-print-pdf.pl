f568274644b04a68f3559c9889f4fd55e8b88033
sushi <sushi>
2006-08-01 00:06:10 +0000

    general tidyup.

--- barcodes/label-print-pdf.pl
+++ barcodes/label-print-pdf.pl
@@ -11,21 +11,19 @@
 
 # the reason for this goofyness, it that i couldnt find a single perl package that handled both barcodes and decent text placement.
 
-use lib '/usr/local/opus-import/intranet/modules';
-use C4::Context("/etc/koha-opus-import.conf");
+#use lib '/usr/local/opus-import/intranet/modules';
+#use C4::Context("/etc/koha-opus-import.conf");
 
 use strict;
 use CGI;
 use C4::Labels;
 use C4::Auth;
-use C4::Bull;
 use C4::Output;
 use C4::Interface::CGI::Output;
 use C4::Context;
 use HTML::Template;
 use PDF::Reuse;
 use PDF::Reuse::Barcode;
-use PDF::Report;
 use POSIX;
 use Text::Wrap;
 
@@ -67,19 +65,19 @@ prMbox( $lowerLeftX, $lowerLeftY, $upperRightX, $upperRightY );
 prFont('courier');    # Just setting a font
 prFontSize(9);
 
-my $margin       = 36;
+my $margin           = 36;
 my $top_text_margin  = 10;
-my $left_text_margin  = 3;
-my $label_height = 90;
-my $spine_width  = 72;
-my $circ_width   = 207;
-my $colspace     = 27;
-my $x_pos_spine  = 36;
-my $x_pos_circ1  = 135;
-my $x_pos_circ2  = 369;
-my $pageheight   = 792;
-my $line_spacer  = 10;
-my $label_rows = 8;
+my $left_text_margin = 3;
+my $label_height     = 90;
+my $spine_width      = 72;
+my $circ_width       = 207;
+my $colspace         = 27;
+my $x_pos_spine      = 36;
+my $x_pos_circ1      = 135;
+my $x_pos_circ2      = 369;
+my $pageheight       = 792;
+my $line_spacer      = 10;
+my $label_rows       = 8;
 
 my $str;
 
@@ -98,7 +96,8 @@ my $rowspace         = 36;
 my $page_break_count = $startrow;
 my $codetype         = 'Code39';
 
-DrawBorder ( $lowerLeftX, $lowerLeftY, $upperRightX, $upperRightY );
+#do page border
+drawbox( $lowerLeftX, $lowerLeftY, $upperRightX, $upperRightY );
 
 my $item;
 
@@ -114,8 +113,7 @@ foreach $item (@resultsloop) {
     }
     if ( $printingtype eq 'barcode' || $printingtype eq 'both' ) {
 
-        warn
-"COUNT=$i2, PBREAKCNT=$page_break_count, X,Y POS x=$x_pos_circ1, y=$y_pos";
+        #warn "COUNT=$i2, PBREAKCNT=$page_break_count, X,Y POS x=$x_pos_circ1, y=$y_pos";
 
         build_circ_barcode( $x_pos_circ1, $y_pos, $item->{'barcode'},
             $conf_data->{'barcodetype'} );
@@ -126,7 +124,7 @@ foreach $item (@resultsloop) {
 
     #-----------------draw spine text
     if ( $printingtype eq 'spine' || $printingtype eq 'both' ) {
-        warn "PRINTTYPE = $printingtype";
+        #warn "PRINTTYPE = $printingtype";
 
         # add your printable fields manually in here
         my @fields =
@@ -139,7 +137,7 @@ foreach $item (@resultsloop) {
             # and the item record has some values for this field, display it.
             if ( $conf_data->{"$field"} && $item->{"$field"} ) {
 
-warn "CONF_TYPE = $field";
+                #warn "CONF_TYPE = $field";
 
                 # get the string
                 $str = $item->{"$field"};
@@ -160,14 +158,14 @@ warn "CONF_TYPE = $field";
                 # then loop for each string line
                 foreach my $str (@strings) {
 
-                    warn "HPOS ,  VPOS $hPos, $vPos ";
+                    #warn "HPOS ,  VPOS $hPos, $vPos ";
                     prText( $hPos, $vPos, $str );
                     $vPos = $vPos - $line_spacer;
                 }
             }    # if field is valid
         }    #foreach feild
     }
-        $y_pos = ( $y_pos - $label_height );
+    $y_pos = ( $y_pos - $label_height );
 
     #-----------------draw spine text
 
@@ -184,17 +182,4 @@ warn "CONF_TYPE = $field";
 prEnd();
 
 print $cgi->redirect("/intranet-tmpl/barcodes/new.pdf");
-
-sub DrawBorder {
-    my ( $llx, $lly, $urx, $ury ) = @_;
-    $str = "q\n";    # save the graphic state
-    $str .= "4 w\n";                       # border color red
-    $str .= "0.0 0.0 0.0  RG\n";           # border color red
-    $str .= "1 1 1 rg\n";                  # fill color blue
-    $str .= "$llx $lly $urx $ury re\n";    # a rectangle
-    $str .= "B\n";                         # fill (and a little more)
-    $str .= "Q\n";                         # save the graphic state
-    prAdd($str);
-
-}
 
