01d9a65f9cf71c86d0483d403d3989c16a3d865a
tipaul <tipaul>
2006-08-11 16:33:01 +0000

    adding menu-tools item

---
+++ tools/tools-home.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+use strict;
+use CGI;
+use C4::Auth;
+use C4::Output;
+use C4::Interface::CGI::Output;
+use C4::Database;
+use HTML::Template;
+
+my $query = new CGI;
+my ($template, $loggedinuser, $cookie)
+    = get_template_and_user({template_name => "tools/tools-home.tmpl",
+			     query => $query,
+			     type => "intranet",
+			     authnotrequired => 0,
+			     flagsrequired => {tools => 1},
+			     debug => 1,
+			     });
+
+output_html_with_http_headers $query, $cookie, $template->output;
