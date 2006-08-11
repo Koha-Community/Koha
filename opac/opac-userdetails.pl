a0b6741c9377a77fee4474f55b2b8cd2e76266d6
toins <toins>
2006-08-11 09:14:01 +0000

    Add a call to C4::Members & GPL added.

--- opac/opac-userdetails.pl
+++ opac/opac-userdetails.pl
@@ -1,4 +1,23 @@
 #!/usr/bin/perl
+
+# Copyright 2000-2002 Katipo Communications
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Koha; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+# Suite 330, Boston, MA  02111-1307 USA
+
+
 use strict;
 require Exporter;
 use CGI;
@@ -10,6 +29,7 @@ use C4::Search;
 use HTML::Template;
 use C4::Interface::CGI::Output;
 use C4::Date;
+use C4::Members;
 
 my $query = new CGI;
 my ($template, $borrowernumber, $cookie) 
