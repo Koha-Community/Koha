cc27713c1aff70c4a17182a17fe4dde633aae6de
toins <toins>
2006-12-08 11:24:58 +0000

    now enable to manage reviews from intranet.

--- reviews/reviewswaiting.pl
+++ reviews/reviewswaiting.pl
@@ -1,13 +1,29 @@
 #!/usr/bin/perl
 
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Koha; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+# Suite 330, Boston, MA  02111-1307 USA
+
 use strict;
 use CGI;
 use C4::Auth;
 use C4::Output;
 use C4::Interface::CGI::Output;
 use C4::Context;
-use HTML::Template;
 use C4::Review;
+use C4::Members;
+use C4::Biblio;
 
 my $query = new CGI;
 my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
@@ -32,6 +48,17 @@ elsif ( $op eq 'delete' ) {
 }
 
 my $reviews = getallreviews(0);
+
+foreach ( @$reviews ) {
+    my $borrowernumber = $_->{borrowernumber};
+    my $borrowerData   = GetMember('',$borrowernumber);
+    my $biblioData     = GetBiblioData($_->{biblionumber});
+    # setting some borrower info into this hash
+    $_->{bibliotitle} = $biblioData->{'title'};
+    $_->{surname}     = $borrowerData->{'surname'};
+    $_->{firstname}   = $borrowerData->{'firstname'};
+}
+
 $template->param( reviews => $reviews );
 
 output_html_with_http_headers $query, $cookie, $template->output;
