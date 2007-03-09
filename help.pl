49c66d7d4e8ea46f6b1bf531cd90986408fb41f9
tipaul <tipaul>
2007-03-09 15:26:29 +0000

    rel_3_0 moved to HEAD

--- help.pl
+++ help.pl
@@ -17,7 +17,7 @@
 # Koha; if not, write to the Free Software Foundation, Inc., 59 Temple Place,   
 # Suite 330, Boston, MA  02111-1307 USA
 
-use HTML::Template;
+
 use strict;
 require Exporter;
 use C4::Output;  # contains gettemplate
