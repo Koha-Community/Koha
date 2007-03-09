13f6840c71032c41c2d37c809c0eff698b442f05
rych <rych>
2007-03-09 20:33:59 +0000

    add mainpage

---
+++ mainpage.pl
@@ -0,0 +1,80 @@
+#!/usr/bin/perl
+
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Koha; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+# Suite 330, Boston, MA  02111-1307 USA
+#
+
+use strict;
+require Exporter;
+
+use C4::Output;    # contains gettemplate
+use C4::Interface::CGI::Output;
+use CGI;
+use C4::Auth;
+use C4::AuthoritiesMarc;
+use C4::Koha;
+use C4::NewsChannels;
+my $query     = new CGI;
+my $authtypes = getauthtypes;
+my @authtypesloop;
+
+foreach my $thisauthtype ( sort { $authtypes->{$a} <=> $authtypes->{$b} }
+    keys %$authtypes )
+{
+    my %row = (
+        value        => $thisauthtype,
+        authtypetext => $authtypes->{$thisauthtype}{'authtypetext'},
+    );
+    push @authtypesloop, \%row;
+}
+
+my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
+    {
+        template_name   => "intranet-main.tmpl",
+        query           => $query,
+        type            => "intranet",
+        authnotrequired => 0,
+        flagsrequired   => {
+    #        catalogue        => 1,
+    #        circulate        => 1,
+    #        parameters       => 1,
+    #        borrowers        => 1,
+    #        permissions      => 1,
+    #        reserveforothers => 1,
+    #        borrow           => 1,
+    #        reserveforself   => 1,
+    #        editcatalogue    => 1,
+    #        updatecharges    => 1,
+        },
+        debug => 1,
+    }
+);
+
+my $marc_p = C4::Context->boolean_preference("marc");
+
+$template->param(
+    NOTMARC       => !$marc_p,
+    authtypesloop => \@authtypesloop
+);
+
+my $all_koha_news = &GetNewsToDisplay("koha");
+my $koha_news_count = scalar @$all_koha_news;
+
+$template->param(
+    koha_news       => $all_koha_news,
+    koha_news_count => $koha_news_count
+);
+
+output_html_with_http_headers $query, $cookie, $template->output;
