1788e7c8706a0de2c37340b79537c929a5f039e2
rych <rych>
2007-03-16 16:56:48 +0000

    *** empty log message ***

---
+++ misc/sql-datas/en/optional/itemtypes_default.sql
@@ -0,0 +1,8 @@
+INSERT INTO `itemtypes` VALUES ('BKS', 'Books, Booklets, Workbooks',5,0,0,'','');
+INSERT INTO `itemtypes` VALUES ('CF', 'CD-ROMs, DVD-ROMs, General Online Resources',5,0,0,'','');
+INSERT INTO `itemtypes` VALUES ('SR', 'Audio Cassettes, CDs',5,0,0,'','');
+INSERT INTO `itemtypes` VALUES ('VR', 'DVDs, VHS',5,0,0,'','');
+INSERT INTO `itemtypes` VALUES ('AR', 'Models',5,0,0,'','');
+INSERT INTO `itemtypes` VALUES ('KT', 'Kits',5,0,0,'','');
+INSERT INTO `itemtypes` VALUES ('IR', 'Binders',5,0,0,'','');
+INSERT INTO `itemtypes` VALUES ('SER', 'Serials',5,0,0,'','');
