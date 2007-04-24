1ca8a9ceb8b347ffe2bea65f6e5484fb20da9354
hdl <hdl>
2007-04-24 13:56:06 +0000

    I forgot this script.

--- mainpage.pl
+++ mainpage.pl
@@ -19,9 +19,8 @@
 use strict;
 require Exporter;
 
-use C4::Output;    # contains gettemplate
-use C4::Interface::CGI::Output;
 use CGI;
+use C4::Output;
 use C4::Auth;
 use C4::AuthoritiesMarc;
 use C4::Koha;
