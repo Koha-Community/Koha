57cdaee5de4cfc9878d75d74be6cf395749608e7
hdl <hdl>
2007-07-20 07:49:06 +0000

    Putting order list into Template rather than setting this in a script. For translations.

--- opac/opac-search.pl
+++ opac/opac-search.pl
@@ -342,10 +342,8 @@ if ( $template_name eq "opac-advsearch.tmpl" ) {
 
     # load the sort_by options for the template
     my $sort_by      = $cgi->param('sort_by');
-    my $sort_by_loop = displaySortby($sort_by);
-    $template->param(
-        sort_by_loop => $sort_by_loop,
-     );
+    $sort_by=$1.($2 eq "lt"?'d':'a') if ($sort_by=~/1=(\d+) \&([a-z]+)\;/);
+    $template->param( sort_by => $sort_by);
 
     output_html_with_http_headers $cgi, $cookie, $template->output;
     exit;
@@ -366,8 +364,8 @@ my @sort_by;
 
 # load the sort_by options for the template
 my $sort_by      = $params->{'sort_by'};
-my $sort_by_loop = displaySortby($sort_by);
-$template->param( sort_by_loop => $sort_by_loop );
+$sort_by=~$1.($2 eq "lt"?'d':'a') if ($sort_by=~/1=(\d+) \&([a-z]+)\;/);
+$template->param( sort_by => $sort_by);
 
 #
 # Use the servers defined, or just search our local catalog(default)
