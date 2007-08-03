b97261cb76ee1ab370b3840bdafd5dcbb5039655
Chris Cormack <crc@arwen.metavore.com>
2007-08-03 17:31:16 -0500

    Perltidy opac-main.pl

--- opac/opac-main.pl
+++ opac/opac-main.pl
@@ -23,11 +23,11 @@ use CGI;
 use C4::Auth;    # get_template_and_user
 use C4::Output;
 use C4::BookShelves;
-use C4::Languages;           # getTranslatedLanguages
-use C4::Branch;         # GetBranches
-use C4::Members;        # GetMember
-use C4::NewsChannels;   # get_opac_news
-use C4::Acquisition;    # GetRecentAcqui
+use C4::Languages;       # getTranslatedLanguages
+use C4::Branch;          # GetBranches
+use C4::Members;         # GetMember
+use C4::NewsChannels;    # get_opac_news
+use C4::Acquisition;     # GetRecentAcqui
 
 my $input = new CGI;
 my $dbh   = C4::Context->dbh;
@@ -44,52 +44,59 @@ my ( $template, $borrowernumber, $cookie ) = get_template_and_user(
     }
 );
 
-if($limit) {
+if ($limit) {
     my $recentacquiloop = GetRecentAcqui($limit);
-#     warn Data::Dumper::Dumper($recentacquiloop);
-    $template->param(
-        recentacquiloop => $recentacquiloop,
-    );
+
+    #     warn Data::Dumper::Dumper($recentacquiloop);
+    $template->param( recentacquiloop => $recentacquiloop, );
 }
 
 # SearchMyLibraryFirst
-if (C4::Context->preference("SearchMyLibraryFirst")){
-  if (C4::Context->userenv){
-  my $branches = GetBranches();
-  my @branchloop;  
-  
-  foreach my $thisbranch (keys %$branches) {
-        my $selected = 1 if (C4::Context->userenv && ($thisbranch eq C4::Context->userenv->{branch}));
+if ( C4::Context->preference("SearchMyLibraryFirst") ) {
+    if ( C4::Context->userenv ) {
+        my $branches = GetBranches();
+        my @branchloop;
+
+        foreach my $thisbranch ( keys %$branches ) {
+            my $selected = 1
+              if ( C4::Context->userenv
+                && ( $thisbranch eq C4::Context->userenv->{branch} ) );
+
 #         warn $thisbranch;
 #         warn C4::Context->userenv;
 #         warn C4::Context->userenv->{branch};
 #         warn " => ".C4::Context->userenv && ($thisbranch eq C4::Context->userenv->{branch});
-        my %row =(value => $thisbranch,
-                                selected => $selected,
-                                branchname => $branches->{$thisbranch}->{'branchname'},
-                        );
-        push @branchloop, \%row;
-  }
-    $template->param("mylibraryfirst"=>1,branchloop=>\@branchloop);
-  } else {
-    $template->param("mylibraryfirst"=>0)
-   }
+            my %row = (
+                value      => $thisbranch,
+                selected   => $selected,
+                branchname => $branches->{$thisbranch}->{'branchname'},
+            );
+            push @branchloop, \%row;
+        }
+        $template->param( "mylibraryfirst" => 1, branchloop => \@branchloop );
+    }
+    else {
+        $template->param( "mylibraryfirst" => 0 );
+    }
 }
 
-    my $borrower = GetMember( $borrowernumber,'borrowernumber' );
+my $borrower = GetMember( $borrowernumber, 'borrowernumber' );
 my @languages;
-my $counter = 0;
+my $counter   = 0;
 my $langavail = getTranslatedLanguages('opac');
-foreach my $language ( @$langavail ) {
-    #   next if $currently_selected_languages->{$language};
-	#   FIXME: could incorporate language_name and language_locale_name for better display
-    push @languages, { language => $language->{'language_code'}, counter => $counter };
+foreach my $language (@$langavail) {
+
+#   next if $currently_selected_languages->{$language};
+#   FIXME: could incorporate language_name and language_locale_name for better display
+    push @languages,
+      { language => $language->{'language_code'}, counter => $counter };
     $counter++;
 }
 
 # Template params
 if ( $counter > 1 ) {
-    $template->param(languages => \@languages) if C4::Context->preference('opaclanguagesdisplay');
+    $template->param( languages => \@languages )
+      if C4::Context->preference('opaclanguagesdisplay');
 }
 
 $template->param(
@@ -99,10 +106,10 @@ $template->param(
 
 # display news
 # use cookie setting for language, bug default to syspref if it's not set
-my $news_lang = $input->cookie('KohaOpacLanguage') || C4::Context->preference('opaclanguages');
-my  $all_koha_news  = &GetNewsToDisplay( $news_lang );
+my $news_lang = $input->cookie('KohaOpacLanguage')
+  || C4::Context->preference('opaclanguages');
+my $all_koha_news   = &GetNewsToDisplay($news_lang);
 my $koha_news_count = scalar @$all_koha_news;
-
 
 $template->param(
     koha_news       => $all_koha_news,
