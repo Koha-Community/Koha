f197e25ad9e877a0de4994473d69b02a62b1dc4f
Paul POULAIN <paul@koha-fr.org>
2007-08-08 11:59:50 +0200

    wrong redirect link fixed
    Signed-off-by: Chris Cormack <crc@liblime.com>

--- admin/aqbookfund.pl
+++ admin/aqbookfund.pl
@@ -162,7 +162,7 @@ elsif ($op eq 'add_validate') {
             $input->param('branchcode')||''
         );
     }
-    $input->redirect('aqbookfund.pl');
+    $input->redirect('/cgi-bin/koha/aqbookfund.pl');
 # END $OP eq ADD_VALIDATE
 }
 ################## DELETE_CONFIRM ##################################
