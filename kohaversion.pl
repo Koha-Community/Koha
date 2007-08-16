f620a771cb7e17761430431e3fbb123785766b48
Paul POULAIN <paul@koha-fr.org>
2007-08-16 12:28:49 +0200

    Adding KOHAVERSION management.
    (adding forgotten kohaversion.pl)
    
    Signed-off-by: Chris Cormack <crc@liblime.com>

---
+++ kohaversion.pl
@@ -0,0 +1,14 @@
+# the next koha public release version number;
+# the kohaversion is divided in 4 parts :
+# - #1 : the major number. 3 atm
+# - #2 : the functionnal release. 00 atm
+# - #3 : the subnumber, moves only on a public release
+# - #4 : the developper version. The 4th number is the database subversion. 
+#        used by developpers when the database changes. updatedatabase take care of the changes itself
+#        and is automatically called by Auth.pm when needed.
+
+sub kohaversion {
+    return "3.00.00.001";
+}
+
+1;
