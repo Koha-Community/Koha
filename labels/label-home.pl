112faa65c939780ff4f134b24d2e63f2fb54a241
Paul POULAIN <paul@koha-fr.org>
2007-09-03 10:20:41 +0200

    commenting Smart::Comments
    Signed-off-by: Chris Cormack <crc@liblime.com>

--- labels/label-home.pl
+++ labels/label-home.pl
@@ -9,7 +9,7 @@ use C4::Output;
 use C4::Context;
 use HTML::Template;
 
-use Smart::Comments;
+# use Smart::Comments;
 
 my $query = new CGI;
 my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
