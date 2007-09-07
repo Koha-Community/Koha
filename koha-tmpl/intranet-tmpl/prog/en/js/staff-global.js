e817f5d20380aac6df547b28b59107c8b361733b
Owen Leonard <oleonard@myacpl.org>
2007-09-07 14:04:28 -0500

    New basic page structure implemented for circ pages: global main menu in the header, resident search box and logo, followed by breadcrumb navigation. Added circ-menu.inc for left-hand sidebar menu (needs revision), circ-search.inc which defines the resident search box for circ pages, additions to staff-global.js to handle tab-switching in search box.
    Signed-off-by: Chris Cormack <crc@liblime.com>

--- koha-tmpl/intranet-tmpl/prog/en/js/staff-global.js
+++ koha-tmpl/intranet-tmpl/prog/en/js/staff-global.js
@@ -3,4 +3,26 @@
  $(document).ready(function() {
  	$(".focus").focus();
  });
+ 
+ $(document).ready(function() {
+	$('a#showCatSearch').click(function() {
+		$('#catalog_search').show();
+		$('#circ_search').hide();
+		$('#showCircSearch').parents("li").addClass('off').removeClass('on');
+		$('#showCatSearch').parents("li").addClass('on').removeClass('off');
+		$('#search-form').val($('#findborrower').val()).focus();
+		$('#residenttip').html("Enter search keywords:");
+		return false;
+	});
+	
+	$('a#showCircSearch').click(function() {
+		$('#circ_search').show();
+		$('#catalog_search').hide();
+		$('#showCatSearch').parents("li").addClass('off').removeClass('on');
+		$('#showCircSearch').parents("li").addClass('on').removeClass('off');
+		$('#findborrower').val($('#search-form').val()).focus();
+		$('#residenttip').html("Enter patron card number or partial name:");
+		return false;
+	});
+});
 
