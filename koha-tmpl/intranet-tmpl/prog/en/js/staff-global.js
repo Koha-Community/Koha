bccf46fade416cbc908ac294d96ea534b1a73a3d
Owen Leonard <oleonard@myacpl.org>
2007-09-10 15:08:29 -0500

    More work on resident search, adding breadcrumbs and resident search to member pages.
    Signed-off-by: Chris Cormack <crc@liblime.com>

--- koha-tmpl/intranet-tmpl/prog/en/js/staff-global.js
+++ koha-tmpl/intranet-tmpl/prog/en/js/staff-global.js
@@ -8,7 +8,9 @@
 	$('a#showCatSearch').click(function() {
 		$('#catalog_search').show();
 		$('#circ_search').hide();
+		$('#patron_search').hide();
 		$('#showCircSearch').parents("li").addClass('off').removeClass('on');
+		$('#showPatronSearch').parents("li").addClass('off').removeClass('on');
 		$('#showCatSearch').parents("li").addClass('on').removeClass('off');
 		$('#search-form').val($('#findborrower').val()).focus();
 		$('#residenttip').html("Enter search keywords:");
@@ -18,11 +20,26 @@
 	$('a#showCircSearch').click(function() {
 		$('#circ_search').show();
 		$('#catalog_search').hide();
+		$('#patron_search').hide();
 		$('#showCatSearch').parents("li").addClass('off').removeClass('on');
+		$('#showPatronSearch').parents("li").addClass('off').removeClass('on');
 		$('#showCircSearch').parents("li").addClass('on').removeClass('off');
 		$('#findborrower').val($('#search-form').val()).focus();
 		$('#residenttip').html("Enter patron card number or partial name:");
 		return false;
 	});
-});
+	
+		$('a#showPatronSearch').click(function() {
+		$('#patron_search').show();
+		$('#catalog_search').hide();
+		$('#circ_search').hide();
+		$('#showCircSearch').parents("li").addClass('off').removeClass('on');
+		$('#showCatSearch').parents("li").addClass('off').removeClass('on');
+		$('#showPatronSearch').parents("li").addClass('on').removeClass('off');
+		$('#findborrower').val($('#search-form').val()).focus();
+		$('#residenttip').html("Enter patron card number or partial name:");
+		return false;
+	});
+	
+ });
 
