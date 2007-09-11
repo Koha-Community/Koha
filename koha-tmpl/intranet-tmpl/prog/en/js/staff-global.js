d5584e103037b5fa2baa6fb2a6db98425eba474f
Owen Leonard <oleonard@myacpl.org>
2007-09-11 15:02:18 -0500

    Trying new jquery tabs system. Appearance may not be quite right
    Signed-off-by: Chris Cormack <crc@liblime.com>

--- koha-tmpl/intranet-tmpl/prog/en/js/staff-global.js
+++ koha-tmpl/intranet-tmpl/prog/en/js/staff-global.js
@@ -2,44 +2,10 @@
 
  $(document).ready(function() {
  	$(".focus").focus();
+	$('#toplevelmenu').clickMenu(); 
+	$('#header_search').tabs();
+	
+	
  });
  
- $(document).ready(function() {
-	$('a#showCatSearch').click(function() {
-		$('#catalog_search').show();
-		$('#circ_search').hide();
-		$('#patron_search').hide();
-		$('#showCircSearch').parents("li").addClass('off').removeClass('on');
-		$('#showPatronSearch').parents("li").addClass('off').removeClass('on');
-		$('#showCatSearch').parents("li").addClass('on').removeClass('off');
-		$('#search-form').val($('#findborrower').val()).focus();
-		$('#residenttip').html("Enter search keywords:");
-		return false;
-	});
-	
-	$('a#showCircSearch').click(function() {
-		$('#circ_search').show();
-		$('#catalog_search').hide();
-		$('#patron_search').hide();
-		$('#showCatSearch').parents("li").addClass('off').removeClass('on');
-		$('#showPatronSearch').parents("li").addClass('off').removeClass('on');
-		$('#showCircSearch').parents("li").addClass('on').removeClass('off');
-		$('#findborrower').val($('#search-form').val()).focus();
-		$('#residenttip').html("Enter patron card number or partial name:");
-		return false;
-	});
-	
-		$('a#showPatronSearch').click(function() {
-		$('#patron_search').show();
-		$('#catalog_search').hide();
-		$('#circ_search').hide();
-		$('#showCircSearch').parents("li").addClass('off').removeClass('on');
-		$('#showCatSearch').parents("li").addClass('off').removeClass('on');
-		$('#showPatronSearch').parents("li").addClass('on').removeClass('off');
-		$('#findborrower').val($('#search-form').val()).focus();
-		$('#residenttip').html("Enter patron card number or partial name:");
-		return false;
-	});
-	
- });
 
