26664664074e6698f6816c03fd01fc512381649a
Owen Leonard <oleonard@myacpl.org>
2007-09-24 15:17:00 -0500

    Corrections, and first test of Circ toolbar
    Signed-off-by: Chris Cormack <crc@liblime.com>

--- koha-tmpl/intranet-tmpl/prog/en/js/staff-global.js
+++ koha-tmpl/intranet-tmpl/prog/en/js/staff-global.js
@@ -3,9 +3,11 @@
  $(document).ready(function() {
  	$(".focus").focus();
 	$('#toplevelmenu').clickMenu(); 
-	$('#header_search').tabs();
-	
-	
+	$('#header_search').tabs({
+    onShow: function() {
+        alert($('#header_search').children("div:visible").children("input:eq(0)").focus());
+    }
+});
  });
  
 
