5cb229626bb530a8474174e6cf92f2d9319fa5ca
Joshua Ferraro <jmf@liblime.com>
2007-10-01 17:29:48 -0500

    sample label data
    Signed-off-by: Chris Cormack <crc@liblime.com>

--- installer/data/en/optional/sample_labels.sql
+++ installer/data/en/optional/sample_labels.sql
@@ -8,4 +8,8 @@ INSERT INTO `labels_templates` VALUES
 (5,'Avery 8163','2rows x 5 rows',8.5,11,4,2,0.5,0.17,2,5,0.2,0.01,NULL,'INCH',11),
 (6,'cards','Avery 5160 | 1 x 2-5/8 : 1 x 2-5/8\"  [3x10] : equivalent: Gaylord JD-ML3000',8.5,11,2.75,1.05,0.25,0,3,10,0.2,0.01,NULL,'INCH',8);
 UNLOCK TABLES; 
-
+LOCK TABLES `labels_conf` WRITE;
+/*!40000 ALTER TABLE `labels_conf` DISABLE KEYS */;
+INSERT INTO `labels_conf` VALUES (5,'CODE39',2,3,0,0,0,0,4,1,0,0,1,'BIBBAR','biblio and barcode',1,NULL,NULL,0),(6,'CODE39',2,0,0,0,3,4,0,1,0,3,1,'BAR','alternating',1,1,NULL,0);
+/*!40000 ALTER TABLE `labels_conf` ENABLE KEYS */;
+UNLOCK TABLES;
