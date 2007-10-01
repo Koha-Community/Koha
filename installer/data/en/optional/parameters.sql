ff5cd2a0e8119536fd2deed6891741597d54911a
Joshua Ferraro <jmf@liblime.com>
2007-10-01 17:29:47 -0500

    more marc21 sample data
    Signed-off-by: Chris Cormack <crc@liblime.com>

--- installer/data/en/optional/parameters.sql
+++ installer/data/en/optional/parameters.sql
@@ -1,8 +1,2 @@
 INSERT INTO `currency` (currency, rate) VALUES ('USD', 1.0);
 
-INSERT INTO `z3950servers` (`host`, `port`, `db`, `userid`, `password`, `name`, `id`, `checked`, `rank`, `syntax`) VALUES
-('z3950.loc.gov',7090,'Voyager','','','LIBRARY OF CONGRESS',1,1,2,'USMARC'),
-('amicus.collectionscanada.ca',210,'NL','','','AMICUS',2,1,3,'USMARC'),
-('66.213.78.76',9999,'NPLKoha','','','NELSONVILLE PUBLIC LIBRARY',3,0,4,'USMARC');
-
-
