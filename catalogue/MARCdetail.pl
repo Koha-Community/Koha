9dd4f305f1f3b9b8d238f026effa765fe7fdef06
Paul POULAIN <paul@koha-fr.org>
2007-10-03 19:00:56 +0200

    bugfix $itemtype => $value
    Signed-off-by: Chris Cormack <crc@liblime.com>

--- catalogue/MARCdetail.pl
+++ catalogue/MARCdetail.pl
@@ -69,7 +69,7 @@ sub get_authorised_value_desc ($$$$$$) {
     #---- itemtypes
     if ( $tagslib->{$tag}->{$subfield}->{'authorised_value'} eq "itemtypes" ) {
 
-                  my $itemtypedef = getitemtypeinfo($itemtype);
+                  my $itemtypedef = getitemtypeinfo($value);
               return $itemtypedef->{description};
     }
 
