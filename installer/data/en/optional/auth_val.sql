cac444cedce8ece33012127278aeae60831ef4c0
Joshua Ferraro <jmf@liblime.com>
2007-10-06 16:08:38 -0500

    improvements to english auth values
    Signed-off-by: Chris Cormack <crc@liblime.com>
    Signed-off-by: Joshua Ferraro <jmf@liblime.com>

--- installer/data/en/optional/auth_val.sql
+++ installer/data/en/optional/auth_val.sql
@@ -1,16 +1,32 @@
-INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('ITEMLOST','0','Available');
+-- availability statuses
+INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('ITEMLOST','0','');
 INSERT INTO `authorised_values`  (category, authorised_value, lib) VALUES ('ITEMLOST','2','Long Overdue (Lost)');
 INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('ITEMLOST','1','Lost');
 INSERT INTO `authorised_values`  (category, authorised_value, lib ) VALUES ('ITEMLOST','3','Lost and Paid For');
-INSERT INTO `authorised_values`  (category, authorised_value, lib )VALUES ('ITEMLOST','4','Missing');
-INSERT INTO `authorised_values`  (category, authorised_value, lib) VALUES ('BINDING','0','NOT Damaged');
-INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('BINDING','1','Damaged');
+INSERT INTO `authorised_values`  (category, authorised_value, lib )VALUES ('ITEMLOST','4','Missing in Inventory');
+
+-- damaged status of an item
+INSERT INTO `authorised_values`  (category, authorised_value, lib) VALUES ('DAMAGED','0','');
+INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('DAMAGED','1','Damaged');
+
+-- location qualification for an item, departments are linked by default to items.location
+INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('SHELF_LOC','0','');
+INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('SHELF_LOC','1','Reference');
+INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('SHELF_LOC','2','Fiction');
+INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('SHELF_LOC','3','Biography');
+INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('SHELF_LOC','4','Media');
+INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('SHELF_LOC','4','New Book Shelf');
+
+-- location qualification for an item, linked to items.stack
+INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('STACK','0','');
+INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('STACK','1','Special Collection');
+
+-- loanability status of an item, linked to items.notforloan
 INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('NOTFORLOAN','-1','Ordered');
-INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('NOTFORLOAN','0','For Loan');
+INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('NOTFORLOAN','0','');
 INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('NOTFORLOAN','1','Not For Loan');
 INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('NOTFORLOAN','2','Staff Collection');
-INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('DPT','0','Reference');
-INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('DPT','1','Stacks');
-INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('DPT','2','Fiction');
-INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('DPT','3','Biography');
-INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('DPT','4','Media');
+
+-- restricted status of an item, linked to items.restricted
+INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('RESTRICTED','0','');
+INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('RESTRICTED','1','Restricted Access');
