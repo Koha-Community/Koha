158033a4b58944212c5290aa82caa8687b678354
Joshua Ferraro <jmf@liblime.com>
2007-10-29 16:45:11 -0500

    adding sample data
    Signed-off-by: Chris Cormack <crc@liblime.com>
    Signed-off-by: Joshua Ferraro <jmf@liblime.com>

---
+++ installer/data/en/optional/sample_budgets.sql
@@ -0,0 +1,2 @@
+INSERT INTO `aqbudget` VALUES ('CHILD','2007-01-01','2007-12-31','5000.00',1,''),('GEN','2007-01-01','2007-12-31','20000.00',2,''),('REF','2007-01-01','2007-12-31','5000.00',3,'');
+INSERT INTO `aqbookfund` VALUES ('CHILD','Children\'s Materials',NULL,''),('GEN','General Stacks',NULL,''),('REF','Reference Materials',NULL,'');
---
+++ installer/data/en/optional/sample_holidays.sql
@@ -0,0 +1,4 @@
+INSERT INTO `repeatable_holidays` VALUES 
+(2,'',0,NULL,NULL,'','Sundays'),
+(3,'',NULL,1,1,'','New Year\'s Day'),
+(4,'',NULL,25,12,'','Christmas');
