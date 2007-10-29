d314f5d2ed360a17860c6dd0ceec69af27582841
Joshua Ferraro <jmf@liblime.com>
2007-10-29 16:45:03 -0500

    adding itemtype images to sample data
    Signed-off-by: Chris Cormack <crc@liblime.com>
    Signed-off-by: Joshua Ferraro <jmf@liblime.com>

--- installer/data/en/optional/sample_itemtypes.sql
+++ installer/data/en/optional/sample_itemtypes.sql
@@ -1,8 +1,8 @@
-INSERT INTO `itemtypes` VALUES ('BKS', 'Books, Booklets, Workbooks',5,0,0,'','');
-INSERT INTO `itemtypes` VALUES ('CF', 'CD-ROMs, DVD-ROMs, General Online Resources',5,0,0,'','');
-INSERT INTO `itemtypes` VALUES ('SR', 'Audio Cassettes, CDs',5,0,0,'','');
-INSERT INTO `itemtypes` VALUES ('VR', 'DVDs, VHS',5,0,0,'','');
-INSERT INTO `itemtypes` VALUES ('AR', 'Models',5,0,0,'','');
-INSERT INTO `itemtypes` VALUES ('KT', 'Kits',5,0,0,'','');
-INSERT INTO `itemtypes` VALUES ('IR', 'Binders',5,0,0,'','');
-INSERT INTO `itemtypes` VALUES ('SER', 'Serials',5,0,0,'','');
+INSERT INTO `itemtypes` VALUES ('BKS', 'Books, Booklets, Workbooks',5,0,0,'Non-fiction.gif','');
+INSERT INTO `itemtypes` VALUES ('CF', 'CD-ROMs, DVD-ROMs, General Online Resources',5,0,0,'CDM.gif','');
+INSERT INTO `itemtypes` VALUES ('SR', 'Audio Cassettes, CDs',5,0,0,'AVNF.gif','');
+INSERT INTO `itemtypes` VALUES ('VR', 'DVDs, VHS',5,0,0,'Video disk.gif','');
+INSERT INTO `itemtypes` VALUES ('AR', 'Models',5,0,1,'EASY.gif','');
+INSERT INTO `itemtypes` VALUES ('KT', 'Kits',5,0,0,'Kit.gif','');
+INSERT INTO `itemtypes` VALUES ('IR', 'Binders',5,0,0,'Faculty Course Materials.gif','');
+INSERT INTO `itemtypes` VALUES ('SER', 'Serials',5,0,0,'MZ.gif','');
