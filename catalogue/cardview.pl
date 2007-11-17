df38d5c66f13088caacb78165196faed3a7761c3
Joshua Ferraro <jmf@liblime.com>
2007-11-16 21:40:39 -0600

    fixing facets, add cardview (XSLT), adding dependencies, save Record
    Signed-off-by: Chris Cormack <crc@liblime.com>
    Signed-off-by: Joshua Ferraro <jmf@liblime.com>

---
+++ catalogue/cardview.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl
+use strict;
+require Exporter;
+use CGI;
+use XML::LibXSLT;
+use XML::LibXML;
+use C4::Koha;
+use C4::Auth;
+use C4::Biblio;
+use C4::Languages qw(getTranslatedLanguages);
+
+my $query=new CGI;
+my ($template, $borrowernumber, $cookie) 
+    = get_template_and_user({template_name => "opac-detail.tmpl",
+			     query => $query,
+			     type => "opac",
+			     authnotrequired => 1,
+			     flagsrequired => {borrow => 1},
+			     });
+
+# load the languages
+my @languages_options = getTranslatedLanguages($query);
+my $languages_count = @languages_options;
+if($languages_count > 1){
+        $template->param(languages => \@languages_options);
+}
+my $biblionumber=$query->param('biblionumber');
+$template->param(biblionumber => $biblionumber);
+
+# grab the XML, run it through our stylesheet, push it out to the browser
+my $xmlrecord = GetXmlBiblio($biblionumber);
+#my $xslfile = "/home/kohacat/etc/xslt/MARC21slim2HTML.xsl";
+#my $xslfile = "/home/kohacat/etc/xslt/MARC21slim2English.xsl";
+my $xslfile = C4::Context->config('intranetdir')."/koha-tmpl/intranet-tmpl/prog/en/xslt/compact.xsl";
+my $parser = XML::LibXML->new();
+my $xslt = XML::LibXSLT->new();
+my $source = $parser->parse_string($xmlrecord);
+my $style_doc = $parser->parse_file($xslfile);
+my $stylesheet = $xslt->parse_stylesheet($style_doc);
+my $results = $stylesheet->transform($source);
+my $newxmlrecord = $stylesheet->output_string($results);
+print "Content-type: text/html\n\n";
+print $newxmlrecord;
---
+++ catalogue/export.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/perl
+use HTML::Template;
+use strict;
+require Exporter;
+use C4::Record;
+use C4::Auth;
+use C4::Output;
+use C4::Biblio;
+use CGI;
+use C4::Auth;
+
+my $query = new CGI;
+my $op=$query->param("op");
+my $format=$query->param("format");
+if ($op eq "export") {
+	my $biblionumber = $query->param("bib");
+	my $dbh=C4::Context->dbh;
+	my $sth;
+	if ($biblionumber) {
+		$sth=$dbh->prepare("SELECT marc FROM biblioitems WHERE biblionumber =?");
+		$sth->execute($biblionumber);
+	}
+	while (my ($marc) = $sth->fetchrow) {
+		if ($marc){
+
+			if ($format =~ /marcxml/) {
+				$marc = marc2marcxml($marc);
+			}
+			elsif ($format=~ /mods/) {
+				$marc = marc2modsxml($marc);
+			}
+			elsif ($format =~ /dc/) {
+				my $error;
+				($error,$marc) = marc2dcxml($marc,1);
+				$format = "dublin-core.xml";
+			}
+			elsif ($format =~ /marc8/) {
+				$marc = changeEncoding($marc,"MARC","MARC21","MARC-8");
+				$marc = $marc->as_usmarc();
+			}
+			elsif ($format =~ /utf8/) {
+				#default
+			}
+			print $query->header(
+				-type => 'application/octet-stream',
+                -attachment=>"bib-$biblionumber.$format");
+			print $marc;
+		}
+	}
+}
