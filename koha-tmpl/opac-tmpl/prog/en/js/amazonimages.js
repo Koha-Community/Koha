71578b5dcf7d4a6bee7bc5b24a322f2b7c82cc9f
Owen Leonard <oleonard@myacpl.org>
2007-11-26 14:59:56 -0600

    A few minor changes: Tweaking amazon image js to try to fix spotty behavior (not very successfully), adding "renew all" button to opac-user, though it doesn't actually work yet.
    Signed-off-by: Joshua Ferraro <jmf@liblime.com>

--- koha-tmpl/opac-tmpl/prog/en/js/amazonimages.js
+++ koha-tmpl/opac-tmpl/prog/en/js/amazonimages.js
@@ -1,15 +1,14 @@
 // http://www.oreillynet.com/pub/a/javascript/2003/10/21/amazonhacks.html
 function verify_images() {
-    for (var i = 0; i < document.images.length; i++) {
-        img = document.images[i];
-        if ((img.src.indexOf('images.amazon.com') >= 0) || (img.src.indexOf('g-images.amazon.com') >=0)) {
-            w = img.width;
-            h = img.height;
+	$("img").each(function(i){
+	       if ((this.src.indexOf('images.amazon.com') >= 0) || (this.src.indexOf('g-images.amazon.com') >=0)) {
+            w = this.width;
+            h = this.height;
             if ((w == 1) || (h == 1)) {
-                img.src = 'http://g-images.amazon.com/images/G/01/x-site/icons/no-img-sm.gif';
-            } else if ((img.complete != null) && (!img.complete)) {
-                img.src = 'http://g-images.amazon.com/images/G/01/x-site/icons/no-img-sm.gif';
+                this.src = 'http://g-images.amazon.com/images/G/01/x-site/icons/no-img-sm.gif';
+            } else if ((this.complete != null) && (!this.complete)) {
+                this.src = 'http://g-images.amazon.com/images/G/01/x-site/icons/no-img-sm.gif';
             }
         }
-    }
-}
+		});
+		}
