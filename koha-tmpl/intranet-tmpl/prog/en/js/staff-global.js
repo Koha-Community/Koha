b72a0d0653a2f5bade544bde15df9a8795107f36
Owen Leonard <oleonard@myacpl.org>
2007-12-04 15:00:07 -0600

    Changes to image-load check (used for patron images and amazon book covers) to tweak when the script checked for loaded images (now window.load() instead of document.ready())
    Signed-off-by: Chris Cormack <crc@liblime.com>
    Signed-off-by: Joshua Ferraro <jmf@liblime.com>

--- koha-tmpl/intranet-tmpl/prog/en/js/staff-global.js
+++ koha-tmpl/intranet-tmpl/prog/en/js/staff-global.js
@@ -16,3 +16,17 @@ function _(s) { return s } // dummy function for gettext
  });
  
 
+// http://www.oreillynet.com/pub/a/javascript/2003/10/21/amazonhacks.html
+function verify_patron_images() {
+    for (var i = 0; i < document.images.length; i++) {
+        img = document.images[i];
+        if ((img.src.indexOf('patronimages') >= 0)) {
+			w = img.width;
+            h = img.height;
+     if ((w == 0) && (h == 0) || ((img.complete != null) && (!img.complete))) {
+               img.src = '/intranet-tmpl/prog/img/patron-blank.png';
+			}
+        }
+    }
+}
+
