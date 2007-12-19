3a8512681325cc79b96b07e511262d0f57361d0b
Chris Nighswonger <cnighswonger@foundations.edu>
2007-12-19 12:29:07 -0600

    Removing white spaces from directory and filenames as they restrict portability across OS platforms.
    Signed-off-by: Chris Cormack <crc@liblime.com>
    Signed-off-by: Joshua Ferraro <jmf@liblime.com>

--- installer/data/mysql/en/optional/sample_itemtypes.sql
+++ installer/data/mysql/en/optional/sample_itemtypes.sql
@@ -4,5 +4,5 @@ INSERT INTO `itemtypes` VALUES ('SR', 'Audio Cassettes, CDs',5,0,0,'AVNF.gif',''
 INSERT INTO `itemtypes` VALUES ('VR', 'DVDs, VHS',5,0,0,'Video-disc.gif','');
 INSERT INTO `itemtypes` VALUES ('AR', 'Models',5,0,1,'EASY.gif','');
 INSERT INTO `itemtypes` VALUES ('KT', 'Kits',5,0,0,'KIT.gif','');
-INSERT INTO `itemtypes` VALUES ('IR', 'Binders',5,0,0,'Faculty Course Materials.gif','');
+INSERT INTO `itemtypes` VALUES ('IR', 'Binders',5,0,0,'Faculty-Course-Materials.gif','');
 INSERT INTO `itemtypes` VALUES ('SER', 'Serials',5,0,0,'MZ.gif','');
