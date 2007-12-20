9d23361f244a845ed4e09c37d7dc1d4a098fa666
Owen Leonard <oleonard@myacpl.org>
2007-12-19 20:28:12 -0600

    Even more applying standard dialog styles to confirmation messages. Also fixing alternating table row colors in stopwords.pl
    Signed-off-by: Chris Cormack <crc@liblime.com>
    Signed-off-by: Joshua Ferraro <jmf@liblime.com>

--- admin/stopwords.pl
+++ admin/stopwords.pl
@@ -136,12 +136,12 @@ if ($op eq 'add_form') {
 	my $env;
 	my ($count,$results)=StringSearch($env,$searchfield,'web');
 	my @loop;
-	my $toggle = 'white';
+	my $toggle = 0;
 	for (my $i=$offset; $i < ($offset+$pagesize<$count?$offset+$pagesize:$count); $i++){
 		my %row = (word => $results->[$i]{'word'},
 			   toggle => $toggle);
 		push @loop, \%row;
-		$toggle = ($toggle eq 'white') ? '#ffffcc' : 'white' ;
+		$toggle = ($toggle eq 0) ? 1 : 0 ;
 	}
 	$template->param(loop => \@loop);
 
