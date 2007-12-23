ba7287c03ab13665d532deaa3c2ba175627246a6
Joshua Ferraro <jmf@liblime.com>
2007-12-23 16:46:31 -0500

    nomenclature cleanup in the OPAC detail and user pages
    Signed-off-by: Joshua Ferraro <jmf@liblime.com>

--- opac/opac-detail.pl
+++ opac/opac-detail.pl
@@ -26,6 +26,7 @@ use C4::Koha;
 use C4::Serials;    #uses getsubscriptionfrom biblionumber
 use C4::Output;
 use C4::Biblio;
+use C4::Dates qw/format_date/;
 use C4::XISBN qw(get_xisbns get_biblio_from_xisbn);
 use C4::Amazon;
 use C4::Review;
@@ -125,6 +126,7 @@ foreach ( @$reviews ) {
     $_->{title}     = $borrowerData->{'title'};
     $_->{surname}   = $borrowerData->{'surname'};
     $_->{firstname} = $borrowerData->{'firstname'};
+    $_->{datereviewed} = format_date($_->{datereviewed});
 }
 
 $template->param(
