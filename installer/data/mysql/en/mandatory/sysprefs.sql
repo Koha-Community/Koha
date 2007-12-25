ab8a6af819010a464cec45f8e9558560816a6678
Joshua Ferraro <jmf@liblime.com>
2007-12-25 13:50:27 -0500

    removing unused ReadingHistory syspref
    Signed-off-by: Joshua Ferraro <jmf@liblime.com>

--- installer/data/mysql/en/mandatory/sysprefs.sql
+++ installer/data/mysql/en/mandatory/sysprefs.sql
@@ -83,7 +83,6 @@ INSERT INTO `systempreferences` (variable,value,explanation,options,type) VALUES
 INSERT INTO `systempreferences` (variable,value,explanation,options,type) VALUES('opacuserlogin','1','Enable or disable display of user login features',NULL,'YesNo');
 INSERT INTO `systempreferences` (variable,value,explanation,options,type) VALUES('patronimages','0','Enable patron images for the Staff Client; to enable, specify a file extension (e.g., png)','','Free');
 INSERT INTO `systempreferences` (variable,value,explanation,options,type) VALUES('printcirculationslips','1','If ON, enable printing circulation receipts','','free');
-INSERT INTO `systempreferences` (variable,value,explanation,options,type) VALUES('ReadingHistory','1','If ON, enables circulation history','','YesNo');
 INSERT INTO `systempreferences` (variable,value,explanation,options,type) VALUES('RequestOnOpac','1','If ON, globally enables patron holds on OPAC',NULL,'YesNo');
 INSERT INTO `systempreferences` (variable,value,explanation,options,type) VALUES('ReservesMaxPickUpDelay','7','Define the Maximum delay to pick up an item on hold','','free');
 INSERT INTO `systempreferences` (variable,value,explanation,options,type) VALUES('ReturnBeforeExpiry','0','If ON, checkout will be prevented if returndate is after patron card expiry',NULL,'YesNo');
