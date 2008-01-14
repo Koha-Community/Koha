68ddaa54aaad6429ad49b660f4726c3f68bb2cde
Owen Leonard <oleonard@myacpl.org>
2008-01-14 12:58:59 -0600

    Overhaul to Koha's js-menu systems. New language-picker in the footer required more flexibility than jquery's clickmenu plugin offered, so both the language picker and the main navigation menu now use YUI's menu plugin. The necessary dependencies have been added, and ClickMenu-related files removed.
    Signed-off-by: Chris Cormack <crc@liblime.com>
    Signed-off-by: Joshua Ferraro <jmf@liblime.com>

--- koha-tmpl/intranet-tmpl/prog/en/js/staff-global.js
+++ koha-tmpl/intranet-tmpl/prog/en/js/staff-global.js
@@ -4,8 +4,6 @@ function _(s) { return s } // dummy function for gettext
 
  $(document).ready(function() {
  	$(".focus").focus();
-	$('#toplevelmenu').clickMenu(); 
-	$('#i18nMenu').clickMenu();
 	$('#header_search').tabs({
 		onShow: function() {
 	        $('#header_search').find('div:visible').find('input').eq(0).focus();
@@ -30,3 +28,63 @@ function verify_patron_images() {
         }
     }
 }
+
+            YAHOO.util.Event.onContentReady("header", function () {
+				var oMoremenu = new YAHOO.widget.Menu("moremenu", { zindex: 2 });
+
+				function positionoMoremenu() {
+					oMoremenu.align("tl", "bl");
+				}
+
+                oMoremenu.subscribe("beforeShow", function () {
+                    if (this.getRoot() == this) {
+						positionoMoremenu();
+                    }
+                });
+
+				oMoremenu.render();
+
+                oMoremenu.cfg.setProperty("context", ["showmore", "tl", "bl"]);
+
+				function onShowMoreClick(p_oEvent) {
+                    // Position and display the menu        
+                    positionoMoremenu();
+                    oMoremenu.show();
+                    // Stop propagation and prevent the default "click" behavior
+                    YAHOO.util.Event.stopEvent(p_oEvent);	
+				}
+
+				YAHOO.util.Event.addListener("showmore", "click", onShowMoreClick);
+
+                YAHOO.widget.Overlay.windowResizeEvent.subscribe(positionoMoremenu);
+            });
+
+YAHOO.util.Event.onContentReady("changelanguage", function () {
+                var oMenu = new YAHOO.widget.Menu("sublangs", { zindex: 2 });
+
+	            function positionoMenu() {
+                    oMenu.align("bl", "tl");
+                }
+
+                oMenu.subscribe("beforeShow", function () {
+                    if (this.getRoot() == this) {
+						positionoMenu();
+                    }
+                });
+
+                oMenu.render();
+
+				oMenu.cfg.setProperty("context", ["showlang", "bl", "tl"]);
+
+				function onYahooClick(p_oEvent) {
+                    // Position and display the menu        
+                    positionoMenu();
+                    oMenu.show();
+                    // Stop propagation and prevent the default "click" behavior
+                    YAHOO.util.Event.stopEvent(p_oEvent);
+                }
+
+				YAHOO.util.Event.addListener("showlang", "click", onYahooClick);
+
+				YAHOO.widget.Overlay.windowResizeEvent.subscribe(positionoMenu);
+            });
