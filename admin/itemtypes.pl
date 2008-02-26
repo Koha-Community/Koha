de04ff255d34f2cec05137a5308b368a962bdb3d
Chris Nighswonger <cnighswonger@foundations.edu>
2008-02-27 12:45:08 +1300

    Fixing broken call to getitemtypeimagesrcfromurl
    Signed-off-by: Chris Cormack <chris@bigballofwax.co.nz>
    Signed-off-by: Joshua Ferraro <jmf@liblime.com>

--- admin/itemtypes.pl
+++ admin/itemtypes.pl
@@ -271,7 +271,7 @@ else {    # DEFAULT
     my @loop;
     foreach my $itemtype ( @{$results}[ $first .. $last ] ) {
         $itemtype->{toggle} = ($toggle++ % 2) ? 0 : 1 ;
-        $itemtype->{imageurl} = getitemtypeimagesrcfromurl( $itemtype->{imageurl} );
+        $itemtype->{imageurl} = getitemtypeimagesrc('intranet') . "/$itemtype->{imageurl}";
         $itemtype->{rentalcharge} = sprintf( '%.2f', $itemtype->{rentalcharge} );
         push( @loop, $itemtype );
     }
