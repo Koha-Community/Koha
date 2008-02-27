d37e30e936af11b92c759df57356ac1ebbb327b4
Owen Leonard <oleonard@myacpl.org>
2008-02-28 10:18:32 +1300

    Further changes to use YUI buttons for cart and menu
    Signed-off-by: Chris Cormack <chris@bigballofwax.co.nz>
    Signed-off-by: Joshua Ferraro <jmf@liblime.com>

--- koha-tmpl/opac-tmpl/prog/en/js/script.js
+++ koha-tmpl/opac-tmpl/prog/en/js/script.js
@@ -72,16 +72,7 @@ YAHOO.util.Event.onContentReady("listsmenu", function () {
 	    new YAHOO.widget.Button("searchsubmit");
 	    new YAHOO.widget.Button("cartmenulink");
 	    new YAHOO.widget.Button("listsmenulink");
-   /*	$('.btn').each(function(){
-	  var b = $(this);
-	  var tt = b.text() || b.val();
-	  if ($(':submit,:button',this)) {
-	  b = $('<a>').insertAfter(this). addClass(this.className).attr('id',this.id);
-	  $(this).remove();
-	  }
-	  b.text('').css({cursor:'pointer'}). prepend('<i></i>')
-	  .append( $('<span>').text(tt).append('<i></i><span></span>') );
-	  }); */
+	$("#cartmenulink").find("a").append(" <span class=\"cartcount\" style=\"background-color : #FFC; font-size : 79%; padding: 2px;\">18</span>");
 	$("#searchsubmit").click(function(){
 		$("#searchform").submit();
 	})
