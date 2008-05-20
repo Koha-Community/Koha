171f7ed912cc383bd1091bfbade8ec83205629b7
Galen Charlton <galen.charlton@liblime.com>
2008-05-20 11:09:16 -0500

    various bugfixes for MARC21 Z39.50 targets
    [1] Added MARC-8 as an encoding choice on the target
        administration page.
    [2] Fixed TMPL coding errors.
    [3] Set the correct encoding (as it happens, 'MARC-8')
        for all of the English sample Z39.50 targets.
    
    Documentation note: due to this patch and the ones from
    Frederic, screenshots of the page for editing a Z39.50
    target should be redone.
    
    Signed-off-by: Joshua Ferraro <jmf@liblime.com>

--- installer/data/mysql/en/optional/sample_z3950_servers.sql
+++ installer/data/mysql/en/optional/sample_z3950_servers.sql
@@ -1,7 +1,7 @@
 INSERT INTO `z3950servers`
-(`host`, `port`, `db`, `userid`, `password`, `name`, `id`, `checked`, `rank`, `syntax`) VALUES 
-('z3950.loc.gov',7090,'Voyager','','','LIBRARY OF CONGRESS',1,1,1,'USMARC'),
-('bobcat.nyu.edu',210,'advance','','','NEW YORK UNIVERSITY LIBRARIES',5,0,0,'USMARC'),
-('clio-db.cc.columbia.edu',7090,'voyager','','','COLUMBIA UNIVERSITY',6,0,0,'USMARC'),
-('catnyp.nypl.org',210,'innopac','','','NEW YORK PUBLIC LIBRARY',7,0,0,'USMARC'),
-('siris-libraries.si.edu',210,'Default','','','SMITHSONIAN INSTITUTION LIBRARIES',10,0,0,'USMARC'); 
+(`host`, `port`, `db`, `userid`, `password`, `name`, `id`, `checked`, `rank`, `syntax`, `encoding`) VALUES 
+('z3950.loc.gov',7090,'Voyager','','','LIBRARY OF CONGRESS',1,1,1,'USMARC','MARC-8'),
+('bobcat.nyu.edu',210,'advance','','','NEW YORK UNIVERSITY LIBRARIES',5,0,0,'USMARC','MARC-8'),
+('clio-db.cc.columbia.edu',7090,'voyager','','','COLUMBIA UNIVERSITY',6,0,0,'USMARC','MARC-8'),
+('catnyp.nypl.org',210,'innopac','','','NEW YORK PUBLIC LIBRARY',7,0,0,'USMARC','MARC-8'),
+('siris-libraries.si.edu',210,'Default','','','SMITHSONIAN INSTITUTION LIBRARIES',10,0,0,'USMARC','MARC-8'); 
