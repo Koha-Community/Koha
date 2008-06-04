2cc75039d8cc3f55d974879e4caa4472a50af33f
Joe Atzberger (siptest <atz4sip@arwen.metavore.com>
2008-06-04 18:14:45 -0500

    Object oriented module is not an Exporter. 2 subs added, also.
    Signed-off-by: Joshua Ferraro <jmf@liblime.com>

--- C4/SIP/Sip/Configuration/Institution.pm
+++ C4/SIP/Sip/Configuration/Institution.pm
@@ -8,7 +8,7 @@ package Sip::Configuration::Institution;
 use strict;
 use warnings;
 use English;
-use Exporter;
+# use Exporter;
 
 sub new {
     my ($class, $obj) = @_;
@@ -24,8 +24,17 @@ sub new {
 
 sub name {
     my $self = shift;
-
     return $self->{name};
+}
+
+sub policy {
+    my $self = shift;
+    return $self->{policy};
+}
+
+sub parms {
+    my $self = shift;
+    return $self->{parms};
 }
 
 1;
