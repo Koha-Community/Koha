5ae7d4ea9738ab0c2ca499a578fdd3d23efdd836
Joshua Ferraro <jmf@liblime.com>
2008-06-18 19:46:13 -0500

    Circulate permission misspelling

--- circ/view_holdsqueue.pl
+++ circ/view_holdsqueue.pl
@@ -39,7 +39,7 @@ my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
         query           => $query,
         type            => "intranet",
         authnotrequired => 0,
-        flagsrequired   => { circulation => 1 },
+        flagsrequired   => { circulate => 1 },
         debug           => 1,
     }
 );
