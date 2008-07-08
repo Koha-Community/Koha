08c3c60f57394f1d5070b0eecb07cb476b4ac516
Joshua Ferraro <jmf@liblime.com>
2008-07-07 23:31:51 -0500

    Bug 1881: Managing Suggestions - default SUGGEST authorised values

--- installer/data/mysql/en/optional/auth_val.sql
+++ installer/data/mysql/en/optional/auth_val.sql
@@ -1,31 +1,34 @@
+-- Reasons for acceptance or rejection of suggestions in acquisitions
+INSERT INTO authorised_values (category, authorised_value, lib) VALUES ('SUGGEST','BSELL','Bestseller');
+INSERT INTO authorised_values (category, authorised_value, lib) VALUES ('SUGGEST','SCD','Shelf Copy Damaged');
+INSERT INTO authorised_values (category, authorised_value, lib) VALUES ('SUGGEST','LCL','Library Copy Lost');
+INSERT INTO authorised_values (category, authorised_value, lib) VALUES ('SUGGEST','AVILL','Available via ILL');
+
 -- availability statuses
 INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('LOST','0','');
 INSERT INTO `authorised_values`  (category, authorised_value, lib) VALUES ('LOST','2','Long Overdue (Lost)');
 INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('LOST','1','Lost');
 INSERT INTO `authorised_values`  (category, authorised_value, lib ) VALUES ('LOST','3','Lost and Paid For');
-INSERT INTO `authorised_values`  (category, authorised_value, lib )VALUES ('LOST','4','Missing in Inventory');
-INSERT INTO `authorised_values`  (category, authorised_value, lib )VALUES ('LOST','5','Missing in Holdqueuey');
+INSERT INTO `authorised_values`  (category, authorised_value, lib )VALUES ('LOST','4','Missing');
 
 -- damaged status of an item
 INSERT INTO `authorised_values`  (category, authorised_value, lib) VALUES ('DAMAGED','0','');
 INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('DAMAGED','1','Damaged');
 
 -- location qualification for an item, departments are linked by default to items.location
-INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('LOC','0','');
-INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('LOC','1','Reference');
-INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('LOC','2','Fiction');
-INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('LOC','3','Biography');
-INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('LOC','4','Media');
-INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('LOC','4','New Book Shelf');
+INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('LOC','FIC','Fiction');
+INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('LOC','CHILD','Children\'s Area');
+INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('LOC','DISPLAY','On Display');
+INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('LOC','NEW','New Materials Shelf');
+INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('LOC','STAFF','Staff Office');
+INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('LOC','GEN','General Stacks');
+INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('LOC','AV','Audio Visual');
+INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('LOC','REF','Reference');
 
 -- collection codes for an item
 INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('CCODE','FIC','Fiction');
 INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('CCODE','REF','Reference');
 INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('CCODE','NFIC','Non Fiction');
-
--- location qualification for an item, linked to items.stack
-INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('STACK','0','');
-INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('STACK','1','Special Collection');
 
 -- withdrawn status of an item, linked to items.wthdrawn
 INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('WITHDRAWN','0','');
