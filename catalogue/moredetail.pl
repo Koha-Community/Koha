cce9d3156e0106f282ddad4172361d53c6d80cb6
Michael Hafen <mdhafen@tech.washk12.org>
2008-08-21 15:55:27 -0600

    If itemnumber is sent to catalogue/moredetail.pl use it
    If itemnumber is part of the url for catalogue/moredetail.pl put it to use.
    This happens in members/boraccount.pl if a fine is attached to a barcode.
    
    This patch makes moredetail.pl show only the item specified by itemnumber.
    
    Signed-off-by: Galen Charlton <galen.charlton@liblime.com>

--- catalogue/moredetail.pl
+++ catalogue/moredetail.pl
@@ -51,6 +51,7 @@ my ($template, $loggedinuser, $cookie) = get_template_and_user({
 
 my $biblionumber=$query->param('biblionumber');
 my $title=$query->param('title');
+my $itemnumber=$query->param('itemnumber');
 # my $bi=$query->param('bi');
 # $bi = $biblionumber unless $bi;
 my $data=GetBiblioData($biblionumber);
@@ -84,6 +85,10 @@ my $itemtypes = GetItemTypes;
 $data->{'itemtypename'} = $itemtypes->{$data->{'itemtype'}}->{'description'};
 $results[0]=$data;
 foreach my $item (@items){
+    if ( $itemnumber && $itemnumber != $item->{'itemnumber'} ) {
+	undef $item;
+	next;
+    }
     $item->{itemlostloop}= GetAuthorisedValues(GetAuthValCode('items.itemlost',$fw),$item->{itemlost}) if GetAuthValCode('items.itemlost',$fw);
     $item->{itemdamagedloop}= GetAuthorisedValues(GetAuthValCode('items.damaged',$fw),$item->{damaged}) if GetAuthValCode('items.damaged',$fw);
     $item->{'collection'} = $ccodes->{$item->{ccode}};
