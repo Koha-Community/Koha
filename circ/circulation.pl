0506218f8bf386236bbf9a6b18c7b2ae886dbb4b
Michael Hafen <mdhafen@tech.washk12.org>
2008-08-27 16:46:57 -0600

    javascript redirect when selecting a patron on circulation.pl Patron Selection Screen
    This adds a little javascript to the Patron Selection form input after searching for a patron to check out to.
    On click it redirects to checking out to the clicked patron.
    
    Signed-off-by: Galen Charlton <galen.charlton@liblime.com>

--- circ/circulation.pl
+++ circ/circulation.pl
@@ -525,6 +525,7 @@ if ($borrowerslist) {
         -id       => 'borrowernumber',
         -values   => \@values,
         -labels   => \%labels,
+	-onclick  => "window.location = '/cgi-bin/koha/circ/circulation.pl?borrowernumber=' + this.value;",
         -size     => 7,
         -tabindex => '',
         -multiple => 0
