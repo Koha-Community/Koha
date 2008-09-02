0fb1c26d1c58e65a1266188e4815e962206b21fc
Galen Charlton <galen.charlton@liblime.com>
2008-09-02 15:36:02 -0500

    Revert "If itemnumber is sent to catalogue/moredetail.pl use it"
    Applied wrong version of patch.  Oops.
    
    This reverts commit 9eb1465b2874a8a65195065bed083d26614ca287.

--- catalogue/moredetail.pl
+++ catalogue/moredetail.pl
@@ -51,7 +51,6 @@ my ($template, $loggedinuser, $cookie) = get_template_and_user({
 
 my $biblionumber=$query->param('biblionumber');
 my $title=$query->param('title');
-my $itemnumber=$query->param('itemnumber');
 # my $bi=$query->param('bi');
 # $bi = $biblionumber unless $bi;
 my $data=GetBiblioData($biblionumber);
@@ -85,10 +84,6 @@ my $itemtypes = GetItemTypes;
 $data->{'itemtypename'} = $itemtypes->{$data->{'itemtype'}}->{'description'};
 $results[0]=$data;
 foreach my $item (@items){
-    if ( $itemnumber && $itemnumber != $item->{'itemnumber'} ) {
-	undef $item;
-	next;
-    }
     $item->{itemlostloop}= GetAuthorisedValues(GetAuthValCode('items.itemlost',$fw),$item->{itemlost}) if GetAuthValCode('items.itemlost',$fw);
     $item->{itemdamagedloop}= GetAuthorisedValues(GetAuthValCode('items.damaged',$fw),$item->{damaged}) if GetAuthValCode('items.damaged',$fw);
     $item->{'collection'} = $ccodes->{$item->{ccode}};
