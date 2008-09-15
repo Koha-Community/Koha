df13aeee3bfe4e3789ed731b8028277d09853356
Owen Leonard <oleonard@myacpl.org>
2008-09-15 11:33:32 -0500

    Adding javascript to prevent premature submission of forms when using a barcode scanner (or by hitting the enter key). Addresses Bug 2591
    Signed-off-by: Galen Charlton <galen.charlton@liblime.com>

--- koha-tmpl/intranet-tmpl/prog/en/js/staff-global.js
+++ koha-tmpl/intranet-tmpl/prog/en/js/staff-global.js
@@ -70,3 +70,21 @@ YAHOO.util.Event.onContentReady("changelanguage", function () {
 
 				YAHOO.widget.Overlay.windowResizeEvent.subscribe(positionoMenu);
             });
+			
+// http://jennifermadden.com/javascript/stringEnterKeyDetector.html
+function checkEnter(e){ //e is event object passed from function invocation
+	var characterCode; // literal character code will be stored in this variable
+	if(e && e.which){ //if which property of event object is supported (NN4)
+		e = e;
+		characterCode = e.which; //character code is contained in NN4's which property
+	} else {
+		e = event;
+		characterCode = e.keyCode; //character code is contained in IE's keyCode property
+	}
+
+	if(characterCode == 13){ //if generated character code is equal to ascii 13 (if enter key)
+		return false;
+	} else {
+		return true;
+	}
+}
