9c811d13bef745b0843a22d6e3e7a7df6183851f
Joe Atzberger <joe.atzberger@liblime.com>
2008-09-18 19:02:49 -0500

    Revising the stub for ILS/Transaction/FeePayment.pm
    Obviously more implementation is needed.
    
    Signed-off-by: Galen Charlton <galen.charlton@liblime.com>

--- C4/SIP/ILS/Transaction/FeePayment.pm
+++ C4/SIP/ILS/Transaction/FeePayment.pm
@@ -1,13 +1,18 @@
 package ILS::Transaction::FeePayment;
 
-use Exporter;
 use warnings;
 use strict;
 
+use vars qw($VERSION @ISA $debug);
+
+BEGIN {
+	$VERSION = 1.00;
+	@ISA = qw(ILS::Transaction);
+	$debug = 0;
+}
+
 use ILS;
 use ILS::Transaction;
-
-our @ISA = qw(Exporter ILS::Transaction);
 
 1;
 __END__
