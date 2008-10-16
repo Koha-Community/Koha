755b509cd6f97f581fd5332a9b96f2f5569cac33
Owen Leonard <oleonard@myacpl.org>
2008-10-16 10:45:29 -0500

    Steamlining javascript for cloning additem block and tweaking style.
    Signed-off-by: Galen Charlton <galen.charlton@liblime.com>

--- koha-tmpl/intranet-tmpl/prog/en/js/additem.js
+++ koha-tmpl/intranet-tmpl/prog/en/js/additem.js
@@ -1,40 +1,3 @@
-function countItemBlocks() {
-    var outeritemblock = document.getElementById('outeritemblock');
-    var quantityrec = document.getElementById('quantityrec');
-    var origquantityrec = document.getElementById('origquantityrec');
-    var itemblockcount = outeritemblock.getElementsByTagName('div');
-    var num = parseFloat(origquantityrec.value) + itemblockcount.length;
-    quantityrec.setAttribute('value',num);
-}
-function deleteItemBlock(index) {
-    var aDiv = document.getElementById(index);
-    aDiv.parentNode.removeChild(aDiv);
-    countItemBlocks();
-}
-function cloneItemBlock(index) {    
-    var original = document.getElementById(index); //original <div>
-    var clone = original.cloneNode(true);
-    // set the attribute for the new 'div' subfields
-    clone.setAttribute('id',index + index);//set another id.
-    var NumTabIndex;
-    NumTabIndex = parseInt(original.getAttribute('tabindex'));
-    if(isNaN(NumTabIndex)) NumTabIndex = 0;
-    clone.setAttribute('tabindex',NumTabIndex+1);
-    var CloneButtonPlus;
-    var CloneButtonMinus;
-  //  try{
-        CloneButtonPlus = clone.getElementsByTagName('a')[0];
-        CloneButtonPlus.setAttribute('onclick',"cloneItemBlock('" + index + index + "')");
-    CloneButtonMinus = clone.getElementsByTagName('a')[1];
-    CloneButtonMinus.setAttribute('onclick',"deleteItemBlock('" + index + index + "')");
-    CloneButtonMinus.setAttribute('style',"display:inline");
-   // }
-    //catch(e){        // do nothig if ButtonPlus & CloneButtonPlus don't exist.
-    //}
-    // insert this line on the page    
-    original.parentNode.insertBefore(clone,original.nextSibling);
-    countItemBlocks();
-}
 function check_additem() {
 	var	barcodes = document.getElementsByName('barcode');
 	var success = true;
@@ -51,3 +14,19 @@ function check_additem() {
 	// duplicates within the form.  
 	return success;
 }
+$(document).ready(function(){
+	$(".cloneItemBlock").click(function(){
+		$(this).parent().parent().clone(true).insertAfter($(this).parent().parent()).find("a.deleteItemBlock").show();
+		var quantityrec = parseFloat($("#quantityrec").attr("value"));
+		quantityrec++;
+		$("#quantityrec").attr("value",quantityrec);
+		return false;
+	});
+	$(".deleteItemBlock").click(function(){
+		$(this).parent().parent().remove();
+		var quantityrec = parseFloat($("#quantityrec").attr("value"));
+		quantityrec--;
+		$("#quantityrec").attr("value",quantityrec);
+		return false;
+	});
+});
