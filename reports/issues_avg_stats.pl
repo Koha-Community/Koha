d35bed6b40ff6082c2924977ed33474388104f0d
Galen Charlton <galen.charlton@liblime.com>
2008-10-23 12:05:56 -0500

    bug 2543: follow-up patch
    Add 'use C4::Reports' lines that original patch
    omitted.
    
    Note that original patch prevented these two reports
    from even compiling.  Not good.
    
    Signed-off-by: Galen Charlton <galen.charlton@liblime.com>

--- reports/issues_avg_stats.pl
+++ reports/issues_avg_stats.pl
@@ -26,6 +26,7 @@ use C4::Branch; # GetBranches
 use C4::Output;
 use C4::Koha;
 use C4::Circulation;
+use C4::Reports;
 use C4::Dates qw/format_date format_date_in_iso/;
 use Date::Calc qw(Delta_Days);
 
