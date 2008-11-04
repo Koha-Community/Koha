04417c2b730872a45a2f8c87030109dbaf5be2c4
Owen Leonard <oleonard@myacpl.org>
2008-11-04 10:21:47 -0600

    Fix for Bug 2700, Keeping Data When Adding Multiple Items
    When cloning the set of inputs, the new js
    increments the IDs of each form field (to keep them unique) and automatically
    selects the option that was selected in the cloned group.
    
    Signed-off-by: Galen Charlton <galen.charlton@liblime.com>

--- koha-tmpl/intranet-tmpl/prog/en/js/additem.js
+++ koha-tmpl/intranet-tmpl/prog/en/js/additem.js
@@ -16,7 +16,31 @@ function check_additem() {
 }
 $(document).ready(function(){
 	$(".cloneItemBlock").click(function(){
-		$(this).parent().parent().clone(true).insertAfter($(this).parent().parent()).find("a.deleteItemBlock").show();
+		var clonedRow = $(this).parent().parent().clone(true);
+		clonedRow.insertAfter($(this).parent().parent()).find("a.deleteItemBlock").show();
+		// find ID of cloned row so we can increment it for the clone
+		var count = $("input[id^=volinf]",clonedRow).attr("id");
+		var current = Number(count.replace("volinf",""));
+		var increment = current + 1;
+		// loop over inputs
+		var inputs = ["volinf","barcode"];
+		jQuery.each(inputs,function() {
+			// increment IDs of labels and inputs in the clone
+			$("label[for="+this+current+"]",clonedRow).attr("for",this+increment);
+			$("input[name="+this+"]",clonedRow).attr("id",this+increment);
+		});
+		// loop over selects
+		var selects = ["homebranch","location","itemtype","ccode"];
+		jQuery.each(selects,function() {
+			// increment IDs of labels and selects in the clone
+			$("label[for="+this+current+"]",clonedRow).attr("for",this+increment);
+			$("input[name="+this+"]",clonedRow).attr("id",this+increment);
+			$("select[name="+this+"]",clonedRow).attr("id",this+increment);
+			// find the selected option and select it in the clone
+			var selectedVal = $("select#"+this+current).find("option:selected").attr("value");
+			$("select[name="+this+"] option[value="+selectedVal+"]",clonedRow).attr("selected","selected");
+		});
+		
 		var quantityrec = parseFloat($("#quantityrec").attr("value"));
 		quantityrec++;
 		$("#quantityrec").attr("value",quantityrec);
