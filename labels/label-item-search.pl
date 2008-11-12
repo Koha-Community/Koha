426260de1aa986530c805f2a6d6a845272c1224f
Mason James <mason.loves.sushi@gmail.com>
2008-11-12 19:55:39 +1300

    The current searching in labels is a bit minimal, and current only does keyword searching.
    there is some old 2.2 code for more complete labels-searching, thats been commented out
    
    ive logged a 'bug' for this - 2777
    
    http://bugs.koha.org/cgi-bin/bugzilla/show_bug.cgi?id=2777
    
    this fix...
      - enables the index-searching functionality
      - adds date-ranges
      - uses the numSearchResults syspref, not a hardcoded '20'
      - rewrote old 2.2 page-num code, (didnt work for 3.x)
      - some indent and whitepages tidys.
      - unused 2.2 search-code removed.
    
    Mason
    
    Signed-off-by: Galen Charlton <galen.charlton@liblime.com>

--- labels/label-item-search.pl
+++ labels/label-item-search.pl
@@ -34,7 +34,10 @@ use C4::Dates;
 use C4::Koha;    # XXX subfield_is_koha_internal_p
 use C4::Debug;
 use List::Util qw( max min );
+use POSIX;
+
 #use Smart::Comments;
+#use Data::Dumper;
 
 BEGIN {
     $debug = $debug || $cgi_debug;
@@ -49,148 +52,185 @@ BEGIN {
 # scrolling lists on the page !?!? That's why this function was written.
 
 my $query           = new CGI;
-my $type            = $query->param('type');
-my $op              = $query->param('op');
-my $batch_id        = $query->param('batch_id');
-my $dateaccessioned = $query->param('dateaccessioned');
+
+my $type      = $query->param('type');
+my $op        = $query->param('op');
+my $batch_id  = $query->param('batch_id');
+my $ccl_query = $query->param('ccl_query');
 
 my $dbh = C4::Context->dbh;
 
-my $startfrom = $query->param('startfrom') || 0;
+my $startfrom = $query->param('startfrom') || 1;
 my ( $template, $loggedinuser, $cookie );
-my $total_hits;
-my (@marclist,@and_or,@excluding,@operator,@value,$orderby,@tags,$results,$total,$error,$marcresults);
-# XXX should this be maxItemsInSearchResults or numSearchResults preference instead of 19?
-my $resultsperpage = $query->param('resultsperpage') || 19;
+my ($total_hits, $orderby, $results, $total, $error, $marcresults, $idx,	$datefrom, $dateto, $ccl_textbox);
+
+my $resultsperpage = C4::Context->preference('numSearchResults') || '20';
 
 my $show_results = 0;
-if ( $op eq "do_search" ) {
-    @marclist  = $query->param('marclist');
-    @and_or    = $query->param('and_or');
-    @excluding = $query->param('excluding');
-    @operator  = $query->param('operator');
-    @value     = $query->param('value');
-    $orderby   = $query->param('orderby');
-	if (scalar @marclist) {
-      #catalogsearch( $dbh, \@tags, \@and_or, \@excluding, \@operator, \@value,
-      #  $startfrom * $resultsperpage,
-      #  $resultsperpage, $orderby );
-		( $error, $marcresults, $total_hits ) = SimpleSearch( $marclist[0], $startfrom, $resultsperpage );
-		if ($marcresults) {
-			$show_results = scalar @$marcresults;
-		} else {
-			warn "ERROR label-item-search: no results from SimpleSearch";
-			# leave $show_results undef
-		}
+
+if ($op eq "do_search") {
+	$idx       = $query->param('idx');
+	$ccl_textbox = $query->param('ccl_textbox');
+     if ($ccl_textbox && $idx) {
+    $ccl_query = "$idx=$ccl_textbox" ;
+    }
+
+	$datefrom = $query->param('datefrom');
+	$dateto   = $query->param('dateto');
+
+	if ($datefrom) {
+		$datefrom = C4::Dates->new($datefrom);
+        $ccl_query .= ' and ' if $ccl_textbox;
+		$ccl_query .= "acqdate,st-date-normalized,ge=" .  $datefrom->output("iso");
+	}
+
+	if ($dateto) {
+		$dateto = C4::Dates->new($dateto);
+        $ccl_query .= ' and ' if ($ccl_textbox || $datefrom) ;
+		$ccl_query .= "acqdate,st-date-normalized,le=".  $dateto->output("iso");
+	}
+
+    my $offset =    $startfrom > 1 ? $startfrom - 1 : 0;
+	($error, $marcresults, $total_hits) =
+	  SimpleSearch($ccl_query, $offset, $resultsperpage);
+
+	if ($marcresults) {
+		$show_results = scalar @$marcresults;
+	} else {
+		warn "ERROR label-item-search: no results from SimpleSearch";
+
+		# leave $show_results undef
 	}
 }
 
-if ( $show_results ) {
-  ( $template, $loggedinuser, $cookie ) = get_template_and_user(
-        {
-            template_name   => "labels/result.tmpl",
-            query           => $query,
-            type            => "intranet",
-            authnotrequired => 0,
-            flagsrequired   => { catalogue => 1 },
-            debug           => 1,
-        }
-    );
-
+if ($show_results) {
 	my $hits = $show_results;
-        my (@results, @items);
-        # This code needs to be refactored using these subs...
-        #my @items = &GetItemsInfo( $biblio->{biblionumber}, 'intra' );
-        #my $dat = &GetBiblioData( $biblio->{biblionumber} );
-	for(my $i=0; $i<$hits; $i++) {
-        #DEBUG Notes: Decode the MARC record from each resulting MARC record...
-	my $marcrecord = MARC::File::USMARC::decode($marcresults->[$i]);
-        #DEBUG Notes: Transform it to Koha form...
-	my $biblio = TransformMarcToKoha(C4::Context->dbh,$marcrecord,'');
-	# Begin building the hash for the template...
-        # I don't think we need this with the current template design, but I'm leaving it in place. -fbcit
-	#$biblio->{highlight}       = ($i % 2)?(1):(0);
-        #DEBUG Notes: Stuff the bib into @results...
-        push @results, $biblio;
-	my $biblionumber = $biblio->{'biblionumber'};
-        #DEBUG Notes: Grab the item numbers associated with this MARC record...
-        my $itemnums = get_itemnumbers_of($biblionumber);
-        #DEBUG Notes: Retrieve the item data for each number... 
-        my $iii = $itemnums->{$biblionumber};
-	    if ($iii) {
-	        my $item_results =  GetItemInfosOf( @$iii );
-                foreach my $item (keys %$item_results) {
-                    #DEBUG Notes: Build an array element 'item' of the correct bib (results) hash which contains item-specific data...
-                    if ($item_results->{$item}->{'biblionumber'} eq $results[$i]->{'biblionumber'}) {
-                        # NOTE: The order of the elements in this array must be preserved or the table dependent on it will be incorrectly rendered.
-                        # This is a real hack, but I can't think of a better way right now. -fbcit
-                        # It is conceivable that itemcallnumber and/or barcode fields might be empty so the trinaries cover this possibility.
-                        push @{$results[$i]->{'item'}}, { i_itemnumber1         => $item_results->{$item}->{'itemnumber'} };
-                        push @{$results[$i]->{'item'}}, { i_itemcallnumber      => ($item_results->{$item}->{'itemcallnumber'} ? $item_results->{$item}->{'itemcallnumber'} : 'NA') };
-                        push @{$results[$i]->{'item'}}, { i_dateaccessioned     => $item_results->{$item}->{'dateaccessioned'} };
-                        push @{$results[$i]->{'item'}}, { i_barcode             => ($item_results->{$item}->{'barcode'} ? $item_results->{$item}->{'barcode'} : 'NA')};
-                        push @{$results[$i]->{'item'}}, { i_itemnumber2         => $item_results->{$item}->{'itemnumber'} };
-                    }
-                }
-	    }
-        }
-        $debug and warn "**********\@results**********\n";
-        $debug and warn Dumper(@results);
-  
-    my @field_data = ();
+	my (@results, @items);
 
-	# FIXME: this relies on symmetric order of CGI params that IS NOT GUARANTEED by spec.
+	# This code needs to be refactored using these subs...
+	#my @items = &GetItemsInfo( $biblio->{biblionumber}, 'intra' );
+	#my $dat = &GetBiblioData( $biblio->{biblionumber} );
+	for (my $i = 0 ; $i < $hits ; $i++) {
 
-    for ( my $i = 0 ; $i <= $#marclist ; $i++ ) {
-        push @field_data, { term => "marclist",  val => $marclist[$i] };
-        push @field_data, { term => "and_or",    val => $and_or[$i] };
-        push @field_data, { term => "excluding", val => $excluding[$i] };
-        push @field_data, { term => "operator",  val => $operator[$i] };
-        push @field_data, { term => "value",     val => $value[$i] };
-    }
+		#DEBUG Notes: Decode the MARC record from each resulting MARC record...
+		my $marcrecord = MARC::File::USMARC::decode($marcresults->[$i]);
 
-    my @numbers = ();
-    $total = $total_hits;
-    if ( $total > $resultsperpage ) {
-        for ( my $i = 1 ; $i < $total / $resultsperpage + 1 ; $i++ ) {
-            if ( $i < 16 ) {
-                my $highlight = 0;
-                ( $startfrom == ( $i - 1 ) ) && ( $highlight = 1 );
-                push @numbers,
-                  {
-                    number     => $i,
-                    highlight  => $highlight,
-                    searchdata => \@field_data,
-                    startfrom  => ( $i - 1 )
-                  };
-            }
-        }
-    }
+		#DEBUG Notes: Transform it to Koha form...
+		my $biblio = TransformMarcToKoha(C4::Context->dbh, $marcrecord, '');
 
-    # multi page display gestion
-    my $displaynext = 0;
-    my $displayprev = $startfrom;
-    # XXX Kludge. We show the "next" link if we retrieved the max number of results. There could be 0 more.
-    if ( scalar @results == $resultsperpage ) {
-        $displaynext = 1;
-    }
+# Begin building the hash for the template...
+# I don't think we need this with the current template design, but I'm leaving it in place. -fbcit
+#$biblio->{highlight}       = ($i % 2)?(1):(0);
+#DEBUG Notes: Stuff the bib into @results...
+		push @results, $biblio;
+		my $biblionumber = $biblio->{'biblionumber'};
 
-    $template->param(
-        result         => \@results,
-        startfrom      => $startfrom,
-        displaynext    => $displaynext,
-        displayprev    => $displayprev,
-        resultsperpage => $resultsperpage,
-        startfromnext  => $startfrom + min( $resultsperpage, scalar @results ),
-        startfromprev  => max( $startfrom - $resultsperpage, 0 ),
-        searchdata     => \@field_data,
-        total          => $total_hits,
-        from           => $startfrom + 1,
-        to             => $startfrom + min( $resultsperpage, scalar @results ),
-        numbers        => \@numbers,
-        batch_id       => $batch_id,
-        type           => $type,
-    );
+		#DEBUG Notes: Grab the item numbers associated with this MARC record...
+		my $itemnums = get_itemnumbers_of($biblionumber);
+
+		#DEBUG Notes: Retrieve the item data for each number...
+		my $iii = $itemnums->{$biblionumber};
+		if ($iii) {
+			my $item_results = GetItemInfosOf(@$iii);
+			foreach my $item (keys %$item_results) {
+
+#DEBUG Notes: Build an array element 'item' of the correct bib (results) hash which contains item-specific data...
+				if ($item_results->{$item}->{'biblionumber'} eq
+					$results[$i]->{'biblionumber'}) {
+
+# NOTE: The order of the elements in this array must be preserved or the table dependent on it will be incorrectly rendered.
+# This is a real hack, but I can't think of a better way right now. -fbcit
+# It is conceivable that itemcallnumber and/or barcode fields might be empty so the trinaries cover this possibility.
+					push @{ $results[$i]->{'item'} }, { i_itemnumber1 =>
+						  $item_results->{$item}->{'itemnumber'} };
+					push @{ $results[$i]->{'item'} },
+					  { i_itemcallnumber => (
+							  $item_results->{$item}->{'itemcallnumber'}
+							? $item_results->{$item}->{'itemcallnumber'} : 'NA'
+						)
+					  };
+					push @{ $results[$i]->{'item'} }, { i_dateaccessioned =>
+						  $item_results->{$item}->{'dateaccessioned'} };
+					push @{ $results[$i]->{'item'} },
+					  { i_barcode => (
+							  $item_results->{$item}->{'barcode'}
+							? $item_results->{$item}->{'barcode'} : 'NA'
+						)
+					  };
+					push @{ $results[$i]->{'item'} }, { i_itemnumber2 =>
+						  $item_results->{$item}->{'itemnumber'} };
+				}
+			}
+		}
+	}
+	$debug and warn "**********\@results**********\n";
+	$debug and warn Dumper(@results);
+
+	($template, $loggedinuser, $cookie) = get_template_and_user(
+		{   template_name   => "labels/result.tmpl",
+			query           => $query,
+			type            => "intranet",
+			authnotrequired => 0,
+			flagsrequired   => { borrowers => 1 },
+			flagsrequired   => { catalogue => 1 },
+			debug           => 1,
+		}
+	);
+
+	# build page nav stuff.
+	my (@field_data, @numbers);
+	$total = $total_hits;
+
+	my ($from, $to, $startfromnext, $startfromprev, $displaynext, $displayprev);
+
+	if ($total > $resultsperpage) {
+		my $num_of_pages = ceil($total / $resultsperpage + 1);
+		for (my $page = 1 ; $page < $num_of_pages ; $page++) {
+			my $startfrm = (($page - 1) * $resultsperpage) + 1;
+			push @numbers,
+			  { number    => $page,
+				startfrom => $startfrm
+			  };
+		}
+
+		$from          = $startfrom;
+		$startfromprev = $startfrom - $resultsperpage;
+		$startfromnext = $startfrom + $resultsperpage;
+
+		$to =
+		    $startfrom + $resultsperpage > $total
+		  ? $total
+		  : $startfrom + $resultsperpage - 1;
+
+		# multi page display
+		my $displaynext = 0;
+		my $displayprev = $startfrom;
+
+		$displaynext = 1 if (scalar @results == $resultsperpage);
+
+		$template->param(
+
+			total          => $total_hits,
+			from           => $from,
+			to             => $to,
+			startfromnext  => $startfromnext,
+			startfromprev  => $startfromprev,
+			startfrom      => $startfrom,
+			displaynext    => $displaynext,
+			displayprev    => $displayprev,
+			resultsperpage => $resultsperpage,
+			numbers        => \@numbers,
+
+		);
+	}
+
+	$template->param(
+		result    => \@results,
+		batch_id  => $batch_id,
+		type      => $type,
+		idx       => $idx,
+		ccl_query => $ccl_query,
+	);
 }
 
 #
@@ -213,9 +253,9 @@ else {
     foreach my $thisitemtype (keys %$itemtypes) {
             my %row =(value => $thisitemtype,
                            description => $itemtypes->{$thisitemtype}->{'description'},
-                            );  
+                            );
             push @itemtypeloop, \%row;
-    }  
+    }
     $template->param(
     itemtypeloop =>\@itemtypeloop,
     batch_id     => $batch_id,
@@ -232,3 +272,10 @@ output_html_with_http_headers $query, $cookie, $template->output;
 # Local Variables:
 # tab-width: 4
 # End:
+
+
+
+
+
+
+
