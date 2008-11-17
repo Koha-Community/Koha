177478062360c642662e935c7c266284a5e46cac
Frederic Demians <f.demians@tamil.fr>
2008-11-17 17:38:46 +0100

    Add default classification sources and filing rules to fr-FR
    fr-FR install wasn't populating classication rules tables:
    
      * class_sort_rules
      * class_sources
    
    This patch add default values for those tables
    mandatory during installation process.
    
    Signed-off-by: Galen Charlton <galen.charlton@liblime.com>

---
+++ installer/data/mysql/fr-FR/1-Obligatoire/class_sources.sql
@@ -0,0 +1,36 @@
+-- 
+-- Default classification sources and filing rules
+-- for Koha.
+--
+-- Copyright (C) 2007 LiblimeA
+--
+-- This file is part of Koha.
+--
+-- Koha is free software; you can redistribute it and/or modify it under the
+-- terms of the GNU General Public License as published by the Free Software
+-- Foundation; either version 2 of the License, or (at your option) any later
+-- version.
+-- 
+-- Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+-- WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+-- A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+-- 
+-- You should have received a copy of the GNU General Public License along with
+-- Koha; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+-- Suite 330, Boston, MA  02111-1307 USA
+
+-- class sorting (filing) rules
+INSERT INTO `class_sort_rules` (`class_sort_rule`, `description`, `sort_routine`) VALUES
+                               ('dewey', 'Règle de classement par défaut DCC', 'Dewey'),
+                               ('lcc', 'Régle de classement par déf. Bibliothèque du Congrès', 'LCC'),
+                               ('generic', 'Règle de classement par défaut générique', 'Generic');
+
+
+-- classification schemes or sources
+INSERT INTO `class_sources` (`cn_source`, `description`, `used`, `class_sort_rule`) VALUES
+                            ('ddc', 'Classification décimale Dewey', 1, 'dewey'),
+                            ('lcc', 'Classification de la Bibliothèque du Congrès', 1, 'lcc'),
+                            ('udc', 'Classification décimale universelle', 0, 'generic'),
+                            ('sudocs', 'SuDoc Classification (U.S. GPO)', 0, 'generic'),
+                            ('anscr', 'ANSCR (Sound Recordings)', 0, 'generic'),
+                            ('z', 'Autre schéma générique de classification', 0, 'generic');
