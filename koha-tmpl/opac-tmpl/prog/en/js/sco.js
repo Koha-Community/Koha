4f156d005d69cbf5230f6597989ee5167b520c72
Owen Leonard <oleonard@myacpl.org>
2008-11-26 14:46:07 -0600

    Cleaning up Self-Checkout interface, adding some basic formatting based on other standard styles.
    Patch also removes a hard-coded English string from the script and some unused functions from sco.js.
    
    Signed-off-by: Galen Charlton <galen.charlton@liblime.com>
    Signed-off-by: Henri-Damien LAURENT <henridamien.laurent@biblibre.com>

--- koha-tmpl/opac-tmpl/prog/en/js/sco.js
+++ koha-tmpl/opac-tmpl/prog/en/js/sco.js
@@ -1,20 +1,5 @@
-function focusOnFirst(){
-	;	// FIXME: do something here?
-}
-
 function sco_init(valid_session) {
 	if (valid_session == 1) {
   		setTimeout("location.href='/cgi-bin/koha/sco/sco-main.pl?op=logout';",120000);
 	}
-	if(document.forms['mainform']){
-		document.forms['mainform'].elements[0].focus();
-	}
-}
-    
-function confirmDelete(message) {
-	return (confirm(message) ? true : false);
-}
-
-function Dopop(link) {
-	newin=window.open(link,'popup','width=500,height=400,toolbar=false,scrollbars=yes');
 }
