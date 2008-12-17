3886be26ff38ad6b99148dc2e4d8c2d874cf9bea
Andrew Moore <andrew.moore@liblime.com>
2008-12-17 14:09:25 -0600

    Bug 2799: database update to "branch transfer limits"
    This patch cleans up kohastructure.sql a bit:
     * moved the table definition to above the ending comments
     * added a "drop table..." statement
    
    Signed-off-by: Galen Charlton <galen.charlton@liblime.com>

--- installer/data/mysql/kohastructure.sql
+++ installer/data/mysql/kohastructure.sql
@@ -2300,6 +2300,19 @@ CREATE TABLE `borrower_message_transport_preferences` (
   CONSTRAINT `borrower_message_transport_preferences_ibfk_2` FOREIGN KEY (`message_transport_type`) REFERENCES `message_transport_types` (`message_transport_type`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Table structure for the table branch_transfer_limits
+--
+
+DROP TABLE IF EXISTS `branch_transfer_limits`;
+CREATE TABLE branch_transfer_limits (
+    limitId int(8) NOT NULL auto_increment,
+    toBranch varchar(4) NOT NULL,
+    fromBranch varchar(4) NOT NULL,
+    itemtype varchar(4) NOT NULL,  
+    PRIMARY KEY  (limitId)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
@@ -2309,15 +2322,3 @@ CREATE TABLE `borrower_message_transport_preferences` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
---
--- Table structure for the table branch_transfer_limits
---
-
-CREATE TABLE branch_transfer_limits (
-    limitId int(8) NOT NULL auto_increment,
-    toBranch varchar(4) NOT NULL,
-    fromBranch varchar(4) NOT NULL,
-    itemtype varchar(4) NOT NULL,  
-    PRIMARY KEY  (limitId)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-                                                                                                                                                            
