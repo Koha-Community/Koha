4e51d9b8fbd75a11a82addd406e7739c43ff61d7
stan <stan@brinkerhoff.org>
2009-01-01 12:39:33 -0500

    Enable use warnings;, clean up complainy functions, fix a few tab/space formatting issues.
    Refactoring KOHAVERSION to be less crufty, enabling use warnings, change formatting slightly in places.
    
    Signed-off-by: Galen Charlton <galen.charlton@liblime.com>
    Signed-off-by: Henri-Damien LAURENT <henridamien.laurent@biblibre.com>

--- C4/Context.pm
+++ C4/Context.pm
@@ -16,6 +16,7 @@ package C4::Context;
 # Koha; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
 # Suite 330, Boston, MA  02111-1307 USA
 
+use warnings;
 use strict;
 use warnings;
 use vars qw($VERSION $AUTOLOAD $context @context_stack);
