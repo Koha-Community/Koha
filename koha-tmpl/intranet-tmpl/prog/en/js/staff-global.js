b0deccd971afb9a105cb61947ae1b95431e79b1c
Owen Leonard <oleonard@myacpl.org>
2009-02-10 14:45:36 -0600

    Changes to improve keyboard access to global search box tabs.
    Signed-off-by: Galen Charlton <galen.charlton@liblime.com>

--- koha-tmpl/intranet-tmpl/prog/en/js/staff-global.js
+++ koha-tmpl/intranet-tmpl/prog/en/js/staff-global.js
@@ -6,9 +6,9 @@ function _(s) { return s } // dummy function for gettext
  	$(".focus").focus();
 	$('#header_search > ul').tabs().bind('show.ui-tabs', function(e, ui) { $('#header_search > div:not(.ui-tabs-hide)').find('input').eq(0).focus(); });
 	$(".close").click(function(){ window.close(); });
-	if($("#header_search #checkin_search").length > 0){ $.hotkeys.add('Alt+r',function (){ $("#header_search > ul").tabs("select","#checkin_search"); }); } else { $.hotkeys.add('Alt+r',function (){ location.href="/cgi-bin/koha/circ/returns.pl"; }); }
-	if($("#header_search #circ_search").length > 0){ $.hotkeys.add('Alt+u',function (){ $("#header_search > ul").tabs("select","#circ_search"); }); } else { $.hotkeys.add('Alt+u',function(){ location.href="/cgi-bin/koha/circ/circulation.pl"; }); }
-	if($("#header_search #catalog_search").length > 0){ $.hotkeys.add('Alt+q',function (){ $("#header_search > ul").tabs("select","#catalog_search"); }); } else { $.hotkeys.add('Alt+q',function(){ location.href="/cgi-bin/koha/catalogue/search.pl"; }); }
+	if($("#header_search #checkin_search").length > 0){ $.hotkeys.add('Alt+r',function (){ $("#header_search > ul").tabs("select","#checkin_search"); $("#ret_barcode").focus(); }); } else { $.hotkeys.add('Alt+r',function (){ location.href="/cgi-bin/koha/circ/returns.pl"; }); }
+	if($("#header_search #circ_search").length > 0){ $.hotkeys.add('Alt+u',function (){ $("#header_search > ul").tabs("select","#circ_search"); $("#findborrower").focus(); }); } else { $.hotkeys.add('Alt+u',function(){ location.href="/cgi-bin/koha/circ/circulation.pl"; }); }
+	if($("#header_search #catalog_search").length > 0){ $.hotkeys.add('Alt+q',function (){ $("#header_search > ul").tabs("select","#catalog_search"); $("#search-form").focus(); }); } else { $.hotkeys.add('Alt+q',function(){ location.href="/cgi-bin/koha/catalogue/search.pl"; }); }
  });
  
              YAHOO.util.Event.onContentReady("header", function () {
