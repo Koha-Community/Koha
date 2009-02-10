e6f3fb9591844659bb7e6ccf909ec53cf6b7116a
Joe Atzberger <joe.atzberger@liblime.com>
2009-02-10 14:16:31 -0600

    Make sax_parser_print provide more obvious feedback.
    Signed-off-by: Galen Charlton <galen.charlton@liblime.com>

--- misc/sax_parser_print.pl
+++ misc/sax_parser_print.pl
@@ -2,5 +2,10 @@
 # check the current SAX Parser
 use XML::SAX::ParserFactory;
 $parser = XML::SAX::ParserFactory->parser();
-print "$parser\n";
-
+print "Koha wants something like:
+    XML::LibXML::SAX::Parser=HASH(0x81fe220)
+You have:
+    $parser\n";
+print "Looks " .
+    ($parser =~ /^XML::LibXML::SAX::Parser=HASH/ ?
+    "good.\n" : "bad, check INSTALL.* documentation.\n");
