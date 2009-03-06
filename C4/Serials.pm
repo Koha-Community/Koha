dd9b33f1e9546f3cbc9087a47858a152b9895dc9
Henri-Damien LAURENT <henridamien.laurent@biblibre.com>
2009-03-06 12:03:21 +0100

    Fix :Fatal Errors occurs when date is not a regular date
    When the start date or the "current date" is not a regular date
    the transformation and checkdate fails because there is no
    Year/Month/Date
    This patch makes it fail more 'silenty' for the users and raises an
    error that could be coped with.
    
    Signed-off-by: Galen Charlton <galen.charlton@liblime.com>

--- C4/Serials.pm
+++ C4/Serials.pm
@@ -1806,7 +1806,7 @@ sub HasSubscriptionExpired {
 	  return 0 unless $res;
       my @res=split (/-/,$res);
       my @endofsubscriptiondate=split(/-/,$expirationdate);
-      return 2 if (not check_date(@res) || not check_date(@endofsubscriptiondate));
+      return 2 if (scalar(@res)!=3 || scalar(@endofsubscriptiondate)!=3||not check_date(@res) || not check_date(@endofsubscriptiondate));
       return 1 if ( (@endofsubscriptiondate && Delta_Days($res[0],$res[1],$res[2],
                   $endofsubscriptiondate[0],$endofsubscriptiondate[1],$endofsubscriptiondate[2]) <= 0)
                   || (!$res));
