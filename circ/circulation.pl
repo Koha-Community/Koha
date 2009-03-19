95909b0d815d3c89d3ee664ee078a6a22368eded
Mason James <mason.loves.sushi@gmail.com>
2009-03-20 11:52:26 +1300

    fix to remove leading/trailing whitespace from entered barcode.
    Signed-off-by: Galen Charlton <galen.charlton@liblime.com>

--- circ/circulation.pl
+++ circ/circulation.pl
@@ -112,6 +112,7 @@ if (C4::Context->preference("AutoLocation") ne 1) { # FIXME: string comparison t
 }
 
 my $barcode        = $query->param('barcode') || '';
+$barcode =~  s/^[ \t]+|[ \t]+$//g; # remove leading/trailing whitespace
 
 $barcode = barcodedecode($barcode) if( $barcode && C4::Context->preference('itemBarcodeInputFilter'));
 my $stickyduedate  = $query->param('stickyduedate');
