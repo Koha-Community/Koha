1caa108ef0a6ebe1c5d6ab33ce663fadd6ebd7eb
Joe Atzberger <joe.atzberger@liblime.com>
2009-03-27 10:34:33 -0500

    runreport.pl and prep work on Guided.pm
    The old location of runreport.pl was under tools, leaving it exposed
    to web requests.  This is a security flaw since it does NOT check any
    Auth and allows the user to request any Saved Report be run.  This is
    not a problem under misc/crontab/ and it suggests the more appropriate use.
    
    Guided.pm is not fixed here (see bug 3066), but it is prepared to be fixed
    and made compatible with runreport as detailed in the perldoc.
    
    Signed-off-by: Galen Charlton <galen.charlton@liblime.com>

--- C4/Reports/Guided.pm
+++ C4/Reports/Guided.pm
@@ -18,11 +18,13 @@ package C4::Reports::Guided;
 # Suite 330, Boston, MA  02111-1307 USA
 
 use strict;
+# use warnings;  # FIXME: this module needs a lot of repair to run clean under warnings
 use CGI;
 
 use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
 use C4::Context;
 use C4::Output;
+use C4::Dates;
 use XML::Simple;
 use XML::Dumper;
 use C4::Debug;
@@ -354,9 +356,14 @@ sub get_criteria {
 =cut
 
 # FIXME: This needs to be generalized to reports in general
+# FIXME: This should NOT have ANY formatting responsibilities.  
+# Instead, is should just be returning a prepared sth.
+# FIXME: $type is a TOTALLY UNUSED "required" argument?
 
 sub execute_query ($$$$;$$) {
     my ( $sql, $type, $offset, $limit, $format, $id ) = @_;
+    $format or $format = '';
+    $debug and print STDERR "execute_query($sql, $type, $offset, $limit, $format, $id)\n";
     my @params;
     my $total = 0;
     my ($useroffset, $userlimit);
@@ -424,12 +431,15 @@ sub execute_query ($$$$;$$) {
         while ( my @data = $sth->fetchrow_array() ) {
             # if the field is a date field, it needs formatting
             foreach my $data (@data) {
+                unless (defined $data) {
+                    $data = ''; # suppress undef, so fields are joinable
+                    next;
+                }
                 next unless $data =~ C4::Dates->regexp("iso");
-                my $date = C4::Dates->new($data, "iso");
-                $data = $date->output();
+                $data = C4::Dates->new($data, "iso")->output();
             }
             # tabular
-            my %temphash;
+            my %temphash;   # FIXME: already declared %temphash in same scope.
             my $row = join( '</td><td>', @data );
             $row = "<tr><td>$row</td></tr>";
             $temphash{'row'} = $row;
@@ -445,7 +455,7 @@ sub execute_query ($$$$;$$) {
                 $string .="\n" . $row;
             }
             elsif ($format eq 'url'){
-                my $temphash;
+                my $temphash;   # FIXME: already declared %temphash in same scope.  TWICE!!
                 @$temphash{@$colnames}=@data;
                 push @xmlarray,$temphash;
             }
