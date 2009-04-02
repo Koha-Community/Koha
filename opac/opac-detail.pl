b15a74c8d7168938d8dae28f8cc6709a110dccfc
Christopher Hyde <chyde@ebpl.org>
2009-04-02 10:50:35 -0400

    Changes Made to further optimize and condense code, include code documentation, and fix hardcoded instead of syspref'd library thing id
    Signed-off-by: Galen Charlton <galen.charlton@liblime.com>

--- opac/opac-detail.pl
+++ opac/opac-detail.pl
@@ -357,17 +357,16 @@ if ( C4::Context->preference("SyndeticsEnabled")
     };
     warn $@ if $@;
 }
-# LibraryThingForLibraries 
+
+# LibraryThingForLibraries ID Code and Tabbed View Option
 if( C4::Context->preference('LibraryThingForLibrariesEnabled') ) 
 { 
- $template->param(LibraryThingForLibrariesID =>
+$template->param(LibraryThingForLibrariesID =>
 C4::Context->preference('LibraryThingForLibrariesID') ); 
-} 
-if( C4::Context->preference('LibraryThingForLibrariesEnabled') )
-{
- $template->param(LibraryThingForLibrariesTabbedView =>
+$template->param(LibraryThingForLibrariesTabbedView =>
 C4::Context->preference('LibraryThingForLibrariesTabbedView') );
-}
+} 
+
 
 # Babelthèque
 if ( C4::Context->preference("Babeltheque") ) {
