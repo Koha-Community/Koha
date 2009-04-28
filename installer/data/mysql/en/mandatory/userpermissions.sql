6dcdc281bab21d97b5add41b6528047f4d02997c
Paul Poulain <paul.poulain@biblibre.com>
2009-04-28 23:14:22 +0200

    granular perms for acquisitions

--- installer/data/mysql/en/mandatory/userpermissions.sql
+++ installer/data/mysql/en/mandatory/userpermissions.sql
@@ -15,5 +15,18 @@ INSERT INTO permissions (module_bit, code, description) VALUES
    (13, 'import_patrons', 'Import patron data'),
    (13, 'delete_anonymize_patrons', 'Delete old borrowers and anonymize circulation history (deletes borrower reading history)'),
    (13, 'batch_upload_patron_images', 'Upload patron images in batch or one at a time'),
-   (13, 'schedule_tasks', 'Schedule tasks to run')
+   (13, 'schedule_tasks', 'Schedule tasks to run'),
+   (13, 'batchmod', 'Perform batch modification of items'),
+   (13, 'batchdel', 'Perform batch deletion of items')
+   (11, 'vendors_manage', 'Manage vendors'),
+   (11, 'contracts_manage', 'Manage contracts'),
+   (11, 'period_manage', 'Manage periods'),
+   (11, 'budget_manage', 'Manage budgets'),
+   (11, 'budget_modify', 'Modify budget (can''t create lines, but can modify existing ones)'),
+   (11, 'planning_manage', 'Manage budget plannings'),
+   (11, 'order_manage', 'Manage orders & basket'),
+   (11, 'group_manage', 'Manage orders & basketgroups'),
+   (11, 'order_receive', 'Manage orders & basket')
+   (11, 'budget_add_del', 'Add and delete budgets (but cant modify budgets)')
 ;
+>>>>>>> granular perms for acquisitions:installer/data/mysql/en/mandatory/userpermissions.sql
--- installer/data/mysql/fr-FR/1-Obligatoire/userpermissions.sql
+++ installer/data/mysql/fr-FR/1-Obligatoire/userpermissions.sql
@@ -15,5 +15,15 @@ INSERT INTO permissions (module_bit, code, description) VALUES
    (13, 'import_patrons', 'Import patron data'),
    (13, 'delete_anonymize_patrons', 'Delete old borrowers and anonymize circulation history (deletes borrower reading history)'),
    (13, 'batch_upload_patron_images', 'Upload patron images in batch or one at a time'),
-   (13, 'schedule_tasks', 'Schedule tasks to run')
+   (13, 'schedule_tasks', 'Schedule tasks to run'),
+   (11, 'vendors_manage', 'Manage vendors'),
+   (11, 'contracts_manage', 'Gérer les contrats'),
+   (11, 'period_manage', 'Gérer les périodes'),
+   (11, 'budget_manage', 'Gérer les budgets'),
+   (11, 'budget_modify', 'Modifier les budgets (impossible de créer les lignes, mais possible de modifier celles qui existent'),
+   (11, 'planning_manage', 'Gérer de la planification des budgets'),
+   (11, 'order_manage', 'Gérer les commandes et les paniers'),
+   (11, 'group_manage', 'Gérer les commandes et les bons de commande'),
+   (11, 'order_receive', 'Gérer les réceptions')
+   (11, 'budget_add_del', 'Ajouter et supprimer les budgets (mais pas modifier)')
 ;
