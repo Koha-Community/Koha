2eb8039732d287b05e435899bda3a7281902947b
Paul Poulain <paul.poulain@biblibre.com>
2009-04-28 22:47:31 +0200

    mostly reindenting

--- C4/Input.pm
+++ C4/Input.pm
@@ -128,22 +128,22 @@ sub buildCGIsort {
 	if ($sth->rows>0){
 		my @values;
 		my %labels;
-		
+
 		for (my $i =0;$i<$sth->rows;$i++){
 			my $results = $sth->fetchrow_hashref;
- 			push @values, $results->{authorised_value};
- 			$labels{$results->{authorised_value}}=$results->{lib};
+			push @values, $results->{authorised_value};
+			$labels{$results->{authorised_value}}=$results->{lib};
 		}
- 		unshift(@values,"");
 		$CGISort= CGI::scrolling_list(
- 					-name => $input_name,
- 					-values => \@values,
- 					-labels => \%labels,
+					-name => $input_name,
+					-id =>   $input_name,
+					-values => \@values,
+					-labels => \%labels,
 					-default=> $data,
- 					-size => 1,
- 					-multiple => 0);
+					-size => 1,
+					-multiple => 0);
 	}
-	$sth->finish; 
+	$sth->finish;
 	return $CGISort;
 }
 END { }       # module clean-up code here (global destructor)
