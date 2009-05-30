ab9a7481ec8dffa12c9de14838268a3e21e1a08c
Garry Collum <gcollum@gmail.com>
2009-05-30 18:30:13 -0400

    Bug 3268: Adds the ability to edit the description of a holiday.
    Adds another radio button to the edit holiday form. This is a partial fix, because you cannot make an edit to a single holiday for multiple branches.
    
    Also fixed a small XHTML error and a wording error from my previous patch on holidays.tmpl (bug 3274).
    
    Signed-off-by: Galen Charlton <galen.charlton@liblime.com>
    Signed-off-by: Henri-Damien LAURENT <henridamien.laurent@biblibre.com>

--- tools/exceptionHolidays.pl
+++ tools/exceptionHolidays.pl
@@ -19,6 +19,7 @@ my $month = $input->param('showMonth');
 my $year = $input->param('showYear');
 my $title = $input->param('showTitle');
 my $description = $input->param('showDescription');
+my $holidaytype = $input->param('showHolidayType');
 
 my $calendardate = sprintf("%04d-%02d-%02d", $year, $month, $day);
 my $isodate = C4::Dates->new($calendardate, 'iso');
@@ -40,6 +41,29 @@ if ($input->param('showOperation') eq 'exception') {
 									    year => $year,
 						                title => $title,
 						                description => $description);
+} elsif ($input->param('showOperation') eq 'edit') {
+    if($holidaytype eq 'weekday') {
+      $calendar->ModWeekdayholiday(weekday => $weekday,
+                                   title => $title,
+                                   description => $description);
+    } elsif ($holidaytype eq 'daymonth') {
+      $calendar->ModDaymonthholiday(day => $day,
+                                    month => $month,
+                                    title => $title,
+                                    description => $description);
+    } elsif ($holidaytype eq 'ymd') {
+      $calendar->ModSingleholiday(day => $day,
+                                  month => $month,
+                                  year => $year,
+                                  title => $title,
+                                  description => $description);
+    } elsif ($holidaytype eq 'exception') {
+      $calendar->ModExceptionholiday(day => $day,
+                                  month => $month,
+                                  year => $year,
+                                  title => $title,
+                                  description => $description);
+    }
 } elsif ($input->param('showOperation') eq 'delete') {
 	$calendar->delete_holiday(weekday => $weekday,
 	                          day => $day,
