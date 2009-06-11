63e871dd401c33f288202358639bf098af596148
Jesse Weaver <pianohacker@gmail.com>
2009-06-11 14:49:15 -0600

    Bug 3482 Allow hold notices to be sent in print form
    This is done by saving the notices in the message_queue table with
    type 'print'. The notices are generated from a notice named
    HOLD_PRINT. At the end of the day, they are dumped to an HTML file and
    marked as sent by a new cronjob.
    
    This setup is intended to be temporary; modules/batch/ shouldn't be around
    forever.
    
    Mandatory SQL:
    INSERT INTO message_transport_types (message_transport_type) values ('print');

--- C4/Letters.pm
+++ C4/Letters.pm
@@ -41,7 +41,7 @@ BEGIN {
 	$VERSION = 3.01;
 	@ISA = qw(Exporter);
 	@EXPORT = qw(
-	&GetLetters &getletter &addalert &getalert &delalert &findrelatedto &SendAlerts
+	&GetLetters &getletter &addalert &getalert &delalert &findrelatedto &SendAlerts GetPrintMessages
 	);
 }
 
@@ -664,6 +664,26 @@ sub GetRSSMessages {
     
     return _get_unsent_messages( { message_transport_type => 'rss',
                                    limit                  => $params->{'limit'},
+                                   borrowernumber         => $params->{'borrowernumber'}, } );
+}
+
+=head2 GetPrintMessages
+
+=over 4
+
+my $message_list = GetPrintMessages( { borrowernumber => $borrowernumber } )
+
+Returns a arrayref of all queued print messages (optionally, for a particular
+person).
+
+=back
+
+=cut
+
+sub GetPrintMessages {
+    my $params = shift || {};
+    
+    return _get_unsent_messages( { message_transport_type => 'print',
                                    borrowernumber         => $params->{'borrowernumber'}, } );
 }
 
