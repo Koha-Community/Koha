d5131cf99e81c14def75362243281ed205b1052c
Joe Atzberger (siptest <atz4sip@arwen.metavore.com>
2009-06-16 12:11:35 -0500

    Expand interactive SIP tests.
    Also tweak interactive item dump to suppress huge fields (marc, marcxml).
    
    Signed-off-by: Galen Charlton <galen.charlton@liblime.com>
    Signed-off-by: Henri-Damien LAURENT <henridamien.laurent@biblibre.com>

--- C4/SIP/interactive_item_dump.pl
+++ C4/SIP/interactive_item_dump.pl
@@ -7,12 +7,38 @@ use strict;
 use ILS::Item;
 use Data::Dumper;
 
+my $compare = (@ARGV) ? shift : 0;
 while (1) {
 	print "Enter item barcode: ";
 	my $in = <>;
 	defined($in) or last;
 	chomp($in);
 	last unless $in;
-	my $patron = ILS::Item->new($in);
-	print Dumper($patron);
+	my $item = ILS::Item->new($in);
+    unless ($item) {
+        print "No item ($in)";
+        next;
+    }
+    for (qw(marc marcxml)) {
+        $item->{$_} = 'suppressed...';
+    }
+    my $queue = $item->hold_queue();
+    print "Item ($in): ", Dumper($item);
+    print "hold_queue: ", Dumper($queue);
+    my $holdernumber;
+	if ($queue and scalar(@$queue)) {
+        $holdernumber = $queue->[0]->{borrowernumber};
+        print "first borrowernumber: $holdernumber\n";
+    }
+    if ($compare) {
+        print "Enter patron barcode: ";
+        my $barcode = <>;
+        defined($barcode) or next;
+        chomp($barcode);
+        next unless $barcode;
+        my $x = ILS::Item::_barcode_to_borrowernumber($barcode) || 'UNDEF';
+        print  "         converts to: $x\n";
+        printf "         compares as: %s\n", 
+            ($item->barcode_is_borrowernumber($barcode,$holdernumber) ? 'TRUE' : 'FALSE');
+    }
 }
