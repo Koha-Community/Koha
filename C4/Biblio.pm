a073d84092dd0e01788ae64805d32097df4a96c1
Frédéric Demians <f.demians@tamil.fr>
2009-07-15 21:59:56 +0200

    Bug 3423 - In Cataloging, fields are not ordered by tag number
    In cataloging, depending on installation and how biblio framework has been
    created/modified, fields are not displayed ordered by tag number. For example,
    in UNIMARC you can have in tab '2':
    
      225
      200
      210
    
    This patch order fields in tabs by tag and letter in the tag.
    
    Signed-off-by: Galen Charlton <galen.charlton@liblime.com>

--- C4/Biblio.pm
+++ C4/Biblio.pm
@@ -938,6 +938,7 @@ sub GetUsedMarcStructure($){
         FROM   marc_subfield_structure
         WHERE   tab > -1 
             AND frameworkcode = ?
+        ORDER BY tagfield, tagsubfield
     /;
     my @results;
     my $sth = $dbh->prepare($query);
