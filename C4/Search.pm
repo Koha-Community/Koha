b13cea0a02b4bf0b122f3396cbd0459742420d7e
Henri-Damien LAURENT <henridamien.laurent@biblibre.com>
2009-07-20 12:27:11 +0200

    Bug Fixing C4::Search } missing

--- C4/Search.pm
+++ C4/Search.pm
@@ -2147,6 +2147,7 @@ sub z3950_search_args {
         push @$array, { name=>$field, value=>$bibrec->{$field}, encvalue=>$encvalue } if defined $bibrec->{$field};
     }
     return $array;
+}
 
 =head2 BiblioAddAuthorities
 
