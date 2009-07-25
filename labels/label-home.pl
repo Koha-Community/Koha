b44db285a6392839360fc0941f35e8e9fd39a2d4
Chris Nighswonger <cnighswonger@foundations.edu>
2009-07-24 23:02:09 -0400

    [10/40] Work on Label Tool Homepage.

--- labels/label-home.pl
+++ labels/label-home.pl
@@ -4,18 +4,14 @@ use strict;
 use CGI;
 use C4::Auth;
 use C4::Output;
-use C4::Labels;
-use C4::Output;
 use C4::Context;
 use HTML::Template::Pro;
 
-# use Smart::Comments;
-
-my $query = new CGI;
+my $cgi = new CGI;
 my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
     {
         template_name   => "labels/label-home.tmpl",
-        query           => $query,
+        query           => $cgi,
         type            => "intranet",
         authnotrequired => 0,
         flagsrequired   => { catalogue => 1 },
@@ -23,64 +19,4 @@ my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
     }
 );
 
-# little block for displaying active layout/template/batch in templates
-# ----------
-my $batch_id     = $query->param('batch_id');
-my $active_layout = get_active_layout();
-my $active_template = GetActiveLabelTemplate();
-my $active_layout_name = $active_layout->{'layoutname'};
-my $active_template_name = $active_template->{'tmpl_code'};
-# ----------
-
-my $data = get_label_options();
-my $op =  $query->param('op');
-my $layout_id =  $query->param('layout_id');
-
-my @label_templates = GetAllLabelTemplates();
-my @printingtypes       = get_printingtypes();
-my @layouts       = get_layouts();
-my @barcode_types = get_barcode_types();
-#my @batches = get_batches();   #This is not used afaics -fbcit
-
-if ($op eq 'delete_layout') {	# had been assignment! serious error!
-	delete_layout($layout_id);
-}
-
-###  $data
-
-$template->param( guidebox => 1 ) if ( $data->{'guidebox'} );
-$template->param( "papertype_$data->{'papertype'}"       => 1 );
-$template->param( "$data->{'barcodetype'}_checked" => 1 );
-$template->param( "startrow" . $data->{'startrow'} . "_checked" => 1 );
-$template->param(
-    op => $op,
-    active_layout_name => $active_layout_name,
-    active_template_name => $active_template_name,
-
-    label_templates => \@label_templates,
-    barcode_types   => \@barcode_types,
-    printingtypes    => \@printingtypes,
-layout_loop => \@layouts,
-
-#batches => \@batches, #This is not used afaics -fbcit
-     id             => $data->{'id'},
-    barcodetype    => $data->{'barcodetype'},
-      papertype => $data->{'papertype'},
-
-    tx_author         => $data->{'author'},
-    tx_barcode        => $data->{'barcode'},
-   tx_title          => $data->{'title'},
-    tx_isbn           => $data->{'isbn'},
-    tx_issn           => $data->{'issn'},
-    tx_itemtype            => $data->{'itemtype'},
-    tx_dewey          => $data->{'dewey'},
-    tx_class          => $data->{'class'},
-    tx_subclass       => $data->{'subclass'},
-    tx_itemcallnumber => $data->{'itemcallnumber'},
-
-    startlabel     => $data->{'startlabel'},
-    fontsize       => $active_template->{'fontsize'},
-
-);
-
-output_html_with_http_headers $query, $cookie, $template->output;
+output_html_with_http_headers $cgi, $cookie, $template->output;
