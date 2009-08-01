ed62c0828812a234376a6ac667f14ec8dcf1f296
Sébastien Hinderer <Sebastien.Hinderer@ens-lyon.org>
2009-08-01 13:05:55 +0200

    Database structure modification.
    Adds the country and B_country columns to the borrowers table.
    
    Signed-off-by: Galen Charlton <gmcharlt@gmail.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -2535,6 +2535,22 @@ if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
     print "Upgrade to $DBversion done (added a preference to hide the patrons name in the staff catalog)";
 }
 
+=item
+
+    Deal with borrowers
+
+=cut
+
+$DBversion = "3.01.00.039";
+if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
+    # update borrowers table
+    # 
+    $dbh->do("ALTER TABLE borrowers ADD `country` text AFTER zipcode");
+    $dbh->do("ALTER TABLE borrowers ADD `B_country` text AFTER B_zipcode");
+    print "Upgrade to $DBversion done (branches)\n";
+    SetVersion ($DBversion);
+}
+
 =item DropAllForeignKeys($table)
 
   Drop all foreign keys of the table $table
