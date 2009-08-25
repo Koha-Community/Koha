b459a5df5c1392f62234a8e324a31770f0ef60ea
Henri-Damien LAURENT <henridamien.laurent@biblibre.com>
2009-08-26 01:40:28 +0200

    Bug Fixing : export would export no biblios when
    IndependantBranches set and user is kohaadmin
    I think we can escape defaulting to the userenv Branch with
    restricting the list to the user branch
    
    Signed-off-by: Galen Charlton <gmcharlt@gmail.com>

--- tools/export.pl
+++ tools/export.pl
@@ -80,8 +80,8 @@ if ($op eq "export") {
         ($itemtype && C4::Context->preference('item-level_itypes'));
     my $query = $items_filter ?
         "SELECT DISTINCT biblioitems.biblionumber
-         FROM biblioitems,items
-         WHERE biblioitems.biblionumber=items.biblionumber "
+         FROM biblioitems JOIN items
+         USING (biblionumber) WHERE 1"
         :
         "SELECT biblioitems.biblionumber FROM biblioitems WHERE biblionumber >0 ";
                   
@@ -123,6 +123,7 @@ if ($op eq "export") {
         $query .= (C4::Context->preference('item-level_itypes')) ? " AND items.itype = ? " : " AND biblioitems.itemtype = ?";
         push @sql_params, $itemtype;
     }
+    warn "$query, @sql_params";
     my $sth = $dbh->prepare($query);
     $sth->execute(@sql_params);
     
