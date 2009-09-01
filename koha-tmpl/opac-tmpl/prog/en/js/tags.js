26878175769fec391cfd6a586371f88c4a9b77fb
Henri-Damien LAURENT <henridamien.laurent@biblibre.com>
2009-09-01 21:46:03 +0200

    Adding strings to translate in tags.js
    Signed-off-by: Galen Charlton <gmcharlt@gmail.com>

--- koha-tmpl/opac-tmpl/prog/en/js/tags.js
+++ koha-tmpl/opac-tmpl/prog/en/js/tags.js
@@ -17,7 +17,7 @@ if (typeof(readCookie) == "undefined") {
 	readCookie = function (name) { // from http://www.quirksmode.org/js/cookies.html
 		var nameEQ = name + "=";
 		var ca = document.cookie.split(';');
-		for(var i=0;i < ca.length;i++) {
+		for (var i=0;i < ca.length;i++) {
 			var c = ca[i];
 			while (c.charAt(0)==' '){ c = c.substring(1,c.length); }
 			if (c.indexOf(nameEQ) == 0){ return c.substring(nameEQ.length,c.length); }
@@ -54,9 +54,9 @@ KOHA.Tags = {
 	},
 	common_status : function(addcount, delcount, errcount) {
 	    var cstat = "";
-	    if (addcount && addcount > 0) {cstat += "Added "   + addcount + (addcount==1 ? " tag" : " tags") + ".  " ;}
-	    if (delcount && delcount > 0) {cstat += "Deleted " + delcount + (delcount==1 ? " tag" : " tags") + ".  " ;}
-	    if (errcount && errcount > 0) {cstat += (errcount==1 ? "ERROR" : errcount + " ERRORS") + " during operation.";}
+	    if (addcount && addcount > 0) {cstat += _("Added ")   + addcount + (addcount==1 ? _(" tag") : _(" tags")) + ".  " ;}
+	    if (delcount && delcount > 0) {cstat += _("Deleted ") + delcount + (delcount==1 ? _(" tag") : _(" tags")) + ".  " ;}
+	    if (errcount && errcount > 0) {cstat += (errcount==1 ? _("ERROR") : errcount + _(" ERRORS")) + _(" during operation.");}
 	    return cstat;
 	},
 	set_tag_status : function(tagid, newstatus) {
