fabe0a4e1910b5da34bb429c87bfdf99b2fd061e
Matthias Meusburger <matthias.meusburger@biblibre.com>
2009-10-15 15:46:10 +0200

    Add missing opac authorised values column

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -3071,6 +3071,13 @@ if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
     print "Upgrade to $DBversion done (intranetbookbag syspref added)\n";
     SetVersion ($DBversion);
 }
+$DBversion = "3.01.00.117";
+if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
+    $dbh->do(  qq# ALTER TABLE authorised_values ADD COLUMN `lib_opac` VARCHAR(80) default NULL AFTER `lib` #);
+
+    print "Upgrade to $DBversion done (opac authorised values added)\n";
+    SetVersion ($DBversion);
+}
 
 
 =item DropAllForeignKeys($table)
