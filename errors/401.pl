6f3890a6eddbc3573badfd03ab18e1afd5640183
Sébastien Hinderer <Sebastien.Hinderer@ens-lyon.org>
2009-11-01 01:04:19 +0100

    Bugfix in errors/*.pl and opac/errors/*.pl
    The scripts did not define the status printed on the first line of
    HTTP headers correctly: they were sending 200 OK,
    which was confusing for browsers.
    
    Signed-off-by: Galen Charlton <gmcharlt@gmail.com>

--- errors/401.pl
+++ errors/401.pl
@@ -34,4 +34,4 @@ my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
     }
 );
 $template->param( admin => $admin );
-output_html_with_http_headers $query, $cookie, $template->output;
+output_with_http_headers $query, $cookie, $template->output, 'html', '401 Unauthorized';
