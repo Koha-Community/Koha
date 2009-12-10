e291d4c40161003bbb0f3c8652d8083eb325da4b
Chris Cormack <chrisc@catalyst.net.nz>
2009-12-10 15:02:38 +1300

    Bug 3861 cannot place hold in opac

--- opac/opac-reserve.pl
+++ opac/opac-reserve.pl
@@ -18,12 +18,12 @@
 use strict;
 use warnings;
 use CGI;
-use C4::Biblio;
-use C4::Items;
 use C4::Auth;    # checkauth, getborrowernumber.
 use C4::Koha;
 use C4::Circulation;
 use C4::Reserves;
+use C4::Biblio;
+use C4::Items;
 use C4::Output;
 use C4::Dates qw/format_date/;
 use C4::Context;
