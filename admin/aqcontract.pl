10c96100586e7272b35c0052cd5b684a54cae108
Jean-André Santoni <jeanandre.santoni@biblibre.com>
2009-12-10 12:00:46 +0100

    [MT1234] Code cleaning

--- admin/aqcontract.pl
+++ admin/aqcontract.pl
@@ -51,8 +51,8 @@ my $script_name    = "/cgi-bin/koha/admin/aqcontract.pl";
 my $contractnumber = $input->param('contractnumber');
 my $booksellerid   = $input->param('booksellerid');
 my $op             = $input->param('op') || '';
-my @bookseller = GetBookSellerFromId("$booksellerid");
 
+my @bookseller = GetBookSellerFromId($booksellerid);
 
 my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
     {   template_name   => "admin/aqcontract.tmpl",
@@ -69,7 +69,7 @@ $template->param(
     contractnumber => $contractnumber,
     searchfield    => $searchfield, 
     booksellerid   => $booksellerid,
-    name           => $bookseller[0]->{name},
+    booksellername => $bookseller[0]->{name},
     DHTMLcalendar_dateformat => C4::Dates->DHTMLcalendar(),
 );
 
@@ -77,7 +77,6 @@ $template->param(
 if ( $op eq 'add_form' ) {
     $template->param( add_form => 1 );
     my $data;
-    my $booksellername;
 
     #---- if primkey exists, it's a modify action, so read values to modify...
     if ($contractnumber) {
@@ -86,14 +85,6 @@ if ( $op eq 'add_form' ) {
         $sth->execute($contractnumber);
         $data = $sth->fetchrow_hashref;
         $sth->finish;
-
-        for ( @bookseller ) {
-            $booksellername = $$_{name} if $$_{id} eq $$data{booksellerid};
-        }
-    } else {
-        for ( @bookseller ) {
-            $booksellername = $$_{name} if $$_{id} eq $booksellerid;
-        }
     }
 
     $template->param(
@@ -102,54 +93,51 @@ if ( $op eq 'add_form' ) {
         contractdescription      => $data->{'contractdescription'},
         contractstartdate        => format_date( $data->{'contractstartdate'} ),
         contractenddate          => format_date( $data->{'contractenddate'} ),
-        booksellername           => $booksellername,
         DHTMLcalendar_dateformat => C4::Dates->DHTMLcalendar(),
     );
 
     # END $OP eq ADD_FORM
-
-    #ADD_VALIDATE: called by add_form, used to insert/modify data in DB
 }
+#ADD_VALIDATE: called by add_form, used to insert/modify data in DB
 elsif ( $op eq 'add_validate' ) {
 ## Please see file perltidy.ERR
-  $template->param( add_validate => 1 );
-  my $is_a_modif = $input->param("is_a_modif");
-  my $dbh        = C4::Context->dbh;
-  if ($is_a_modif) {
-      my $sth = $dbh->prepare(
-          "UPDATE aqcontract SET contractstartdate=?,
-                contractenddate=?,
-                contractname=?,
-                contractdescription=?,
-                booksellerid=? WHERE contractnumber=?"
-      );
-      $sth->execute(
-          format_date_in_iso( $input->param('contractstartdate') ),
-          format_date_in_iso( $input->param('contractenddate') ),
-          $input->param('contractname'),
-          $input->param('contractdescription'),
-          $input->param('booksellerid'),
-          $input->param('contractnumber')
-      );
-      $sth->finish;
-  } else {
-      my $sth = $dbh->prepare("INSERT INTO aqcontract  (contractname,contractdescription,booksellerid,contractstartdate,contractenddate) values (?, ?, ?, ?, ?)");
-      $sth->execute(
-          $input->param('contractname'),
-          $input->param('contractdescription'),
-          $input->param('booksellerid'),
-          format_date_in_iso( $input->param('contractstartdate') ),
-          format_date_in_iso( $input->param('contractenddate') )
-      );
-      $sth->finish;
-  }
-  print "Content-Type: text/html\n\n<META HTTP-EQUIV=Refresh CONTENT=\"0; URL=aqcontract.pl?booksellerid=$booksellerid\"></html>";
-  exit;
+    $template->param( add_validate => 1 );
+    my $is_a_modif = $input->param("is_a_modif");
+    my $dbh        = C4::Context->dbh;
+    if ($is_a_modif) {
+        my $sth = $dbh->prepare(
+            "UPDATE aqcontract SET contractstartdate=?,
+            contractenddate=?,
+            contractname=?,
+            contractdescription=?,
+            booksellerid=? WHERE contractnumber=?"
+        );
+        $sth->execute(
+            format_date_in_iso( $input->param('contractstartdate') ),
+            format_date_in_iso( $input->param('contractenddate') ),
+            $input->param('contractname'),
+            $input->param('contractdescription'),
+            $input->param('booksellerid'),
+            $input->param('contractnumber')
+        );
+        $sth->finish;
+    } else {
+        my $sth = $dbh->prepare("INSERT INTO aqcontract  (contractname,contractdescription,booksellerid,contractstartdate,contractenddate) values (?, ?, ?, ?, ?)");
+        $sth->execute(
+            $input->param('contractname'),
+            $input->param('contractdescription'),
+            $input->param('booksellerid'),
+            format_date_in_iso( $input->param('contractstartdate') ),
+            format_date_in_iso( $input->param('contractenddate') )
+        );
+        $sth->finish;
+    }
+    print "Content-Type: text/html\n\n<META HTTP-EQUIV=Refresh CONTENT=\"0; URL=aqcontract.pl?booksellerid=$booksellerid\"></html>";
+    exit;
 
-  # END $OP eq ADD_VALIDATE
-
-#DELETE_CONFIRM: called by default form, used to confirm deletion of data in DB
+    # END $OP eq ADD_VALIDATE
 }
+#DELETE_CONFIRM: called by default form, used to confirm deletion of data in DB
 elsif ( $op eq 'delete_confirm' ) {
     $template->param( delete_confirm => 1 );
 
@@ -178,9 +166,8 @@ elsif ( $op eq 'delete_confirm' ) {
     );
 
     # END $OP eq DELETE_CONFIRM
-
-    #DELETE_CONFIRMED: called by delete_confirm, used to effectively confirm deletion of data in DB
 }
+#DELETE_CONFIRMED: called by delete_confirm, used to effectively confirm deletion of data in DB
 elsif ( $op eq 'delete_confirmed' ) {
     $template->param( delete_confirmed => 1 );
     my $dbh            = C4::Context->dbh;
@@ -192,30 +179,22 @@ elsif ( $op eq 'delete_confirmed' ) {
     exit;
 
     # END $OP eq DELETE_CONFIRMED
-    # DEFAULT: Builds a list of contracts and displays them
-} else {
+}
+# DEFAULT: Builds a list of contracts and displays them
+else {
     $template->param(else => 1);
     my @loop;
     my ($count,$results)=StringSearch($searchfield);
-    my $toggle = 0;
     for (my $i=0; $i < $count; $i++){
         if ( ($input->param('booksellerid') && $results->[$i]{'booksellerid'} == $input->param('booksellerid')) || ! $input->param('booksellerid') ) {
-            my %row = (contractnumber => $results->[$i]{'contractnumber'},
-                    contractname => $results->[$i]{'contractname'},
-                    contractdescription => $results->[$i]{'contractdescription'},
-                    contractstartdate => format_date($results->[$i]{'contractstartdate'}),
-                    contractenddate => format_date($results->[$i]{'contractenddate'}),
-                    booksellerid => $results->[$i]{'booksellerid'},
-                    toggle => $toggle );
-            push @loop, \%row;
-            if ( $toggle eq 0 )
-            {
-                $toggle = 1;
-            }
-            else
-            {
-                $toggle = 0;
-            }
+            push @loop, {
+                contractnumber => $results->[$i]{'contractnumber'},
+                contractname => $results->[$i]{'contractname'},
+                contractdescription => $results->[$i]{'contractdescription'},
+                contractstartdate => format_date($results->[$i]{'contractstartdate'}),
+                contractenddate => format_date($results->[$i]{'contractenddate'}),
+                booksellerid => $results->[$i]{'booksellerid'},
+            };
         }
     }
     for my $contract (@loop) {
