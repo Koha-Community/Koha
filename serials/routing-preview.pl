b33641ed1cabf876b3da89591973950acf69ea5e
Chris Cormack <chrisc@catalyst.net.nz>
2009-12-15 13:57:25 +1300

    Bug 3903 - Members names not showing on serial routing list

--- serials/routing-preview.pl
+++ serials/routing-preview.pl
@@ -103,7 +103,7 @@ if($ok){
 my @results;
 my $data;
 for(my $i=0;$i<$routing;$i++){
-    $data=GetMember($routinglist[$i]->{'borrowernumber'},'borrowernumber');
+    $data=GetMember('borrowernumber' => $routinglist[$i]->{'borrowernumber'});
     $data->{'location'}=$data->{'branchcode'};
     $data->{'name'}="$data->{'firstname'} $data->{'surname'}";
     $data->{'routingid'}=$routinglist[$i]->{'routingid'};
