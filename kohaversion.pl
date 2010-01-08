a75558340adfe1681d6fa0476c21ab3ed4ab7c31
Chris Nighswonger <cnighswonger@foundations.edu>
2010-01-08 11:20:09 -0500

    [29/30] Bumping version number to 3.01.00.101

--- kohaversion.pl
+++ kohaversion.pl
@@ -3,14 +3,14 @@
 # - #1 : the major number. 3 atm
 # - #2 : the functionnal release. 00 atm
 # - #3 : the subnumber, moves only on a public release
-# - #4 : the developer version. The 4th number is the database subversion. 
+# - #4 : the developer version. The 4th number is the database subversion.
 #        used by developers when the database changes. updatedatabase take care of the changes itself
 #        and is automatically called by Auth.pm when needed.
 
 use strict;
 
 sub kohaversion {
-    our $VERSION = '3.01.00.100';
+    our $VERSION = '3.01.00.101';
     # version needs to be set this way
     # so that it can be picked up by Makefile.PL
     # during install
