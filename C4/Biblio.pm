8b4501f7c73503f6594db38e216d34e95bc03e2e
Galen Charlton <gmcharlt@gmail.com>
2010-02-02 19:26:04 -0500

    Revert "Populated MARC 952$2 field for serials-edit.pl"
    This reverts commit 708b4797e3fc253b8dac2a0fa0d16e30839196ef.
    
    Had introduced following error:
    
    Global symbol "$value" requires explicit package name at C4/Biblio.pm line 2381.
    
    Signed-off-by: Galen Charlton <gmcharlt@gmail.com>

--- C4/Biblio.pm
+++ C4/Biblio.pm
@@ -2370,22 +2370,6 @@ sub PrepareItemrecordDisplay {
                         }
 
                         #---- "true" authorised value
-                    }
-                    elsif ( $tagslib->{$tag}->{$subfield}->{authorised_value} eq
-                        "cn_source" )
-                    {
-                      my $class_sources = GetClassSources();
-                      my $default_source = C4::Context->preference("DefaultClassificationSource");
-                      foreach my $class_source (sort keys %$class_sources) {
-                        next unless $class_sources->{$class_source}->{'used'} or
-                                    ($value and $class_source eq $value)      or
-                                    ($class_source eq $default_source);
-                        push @authorised_values, $class_source;
-                        $authorised_lib{$class_source} = $class_sources->{$class_source}->{'description'};
-                      }
-                      $value = $default_source unless ($value);
-
-                      #---- "true" authorised value
                     } else {
                         $authorised_values_sth->execute( $tagslib->{$tag}->{$subfield}->{authorised_value} );
                         push @authorised_values, ""
