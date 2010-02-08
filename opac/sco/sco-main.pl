372ebb8bdd017a1b18c3f867944851ed7f5dbe98
Galen Charlton <gmcharlt@gmail.com>
2010-02-08 09:01:25 -0500

    set koha_login_context in order to get auto-login for SCO working
    Signed-off-by: Galen Charlton <gmcharlt@gmail.com>

--- opac/sco/sco-main.pl
+++ opac/sco/sco-main.pl
@@ -35,8 +35,9 @@ if (C4::Context->preference('AutoSelfCheckAllowed'))
 {
 	my $AutoSelfCheckID = C4::Context->preference('AutoSelfCheckID');
 	my $AutoSelfCheckPass = C4::Context->preference('AutoSelfCheckPass');
-	$query->param(-name=>'userid',-values=>[$AutoSelfCheckID]),
-	$query->param(-name=>'password',-values=>[$AutoSelfCheckPass]),
+	$query->param(-name=>'userid',-values=>[$AutoSelfCheckID]);
+	$query->param(-name=>'password',-values=>[$AutoSelfCheckPass]);
+    $query->param(-name=>'koha_login_context',-values=>['sco']);
 }
 my ($template, $loggedinuser, $cookie) = get_template_and_user({
     template_name   => "sco/sco-main.tmpl",
