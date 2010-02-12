33230acc2908972475131c4da7472096e78bef3b
Frédéric Demians <f.demians@tamil.fr>
2010-02-12 07:50:46 +0100

    Bug 4154 - Include preferences translation into install.pl
    Signed-off-by: Galen Charlton <gmcharlt@gmail.com>

--- misc/translator/install.pl
+++ misc/translator/install.pl
@@ -2,49 +2,31 @@
 # script to update all translations
 use strict;
 use warnings;
-use lib '/home/jmf/kohaclone';
-use C4::Languages;
-# Go through the theme/module combinations we need to update.
-my $dir = "po";
-my $po;
-opendir (DIR,$dir);
-while (defined($po = readdir(DIR))) {
-    next if $po =~ /(^\.\.?$|img|fam)/;
-    $po =~  /^(.*)-i-(staff|opac).*/;
-    print "lang: $1 interface:$2\n";
-    my ($lang,$interface) = ($1,$2);
-    $interface =~ s/staff/intranet/;
-    system("mkdir ../../koha-tmpl/$interface-tmpl/prog/$lang");
-    system("./tmpl_process3.pl install -i ../../koha-tmpl/$interface-tmpl/prog/en/ -o ../../koha-tmpl/$interface-tmpl/prog/$lang -s po/$po -r");
-}
+
+# Doesn't' handle anymore 'themes' since there is no theme .po files at all
+
+# Get all available language codes
+opendir my $fh, "po";
+my @langs =  map { ($_) =~ /(.*)-i-opac/ } 
+    grep { $_ =~ /.*-opac-/ } readdir($fh);
 closedir DIR;
 
-=head
-my($theme, $module) = @$spec;
-   my $pid = fork;
-   die "fork: $!\n" unless defined $pid;
-   if (!$pid) {
-
-      # If current directory is translator/po instead of translator,
-      # then go back to the parent
-      if ($chdir_needed_p) {
-	 chdir('..') || die "..: cd: $!\n";
-      }
-
-      # Now call tmpl_process3.pl to do the real work
-      #
-      # Traditionally, the pot file should be named PACKAGE.pot
-      # (for Koha probably something like koha_intranet_css.pot),
-      # but this is not Koha's convention.
-      #
-      my $target = "po/${theme}_${module}" . ($pot_p? ".pot": "_$lang.po");
-      rename($target, "$target~") if $pot_p;
-      exec('./tmpl_process3.pl', ($pot_p? 'create': 'update'),
-	    '-i', "../../koha-tmpl/$module-tmpl/$theme/en/",
-	    '-s', $target, '-r');
-
-      die "tmpl_process3.pl: exec: $!\n";
-   }
-   wait;
+# Map interface name to .po file suffix
+my %its = (
+    opac     => '-i-opac-t-prog-v-3002000.po',
+    intranet => '-i-staff-t-prog-v-3002000.po',
+);
+for my $lang ( @langs ) {
+    print "Language: $lang\n";
+    while ( my ($interface, $suffix) = each %its ) {
+        my $cmd = "mkdir ../../koha-tmpl/$interface-tmpl/prog/$lang";
+        system $cmd;
+        $cmd =
+            "./tmpl_process3.pl install " .
+            "-i ../../koha-tmpl/$interface-tmpl/prog/en/ " .
+            "-o ../../koha-tmpl/$interface-tmpl/prog/$lang ".
+            "-s po/$lang$suffix -r";
+        system $cmd;
+    }
+    system "./pref-trans install $lang";
 }
-=cut
