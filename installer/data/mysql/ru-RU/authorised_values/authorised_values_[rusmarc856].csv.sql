cd9c74c4170c927c552101671199bff38129ae24
Chris Cormack <chris@bigballofwax.co.nz>
2010-02-26 21:36:26 +1300

    Deleting unneeded Russian files - Thanks Serhij Dubyk

--- installer/data/mysql/ru-RU/authorised_values/authorised_values_[rusmarc856].csv.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `authorised_values` (`id`, `category`, `authorised_value`, `lib`, `imageurl`) VALUES (LAST_INSERT_ID( ) + 0,'rusmarc856','1#','FTP','NULL');
-INSERT INTO `authorised_values` (`id`, `category`, `authorised_value`, `lib`, `imageurl`) VALUES (LAST_INSERT_ID( ) + 1,'rusmarc856','0#','Электронная почта','NULL');
