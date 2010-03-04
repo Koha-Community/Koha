9be0f6d57a942e7b14966bcba5008e4ffc4b177a
Chris Cormack <chrisc@catalyst.net.nz>
2010-03-04 14:25:29 +1300

    Italian SQL updates
    Signed-off-by: Chris Cormack <chris@bigballofwax.co.nz>

---
+++ installer/data/mysql/it-IT/necessari/message_transport_types.sql
@@ -0,0 +1,9 @@
+SET FOREIGN_KEY_CHECKS=0;
+
+INSERT INTO message_transport_types
+(message_transport_type)
+values
+('email'),
+('sms');
+
+SET FOREIGN_KEY_CHECKS=1;
---
+++ installer/data/mysql/it-IT/necessari/parameters.sql
@@ -0,0 +1,8 @@
+SET FOREIGN_KEY_CHECKS=0;
+
+INSERT INTO `currency` (currency, rate, symbol) VALUES
+('USD', 1.4, '$'),
+('GBP', .8, '£'),
+('EUR', 1, '€');
+
+SET FOREIGN_KEY_CHECKS=1;
