b340fb834c11564738fe8f1c1a93af7a641ba2ca
Galen Charlton <gmcharlt@gmail.com>
2010-03-10 10:25:50 -0500

    bug 4314: add using_https environment variable
    Added new HTML template variable, using_https, for use
    of OPAC or staff-side templates that may need to know
    whether to use http:// or https:// links to off-site
    content.
    
    Signed-off-by: Galen Charlton <gmcharlt@gmail.com>

--- C4/Auth.pm
+++ C4/Auth.pm
@@ -353,6 +353,7 @@ sub get_template_and_user {
             XSLTDetailsDisplay           => C4::Context->preference("XSLTDetailsDisplay"),
             XSLTResultsDisplay           => C4::Context->preference("XSLTResultsDisplay"),
             BranchesLoop                 => GetBranchesLoop(),
+            using_https                  => $in->{'query'}->https() ? 1 : 0,
     );
 
     if ( $in->{'type'} eq "intranet" ) {
