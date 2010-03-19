3d86d6af53281c267d41d332489b41ffa061e596
Colin Campbell <colin.campbell@ptfs-europe.com>
2010-03-19 16:09:30 +0000

    Refactor for Human Readability
    Fixes  generation of empty index page when total_parcels is a multiple of pagesize
    Remove some calculations whose results were not used
    Make variable names meaningful (results and count arnt)
    Fix errors in pod syntax
    Make what code does clearer to readers who are not computers
    Use a subroutine
    
    Signed-off-by: Galen Charlton <gmcharlt@gmail.com>

--- acqui/parcels.pl
+++ acqui/parcels.pl
@@ -27,6 +27,7 @@
 parcels.pl
 
 =head1 DESCRIPTION
+
 This script shows all orders/parcels receipt or pending for a given supplier.
 It allows to write an order/parcels as 'received' when he arrives.
 
@@ -35,9 +36,11 @@ It allows to write an order/parcels as 'received' when he arrives.
 =over 4
 
 =item supplierid
+
 To know the supplier this script has to show orders.
 
 =item orderby
+
 sort list of order by 'orderby'.
 Orderby can be equals to
     * datereceived desc (default value)
@@ -48,12 +51,15 @@ Orderby can be equals to
 =item filter
 
 =item datefrom
+
 To filter on date
 
 =item dateto
+
 To filter on date
 
 =item resultsperpage
+
 To know how many results have to be display / page.
 
 =back
@@ -61,6 +67,7 @@ To know how many results have to be display / page.
 =cut
 
 use strict;
+use warnings;
 use CGI;
 use C4::Auth;
 use C4::Output;
@@ -69,92 +76,109 @@ use C4::Dates qw/format_date/;
 use C4::Acquisition;
 use C4::Bookseller;
 
-my $input=new CGI;
-my $supplierid=$input->param('supplierid');
-my $order=$input->param('orderby') || "datereceived desc";
-my $startfrom=$input->param('startfrom');
-my $code=$input->param('filter');
-my $datefrom=$input->param('datefrom');
-my $dateto=$input->param('dateto');
+my $input          = CGI->new;
+my $supplierid     = $input->param('supplierid');
+my $order          = $input->param('orderby') || 'datereceived desc';
+my $startfrom      = $input->param('startfrom');
+my $code           = $input->param('filter');
+my $datefrom       = $input->param('datefrom');
+my $dateto         = $input->param('dateto');
 my $resultsperpage = $input->param('resultsperpage');
+$resultsperpage ||= 20;
 
+my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
+    {   template_name   => 'acqui/parcels.tmpl',
+        query           => $input,
+        type            => 'intranet',
+        authnotrequired => 0,
+        flagsrequired   => { acquisition => 1 },
+        debug           => 1,
+    }
+);
 
-my ($template, $loggedinuser, $cookie)
-    = get_template_and_user({template_name => "acqui/parcels.tmpl",
-                 query => $input,
-                 type => "intranet",
-                 authnotrequired => 0,
-                 flagsrequired => {acquisition => 'order_receive'},
-                 debug => 1,
-});
-
-
-my $bookseller=GetBookSellerFromId($supplierid);
-$resultsperpage = 20 unless ($resultsperpage);
-my @results =GetParcels($supplierid, $order, $code,$datefrom,$dateto);
-my $count = scalar @results;
+my $bookseller = GetBookSellerFromId($supplierid);
+my @parcels = GetParcels( $supplierid, $order, $code, $datefrom, $dateto );
+my $count_parcels = @parcels;
 
 # multi page display gestion
-$startfrom=0 unless ($startfrom);
-if ($count>$resultsperpage){
-    my $displaynext=0;
-    my $displayprev=$startfrom;
-    if(($count - ($startfrom+$resultsperpage)) > 0 ) {
+$startfrom ||= 0;
+if ( $count_parcels > $resultsperpage ) {
+    set_page_navigation( $count_parcels, $startfrom, $resultsperpage );
+}
+my $loopres = [];
+
+my $next_page_start = $startfrom + $resultsperpage;
+my $last_row = ( $next_page_start < $count_parcels  ) ? $next_page_start - 1 : $count_parcels - 1;
+for my $i ( $startfrom .. $last_row) {
+    my $p = $parcels[$i];
+
+    push @{$loopres},
+      { number           => $i + 1,
+        code             => $p->{booksellerinvoicenumber},
+        nullcode         => $p->{booksellerinvoicenumber} eq 'NULL',
+        emptycode        => $p->{booksellerinvoicenumber} eq q{},
+        raw_datereceived => $p->{datereceived},
+        datereceived     => format_date( $p->{datereceived} ),
+        bibcount         => $p->{biblio},
+        reccount         => $p->{itemsreceived},
+        itemcount        => $p->{itemsexpected},
+      };
+}
+if ($count_parcels) {
+    $template->param( searchresults => $loopres, count => $count_parcels );
+}
+$template->param(
+    orderby                  => $order,
+    filter                   => $code,
+    datefrom                 => $datefrom,
+    dateto                   => $dateto,
+    resultsperpage           => $resultsperpage,
+    name                     => $bookseller->{'name'},
+    DHTMLcalendar_dateformat => C4::Dates->DHTMLcalendar(),
+    datereceived_today       => C4::Dates->new()->output(),
+    supplierid               => $supplierid,
+    GST                      => C4::Context->preference('gist'),
+);
+
+output_html_with_http_headers $input, $cookie, $template->output;
+
+sub set_page_navigation {
+    my ( $total_rows, $startfrom, $resultsperpage ) = @_;
+    my $displaynext = 0;
+    my $displayprev = $startfrom;
+    my $next_row    = $startfrom + $resultsperpage;
+    my $prev_row    = $startfrom - $resultsperpage;
+
+    if ( $total_rows - $next_row > 0 ) {
         $displaynext = 1;
     }
 
-    my @numbers = ();
-    if ($count>$resultsperpage) {
-        for (my $i=1; $i<$count/$resultsperpage+1; $i++) {
-            if ($i<16) {
-                my $highlight=0;
-                ($startfrom/$resultsperpage==($i-1)) && ($highlight=1);
-                push @numbers, { number => $i,
-                    highlight => $highlight ,
-#                   searchdata=> "test",
-                    startfrom => ($i-1)*$resultsperpage};
-            }
+    # set up index numbers for paging
+    my $numbers = [];
+    if ( $total_rows > $resultsperpage ) {
+        my $pages = $total_rows / $resultsperpage;
+        if ( $total_rows % $resultsperpage ) {
+            ++$pages;
+        }
+
+        # set up page indexes for at max 15 pages
+        my $max_idx = ( $pages < 15 ) ? $pages : 15;
+        my $current_page = ( $startfrom / $resultsperpage ) - 1;
+        for my $idx ( 1 .. $max_idx ) {
+            push @{$numbers},
+              { number    => $idx,
+                startfrom => ( $idx - 1 ) * $resultsperpage,
+                highlight => ( $idx == $current_page ),
+              };
         }
     }
 
-    my $from = $startfrom*$resultsperpage+1;
-    my $to;
-    if($count < (($startfrom+1)*$resultsperpage)){
-        $to = $count;
-    } else {
-        $to = (($startfrom+1)*$resultsperpage);
-    }
-    $template->param(numbers=>\@numbers, 
-                     displaynext=>$displaynext,
-                     displayprev=>$displayprev,
-                     nextstartfrom=>(($startfrom+$resultsperpage<$count)?$startfrom+$resultsperpage:$count),
-                     prevstartfrom=>(($startfrom-$resultsperpage>0)?$startfrom-$resultsperpage:0)
-                    );
+    $template->param(
+        numbers     => $numbers,
+        displaynext => $displaynext,
+        displayprev => $displayprev,
+        nextstartfrom => ( ( $next_row < $total_rows ) ? $next_row : $total_rows ),
+        prevstartfrom => ( ( $prev_row > 0 ) ? $prev_row : 0 )
+    );
+    return;
 }
-my @loopres;
-
-for (my $i=$startfrom;$i<=($startfrom+$resultsperpage-1<$count-1?$startfrom+$resultsperpage-1:$count-1);$i++){
-
-    my %cell;
-    $cell{number}=$i+1;
-    $cell{code}=$results[$i]->{booksellerinvoicenumber};
-    $cell{nullcode}=$results[$i]->{booksellerinvoicenumber} eq "NULL";
-    $cell{emptycode}=$results[$i]->{booksellerinvoicenumber} eq '';
-    $cell{raw_datereceived}=$results[$i]->{datereceived};
-    $cell{datereceived}=format_date($results[$i]->{datereceived});
-    $cell{bibcount}=$results[$i]->{biblio};
-    $cell{reccount}=$results[$i]->{itemsreceived};
-    $cell{itemcount}=$results[$i]->{itemsexpected};
-    push @loopres, \%cell;
-}
-$template->param(searchresults=>\@loopres, count=>$count) if ($count);
-$template->param(orderby=>$order, filter=>$code, datefrom=>$datefrom,dateto=>$dateto, resultsperpage=>$resultsperpage);
-$template->param(
-        name => $bookseller->{'name'},
-        DHTMLcalendar_dateformat => C4::Dates->DHTMLcalendar(),
-		datereceived_today => C4::Dates->new()->output(),
-		supplierid => $supplierid,
-	    GST => C4::Context->preference("gist"),
-        );
-
-output_html_with_http_headers $input, $cookie, $template->output;
