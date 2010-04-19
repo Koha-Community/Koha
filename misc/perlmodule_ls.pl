5647479f70e5cc822faecd1854dae2368d2611fa
Donovan Jones <donovan@catalyst.net.nz>
2010-04-19 15:17:27 +1200

    Bug 2505 - add strict and warnings to perlmodule_ls

--- misc/perlmodule_ls.pl
+++ misc/perlmodule_ls.pl
@@ -1,11 +1,12 @@
 #!/usr/bin/perl
 
-#use strict;
-#use warnings; FIXME - Bug 2505
+use strict;
+use warnings;
 
 use ExtUtils::Installed;
+
 my $instmod = ExtUtils::Installed->new();
 foreach my $module ($instmod->modules()) {
-my $version = $instmod->version($module) || "???";
-       print "$module -- $version\n";
+    my $version = $instmod->version($module) || "???";
+    print "$module -- $version\n";
 }
