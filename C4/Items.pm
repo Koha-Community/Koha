603574e62bd1ec2eeec9421f8be986dbfc3f9bbc
Nahuel ANGELINETTI <nahuel.angelinetti@biblibre.com>
2010-04-23 13:54:30 +0200

    (bug #4263) dateaccessionned is cleaned on item modification
    Every item modification, date accessionned is cleaned, if there is no modification made, we must'nt reset to "undef" the value.

--- C4/Items.pm
+++ C4/Items.pm
@@ -422,8 +422,9 @@ sub ModItemFromMarc {
 	my $localitemmarc=MARC::Record->new;
 	$localitemmarc->append_fields($item_marc->field($itemtag));
     my $item = &TransformMarcToKoha( $dbh, $localitemmarc, $frameworkcode, 'items');
+
     foreach my $item_field (keys %default_values_for_mod_from_marc) {
-        $item->{$item_field} = $default_values_for_mod_from_marc{$item_field} unless exists $item->{$item_field};
+        $item->{$item_field} = $default_values_for_mod_from_marc{$item_field} unless (exists $item->{$item_field} or not $default_values_for_mod_from_marc{$item_field});
     }
     my $unlinked_item_subfields = _get_unlinked_item_subfields($localitemmarc, $frameworkcode);
 
