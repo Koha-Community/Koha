891a0248f75748d46a7a2c1ceb9b1409158f5275
Chris Cormack <chrisc@catalyst.net.nz>
2010-05-07 11:24:23 +1200

    wr-70205 Rental discount not being respected
    Signed-off-by: Galen Charlton <gmcharlt@gmail.com>

--- C4/Circulation.pm
+++ C4/Circulation.pm
@@ -2288,7 +2288,7 @@ sub GetIssuingCharges {
         my $q2 = "SELECT rentaldiscount FROM borrowers
             LEFT JOIN issuingrules ON borrowers.categorycode = issuingrules.categorycode
             WHERE borrowers.borrowernumber = ?
-            AND issuingrules.itemtype = ?";
+            AND (issuingrules.itemtype = ? OR issuingrules.itemtype = '*')";
         my $sth2 = $dbh->prepare($q2);
         $sth2->execute( $borrowernumber, $item_type );
         if ( my $data2 = $sth2->fetchrow_hashref ) {
