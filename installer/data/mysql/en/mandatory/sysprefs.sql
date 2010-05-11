853bc200b35848c9f6fb7752ef252ce53ef5a45b
Chris Cormack <chrisc@catalyst.net.nz>
2010-05-11 17:13:59 +1200

    Follow up to bug3093 patch
    Signed-off-by: Galen Charlton <gmcharlt@gmail.com>

--- installer/data/mysql/en/mandatory/sysprefs.sql
+++ installer/data/mysql/en/mandatory/sysprefs.sql
@@ -287,3 +287,4 @@ INSERT INTO `systempreferences` ( `variable` , `value` , `options` , `explanatio
 INSERT INTO `systempreferences` (variable,value,explanation,options,type) VALUES('ILS-DI','0','Enables ILS-DI services at OPAC.','','YesNo');
 INSERT INTO `systempreferences` (variable,value,explanation,options,type) VALUES('ILS-DI:Authorized_IPs','','.','Restricts usage of ILS-DI to some IPs','Free');
 INSERT INTO `systempreferences` (variable,value,explanation,options,type) VALUES ('OverduesBlockCirc','noblock','When checking out an item should overdues block checkout, generate a confirmation dialogue, or allow checkout','noblock|confirmation|block','Choice');
+INSERT INTO `systempreferences` (variable,value,explanation,options,type) VALUES ('DisplayMultiPlaceHold','1','Display the ability to place multiple holds or not','','YesNo');
