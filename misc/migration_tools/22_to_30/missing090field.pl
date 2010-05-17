d4aa764f801ac463d099378989c07fe12d86e6ba
Nahuel ANGELINETTI <nahuel.angelinetti@biblibre.com>
2010-05-17 15:43:37 +0200

    (bug #4518) enhance 2.2 to 3.0 scripts
    Signed-off-by: Chris Cormack <chrisc@catalyst.net.nz>
    (cherry picked from commit e0b029a4f5ff378291e86fa55e07841297d611e7)
    
    Signed-off-by: Chris Nighswonger <chris.nighswonger@gmail.com>

--- misc/migration_tools/22_to_30/missing090field.pl
+++ misc/migration_tools/22_to_30/missing090field.pl
@@ -21,7 +21,7 @@ use MARC::File::USMARC;
 $|=1;
 my $dbh = C4::Context->dbh;
 
-my $sth=$dbh->prepare("select m.biblionumber,b.biblioitemnumber from biblio m left join biblioitems b on b.biblionumber=m.biblionumber");
+my $sth=$dbh->prepare("SELECT biblionumber, biblioitemnumber FROM biblioitems");
 $sth->execute();
 
 my $i=1;
@@ -35,6 +35,8 @@ while (my ($biblionumber,$biblioitemnumber)=$sth->fetchrow ){
 sub MARCmodbiblionumber{
     my ($biblionumber,$biblioitemnumber,$record)=@_;
     
+    return unless $record;
+
     my ($tagfield,$biblionumtagsubfield) = &GetMarcFromKohaField("biblio.biblionumber","");
     my ($tagfield2,$biblioitemtagsubfield) = &GetMarcFromKohaField("biblio.biblioitemnumber","");
         
