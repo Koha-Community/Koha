572670d377bfe11357316c4f9d69a9c5f00e5649
Galen Charlton <gmcharlt@gmail.com>
2010-07-22 19:27:44 -0400

    bug 4989: set DB rev to 145
    Signed-off-by: Galen Charlton <gmcharlt@gmail.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -3694,12 +3694,13 @@ if (C4::Context->preference('Version') < TransformToNum($DBversion)){
     SetVersion ($DBversion);
 }
 
-$DBversion = "3.02.00.025";
+$DBversion = "3.01.00.145";
 if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
     $dbh->do("ALTER TABLE borrowers ADD KEY `guarantorid` (guarantorid);");
     print "Upgrade to $DBversion done (Add index on guarantorid)\n";
     SetVersion ($DBversion);
 }
+
 =item DropAllForeignKeys($table)
 
   Drop all foreign keys of the table $table
--- kohaversion.pl
+++ kohaversion.pl
@@ -11,7 +11,7 @@ use strict;
 
 sub kohaversion {
 
-    our $VERSION = '3.01.00.144';
+    our $VERSION = '3.01.00.145';
 
     # version needs to be set this way
     # so that it can be picked up by Makefile.PL
