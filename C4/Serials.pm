c19a56f9cd149bb6b1e587954edbdc3db969bdcf
Colin Campbell <colin.campbell@ptfs-europe.com>
2010-09-23 17:04:28 +0100

    bug_5245 Fix SQL syntax in GetItems
    Routine never executed query due to syntax error
    and returned undef
    made the call to fetchrow_array explicit as the old fetchrow alias
    is no longer documented in perldoc DBI
    
    Signed-off-by: Galen Charlton <gmcharlt@gmail.com>

--- C4/Serials.pm
+++ C4/Serials.pm
@@ -2082,15 +2082,15 @@ returns a count of items from serial matching the subscriptionid
 sub HasItems {
     my ($subscriptionid) = @_;
     my $dbh              = C4::Context->dbh;
-    my $query = qq|
+    my $query = q|
             SELECT COUNT(serialitems.itemnumber)
             FROM   serial 
 			LEFT JOIN serialitems USING(serialid)
-            WHERE  subscriptionid=? AND serialitems.serialid NOT NULL
+            WHERE  subscriptionid=? AND serialitems.serialid IS NOT NULL
         |;
     my $sth=$dbh->prepare($query);
     $sth->execute($subscriptionid);
-    my ($countitems)=$sth->fetchrow;
+    my ($countitems)=$sth->fetchrow_array();
     return $countitems;  
 }
 
