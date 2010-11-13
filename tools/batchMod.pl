3f5d9537db4b07a993cc0a11fd4c03acb1859d20
Owen Leonard <oleonard@myacpl.org>
2010-11-13 12:58:54 -0500

    Fix for Bug 4451 Batch item tool can't process file with Windows line endings
    Signed-off-by: Galen Charlton <gmcharlt@gmail.com>
    Signed-off-by: Chris Cormack <chrisc@catalyst.net.nz>

--- tools/batchMod.pl
+++ tools/batchMod.pl
@@ -169,7 +169,7 @@ if ($op eq "show"){
     my @contentlist;
     if ($filefh){
         while (my $content=<$filefh>){
-            chomp $content;
+            $content =~ s/[\r\n]*$//g;
             push @contentlist, $content if $content;
         }
 
