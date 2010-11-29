1880cc14e834f8c40c206f1500968b3f5b001501
Chris Nighswonger <chris.nighswonger@gmail.com>
2010-11-29 14:43:24 -0500

    Updating Version Number to 3.02.01.000
    Updating version number for 3.2.1 release.
    
    Signed-off-by: Chris Nighswonger <chris.nighswonger@gmail.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -3842,6 +3842,12 @@ if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
     SetVersion ($DBversion);
 }
 
+$DBversion = '3.02.01.000';
+if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
+    print "Upgrade to $DBversion done (Incrementing version for 3.2.1 release.)\n";
+    SetVersion ($DBversion);
+}
+
 =head1 FUNCTIONS
 
 =head2 DropAllForeignKeys($table)
