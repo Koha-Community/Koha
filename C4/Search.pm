f60ec1e18f2668f870231bffc9b424b413fe3c91
Ian Walls <ian.walls@bywatersolutions.com>
2011-01-30 21:43:55 -0500

    Bug 2341: items marked 'on order' not reserveable from search results
    Items created as part of the acquisitions process, and assigned the temporary notforloan value of -1,
    cannot be placed on hold from the search results in either the OPAC or staff client (the link is missing).
    
    This patch changes the evaluation of items->notforloan from a Boolean (if $items->{notforloan}) to a comparison
    (if $items->{notforloan} > 0).  Any notforloan status with a negative value can therefore be reserved.
    
    Signed-off-by: Nicole Engard <nengard@bywatersolutions.com>
    Signed-off-by: Chris Cormack <chrisc@catalyst.net.nz>
    (cherry picked from commit f692123bf1b2b843d348a897852018caa04de76f)
    
    Signed-off-by: Chris Nighswonger <chris.nighswonger@gmail.com>

--- C4/Search.pm
+++ C4/Search.pm
@@ -1610,7 +1610,7 @@ sub searchResults {
                 if (   $item->{wthdrawn}
                     || $item->{itemlost}
                     || $item->{damaged}
-                    || $item->{notforloan}
+                    || $item->{notforloan} > 0
 		    || $reservestatus eq 'Waiting'
                     || ($transfertwhen ne ''))
                 {
