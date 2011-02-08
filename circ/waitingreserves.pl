b2138a7ec64dead316286df4a822bf89d051f4cb
Christophe Croullebois <christophe.croullebois@biblibre.com>
2011-02-08 17:58:54 +0100

    Bug 6121: Holds awaiting pickup
    On holds over, when cancelling hold and returning to his branch a bug fill the branchtransfers.tobranch field with the branchname instead of the branch code.
    
    BibLibre MT5647
    
    Signed-off-by: Paul Poulain <paul.poulain@biblibre.com>
    Signed-off-by: Chris Cormack <chrisc@catalyst.net.nz>

--- circ/waitingreserves.pl
+++ circ/waitingreserves.pl
@@ -125,7 +125,8 @@ foreach my $num (@getreserves) {
     $getreserv{'itemnumber'}     = $gettitle->{'itemnumber'};
     $getreserv{'biblionumber'}   = $gettitle->{'biblionumber'};
     $getreserv{'barcode'}        = $gettitle->{'barcode'};
-    $getreserv{'homebranch'}     = GetBranchName($gettitle->{'homebranch'});
+    $getreserv{'branchname'}     = GetBranchName($gettitle->{'homebranch'});
+    $getreserv{'homebranch'}     = $gettitle->{'homebranch'};
     $getreserv{'holdingbranch'}  = $gettitle->{'holdingbranch'};
     $getreserv{'itemcallnumber'} = $gettitle->{'itemcallnumber'};
     if ( $gettitle->{'homebranch'} ne $gettitle->{'holdingbranch'} ) {
