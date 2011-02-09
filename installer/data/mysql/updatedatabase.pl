14a2c4433d51fe4755b6589054a1e8b2cbf1375b
Chris Nighswonger <chris.nighswonger@gmail.com>
2011-02-09 12:14:19 -0500

    Updating Version Number to 3.02.03.002

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -3924,7 +3924,7 @@ if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
     SetVersion ($DBversion);
 }
 
-$DBversion = '3.03.00.XXX';
+$DBversion = '3.02.03.002';
 if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
     $dbh->do( q|update language_descriptions set description = 'Nederlands' where lang = 'nl' and subtag = 'nl'|);
     $dbh->do( q|update language_descriptions set description = 'Dansk' where lang = 'da' and subtag = 'da'|);
