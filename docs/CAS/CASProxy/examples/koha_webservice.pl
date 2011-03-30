814e486e582abec1787b57228776beb7981423cd
Chris Cormack <chrisc@catalyst.net.nz>
2011-03-31 10:06:46 +1300

    Bug 5630 CAS improvements
    Squashed commit of the following:
    
    commit 0e13a5278e11b288e48190dc26f31e96d06598dd
    Author: Henri-Damien LAURENT <henridamien.laurent@biblibre.com>
    Date:   Wed Jan 19 21:24:39 2011 +0100
    
        Bug 5630 : fixing C4/Auth.pm
    
    commit b55abc7a0dc1ca43b2610a27246293e9a9346e18
    Author: Matthias Meusburger <matthias.meusburger@biblibre.com>
    Date:   Wed Jan 19 21:24:38 2011 +0100
    
        Bug 5630 : Adds CAS documentation
    
    commit df0098a6a65465e6e734f99f65fb453dd3fa11d1
    Author: Henri-Damien LAURENT <henridamien.laurent@biblibre.com>
    Date:   Wed Jan 19 21:24:37 2011 +0100
    
        Bug 5630 : ilsdi service AuthenticatePatron doesn't with CAS syspref on
    
        Signed-off-by: Henri-Damien LAURENT <henridamien.laurent@biblibre.com>
    
    commit 31c8f0c0facfafae011ad24c9d458c50f2fad296
    Author: Matthias Meusburger <matthias.meusburger@biblibre.com>
    Date:   Wed Jan 19 21:24:36 2011 +0100
    
        Bug 5630 : Adds the ability to authenticate against multiple CAS servers
    
    commit 9d0def826135d5756533dc0dcf8e0a107d1ac8fc
    Author: Henri-Damien LAURENT <henridamien.laurent@biblibre.com>
    Date:   Wed Jan 19 21:24:34 2011 +0100
    
        Auth_with_cas : removing a warning
    
        $sth was defined twice in a function
        Removing the second definition
    
    commit 5ee550e9a2bb7ab6bc09f14fced6ce0df8011eb0
    Author: Matthias Meusburger <matthias.meusburger@biblibre.com>
    Date:   Wed Jan 19 21:24:33 2011 +0100
    
        Bug 6012 : MT 2270: CAS proxy
    
        CAS Proxy
        Examples included are now really usable
    
        Signed-off-by: Chris Cormack <chrisc@catalyst.net.nz>
    Signed-off-by: Paul Poulain <paul.poulain@biblibre.com>
    
    Signed-off-by: Chris Cormack <chrisc@catalyst.net.nz>
    (cherry picked from commit 046c996c2fa8150b8c6a037976c9c22826e04b8b)
    
    Signed-off-by: Chris Nighswonger <chris.nighswonger@gmail.com>

---
+++ docs/CAS/CASProxy/examples/koha_webservice.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/perl
+
+# Copyright 2009 SARL BibLibre
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Koha; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+# Suite 330, Boston, MA  02111-1307 USA
+
+=head1 DESCRIPTION
+
+# Here is an exemple of a simple phony webservice, returning "Hello World" if the user is authenticated
+# The purpose is to show how CAS Proxy can work with koha
+# In this configuration, this page acts as a CAS Client, instead of the user's browser.
+# This page is meant to be called from a foreign application
+
+=head1 CGI PARAMETERS
+
+=item PT
+The Proxy Ticket, needed for check_api_auth, that will try to make the CAS Server validate it.
+
+=cut 
+
+use utf8;
+use strict;
+use warnings;
+binmode(STDOUT, ":utf8");
+
+use C4::Auth qw(check_api_auth);
+use C4::Output;
+use C4::Context;
+use CGI;
+
+my $cgi = new CGI;
+
+print CGI::header('-type'=>'text/plain', '-charset'=>'utf-8');
+
+# The authentication : if $cgi contains a PT parameter, and CAS is enabled (casAuthentication syspref),
+# a CAS Proxy authentication will take place
+my ( $status, $cookie_, $sessionID ) = check_api_auth( $cgi, {circulate => 'override_renewals'});
+
+if ($status ne 'ok') {
+    print "Authentication failed : $status";
+} else {
+    print "Hello World!";
+}
+exit 0;
+
