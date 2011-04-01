9e3dbeaf39f9327428b07877db203cb0d6a57977
Chris Cormack <chrisc@catalyst.net.nz>
2011-04-01 15:55:06 +1300

    Fixing FSF address in license statements

--- C4/Installer/PerlModules.pm
+++ C4/Installer/PerlModules.pm
@@ -239,8 +239,8 @@ This file is part of Koha.
 Koha is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software
 Foundation; either version 2 of the License, or (at your option) any later version.
 
-You should have received a copy of the GNU General Public License along with Koha; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
-Suite 330, Boston, MA  02111-1307 USA
+You should have received a copy of the GNU General Public License along with Koha; if not, write to the Free Software Foundation, Inc., 51 Franklin Street,
+Fifth Floor, Boston, MA 02110-1301 USA.
 
 =head1 DISCLAIMER OF WARRANTY
 
