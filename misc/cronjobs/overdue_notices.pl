a3e55473555ee4b3a3dd4af81f1b163519278775
Julian Maurice <julian.maurice@biblibre.com>
2011-04-04 16:52:03 +0200

    Bug 5098: Add csv headers in overdues
    Signed-off-by: Paul Poulain <paul.poulain@biblibre.com>
    Signed-off-by: Chris Cormack <chrisc@catalyst.net.nz>

--- misc/cronjobs/overdue_notices.pl
+++ misc/cronjobs/overdue_notices.pl
@@ -584,10 +584,14 @@ END_SQL
                 local $, = "\f";    # pagebreak
                 print @output_chunks;
         }
+        # Generate the content of the csv with headers
+        my $content = join(";", qw(title name surname address1 address2 zipcode city email itemcount itemsinfo due_date issue_date)) . "\n";
+        $content .= join( "\n", @output_chunks );
+            
         my $attachment = {
             filename => defined $csvfilename ? 'attachment.csv' : 'attachment.txt',
             type => 'text/plain',
-            content => join( "\n", @output_chunks )
+            content => $content, 
         };
 
         my $letter = {
