2fd51eb84447dd068d0f290648cfb74a866a933e
Chris Cormack <chrisc@catalyst.net.nz>
2011-04-05 16:50:36 +1200

    Bug 5917 using_https should be a global

--- installer/html-template-to-template-toolkit.pl
+++ installer/html-template-to-template-toolkit.pl
@@ -29,7 +29,11 @@ my $tmpl_out_dir     = 'koha-tt';
 
 # template toolkit variables NOT to scope, in other words, variables that need to remain global (case sensitive)
 my @globals = ("themelang","JacketImages","OPACAmazonCoverImages","GoogleJackets","BakerTaylorEnabled",
-"SyndeticsEnabled", "OpacRenewalAllowed", "item_level_itypes","noItemTypeImages");
+"SyndeticsEnabled", "OpacRenewalAllowed", "item_level_itypes","noItemTypeImages",
+"virtualshelves", "RequestOnOpac", "COinSinOPACResults", "OPACXSLTResultsDisplay",
+"OPACItemsResultsDisplay", "LibraryThingForLibrariesID", "opacuserlogin", "TagsEnabled",
+"TagsShowOnList", "TagsInputOnList","loggedinusername","AllowOnShelfHolds","opacbookbag",
+"OPACAmazonEnabled", "SyndeticsCoverImages","using_https");
 
 # Arguments:
 my $KOHA_ROOT;
