ef9c269d7a13ab8553b41005574bc65818d9821e
Chris Cormack <chrisc@catalyst.net.nz>
2011-04-05 14:57:13 +1200

    Incrementing database version

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -4217,7 +4217,7 @@ if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
     SetVersion ($DBversion);
 }
 
-$DBversion = "3.03.00.XXX";
+$DBversion = "3.03.00.043";
 if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
 
     $dbh->do("INSERT INTO authorised_values (category,authorised_value,lib,lib_opac) VALUES ('YES_NO','0','No','No')");
