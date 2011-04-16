26257530291ea7dad39e2f924714cb49aecee3ca
Chris Nighswonger <chris.nighswonger@gmail.com>
2011-04-16 13:32:39 -0400

    Fixing typo in kohaversion.pl

--- kohaversion.pl
+++ kohaversion.pl
@@ -16,7 +16,7 @@ the kohaversion is divided in 4 parts :
 use strict;
 
 sub kohaversion {
-    our $VERSION = '3.02.07.0022';
+    our $VERSION = '3.02.07.002';
     # version needs to be set this way
     # so that it can be picked up by Makefile.PL
     # during install
