e064aba47e2047df9feeac985573784f6eff5912
Ian Walls <ian.walls@bywatersolutions.com>
2011-05-03 11:37:46 -0400

    Bug 6294: {BIBLIONUMBER} parsing for SearchForTitleIn
    Adds support for parsing of the biblionumber into the SearchForTitleIn system preference, allowing
    external sites with APIs (like Facebook or Twitter) to do link-backs to the details page for the
    title.
    
    Sample code for implementing Facebook "like" links and Twitter "tweet this" are included on the bug report
    
    Signed-off-by: Nicole C. Engard <nengard@bywatersolutions.com>
    Signed-off-by: Chris Cormack <chrisc@catalyst.net.nz>

--- opac/opac-detail.pl
+++ opac/opac-detail.pl
@@ -562,6 +562,7 @@ if (my $search_for_title = C4::Context->preference('OPACSearchForTitleIn')){
     $dat->{title} ? $search_for_title =~ s/{TITLE}/$dat->{title}/g : $search_for_title =~ s/{TITLE}//g;
     $isbn ? $search_for_title =~ s/{ISBN}/$isbn/g : $search_for_title =~ s/{ISBN}//g;
     $marccontrolnumber ? $search_for_title =~ s/{CONTROLNUMBER}/$marccontrolnumber/g : $search_for_title =~ s/{CONTROLNUMBER}//g;
+    $search_for_title =~ s/{BIBLIONUMBER}/$biblionumber/g;
  $template->param('OPACSearchForTitleIn' => $search_for_title);
 }
 
