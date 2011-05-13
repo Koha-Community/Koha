fa203e9761481e4201f5d24c409eee8228204654
Zeno Tajoli <tajoli@cilea.it>
2011-05-13 09:27:54 +0200

    Bug 5462: Update of the italian sql files The bug is about strings that are used as variables by Template::Toolkit The strings need to be like valid perl variable names. To fix the bug we need to fix also .sql files. Others .sql files are OK. Italian .sql file needs this fix
    Signed-off-by: Chris Cormack <chrisc@catalyst.net.nz>
    Signed-off-by: Ian Walls <ian.walls@bywatersolutions.com>

--- installer/data/mysql/it-IT/necessari/sample_notices_message_attributes.sql
+++ installer/data/mysql/it-IT/necessari/sample_notices_message_attributes.sql
@@ -3,10 +3,9 @@ SET FOREIGN_KEY_CHECKS=0;
 insert into `message_attributes`
 (`message_attribute_id`, message_name, `takes_days`)
 values
-(1, 'Item DUE', 0),
-(2, 'Advance Notice', 1),
-(4, 'Hold Filled', 0),
-(5, 'Item Check-in', 0),
-(6, 'Item Checkout', 0);
-
+(1, 'Item_Due', 0),
+(2, 'Advance_Notice', 1),
+(4, 'Hold_Filled', 0),
+(5, 'Item_Check_in', 0),
+(6, 'Item_Checkout', 0);
 SET FOREIGN_KEY_CHECKS=1;
