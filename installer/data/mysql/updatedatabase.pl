c04c4265ad63238a5803aaca86da8efe564bbb61
Chris Cormack <chris@bigballofwax.co.nz>
2011-07-23 20:09:08 +1200

    Bug 5429 : Covers for detail page and database update
    Signed-off-by: Magnus Enger <magnus@enger.priv.no>
    Signed-off-by: Paul Poulain <paul.poulain@biblibre.com>
    Signed-off-by: Chris Cormack <chrisc@catalyst.net.nz>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -4384,6 +4384,13 @@ if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
     SetVersion ($DBversion);
 }
 
+$DBversion = "3.05.00.XXX";
+if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
+    $dbh->do("INSERT INTO systempreferences (variable,value,explanation,options,type) VALUES('OpenLibraryCovers',0,'If ON Openlibrary book covers will be show',NULL,'YesNo');");
+    print "Upgrade to $DBversion done (Add syspref OpenLibraryCovers)\n";
+    SetVersion($DBversion);
+}
+
 =head1 FUNCTIONS
 
 =head2 DropAllForeignKeys($table)
