16eb069c9270263db2c27b54734eeea950f0c931
Frère Sébastien Marie <semarie-koha@latrappe.fr>
2011-07-27 11:48:29 +0200

    Bug 5131 :restrict use of sort_by value to allowed values
    The user input for sort_by value was used without care, resulting the possibility for user to set any Template Variable to 1.
    
    This patch restrict the values to sort field.
    The list of allowd_sortby was taken from 'includes/resort_form.inc'.
    
    Signed-off-by: Chris Cormack <chrisc@catalyst.net.nz>
    Signed-off-by: Paul Poulain <paul.poulain@biblibre.com>
    Signed-off-by: Chris Cormack <chrisc@catalyst.net.nz>
    (cherry picked from commit 649573ad244b95e1b90df7d0925f31f735ae50f7)
    
    Signed-off-by: Chris Nighswonger <chris.nighswonger@gmail.com>

--- opac/opac-search.pl
+++ opac/opac-search.pl
@@ -276,10 +276,13 @@ my @sort_by;
 my $default_sort_by = C4::Context->preference('OPACdefaultSortField')."_".C4::Context->preference('OPACdefaultSortOrder') 
     if (C4::Context->preference('OPACdefaultSortField') && C4::Context->preference('OPACdefaultSortOrder'));
 
+my @allowed_sortby = qw /acqdate_asc acqdate_dsc author_az author_za call_number_asc call_number_dsc popularity_asc popularity_dsc pubdate_asc pubdate_dsc relevance title_az title_za/; 
 @sort_by = split("\0",$params->{'sort_by'}) if $params->{'sort_by'};
 $sort_by[0] = $default_sort_by if !$sort_by[0] && defined($default_sort_by);
 foreach my $sort (@sort_by) {
-    $template->param($sort => 1);   # FIXME: security hole.  can set any TMPL_VAR here
+    if ( $sort ~~ @allowed_sortby ) {
+        $template->param($sort => 1);
+    }
 }
 $template->param('sort_by' => $sort_by[0]);
 
