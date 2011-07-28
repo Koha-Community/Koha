19e4e59659244a71e6be8d74e15285ea4827ee9f
Chris Nighswonger <chris.nighswonger@gmail.com>
2011-07-28 08:38:24 -0400

    Updating Version Number to 3.04.03.000

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -4357,6 +4357,12 @@ if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
     SetVersion ($DBversion);
 }
 
+$DBversion = "3.04.03.000";
+if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
+    print "Upgrade to $DBversion done (Incrementing version for 3.4.3 release. See release notes for details.)\n";
+    SetVersion ($DBversion);
+}
+
 =head1 FUNCTIONS
 
 =head2 DropAllForeignKeys($table)
