12fbac7a4e08cb25aced02abee8c830755e86bac
Chris Cormack <chrisc@catalyst.net.nz>
2011-08-04 13:33:00 +1200

    Bumping database version

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -4384,7 +4384,7 @@ if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
     SetVersion ($DBversion);
 }
 
-$DBversion = "3.05.00.XXX";
+$DBversion = "3.05.00.007";
 if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
     $dbh->do("INSERT INTO systempreferences (variable,value,explanation,options,type) VALUES('OpenLibraryCovers',0,'If ON Openlibrary book covers will be show',NULL,'YesNo');");
     print "Upgrade to $DBversion done (Add syspref OpenLibraryCovers)\n";
