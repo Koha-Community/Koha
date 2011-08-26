d2c24f3bbff5c3ba96ce5ae8baa2313fabf786d5
Colin Campbell <colin.campbell@ptfs-europe.com>
2011-08-26 16:17:11 +0100

    Bug 6790: Remove unnecessary variable from getroutinglist return
    getroutinglist returns a count variable to indicate how many elements
    are in the array. This is almost always a serious code smell. (We are
    programming in a list manipulating language) The routine was executing
    am unnecessary loop just to maintain that var.
    Removed the variable from the routine and perldoc
    refactored calls of the routine removed the c-style loops for
    more idiomatic and maintainable for loops
    renamed some opaquely named variables
    removed a call to the routine where nothing was done with the data
    moved some html out of the calling script and into the template
    
    Signed-off-by: Chris Cormack <chris@bigballofwax.co.nz>
    Signed-off-by: Paul Poulain <paul.poulain@biblibre.com>

--- C4/Serials.pm
+++ C4/Serials.pm
@@ -2019,12 +2019,11 @@ sub delroutingmember {
 
 =head2 getroutinglist
 
-($count,@routinglist) = getroutinglist($subscriptionid)
+@routinglist = getroutinglist($subscriptionid)
 
 this gets the info from the subscriptionroutinglist for $subscriptionid
 
 return :
-a count of the number of members on routinglist
 the routinglist as an array. Each element of the array contains a hash_ref containing
 routingid - a unique id, borrowernumber, ranking, and biblionumber of subscription
 
@@ -2034,20 +2033,14 @@ sub getroutinglist {
     my ($subscriptionid) = @_;
     my $dbh              = C4::Context->dbh;
     my $sth              = $dbh->prepare(
-        "SELECT routingid, borrowernumber, ranking, biblionumber 
+        'SELECT routingid, borrowernumber, ranking, biblionumber
             FROM subscription 
             JOIN subscriptionroutinglist ON subscription.subscriptionid = subscriptionroutinglist.subscriptionid
-            WHERE subscription.subscriptionid = ? ORDER BY ranking ASC
-                              "
+            WHERE subscription.subscriptionid = ? ORDER BY ranking ASC'
     );
     $sth->execute($subscriptionid);
-    my @routinglist;
-    my $count = 0;
-    while ( my $line = $sth->fetchrow_hashref ) {
-        $count++;
-        push( @routinglist, $line );
-    }
-    return ( $count, @routinglist );
+    my $routinglist = $sth->fetchall_arrayref({});
+    return @{$routinglist};
 }
 
 =head2 countissuesfrom
