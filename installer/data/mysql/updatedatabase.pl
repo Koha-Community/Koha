1ffdbfa5e9c3f4d63244f0897ac9b8f5eb1177d2
Chris Cormack <chrisc@catalyst.net.nz>
2011-10-18 09:46:45 +1300

    Bumping database version

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -4516,7 +4516,7 @@ if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
     SetVersion($DBversion);
 }
 
-$DBversion = "3.05.00.XXX";
+$DBversion = "3.05.00.020";
 if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
     $dbh->do("INSERT INTO `systempreferences` (variable,value,options,explanation,type) VALUES ('AcqViewBaskets','user','user|branch|all','Define which baskets a user is allowed to view: his own only, any within his branch or all','Choice')");
     print "Upgrade to $DBversion done (Add syspref AcqViewBaskets)\n";
