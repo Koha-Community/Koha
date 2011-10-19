8935132c5d73eeb716fd269dcf69c61998b9177d
Chris Cormack <chrisc@catalyst.net.nz>
2011-10-20 03:20:57 +1300

    Bumping db version

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -4523,7 +4523,7 @@ if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
     SetVersion($DBversion);
 }
 
-$DBversion = "3.05.00.XXX";
+$DBversion = "3.05.00.021";
 if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
     $dbh->do("ALTER TABLE borrower_attribute_types ADD COLUMN display_checkout TINYINT(1) NOT NULL DEFAULT '0';");
     print "Upgrade to $DBversion done (Added a display_checkout field in borrower_attribute_types table)\n"; 
