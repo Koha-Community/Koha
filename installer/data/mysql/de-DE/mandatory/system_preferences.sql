5638111e53fa102328ac4183ad3fc14c406698bb
Katrin Fischer <Katrin.Fischer.83@web.de>
2011-10-24 14:57:52 +0200

    Bug 7079: [SIGNED-OFF] Default values for German system preferences
    Patch adds updates for system preferences to the sql file
    in the installer directory:
    
    - New: AmazonLocale: DE
    - borrowerRelationship: Vater|Mutter
    - BorrowersTitles: Herr|Frau
    - New: CurrencyFormat: FR
    - New: dateformat: metric
    - New: DefaultClassificationSource: z
    - New: OpacMainUserBlock: translated content
    - New: OpacNav: translated content
    - New: OpacNavBottom: translated content
    - New: OPACSearchForTitleIn: translated content
    
    To test:
    A) Do a full installation with German sample files
    B) Run the sql file 'system_preferences.sql' against your database
    
    Verfy values of system preferences have been changed.
    
    Signed-off-by: Magnus Enger <magnus@enger.priv.no>
    Patch applied cleanly. I ran through the webinstaller in German on an empty
    database, and made sure system_preferences were loaded. No complaints from
    the database and the systprefs are set to the right values when I check in the
    staff client.
    
    Signed-off-by: Paul Poulain <paul.poulain@biblibre.com>
    (cherry picked from commit c1d824deaa65f0a64071e6925ae2238beb0a0912)
    
    Signed-off-by: Chris Nighswonger <chris.nighswonger@gmail.com>

--- installer/data/mysql/de-DE/mandatory/system_preferences.sql
+++ installer/data/mysql/de-DE/mandatory/system_preferences.sql
@@ -16,5 +16,17 @@
 -- with Koha; if not, write to the Free Software Foundation, Inc.,
 -- 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+UPDATE systempreferences SET value = 'DE' WHERE variable = 'AmazonLocale';
 UPDATE systempreferences SET value = 'Vater|Mutter' WHERE variable = 'borrowerRelationship';
 UPDATE systempreferences SET value = 'Herr|Frau' WHERE variable = 'BorrowersTitles';
+UPDATE systempreferences SET value = 'FR' WHERE variable = 'CurencyFormat';
+UPDATE systempreferences SET value = 'metric' WHERE variable = 'dateformat';
+UPDATE systempreferences SET value = 'z' WHERE variable = 'DefaultClassificationSource';
+UPDATE systempreferences SET value = 'Willkommen im Koha-Katalog! (OpacMainUserBlock)' WHERE variable = 'OpacMainUserBlock';
+UPDATE systempreferences SET value = 'Wichtige Links hier...(OpacNav)' WHERE variable = 'OpacNav';
+UPDATE systempreferences SET value = '...oder hier (OpacNavBottom)' WHERE variable = 'OpacNavBottom';
+UPDATE systempreferences SET value = 
+    '<li><a href="http://worldcat.org/search?q={TITLE}" target="_blank">Andere Bibliotheken (WorldCat)</a></li>
+    <li><a href="http://www.scholar.google.com/scholar?q={TITLE}" target="_blank">Google Scholar</a></li>
+    <li><a href="http://www.bookfinder.com/search/?author={AUTHOR}&amp;title={TITLE}&amp;st=xl&amp;ac=qr" target="_blank">Online-Buchhandel (Bookfinder.com)</a></li>' 
+    WHERE variable = 'OPACSearchForTitleIn';
