8a84374562a88942a34904d1cec225f754a9a195
Mason James <mtj@kohaaloha.com>
2011-11-05 23:28:48 +1300

    Bug 7164 - add Koha history timeline tab to 'About' page
    Includes follow up by Chris Cormack fixing formatting in docs/history.txt
    
    [MINOR EDIT] Added html filter to output for validity's sake
    
    Signed-off-by: Owen Leonard <oleonard@myacpl.org>
    (cherry picked from commit 6bc414a3d418cd5255a178271207121f3cb9878a)
    
    Signed-off-by: Chris Nighswonger <chris.nighswonger@gmail.com>

--- about.pl
+++ about.pl
@@ -28,6 +28,8 @@ use C4::Auth;
 use C4::Context;
 use C4::Installer;
 
+#use Smart::Comments '####';
+
 my $query = new CGI;
 my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
     {
@@ -101,7 +103,46 @@ foreach (@components) {
         $row = [];
     }
 }
+## ## $table
 
 $template->param( table => $table );
+
+
+## ------------------------------------------
+## Koha time line code
+
+#get file location
+my $dir = C4::Context->config('intranetdir');
+open( FILE, "$dir" . "/docs/history.txt" );
+my $i = 0;
+
+my @rows2 = ();
+my $row2  = [];
+
+my @lines = <FILE>;
+close(FILE);
+
+shift @lines; #remove header row
+
+foreach (@lines) {
+    my ( $date, $desc, $tag ) = split(/\t/);
+    push(
+        @rows2,
+        {
+            date => $date,
+            desc => $desc,
+        }
+    );
+}
+
+my $table2 = [];
+#foreach my $row2 (@rows2) {
+foreach  (@rows2) {
+    push (@$row2, $_);
+    push( @$table2, { row2 => $row2 } );
+    $row2 = [];
+}
+
+$template->param( table2 => $table2 );
 
 output_html_with_http_headers $query, $cookie, $template->output;
