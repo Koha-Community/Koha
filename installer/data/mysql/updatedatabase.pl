e7a667bb8aa22ff6ce003694e83080f3a4d8968e
Chris Nighswonger <chris.nighswonger@gmail.com>
2011-11-09 13:54:18 -0500

    Updating Version Number to 3.04.06.000

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -4446,6 +4446,12 @@ if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
     SetVersion($DBversion);
 }
 
+$DBversion = "3.04.06.000";
+if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
+    print "Upgrade to $DBversion done (Incrementing version for 3.4.6 release. See release notes for details.)\n";
+    SetVersion ($DBversion);
+}
+
 =head1 FUNCTIONS
 
 =head2 DropAllForeignKeys($table)
--- kohaversion.pl
+++ kohaversion.pl
@@ -16,7 +16,7 @@ the kohaversion is divided in 4 parts :
 use strict;
 
 sub kohaversion {
-    our $VERSION = '3.04.05.003';
+    our $VERSION = '3.04.06.000';
     # version needs to be set this way
     # so that it can be picked up by Makefile.PL
     # during install
