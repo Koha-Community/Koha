40bd14e0ea98152377a0a416482fe0227fee17bd
Chris Cormack <chrisc@catalyst.net.nz>
2011-11-21 20:30:19 +1300

    Bug 4330: Missing License Statement
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>

--- opac/sco/sco-main.pl
+++ opac/sco/sco-main.pl
@@ -3,6 +3,22 @@
 # This code has been modified by Trendsetters (originally from opac-user.pl)
 # This code has been modified by rch
 # Parts Copyright 2010-2011, ByWater Solutions (those related to username/password auth)
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 # We're going to authenticate a self-check user.  we'll add a flag to borrowers 'selfcheck'
 #
 # We're in a controlled environment; we trust the user.
