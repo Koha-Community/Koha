e177c9baed2c0ab8418e2205072adc9f68ea69ae
Chris Cormack <chrisc@catalyst.net.nz>
2011-11-25 20:43:02 +1300

    Bug 6629 vulnerability fix
    Signed-off-by: Chris Nighswonger <chris.nighswonger@gmail.com>

--- C4/Output.pm
+++ C4/Output.pm
@@ -160,6 +160,7 @@ sub themelanguage {
       if $http_accept_language;
     # But, if there's a cookie set, obey it
     $lang = $query->cookie('KohaOpacLanguage') if (defined $query and $query->cookie('KohaOpacLanguage'));
+    $lang =~ s/[^a-zA-Z_-]*//g; 
     # Fall back to English
     my @languages;
     if ($interface eq 'intranet') {
