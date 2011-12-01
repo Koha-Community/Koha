c183f0a661a893390c2cf1b3dab80d88d3c52690
Frère Sébastien Marie <semarie-koha@latrappe.fr>
2011-12-01 19:02:11 +0100

    Bug 7282 - invalid language selection
    - ensure that without cookie, language selection is based on browser
      preferences
    - refactor function to obtain langugage for stem in search (opac + intranet)
    
    Signed-off-by: Frédéric Demians <f.demians@tamil.fr>
    
    To reproduce the bug:
    
    - Activate en and fr-FR for OPAC
    - Clear your browser cookies
    - Select your language preference in your browser: fr, fr-fr, en
    - Load OPAC main page
    - OPAC is displayed in English, rather than French as asked by browser
      preferences
    
      Apply the patch and test:
    
      - Clear your browser cookies
      - Load OPAC main page
      - Pages are displayed in French
    
    Signed-off-by: Paul Poulain <paul.poulain@biblibre.com>
    (cherry picked from commit 0ad7e76cb7cebf16310a95377f88047f09b66d24)
    
    Signed-off-by: Chris Nighswonger <chris.nighswonger@gmail.com>

--- opac/opac-search.pl
+++ opac/opac-search.pl
@@ -388,7 +388,7 @@ my ($error,$query,$simple_query,$query_cgi,$query_desc,$limit,$limit_cgi,$limit_
 my @results;
 
 ## I. BUILD THE QUERY
-my $lang = C4::Templates::getlanguagecookie($cgi);
+my $lang = C4::Templates::getlanguage($cgi, 'opac');
 ( $error,$query,$simple_query,$query_cgi,$query_desc,$limit,$limit_cgi,$limit_desc,$stopwords_removed,$query_type) = buildQuery(\@operators,\@operands,\@indexes,\@limits,\@sort_by, 0, $lang);
 
 sub _input_cgi_parse ($) { 
