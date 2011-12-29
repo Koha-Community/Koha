282ec272b12a6597705eea6c5b9d42f50ab6438e
Chris Cormack <chrisc@catalyst.net.nz>
2011-12-30 09:25:29 +1300

    Bug 7387 : Adding a caching plugin for Template::Toolkit
    To use this you need to do something like
    
    -[% INCLUDE 'doc-head-open.inc' %]
    +[% USE cache = Cache %]
    +[% cache.inc(
    +    'template' => 'doc-head-open.inc',
    +    'keys' => {'dochead' => 'dochead'},
    +    'ttl' => 360
    +    )
    +%]
    
    Signed-off-by: Jared Camins-Esakov <jcamins@cpbibliography.com>
    Signed-off-by: Bernardo Gonzalez Kriegel <bgkriegel@gmail.com>
    
    Comment: Works. Tested modifying as described opac-main.tt
    Follow up to fix tabulations.
    
    Signed-off-by: Elliott Davis <elliott@bywatersolions.com>
    Signed-off-by: Jared Camins-Esakov <jcamins@cpbibliography.com>

---
+++ Koha/Template/Plugin/Cache.pm
@@ -0,0 +1,120 @@
+package Koha::Template::Plugin::Cache;
+
+# Copyright Catalyst IT 2011
+
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use strict;
+use warnings;
+use vars qw( $VERSION );
+use base qw( Template::Plugin );
+use Template::Plugin;
+use C4::Context;
+$VERSION = '0.01';
+
+#------------------------------------------------------------------------
+# new(\%options)
+#------------------------------------------------------------------------
+
+sub new {
+    my ( $class, $context, $params ) = @_;
+    my $cache;
+    if ( $params->{cache} ) {
+        $cache = delete $params->{cache};
+    }
+    else {
+        require Koha::Cache;
+	$cache = Koha::Cache->new( { 'cache_type' => 'memcached', 'cache_servers' => C4::Context->config('memcached_servers') });
+    }
+    my $self = bless {
+        CACHE   => $cache,
+        CONFIG  => $params,
+        CONTEXT => $context,
+    }, $class;
+    return $self;
+}
+
+#------------------------------------------------------------------------
+# $cache->include({
+#                 template => 'foo.html',
+#                 keys     => {'user.name', user.name},
+#                 ttl      => 60, #seconds
+#                });
+#------------------------------------------------------------------------
+
+sub inc {
+    my ( $self, $params ) = @_;
+    $self->_cached_action( 'include', $params );
+}
+
+sub proc {
+    my ( $self, $params ) = @_;
+    $self->_cached_action( 'process', $params );
+}
+
+sub _cached_action {
+    my ( $self, $action, $params ) = @_;
+    my $key;
+    if ( $params->{key} ) {
+        $key = delete $params->{key};
+    }
+    else {
+        my $cache_keys = $params->{keys};
+        $key = join(
+            ':',
+            (
+                $params->{template},
+		map { "$_=$cache_keys->{$_}" } keys %{$cache_keys}
+            )
+        );
+    }
+    my $result = $self->{CACHE}->get_from_cache($key);
+    if ( !$result ) {
+	warn "here in not in cache";
+	$result = $self->{CONTEXT}->$action( $params->{template} );
+	$self->{CACHE}->set_in_cache( $key, $result, $params->{ttl} );
+    }
+    return $result;
+}
+
+1;
+
+=head1 NAME
+
+Koha::Template::Plugin::Cache - cache output of templates
+
+=head1 SYNOPSIS
+
+  [% USE cache = Cache %]
+
+
+  [% cache.inc(
+                   'template' => 'slow.html',
+                   'keys' => {'user.name' => user.name},
+                   'ttl' => 360
+                   ) %]
+
+  # or with a pre-defined Cache::* object and key
+
+  [% USE cache = Cache( cache => mycache ) %]
+
+  [% cache.inc(
+                       'template' => 'slow.html',
+                       'key'      => mykey,
+                       'ttl'      => 360
+                       )  %]
+
+__END__
