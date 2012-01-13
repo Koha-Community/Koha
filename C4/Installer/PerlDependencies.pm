c1ee7a750f090ee63e461bc5fe46ca08fc232245
Chris Cormack <chrisc@catalyst.net.nz>
2012-01-13 17:16:51 +1300

    Bug 6836: Adding dependency on Modern::Perl
    Signed-off-by: Julian Maurice <julian.maurice@biblibre.com>
    Signed-off-by: Katrin Fischer <Katrin.Fischer.83@web.de>
    Signed-off-by: Paul Poulain <paul.poulain@biblibre.com>

--- C4/Installer/PerlDependencies.pm
+++ C4/Installer/PerlDependencies.pm
@@ -479,6 +479,11 @@ our $PERL_DEPS = {
         'required' => '0',
         'min_ver'  => '1.03',
     },
+    'Modern::Perl' => {
+        'usage'    => 'Core',
+        'required' => '1',
+        'min_ver'  => '1.03',
+    },
 };
 
 1;
