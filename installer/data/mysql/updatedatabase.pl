43a46c507e729be9dc701123ded422fb666bb40c
Paul Poulain <paul.poulain@biblibre.com>
2012-01-24 12:00:37 +0100

    Bug 1633 localcoverimages, updatedatabase & kohaversion for pushing

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -4626,6 +4626,34 @@ if ( C4::Context->preference("Version") < TransformToNum($DBversion) ) {
     SetVersion($DBversion);
 }
 
+$DBversion = "3.07.00.010";
+if ( C4::Context->preference("Version") < TransformToNum($DBversion) ) {
+    $dbh->do(
+        q|CREATE TABLE `biblioimages` (
+          `imagenumber` int(11) NOT NULL AUTO_INCREMENT,
+          `biblionumber` int(11) NOT NULL,
+          `mimetype` varchar(15) NOT NULL,
+          `imagefile` mediumblob NOT NULL,
+          `thumbnail` mediumblob NOT NULL,
+          PRIMARY KEY (`imagenumber`),
+          CONSTRAINT `bibliocoverimage_fk1` FOREIGN KEY (`biblionumber`) REFERENCES `biblio` (`biblionumber`) ON DELETE CASCADE ON UPDATE CASCADE
+          ) ENGINE=InnoDB DEFAULT CHARSET=utf8|
+    );
+    $dbh->do(
+        q|INSERT INTO `systempreferences` (variable,value,explanation,options,type) VALUES ('OPACLocalCoverImages','0','Display local cover images on OPAC search and details pages.','1','YesNo')|
+        );
+    $dbh->do(
+        q|INSERT INTO `systempreferences` (variable,value,explanation,options,type) VALUES ('LocalCoverImages','0','Display local cover images on intranet search and details pages.','1','YesNo')|
+        );
+    $dbh->do(
+        q|INSERT INTO `systempreferences` (variable,value,explanation,options,type) VALUES ('AllowMultipleCovers','0','Allow multiple cover images to be attached to each bibliographic record.','1','YesNo')|
+    );
+    $dbh->do(
+        q|INSERT INTO permissions (module_bit, code, description) VALUES (13, 'upload_local_cover_images', 'Upload local cover images')|
+    );
+    print "Upgrade done (Added support for local cover images)\n";
+    SetVersion($DBversion);
+}
 =head1 FUNCTIONS
 
 =head2 DropAllForeignKeys($table)
