33709dcc991e67aee64d8076fb3f05f5a76358fe
Paul Poulain <paul.poulain@biblibre.com>
2012-01-27 11:56:41 +0100

    Bug 1633, follow-up, french string

--- installer/data/mysql/fr-FR/1-Obligatoire/userpermissions.sql
+++ installer/data/mysql/fr-FR/1-Obligatoire/userpermissions.sql
@@ -36,7 +36,7 @@ INSERT INTO permissions (module_bit, code, description) VALUES
    (13, 'rotating_collections', 'Gérer les collections tournantes'),
    (13, 'items_batchmod', 'Modifier les exemplaires par lot'),
    (13, 'items_batchdel', 'Supprimer les exemplaires par lot'),
-   (13, 'upload_local_cover_images', 'Upload local cover images'),
+   (13, 'upload_local_cover_images', 'Téléchargement des images de couverture'),
    (15, 'check_expiration', 'Contrôler l''expiration d''un périodique'),
    (15, 'claim_serials', 'Réclamer les périodiques manquants'),
    (15, 'create_subscription', 'Créer de nouveaux abonnements'),
