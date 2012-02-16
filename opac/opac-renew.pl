a26ce485efc2b8c8cede4af76b39ab1709e271e9
Chris Cormack <chrisc@catalyst.net.nz>
2012-02-17 09:23:53 +1300

    Bug 7551 : Can only renew for the user you are logged in as now
    Signed-off-by: Owen Leonard <oleonard@myacpl.org>
    Signed-off-by: Paul Poulain <paul.poulain@biblibre.com>
    (cherry picked from commit 497f49d8b69e0be9e192b48bf40f2f5ab6cc92ce)
    
    Signed-off-by: Chris Nighswonger <chris.nighswonger@gmail.com>
    (cherry picked from commit fb666678a12559949f95b9ec8f09939ea0d57663)
    
    Signed-off-by: Chris Nighswonger <chris.nighswonger@gmail.com>

--- opac/opac-renew.pl
+++ opac/opac-renew.pl
@@ -41,7 +41,7 @@ my ( $template, $borrowernumber, $cookie ) = get_template_and_user(
 	}
 ); 
 my @items          = $query->param('item');
-$borrowernumber = $query->param('borrowernumber') || $query->param('bornum');
+
 my $opacrenew = C4::Context->preference("OpacRenewalAllowed");
 
 my $errorstring='';
