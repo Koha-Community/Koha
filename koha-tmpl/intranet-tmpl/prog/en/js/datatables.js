364b9a2da9c28eeff5451dad7b394cb4fafb9ab9
Jonathan Druart <jonathan.druart@biblibre.com>
2012-02-17 15:55:18 +0100

    Bug 8099: DataTables integration in acquisition module [2]
    acqui-home.tt
    
    http://bugs.koha-community.org/show_bug.cgi?id=8099
    Signed-off-by: Katrin Fischer <Katrin.Fischer.83@web.de>
    Signed-off-by: Paul Poulain <paul.poulain@biblibre.com>

--- koha-tmpl/intranet-tmpl/prog/en/js/datatables.js
+++ koha-tmpl/intranet-tmpl/prog/en/js/datatables.js
@@ -220,6 +220,22 @@ function dt_overwrite_html_sorting_localeCompare() {
             return (b > a) ? 1 : ((b < a) ? -1 : 0);
         }
     };
+
+    jQuery.fn.dataTableExt.oSort['num-html-asc']  = function(a,b) {
+        var x = a.replace( /<.*?>/g, "" );
+        var y = b.replace( /<.*?>/g, "" );
+        x = parseFloat( x );
+        y = parseFloat( y );
+        return ((x < y) ? -1 : ((x > y) ?  1 : 0));
+    };
+
+    jQuery.fn.dataTableExt.oSort['num-html-desc'] = function(a,b) {
+        var x = a.replace( /<.*?>/g, "" );
+        var y = b.replace( /<.*?>/g, "" );
+        x = parseFloat( x );
+        y = parseFloat( y );
+        return ((x < y) ?  1 : ((x > y) ? -1 : 0));
+    };
 }
 
 // Sorting on string without accentued characters
