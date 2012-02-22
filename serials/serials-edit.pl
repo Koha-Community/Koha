bc5971a71fd474d744c182d2c44e48bef275321c
Chris Nighswonger <chris.nighswonger@gmail.com>
2012-02-22 14:40:41 -0500

    Revert "Bug 7546 - cannot view subscription detail"
    This reverts commit f6c55445ed2ef6fe5eaf99831b9ec180d12707ec.
    -This is related to de-nesting work (Bug 6875) which is unstable

--- serials/serials-edit.pl
+++ serials/serials-edit.pl
@@ -72,7 +72,6 @@ use C4::Koha;
 use C4::Output;
 use C4::Context;
 use C4::Serials;
-use C4::Search qw/enabled_staff_search_views/;
 use List::MoreUtils qw/uniq/;
 
 my $query           = CGI->new();
@@ -404,15 +403,14 @@ output_html_with_http_headers $query, $cookie, $template->output;
 
 sub get_default_view {
     my $defaultview = C4::Context->preference('IntranetBiblioDefaultView');
-    my %views       = C4::Search::enabled_staff_search_views();
-    if ( $defaultview eq 'isbd' && $views{can_view_ISBD} ) {
+    my $views = { C4::Search::enabled_staff_search_views };
+    if ($defaultview eq 'isbd' && $views->{can_view_ISBD}) {
         return 'ISBDdetail';
-    }
-    elsif ( $defaultview eq 'marc' && $views{can_view_MARC} ) {
+    } elsif  ($defaultview eq 'marc' && $views->{can_view_MARC}) {
         return 'MARCdetail';
-    }
-    elsif ( $defaultview eq 'labeled_marc' && $views{can_view_labeledMARC} ) {
+    } elsif  ($defaultview eq 'labeled_marc' && $views->{can_view_labeledMARC}) {
         return 'labeledMARCdetail';
+    } else {
+        return 'detail';
     }
-    return 'detail';
 }
--- serials/subscription-detail.pl
+++ serials/subscription-detail.pl
@@ -24,7 +24,6 @@ use C4::Dates qw/format_date/;
 use C4::Serials;
 use C4::Output;
 use C4::Context;
-use C4::Search qw/enabled_staff_search_views/;
 use Date::Calc qw/Today Day_of_Year Week_of_Year Add_Delta_Days/;
 use Carp;
 
@@ -144,15 +143,14 @@ output_html_with_http_headers $query, $cookie, $template->output;
 
 sub get_default_view {
     my $defaultview = C4::Context->preference('IntranetBiblioDefaultView');
-    my %views       = C4::Search::enabled_staff_search_views();
-    if ( $defaultview eq 'isbd' && $views{can_view_ISBD} ) {
+    my $views = { C4::Search::enabled_staff_search_views };
+    if ($defaultview eq 'isbd' && $views->{can_view_ISBD}) {
         return 'ISBDdetail';
-    }
-    elsif ( $defaultview eq 'marc' && $views{can_view_MARC} ) {
+    } elsif  ($defaultview eq 'marc' && $views->{can_view_MARC}) {
         return 'MARCdetail';
-    }
-    elsif ( $defaultview eq 'labeled_marc' && $views{can_view_labeledMARC} ) {
+    } elsif  ($defaultview eq 'labeled_marc' && $views->{can_view_labeledMARC}) {
         return 'labeledMARCdetail';
+    } else {
+        return 'detail';
     }
-    return 'detail';
 }
