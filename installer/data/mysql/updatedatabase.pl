c55c58ee4b674ef6bd62c4d495d8ee8c799c968a
Paul Poulain <paul.poulain@biblibre.com>
2012-02-27 16:17:42 +0100

    Bug 6039 follow-up, DBrev number

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -4712,7 +4712,7 @@ if ( C4::Context->preference("Version") < TransformToNum($DBversion) ) {
     SetVersion($DBversion);
 }
 
-$DBversion = "3.07.00.XXX";
+$DBversion = "3.07.00.017";
 if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
     $dbh->do("INSERT INTO `systempreferences` (variable,value,explanation,options,type) VALUES('TransferWhenCancelAllWaitingHolds','0','Transfer items when cancelling all waiting holds',NULL,'YesNo')");
     print "Upgrade to $DBversion done (Add sysprefs to control transfer when cancel all waiting holds)\n";
