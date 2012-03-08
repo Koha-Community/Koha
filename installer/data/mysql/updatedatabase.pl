f7d7fd761138c3935fedafbe31407d84477e8428
Paul Poulain <paul.poulain@biblibre.com>
2012-03-08 17:02:00 +0100

    Bug 7493 follow-up DBRev number

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -4765,7 +4765,7 @@ if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
     SetVersion($DBversion);
 }
 
-$DBversion = "3.07.00.XXX";
+$DBversion = "3.07.00.022";
 if ( C4::Context->preference("Version") < TransformToNum($DBversion) ) {
     $dbh->do("DELETE FROM reviews WHERE biblionumber NOT IN (SELECT biblionumber from biblio)");
     $dbh->do("UPDATE reviews SET borrowernumber = NULL WHERE borrowernumber NOT IN (SELECT borrowernumber FROM borrowers)");
