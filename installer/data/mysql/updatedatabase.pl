8705e18aa5d9fe3f59625d84ee6360645c28c1c4
Paul Poulain <paul.poulain@biblibre.com>
2012-03-22 16:02:10 +0100

    Bug 7154 follow-up DBRev
    Signed-off-by: Paul Poulain <paul.poulain@biblibre.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -5004,7 +5004,7 @@ if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
 
 
 
-$DBversion = "3.07.00.XXX";
+$DBversion = "3.07.00.034";
 if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
     $dbh->do("ALTER TABLE borrower_attribute_types ADD COLUMN category_code VARCHAR(10) NULL DEFAULT NULL AFTER `display_checkout`");
     $dbh->do("ALTER TABLE borrower_attribute_types ADD COLUMN class VARCHAR(255)  NOT NULL DEFAULT '' AFTER `category_code`");
