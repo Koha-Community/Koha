b09ea817545bba136a5122baf79fa7857c46ec41
Magnus Enger <magnus@enger.priv.no>
2012-03-26 22:05:45 +0200

    Bug 7436 - Set itemtypes.rentalcharge = 0 in sample data for nb-NO and de-DE
    Set the rentalcharge = 0 for sample itemtypes (except Music for nb-NO).
    
    To test:
    Run through the web-installer for nb-NO and de-DE, and check that
    itemtypes are displayed correctly for each language.
    
    Signed-off-by: Katrin Fischer <Katrin.Fischer.83@web.de>.
    Note: You have to install the language first in order to be able
    to select it in the web installer. There was a problem
    in the po files for nb-NO that kept all files from being created
    correctly. I fixed the po file and did an installation with all sample
    files. There is a problem with z3950servers.sql that is not caused
    by this patch:
    z3950servers.sql
    ERROR 1136 (21S01) at line 1: Column count doesn't match value count at row 1
    
    Web installer for de-DE ran without problems on the sample files.
    
    Signed-off-by: Paul Poulain <paul.poulain@biblibre.com>
    Signed-off-by: Jared Camins-Esakov <jcamins@cpbibliography.com>

--- installer/data/mysql/de-DE/optional/sample_itemtypes.sql
+++ installer/data/mysql/de-DE/optional/sample_itemtypes.sql
@@ -1,8 +1,8 @@
-INSERT INTO `itemtypes` VALUES ('BK', 'Bücher',5,0,'bridge/book.gif','');
-INSERT INTO `itemtypes` VALUES ('MX', 'Medienkombinationen',5,0,'bridge/kit.gif','');
-INSERT INTO `itemtypes` VALUES ('CF', 'Datenträger',5,0,'bridge/computer_file.gif','');
-INSERT INTO `itemtypes` VALUES ('MP', 'Karten',5,0,'bridge/map.gif','');
-INSERT INTO `itemtypes` VALUES ('VM', 'AV-Medien',5,1,'bridge/dvd.gif','');
-INSERT INTO `itemtypes` VALUES ('MU', 'Musik',5,0,'bridge/sound.gif','');
-INSERT INTO `itemtypes` VALUES ('CR', 'Zeitschriften',5,0,'bridge/periodical.gif','');
+INSERT INTO `itemtypes` VALUES ('BK', 'Bücher',0,0,'bridge/book.gif','');
+INSERT INTO `itemtypes` VALUES ('MX', 'Medienkombinationen',0,0,'bridge/kit.gif','');
+INSERT INTO `itemtypes` VALUES ('CF', 'Datenträger',0,0,'bridge/computer_file.gif','');
+INSERT INTO `itemtypes` VALUES ('MP', 'Karten',0,0,'bridge/map.gif','');
+INSERT INTO `itemtypes` VALUES ('VM', 'AV-Medien',0,1,'bridge/dvd.gif','');
+INSERT INTO `itemtypes` VALUES ('MU', 'Musik',0,0,'bridge/sound.gif','');
+INSERT INTO `itemtypes` VALUES ('CR', 'Zeitschriften',0,0,'bridge/periodical.gif','');
 INSERT INTO `itemtypes` VALUES ('REF', 'Nachschlagewerke',0,1,'','');
