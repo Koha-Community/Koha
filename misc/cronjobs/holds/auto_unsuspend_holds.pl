b7a6071cf503f2402595fe6d2411459606591821
Paul Poulain <paul.poulain@biblibre.com>
2012-03-29 15:10:16 +0200

    bug 7641 follow-up: activate use strict (see coding guidelines)

--- misc/cronjobs/holds/auto_unsuspend_holds.pl
+++ misc/cronjobs/holds/auto_unsuspend_holds.pl
@@ -17,8 +17,8 @@
 # with Koha; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-#use strict;
-#use warnings; FIXME - Bug 2505
+use strict;
+use warnings;
 
 BEGIN {
     # find Koha's Perl modules
