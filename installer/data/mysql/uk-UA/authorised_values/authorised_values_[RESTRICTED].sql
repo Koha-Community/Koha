2e1d7d751875b0339f2846317420024f07deff49
Katrin Fischer <Katrin.Fischer.83@web.de>
2012-04-02 19:28:37 +0200

    Bug 2399: All status fields in the item edit interface offer two blank/null entries per dropdown instead of one
    Removes the '0' values without description from the sample date for
    authorised values.
    
    To test:
    1) Verify that SQL files load correctly
    - truncate authorised_values; /* empty authorised values table */
    - load sample data using one of the SQL files
    2) Check cataloging interface - all status fields should only show 1 empty line
    
    Signed-off-by: Chris Cormack <chrisc@catalyst.net.nz>
    Signed-off-by: Jared Camins-Esakov <jcamins@cpbibliography.com>

--- installer/data/mysql/uk-UA/authorised_values/authorised_values_[RESTRICTED].sql
+++ installer/data/mysql/uk-UA/authorised_values/authorised_values_[RESTRICTED].sql
@@ -1,5 +1,4 @@
 DELETE FROM authorised_values WHERE category='RESTRICTED';
 
 INSERT INTO `authorised_values` (`category`, `authorised_value`, `lib`) VALUES 
-('RESTRICTED','0',''),
 ('RESTRICTED','1','Обмежений доступ');
