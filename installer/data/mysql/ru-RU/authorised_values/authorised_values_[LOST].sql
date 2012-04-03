15093a3a8c3d063099e97a66b4bcae744efa64c0
Katrin Fischer <Katrin.Fischer.83@web.de>
2012-04-03 20:23:47 +0200

    Bug 2399: Follow up removing MySQLisms from ru-RU and uk-UA sample files
    Signed-off-by: Chris Cormack <chrisc@catalyst.net.nz>
    Signed-off-by: Paul Poulain <paul.poulain@biblibre.com>
    Signed-off-by: Chris Cormack <chrisc@catalyst.net.nz>
    Signed-off-by: Jared Camins-Esakov <jcamins@cpbibliography.com>

--- installer/data/mysql/ru-RU/authorised_values/authorised_values_[LOST].sql
+++ installer/data/mysql/ru-RU/authorised_values/authorised_values_[LOST].sql
@@ -1,12 +1,8 @@
 DELETE FROM authorised_values WHERE category='LOST';
 
-INSERT INTO `authorised_values` (`id`, `category`, `authorised_value`, `lib`) VALUES (LAST_INSERT_ID( ) + 0,'LOST','2','Длительная просрочка (утрачено)');
-
-INSERT INTO `authorised_values` (`id`, `category`, `authorised_value`, `lib`) VALUES (LAST_INSERT_ID( ) + 1,'LOST','1','Утрачено');
-
-INSERT INTO `authorised_values` (`id`, `category`, `authorised_value`, `lib`) VALUES (LAST_INSERT_ID( ) + 2,'LOST','3','Потеряны и заплачено за экземпляр');
-
-INSERT INTO `authorised_values` (`id`, `category`, `authorised_value`, `lib`) VALUES (LAST_INSERT_ID( ) + 3,'LOST','5','Отсутствует при запросе на резервирование');
-
-INSERT INTO `authorised_values` (`id`, `category`, `authorised_value`, `lib`) VALUES (LAST_INSERT_ID( ) + 4,'LOST','4','Отсутствует при инвентиризации');
+INSERT INTO authorised_values (id, category, authorised_value, lib) VALUES (LAST_INSERT_ID( ) + 0,'LOST','2','Длительная просрочка (утрачено)');
+INSERT INTO authorised_values (id, category, authorised_value, lib) VALUES (LAST_INSERT_ID( ) + 1,'LOST','1','Утрачено');
+INSERT INTO authorised_values (id, category, authorised_value, lib) VALUES (LAST_INSERT_ID( ) + 2,'LOST','3','Потеряны и заплачено за экземпляр');
+INSERT INTO authorised_values (id, category, authorised_value, lib) VALUES (LAST_INSERT_ID( ) + 3,'LOST','5','Отсутствует при запросе на резервирование');
+INSERT INTO authorised_values (id, category, authorised_value, lib) VALUES (LAST_INSERT_ID( ) + 4,'LOST','4','Отсутствует при инвентиризации');
 
