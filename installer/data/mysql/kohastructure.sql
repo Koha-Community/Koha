1f6cc04bff3065d2c719a14e8f8af624f64c8ba9
Dobrica Pavlinusic <dpavlin@rot13.org>
2012-04-05 01:03:30 +0200

    Bug 7886 - C4/ShelfBrowser slow SQL performance
    ShelfBrowser uses two SQL queries with syntax
    
    WHERE ((cn_sort = ? AND itemnumber < ?) OR cn_sort < ?)
    AND homebranch = ? AND location = ? AND ccode = ?
    
    homebranch already has index, and adding indexes on ccode and location
    improves performance by 30% for each query since MySQL is able to use
    index_merge intersect(items_ccode,homebranch,items_location)
    
    Signed-off-by: Paul Poulain <paul.poulain@biblibre.com>
    
    Signed-off-by: Jared Camins-Esakov <jcamins@cpbibliography.com>

--- installer/data/mysql/kohastructure.sql
+++ installer/data/mysql/kohastructure.sql
@@ -1043,6 +1043,9 @@ CREATE TABLE `items` ( -- holdings/item information
   KEY `itembibnoidx` (`biblionumber`),
   KEY `homebranch` (`homebranch`),
   KEY `holdingbranch` (`holdingbranch`),
+  KEY `itemcallnumber` (`itemcallnumber`),
+  KEY `items_location` (`location`),
+  KEY `items_ccode` (`ccode`),
   CONSTRAINT `items_ibfk_1` FOREIGN KEY (`biblioitemnumber`) REFERENCES `biblioitems` (`biblioitemnumber`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `items_ibfk_2` FOREIGN KEY (`homebranch`) REFERENCES `branches` (`branchcode`) ON UPDATE CASCADE,
   CONSTRAINT `items_ibfk_3` FOREIGN KEY (`holdingbranch`) REFERENCES `branches` (`branchcode`) ON UPDATE CASCADE
