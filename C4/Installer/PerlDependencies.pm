4953b57fcdd68bb06bec192fd71a511e0cea8ecd
Chris Nighswonger <cnighswonger@foundations.edu>
2012-04-19 10:50:21 -0400

    Bug 7977: Adding DateTime::Format::MySQL as a core dependency
    Signed-off-by: Jared Camins-Esakov <jcamins@cpbibliography.com>
    Signed-off-by: Mason James <mtj@kohaaloha.com>

--- C4/Installer/PerlDependencies.pm
+++ C4/Installer/PerlDependencies.pm
@@ -209,6 +209,11 @@ our $PERL_DEPS = {
         'required' => '1',
         'min_ver'  => '0.04'
     },
+    'DateTime::Format::MySQL' => {
+        'usage'    => 'Core',
+        'required' => '1',
+        'min_ver'  => '0.04'
+    },
     'DateTime::Set' => {
         'usage'    => 'Core',
         'required' => '1',
