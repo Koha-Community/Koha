12cb095bac53f049d4148aec27a063a4dfa2c24e
Chris Nighswonger <cnighswonger@foundations.edu>
2012-04-20 16:12:26 -0400

    Bug 7977: QOTD uploader to enable uploading csv files containing quotes
    This series will add a DataTable's based upload/editor with which
    to upload csv files containing quotes to be used by the QOTD
    feature.
    
    The file should be formatted thusly:
    
    "source","text-of-quote"
    "source","text-of-quote"
    ...
    
    Note: This work serves as a good example of potential improvements
    in all other "editor" and file upload areas of Koha.
    
    This patch is a squash of the following work:
    
    --Adding code to parse CSV file contents and push it into a DataTable
    --Adding in jEditable to enable table editing
    --Adding ajax to post data back to the server to be saved
    --Fixing edit and adding delete functionality
    --Adding some missing css as well as server feedback on save
    --Fixing a bug which limited the number of quotes which could be uploaded
    --Also fixing a minor bug with fnCSVToArray and doing some style cleanup.
    --Adding sanity checks to verify file type and size
    --Implements YUI button widget/toolbar
    --Improved handling of hiding uploader UI
    --Adds row selectability
    --Adds multi-delete capability
    --Adds YUI button/toolbar widget
    --Fixing capitalization in quote uploader
    --Implements improvments suggested by jcamins and oleonard
    
    Signed-off-by: Jared Camins-Esakov <jcamins@cpbibliography.com>
    Signed-off-by: Mason James <mtj@kohaaloha.com>

---
+++ tools/quotes/quotes-upload_ajax.pl
@@ -0,0 +1,68 @@
+#!/usr/bin/perl
+
+# Copyright 2012 Foundations Bible College Inc.
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use strict;
+use warnings;
+
+use CGI;
+use JSON;
+use autouse 'Data::Dumper' => qw(Dumper);
+
+use C4::Auth;
+use C4::Koha;
+use C4::Context;
+use C4::Output;
+
+my $cgi = new CGI;
+my $dbh = C4::Context->dbh;
+
+my ( $template, $borrowernumber, $cookie ) = get_template_and_user(
+    {
+        template_name   => "",
+        query           => $cgi,
+        type            => "intranet",
+        authnotrequired => 0,
+        flagsrequired   => { tools => 'edit_quotes' },
+        debug           => 1,
+    }
+);
+
+my $success = 'true';
+
+my $quotes = decode_json($cgi->param('quote'));
+my $action = $cgi->param('action');
+
+my $sth = $dbh->prepare('INSERT INTO quotes (source, text) VALUES (?, ?);');
+
+my $insert_count = 0;
+
+foreach my $quote (@$quotes) {
+    $insert_count++ if $sth->execute($quote->[0], $quote->[1]);
+    if ($sth->err) {
+        warn sprintf('Database returned the following error: %s', $sth->errstr);
+        $success = 'false';
+    }
+}
+
+print $cgi->header('application/json');
+
+print to_json({
+                success => $success,
+                records => $insert_count,
+});
