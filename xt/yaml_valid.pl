bc02be6c1692a3fc67112f4afad571993a08d4f9
Claire Hernandez <claire.hernandez@biblibre.com>
2012-04-23 15:11:46 +0200

    Bug 7994: scripts to check yaml files
    $ prove yaml_valid.t
    checks a closed list of files
    $ perl yaml_valid.pl -f myfile.yaml
    try to load a yaml file with YAML::LoadFile and show errors
    
    (both files pertidied)
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Chris Cormack <chrisc@catalyst.net.nz>
    (cherry picked from commit 5cb8c9f6e44aa9874231bae5a909cb4a4f3473b5)
    
    Signed-off-by: Jared Camins-Esakov <jcamins@cpbibliography.com>

---
+++ xt/yaml_valid.pl
@@ -0,0 +1,67 @@
+#!/usr/bin/perl
+
+# Copyright (C) 2012 BibLibre
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use Modern::Perl;
+use Getopt::Long;
+use YAML;
+
+my $usage = <<EOF;
+yaml_valid.pl - give it a filename and it will told you if it is an exact yaml file.
+    -h|--help           Print this help and exit;
+    -f|--file           File to check
+
+  Tests yaml config files
+  It does not tell if the params are correct, only if the file is well-formed (ie: readable by yaml)
+EOF
+
+my $help = 0;
+my $file = 0;
+GetOptions(
+    "help"   => \$help,
+    "file=s" => \$file,
+) or die $usage;
+die $usage if $help;
+
+say "Testing file: $file";
+eval { YAML::LoadFile($file); };
+if ($@) {
+    print "KO!\n$@\n";
+}
+else {
+    print "Loading and Syntax OK\n";
+}
+
+#yaml_file_ok("$file", "$file is YAML");
+
+=head1 NAME
+
+yaml_valid.pl
+
+=head1 DESCRIPTION
+
+  Tests yaml config files
+  It does not tell if the params are correct, only if the file is well-formed (ie: readable by yaml)
+
+=head1 USAGE
+
+From Koha root directory:
+
+perl xt/yaml_valid.pl -f filename.yaml
+
+=cut
