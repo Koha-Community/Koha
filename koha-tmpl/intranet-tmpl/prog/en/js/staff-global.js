68b30468c3e8998a5eaa3054c70f44dc61d3721c
Owen Leonard <oleonard@myacpl.org>
2012-04-24 10:49:15 -0400

    Bug 8143 [REVISED] Upgrade jQuery tabs to current jQueryUI version
    Current jQuery-driven tabs are done using a very old
    version of the tabs plugin. This patch upgrades jQueryUI
    to the latest version and adds the tabs widget dependency
    to the jqueryui js file and updates the syntax for existing
    tabs:
    
    - $("#foo > ul").tabs(); changes to $("#foo").tabs();
    - Remove full URL from tab links (use #anchor only).
    
    Pages with "static" tabs (tabs which are built in the
    markup rather than generated by the plugin) have been
    modified to use their own style. Examples: pay.tt in
    the staff client and opac-readingrecord.tt in the OPAC.
    
    Edit: Minor revision to some uncorrected markup
    
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Paul Poulain <paul.poulain@biblibre.com>

--- koha-tmpl/intranet-tmpl/prog/en/js/staff-global.js
+++ koha-tmpl/intranet-tmpl/prog/en/js/staff-global.js
@@ -4,11 +4,11 @@ if ( KOHA === undefined ) var KOHA = {};
 function _(s) { return s } // dummy function for gettext
 
  $(document).ready(function() {
-	$('#header_search > ul').tabs().bind('show.ui-tabs', function(e, ui) { $('#header_search > div:not(.ui-tabs-hide)').find('input').eq(0).focus(); });
+    $('#header_search').tabs().bind('tabsshow', function(e, ui) { $('#header_search > div:not(.ui-tabs-hide)').find('input').eq(0).focus(); });
 	$(".close").click(function(){ window.close(); });
-	if($("#header_search #checkin_search").length > 0){ $(document).bind('keydown','Alt+r',function (){ $("#header_search > ul").tabs("select","#checkin_search"); $("#ret_barcode").focus(); }); } else { $(document).bind('keydown','Alt+r',function (){ location.href="/cgi-bin/koha/circ/returns.pl"; }); }
-	if($("#header_search #circ_search").length > 0){ $(document).bind('keydown','Alt+u',function (){ $("#header_search > ul").tabs("select","#circ_search"); $("#findborrower").focus(); }); } else { $(document).bind('keydown','Alt+u',function(){ location.href="/cgi-bin/koha/circ/circulation.pl"; }); }
-	if($("#header_search #catalog_search").length > 0){ $(document).bind('keydown','Alt+q',function (){ $("#header_search > ul").tabs("select","#catalog_search"); $("#search-form").focus(); }); } else { $(document).bind('keydown','Alt+q',function(){ location.href="/cgi-bin/koha/catalogue/search.pl"; }); }
+    if($("#header_search #checkin_search").length > 0){ $(document).bind('keydown','Alt+r',function (){ $("#header_search").tabs("select","#checkin_search"); $("#ret_barcode").focus(); }); } else { $(document).bind('keydown','Alt+r',function (){ location.href="/cgi-bin/koha/circ/returns.pl"; }); }
+    if($("#header_search #circ_search").length > 0){ $(document).bind('keydown','Alt+u',function (){ $("#header_search").tabs("select","#circ_search"); $("#findborrower").focus(); }); } else { $(document).bind('keydown','Alt+u',function(){ location.href="/cgi-bin/koha/circ/circulation.pl"; }); }
+    if($("#header_search #catalog_search").length > 0){ $(document).bind('keydown','Alt+q',function (){ $("#header_search").tabs("select","#catalog_search"); $("#search-form").focus(); }); } else { $(document).bind('keydown','Alt+q',function(){ location.href="/cgi-bin/koha/catalogue/search.pl"; }); }
  	$(".focus").focus();
  });
  
