b4da1aafd4f9bbeabda541abda31a39470dbeb57
Jared Camins-Esakov <jcamins@cpbibliography.com>
2012-05-10 09:01:06 -0400

    Revert "Bug 7837 - nb-NO z3950servers.sql misses column names"
    This reverts commit f1d92920e102d1a6d1a77613f3adeae01d0dff48.
    
    Thanks to Katrin Fischer to pointing out that this patch should not have been
    cherry-picked to 3.6.x.

--- installer/data/mysql/nb-NO/2-Valgfritt/z3950servers.sql
+++ installer/data/mysql/nb-NO/2-Valgfritt/z3950servers.sql
@@ -1,6 +1,6 @@
-INSERT INTO z3950servers (host, port, db, userid, password, name, id, checked, rank, syntax, timeout, icon, position, type, encoding, description) VALUES
-('z3950.loc.gov',7090,'Voyager','','','LIBRARY OF CONGRESS',1,0,0,'USMARC',0,NULL,'primary','zed','MARC-8',''),
-('z3950.bibsys.no',2100,'BIBSYS','','','BIBSYS',12,1,1,'NORMARC',0,NULL,'primary','zed','ISO_6937',''),
-('z3950.nb.no',2100,'Norbok','','','NORBOK',13,0,0,'NORMARC',0,NULL,'primary','zed','ISO_6937',''),
-('z3950.nb.no',2100,'Sambok','','','SAMBOK',14,0,0,'NORMARC',0,NULL,'primary','zed','ISO_6937',''),
-('z3950.deich.folkebibl.no',210,'data','','','DEICHMAN',15,0,0,'NORMARC',0,NULL,'primary','zed','ISO_6937','');
+INSERT INTO `z3950servers` VALUES 
+('z3950.loc.gov',7090,'Voyager','','','LIBRARY OF CONGRESS',1,0,0,'USMARC',NULL,'primary','zed','MARC-8',''),
+('z3950.bibsys.no',2100,'BIBSYS','','','BIBSYS',12,1,1,'NORMARC',NULL,'primary','zed','ISO_6937',''),
+('z3950.nb.no',2100,'Norbok','','','NORBOK',13,0,0,'NORMARC',NULL,'primary','zed','ISO_6937',''),
+('z3950.nb.no',2100,'Sambok','','','SAMBOK',14,0,0,'NORMARC',NULL,'primary','zed','ISO_6937',''),
+('z3950.deich.folkebibl.no',210,'data','','','DEICHMAN',15,0,0,'NORMARC',NULL,'primary','zed','ISO_6937','');
