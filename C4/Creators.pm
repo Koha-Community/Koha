88b6e0f90a32af03ed6c4876e0917176bafe8376
Chris Cormack <chris@bigballofwax.co.nz>
2012-05-21 20:06:36 +1200

    Bug 4330 : Adding copyright headers and fixing FSF addresses
    Signed-off-by: Jared Camins-Esakov <jcamins@cpbibliography.com>
    Signed-off-by: Paul Poulain <paul.poulain@biblibre.com>

--- C4/Creators.pm
+++ C4/Creators.pm
@@ -1,5 +1,22 @@
 package C4::Creators;
 
+# Copyright 2010 Foundations
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 BEGIN {
     use version; our $VERSION = qv('1.0.0_1');
     use vars qw(@EXPORT @ISA);
