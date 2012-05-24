4533ff7dd3be3ae76efaf91c29e8d510c8f8bff5
Paul Poulain <paul.poulain@biblibre.com>
2012-05-24 15:47:37 +0200

    Bug 7398 follow-up DBRev number

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -5271,7 +5271,7 @@ if ( C4::Context->preference("Version") < TransformToNum($DBversion) ) {
 
 
 
-$DBversion = "3.07.00.XXX";
+$DBversion = "3.09.00.006";
 if ( C4::Context->preference("Version") < TransformToNum($DBversion) ) {
     $dbh->do("UPDATE systempreferences SET
                 variable = 'OPACShowHoldQueueDetails',
