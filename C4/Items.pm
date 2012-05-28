fab56d325774fab28008e3afe5f6e1a3fae786a0
Jared Camins-Esakov <jcamins@cpbibliography.com>
2012-05-28 19:27:45 -0400

    Bug 7847: OPAC search dies with Plack
    Due to a dependency cycle between C4::Search and C4::Items, searches
    in the OPAC die spectacularly under Plack. This counter-patch extends
    dpavlin's solution and replaces use with require for C4::Search in
    C4::Items and for C4::Items in C4::Search.
    
    Signed-off-by: Paul Poulain <paul.poulain@biblibre.com>

--- C4/Items.pm
+++ C4/Items.pm
@@ -30,7 +30,6 @@ use MARC::Record;
 use C4::ClassSource;
 use C4::Log;
 use List::MoreUtils qw/any/;
-use C4::Search;
 use Data::Dumper; # used as part of logging item record changes, not just for
                   # debugging; so please don't remove this
 
@@ -2481,6 +2480,7 @@ counts Usage of itemnumber in Analytical bibliorecords.
 
 sub GetAnalyticsCount {
     my ($itemnumber) = @_;
+    require C4::Search;
     if (C4::Context->preference('NoZebra')) {
         # Read the index Koha-Auth-Number for this authid and count the lines
         my $result = C4::Search::NZanalyse("hi=$itemnumber");
