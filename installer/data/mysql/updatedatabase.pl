2bb8793cc9560f7e9a5a5c824af2b3985fbe5133
Jared Camins-Esakov <jcamins@cpbibliography.com>
2012-06-24 07:42:25 -0400

    Updating version for 3.6.6 release

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -4682,6 +4682,12 @@ if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
     SetVersion ($DBversion);
 }
 
+$DBversion = "3.06.06.000";
+if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
+    print "Upgrade to $DBversion done (Incrementing version for 3.6.6 release. See release notes for details.) \n";
+    SetVersion ($DBversion);
+}
+
 =head1 FUNCTIONS
 
 =head2 DropAllForeignKeys($table)
