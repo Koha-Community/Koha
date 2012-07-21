bbbf3055747f24c25670cfb1de977f25424551ca
Mark Tompsett <mtompset@hotmail.com>
2012-07-21 22:04:23 +0800

    Bug 8489: Revise Ubuntu install files
    1) Changed ubuntu-pkg-check.sh to use lsb_release to determine version
       of ubuntu.{version}.packages file. If a versioned file does not
       exist, the user is warned, and ubuntu.packages is used. Told the
       user which file is being used. Caught apt-cache policy errors nicely.
       NOTE: ubuntu.packages MUST BE A COPY OF THE
       LATEST ubuntu.{version}.packages FILE!
    2) Moved: ubuntu.packages -> ubuntu.10.04.packages
              ubuntu_maverick.packages -> ubuntu.10.10.packages
              ubuntu.precise.packages -> ubuntu.12.04 packages
                 (last file created new, but effectively supersedes bug 8488)
    3) Tweaked ubuntu.12.04.packages:
       * changed yaz3 references to yaz4
       * dropped libmysqlclient16-dev
       * changed libmysqlclient16 to libmysqlclient18
    4) Copied the ubuntu.12.04.packages file to ubuntu.packages
    
    Signed-off-by: Jared Camins-Esakov <jcamins@cpbibliography.com>

--- install_misc/ubuntu-pkg-check.sh
+++ install_misc/ubuntu-pkg-check.sh
@@ -1,19 +1,27 @@
 #!/bin/sh
 
-UBUNTU_PACKAGES=`dirname $0`/ubuntu.packages
+# determine what directory this script is in, because the packages files
+# should be there too.
+DIR=`dirname $0`
+
+#determine which vbersion of ubuntu
+VERSION=`lsb_release -r | cut -f2 -d'	'`
+UBUNTU_PACKAGES=$DIR/ubuntu.$VERSION.packages
 
 # sanity checks
-
 if [ ! -e $UBUNTU_PACKAGES ]; then
-  echo ERROR:  Could not find $UBUNTU_PACKAGES file for running check.
-  exit
+   echo "WARNING! We strongly recommend an LTS release."
+   UBUNTU_PACKAGES=$DIR/ubuntu.packages
 fi
+echo "Using the $UBUNTU_PACKAGES file."
 
 # main
-
 UBUNTU_PACKAGES_LIST=`awk '{print $1}' $UBUNTU_PACKAGES | grep -v '^\s*#' | grep -v '^\s*$'`
 for F in $UBUNTU_PACKAGES_LIST; do
-  UBUNTU_PKG_POLICY=`apt-cache policy $F | grep "Installed:"`
-  UBUNTU_PKG_VERSION=`echo $UBUNTU_PKG_POLICY | awk '{print $2}'`
-  echo "$F = $UBUNTU_PKG_VERSION"
+   UBUNTU_PKG_POLICY=`apt-cache policy $F 2> /dev/null | grep "Installed:"`
+   if [ "${#UBUNTU_PKG_POLICY}" -eq "0" ]; then
+      UBUNTU_PKG_POLICY="Installed: \(none\)\*"
+   fi
+   UBUNTU_PKG_VERSION=`echo $UBUNTU_PKG_POLICY | awk '{print $2}'`
+   echo "$F = $UBUNTU_PKG_VERSION"
 done
