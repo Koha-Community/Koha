08ea2b7a34ca52e0b1049c610498947644a91ff9
Katrin Fischer <Katrin.Fischer.83@web.de>
2012-08-22 22:33:26 +0200

    Bug 3782: Remove broken z39.50 targets from sample files
    Removes NYPL and NYU from the list of sample z39.50 servers, because
    they don't work or only work inconsistently.
    
    Signed-off-by: Chris Cormack <chrisc@catalyst.net.nz>
    Signed-off-by: Paul Poulain <paul.poulain@biblibre.com>
    Signed-off-by: Chris Cormack <chrisc@catalyst.net.nz>
    
    Signed-off-by: Jared Camins-Esakov <jcamins@cpbibliography.com>

--- installer/data/mysql/es-ES/optional/sample_z3950_servers.sql
+++ installer/data/mysql/es-ES/optional/sample_z3950_servers.sql
@@ -1,7 +1,5 @@
 INSERT INTO `z3950servers`
 (`host`, `port`, `db`, `userid`, `password`, `name`, `id`, `checked`, `rank`, `syntax`, `encoding`) VALUES 
 ('z3950.loc.gov',7090,'Voyager','','','LIBRARY OF CONGRESS',1,1,1,'USMARC','MARC-8'),
-('hopkins1.bobst.nyu.edu',9991,'NYU01PUB','','','NEW YORK UNIVERSITY LIBRARIES',5,0,0,'USMARC','MARC-8'),
 ('clio-db.cc.columbia.edu',7090,'voyager','','','COLUMBIA UNIVERSITY',6,0,0,'USMARC','MARC-8'),
-('catalog.nypl.org',210,'innopac','','','NEW YORK PUBLIC LIBRARY',7,0,0,'USMARC','MARC-8'),
 ('siris-libraries.si.edu',210,'Default','','','SMITHSONIAN INSTITUTION LIBRARIES',10,0,0,'USMARC','MARC-8'); 
--- installer/data/mysql/pl-PL/optional/sample_z3950_servers.sql
+++ installer/data/mysql/pl-PL/optional/sample_z3950_servers.sql
@@ -2,6 +2,4 @@ INSERT INTO `z3950servers`
 (`host`, `port`, `db`, `userid`, `password`, `name`, `id`, `checked`, `rank`, `syntax`, `encoding`) VALUES
 ('193.59.172.100',210,'INNOPAC','','','BIBLIOTEKA NARODOWA',1,1,1,'MARC21','UTF8'),
 ('62.181.1.243',4321,'bilbo','','','BP BEMOWO',2,1,1,'MARC21','UTF8'),
-('z3950.loc.gov',7090,'Voyager','','','LIBRARY OF CONGRESS',3,0,1,'USMARC','MARC-8'),
-('bobcat.nyu.edu',210,'advance','','','NEW YORK UNIVERSITY LIBRARIES',4,0,0,'USMARC','MARC-8'),
-('catnyp.nypl.org',210,'innopac','','','NEW YORK PUBLIC LIBRARY',5,0,0,'USMARC','MARC-8');
+('z3950.loc.gov',7090,'Voyager','','','LIBRARY OF CONGRESS',3,0,1,'USMARC','MARC-8');
