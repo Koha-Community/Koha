7251a9339d8443ce7575fd7c1bb788aeff9c5673
Jonathan Druart <jonathan.druart@biblibre.com>
2012-08-30 10:33:38 +0200

    Bug 8674: Followup: Add POD for misc/batchdeletebiblios.pl
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Paul Poulain <paul.poulain@biblibre.com>

--- misc/batchdeletebiblios.pl
+++ misc/batchdeletebiblios.pl
@@ -1,32 +1,18 @@
 #!/usr/bin/perl
 
-# Copyright 2012 BibLibre
-# This file is part of Koha.
-#
-# Koha is free software; you can redistribute it and/or modify it under the
-# terms of the GNU General Public License as published by the Free Software
-# Foundation; either version 2 of the License, or (at your option) any later
-# version.
-#
-# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along with
-# Koha; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
-# Suite 330, Boston, MA  02111-1307 USA
-
-=head1 batchdeletebiblios.pl
-
-    This script batch deletes biblios which contain a biblionumber present in file passed in parameter.
-    If one biblio has items, it is not deleted.
-
-=cut
-
 use Modern::Perl;
+use Getopt::Long;
+use Pod::Usage;
+use IO::File;
+
 use C4::Biblio;
 
-use IO::File;
+my ($help, $files);
+GetOptions(
+    'h|help' => \$help,
+);
+
+pod2usage(1) if $help or not @ARGV;
 
 for my $file ( @ARGV ) {
     say "Find biblionumber in file $file";
@@ -50,3 +36,54 @@ for my $file ( @ARGV ) {
         }
     }
 }
+
+exit(0);
+
+__END__
+
+=head1 NAME
+
+batchdeletebiblios.pl
+
+=head1 SYNOPSIS
+
+./batchdeletebiblio.pl file1 [file2 ... filen]
+
+This script batch deletes biblios which contain a biblionumber present in file passed in parameter.
+If one biblio has items, it is not deleted.
+
+=head1 OPTIONS
+
+=over 8
+
+=item B<-h|--help>
+
+prints this help message
+
+=back
+
+=head1 AUTHOR
+
+Jonathan Druart <jonathan.druart@biblibre.com>
+
+=head1 COPYRIGHT
+
+Copyright 2012 BibLibre
+
+=head1 LICENSE
+
+This file is part of Koha.
+
+Koha is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later version.
+
+You should have received a copy of the GNU General Public License along
+with Koha; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+=head1 DISCLAIMER OF WARRANTY
+
+Koha is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+=cut
