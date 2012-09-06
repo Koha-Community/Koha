f362888e0c0d50fefc9daa429fbb1fce39ccfc1a
Kyle M Hall <kyle@bywatersolutions.com>
2012-09-06 09:23:08 -0400

    Bug 4173: Followup: Document KohaAuthorisedValues plugin
    Signed-off-by: Paul Poulain <paul.poulain@biblibre.com>
    Signed-off-by: Chris Cormack <chrisc@catalyst.net.nz>

--- Koha/Template/Plugin/KohaAuthorisedValues.pm
+++ Koha/Template/Plugin/KohaAuthorisedValues.pm
@@ -24,6 +24,24 @@ use base qw( Template::Plugin );
 
 use C4::Koha;
 
+=pod
+
+This plugin allows one to get the description for an authorised value
+from within a template.
+
+First, include the line '[% USE KohaAuthorisedValues %]' at the top
+of the template to enable the plugin.
+
+To use, call KohaAuthorisedValues.GetByCode with the category
+of the authorised value, the code to look up, and optionally,
+the OPAC flag ( if set, it will return the OPAC description rather
+than the Intranet description ).
+
+For example: [% KohaAuthorisedValues.GetByCode( 'LOST', item.itemlost, 1 ) %]
+will print the OPAC description for the LOST value stored in item.itemlost.
+
+=cut
+
 sub GetByCode {
     my ( $self, $category, $code, $opac ) = @_;
     return GetAuthorisedValueByCode( $category, $code, $opac );
