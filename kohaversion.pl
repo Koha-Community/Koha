5a81850fe402eb93c27ea249b9d65d67ec9b314d
Paul Poulain <paul.poulain@biblibre.com>
2012-09-13 11:48:42 +0200

    Bug 8209 follow-up DBRev number

--- kohaversion.pl
+++ kohaversion.pl
@@ -16,7 +16,7 @@ the kohaversion is divided in 4 parts :
 use strict;
 
 sub kohaversion {
-    our $VERSION = '3.09.00.038';
+    our $VERSION = '3.09.00.039';
     # version needs to be set this way
     # so that it can be picked up by Makefile.PL
     # during install
