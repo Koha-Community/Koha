07684a7e1128c944c3a20f63b40ac3b36cca2966
Chris Cormack <chrisc@catalyst.net.nz>
2012-09-16 20:12:58 +1200

    Bug 8002 : Database bump

--- kohaversion.pl
+++ kohaversion.pl
@@ -16,7 +16,7 @@ the kohaversion is divided in 4 parts :
 use strict;
 
 sub kohaversion {
-    our $VERSION = '3.08.04.001';
+    our $VERSION = '3.08.04.002';
     # version needs to be set this way
     # so that it can be picked up by Makefile.PL
     # during install
