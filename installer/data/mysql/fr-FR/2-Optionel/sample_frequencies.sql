98d95ae8b8f6590883beb82f9590a3e457f44f74
Julian Maurice <julian.maurice@biblibre.com>
2012-09-17 12:36:01 +0200

    Bug 7688: Translate sample frequencies for french
    Signed-off-by: Bernardo Gonzalez Kriegel <bgkriegel@gmail.com>
    
    Signed-off-by: Paul Poulain <paul.poulain@biblibre.com>
    Signed-off-by: Jared Camins-Esakov <jcamins@cpbibliography.com>

--- installer/data/mysql/fr-FR/2-Optionel/sample_frequencies.sql
+++ installer/data/mysql/fr-FR/2-Optionel/sample_frequencies.sql
@@ -1,16 +1,16 @@
 INSERT INTO subscription_frequencies
     (description, unit, unitsperissue, issuesperunit, displayorder)
 VALUES
-    ('2/day', 'day', 1, 2, 1),
-    ('1/day', 'day', 1, 1, 2),
-    ('3/week', 'week', 1, 3, 3),
-    ('1/week', 'week', 1, 1, 4),
-    ('1/2 weeks', 'week', 2, 1, 5),
-    ('1/3 weeks', 'week', 3, 1, 6),
-    ('1/month', 'month', 1, 1, 7),
-    ('1/2 months', 'month', 2, 1, 8),
-    ('1/3 months', 'month', 3, 1, 9),
-    ('2/year', 'month', 6, 1, 10),
-    ('1/year', 'year', 1, 1, 11),
-    ('1/2 year', 'year', 2, 1, 12),
-    ('Irregular', NULL, 1, 1, 13);
+    ('2/jour', 'day', 1, 2, 1),
+    ('1/jour', 'day', 1, 1, 2),
+    ('3/semaine', 'week', 1, 3, 3),
+    ('1/semaine', 'week', 1, 1, 4),
+    ('1/2 semaines', 'week', 2, 1, 5),
+    ('1/3 semaines', 'week', 3, 1, 6),
+    ('1/mois', 'month', 1, 1, 7),
+    ('1/2 mois', 'month', 2, 1, 8),
+    ('1/3 mois', 'month', 3, 1, 9),
+    ('2/an', 'month', 6, 1, 10),
+    ('1/an', 'year', 1, 1, 11),
+    ('1/2 ans', 'year', 2, 1, 12),
+    ('Irrégulier', NULL, 1, 1, 13);
