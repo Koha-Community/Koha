a14290f1a735f54f78776a7b01e64e3990b9600b
Paul Poulain <paul.poulain@biblibre.com>
2012-09-18 11:23:42 +0200

    Bug 8336 follow-up, tabs to space
    Signed-off-by: Chris Cormack <chrisc@catalyst.net.nz>

--- C4/SIP/ILS/Transaction/Renew.pm
+++ C4/SIP/ILS/Transaction/Renew.pm
@@ -15,40 +15,40 @@ use C4::Members;
 use base qw(ILS::Transaction);
 
 my %fields = (
-	renewal_ok => 0,
+    renewal_ok => 0,
 );
 
 sub new {
-	my $class = shift;
-	my $self = $class->SUPER::new();
+    my $class = shift;
+    my $self = $class->SUPER::new();
 
     foreach my $element (keys %fields) {
-		$self->{_permitted}->{$element} = $fields{$element};
-	}
+        $self->{_permitted}->{$element} = $fields{$element};
+    }
 
-	@{$self}{keys %fields} = values %fields;	# overkill?
-	return bless $self, $class;
+    @{$self}{keys %fields} = values %fields;	# overkill?
+    return bless $self, $class;
 }
 
 sub do_renew_for  {
-	my $self = shift;
-	my $borrower = shift;
-	my ($renewokay,$renewerror) = CanBookBeRenewed($borrower->{borrowernumber},$self->{item}->{itemnumber});
-	if ($renewokay){
+    my $self = shift;
+    my $borrower = shift;
+    my ($renewokay,$renewerror) = CanBookBeRenewed($borrower->{borrowernumber},$self->{item}->{itemnumber});
+    if ($renewokay){
         $self->{due} = undef;
         my $due_date = AddIssue( $borrower, $self->{item}->id, undef, 0 );
         if ($due_date) {
             $self->{due} = $due_date;
         }
         $self->renewal_ok(1);
-	} else {
+    } else {
         $renewerror=~s/on_reserve/Item unavailable due to outstanding holds/;
         $renewerror=~s/too_many/Item has reached maximum renewals/;
-		$self->screen_msg($renewerror);
-		$self->renewal_ok(0);
-	}
-	$self->ok(1);
-	return;
+        $self->screen_msg($renewerror);
+        $self->renewal_ok(0);
+    }
+    $self->ok(1);
+    return;
 }
 
 sub do_renew {
