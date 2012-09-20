88e822bfd7a7952189d59613368de1c83789123a
Christophe Croullebois <christophe.croullebois@biblibre.com>
2012-09-20 09:16:19 +0200

    Bug 8795: Adding the login branch as default in orderreceive.tt
    The homebranch and holdingbranch ddl in item block are not default selected.
    I just put the default choice as the login branch.
    
    Signed-off-by: Owen Leonard <oleonard@myacpl.org>
    
    Before the patch, orderreceive.pl showed the branches alphabetically.
    After the patch, it automatically selects the branch I'm logged in to.
    
    Signed-off-by: Jonathan Druart <jonathan.druart@biblibre.com>
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Signed-off-by: Jared Camins-Esakov <jcamins@cpbibliography.com>

--- C4/Items.pm
+++ C4/Items.pm
@@ -2720,6 +2720,7 @@ sub PrepareItemrecordDisplay {
                                 $authorised_lib{$branchcode} = $branchname;
                             }
                         }
+                        $defaultvalue = C4::Context->userenv->{branch};
 
                         #----- itemtypes
                     } elsif ( $tagslib->{$tag}->{$subfield}->{authorised_value} eq "itemtypes" ) {
