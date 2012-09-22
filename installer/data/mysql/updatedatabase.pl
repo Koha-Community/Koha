b577c32064f391c281b0414a999231ba2cf2903a
Jared Camins-Esakov <jcamins@cpbibliography.com>
2012-09-22 07:26:26 -0400

    Incrementing version number for 3.6.9 release

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -4719,6 +4719,12 @@ if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
     SetVersion ($DBversion);
 }
 
+$DBversion = "3.06.09.000";
+if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
+    print "Upgrade to $DBversion done (Incrementing version for 3.6.9 release. See release notes for details.) \n";
+    SetVersion ($DBversion);
+}
+
 
 =head1 FUNCTIONS
 
