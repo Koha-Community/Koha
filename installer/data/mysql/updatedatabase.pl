0ccb53477aea06ae1e8865cf81be962578437c16
Paul Poulain <paul.poulain@biblibre.com>
2012-09-24 21:32:48 +0200

    Bug 5356 follow-up DBRev number

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -5974,11 +5974,11 @@ if ( C4::Context->preference("Version") < TransformToNum($DBversion) ) {
 }
 
 
-$DBversion = "3.09.00.XXX";
+$DBversion = "3.09.00.057";
 if ( C4::Context->preference("Version") < TransformToNum($DBversion) ) {
     $dbh->do("ALTER TABLE aqbasket ADD deliveryplace VARCHAR(10) default NULL AFTER basketgroupid;");
     $dbh->do("ALTER TABLE aqbasket ADD billingplace VARCHAR(10) default NULL AFTER deliveryplace;");
-    print "Upgrade to $DBversion done (Added billingplace, deliveryplace to the aqbasket table)\n";
+    print "Upgrade to $DBversion done (Bug 5356: Added billingplace, deliveryplace to the aqbasket table)\n";
     SetVersion($DBversion);
 }
 
