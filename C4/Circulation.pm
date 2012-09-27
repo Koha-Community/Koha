4d9d883f6ffcfaff4b9ac1fbb09f8535fab7a43b
Mark Tompsett <mtompset@hotmail.com>
2012-09-27 14:12:00 +0800

    Bug 8834 - Call to C4::Context::preference fails to pass $self in Circulation.pm
    Changed:
        my $circcontrol = C4::Context::preference('CircControl');
    To:
        my $circcontrol = C4::Context->preference('CircControl');
    This will pass $self and 'CircControl' which will then prevent
    a "Use of uninitialized value" error log entry.
    
    Signed-off-by: Julian Maurice <julian.maurice@biblibre.com>
    Signed-off-by: Paul Poulain <paul.poulain@biblibre.com>
    Signed-off-by: Chris Cormack <chrisc@catalyst.net.nz>

--- C4/Circulation.pm
+++ C4/Circulation.pm
@@ -1794,7 +1794,7 @@ sub _debar_user_on_return {
     # $deltadays is a DateTime::Duration object
     my $deltadays = $calendar->days_between( $dt_due, $dt_today );
 
-    my $circcontrol = C4::Context::preference('CircControl');
+    my $circcontrol = C4::Context->preference('CircControl');
     my $issuingrule =
       GetIssuingRule( $borrower->{categorycode}, $item->{itype}, $branchcode );
     my $finedays = $issuingrule->{finedays};
