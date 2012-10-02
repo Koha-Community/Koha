4e6faebc94a683b76fa096ebd3f899dd8676ca31
Kyle M Hall <kyle@bywatersolutions.com>
2012-10-02 09:25:16 -0400

    Bug 8860 - can't merge if you don't have fast cataloging permissions
    Signed-off-by: Owen Leonard <oleonard@myacpl.org>
    Signed-off-by: Chris Cormack <chris@bigballofwax.co.nz>

--- cataloguing/merge_ajax.pl
+++ cataloguing/merge_ajax.pl
@@ -16,7 +16,7 @@ use CGI::Cookie; # need to check cookies before
                  # having CGI parse the POST request
 
 my %cookies = fetch CGI::Cookie;
-my ($auth_status, $sessionID) = check_cookie_auth($cookies{'CGISESSID'}->value, { editcatalogue => '1' });
+my ($auth_status, $sessionID) = check_cookie_auth($cookies{'CGISESSID'}->value, { editcatalogue => 'edit_catalogue' });
 if ($auth_status ne "ok") {
     my $reply = CGI->new("");
     print $reply->header(-type => 'text/html');
