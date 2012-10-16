d68a9f0c59a54ab3826130009f27c2fea3fb438a
Jonathan Druart <jonathan.druart@biblibre.com>
2012-10-16 09:31:08 +0200

    Bug 5356: Followup Change font in the layout3pages pdf
    Signed-off-by: Paul Poulain <paul.poulain@biblibre.com>

--- acqui/pdfformat/layout3pages.pm
+++ acqui/pdfformat/layout3pages.pm
@@ -93,6 +93,7 @@ sub printorders {
         $text->translate(20/mm,  ($height-15)/mm);
         $text->text("Order N°".$basketgroup->{'id'}.". Basket N° ".$basket->{basketno}.". ".$basket->{booksellernote});
         $text->translate(20/mm,  ($height-20)/mm);
+        $text->font( $pdf->corefont("Times", -encoding => "utf8"), 4/mm );
         $text->text( ( $basket->{billingplace} ? "Billing at " . C4::Branch::GetBranchName( $basket->{billingplace} ) : "" )
             . ( $basket->{billingplace} and $basket->{deliveryplace} ? " and " : "" )
             . ( $basket->{deliveryplace} ? "delivery at " . C4::Branch::GetBranchName( $basket->{deliveryplace}) : "" )
