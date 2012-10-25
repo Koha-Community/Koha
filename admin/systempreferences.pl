205993cf65f7fc07ab617a1caaa5d709f93d938e
Paul Poulain <paul.poulain@biblibre.com>
2012-10-25 16:08:31 +0200

    Bug 8895 follow-up fix for Jonathan's QA comment
    useless use of defined($data->{value}) because it's set to "" 2 lines earlier

--- admin/systempreferences.pl
+++ admin/systempreferences.pl
@@ -422,7 +422,7 @@ sub StringSearch {
                 while ( my $data = $sth->fetchrow_hashref ) {
                     unless (defined $data->{value}) { $data->{value} = "";}
                     $data->{shortvalue} = $data->{value};
-                    $data->{shortvalue} = substr( $data->{value}, 0, 60 ) . "..." if defined( $data->{value} ) and length( $data->{value} ) > 60;
+                    $data->{shortvalue} = substr( $data->{value}, 0, 60 ) . "..." if length( $data->{value} ) > 60;
                     push( @results, $data );
                     $cnt++;
                 }
