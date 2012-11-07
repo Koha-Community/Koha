a0819d9baa8167d18bd442a4b64964cbc888095d
Katrin Fischer <Katrin.Fischer.83@web.de>
2012-11-07 16:04:06 +0100

    Bug 9024: Add missing sample data to existing files
    Adds missing authorised values (enhancement)
    
    Adds missing slip notice templates (bug fix)
    Koha will complain if you try to print a slip and there is no notice
    defined for them, so this is not really an improvement, but kind of a bug fix.
    
    Signed-off-by: Mirko Tietgen <mirko@abunchofthings.net>
    Passed-QA-by: Paul Poulain <paul.poulain@biblibre.com>
    Signed-off-by: Jared Camins-Esakov <jcamins@cpbibliography.com>

--- installer/data/mysql/de-DE/optional/auth_val.sql
+++ installer/data/mysql/de-DE/optional/auth_val.sql
@@ -46,3 +46,7 @@ INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('MANUA
 
 -- custom borrower notes
 INSERT INTO `authorised_values` (category, authorised_value, lib) VALUES ('BOR_NOTES','ADDR','Adresse kontrollieren');
+
+-- OPAC Suggestions reasons
+INSERT INTO authorised_values (category,authorised_value,lib,lib_opac) VALUES ('OPAC_SUG','damaged','Vorhandenes Exemplar ist beschädigt','Vorhandenes Exemplare ist beschädigt');
+INSERT INTO authorised_values (category,authorised_value,lib,lib_opac) VALUES ('OPAC_SUG','bestseller','Neuerscheinung von bekanntem Verfasser','Neuerscheinung von bekanntem Verfasser');
