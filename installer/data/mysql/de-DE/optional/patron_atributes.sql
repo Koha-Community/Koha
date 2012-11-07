27e2f67898881fcd54ec12d9eff7e6e6a81f059e
Katrin Fischer <Katrin.Fischer.83@web.de>
2012-11-07 15:39:48 +0100

    Bug 9024: Adding sample patron attributes to German web installer
    Adds SHOW_BCODE as sample patron attribute to the German web installer.
    
    To test:
    - Run the web installer in German, make sure the new sample data shows up
    and can be installed.
    
    Signed-off-by: Mirko Tietgen <mirko@abunchofthings.net>
    Passed-QA-by: Paul Poulain <paul.poulain@biblibre.com>
    Signed-off-by: Jared Camins-Esakov <jcamins@cpbibliography.com>

---
+++ installer/data/mysql/de-DE/optional/patron_atributes.sql
@@ -0,0 +1,2 @@
+INSERT INTO `borrower_attribute_types` (`code`, `description`, `repeatable`, `unique_id`, `opac_display`, `password_allowed`, `staff_searchable`, `authorised_value_category`)
+VALUES ('SHOW_BCODE',  'Zeige den Barcode in der Exemplarübersicht', 0, 0, 1, 0, 0, 'YES_NO');
