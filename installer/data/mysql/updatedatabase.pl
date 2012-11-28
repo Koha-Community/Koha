fc5f9034b6fa86ef76e01cb8a5cb8289bc48a8f8
Jared Camins-Esakov <jcamins@cpbibliography.com>
2012-11-28 18:44:13 -0500

    Bug 9030: Increment version number
    Database update for bug 9030 has been given version 3.11.00.002

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -6083,7 +6083,7 @@ if ( C4::Context->preference("Version") < TransformToNum($DBversion) ) {
     SetVersion($DBversion);
 }
 
-$DBversion = "3.11.00.XXX";
+$DBversion = "3.11.00.002";
 if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
     $dbh->do(q{
         DELETE from aqorders_items where ordernumber NOT IN (SELECT ordernumber FROM aqorders);
