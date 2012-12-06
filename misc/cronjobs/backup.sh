5889a4486807a0316be93856e36591f79603dcba
Tomas Cohen Arazi <tomascohen@gmail.com>
2012-12-06 16:53:17 -0300

    Bug 9235 - Hardcoded values in cronjobs/backup.sh
    This patch makes the script read those parameters from the corresponding
    $KOHA_CONF file.
    
    Regards
    To+
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Jonathan Druart <jonathan.druart@biblibre.com>
    Signed-off-by: Jared Camins-Esakov <jcamins@cpbibliography.com>
    Signed-off-by: Chris Cormack <chrisc@catalyst.net.nz>

--- misc/cronjobs/backup.sh
+++ misc/cronjobs/backup.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 # Script to create daily backups of the Koha database.
 # Based on a script by John Pennington
+
+DATABASE=`xmlstarlet sel -t -v 'yazgfs/config/database' $KOHA_CONF`
+HOSTNAME=`xmlstarlet sel -t -v 'yazgfs/config/hostname' $KOHA_CONF`
+PORT=`xmlstarlet sel -t -v 'yazgfs/config/port' $KOHA_CONF`
+USER=`xmlstarlet sel -t -v 'yazgfs/config/user' $KOHA_CONF`
+PASS=`xmlstarlet sel -t -v 'yazgfs/config/pass' $KOHA_CONF`
 BACKUPDIR=`xmlstarlet sel -t -v 'yazgfs/config/backupdir' $KOHA_CONF`
-KOHA_DATE=`date '+%y%m%d'`
+KOHA_DATE=`date '+%Y%m%d'`
 KOHA_BACKUP=$BACKUPDIR/koha-$KOHA_DATE.sql.gz
 
-mysqldump --single-transaction -u koha -ppassword koha | gzip -9 > $KOHA_BACKUP
-
-#mv $KOHA_BACKUP /home/kohaadmin &&
-#chown kohaadmin.users /home/kohaadmin/koha-$KOHA_DATE.dump.gz &&
-#chmod 600 /home/kohaadmin/koha-$KOHA_DATE.dump.gz &&
-# Makes the compressed dump file property of the kohaadmin user.
-# Make sure that you replace kohaadmin with a real user.
+mysqldump --single-transaction --user=$USER --password="$PASS" --port=$PORT --host=$HOST $DATABASE| gzip -9 > $KOHA_BACKUP
 
 if [ -f $KOHA_BACKUP ] ; then
-echo "$KOHA_BACKUP was successfully created." | mail kohaadmin -s $KOHA_BACKUP
+echo "$KOHA_BACKUP was successfully created." | mail $USER -s $KOHA_BACKUP
 else
-echo "$KOHA_BACKUP was NOT successfully created." | mail kohaadmin -s $KOHA_BACKUP
+echo "$KOHA_BACKUP was NOT successfully created." | mail $USER -s $KOHA_BACKUP
 fi
 
 # Notifies kohaadmin of (un)successful backup creation
