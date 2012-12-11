e8a23877e505cafe9b4708af75d4b430054ea5c7
Fridolyn SOMERS <fridolyn.somers@biblibre.com>
2012-12-11 17:33:11 +0100

    Bug 9263: Trim barcodes in offline circulation
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Passed-QA-by: M. de Rooy <m.de.rooy@rijksmuseum.nl>
    Signed-off-by: Jared Camins-Esakov <jcamins@cpbibliography.com>

--- offline_circ/service.pl
+++ offline_circ/service.pl
@@ -33,24 +33,36 @@ my ($status, $cookie, $sessionId) = C4::Auth::check_api_auth($cgi, undef);
 my $result;
 
 if ($status eq 'ok') { # if authentication is ok
+
+    my $userid     = $cgi->param('userid')     || '';
+    my $branchcode = $cgi->param('branchcode') || '';
+    my $timestamp  = $cgi->param('timestamp')  || '';
+    my $action     = $cgi->param('action')     || '';
+    my $barcode    = $cgi->param('barcode')    || '';
+    $barcode    =~ s/^\s+//;
+    $barcode    =~ s/\s+$//;
+    my $cardnumber = $cgi->param('cardnumber') || '';
+    $cardnumber =~ s/^\s+//;
+    $cardnumber =~ s/\s+$//;
+
     if ( $cgi->param('pending') eq 'true' ) { # if the 'pending' flag is true, we store the operation in the db instead of directly processing them
         $result = AddOfflineOperation(
-            $cgi->param('userid')     || '',
-            $cgi->param('branchcode') || '',
-            $cgi->param('timestamp')  || '',
-            $cgi->param('action')     || '',
-            $cgi->param('barcode')    || '',
-            $cgi->param('cardnumber') || '',
+            $userid,
+            $branchcode,
+            $timestamp,
+            $action,
+            $barcode,
+            $cardnumber,
         );
     } else {
         $result = ProcessOfflineOperation(
             {
-                'userid'      => $cgi->param('userid'),
-                'branchcode'  => $cgi->param('branchcode'),
-                'timestamp'   => $cgi->param('timestamp'),
-                'action'      => $cgi->param('action'),
-                'barcode'     => $cgi->param('barcode'),
-                'cardnumber'  => $cgi->param('cardnumber'),
+                'userid'      => $userid,
+                'branchcode'  => $branchcode,
+                'timestamp'   => $timestamp,
+                'action'      => $action,
+                'barcode'     => $barcode,
+                'cardnumber'  => $cardnumber,
             }
         );
     }
