98ed353928a9265894e715a6ae6c148f0e7421bd
Jonathan Druart <jonathan.druart@biblibre.com>
2012-12-13 14:17:07 +0100

    Bug 9281: FIX add an order from a staged file
    In GetMember the hash parameter contained
    'C4::Members' => 'borrowernumber'
    XXX => undef
    
    Test plan:
    In the acquition module, try to add an order to a basket from a staged file.
    Without this patch, you get an error:
      DBD::mysql::st execute failed: called with 2 bind variables when 1 are needed at /home/koha/src/C4/Members.pm line 559
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Katrin Fischer <Katrin.Fischer.83@web.de>
    All tests pass, error only appearing in the log file.
    
    Test plan:
    1) Create a basket
    2) Add one or multiple order lines using a staged file and the 'batch' feature.
       This is done by using the "Save" button at the bottom of the page instead
       of the "Add order" link next to a single entry.
    3) Check your koha-error_log for the error above.
    Signed-off-by: Jared Camins-Esakov <jcamins@cpbibliography.com>
    
    Signed-off-by: Chris Cormack <chris@bigballofwax.co.nz>

--- acqui/addorderiso2709.pl
+++ acqui/addorderiso2709.pl
@@ -188,7 +188,7 @@ if ($op eq ""){
             SetImportRecordStatus( $biblio->{'import_record_id'}, 'imported' );
         }
         # 3rd add order
-        my $patron = C4::Members->GetMember( borrowernumber => $loggedinuser );
+        my $patron = C4::Members::GetMember( borrowernumber => $loggedinuser );
         my $branch = C4::Branch->GetBranchDetail( $patron->{branchcode} );
         my ($invoice);
         # get quantity in the MARC record (1 if none)
