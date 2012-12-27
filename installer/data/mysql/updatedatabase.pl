d97d8ee1ce230b76a71dc5ad1154b78cb73d9dee
Jared Camins-Esakov <jcamins@cpbibliography.com>
2012-12-27 09:57:27 -0500

    Bug 9162: Increment version number
    Database update for bug 9162 has been given version 3.11.00.013

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -6324,7 +6324,7 @@ if ( CheckVersion($DBversion) ) {
     SetVersion($DBversion);
 }
 
-$DBversion = "3.11.00.XXX";
+$DBversion = "3.11.00.013";
 if ( CheckVersion($DBversion) ) {
     $dbh->do("INSERT INTO systempreferences (variable,value,explanation,options,type) VALUES('NotesBlacklist','','List of notes fields that should not appear in the title notes/description separator of details',NULL,'free')");
     print "Upgrade to $DBversion done (Bug 9162 - Add a system preference to set which notes fields appears on title notes/description separator)\n";
