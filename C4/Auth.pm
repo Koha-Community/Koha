69a8048eeecacff99b64fa722fa8850891476031
Kyle M Hall <kyle@bywatersolutions.com>
2013-02-19 11:41:59 -0500

    Bug 9541: make OPAC login page respect OPAC_CSS_OVERRIDE
    When clicking the login link for opac-user.pl in a multiple branch
    scenario the environment variable for OPAC_CSS_OVERRIDE was ignored from
    the koha-conf.xml file.  It seems like is is working on every page in
    the opac except for the login page.
    
    Test Plan:
    1) Set up a Koha server with 2 separate catalog configurations
       ( e.g. opac1.kohatest, opac2.kohatest )
    2) Set the OPAC_CSS_OVERRIDE directive for separate css files
       in each opac
    3) Browse to the opac login page, note the css is not applied
    4) Apply this patch
    5) Reload the page, note the css is now applied
    
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>
    Signed-off-by: Chris Cormack <chris@bigballofwax.co.nz>
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>
    (cherry picked from commit 71b79d59908cd85130afd8600eff0f93fc79b8a9)
    Signed-off-by: Tomas Cohen Arazi <tomascohen@gmail.com>

--- C4/Auth.pm
+++ C4/Auth.pm
@@ -1022,6 +1022,7 @@ sub checkauth {
         PatronSelfRegistration => C4::Context->preference("PatronSelfRegistration"),
         PatronSelfRegistrationDefaultCategory => C4::Context->preference("PatronSelfRegistrationDefaultCategory"),
         persona            => C4::Context->preference("Persona"),
+        opac_css_override => $ENV{'OPAC_CSS_OVERRIDE'},
     );
 
     $template->param( OpacPublic => C4::Context->preference("OpacPublic"));
