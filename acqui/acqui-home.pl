ed8e506953b78de0a09b87011709281054641ab0
Owen Leonard <oleonard@myacpl.org>
2013-03-11 16:11:26 -0400

    Bug 9744 - Add a column Fund name on the home page of acquisitions
    This patch offers an alternative option to fix Bug 9744. In this version
    the table of funds swaps positions with the suggestions block so that
    the table has the whole width of the screen. This eliminates the need to
    adjust its float property.
    
    Other changes:
    
    - Simplification of the column and row-hiding JavaScript
    - The addition of an "Active" column to be shown when all funds
      are shown (this helps indicate to the user which rows were hidden)
    - Linking the fund owner to their patron record
    - Linking the fund id, given the correct permissions, to the view of all
      funds for that budget (the best alternative to linking to a view of
      the fund details, which we do not have).
    - Correcting permission level required to add a budget
    
    To test, view the acquisitions home page. The layout should feel
    comfortable. The table of available funds should show the fund name.
    The checkbox to show all funds should work correctly.
    
    Signed-off-by: caroline very-mathieu <caroline.very-mathieu@nimes-ville.fr>
    Signed-off-by: Chris Cormack <chrisc@catalyst.net.nz>

--- acqui/acqui-home.pl
+++ acqui/acqui-home.pl
@@ -102,8 +102,9 @@ foreach my $budget ( @{$budget_arr} ) {
 
     my $member = GetMember( borrowernumber => $budget->{budget_owner_id} );
     if ($member) {
-        $budget->{budget_owner} =
-          $member->{'firstname'} . ' ' . $member->{'surname'};
+        $budget->{budget_owner_firstname} = $member->{'firstname'};
+        $budget->{budget_owner_surname} = $member->{'surname'};
+        $budget->{budget_owner_borrowernumber} = $member->{'borrowernumber'};
     }
 
     if ( !defined $budget->{budget_amount} ) {
