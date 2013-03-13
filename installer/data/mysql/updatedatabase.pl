ffc8076d1ac9de7ce8a1b3a525797366b3f7fea1
Jared Camins-Esakov <jcamins@cpbibliography.com>
2013-03-13 08:15:09 -0400

    Bug 6898: Increment version number
    Database update for bug 6898 has been given version 3.11.00.027

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -6486,7 +6486,7 @@ if ( CheckVersion($DBversion) ) {
     SetVersion($DBversion);
 }
 
-$DBversion = "3.11.00.XXX";
+$DBversion = "3.11.00.027";
 if ( CheckVersion($DBversion) ) {
     $dbh->do(q{
         INSERT INTO permissions ( module_bit, code, description )
