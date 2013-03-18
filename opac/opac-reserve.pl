388a4bd47cbd4f031b5f48674572725da0d4403a
Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
2013-03-18 11:20:42 +0100

    Bug 10321: Followup for hold notes
    Based on work for report 9722.
    This patch resolves a small display problem with the number of columns of the
    table on opac-reserve.
    
    Signed-off-by: Srdjan <srdjan@catalyst.net.nz>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@biblibre.com>
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>

--- opac/opac-reserve.pl
+++ opac/opac-reserve.pl
@@ -548,13 +548,14 @@ if ( $numBibsAvailable == 0 || $anyholdable == 0) {
     $template->param( none_available => 1 );
 }
 
-my $itemTableColspan = 8;
+my $itemTableColspan = 9;
 if (! $template->{VARS}->{'OPACItemHolds'}) {
     $itemTableColspan--;
 }
 if (! $template->{VARS}->{'singleBranchMode'}) {
     $itemTableColspan--;
 }
+$itemTableColspan-- if !$show_holds_count && !$show_priority;
 my $show_notes=C4::Context->preference('OPACShowHoldNotes');
 $template->param(OPACShowHoldNotes=>$show_notes);
 $itemTableColspan-- if !$show_notes;
