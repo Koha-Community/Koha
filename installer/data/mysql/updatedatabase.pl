3293434c23dad2298e38dc393ca2e000ef068cef
Jared Camins-Esakov <jcamins@cpbibliography.com>
2013-03-23 09:54:28 -0400

    Bump version for Koha 3.12.00-beta1 release

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -6742,6 +6742,13 @@ if ( CheckVersion($DBversion) ) {
     SetVersion ($DBversion);
 }
 
+$DBversion = "3.11.00.200";
+if ( CheckVersion($DBversion) ) {
+    print "Upgrade to $DBversion done (3.12-beta1 release)\n";
+    SetVersion ($DBversion);
+}
+
+
 =head1 FUNCTIONS
 
 =head2 TableExists($table)
