629f31a79df41547db20aff5b63134ef1fd85e89
Kyle M Hall <kyle@bywatersolutions.com>
2013-04-10 13:49:16 -0400

    Bug 10027: (follow-up) tidy members/printinvoice.pl
    Signed-off-by: Chris Cormack <chrisc@catalyst.net.nz>
    Signed-off-by: Katrin Fischer <Katrin.Fischer.83@web.de>
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>
    (cherry picked from commit 70f23f2a69a66b76d411f81787e8e63e0a87a2a1)
    Signed-off-by: Tomas Cohen Arazi <tomascohen@gmail.com>

--- members/printinvoice.pl
+++ members/printinvoice.pl
@@ -1,9 +1,7 @@
 #!/usr/bin/perl
 
-
 #writen 3rd May 2010 by kmkale@anantcorp.com adapted from boraccount.pl by chris@katipo.oc.nz
 #script to print fee receipts
-
 
 # Copyright Koustubha Kale
 #
@@ -33,110 +31,112 @@ use C4::Members;
 use C4::Branch;
 use C4::Accounts;
 
-my $input=new CGI;
+my $input = new CGI;
 
+my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
+    {   template_name   => "members/printinvoice.tmpl",
+        query           => $input,
+        type            => "intranet",
+        authnotrequired => 0,
+        flagsrequired   => { borrowers => 1, updatecharges => 1 },
+        debug           => 1,
+    }
+);
 
-my ($template, $loggedinuser, $cookie)
-    = get_template_and_user({template_name => "members/printinvoice.tmpl",
-                            query => $input,
-                            type => "intranet",
-                            authnotrequired => 0,
-                            flagsrequired => {borrowers => 1, updatecharges => 1},
-                            debug => 1,
-                            });
-
-my $borrowernumber=$input->param('borrowernumber');
-my $action = $input->param('action') || '';
+my $borrowernumber  = $input->param('borrowernumber');
+my $action          = $input->param('action') || '';
 my $accountlines_id = $input->param('accountlines_id');
 
 #get borrower details
-my $data=GetMember('borrowernumber' => $borrowernumber);
+my $data = GetMember( 'borrowernumber' => $borrowernumber );
 
-if ( $action eq 'print' ) {
-#  ReversePayment( $borrowernumber, $input->param('accountno') );
-}
-
-if ( $data->{'category_type'} eq 'C') {
-   my  ( $catcodes, $labels ) =  GetborCatFromCatType( 'A', 'WHERE category_type = ?' );
-   my $cnt = scalar(@$catcodes);
-   $template->param( 'CATCODE_MULTI' => 1) if $cnt > 1;
-   $template->param( 'catcode' =>    $catcodes->[0])  if $cnt == 1;
+if ( $data->{'category_type'} eq 'C' ) {
+    my ( $catcodes, $labels ) = GetborCatFromCatType( 'A', 'WHERE category_type = ?' );
+    my $cnt = scalar(@$catcodes);
+    $template->param( 'CATCODE_MULTI' => 1 ) if $cnt > 1;
+    $template->param( 'catcode' => $catcodes->[0] ) if $cnt == 1;
 }
 
 #get account details
-my ($total,$accts,$numaccts)=GetMemberAccountRecords($borrowernumber);
+my ( $total, $accts, $numaccts ) = GetMemberAccountRecords($borrowernumber);
 my $totalcredit;
-if($total <= 0){
-        $totalcredit = 1;
+if ( $total <= 0 ) {
+    $totalcredit = 1;
 }
-my @accountrows; # this is for the tmpl-loop
+
+my @accountrows;    # this is for the tmpl-loop
 
 my $toggle;
-for (my $i=0;$i<$numaccts;$i++){
-   next if ($accts->[$i]{'accountlines_id'} ne $accountlines_id );
-    if($i%2){
-            $toggle = 0;
+for ( my $i = 0 ; $i < $numaccts ; $i++ ) {
+    next if ( $accts->[$i]{'accountlines_id'} ne $accountlines_id );
+
+    if ( $i % 2 ) {
+        $toggle = 0;
     } else {
-            $toggle = 1;
+        $toggle = 1;
     }
+
     $accts->[$i]{'toggle'} = $toggle;
-    $accts->[$i]{'amount'}+=0.00;
-    if($accts->[$i]{'amount'} <= 0){
+    $accts->[$i]{'amount'} += 0.00;
+
+    if ( $accts->[$i]{'amount'} <= 0 ) {
         $accts->[$i]{'amountcredit'} = 1;
     }
-    $accts->[$i]{'amountoutstanding'}+=0.00;
-    if($accts->[$i]{'amountoutstanding'} <= 0){
+
+    $accts->[$i]{'amountoutstanding'} += 0.00;
+    if ( $accts->[$i]{'amountoutstanding'} <= 0 ) {
         $accts->[$i]{'amountoutstandingcredit'} = 1;
     }
-    my %row = ( 'date'              => format_date($accts->[$i]{'date'}),
-                'amountcredit' => $accts->[$i]{'amountcredit'},
-                'amountoutstandingcredit' => $accts->[$i]{'amountoutstandingcredit'},
-                'toggle' => $accts->[$i]{'toggle'},
-                'description'       => $accts->[$i]{'description'},
-				'itemnumber'       => $accts->[$i]{'itemnumber'},
-				'biblionumber'       => $accts->[$i]{'biblionumber'},
-                'amount'            => sprintf("%.2f",$accts->[$i]{'amount'}),
-                'amountoutstanding' => sprintf("%.2f",$accts->[$i]{'amountoutstanding'}),
-                'accountno' => $accts->[$i]{'accountno'},
-                'payment' => ( $accts->[$i]{'accounttype'} eq 'Pay' ),
 
-                );
+    my %row = (
+        'date'                    => format_date( $accts->[$i]{'date'} ),
+        'amountcredit'            => $accts->[$i]{'amountcredit'},
+        'amountoutstandingcredit' => $accts->[$i]{'amountoutstandingcredit'},
+        'toggle'                  => $accts->[$i]{'toggle'},
+        'description'             => $accts->[$i]{'description'},
+        'itemnumber'              => $accts->[$i]{'itemnumber'},
+        'biblionumber'            => $accts->[$i]{'biblionumber'},
+        'amount'                  => sprintf( "%.2f", $accts->[$i]{'amount'} ),
+        'amountoutstanding'       => sprintf( "%.2f", $accts->[$i]{'amountoutstanding'} ),
+        'accountno'               => $accts->[$i]{'accountno'},
+        'payment'                 => ( $accts->[$i]{'accounttype'} eq 'Pay' ),
+    );
 
-    if ($accts->[$i]{'accounttype'} ne 'F' && $accts->[$i]{'accounttype'} ne 'FU'){
-        $row{'printtitle'}=1;
-        $row{'title'} = $accts->[$i]{'title'};
+    if ( $accts->[$i]{'accounttype'} ne 'F' && $accts->[$i]{'accounttype'} ne 'FU' ) {
+        $row{'printtitle'} = 1;
+        $row{'title'}      = $accts->[$i]{'title'};
     }
 
-    push(@accountrows, \%row);
+    push( @accountrows, \%row );
 }
 
 $template->param( adultborrower => 1 ) if ( $data->{'category_type'} eq 'A' );
 
-my ($picture, $dberror) = GetPatronImage($data->{'cardnumber'});
+my ( $picture, $dberror ) = GetPatronImage( $data->{'cardnumber'} );
 $template->param( picture => 1 ) if $picture;
 
 $template->param(
-    finesview           => 1,
-    firstname           => $data->{'firstname'},
-    surname             => $data->{'surname'},
-    borrowernumber      => $borrowernumber,
-    cardnumber          => $data->{'cardnumber'},
-    categorycode        => $data->{'categorycode'},
-    category_type       => $data->{'category_type'},
- #   category_description => $data->{'description'},
-    categoryname		 => $data->{'description'},
-    address             => $data->{'address'},
-    address2            => $data->{'address2'},
-    city                => $data->{'city'},
-    zipcode             => $data->{'zipcode'},
-    country             => $data->{'country'},
-    phone               => $data->{'phone'},
-    email               => $data->{'email'},
-    branchcode          => $data->{'branchcode'},
-	branchname			=> GetBranchName($data->{'branchcode'}),
-    total               => sprintf("%.2f",$total),
-    totalcredit         => $totalcredit,
-	is_child        => ($data->{'category_type'} eq 'C'),
-    accounts            => \@accountrows );
+    finesview      => 1,
+    firstname      => $data->{'firstname'},
+    surname        => $data->{'surname'},
+    borrowernumber => $borrowernumber,
+    cardnumber     => $data->{'cardnumber'},
+    categorycode   => $data->{'categorycode'},
+    category_type  => $data->{'category_type'},
+    categoryname   => $data->{'description'},
+    address        => $data->{'address'},
+    address2       => $data->{'address2'},
+    city           => $data->{'city'},
+    zipcode        => $data->{'zipcode'},
+    country        => $data->{'country'},
+    phone          => $data->{'phone'},
+    email          => $data->{'email'},
+    branchcode     => $data->{'branchcode'},
+    branchname     => GetBranchName( $data->{'branchcode'} ),
+    total          => sprintf( "%.2f", $total ),
+    totalcredit    => $totalcredit,
+    is_child       => ( $data->{'category_type'} eq 'C' ),
+    accounts       => \@accountrows
+);
 
 output_html_with_http_headers $input, $cookie, $template->output;
