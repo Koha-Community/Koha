6363306c10c119a2d362a11082eadf0d84c53f15
Katrin Fischer <Katrin.Fischer.83@web.de>
2013-04-14 23:28:41 +0200

    Bug 10050: Update German web installer files for 3.12 - Language settings
    Activate German and English for OPAC and intranet by default.
    
    To test:
    - Run the German web installer
    - Check that when logging into Koha the German interface is shown
      and you can switch between English and German using the links
      at the bottom of the page
    - Check language relevant settings in the system preferences
      are correctly set
    
    Note: The German po files in master are buggy, please
    use files from translate.koha-community.org for testing.
    
    Signed-off-by: Chris Cormack <chris@bigballofwax.co.nz>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@biblibre.com>
    Signed-off-by: Jared Camins-Esakov <jcamins@cpbibliography.com>
    (cherry picked from commit d8d4d9973fcd992570c6bfc0b2f8d1acd94a2aca)
    
    Signed-off-by: Jared Camins-Esakov <jcamins@cpbibliography.com>

--- installer/data/mysql/de-DE/mandatory/system_preferences.sql
+++ installer/data/mysql/de-DE/mandatory/system_preferences.sql
@@ -34,3 +34,6 @@ UPDATE systempreferences SET value = 'Monday' WHERE variable = 'CalendarFirstDay
 UPDATE systempreferences SET value = '0.07|0.19|0.00' WHERE variable = 'gist';
 UPDATE systempreferences SET value = 'Dieser Text wird über den Systemparameter <a href="/cgi-bin/koha/admin/preferences.pl?op=search&searchfield=RoutingListNote#jumped">RoutlingListNote</a> konfiguriert.' where variable = 'RoutingListNote';
 UPDATE systempreferences SET value = 'barcode stocknumber' WHERE variable = 'uniqueitemfields';
+UPDATE systempreferences SET value = 'de-DE,en' WHERE variable = 'language';
+UPDATE systempreferences SET value = 'de-DE,en' WHERE variable = 'opaclanguages';
+UPDATE systempreferences SET value = '1' WHERE variable = 'opaclanguagesdisplay';
