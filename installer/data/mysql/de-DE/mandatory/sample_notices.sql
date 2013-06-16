ce1eb1db747ef4ff7b6329c76c83f022be66de04
Katrin Fischer <Katrin.Fischer.83@web.de>
2013-06-16 09:09:33 +0200

    Bug 10474: Translate some English strings in German sample notices
    This translates 2 strings missed doing the initial translation in
    ISSUESLIP.
    
    To test:
    - Run the web installer in German.
    Altnernatively:
    - Truncate your letters table and run the SQL against the database.
    
    Check that the notices were added correctly, especially ISSUESLIP.
    
    Signed-off-by: Chris Cormack <chris@bigballofwax.co.nz>
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>
    (cherry picked from commit aecb163f69b238314bbd67215ce2d8950aeab060)
    Signed-off-by: Tomas Cohen Arazi <tomascohen@gmail.com>
    (cherry picked from commit 32de75b948944debe41380293c58cfe02bcbcc2d)
    Signed-off-by: Bernardo Gonzalez Kriegel <bgkriegel@gmail.com>
    (cherry picked from commit 32de75b948944debe41380293c58cfe02bcbcc2d)

--- installer/data/mysql/de-DE/mandatory/sample_notices.sql
+++ installer/data/mysql/de-DE/mandatory/sample_notices.sql
@@ -44,12 +44,12 @@ Fällig am: <<issues.date_due>><br />
 
 <hr>
 
-<h4 style="text-align: center; font-style:italic;">News</h4>
+<h4 style="text-align: center; font-style:italic;">Neuigkeiten</h4>
 <news>
 <div class="newsitem">
 <h5 style="margin-bottom: 1px; margin-top: 1px"><b><<opac_news.title>></b></h5>
 <p style="margin-bottom: 1px; margin-top: 1px"><<opac_news.new>></p>
-<p class="newsfooter" style="font-size: 8pt; font-style:italic; margin-bottom: 1px; margin-top: 1px">Posted on <<opac_news.timestamp>></p>
+<p class="newsfooter" style="font-size: 8pt; font-style:italic; margin-bottom: 1px; margin-top: 1px">Veröffentlicht am <<opac_news.timestamp>></p>
 <hr />
 </div>
 </news>', 1),
