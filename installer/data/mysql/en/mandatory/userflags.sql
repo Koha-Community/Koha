2bcec70262ce8127e3755327d4d5261e0e964844
Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
2013-06-24 12:33:58 +0200

    Bug 7368: Followup for columns names in userflags.sql
    An insert statement should contain the column names to avoid confusion and
    future problems with table definition changes.
    
    This patch updates the English userflags.sql.
    
    Will send updates for the other languages in connection with replacing inserts
    by updates for the additional languages (where possible). For userflags this
    will be possible.
    
    Test plan:
    Do an install (English).
    
    Or -- as a dangerous(!) alternative--:
    Do this ONLY on a restorable database.
    Delete all records in userflags. (Note: This cascades to permissions a.o.)
    Run this file from command line.
    Check if you have records again in userflags. (You also messed up your Koha.)
    Restore your database.
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Tested both ways myself :)
    
    Signed-off-by: Bernardo Gonzalez Kriegel <bgkriegel@gmail.com>
    
    Comment: Work as described. No errors.
    Tested using second alternative
    
    Just a note: if you update other lang files replacing INSERTs for UPDATEs
    then you must install using EN and later apply lang files by hand.
    If you do a translate first before webinstaller, and finish your install
    in your lang, then that kind of file will fail.
    
    Signed-off-by: Jonathan Druart <jonathan.druart@biblibre.com>
    
    mysql> create database test;
    Query OK, 1 row affected (0.00 sec)
    
    % mysql -u koha -p test < installer/data/mysql/kohastructure.sql
    % mysql -u koha -p test < installer/data/mysql/en/mandatory/userflags.sql
    
    mysql> select count(*) from userflags;
    +----------+
    | count(*) |
    +----------+
    |       19 |
    +----------+
    1 row in set (0.00 sec)
    
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>

--- installer/data/mysql/en/mandatory/userflags.sql
+++ installer/data/mysql/en/mandatory/userflags.sql
@@ -1,19 +1,20 @@
-INSERT INTO `userflags` VALUES(0,'superlibrarian','Access to all librarian functions',0);
-INSERT INTO `userflags` VALUES(1,'circulate','Check out and check in items',0);
-INSERT INTO `userflags` VALUES(2,'catalogue','<b>Required for staff login.</b> Staff access, allows viewing of catalogue in staff client.',0);
-INSERT INTO `userflags` VALUES(3,'parameters','Manage Koha system settings (Administration panel)',0);
-INSERT INTO `userflags` VALUES(4,'borrowers','Add or modify patrons',0);
-INSERT INTO `userflags` VALUES(5,'permissions','Set user permissions',0);
-INSERT INTO `userflags` VALUES(6,'reserveforothers','Place and modify holds for patrons',0);
-INSERT INTO `userflags` VALUES(7,'borrow','Borrow books',1);
-INSERT INTO `userflags` VALUES(9,'editcatalogue','Edit Catalog (Modify bibliographic/holdings data)',0);
-INSERT INTO `userflags` VALUES(10,'updatecharges','Manage patrons fines and fees',0);
-INSERT INTO `userflags` VALUES(11,'acquisition','Acquisition and/or suggestion management',0);
-INSERT INTO `userflags` VALUES(12,'management','Set library management parameters (deprecated)',0);
-INSERT INTO `userflags` VALUES(13,'tools','Use all tools (expand for granular tools permissions)',0);
-INSERT INTO `userflags` VALUES(14,'editauthorities','Edit Authorities',0);
-INSERT INTO `userflags` VALUES(15,'serials','Manage serial subscriptions',0);
-INSERT INTO `userflags` VALUES(16,'reports','Allow access to the reports module',0);
-INSERT INTO `userflags` VALUES(17,'staffaccess','Allow staff members to modify permissions for other staff members',0);
-INSERT INTO `userflags` VALUES(18,'coursereserves','Course Reserves',0);
-INSERT INTO `userflags` VALUES(19, 'plugins', 'Koha plugins', '0');
+INSERT INTO userflags (bit, flag, flagdesc, defaulton) VALUES
+(0,'superlibrarian','Access to all librarian functions',0),
+(1,'circulate','Check out and check in items',0),
+(2,'catalogue','<b>Required for staff login.</b> Staff access, allows viewing of catalogue in staff client.',0),
+(3,'parameters','Manage Koha system settings (Administration panel)',0),
+(4,'borrowers','Add or modify patrons',0),
+(5,'permissions','Set user permissions',0),
+(6,'reserveforothers','Place and modify holds for patrons',0),
+(7,'borrow','Borrow books',1),
+(9,'editcatalogue','Edit Catalog (Modify bibliographic/holdings data)',0),
+(10,'updatecharges','Manage patrons fines and fees',0),
+(11,'acquisition','Acquisition and/or suggestion management',0),
+(12,'management','Set library management parameters (deprecated)',0),
+(13,'tools','Use all tools (expand for granular tools permissions)',0),
+(14,'editauthorities','Edit Authorities',0),
+(15,'serials','Manage serial subscriptions',0),
+(16,'reports','Allow access to the reports module',0),
+(17,'staffaccess','Allow staff members to modify permissions for other staff members',0),
+(18,'coursereserves','Course Reserves',0),
+(19, 'plugins', 'Koha plugins', '0');
