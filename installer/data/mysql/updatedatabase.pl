707733ed2992a5d612d22510174301d65d3d7c05
Sonia LEMAIRE <sonia.lemaire@biblibre.com>
2013-07-03 15:53:49 +0200

    Bug 9519: fix code for Italian language search limiter
    'ind' rather than 'ita' was set as the ISO639-2 language
    code for Italian in the fr-FR, it-IT, pl-PL, ru-RU, and uk-UA
    installation SQL.
    
    Signed-off-by: Chris Cormack <chris@bigballofwax.co.nz>
    Signed-off-by: Katrin Fischer <Katrin.Fischer.83@web.de>
    I checked nb-NO and de-DE - language code there had already
    been corrected.
    
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -7029,6 +7029,12 @@ INSERT INTO systempreferences (variable,value,explanation,options,type) VALUES (
     SetVersion($DBversion);
 }
 
+$DBversion = "3.13.00.XXX";
+if ( CheckVersion($DBversion) ) {
+    $dbh->do("UPDATE language_rfc4646_to_iso639 SET iso639_2_code='ita' WHERE rfc4646_subtag='it'");
+    print "Upgrade to $DBversion done (Bug 9519: Wrong language code for Italian in the advanced search language limitations)\n";
+    SetVersion($DBversion);
+}
 
 =head1 FUNCTIONS
 
