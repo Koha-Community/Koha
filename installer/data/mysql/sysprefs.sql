0cfe0fa92328e62d1fbca33ea69869ad4f034c1b
Tomas Cohen Arazi <tomascohen@gmail.com>
2013-07-15 11:56:14 -0300

    Bug 10593: make AuthoritiesLog default to yes
    This trivial patch set the default to 'yes' for this system preference.
    To test,
    On a clean install
    - Navigate Home > Administration > Global System preferences > Logs
    - AuthoritiesLog is set to "Don't log"
    Apply the patch, install koha on a new DB.
    - Navigate Home > Administration > Global System preferences > Logs
    - AuthoritiesLog should be set to "Log"
    
    Sponsored-by: Universidad Nacional de Cordoba
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Katrin Fischer <Katrin.Fischer.83@web.de>
    One line patch, changing the default for new installations only.
    
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>

--- installer/data/mysql/sysprefs.sql
+++ installer/data/mysql/sysprefs.sql
@@ -33,7 +33,7 @@ INSERT INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `
 ('AnonSuggestions','0',NULL,'Set to enable Anonymous suggestions to AnonymousPatron borrowernumber','YesNo'),
 ('AnonymousPatron','0',NULL,'Set the identifier (borrowernumber) of the anonymous patron. Used for Suggestion and reading history privacy',''),
 ('AuthDisplayHierarchy','0','','Display authority hierarchies','YesNo'),
-('AuthoritiesLog','0','0','If ON, log edit/create/delete actions on authorities.','YesNo'),
+('AuthoritiesLog','1',NULL,'If ON, log edit/create/delete actions on authorities.','YesNo'),
 ('authoritysep','--','10','Used to separate a list of authorities in a display. Usually --','free'),
 ('autoBarcode','OFF','incremental|annual|hbyymmincr|EAN13|OFF','Used to autogenerate a barcode: incremental will be of the form 1, 2, 3; annual of the form 2007-0001, 2007-0002; hbyymmincr of the form HB08010001 where HB=Home Branch','Choice'),
 ('AutoCreateAuthorities','0',NULL,'Automatically create authorities that do not exist when cataloging records.','YesNo'),
