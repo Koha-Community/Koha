f9d3e02b831381061d2d298b33818ad926a88c07
Galen Charlton <gmc@esilibrary.com>
2013-07-22 14:46:29 +0000

    Bug 10604: DBRev 3.13.00.014
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -7050,7 +7050,7 @@ if ( CheckVersion($DBversion) ) {
     SetVersion($DBversion);
 }
 
-$DBversion = "3.13.00.XXX";
+$DBversion = "3.13.00.014";
 if ( CheckVersion($DBversion) ) {
     $dbh->do("ALTER TABLE courses MODIFY COLUMN department varchar(80) DEFAULT NULL;");
     $dbh->do("ALTER TABLE courses MODIFY COLUMN term       varchar(80) DEFAULT NULL;");
