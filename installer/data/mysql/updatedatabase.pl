0e43c8ac1bfbbbb7f7c524628f5395628aa9b596
Bernardo Gonzalez Kriegel <bgkriegel@gmail.com>
2013-07-23 18:23:40 -0300

    Database update for 3.10.8 release
    Signed-off-by: Bernardo Gonzalez Kriegel <bgkriegel@gmail.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -6291,6 +6291,12 @@ if ( C4::Context->preference("Version") < TransformToNum($DBversion) ) {
     SetVersion($DBversion);
 }
 
+$DBversion = "3.10.08.000";
+if ( C4::Context->preference("Version") < TransformToNum($DBversion) ) {
+    print "Upgrade to $DBversion done (3.10.8 release)\n";
+    SetVersion($DBversion);
+}
+
 =head1 FUNCTIONS
 
 =head2 TableExists($table)
