9c27e64c1729de2a892e14c049643c135dc7e2a1
Jared Camins-Esakov <jcamins@cpbibliography.com>
2013-07-29 08:49:55 -0400

    Increment version for 3.6.12 security release
    Signed-off-by: Jared Camins-Esakov <jcamins@cpbibliography.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -4732,9 +4732,9 @@ if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
     SetVersion ($DBversion);
 }
 
-$DBversion = "3.06.11.000";
+$DBversion = "3.06.12.000";
 if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
-    print "Upgrade to $DBversion done (Incrementing version for 3.6.10 release. See release notes for details.) \n";
+    print "Upgrade to $DBversion done (Incrementing version for 3.6.12 security release. See release notes for details.) \n";
     SetVersion ($DBversion);
 }
 
