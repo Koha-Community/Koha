b8a16ee2fc2a5c1a3a0939abb77c00b9e354bcc8
Chris Cormack <chrisc@catalyst.net.nz>
2013-08-06 10:45:44 +1200

    Bug 10419: (followup) fix license statement
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>
    Signed-off-by: Jonathan Druart <jonathan.druart@biblibre.com>
    Signed-off-by: Katrin Fischer <Katrin.Fischer.83@web.de>
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>

--- misc/cronjobs/delete_patrons.pl
+++ misc/cronjobs/delete_patrons.pl
@@ -127,7 +127,7 @@ Copyright 2013 BibLibre
 This file is part of Koha.
 
 Koha is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software
-Foundation; either version 2 of the License, or (at your option) any later version.
+Foundation; either version 3 of the License, or (at your option) any later version.
 
 You should have received a copy of the GNU General Public License along
 with Koha; if not, write to the Free Software Foundation, Inc.,
