64c3c216498a0144319639dd381c28c9d717f1bb
Owen Leonard <oleonard@myacpl.org>
2013-08-19 11:16:51 -0400

    Bug 10763 - [SIGNED-OFF] Update POD of C4::Creators::Lib::html_table() to use TT syntax
    This patch updates the example template syntax in the POD for
    C4::Creators::Lib::html_table() to use Template Toolkit syntax.
    
    To test, view the POD for C4::Creators::Lib::html_table() and confirm
    that it looks correct.
    
    Signed-off-by: Magnus Enger <magnus@enger.priv.no>
    Checked the POD with "perldoc C4/Creators/Lib.pm" before and after applying
    the patch. The example now uses TT syntax, and looks sensible.
    
    Signed-off-by: Jonathan Druart <jonathan.druart@biblibre.com>
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>
    (cherry picked from commit 54c6dccca70a142c14fff049cb3212694af86610)
    Signed-off-by: Tomas Cohen Arazi <tomascohen@gmail.com>
    (cherry picked from commit a55080c88593b5e0e0d3d8192ee9c7f09e78d258)
    Signed-off-by: Bernardo Gonzalez Kriegel <bgkriegel@gmail.com>
    (cherry picked from commit a55080c88593b5e0e0d3d8192ee9c7f09e78d258)

--- C4/Creators/Lib.pm
+++ C4/Creators/Lib.pm
@@ -477,37 +477,35 @@ sub get_table_names {
 =head2 C4::Creators::Lib::html_table()
 
 This function returns an arrayref of an array of hashes contianing the supplied data formatted suitably to
-be passed off as a T::P template parameter and used to build an html table.
+be passed off as a template parameter and used to build an html table.
 
    my $table = html_table(header_fields, array_of_row_data);
    $template->param(
-       TABLE => $table,
+       table_loop => $table,
    );
 
     html example:
 
-       <table>
-            <!-- TMPL_LOOP NAME="TABLE" -->
-            <!-- TMPL_IF NAME="header_fields" -->
-            <tr>
-            <!-- TMPL_LOOP NAME="header_fields" -->
-                <th><!-- TMPL_VAR NAME="field_label" --></th>
-            <!-- /TMPL_LOOP -->
-            </tr>
-            <!-- TMPL_ELSE -->
-            <tr>
-            <!-- TMPL_LOOP NAME="text_fields" -->
-            <!-- TMPL_IF NAME="select_field" -->
-                <td align="center"><input type="checkbox" name="action" value="<!-- TMPL_VAR NAME="field_value" -->" /></td>
-            <!-- TMPL_ELSIF NAME="field_value" -->
-                <td><!-- TMPL_VAR NAME="field_value" --></td>
-            <!-- TMPL_ELSE -->
-                <td>&nbsp;</td>
-            <!-- /TMPL_IF -->
-            <!-- /TMPL_LOOP -->
-            </tr>
-            <!-- /TMPL_IF -->
-            <!-- /TMPL_LOOP -->
+        <table>
+            [% FOREACH table_loo IN table_loop %]
+                [% IF ( table_loo.header_fields ) %]
+                    <tr>
+                        [% FOREACH header_field IN table_loo.header_fields %]
+                            <th>[% header_field.field_label %]</th>
+                        [% END %]
+                    </tr>
+                [% ELSE %]
+                    <tr>
+                        [% FOREACH text_field IN table_loo.text_fields %]
+                            [% IF ( text_field.select_field ) %]
+                                <td><input type="checkbox" name="action" value="[% text_field.field_value %]"></td>
+                            [% ELSE %]
+                                <td>[% text_field.field_value %]</td>
+                            [% END %]
+                        [% END %]
+                    </tr>
+                [% END %]
+            [% END %]
         </table>
 
 =cut
