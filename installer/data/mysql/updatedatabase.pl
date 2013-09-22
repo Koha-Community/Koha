107d3d8d268520c1095160b528497db7a59b6cb6
Bernardo Gonzalez Kriegel <bgkriegel@gmail.com>
2013-09-22 17:55:51 -0300

    Database update for 3.10.11 release
    Signed-off-by: Bernardo Gonzalez Kriegel <bgkriegel@gmail.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -6389,6 +6389,12 @@ if ( C4::Context->preference("Version") < TransformToNum($DBversion) ) {
     SetVersion($DBversion);
 }
 
+$DBversion = "3.10.11.000";
+if ( C4::Context->preference("Version") < TransformToNum($DBversion) ) {
+    print "Upgrade to $DBversion done (3.10.11 release)\n";
+    SetVersion($DBversion);
+}
+
 =head1 FUNCTIONS
 
 =head2 TableExists($table)
