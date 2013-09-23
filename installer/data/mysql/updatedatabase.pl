bfd7e9b8a8ade94678fd589d0128d524a59cbed6
Tomas Cohen Arazi <tomascohen@gmail.com>
2013-09-23 10:21:56 -0300

    Increment version for 3.12.5 release

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -6923,6 +6923,12 @@ if ( CheckVersion($DBversion) ) {
     SetVersion ($DBversion);
 }
 
+$DBversion = "3.12.05.000";
+if ( CheckVersion($DBversion) ) {
+    print "Upgrade to $DBversion done (3.12.5 release)\n";
+    SetVersion ($DBversion);
+}
+
 =head1 FUNCTIONS
 
 =head2 TableExists($table)
