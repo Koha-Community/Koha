50e25d212a0298910a5b4322b475df550985fbea
Kyle M Hall <kyle@bywatersolutions.com>
2013-09-26 12:22:39 -0400

    Bug 10954: ensure that payment receipt displays the payment made
    When printing a payment receipt, the payment did not display, only the
    total currently owed.
    
    Test Plan:
    1) Create a payment for a borrower
    2) Print the payment with the "print" button to the right of the payment
    3) Note the missing payment line
    4) Apply this patch
    5) Repeat step 2
    6) Note the payment line now displays
    
    Signed-off-by: cedric.vita@dracenie.com <cedric.vita@dracenie.com>
    Signed-off-by: Paul Poulain <paul.poulain@biblibre.com>
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>

--- members/printfeercpt.pl
+++ members/printfeercpt.pl
@@ -47,7 +47,7 @@ my ($template, $loggedinuser, $cookie)
 
 my $borrowernumber=$input->param('borrowernumber');
 my $action = $input->param('action') || '';
-my $accountno = $input->param('accountno');
+my $accountlines_id = $input->param('accountlines_id');
 
 #get borrower details
 my $data=GetMember('borrowernumber' => $borrowernumber);
@@ -73,7 +73,7 @@ my @accountrows; # this is for the tmpl-loop
 
 my $toggle;
 for (my $i=0;$i<$numaccts;$i++){
-   next if ($accts->[$i]{'accountno'} ne $accountno);
+    next if ( $accts->[$i]{'accountlines_id'} ne $accountlines_id );
     if($i%2){
             $toggle = 0;
     } else {
