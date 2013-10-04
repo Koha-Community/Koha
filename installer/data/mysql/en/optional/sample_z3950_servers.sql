5aae737e7e2fb3aa642ab46ad3d50730c6889282
Galen Charlton <gmc@esilibrary.com>
2013-10-04 14:59:29 +0000

    Bug 10096: (follow-up) add LC authority Z39.50 targets to 'en' sample data
    The LC NAF and SAF authority Z39.50 database are indeed production
    services, so we may as well showcase the new authority Z39.50 search
    functionality.
    
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>

--- installer/data/mysql/en/optional/sample_z3950_servers.sql
+++ installer/data/mysql/en/optional/sample_z3950_servers.sql
@@ -1,5 +1,7 @@
-INSERT INTO `z3950servers`
-(`host`, `port`, `db`, `userid`, `password`, `name`, `id`, `checked`, `rank`, `syntax`, `encoding`) VALUES 
-('lx2.loc.gov',210,'LCDB','','','LIBRARY OF CONGRESS',1,1,1,'USMARC','utf8'),
-('clio-db.cc.columbia.edu',7090,'voyager','','','COLUMBIA UNIVERSITY',6,0,0,'USMARC','MARC-8'),
-('siris-libraries.si.edu',210,'Default','','','SMITHSONIAN INSTITUTION LIBRARIES',10,0,0,'USMARC','MARC-8'); 
+INSERT INTO z3950servers
+(host, port, db, userid, password, name, checked, rank, syntax, encoding,recordtype) VALUES 
+('lx2.loc.gov',210,'LCDB','','','LIBRARY OF CONGRESS',1,1,'USMARC','utf8','biblio'),
+('lx2.loc.gov',210,'NAF','','','LIBRARY OF CONGRESS NAMES',1,1,'USMARC','utf8','authority'),
+('lx2.loc.gov',210,'SAF','','','LIBRARY OF CONGRESS SUBJECTS',1,2,'USMARC','utf8','authority'),
+('clio-db.cc.columbia.edu',7090,'voyager','','','COLUMBIA UNIVERSITY',0,0,'USMARC','MARC-8','biblio'),
+('siris-libraries.si.edu',210,'Default','','','SMITHSONIAN INSTITUTION LIBRARIES',0,0,'USMARC','MARC-8','biblio'); 
