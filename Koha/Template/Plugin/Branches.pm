dd374946850f17ad0707d03282c896c720313494
Kyle M Hall <kyle@bywatersolutions.com>
2013-10-08 14:25:01 -0300

    Bug 8215: TT plugins
    Backporting some template toolkit plugins that are actively being used for
    bugfixes on the stable branch.
    
    Signed-off-by: Tomas Cohen Arazi <tomascohen@gmail.com>

---
+++ Koha/Template/Plugin/Branches.pm
@@ -0,0 +1,38 @@
+package Koha::Template::Plugin::Branches;
+
+# Copyright ByWater Solutions 2012
+
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use Modern::Perl;
+
+use Template::Plugin;
+use base qw( Template::Plugin );
+use Encode qw{encode decode};
+
+use C4::Koha;
+
+sub GetName {
+    my ( $self, $branchcode ) = @_;
+
+    my $query = "SELECT branchname FROM branches WHERE branchcode = ?";
+    my $sth   = C4::Context->dbh->prepare($query);
+    $sth->execute($branchcode);
+    my $b = $sth->fetchrow_hashref();
+    return encode( 'UTF-8', $b->{'branchname'} );
+}
+
+1;
---
+++ Koha/Template/Plugin/ItemTypes.pm
@@ -0,0 +1,39 @@
+package Koha::Template::Plugin::ItemTypes;
+
+# Copyright ByWater Solutions 2012
+
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use Modern::Perl;
+
+use Template::Plugin;
+use base qw( Template::Plugin );
+use Encode qw{encode decode};
+
+use C4::Koha;
+
+sub GetDescription {
+    my ( $self, $itemtype ) = @_;
+
+    my $query = "SELECT description FROM itemtypes WHERE itemtype = ?";
+    my $sth   = C4::Context->dbh->prepare($query);
+    $sth->execute($itemtype);
+    my $d = $sth->fetchrow_hashref();
+    return encode( 'UTF-8', $d->{'description'} );
+
+}
+
+1;
