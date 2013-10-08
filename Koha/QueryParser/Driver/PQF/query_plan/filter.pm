aa8dc5f57601e10eabaac321522b1da1d368d2b3
Jonathan Druart <jonathan.druart@biblibre.com>
2013-10-08 09:32:34 +0200

    Bug 11015: add copyright headers to some files
    This patch adds a copyright headers where they didn't exist in the Koha
    namespace.
    
    Signed-off-by: Robin Sheat <robin@catalyst.net.nz>
    Signed-off-by: Brendan Gallagher <brendan@bywatersolutions.com>
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>
    (cherry picked from commit c3a55042e7cdbb241486acf3448afe70d9438550)
    Signed-off-by: Tomas Cohen Arazi <tomascohen@gmail.com>

--- Koha/QueryParser/Driver/PQF/query_plan/filter.pm
+++ Koha/QueryParser/Driver/PQF/query_plan/filter.pm
@@ -1,4 +1,22 @@
 package Koha::QueryParser::Driver::PQF::query_plan::filter;
+
+# This file is part of Koha.
+#
+# Copyright 2012 C & P Bibliography Services
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
 use base 'OpenILS::QueryParser::query_plan::filter';
 
 use strict;
