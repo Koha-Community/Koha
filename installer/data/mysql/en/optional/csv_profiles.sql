412995b52207b66b7e6994d433eca2038bee5686
Galen Charlton <gmc@esilibrary.com>
2013-10-11 03:52:59 +0000

    Bug 10854: (follow-up) language tweaks
    - adjust description of the default profile
    - adjust description of the sample data SQL
    - adjust description of the download link
    
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>

--- installer/data/mysql/en/optional/csv_profiles.sql
+++ installer/data/mysql/en/optional/csv_profiles.sql
@@ -1,1 +1,2 @@
-INSERT IGNORE INTO export_format( profile, description, content, csv_separator, type ) VALUES ( "issues to claim", "default csv export for issues claims", "SUPPLIER=aqbooksellers.name|TITLE=subscription.title|ISSUE NUMBER=serial.serialseq|LATE SINCE=serial.planneddate", ",", "sql" );
+INSERT IGNORE INTO export_format( profile, description, content, csv_separator, type ) 
+VALUES ( "issues to claim", "default CSV export for serial issue claims", "SUPPLIER=aqbooksellers.name|TITLE=subscription.title|ISSUE NUMBER=serial.serialseq|LATE SINCE=serial.planneddate", ",", "sql" );
