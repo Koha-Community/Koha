b6330d75c1e6937e09435a4e8accb1af2fa1cc6b
Galen Charlton <gmc@esilibrary.com>
2013-10-18 04:47:12 +0000

    Bug 10038: (follow-up) fix tabs
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>
    (cherry picked from commit ce9f504c7903d7431c8e18754f18ddf5bca2727a)
    Signed-off-by: Tomas Cohen Arazi <tomascohen@gmail.com>

--- admin/auth_tag_structure.pl
+++ admin/auth_tag_structure.pl
@@ -47,12 +47,12 @@ my $dbh = C4::Context->dbh;
 # open template
 my ($template, $loggedinuser, $cookie)
     = get_template_and_user({template_name => "admin/auth_tag_structure.tmpl",
-			     query => $input,
-			     type => "intranet",
-			     authnotrequired => 0,
+                 query => $input,
+                 type => "intranet",
+                 authnotrequired => 0,
                  flagsrequired => {parameters => 'parameters_remaining_permissions'},
-			     debug => 1,
-			     });
+                 debug => 1,
+                 });
 
 # get authtype list
 my $authtypes     = getauthtypes;
@@ -68,19 +68,19 @@ foreach my $thisauthtype ( sort keys %{$authtypes} ) {
 my $sth;
 # check that authtype framework is defined in auth_tag_structure if we are on a default action
 if (!$op or $op eq 'authtype_create_confirm') {
-	$sth=$dbh->prepare("select count(*) from auth_tag_structure where authtypecode=?");
-	$sth->execute($authtypecode);
-	my ($authtypeexist) = $sth->fetchrow;
-	if ($authtypeexist) {
-	} else {
-		# if authtype does not exists, then OP must be changed to "create authtype" if we are not on the way to create it
-		# (op = authtyp_create_confirm)
-		if ($op eq "authtype_create_confirm") {
-			duplicate_auth_framework($authtypecode, $existingauthtypecode);
-		} else {
-			$op = "authtype_create";
-		}
-	}
+    $sth=$dbh->prepare("select count(*) from auth_tag_structure where authtypecode=?");
+    $sth->execute($authtypecode);
+    my ($authtypeexist) = $sth->fetchrow;
+    if ($authtypeexist) {
+    } else {
+        # if authtype does not exists, then OP must be changed to "create authtype" if we are not on the way to create it
+        # (op = authtyp_create_confirm)
+        if ($op eq "authtype_create_confirm") {
+            duplicate_auth_framework($authtypecode, $existingauthtypecode);
+        } else {
+            $op = "authtype_create";
+        }
+    }
 }
 $template->param(script_name  => $script_name);
 $template->param(authtypeloop => \@authtypesloop);
@@ -93,47 +93,47 @@ if ($op && $op ne 'authtype_create_confirm') {
 ################## ADD_FORM ##################################
 # called by default. Used to create form to add or  modify a record
 if ($op eq 'add_form') {
-	#---- if primkey exists, it's a modify action, so read values to modify...
-	my $data;
-	if ($searchfield) {
-		$sth=$dbh->prepare("select tagfield,liblibrarian,libopac,repeatable,mandatory,authorised_value from auth_tag_structure where tagfield=? and authtypecode=?");
-		$sth->execute($searchfield,$authtypecode);
-		$data=$sth->fetchrow_hashref;
-	}
-	my $sth = $dbh->prepare("select distinct category from authorised_values");
-	$sth->execute;
-	my @authorised_values;
-	push @authorised_values,"";
-	while ((my $category) = $sth->fetchrow_array) {
-		push @authorised_values, $category;
-	}
-	my $authorised_value  = CGI::scrolling_list(-name=>'authorised_value',
-	        -id=>'authorised_value',
-			-values=> \@authorised_values,
-			-size=>1,
- 			-tabindex=>'',
-			-multiple=>0,
-			-default => $data->{'authorised_value'},
-			);
+    #---- if primkey exists, it's a modify action, so read values to modify...
+    my $data;
+    if ($searchfield) {
+        $sth=$dbh->prepare("select tagfield,liblibrarian,libopac,repeatable,mandatory,authorised_value from auth_tag_structure where tagfield=? and authtypecode=?");
+        $sth->execute($searchfield,$authtypecode);
+        $data=$sth->fetchrow_hashref;
+    }
+    my $sth = $dbh->prepare("select distinct category from authorised_values");
+    $sth->execute;
+    my @authorised_values;
+    push @authorised_values,"";
+    while ((my $category) = $sth->fetchrow_array) {
+        push @authorised_values, $category;
+    }
+    my $authorised_value  = CGI::scrolling_list(-name=>'authorised_value',
+            -id=>'authorised_value',
+            -values=> \@authorised_values,
+            -size=>1,
+            -tabindex=>'',
+            -multiple=>0,
+            -default => $data->{'authorised_value'},
+            );
 
-	if ($searchfield) {
-		$template->param(action => "Modify tag",
-								searchfield => "<input type=\"hidden\" name=\"tagfield\" value=\"$searchfield\" />$searchfield");
-		$template->param('heading_modify_tag_p' => 1);
-	} else {
-		$template->param(action => "Add tag",
-								searchfield => "<input type=\"text\" name=\"tagfield\" size=\"5\" maxlength=\"3\" />");
-		$template->param('heading_add_tag_p' => 1);
-	}
-	$template->param('use_heading_flags_p' => 1);
-	$template->param(liblibrarian => $data->{'liblibrarian'},
-							libopac => $data->{'libopac'},
-							repeatable => "".$data->{'repeatable'},
-							mandatory => "".$data->{'mandatory'},
-							authorised_value => $authorised_value,
-							authtypecode => $authtypecode,
-							);
-													# END $OP eq ADD_FORM
+    if ($searchfield) {
+        $template->param(action => "Modify tag",
+                                searchfield => "<input type=\"hidden\" name=\"tagfield\" value=\"$searchfield\" />$searchfield");
+        $template->param('heading_modify_tag_p' => 1);
+    } else {
+        $template->param(action => "Add tag",
+                                searchfield => "<input type=\"text\" name=\"tagfield\" size=\"5\" maxlength=\"3\" />");
+        $template->param('heading_add_tag_p' => 1);
+    }
+    $template->param('use_heading_flags_p' => 1);
+    $template->param(liblibrarian => $data->{'liblibrarian'},
+                            libopac => $data->{'libopac'},
+                            repeatable => "".$data->{'repeatable'},
+                            mandatory => "".$data->{'mandatory'},
+                            authorised_value => $authorised_value,
+                            authtypecode => $authtypecode,
+                            );
+                                                    # END $OP eq ADD_FORM
 ################## ADD_VALIDATE ##################################
 # called by add_form, used to insert/modify data in DB
 } elsif ($op eq 'add_validate') {
@@ -169,61 +169,61 @@ if ($op eq 'add_form') {
            );
         }
     }
-	print "Content-Type: text/html\n\n<META HTTP-EQUIV=Refresh CONTENT=\"0; URL=auth_tag_structure.pl?searchfield=".$input->param('tagfield')."&authtypecode=$authtypecode\">";
-	exit;
-													# END $OP eq ADD_VALIDATE
+    print "Content-Type: text/html\n\n<META HTTP-EQUIV=Refresh CONTENT=\"0; URL=auth_tag_structure.pl?searchfield=".$input->param('tagfield')."&authtypecode=$authtypecode\">";
+    exit;
+                                                    # END $OP eq ADD_VALIDATE
 ################## DELETE_CONFIRM ##################################
 # called by default form, used to confirm deletion of data in DB
 } elsif ($op eq 'delete_confirm') {
-	$sth=$dbh->prepare("select tagfield,liblibrarian,libopac,repeatable,mandatory,authorised_value from auth_tag_structure where tagfield=?");
-	$sth->execute($searchfield);
-	my $data=$sth->fetchrow_hashref;
-	$template->param(liblibrarian => $data->{'liblibrarian'},
-							searchfield => $searchfield,
-							authtypecode => $authtypecode,
-							);
-													# END $OP eq DELETE_CONFIRM
+    $sth=$dbh->prepare("select tagfield,liblibrarian,libopac,repeatable,mandatory,authorised_value from auth_tag_structure where tagfield=?");
+    $sth->execute($searchfield);
+    my $data=$sth->fetchrow_hashref;
+    $template->param(liblibrarian => $data->{'liblibrarian'},
+                            searchfield => $searchfield,
+                            authtypecode => $authtypecode,
+                            );
+                                                    # END $OP eq DELETE_CONFIRM
 ################## DELETE_CONFIRMED ##################################
 # called by delete_confirm, used to effectively confirm deletion of data in DB
 } elsif ($op eq 'delete_confirmed') {
-	unless (C4::Context->config('demo') eq 1) {
-		my $sth = $dbh->prepare("delete from auth_tag_structure where tagfield=? and authtypecode=?");
-		$sth->execute($searchfield,$authtypecode);
-		my $sth = $dbh->prepare("delete from auth_subfield_structure where tagfield=? and authtypecode=?");
-		$sth->execute($searchfield,$authtypecode);
-	}
+    unless (C4::Context->config('demo') eq 1) {
+        my $sth = $dbh->prepare("delete from auth_tag_structure where tagfield=? and authtypecode=?");
+        $sth->execute($searchfield,$authtypecode);
+        my $sth = $dbh->prepare("delete from auth_subfield_structure where tagfield=? and authtypecode=?");
+        $sth->execute($searchfield,$authtypecode);
+    }
     print "Content-Type: text/html\n\n<META HTTP-EQUIV=Refresh CONTENT=\"0; URL=auth_tag_structure.pl?searchfield=".$input->param('tagfield')."&authtypecode=$authtypecode\">";
     exit;
-													# END $OP eq DELETE_CONFIRMED
+                                                    # END $OP eq DELETE_CONFIRMED
 ################## ITEMTYPE_CREATE ##################################
 # called automatically if an unexisting authtypecode is selected
 } elsif ($op eq 'authtype_create') {
-	$sth = $dbh->prepare("select count(*),auth_tag_structure.authtypecode,authtypetext from auth_tag_structure,auth_types where auth_types.authtypecode=auth_tag_structure.authtypecode group by auth_tag_structure.authtypecode");
-	$sth->execute;
-	my @existingauthtypeloop;
-	while (my ($tot,$thisauthtype,$authtypetext) = $sth->fetchrow) {
-		if ($tot>0) {
-			my %line = ( value => $thisauthtype,
-						authtypetext => $authtypetext,
-					);
-			push @existingauthtypeloop,\%line;
-		}
-	}
+    $sth = $dbh->prepare("select count(*),auth_tag_structure.authtypecode,authtypetext from auth_tag_structure,auth_types where auth_types.authtypecode=auth_tag_structure.authtypecode group by auth_tag_structure.authtypecode");
+    $sth->execute;
+    my @existingauthtypeloop;
+    while (my ($tot,$thisauthtype,$authtypetext) = $sth->fetchrow) {
+        if ($tot>0) {
+            my %line = ( value => $thisauthtype,
+                        authtypetext => $authtypetext,
+                    );
+            push @existingauthtypeloop,\%line;
+        }
+    }
     @existingauthtypeloop = sort { lc($a->{authtypetext}) cmp lc($b->{authtypetext}) }@existingauthtypeloop;
-	$template->param(existingauthtypeloop => \@existingauthtypeloop,
-					authtypecode => $authtypecode,
-					);
+    $template->param(existingauthtypeloop => \@existingauthtypeloop,
+                    authtypecode => $authtypecode,
+                    );
 ################## DEFAULT ##################################
 } else { # DEFAULT
-	# here, $op can be unset or set to "authtype_create_confirm".
-#	warn "authtype : $authtypecode";
-	if  ($searchfield ne '') {
-		 $template->param(searchfield => $searchfield);
-	}
-	my ($count,$results)=StringSearch($searchfield,$authtypecode);
-	my @loop_data = ();
+    # here, $op can be unset or set to "authtype_create_confirm".
+#   warn "authtype : $authtypecode";
+    if  ($searchfield ne '') {
+         $template->param(searchfield => $searchfield);
+    }
+    my ($count,$results)=StringSearch($searchfield,$authtypecode);
+    my @loop_data = ();
     for ( my $i = $offset ; $i < $count ; $i++ ) {
-		my %row_data;  # get a fresh hash for the row data
+        my %row_data;  # get a fresh hash for the row data
         $row_data{tagfield}         = $results->[$i]{'tagfield'};
         $row_data{liblibrarian}     = $results->[$i]{'liblibrarian'};
         $row_data{repeatable}       = $results->[$i]{'repeatable'};
@@ -232,21 +232,21 @@ if ($op eq 'add_form') {
         $row_data{subfield_link}    = "auth_subfields_structure.pl?tagfield=" . $results->[$i]{'tagfield'} . "&amp;authtypecode=" . $authtypecode;
         $row_data{edit}             = "$script_name?op=add_form&amp;searchfield=" . $results->[$i]{'tagfield'} . "&amp;authtypecode=" . $authtypecode;
         $row_data{delete}           = "$script_name?op=delete_confirm&amp;searchfield=" . $results->[$i]{'tagfield'} . "&amp;authtypecode=" . $authtypecode;
-		push(@loop_data, \%row_data);
-	}
-	$template->param(loop => \@loop_data,
-					authtypecode => $authtypecode,
-	);
-	if ($offset>0) {
-		$template->param(isprevpage => $offset,
-						searchfield => $searchfield,
-		 );
-	}
+        push(@loop_data, \%row_data);
+    }
+    $template->param(loop => \@loop_data,
+                    authtypecode => $authtypecode,
+    );
+    if ($offset>0) {
+        $template->param(isprevpage => $offset,
+                        searchfield => $searchfield,
+         );
+    }
     if ( $offset < $count ) {
-		$template->param(
-						searchfield => $searchfield,
-		);
-	}
+        $template->param(
+                        searchfield => $searchfield,
+        );
+    }
 } #---- END $OP eq DEFAULT
 
 output_html_with_http_headers $input, $cookie, $template->output;
@@ -255,37 +255,37 @@ output_html_with_http_headers $input, $cookie, $template->output;
 # the sub used for searches
 #
 sub StringSearch  {
-	my ($searchstring,$authtypecode)=@_;
-	my $dbh = C4::Context->dbh;
-	$searchstring=~ s/\'/\\\'/g;
-	my @data=split(' ',$searchstring);
-	my $sth=$dbh->prepare("Select tagfield,liblibrarian,libopac,repeatable,mandatory,authorised_value from auth_tag_structure where (tagfield >= ? and authtypecode=?) order by tagfield");
-	$sth->execute($data[0], $authtypecode);
-	my @results;
-	while (my $data=$sth->fetchrow_hashref){
+    my ($searchstring,$authtypecode)=@_;
+    my $dbh = C4::Context->dbh;
+    $searchstring=~ s/\'/\\\'/g;
+    my @data=split(' ',$searchstring);
+    my $sth=$dbh->prepare("Select tagfield,liblibrarian,libopac,repeatable,mandatory,authorised_value from auth_tag_structure where (tagfield >= ? and authtypecode=?) order by tagfield");
+    $sth->execute($data[0], $authtypecode);
+    my @results;
+    while (my $data=$sth->fetchrow_hashref){
         push(@results,$data);
-	}
-	return (scalar(@results),\@results);
+    }
+    return (scalar(@results),\@results);
 }
 
 #
 # the sub used to duplicate a framework from an existing one in MARC parameters tables.
 #
 sub duplicate_auth_framework {
-	my ($newauthtype,$oldauthtype) = @_;
-#	warn "TO $newauthtype FROM $oldauthtype";
-	my $sth = $dbh->prepare("select tagfield,liblibrarian,libopac,repeatable,mandatory,authorised_value from auth_tag_structure where authtypecode=?");
-	$sth->execute($oldauthtype);
-	my $sth_insert = $dbh->prepare("insert into auth_tag_structure  (tagfield, liblibrarian, libopac, repeatable, mandatory, authorised_value, authtypecode) values (?,?,?,?,?,?,?)");
-	while ( my ($tagfield,$liblibrarian,$libopac,$repeatable,$mandatory,$authorised_value) = $sth->fetchrow) {
-		$sth_insert->execute($tagfield,$liblibrarian,$libopac,$repeatable,$mandatory,$authorised_value,$newauthtype);
-	}
+    my ($newauthtype,$oldauthtype) = @_;
+#   warn "TO $newauthtype FROM $oldauthtype";
+    my $sth = $dbh->prepare("select tagfield,liblibrarian,libopac,repeatable,mandatory,authorised_value from auth_tag_structure where authtypecode=?");
+    $sth->execute($oldauthtype);
+    my $sth_insert = $dbh->prepare("insert into auth_tag_structure  (tagfield, liblibrarian, libopac, repeatable, mandatory, authorised_value, authtypecode) values (?,?,?,?,?,?,?)");
+    while ( my ($tagfield,$liblibrarian,$libopac,$repeatable,$mandatory,$authorised_value) = $sth->fetchrow) {
+        $sth_insert->execute($tagfield,$liblibrarian,$libopac,$repeatable,$mandatory,$authorised_value,$newauthtype);
+    }
 
-	$sth = $dbh->prepare("select tagfield,tagsubfield,liblibrarian,libopac,repeatable,mandatory,kohafield,tab,authorised_value,value_builder,seealso,hidden from auth_subfield_structure where authtypecode=?");
-	$sth->execute($oldauthtype);
-	$sth_insert = $dbh->prepare("insert into auth_subfield_structure (authtypecode,tagfield,tagsubfield,liblibrarian,libopac,repeatable,mandatory,kohafield,tab,authorised_value,value_builder,seealso,hidden) values (?,?,?,?,?,?,?,?,?,?,?,?,?)");
-	while ( my ( $tagfield, $tagsubfield, $liblibrarian, $libopac, $repeatable, $mandatory, $kohafield,$tab, $authorised_value, $thesaurus_category, $seealso,$hidden) = $sth->fetchrow) {
-		$sth_insert->execute($newauthtype, $tagfield, $tagsubfield, $liblibrarian, $libopac, $repeatable, $mandatory,$kohafield, $tab, $authorised_value, $thesaurus_category, $seealso,$hidden);
-	}
+    $sth = $dbh->prepare("select tagfield,tagsubfield,liblibrarian,libopac,repeatable,mandatory,kohafield,tab,authorised_value,value_builder,seealso,hidden from auth_subfield_structure where authtypecode=?");
+    $sth->execute($oldauthtype);
+    $sth_insert = $dbh->prepare("insert into auth_subfield_structure (authtypecode,tagfield,tagsubfield,liblibrarian,libopac,repeatable,mandatory,kohafield,tab,authorised_value,value_builder,seealso,hidden) values (?,?,?,?,?,?,?,?,?,?,?,?,?)");
+    while ( my ( $tagfield, $tagsubfield, $liblibrarian, $libopac, $repeatable, $mandatory, $kohafield,$tab, $authorised_value, $thesaurus_category, $seealso,$hidden) = $sth->fetchrow) {
+        $sth_insert->execute($newauthtype, $tagfield, $tagsubfield, $liblibrarian, $libopac, $repeatable, $mandatory,$kohafield, $tab, $authorised_value, $thesaurus_category, $seealso,$hidden);
+    }
 }
 
