38edd714c598322e81a32a0dfc5f2ce0da46826d
Jonathan Druart <jonathan.druart@biblibre.com>
2013-11-04 11:55:57 +0100

    Bug 9788: QA followup
    1/ CURRENT_DATE() is a MySQLism and should be replaced with CAST(now() AS
    DATE).
    2/ The date formatting should be done in the template (using the TT
    plugin).
    
    Signed-off-by: Jonathan Druart <jonathan.druart@biblibre.com>
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>

--- catalogue/detail.pl
+++ catalogue/detail.pl
@@ -249,7 +249,7 @@ foreach my $item (@items) {
         $item->{ExpectedAtLibrary}      = $branches->{$expectedAt}{branchname};
         $item->{Reservedcardnumber}             = $ItemBorrowerReserveInfo->{'cardnumber'};
         # Check waiting status
-        $item->{waitingdate} = format_date($wait) if $wait;
+        $item->{waitingdate} = $wait;
     }
 
 
