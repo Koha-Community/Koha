cac06afeb1f03200cfc7ab48162c184be8d1526b
Fridolyn SOMERS <fridolyn.somers@biblibre.com>
2013-11-15 10:43:21 +0100

    Bug 11254: make reservoir search normalize ISBNs
    When importing records, the ISBN is normalized and stored
    into database (see C4::ImportBatch::_add_biblio_fields).  But when
    searching with ISBN into reservoir, it is not normalized
    (see C4::Breeding::BreedingSearch).  So search does not match.
    
    This patch adds the normalisation to reservoir search.  Also, it
    replaces call private method _isbn_cleanup by GetNormalizedISBN,
    the correct public method.  Also allows the reservoir search
    on ISBN with hyphens.
    
    This is intended to fix only reservoir searches.
    
    Revised Test plan
    -----------------
     1) Back up DB
     2) Save copy of attached example somewhere findable
     2) Home -> Tools -> Stage MARC records for import
     3) Click Browse and select the example MARC file
     4) Click Upload file
     5) Tweak as desired then click Stage for import
     6) Click Manage staged records
     7) Click Import this batch into the catalog
     8) Home -> Cataloging
     9) In the Cataloging search text box type 978-0-691-14289-0 and
         click Submit
        -- ISBN13 with hypens not found in reservoir
    10) In the Cataloging search text box type 9780691142890 and
         click Submit
        -- ISBN13 without hypens not found in reservoir
    11) In the Cataloging search text box type 0-691-14289-0 and
         click Submit
        -- ISBN10 with hypens not found in reservoir
    12) In the Cataloging search text box type 0691142890 and
         click Submit
        -- ISBN10 without hypens found in reservoir
    13) Apply patch
    14) Repeat steps 9-12, this time it is always found! :)
    
    Signed-off-by: Mark Tompsett <mtompset@hotmail.com>
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>

--- C4/Breeding.pm
+++ C4/Breeding.pm
@@ -110,7 +110,7 @@ sub ImportBreeding {
             # if isbn found and biblio does not exist, add it. If isbn found and biblio exists, 
             # overwrite or ignore depending on user choice
             # drop every "special" char : spaces, - ...
-            $oldbiblio->{isbn} = C4::Koha::_isbn_cleanup($oldbiblio->{isbn}); # FIXME C4::Koha::_isbn_cleanup should be public
+            $oldbiblio->{isbn} = C4::Koha::GetNormalizedISBN($oldbiblio->{isbn});
             # search if biblio exists
             my $biblioitemnumber;
             if ($oldbiblio->{isbn}) {
@@ -174,6 +174,9 @@ sub BreedingSearch {
     my ($query,@bind);
     my $sth;
     my @results;
+
+    # normalise ISBN like at import
+    $isbn = C4::Koha::GetNormalizedISBN($isbn);
 
     $query = "SELECT import_record_id, file_name, isbn, title, author
               FROM  import_biblios 
