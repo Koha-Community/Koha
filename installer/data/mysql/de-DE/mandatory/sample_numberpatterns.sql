fff04e59e785ce26f52da94730818c541476f7bb
Katrin Fischer <Katrin.Fischer.83@web.de>
2013-11-17 22:56:45 +0100

    Bug 11260: Update German web installer - frequencies and numberpatterns
    Translates the new files for frequencies and numberpatterns
    
    To test:
    - Run the German web installer with all sample files
    - Check all files import without errors, especially frequencies and
      numbering patterns
    - Check frequencies in serials
    - Check numbering patterns in serials
    - Create a subscription, check possible choices
    
    Signed-off-by: Chris Cormack <chrisc@catalyst.net.nz>
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>

--- installer/data/mysql/de-DE/mandatory/sample_numberpatterns.sql
+++ installer/data/mysql/de-DE/mandatory/sample_numberpatterns.sql
@@ -4,22 +4,22 @@ INSERT INTO subscription_numberpatterns
     label2, add2, every2, whenmorethan2, setto2, numbering2,
     label3, add3, every3, whenmorethan3, setto3, numbering3)
 VALUES
-    ('Number', 1, 'Simple Numbering method', 'No.{X}',
-    'Number', 1, 1, 99999, 1, NULL,
+    ('Nummer', 1, 'Einfache Nummerierung', 'Nr. {X}',
+    'Nummer', 1, 1, 99999, 1, NULL,
     NULL, NULL, NULL, NULL, NULL, NULL,
     NULL, NULL, NULL, NULL, NULL, NULL),
 
-    ('Volume, Number, Issue', 2, 'Volume Number Issue 1', 'Vol.{X}, Number {Y}, Issue {Z}',
-    'Volume', 1, 48, 99999, 1, NULL,
-    'Number', 1, 4, 12, 1, NULL,
-    'Issue', 1, 1, 4, 1, NULL),
+    ('Band, Nummer, Heft', 2, 'Band X, Nummer Y, Heft Z', 'Bd. {X}, Nr. {Y}, H. {Z}',
+    'Band', 1, 48, 99999, 1, NULL,
+    'Nummer', 1, 4, 12, 1, NULL,
+    'Heft', 1, 1, 4, 1, NULL),
 
-    ('Volume, Number', 3, 'Volume Number 1', 'Vol {X}, No {Y}',
-    'Volume', 1, 12, 99999, 1, NULL,
-    'Number', 1, 1, 12, 1, NULL,
+    ('Band, Nummer', 3, 'Bd. X, Nr. Y', 'Bd. {X}, Nr. {Y}',
+    'Band', 1, 12, 99999, 1, NULL,
+    'Nummer', 1, 1, 12, 1, NULL,
     NULL, NULL, NULL, NULL, NULL, NULL),
 
-    ('Seasonal', 4, 'Season Year', '{X} {Y}',
-    'Season', 1, 1, 3, 0, 'season',
-    'Year', 1, 4, 99999, 1, NULL,
+    ('Saisonal', 4, 'Saison Jahr', '{X} {Y}',
+    'Saison', 1, 1, 3, 0, 'season',
+    'Jahr', 1, 4, 99999, 1, NULL,
     NULL, NULL, NULL, NULL, NULL, NULL);
