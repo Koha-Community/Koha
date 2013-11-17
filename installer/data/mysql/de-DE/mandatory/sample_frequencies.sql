4ab0acc67c2eadf256958b2f3584007a062d2efb
Katrin Fischer <Katrin.Fischer.83@web.de>
2013-11-17 22:56:45 +0100

    Bug 11260: Update German web installer - frequencies and numberpatterns
    Translates the new files for frequencies and numberpatterns
    
    To test:
    - Run the German web installer with all sample files
    - Check all files import without errors, especially frequencies and
      numbering patterns
    - Check frequencies in serials
    - Check numbering patterns in serials
    - Create a subscription, check possible choices
    
    Signed-off-by: Chris Cormack <chrisc@catalyst.net.nz>
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>
    (cherry picked from commit fff04e59e785ce26f52da94730818c541476f7bb)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- installer/data/mysql/de-DE/mandatory/sample_frequencies.sql
+++ installer/data/mysql/de-DE/mandatory/sample_frequencies.sql
@@ -1,16 +1,16 @@
 INSERT INTO subscription_frequencies
     (description, unit, unitsperissue, issuesperunit, displayorder)
 VALUES
-    ('2/day', 'day', 1, 2, 1),
-    ('1/day', 'day', 1, 1, 2),
-    ('3/week', 'week', 1, 3, 3),
-    ('1/week', 'week', 1, 1, 4),
-    ('1/2 weeks', 'week', 2, 1, 5),
-    ('1/3 weeks', 'week', 3, 1, 6),
-    ('1/month', 'month', 1, 1, 7),
-    ('1/2 months', 'month', 2, 1, 8),
-    ('1/3 months', 'month', 3, 1, 9),
-    ('2/year', 'month', 6, 1, 10),
-    ('1/year', 'year', 1, 1, 11),
-    ('1/2 year', 'year', 2, 1, 12),
-    ('Irregular', NULL, 1, 1, 13);
+    ('2/Tag', 'day', 1, 2, 1),
+    ('1/Tag', 'day', 1, 1, 2),
+    ('3/Woche', 'week', 1, 3, 3),
+    ('1/Woche', 'week', 1, 1, 4),
+    ('1/2 Wochen', 'week', 2, 1, 5),
+    ('1/3 Wochen', 'week', 3, 1, 6),
+    ('1/Monat', 'month', 1, 1, 7),
+    ('1/2 Monate', 'month', 2, 1, 8),
+    ('1/3 Monate', 'month', 3, 1, 9),
+    ('2/Jahr', 'month', 6, 1, 10),
+    ('1/Jahr', 'year', 1, 1, 11),
+    ('1/2 Jahre', 'year', 2, 1, 12),
+    ('Unregelmäßig', NULL, 1, 1, 13);
