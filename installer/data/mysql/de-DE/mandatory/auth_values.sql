5c8674a45907bc07a90875f17ccac24bf3bc2895
Katrin Fischer <Katrin.Fischer.83@web.de>
2013-11-17 21:05:45 +0100

    Bug 11260: Update German web installer for 3.14
    Mandatory:
    - adds auth_values.sql and .txt with YES_NO referenced in patron_attributes.sql
    - updates sample notices with redone slip templates (ISSUESLIP, ISSUEQSLIP,
      RESERVSLIP, TRANSFERSLIP)
    - adds German translations to subtag_registry.sql and copies updated file
      from en to de-DE
    - adds translation for new course_reserves permission to userflags.sql
    - adds translations for new permissions in userpermissions.sql
    - Fixes typo IndependantBranches in English userpermissions.sql
    
    Optional:
    - adds csv_profiles.sql and translation of the serial claims profile
    - adds z39.50 servers for authorities to sample_z3950_servers.sql
    
    To test:
    - Run web installer in English
      - check all sample files, make sure they all install without SQL errors
      - check language list on advanced search displays correctly still
      - check area/language list on tools > news displays correctly still
      - check permissions for correct spelling of IndependentBranches
    
    - Run web installer in German
      - repeat checks from above
      - also check: authorised values, especially existence of YES_NO
      - all permissions are translated now
      - slip notices are all there and look right
      - new z39.50 servers for authorities are available
      - csv profile for serial claims is available and works as expected
    
    ... and anything else you can think of.
    
    Signed-off-by: Chris Cormack <chrisc@catalyst.net.nz>
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>
    (cherry picked from commit bbe64974b02d78fc916f76408a995ac1bf7f6228)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

---
+++ installer/data/mysql/de-DE/mandatory/auth_values.sql
@@ -0,0 +1,2 @@
+INSERT INTO authorised_values (category,authorised_value,lib,lib_opac) VALUES ('YES_NO','0','Nein','Nein');
+INSERT INTO authorised_values (category,authorised_value,lib,lib_opac) VALUES ('YES_NO','1','Ja','Ja');
--- installer/data/mysql/de-DE/mandatory/subtag_registry.sql
+++ installer/data/mysql/de-DE/mandatory/subtag_registry.sql
@@ -25,6 +25,9 @@ VALUES( 'opac', 'i', 'en', 'OPAC');
 INSERT INTO language_descriptions(subtag, type, lang, description)
 VALUES( 'opac', 'i', 'fr', 'OPAC');
 
+INSERT INTO language_descriptions(subtag, type, lang, description)
+VALUES( 'opac', 'i', 'de', 'OPAC');
+
 -- Staff Client
 INSERT INTO language_subtag_registry( subtag, type, description, added)
 VALUES ( 'intranet', 'i', 'Staff Client','2005-10-16');
@@ -35,6 +38,9 @@ VALUES( 'intranet', 'i', 'en', 'Staff Client');
 INSERT INTO language_descriptions(subtag, type, lang, description)
 VALUES( 'intranet', 'i', 'fr', '????');
 
+INSERT INTO language_descriptions(subtag, type, lang, description)
+VALUES( 'intranet', 'i', 'de', 'Dienstoberfläche');
+
 -- Theme (t)
 INSERT INTO language_subtag_registry( subtag, type, description, added)
 VALUES ( 'prog', 't', 'Prog','2005-10-16');
@@ -44,6 +50,9 @@ VALUES( 'prog', 't', 'en', 'Prog');
 
 INSERT INTO language_descriptions(subtag, type, lang, description)
 VALUES( 'prog', 't', 'fr', 'Prog');
+
+INSERT INTO language_descriptions(subtag, type, lang, description)
+VALUES( 'prog', 't', 'de', 'Prog');
 
 -- LANGUAGES
 
@@ -283,7 +292,7 @@ INSERT INTO language_descriptions(subtag, type, lang, description)
 VALUES( 'el', 'language', 'fr', 'Grec Moderne (Après 1453)');
 
 INSERT INTO language_descriptions(subtag, type, lang, description)
-VALUES( 'el', 'language', 'de', 'Griechisch (Nach 1453)');
+VALUES( 'el', 'language', 'de', 'Griechisch (Moern [1453- ]');
 
 -- Hebrew
 INSERT INTO language_subtag_registry( subtag, type, description, added)
@@ -453,6 +462,10 @@ VALUES( 'gl', 'language', 'en', 'Galician');
 
 INSERT INTO language_descriptions(subtag, type, lang, description)
 VALUES( 'gl', 'language', 'fr', 'Galicien');
+
+INSERT INTO language_descriptions(subtag, type, lang, description)
+VALUES( 'gl', 'language', 'de', 'Galicisch');
+
 
 -- Norwegian (bokmål)
 INSERT INTO language_subtag_registry( subtag, type, description, added)
@@ -781,6 +794,9 @@ VALUES( 'Arab', 'script','en', 'Arabic');
 INSERT INTO language_descriptions(subtag, type, lang, description)
 VALUES( 'Arab', 'script','fr', 'Arabic');
 
+INSERT INTO language_descriptions(subtag, type, lang, description)
+VALUES( 'Arab', 'script', 'de', 'Arabisch');
+
 -- Cyrillic
 INSERT INTO language_subtag_registry( subtag, type, description, added)
 VALUES ( 'Cyrl', 'script', 'Cyrillic','2005-10-16');
@@ -793,6 +809,10 @@ VALUES( 'Cyrl', 'script', 'en', 'Cyrillic');
 
 INSERT INTO language_descriptions(subtag, type, lang, description)
 VALUES( 'Cyrl', 'script', 'fr', 'Cyrillic');
+
+INSERT INTO language_descriptions(subtag, type, lang, description)
+VALUES( 'Cyrl', 'script', 'de', 'Kyrillisch');
+
 
 -- Greek
 INSERT INTO language_subtag_registry( subtag, type, description, added)
@@ -807,6 +827,10 @@ VALUES( 'Grek', 'script', 'en', 'Greek');
 INSERT INTO language_descriptions(subtag, type, lang, description)
 VALUES( 'Grek', 'script', 'fr', 'Greek');
 
+INSERT INTO language_descriptions(subtag, type, lang, description)
+VALUES( 'Grek', 'script', 'de', 'Griechisch');
+
+
 -- Han - Simplified
 INSERT INTO language_subtag_registry( subtag, type, description, added)
 VALUES ( 'Hans', 'script', 'Han (Simplified variant)','2005-10-16');
@@ -820,6 +844,10 @@ VALUES( 'Hans', 'script', 'en', 'Han (Simplified variant)');
 INSERT INTO language_descriptions(subtag, type, lang, description)
 VALUES( 'Hans', 'script', 'fr', 'Han (Simplified variant)');
 
+INSERT INTO language_descriptions(subtag, type, lang, description)
+VALUES( 'Hans', 'script', 'de', 'Han (Vereinfachte Variante)');
+
+
 -- Han - Traditional
 INSERT INTO language_subtag_registry( subtag, type, description, added)
 VALUES ( 'Hant', 'script', 'Han (Traditional variant)','2005-10-16');
@@ -829,6 +857,9 @@ VALUES ( 'Hant', 'script', 'Hant', 'Han (Traditional variant)');
 
 INSERT INTO language_descriptions(subtag, type, lang, description)
 VALUES( 'Hant', 'script', 'en', 'Han (Traditional variant)');
+
+INSERT INTO language_descriptions(subtag, type, lang, description)
+VALUES( 'Hant', 'script', 'de', 'Han (Traditionelle Variante)');
 
 -- Hebrew
 INSERT INTO language_subtag_registry( subtag, type, description, added)
@@ -840,6 +871,9 @@ VALUES ( 'Hebr', 'script', 'Hebr', 'Hebrew');
 INSERT INTO language_descriptions(subtag, type, lang, description)
 VALUES( 'Hebr', 'script', 'en', 'Hebrew');
 
+INSERT INTO language_descriptions(subtag, type, lang, description)
+VALUES( 'Hebr', 'script', 'de', 'Hebräisch');
+
 -- Lao
 INSERT INTO language_subtag_registry( subtag, type, description, added)
 VALUES ( 'Laoo', 'script', 'Lao','2005-10-16');
@@ -849,6 +883,10 @@ VALUES ( 'Laoo', 'script', 'lo', 'Lao');
 
 INSERT INTO language_descriptions(subtag, type, lang, description)
 VALUES( 'Laoo', 'script', 'en', 'Lao');
+
+INSERT INTO language_descriptions(subtag, type, lang, description)
+VALUES( 'Laoo', 'script', 'en', 'Laotisch');
+
 
 -- REGIONS
 -- Canada
