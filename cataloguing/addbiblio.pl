335b3059d0d537e2d74250eeb096f8ee71d25c87
Mark Tompsett <mtompset@hotmail.com>
2013-11-27 13:16:56 -0500

    Bug 11316 - plugin icon missing tooltip in addbiblio.pl
    By adding:
        title=\"Tag editor\"
    into the HTML generated in addbiblio, a tooltip displaying
    'Tag editor' appears when the mouse is hovered over the
    plugin icon.
    
    Test Plan:
    1) Log into Staff client
    2) Click the 'Search the catalog' tab
    3) Type something which will generate results
    4) Click the title of a result
    5) Click the 'Edit' button
    6) Scroll down and click the 'Edit Record'
    7) Look through the various tabs and notice that all the plugin
       icons have no tooltip.
    8) Apply the patch
    9) Refresh the page
    10) All the plugin icons should have the tooltip text of
        'Tag editor'.
    
    Signed-off-by: Christopher Brannon <cbrannon@cdalibrary.org>
    Signed-off-by: Katrin Fischer <Katrin.Fischer.83@web.de>
    Adds a title attribute. Works as described.
    
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>
    (cherry picked from commit e45edca42a44d094eabbd3347cc83b43cd2166f4)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- cataloguing/addbiblio.pl
+++ cataloguing/addbiblio.pl
@@ -392,7 +392,7 @@ sub create_input {
                     ($is_readonly ? "readonly=\"readonly\"" : "").
                     "\/>
                     <span class=\"subfield_controls\"><a href=\"#\" class=\"buttonDot tag_editor\"
-                       onclick=\"openAuth(this.parentNode.parentNode.getElementsByTagName('input')[1].id,'".$tagslib->{$tag}->{$subfield}->{authtypecode}."','biblio'); return false;\" tabindex=\"1\">Tag editor</a></span>
+                       onclick=\"openAuth(this.parentNode.parentNode.getElementsByTagName('input')[1].id,'".$tagslib->{$tag}->{$subfield}->{authtypecode}."','biblio'); return false;\" tabindex=\"1\" title=\"Tag editor\">Tag editor</a></span>
             ";
     # it's a plugin field
     }
@@ -421,7 +421,7 @@ sub create_input {
                             size=\"67\"
                             maxlength=\"".$subfield_data{maxlength}."\"
                             onblur=\"Blur$function_name($index_tag); \" \/>
-                            <span class=\"subfield_controls\"><a href=\"#\" class=\"buttonDot tag_editor\" onclick=\"Clic$function_name('$subfield_data{id}'); return false;\" tabindex=\"1\">Tag editor</a></span>
+                            <span class=\"subfield_controls\"><a href=\"#\" class=\"buttonDot tag_editor\" onclick=\"Clic$function_name('$subfield_data{id}'); return false;\" tabindex=\"1\" title=\"Tag editor\">Tag editor</a></span>
                     $javascript";
         } else {
             warn "Plugin Failed: $plugin";
