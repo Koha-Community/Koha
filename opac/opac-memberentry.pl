9a5f737dcbb1c695130a3acafd46a63a44f2e4cf
Kyle M Hall <kyle@bywatersolutions.com>
2013-12-05 09:13:35 -0500

    Bug 11342: fix error in OPAC self registration form if BorrowersTitles is empty
    If BorrowersTitles is empty, it causes the library pulldown on the self
    registration page to be empty, and to have the "Saluation" field have
    the option "branches".
    
    This patch also fixes a minor string capitalization issue.
    
    Test Plan:
    1) Enable OPAC self registration
    2) Set the system preference BorrowersTitles to be empty
    3) View the self registration page
    4) Note the lack of branches in the home library pulldown
    5) Apply this patch
    6) Note the branches now display in the pulldown
    
    Signed-off-by: Christopher Brannon <cbrannon@cdalibrary.org>
    Signed-off-by: Katrin Fischer <Katrin.Fischer.83@web.de>
    Tested in bootstrap and prog OPAC, with BorrowersTitle configured
    and emptied.
    Passes all tests and QA script.
    
    Note: The titles pull down has 2 empty entries in master with
    and without the patch.
    
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>

--- opac/opac-memberentry.pl
+++ opac/opac-memberentry.pl
@@ -59,7 +59,7 @@ $template->param(
     action            => $action,
     hidden            => GetHiddenFields(),
     mandatory         => GetMandatoryFields($action),
-    member_titles     => GetTitles(),
+    member_titles     => GetTitles() || undef,
     branches          => GetBranchesLoop(),
     OPACPatronDetails => C4::Context->preference('OPACPatronDetails'),
 );
