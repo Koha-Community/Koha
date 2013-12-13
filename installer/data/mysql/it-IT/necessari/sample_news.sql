82a36947f72c542dae4edd967291e7ffe7d8b037
Mark Tompsett <mtompset@hotmail.com>
2013-12-13 15:51:36 -0500

    Bug 7567 - Clean up sample news files
    Some files had truncate. Some files had foreign key checks.
    Other files had foreign key checks disabled. Some files gave
    the column names expressly, and others assumed the fields in
    a particular order. Everything was modified to:
    - not truncate
    - not say utf8 (because it should already be by default)
    - not disable foreign key checks (because it should work)
    - expressly name the fields (adding fields shouldn't break it)
    
    TEST PLAN
    ---------
    0) Backup opac_news if you care about your news.
    1) open mysql client
    2) use the koha database
    3) Run the following script
    TRUNCATE opac_news;
    source ~/kohaclone/installer/data/mysql/uk-UA/optional/sample_news.sql
    source ~/kohaclone/installer/data/mysql/it-IT/necessari/sample_news.sql
    source ~/kohaclone/installer/data/mysql/de-DE/optional/sample_news.sql
    source ~/kohaclone/installer/data/mysql/nb-NO/2-Valgfritt/sample_news.sql
    source ~/kohaclone/installer/data/mysql/en/optional/sample_news.sql
    source ~/kohaclone/installer/data/mysql/pl-PL/optional/sample_news.sql
    source ~/kohaclone/installer/data/mysql/es-ES/optional/sample_news.sql
    source ~/kohaclone/installer/data/mysql/ru-RU/optional/sample_news.sql
    source ~/kohaclone/installer/data/mysql/fr-FR/2-Optionel/sample_news.sql
    
    - Were there errors?
    - Did everything add nicely?
        - Not likely, by expressly stating the idnew field, the news
          items in some files would not be added.
    
    4) apply patch
    5) try running the script again
    
    - Were there errors?
    - Did everything add nicely?
    6) Log into the staff client
    7) Tools->News
    - Do you see the news items added?
    
    Signed-off-by: Chris Cormack <chris@bigballofwax.co.nz>
    
    Much needed clean up, making for much more robust and less error prone
    sql
    
    Signed-off-by: Jonathan Druart <jonathan.druart@biblibre.com>
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>

--- installer/data/mysql/it-IT/necessari/sample_news.sql
+++ installer/data/mysql/it-IT/necessari/sample_news.sql
@@ -13,10 +13,6 @@
 -- 
 -- Dump dei dati per la tabella 'opac_news'
 -- 
-SET FOREIGN_KEY_CHECKS=0;
-
-INSERT INTO opac_news (idnew, title, new, lang, timestamp, expirationdate, number) VALUES 
-(1, 'Benvenuto in Koha', '<p>Koha è un gestionale di biblioteca (ILS) completo e Open Source. Sviluppato inzialmente in New Zealand da Katipo Communications Ltd e messo in linea per la prima volta nel gennaio 2000 per il Horowhenua Library Trust, Koha è ora mantenuto da un gruppo di aziende di servizi e dipendenti di biblioteca distribuiti in tutto il mondo.</p>', 'koha', '2007-10-29 00:00:00', '2099-01-10', 1),
-(2, 'Cosa c''è di nuovo ?', '<p>Ora che hai installato cosa, qual''è il passo successivo ? Qui alcuni suggerimenti:</p>\r\n<ul>\r\n<li><a href="http://koha-community.org/documentation/">Leggi la documentazione di Koha</a></li>\r\n<li><a href="http://wiki.koha-community.org">Leggi/aggiorna il Wiki di Koha Wiki</a></li>\r\n<li><a href="http://koha-commaunity.org/support/koha-mailing-lists/">Leggi e contribusci alle liste di discussione</a></li>\r\n<li><a href="http://bugs.koha-community.org">Descrivi e segnala i bug di Koha</a></li>\r\n<li>Chatta con gli utenti e gli sviluppatori di Koha sul server irc.katipo.co.nz,  porta 6667 canale #koha</li>\r\n<li><a href="http://stats.workbuffer.org/irclog/koha/today">Leggi il log della chat di Koha di oggi</a></li>\r\n</ul>\r\n', 'koha', '2007-10-29 00:00:00', '2099-01-10', 2);
-
-SET FOREIGN_KEY_CHECKS=1;
+INSERT INTO opac_news (title, new, lang, timestamp, expirationdate, number) VALUES
+('Benvenuto in Koha', '<p>Koha è un gestionale di biblioteca (ILS) completo e Open Source. Sviluppato inzialmente in New Zealand da Katipo Communications Ltd e messo in linea per la prima volta nel gennaio 2000 per il Horowhenua Library Trust, Koha è ora mantenuto da un gruppo di aziende di servizi e dipendenti di biblioteca distribuiti in tutto il mondo.</p>', 'koha', '2007-10-29 00:00:00', '2099-01-10', 1),
+('Cosa c''è di nuovo ?', '<p>Ora che hai installato cosa, qual''è il passo successivo ? Qui alcuni suggerimenti:</p>\r\n<ul>\r\n<li><a href="http://koha-community.org/documentation/">Leggi la documentazione di Koha</a></li>\r\n<li><a href="http://wiki.koha-community.org">Leggi/aggiorna il Wiki di Koha Wiki</a></li>\r\n<li><a href="http://koha-commaunity.org/support/koha-mailing-lists/">Leggi e contribusci alle liste di discussione</a></li>\r\n<li><a href="http://bugs.koha-community.org">Descrivi e segnala i bug di Koha</a></li>\r\n<li>Chatta con gli utenti e gli sviluppatori di Koha sul server irc.katipo.co.nz,  porta 6667 canale #koha</li>\r\n<li><a href="http://stats.workbuffer.org/irclog/koha/today">Leggi il log della chat di Koha di oggi</a></li>\r\n</ul>\r\n', 'koha', '2007-10-29 00:00:00', '2099-01-10', 2);
