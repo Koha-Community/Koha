0297021030b9fd1b440ec2eb5f367b03f2bfc61c
Fridolin SOMERS <fridolin.somers@biblibre.com>
2013-12-17 18:23:29 +0100

    Bug 2546: DBRev 3.14.00.003
    Signed-off-by: Fridolin SOMERS <fridolin.somers@biblibre.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -7773,7 +7773,7 @@ if(CheckVersion($DBversion)) {
     SetVersion($DBversion);
 }
 
-$DBversion = "3.15.00.XXX";
+$DBversion = "3.14.00.003";
 if ( CheckVersion($DBversion) ) {
     $dbh->do(q{
         UPDATE accountlines
