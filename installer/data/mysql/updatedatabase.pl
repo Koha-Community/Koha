be55359877a0e4d9e772670ce841316a82bef765
Galen Charlton <gmc@esilibrary.com>
2013-12-23 16:21:44 +0000

    Bug 11391: DBRev 3.15.00.009
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -7862,7 +7862,7 @@ if ( CheckVersion($DBversion) ) {
    SetVersion ($DBversion);
 }
 
-$DBversion = "3.15.00.XXX";
+$DBversion = "3.15.00.009";
 if ( CheckVersion($DBversion) ) {
     $dbh->do(q{
         ALTER TABLE suggestions MODIFY suggesteddate DATE NOT NULL
