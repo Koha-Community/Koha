b0c11d03aadcb16fef00a10011080aadc3a38de1
Fridolin Somers <fridolin.somers@biblibre.com>
2014-01-27 12:27:35 +0100

    Bug 11249: DBRev 3.14.02.001
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -7864,7 +7864,7 @@ if ( CheckVersion($DBversion) ) {
     SetVersion ($DBversion);
 }
 
-$DBversion = "3.15.00.XXX";
+$DBversion = "3.14.02.001";
 if(CheckVersion($DBversion)) {
     $dbh->do(q{
         ALTER TABLE `borrowers`
