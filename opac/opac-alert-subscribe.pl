fc7e66d984a7d113c940121a990606361d263332
Sophie Meynieux <sophie.meynieux@biblibre.com>
2014-01-31 12:22:05 +0100

    Bug 11647: require that user log into OPAC to request email subscription notifications
    Signed-off-by: Chris Cormack <chris@bigballofwax.co.nz>
    Signed-off-by: remy juliette <remy.gonzalves@iepg.fr>
    Signed-off-by: Aurelie Fichot <aurelie.fichot@iepg.fr>
    Signed-off-by: Katrin Fischer <Katrin.Fischer.83@web.de>
    Passes QA script and tests.
    Instead of hiding the feature completely, now a message
    tells the user to login to subscribe to the email
    notifications.
    
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>
    (cherry picked from commit 6310c992bf23d164f013ead7c0b2182226279454)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- opac/opac-alert-subscribe.pl
+++ opac/opac-alert-subscribe.pl
@@ -46,7 +46,7 @@ my $biblionumber = $query->param('biblionumber');
         template_name   => "opac-alert-subscribe.tmpl",
         query           => $query,
         type            => "opac",
-        authnotrequired => ( C4::Context->preference("OpacPublic") ? 1 : 0 ),
+        authnotrequired => 0, #( C4::Context->preference("OpacPublic") ? 1 : 0 ),
         debug           => 1,
     }
 );
