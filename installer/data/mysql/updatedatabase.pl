f421f4e8de1efa28aeaecdfd0cbddb5978b7d450
Kyle M Hall <kyle@bywatersolutions.com>
2014-01-31 07:53:44 -0500

    Increment version for 3.08.22 release)

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -5489,6 +5489,12 @@ if ( C4::Context->preference("Version") < TransformToNum($DBversion) ) {
     SetVersion($DBversion);
 }
 
+$DBversion = "3.08.22.000";
+if ( C4::Context->preference("Version") < TransformToNum($DBversion) ) {
+    print "Upgrade to $DBversion done (3.8.22 release)\n";
+    SetVersion($DBversion);
+}
+
 =head1 FUNCTIONS
 
 =head2 TableExists($table)
