042b6f170bac364b41435c759de376f0f8b93b45
Galen Charlton <gmc@esilibrary.com>
2014-02-07 01:17:02 +0000

    bump version for Koha 3.8.23
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -5495,6 +5495,12 @@ if ( C4::Context->preference("Version") < TransformToNum($DBversion) ) {
     SetVersion($DBversion);
 }
 
+$DBversion = "3.08.23.000";
+if ( C4::Context->preference("Version") < TransformToNum($DBversion) ) {
+    print "Upgrade to $DBversion done (3.8.23 release)\n";
+    SetVersion($DBversion);
+}
+
 =head1 FUNCTIONS
 
 =head2 TableExists($table)
