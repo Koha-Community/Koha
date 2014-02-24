63d7386ed00f73be837a57aeb12d75c6893ca0bd
Owen Leonard <oleonard@myacpl.org>
2014-02-24 10:21:20 -0500

    Bug 11829: fix JavaScript error on staff client biblio details pages if template variables are undefined
    The JavaScript included in cat-toolbar.inc expects several variables to
    be passed to the template for use in calculations. Several scripts
    do not pass the required variables to the template, leading to a JS
    error.
    
    This patch re-adds (See Bug 9823) hold count calculation to the required
    scripts and copies the acquisitions-related count calculation from
    detail.pl into related bibliographic detail scripts so that the
    commonly-included cat-toolbar.inc can receive correct values.
    
    To test, locate a bibliographic record which has no holds and is not
    used in a current or deleted order, but which has a local cover image
    attached. Check the following views to confirm there is no JavaScript
    error:
    
    - Normal
    - MARC
    - Labeled MARC
    - ISBD
    - Items
    - Images (from the Images tab on the "Normal" detail page)
    
    Next, locate a bibliographic record with no items attached which is used
    in an Acquisitions order. On each of the pages listed above, choose
    "Delete record" from the Edit menu. This should trigger a warning that
    the record is used in an order. Cancel the deletion.
    
    Finally, delete the order which references the bibliographic record. On
    each of the pages listed above, choose "Delete record" again. This
    should trigger a warning that the record is used in a deleted order.
    Cancel the deletion.
    
    I went through the test plan, everything works OK.
    It resolves Bug 11831 as well.
    
    Signed-off-by: Marc Véron <veron@veron.ch>
    Signed-off-by: Katrin Fischer <Katrin.Fischer.83@web.de>
    - Printing from MARC and ISBD view works again
    - No more Javascript errors found
    - Deleting a record with attached order gives the correct warning,
      both for ordered and cancelled.
    
    Passes all tests and QA script.
    
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>

--- catalogue/ISBDdetail.pl
+++ catalogue/ISBDdetail.pl
@@ -47,6 +47,8 @@ use C4::Members; # to use GetMember
 use C4::Branch;     # GetBranchDetail
 use C4::Serials;    # CountSubscriptionFromBiblionumber
 use C4::Search;		# enabled_staff_search_views
+use C4::Acquisition qw(GetOrdersByBiblionumber);
+
 
 #---- Internal function
 
@@ -112,7 +114,37 @@ $template->param (
     searchid            => $query->param('searchid'),
 );
 
+my @allorders_using_biblio = GetOrdersByBiblionumber ($biblionumber);
+my @deletedorders_using_biblio;
+my @orders_using_biblio;
+my @baskets_orders;
+my @baskets_deletedorders;
 
+foreach my $myorder (@allorders_using_biblio) {
+    my $basket = $myorder->{'basketno'};
+    if ((defined $myorder->{'datecancellationprinted'}) and  ($myorder->{'datecancellationprinted'} ne '0000-00-00') ){
+        push @deletedorders_using_biblio, $myorder;
+        unless (grep(/^$basket$/, @baskets_deletedorders)){
+            push @baskets_deletedorders,$myorder->{'basketno'};
+        }
+    }
+    else {
+        push @orders_using_biblio, $myorder;
+        unless (grep(/^$basket$/, @baskets_orders)){
+            push @baskets_orders,$myorder->{'basketno'};
+            }
+    }
+}
+
+my $count_orders_using_biblio = scalar @orders_using_biblio ;
+$template->param (countorders => $count_orders_using_biblio);
+
+my $count_deletedorders_using_biblio = scalar @deletedorders_using_biblio ;
+$template->param (countdeletedorders => $count_deletedorders_using_biblio);
+
+my $holds = C4::Reserves::GetReservesFromBiblionumber({ biblionumber => $biblionumber, all_dates => 1 });
+my $holdcount = scalar( @$holds );
+$template->param( holdcount => scalar ( @$holds ) );
 
 output_html_with_http_headers $query, $cookie, $template->output;
 
