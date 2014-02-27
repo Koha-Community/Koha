026b757f5cfededb9790e41af2d51c6c7c050408
Kyle M Hall <kyle@bywatersolutions.com>
2014-02-27 08:55:22 -0500

    Bug 11866: do not require special permissions to view course reserves in staff interface
    A librarian with no course reserves permissions cannot view course
    reserves from the staff intranet, yet can from the OPAC. This doesn't
    make much sense. Librarians should not require any course reserves
    permissions to view courses and reserves from the staff intranet.
    
    Test Plan:
    1) Log into staff intranet as a librarian with no course reserves
       permissions
    2) Note you cannot view course reserves
    3) Apply this patch
    4) Note you can now view course reserves
    5) Verify you cannot modify courses or course reserves
    
    Signed-off-by: Owen Leonard <oleonard@myacpl.org>
    
    Bug 11866 [Follow-up] Staff side course reserves too restrictive
    
    This follow-up patch adds a check for the "UseCourseReserves" system
    preference to the display of the Course reserves menu item in the
    header.
    
    To test, view the "More" menu with the "UseCourseReserves" system
    preference on and off. The menu item should appear and disappear
    accordingly.
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Jonathan Druart <jonathan.druart@biblibre.com>
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>
    (cherry picked from commit 9e5fcb0d5dd230946719ee4825a3d41bd18effe7)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- course_reserves/course-reserves.pl
+++ course_reserves/course-reserves.pl
@@ -35,7 +35,6 @@ my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
         query           => $cgi,
         type            => "intranet",
         authnotrequired => 0,
-        flagsrequired   => { coursereserves => '*' },
     }
 );
 
