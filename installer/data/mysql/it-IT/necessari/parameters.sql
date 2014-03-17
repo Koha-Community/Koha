0e6a85ad7c09eccc99fbce9af47d4ae9ddae4350
Jonathan Druart <jonathan.druart@biblibre.com>
2014-03-17 17:21:09 +0100

    Bug 11939: set active currency for non-English sample data
    Revised Test Plan
    -----------------
    The following SQL commands were used to test this.
    
    installer/data/mysql/en/optional/parameters.sql is part of the
    first patch and didn't really need testing again.
    
    installer/data/mysql/de-DE/optional/parameters.sql is not part
    of the patch, but since I was playing with the script, which
    is also attached here, it was included for good measure.
    
    use kohadata;
    SET FOREIGN_KEY_CHECKS=0;
    TRUNCATE currency;
    source /home/mtompset/kohaclone/installer/data/mysql/pl-PL/optional/parameters.sql
    SET FOREIGN_KEY_CHECKS=0;
    TRUNCATE currency;
    source /home/mtompset/kohaclone/installer/data/mysql/es-ES/optional/parameters.sql
    SET FOREIGN_KEY_CHECKS=0;
    TRUNCATE currency;
    source /home/mtompset/kohaclone/installer/data/mysql/ru-RU/optional/sample_currency.sql
    SET FOREIGN_KEY_CHECKS=0;
    TRUNCATE currency;
    source /home/mtompset/kohaclone/installer/data/mysql/uk-UA/optional/sample_currency.sql
    SET FOREIGN_KEY_CHECKS=0;
    TRUNCATE currency;
    source /home/mtompset/kohaclone/installer/data/mysql/it-IT/necessari/parameters.sql
    SET FOREIGN_KEY_CHECKS=0;
    TRUNCATE currency;
    source /home/mtompset/kohaclone/installer/data/mysql/de-DE/optional/parameters.sql
    SET FOREIGN_KEY_CHECKS=0;
    TRUNCATE currency;
    source /home/mtompset/kohaclone/installer/data/mysql/nb-NO/2-Valgfritt/parameters.sql
    SET FOREIGN_KEY_CHECKS=0;
    TRUNCATE currency;
    source /home/mtompset/kohaclone/installer/data/mysql/en/optional/parameters.sql
    
    Signed-off-by: Mark Tompsett <mtompset@hotmail.com>
    Signed-off-by: Katrin Fischer <Katrin.Fischer.83@web.de>
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>
    (cherry picked from commit ed10e5226cc8e9852dfee3f5520976695cb99412)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- installer/data/mysql/it-IT/necessari/parameters.sql
+++ installer/data/mysql/it-IT/necessari/parameters.sql
@@ -1,8 +1,8 @@
 SET FOREIGN_KEY_CHECKS=0;
 
-INSERT INTO `currency` (currency, rate, symbol) VALUES
-('USD', 1.4, '$'),
-('GBP', .8, '£'),
-('EUR', 1, '€');
+INSERT INTO `currency` (currency, rate, symbol, active) VALUES
+('USD', 1.4, '$', 0),
+('GBP', .8, '£', 0),
+('EUR', 1, '€', 1);
 
 SET FOREIGN_KEY_CHECKS=1;
