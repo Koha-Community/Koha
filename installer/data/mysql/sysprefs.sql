377218b506c7caf97bd4b82ee35efc29486f6279
Jonathan Druart <jonathan.druart@biblibre.com>
2014-04-09 10:24:24 +0200

    Bug 12052: New syspref to display message on the OPAC patron summary page
    Test plan:
    
    Fill the OPACMySummaryNote with HTML code or just text.
    The content should be displayed at the OPAC on the summary page for
    patrons.
    
    Signed-off-by: Bernardo Gonzalez Kriegel <bgkriegel@gmail.com>
    Work as described. No koha-qa errors
    
    Signed-off-by: Katrin Fischer <Katrin.Fischer.83@web.de>
    Works as described, passes tests and QA script.
    Rephrased the pref text a little bit, using 'logged in' instead of
    'connected', also added " so the description appears correctly in the
    pref editor.
    
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>
    (cherry picked from commit f82d22b0df63e2924e9c8b35d2a4eb02edc2650e)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- installer/data/mysql/sysprefs.sql
+++ installer/data/mysql/sysprefs.sql
@@ -243,6 +243,7 @@ INSERT INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `
 ('OpacMainUserBlockMobile','',NULL,'Show the following HTML in its own column on the main page of the OPAC (mobile version):','free'),
 ('OPACMobileUserCSS','',NULL,'Include the following CSS for the mobile view on all pages in the OPAC:','free'),
 ('OPACMySummaryHTML','','70|10','Enter the HTML that will appear in a column on the \'my summary\' and \'my reading history\' tabs when a user is logged in to the OPAC. Enter {BIBLIONUMBER}, {TITLE}, {AUTHOR}, or {ISBN} in place of their respective variables in the HTML. Leave blank to disable.','Textarea'),
+('OPACMySummaryNote','','','Note to display on the patron summary page. This note only appears if the patron is connected.','Free'),
 ('OpacNav','Important links here.','70|10','Use HTML tags to add navigational links to the left-hand navigational bar in OPAC','Textarea'),
 ('OpacNavBottom','Important links here.','70|10','Use HTML tags to add navigational links to the left-hand navigational bar in OPAC','Textarea'),
 ('OpacNavRight','','70|10','Show the following HTML in the right hand column of the main page under the main login form','Textarea'),
