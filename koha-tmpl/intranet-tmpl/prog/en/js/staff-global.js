c16cadf21656e6c1796c7eac28db33ead02cf59d
Galen Charlton <gmc@esilibrary.com>
2014-04-11 17:51:50 +0000

    Bug 11563: (follow-up) remove some tabs
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>

--- koha-tmpl/intranet-tmpl/prog/en/js/staff-global.js
+++ koha-tmpl/intranet-tmpl/prog/en/js/staff-global.js
@@ -32,20 +32,20 @@ function _(s) { return s; } // dummy function for gettext
 
 // http://jennifermadden.com/javascript/stringEnterKeyDetector.html
 function checkEnter(e){ //e is event object passed from function invocation
-	var characterCode; // literal character code will be stored in this variable
-	if(e && e.which){ //if which property of event object is supported (NN4)
-		e = e;
-		characterCode = e.which; //character code is contained in NN4's which property
-	} else {
-            e = window.event;
-		characterCode = e.keyCode; //character code is contained in IE's keyCode property
-	}
+    var characterCode; // literal character code will be stored in this variable
+    if(e && e.which){ //if which property of event object is supported (NN4)
+        e = e;
+        characterCode = e.which; //character code is contained in NN4's which property
+    } else {
+        e = window.event;
+        characterCode = e.keyCode; //character code is contained in IE's keyCode property
+    }
 
-	if(characterCode == 13){ //if generated character code is equal to ascii 13 (if enter key)
-		return false;
-	} else {
-		return true;
-	}
+    if(characterCode == 13){ //if generated character code is equal to ascii 13 (if enter key)
+        return false;
+    } else {
+        return true;
+    }
 }
 
 function clearHoldFor(){
