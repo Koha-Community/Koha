82db730d71c0f96b2dbf5ec0f4321b2cf6045593
Jonathan Druart <jonathan.druart@biblibre.com>
2014-04-23 15:45:46 +0200

    Bug 766: Update POD for AuthorisedValues TT plugin
    Signed-off-by: Bernardo Gonzalez Kriegel <bgkriegel@gmail.com>
    Signed-off-by: Katrin Fischer <Katrin.Fischer.83@web.de>
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>

--- Koha/Template/Plugin/AuthorisedValues.pm
+++ Koha/Template/Plugin/AuthorisedValues.pm
@@ -1,22 +1,5 @@
 package Koha::Template::Plugin::AuthorisedValues;
 
-# Copyright ByWater Solutions 2012
-
-# This file is part of Koha.
-#
-# Koha is free software; you can redistribute it and/or modify it under the
-# terms of the GNU General Public License as published by the Free Software
-# Foundation; either version 2 of the License, or (at your option) any later
-# version.
-#
-# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along
-# with Koha; if not, write to the Free Software Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
 use Modern::Perl;
 
 use Template::Plugin;
@@ -26,18 +9,6 @@ use Encode qw{encode decode};
 
 use C4::Koha;
 use C4::Charset;
-
-=pod
-
-To use, first, include the line '[% USE AuthorisedValues %]' at the top
-of the template to enable the plugin.
-
-Now, in a template, you can get the description for an authorised value with
-the following TT code: [% AuthorisedValues.GetByCode( 'CATEGORY', 'AUTHORISED_VALUE_CODE', 'IS_OPAC' ) %]
-
-The parameters are identical to those used by the subroutine C4::Koha::GetAuthorisedValueByCode.
-
-=cut
 
 sub GetByCode {
     my ( $self, $category, $code, $opac ) = @_;
@@ -55,3 +26,53 @@ sub GetAuthValueDropbox {
 }
 
 1;
+
+=head1 NAME
+
+Koha::Template::Plugin::AuthorisedValues - TT Plugin for authorised values
+
+=head1 SYNOPSIS
+
+[% USE AuthorisedValues %]
+
+[% AuthorisedValues.GetByCode( 'CATEGORY', 'AUTHORISED_VALUE_CODE', 'IS_OPAC' ) %]
+
+[% AuthorisedValues.GetAuthValueDropbox( $category, $default ) %]
+
+=head1 ROUTINES
+
+=head2 GetByCode
+
+In a template, you can get the description for an authorised value with
+the following TT code: [% AuthorisedValues.GetByCode( 'CATEGORY', 'AUTHORISED_VALUE_CODE', 'IS_OPAC' ) %]
+
+The parameters are identical to those used by the subroutine C4::Koha::GetAuthorisedValueByCode.
+
+=head2 GetAuthValueDropbox
+
+The parameters are identical to those used by the subroutine C4::Koha::GetAuthValueDropbox
+
+=head1 AUTHOR
+
+Kyle M Hall <kyle@bywatersolutions.com>
+
+Jonathan Druart <jonathan.druart@biblibre.com>
+
+=head1 COPYRIGHT
+
+Copyright ByWater Solutions 2012
+
+Copyright BibLibre 2013
+
+=head1 LICENSE
+
+This file is part of Koha.
+
+Koha is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 3 of the License, or (at your option) any later version.
+
+You should have received a copy of the GNU General Public License along
+with Koha; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+=cut
