92c765307198b0624dcb80b342beedef412cccf7
Tomas Cohen Arazi <tomascohen@gmail.com>
2014-05-05 11:50:55 -0300

    Bug 11404: koha-functions.sh introduced for reuse
    As asked by Robin, a bash lib of functions is introduced with the common
    functions to be reused. Most of the scripts are modified (reduced) to
    include this file and the repeated functions cleaned.
    
    No noticeable change in behaviour should be noticed.
    
    As I've been todl in #debian-mentors, it is used that files for inclusion
    should be installed at the apps directory (i.e. /usr/share/koha/) so this
    patch makes the install script put the file in the bin/ directory.
    
    All koha-* scripts assume the file is there already (and fail otherwise).
    
    Sponsored-by: Universidad Nacional de Cordoba
    Signed-off-by: Robin Sheat <robin@catalyst.net.nz>
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>

---
+++ debian/scripts/koha-functions.sh
@@ -0,0 +1,115 @@
+#!/bin/sh
+#
+# koha-functions.sh -- shared library of helper functions for koha-* scripts
+# Copyright 2014 - Tomas Cohen Arazi
+#                  Universidad Nacional de Cordoba
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+die()
+{
+    echo "$@" 1>&2
+    exit 1
+}
+
+warn()
+{
+    echo "$@" 1>&2
+}
+
+get_apache_config_for()
+{
+    local site=$1
+    local sitefile="/etc/apache2/sites-available/$site"
+
+    if is_instance $site; then
+        if [ -f "$sitefile.conf" ]; then
+            echo "$sitefile.conf"
+        elif [ -f "$sitefile" ]; then
+            echo "$sitefile"
+        fi
+    fi
+}
+
+is_enabled()
+{
+    local site=$1
+    local instancefile=$(get_apache_config_for $site)
+
+    if [ "$instancefile" = "" ]; then
+        return 1
+    fi
+
+    if grep -q '^[[:space:]]*Include /etc/koha/apache-shared-disable.conf' \
+            "$instancefile" ; then
+        return 1
+    else
+        return 0
+    fi
+}
+
+is_instance()
+{
+    local instancename=$1
+
+    if find /etc/koha/sites -mindepth 1 -maxdepth 1 \
+                         -type d -printf '%f\n'\
+          | grep -q -x "$instancename" ; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+is_email_enabled()
+{
+    local instancename=$1
+
+    if [ -e /var/lib/koha/$instancename/email.enabled ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+is_sip_enabled()
+{
+    local instancename=$1
+
+    if [ -e /etc/koha/sites/$instancename/SIPconfig.xml ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+is_zebra_running()
+{
+    local instancename=$1
+
+    if daemon --name="$instancename-koha-zebra" \
+            --user="$instancename-koha.$instancename-koha" \
+            --running ; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+get_instances()
+{
+    find /etc/koha/sites -mindepth 1 -maxdepth 1\
+                         -type d -printf '%f\n' | sort
+}
