88f26fa90742dbd422995d19e8f26e8d81638b65
Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
2014-05-22 15:37:29 +0200

    Bug 6536: Add a form for SRU Field mapping
    This form allows you to enter the match between the standard names of
    the Z3950 search fields and the field names for a specific SRU server.
    This patch only adds the form; it will be used in a subsequent patch.
    
    Test plan:
    Not needed, but you can call the form by constructing a URL like:
        cgi-bin/koha/admin/sru_modmapping.pl?mapping=title=1,author=2
    You should see the form with title and author filled.
    Note that you cannot save the information yet.
    
    Signed-off-by: Giuseppe Angilella <giuseppe.angilella@ct.infn.it>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@gmail.com>

---
+++ admin/sru_modmapping.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl
+
+# Copyright 2014 Rijksmuseum
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
+use CGI;
+use C4::Auth;
+use C4::Output;
+
+# Initialize CGI, template
+
+my $input = new CGI;
+my $mapstr = $input->param('mapping')//'';
+my ( $template, $loggedinuser, $cookie ) = get_template_and_user( {
+    template_name => "admin/sru_modmapping.tt",
+    query => $input,
+    type => "intranet",
+    authnotrequired => 0,
+});
+
+# Main code: convert mapping string to hash structure and show template
+
+my %map;
+foreach my $singlemap ( split ',', $mapstr ) {
+    my @temp = split '=', $singlemap;
+    $map{ $temp[0] } = $temp[1] if @temp>1;
+}
+$template->param( mapping => \%map );
+output_html_with_http_headers $input, $cookie, $template->output;
+
+# End of main code
