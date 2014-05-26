2ff88b27b8c142185af98aa3ff68568c8a7bc8f7
Galen Charlton <gmc@esilibrary.com>
2014-05-26 01:26:43 +0000

    Bug 6254: (follow-up) update DBIC classes using newer version of DBIx::Class::Schema::Loader
    Signed-off-by: Galen Charlton <gmc@esilibrary.com>

--- Koha/Schema/Result/Category.pm
+++ Koha/Schema/Result/Category.pm
@@ -1,17 +1,21 @@
+use utf8;
 package Koha::Schema::Result::Category;
 
 # Created by DBIx::Class::Schema::Loader
 # DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+Koha::Schema::Result::Category
+
+=cut
 
 use strict;
 use warnings;
 
 use base 'DBIx::Class::Core';
 
-
-=head1 NAME
-
-Koha::Schema::Result::Category
+=head1 TABLE: C<categories>
 
 =cut
 
@@ -39,6 +43,7 @@ __PACKAGE__->table("categories");
 =head2 enrolmentperioddate
 
   data_type: 'date'
+  datetime_undef_if_invalid: 1
   is_nullable: 1
 
 =head2 upperagelimit
@@ -120,7 +125,7 @@ __PACKAGE__->add_columns(
   "enrolmentperiod",
   { data_type => "smallint", is_nullable => 1 },
   "enrolmentperioddate",
-  { data_type => "date", is_nullable => 1 },
+  { data_type => "date", datetime_undef_if_invalid => 1, is_nullable => 1 },
   "upperagelimit",
   { data_type => "smallint", is_nullable => 1 },
   "dateofbirthrequired",
@@ -151,6 +156,17 @@ __PACKAGE__->add_columns(
     is_nullable => 0,
   },
 );
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</categorycode>
+
+=back
+
+=cut
+
 __PACKAGE__->set_primary_key("categorycode");
 
 =head1 RELATIONS
@@ -231,8 +247,8 @@ __PACKAGE__->might_have(
 );
 
 
-# Created by DBIx::Class::Schema::Loader v0.07000 @ 2014-05-07 12:15:12
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:CwCE9qxkWafuO9btYXWZeg
+# Created by DBIx::Class::Schema::Loader v0.07025 @ 2014-05-26 01:25:05
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:7am0HIdPPQD5sZyKF4FipQ
 
 
 # You can replace this text with custom content, and it will be preserved on regeneration
