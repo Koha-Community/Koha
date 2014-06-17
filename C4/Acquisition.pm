9fe36e0c705b958859babe565517554b722c66cd
Fridolin Somers <fridolin.somers@biblibre.com>
2014-06-17 16:38:54 +0200

    Bug 12438 - Bad encoding in acquisition basket
    We noticed a bad encoding (diacritics replaced by <?>) in acquisition basket when updating a server to Debian Wheezy.
    We found it comes from a query containing biblio.title twice.
    Maybe the mysql newer version creates this side-effect.
    
    Test plan :
    - Create an order on a record containing a diacritic in title
    - Look at the basket : cgi-bin/koha/acqui/basket.pl?basketno=x
    => Without the patch the record title is bad encoded (with <?>)
    => With this patch the record title is well encoded
    - Check also basket CSV export
    
    Signed-off-by: Paola Rossi <paola.rossi@cineca.it>
    Signed-off-by: Katrin Fischer <Katrin.Fischer.83@web.de>
    Good catch!
    Works as expected, passes tests and QA script.
    Signed-off-by: Tomas Cohen Arazi <tomascohen@gmail.com>
    Duplicated biblio.title is a (minor) bug, and should be removed.
    
    The side-effect of it solving an encoding problem might be seen
    as problematic: it hides a real problem.
    The efforts on 11944 actually solve this encoding problem (11944
    merged into master actually fixes this), so I'm pushing it, for
    a short term solution for stable, with the hope that we will soon
    have 11944 pushed.
    BTW, non-diacritic but non-ASCII characters are not broken either.

--- C4/Acquisition.pm
+++ C4/Acquisition.pm
@@ -1052,7 +1052,6 @@ sub GetOrders {
         SELECT biblio.*,biblioitems.*,
                 aqorders.*,
                 aqbudgets.*,
-                biblio.title,
                 aqorders_transfers.ordernumber_from AS transferred_from,
                 aqorders_transfers.timestamp AS transferred_from_timestamp
         FROM    aqorders
