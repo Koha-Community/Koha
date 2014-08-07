def7816de4d1aebffa73af69718ba94c566c8e85
Tomas Cohen Arazi <tomascohen@gmail.com>
2014-08-07 11:58:15 -0300

    Bug 4045: DBRev 3.17.00.013
    Signed-off-by: Tomas Cohen Arazi <tomascohen@gmail.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -8596,7 +8596,7 @@ if ( CheckVersion($DBversion) ) {
     SetVersion ($DBversion);
 }
 
-$DBversion = "3.17.00.XXX";
+$DBversion = "3.17.00.013";
 if ( CheckVersion($DBversion) ) {
     $dbh->do("INSERT IGNORE INTO systempreferences (variable,value,explanation,options,type) VALUES ('maxreserves',50,'System-wide maximum number of holds a patron can place','','Integer')");
     print "Upgrade to $DBversion done (Re-add system preference maxreserves)\n";
