2668cbdc419fa550968d3a63dec4ac1d09749a17
mbeaulieu <mbeaulieu@inlibro.com>
2014-08-08 08:38:20 -0400

    Bug 11982 - Fixed the amazon no image icon.
    A no image icon will only be displayed if both the amazon and local images are missing.
    Added hyperlink to details on the local image in the search result page.
        modified:   koha-tmpl/intranet-tmpl/prog/en/js/localcovers.js
        modified:   koha-tmpl/intranet-tmpl/prog/en/modules/catalogue/detail.tt
        modified:   koha-tmpl/intranet-tmpl/prog/en/modules/catalogue/results.tt
    
    Signed-off-by: Owen Leonard <oleonard@myacpl.org>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@gmail.com>

--- koha-tmpl/intranet-tmpl/prog/en/js/localcovers.js
+++ koha-tmpl/intranet-tmpl/prog/en/js/localcovers.js
@@ -46,7 +46,44 @@ KOHA.LocalCover = {
                         }
                         $(mydiv).children('.no-image').remove();
                     }
-                })
+                });
+        });
+    },
+    LoadResultsCovers: function(){
+        $("div [id^=local-thumbnail]").each(function(i) {
+            var mydiv = this;
+            var message = document.createElement("span");
+            $(message).attr("class","no-image thumbnail");
+            $(message).html(NO_LOCAL_JACKET);
+            $(mydiv).append(message);
+            var img = $("<img />");
+            img.attr('src','/cgi-bin/koha/catalogue/image.pl?thumbnail=1&biblionumber=' + $(mydiv).attr("class"));
+            img.load(function () {
+                    if (!this.complete || typeof this.naturalWidth == "undefined" || this.naturalWidth <= 1) {
+                        //IE HACK
+                        try {
+                            var otherCovers = $(mydiv).closest('td').find('img');
+                            var nbCovers = otherCovers.length;
+                            if(nbCovers > 0){
+                                var badCovers = 0;
+                                otherCovers.each(function(){
+                                    if(this.naturalWidth <= 1){
+                                        $(this).parent().remove();
+                                        badCovers++;
+                                    }
+                                });
+                                if(badCovers < nbCovers){
+                                    $(mydiv).parent().remove();
+                                }
+                            }
+                        }
+                        catch(err){
+                        }
+                    } else {
+                        $(mydiv).append(img);
+                        $(mydiv).children('.no-image').remove();
+                    }
+                });
         });
     }
 };
