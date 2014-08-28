b4cc38344004348268f168ec8400a3e24c7c1012
Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
2014-08-28 14:59:56 +0200

    Bug 6536: Follow-up: Updating web installer sql files for z3950servers
    Removing icon, description and position from installer sql files.
    Not all files contained them.
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    I have run all sql files on an empty table by script.
    
    Signed-off-by: Tomas Cohen Arazi <tomascohen@gmail.com>

--- installer/data/mysql/fr-FR/2-Optionel/z3950_BNF.sql
+++ installer/data/mysql/fr-FR/2-Optionel/z3950_BNF.sql
@@ -1,1 +1,1 @@
-INSERT INTO `z3950servers` (`host`, `port`, `db`, `userid`, `password`, `name`, `id`, `checked`, `rank`, `syntax`, `icon`, `position`, `type`, `description`) VALUES ('z3950.bnf.fr', 2211, 'TOUT', 'Z3950', 'Z3950_BNF', 'BNF2', 2, 1, 2, 'UNIMARC', NULL, 'primary', 'zed', '');
+INSERT INTO `z3950servers` (`host`, `port`, `db`, `userid`, `password`, `name`, `id`, `checked`, `rank`, `syntax`, `type`) VALUES ('z3950.bnf.fr', 2211, 'TOUT', 'Z3950', 'Z3950_BNF', 'BNF2', 2, 1, 2, 'UNIMARC', 'zed');
