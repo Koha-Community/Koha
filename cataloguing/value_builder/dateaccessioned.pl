8dfc399a2d42859ab510c9d86c73b6522a3c5335
Jonathan Druart <jonathan.druart@biblibre.com>
2014-09-08 13:21:17 +0200

    Bug 12884: Get rid of redefined subroutine warnings in dateaccessioned.pl
    Instead of removing all warnings, we should remove warnings about the
    redefined subroutines.
    
    Test plan:
    - link the dateaccessioned plugin with one of your biblio field
    - edit an item
    - verify they are no warnings in the Koha log file.
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@gmail.com>

--- cataloguing/value_builder/dateaccessioned.pl
+++ cataloguing/value_builder/dateaccessioned.pl
@@ -17,8 +17,9 @@
 # with Koha; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-use strict;
-use warnings;
+use Modern::Perl;
+
+no warnings 'redefine';
 
 =head1
 
