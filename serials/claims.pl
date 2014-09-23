4179c63d7ea1a087b725b0a8da98537aa1d61949
Jonathan Druart <jonathan.druart@biblibre.com>
2014-09-23 12:39:55 +0200

    Bug 12852: C4::Bookseller should not be imported anymore
    This script does not depend on C4::Bookseller anymore. The import can be
    removed.
    
    Signed-off-by: Paola Rossi <paola.rossi@cineca.it>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@gmail.com>

--- serials/claims.pl
+++ serials/claims.pl
@@ -24,7 +24,6 @@ use C4::Auth;
 use C4::Serials;
 use C4::Acquisition;
 use C4::Output;
-use C4::Bookseller qw( GetBookSeller );
 use C4::Context;
 use C4::Letters;
 use C4::Branch;    # GetBranches GetBranchesLoop
