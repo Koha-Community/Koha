836aba0bfbf4300d85e19b96c906c5c0d092a8ea
Tomas Cohen Arazi <tomascohen@gmail.com>
2014-10-28 10:09:30 -0300

    Bug 5342: DBRev 3.17.00.035
    Signed-off-by: Tomas Cohen Arazi <tomascohen@gmail.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -8877,7 +8877,7 @@ if ( CheckVersion($DBversion) ) {
     SetVersion($DBversion);
 }
 
-$DBversion = "3.17.00.XXX";
+$DBversion = "3.17.00.035";
 if ( CheckVersion($DBversion) ) {
     $dbh->do(q|
         ALTER TABLE serial ADD COLUMN claims_count INT(11) DEFAULT 0 after claimdate
