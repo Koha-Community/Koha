2926168aaa44a79efd738221ee4bc942728eae38
Kyle M Hall <kyle@bywatersolutions.com>
2014-11-17 12:31:05 -0500

    Bug 12971 - Regression: Patron print summary doesn't show checkouts
    A patron's print summary should contain a list of checked out items
    as it did in 3.16.2 and earlier.
    
    Please note, as of 3.16.2 reserves were no longer part of the print
    summary and thus are not part of this bug fixing patch.
    
    Test Plan:
    1) Find a patron with checked out items
    2) Choose Print -> Print summary
    3) Note the lack of a list of checkouts
    4) Apply this patch
    5) Reload the page
    5) Print the summary again
    6) Note the list of checkouts
    
    Signed-off-by: Owen Leonard <oleonard@myacpl.org>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    Works as described, apart from the missing status information
    that Owen already noted on the bug.
    Passes tests and QA script.
    Signed-off-by: Mason James <mtj@kohaaloha.com>

---
+++ members/summary-print.pl
@@ -0,0 +1,101 @@
+#!/usr/bin/perl
+
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use Modern::Perl;
+
+use CGI;
+
+use C4::Auth;
+use C4::Output;
+use C4::Members;
+use C4::Koha qw( getitemtypeinfo );
+use C4::Circulation qw( GetIssuingCharges );
+
+my $input          = CGI->new;
+my $borrowernumber = $input->param('borrowernumber');
+
+my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
+    {
+        template_name   => "members/moremember-print.tt",
+        query           => $input,
+        type            => "intranet",
+        authnotrequired => 0,
+        flagsrequired   => { circulate => "circulate_remaining_permissions" },
+        debug           => 1,
+    }
+);
+
+my $data = GetMember( 'borrowernumber' => $borrowernumber );
+
+my ( $total, $accts, $numaccts ) = GetMemberAccountRecords($borrowernumber);
+foreach my $accountline (@$accts) {
+    $accountline->{amount} = sprintf '%.2f', $accountline->{amount};
+    $accountline->{amountoutstanding} = sprintf '%.2f',
+      $accountline->{amountoutstanding};
+
+    if (   $accountline->{accounttype} ne 'F'
+        && $accountline->{accounttype} ne 'FU' )
+    {
+        $accountline->{printtitle} = 1;
+    }
+}
+
+my $roadtype =
+  C4::Koha::GetAuthorisedValueByCode( 'ROADTYPE', $data->{streettype} );
+
+our $totalprice = 0;
+$template->param(
+    %$data,
+
+    borrowernumber => $borrowernumber,
+    address => $data->{'streetnumber'} . " $roadtype " . $data->{'address'},
+
+    accounts => $accts,
+    totaldue => sprintf( "%.2f", $total ),
+
+    issues     => build_issue_data( GetPendingIssues($borrowernumber) ),
+    totalprice => $totalprice,
+);
+
+output_html_with_http_headers $input, $cookie, $template->output;
+
+sub build_issue_data {
+    my $issues = shift;
+
+    my $return;
+
+    my $today = DateTime->now( time_zone => C4::Context->tz );
+    $today->truncate( to => 'day' );
+
+    foreach my $issue ( @{$issues} ) {
+
+        my %row = %{$issue};
+        $totalprice += $issue->{replacementprice};
+
+        #find the charge for an item
+        my ( $charge, $itemtype ) =
+          GetIssuingCharges( $issue->{itemnumber}, $borrowernumber );
+
+        my $itemtypeinfo = getitemtypeinfo($itemtype);
+        $row{'itemtype_description'} = $itemtypeinfo->{description};
+
+        $row{'charge'} = sprintf( "%.2f", $charge );
+
+        push( @{$return}, \%row );
+    }
+    return $return;
+}
