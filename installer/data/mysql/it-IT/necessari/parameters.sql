3cf719433597ad6bf558950270cd564a62164ab4
Mark Tompsett <mtompset@hotmail.com>
2014-11-19 13:28:12 -0500

    Bug 11954 - Clean up currency default files
    It makes no sense to have TRUNCATE or foreign key checks.
    
    TEST PLAN
    ---------
    1) Back up your database
    2) in an mysql client:
         use kohadata;
         SET FOREIGN_KEY_CHECKS=0;
         TRUNCATE currency;
         source /home/mtompset/kohaclone/installer/data/mysql/ru-RU/optional/sample_currency.sql
         SET FOREIGN_KEY_CHECKS=0;
         TRUNCATE currency;
         source /home/mtompset/kohaclone/installer/data/mysql/uk-UA/optional/sample_currency.sql
         SET FOREIGN_KEY_CHECKS=0;
         TRUNCATE currency;
         source /home/mtompset/kohaclone/installer/data/mysql/it-IT/necessari/parameters.sql
         SET FOREIGN_KEY_CHECKS=1;
         QUIT
       -- These should all work without problem.
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@gmail.com>

--- installer/data/mysql/it-IT/necessari/parameters.sql
+++ installer/data/mysql/it-IT/necessari/parameters.sql
@@ -1,8 +1,4 @@
-SET FOREIGN_KEY_CHECKS=0;
-
 INSERT INTO `currency` (currency, rate, symbol, active) VALUES
 ('USD', 1.4, '$', 0),
 ('GBP', .8, '£', 0),
 ('EUR', 1, '€', 1);
-
-SET FOREIGN_KEY_CHECKS=1;
