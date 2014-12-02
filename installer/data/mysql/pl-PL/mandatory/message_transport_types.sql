de08845edc543c449698e01c5ea1eec509c8b271
Rafal Kopaczka <rkk0@poczta.onet.pl>
2014-12-02 14:14:23 +0100

    Bug 13373 [1/2] - Update Polish web installer sample data files.
    Part 1/2 - mandatory files.
    
    Changes:
    - Translated user permission and user flags.
    - Changed untranslated different files from en version
    - Deleted unnecessary files
    
    To test:
    - Aplly patch
    - Run webinstaller on empty database (drop database if necessary)
    - Verify everything works, especially verify you have permission to all
    modules, eg. Tools, Catalouging etc.
    - Check spelling and grammar if you can :)
    
    Signed-off-by: Chris <chris@bigballofwax.co.nz>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@gmail.com>

--- installer/data/mysql/pl-PL/mandatory/message_transport_types.sql
+++ installer/data/mysql/pl-PL/mandatory/message_transport_types.sql
@@ -2,5 +2,6 @@ INSERT INTO message_transport_types
 (message_transport_type)
 values
 ('email'),
+('print'),
 ('sms'),
 ('phone');
--- installer/data/mysql/pl-PL/mandatory/userflags.sql
+++ installer/data/mysql/pl-PL/mandatory/userflags.sql
@@ -1,19 +1,20 @@
-INSERT INTO `userflags` VALUES(0,'superlibrarian','Access to all librarian functions',0);
-INSERT INTO `userflags` VALUES(1,'circulate','Circulate books',0);
-INSERT INTO `userflags` VALUES(2,'catalogue','View Catalog (Librarian Interface)',0);
-INSERT INTO `userflags` VALUES(3,'parameters','Set Koha system parameters',0);
-INSERT INTO `userflags` VALUES(4,'borrowers','Add or modify borrowers',0);
-INSERT INTO `userflags` VALUES(5,'permissions','Set user permissions',0);
-INSERT INTO `userflags` VALUES(6,'reserveforothers','Place and modify holds for patrons',0);
-INSERT INTO `userflags` VALUES(7,'borrow','Borrow books',1);
-INSERT INTO `userflags` VALUES(9,'editcatalogue','Edit Catalog (Modify bibliographic/holdings data)',0);
-INSERT INTO `userflags` VALUES(10,'updatecharges','Update borrower charges',0);
-INSERT INTO `userflags` VALUES(11,'acquisition','Acquisition and/or suggestion management',0);
-INSERT INTO `userflags` VALUES(12,'management','Set library management parameters',0);
-INSERT INTO `userflags` VALUES(13,'tools','Use tools (export, import, barcodes)',0);
-INSERT INTO `userflags` VALUES(14,'editauthorities','Allow to edit authorities',0);
-INSERT INTO `userflags` VALUES(15,'serials','Allow to manage serials subscriptions',0);
-INSERT INTO `userflags` VALUES(16,'reports','Allow to access to the reports module',0);
-INSERT INTO `userflags` VALUES(17,'staffaccess','Modify login / permissions for staff users',0);
-INSERT INTO `userflags` VALUES(18,'coursereserves','Course Reserves',0);
-INSERT INTO `userflags` VALUES(19, 'plugins', 'Koha plugins', '0');
+INSERT INTO userflags (bit, flag, flagdesc, defaulton) VALUES
+(0,'superlibrarian','Dostęp do wszystkich ustawień',0),
+(1,'circulate','Wypożyczanie i zwracanie pozycji',0),
+(2,'catalogue','<b>Dostęp do interfejsu bibliotekarza.</b> Interfejs bibliotekarza, umożliwia przeglądanie katalogu.',0),
+(3,'parameters','Zarządzanie utawieniami systemowymi (Panel administracyjny)',0),
+(4,'borrowers','Dodawanie i modyfikowanie użytkowników',0),
+(5,'permissions','Ustawianie uprawnień użytkownikom',0),
+(6,'reserveforothers','Składanie i modyfikacja zamówień w imieniu użytkowników',0),
+(7,'borrow','Wypożyczanie książek',1),
+(9,'editcatalogue','Modyfikowanie katalogu (Modyfikacja rekordów bibliograficznych/egzemplarza)',0),
+(10,'updatecharges','Zarządzanie należnościami',0),
+(11,'acquisition','Gromadzenie oraz/lub zarządzanie propozycjami zakupu',0),
+(12,'management','Ustawianie zarządzania biblioteką (wycofane)',0),
+(13,'tools','Narzędzia (rozwiń w celu barziej szczegółowego nadawania uprawnień)',0),
+(14,'editauthorities','Modyfikacja haseł wzorcowych',0),
+(15,'serials','Zarządzanie prenumeratami',0),
+(16,'reports','Zezwól na dostęp do modułu raportów',0),
+(17,'staffaccess','Pozwolenie na modyfikacje uprawnień innych pracowników',0),
+(18,'coursereserves','Kursy',0),
+(19, 'plugins', 'Wtyczki Koha', '0');
