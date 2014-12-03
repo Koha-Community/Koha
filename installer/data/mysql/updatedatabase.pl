8ad2ea87158754cd434c135e71398e9719e45d00
Jonathan Druart <jonathan.druart@biblibre.com>
2014-12-03 12:33:48 +0100

    Bug 13379: Extends authorised_values.category to varchar(32)
    This patch does 2 things:
    1/ It fixes a bug caused by the different data fields used for
    authorised_values.category and
    borrower_attribute_types.authorised_value_category (varchar(16) vs
    varchar(10)).
    To reproduce:
    - Create a AV category with a length > 10
    - Try to link a patron attribute type with this AV, it won't work.
    
    2/ Extends this field to varchar(32): sometime 16 is not enough to
    describe something
    
    Signed-off-by: Bernardo Gonzalez Kriegel <bgkriegel@gmail.com>
    No koha-qa errors. Error reproduced, fixed by patch.
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@gmail.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -9642,6 +9642,22 @@ if ( CheckVersion($DBversion) ) {
     SetVersion ($DBversion);
 }
 
+$DBversion = "3.19.00.XXX";
+if(CheckVersion($DBversion)) {
+    $dbh->do(q{
+        ALTER TABLE authorised_values MODIFY COLUMN category VARCHAR(32) NOT NULL DEFAULT ''
+    });
+
+    $dbh->do(q{
+        ALTER TABLE borrower_attribute_types MODIFY COLUMN authorised_value_category VARCHAR(32) DEFAULT NULL
+    });
+
+    print "Upgrade to $DBversion done (Bug 13379 - Modify authorised_values.category to varchar(32))\n";
+    SetVersion($DBversion);
+}
+
+
+
 =head1 FUNCTIONS
 
 =head2 TableExists($table)
