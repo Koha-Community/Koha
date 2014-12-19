43fadbadea9a4504e519dfb0b915e57fd811e0ad
Simith D'Oliveira <simith@inlibro.com>
2014-12-19 14:44:48 -0500

    Bug 13485: Add a page to display links to restricted sites
    To test:
    
    I) Apply the patch
    II) Run updatedatabase.pl
    
    scenario I)
    
    1) Add some content in RestrictedPageContent and RestrictedPageTitle sysprefs.
    2) Add your machine ip (ex. 127.0.0)
    3) Validate cgi-bin/koha/opac-proxypage.pl shows a page with RestrictedPageContent and RestrictedPageTitle contents.
    
    scenario II)
    
    1) Logout opac page
    2) Add some content in RestrictedPageContent and RestrictedPageTitle sysprefs.
    3) Add other ip that your machine ip.
    4) Validate: cgi-bin/koha/opac-proxypage.pl shows a login page.
    5) Validate: cgi-bin/koha/opac-proxypage.pl shows a page with RestrictedPageContent and RestrictedPageTitle contents after login.
    
    Followed both scenarios, works as expected.
    Signed-off-by: Marc Veron <veron@veron.ch>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    
    Bug 13485 [QA Followup]
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    
    Bug 13485 - Atomic Update + Rename Proxy page to Restricted page
    
    Test plan:
    
    I) Apply the patch
    II) Run updatedatabase.pl
    
    scenario I)
    
    1) Add some content in RestrictedPageContent and RestrictedPageTitle sysprefs.
    2) Add your machine ip (ex. 127.0.0)
    3) Validate cgi-bin/koha/opac-restrictedpage.pl shows a page with RestrictedPageContent and RestrictedPageTitle contents.
    
    scenario II)
    
    1) Logout opac page
    2) Add some content in RestrictedPageContent and RestrictedPageTitle sysprefs.
    3) Add a diffrent ip.
    4) Validate: cgi-bin/koha/opac-restrictedpage.pl shows a login page.
    5) Validate: cgi-bin/koha/opac-restrictedpage.pl shows a page with RestrictedPageContent and RestrictedPageTitle contents after login.
    
    	new file:   installer/data/mysql/atomicupdate/Bug13485_RestrictedSitesPage.sql
    	modified:   installer/data/mysql/sysprefs.sql
    	modified:   koha-tmpl/intranet-tmpl/prog/en/modules/admin/preferences/opac.pref
    	renamed:    koha-tmpl/opac-tmpl/bootstrap/en/modules/opac-proxypage.tt -> koha-tmpl/opac-tmpl/bootstrap/en/modules/opac-restrictedpage.tt
    	renamed:    opac/opac-proxypage.pl -> opac/opac-restrictedpage.pl
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@unc.edu.ar>

---
+++ installer/data/mysql/atomicupdate/Bug13485_RestrictedSitesPage.sql
@@ -0,0 +1,4 @@
+INSERT INTO systempreferences (variable,value,options,explanation,type)
+VALUES ('RestrictedPageLocalIPs','',NULL,'Beginning of IP addresses considered as local (comma separated ex: \"127.0.0,127.0.2\")','Free'),
+('RestrictedPageContent','',NULL,'HTML content of the restricted page','TextArea'),
+('RestrictedPageTitle','',NULL,'Title of the restricted page (breadcrumb and header)','Free')
---
+++ opac/opac-restrictedpage.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/perl
+
+# Copyright Solutions inLibro inc 2014
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version .
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use Modern::Perl;
+
+use CGI;
+use C4::Auth;
+use C4::Output;
+
+my $localNetwork  = C4::Context->preference('RestrictedPageLocalIPs');
+my $userIP = $ENV{'REMOTE_ADDR'};
+
+my $withinNetwork = 0;
+foreach my $IPRange ( split( ',', $localNetwork ) )
+{
+    $withinNetwork = ( $userIP =~ /^$IPRange/ );
+    last if $withinNetwork;
+}
+
+my $query = new CGI;
+my ( $template, $borrowernumber, $cookie ) = get_template_and_user(
+    {
+        template_name   => "opac-restrictedpage.tt",
+        query           => $query,
+        type            => "opac",
+        authnotrequired => $withinNetwork,
+        flagsrequired   => { borrow => 1 },
+        debug           => 1,
+    }
+);
+
+$template->param(
+                    RestrictedPageContent => C4::Context->preference('RestrictedPageContent'),
+                    RestrictedPageTitle => C4::Context->preference('RestrictedPageTitle')
+                );
+
+output_html_with_http_headers $query, $cookie, $template->output;
