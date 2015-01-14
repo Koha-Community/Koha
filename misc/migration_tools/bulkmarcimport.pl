002b79c200f03adc162a5f59b988e8eb6c65e229
Thomas <tomsStudy@gmail.com>
2015-01-14 20:20:36 +0000

    Bug 13531: QA follow up
    A minor QA comment.
    
    ::: misc/migration_tools/bulkmarcimport.pl
    @@ +271,5 @@
    >          my ( $error, $results, $totalhits ) = C4::Search::SimpleSearch( $query, 0, 3, [$server] );
    > +        # changed to warn so able to continue with one broken record
    > +        if ( defined $error ) {
    > +            warn "unable to search the database for duplicates : $error";
    > +            next;
    
    For consistency with the rest of the script, should this perhaps be:
    
    next RECORD;
    
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@gmail.com>

--- misc/migration_tools/bulkmarcimport.pl
+++ misc/migration_tools/bulkmarcimport.pl
@@ -272,7 +272,7 @@ RECORD: while (  ) {
         # changed to warn so able to continue with one broken record
         if ( defined $error ) {
             warn "unable to search the database for duplicates : $error";
-            next;
+            next RECORD;
         }
         $debug && warn "$query $server : $totalhits";
         if ( $results && scalar(@$results) == 1 ) {
