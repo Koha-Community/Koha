761527b0267beedcd35dddde4388a855d35adbc3
Tomas Cohen Arazi <tomascohen@gmail.com>
2015-02-09 15:50:41 -0300

    Bug 13007: DBRev 3.19.00.009
    Signed-off-by: Tomas Cohen Arazi <tomascohen@gmail.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -9732,7 +9732,7 @@ if ( CheckVersion($DBversion) ) {
     SetVersion($DBversion);
 }
 
-$DBversion = "3.19.00.XXX";
+$DBversion = "3.19.00.009";
 if ( CheckVersion($DBversion) ) {
     $dbh->do(q|
         UPDATE suggestions s SET s.budgetid = NULL
