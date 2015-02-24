06608a24408c74d0975665b24c1e80fec5669385
Tomas Cohen Arazi <tomascohen@gmail.com>
2015-02-24 11:27:03 -0300

    Bug 13523: DBRev 3.19.00.012
    Signed-off-by: Tomas Cohen Arazi <tomascohen@gmail.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -9779,7 +9779,7 @@ if ( CheckVersion($DBversion) ) {
     SetVersion ($DBversion);
 }
 
-$DBversion = "3.19.00.XXX";
+$DBversion = "3.19.00.012";
 if(CheckVersion($DBversion)) {
     $dbh->do(q{
         ALTER TABLE biblioitems MODIFY COLUMN marcxml longtext
