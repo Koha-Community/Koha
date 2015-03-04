7a62fc2b2a180ce6748adfe32aad33065a955f16
Jonathan Druart <jonathan.druart@biblibre.com>
2015-03-04 12:26:34 +0100

    Bug 5338: Use insert ignore to avoid a warning if 'SER' already exists
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -10448,11 +10448,11 @@ if ( CheckVersion($DBversion) ) {
 $DBversion = "3.19.00.XXX";
 if ( CheckVersion($DBversion) ) {
     $dbh->do(q|
-        INSERT INTO authorised_values (category, authorised_value, lib) VALUES
+        INSERT IGNORE INTO authorised_values (category, authorised_value, lib) VALUES
         ('REPORT_GROUP', 'SER', 'Serials')
     |);
 
-    print "Upgrade to $DBversion done (Bug 5338 - Add Serial to the report groups)\n";
+    print "Upgrade to $DBversion done (Bug 5338 - Add Serial to the report groups if does not exist)\n";
     SetVersion ($DBversion);
 }
 
