5daea4197e488a0e571f925205a5ae9f22754de4
Tomas Cohen Arazi <tomascohen@gmail.com>
2015-03-05 15:49:25 +0100

    Bug 11395: DBRev 3.19.00.013
    Signed-off-by: Tomas Cohen Arazi <tomascohen@gmail.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -9794,7 +9794,7 @@ if(CheckVersion($DBversion)) {
 }
 
 
-$DBversion = "3.19.00.XXX";
+$DBversion = "3.19.00.013";
 if ( CheckVersion($DBversion) ) {
     $dbh->do(q|
         INSERT INTO permissions (module_bit, code, description) VALUES
