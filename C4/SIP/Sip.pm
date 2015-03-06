0817cf906f39d5b564f917ac4a2f0372fa332c43
Colin Campbell <colin.campbell@ptfs-europe.com>
2015-03-06 11:57:09 +0000

    Bug 13807 Rework main input loop in SIPServer
    Debugging various problems in SIPServer and control of it, found it
    could loop on unread buffers (e.g. the LF of a CRLF if it was only
    expecting CR) making it unresponsive to signals.
    Reworked the input loop with an eye to removing unnecessary whiles
    and replacing the while(1) by a while( connection valid)
    Enhanced the timeout code by wapping in an eval.
    Moved the logic from SIP_read_packet into the server itself
    Hopefully this makes the already baroque code easier to navigate
    and it did seem the server was the logical place for this
    Removed no longer iused SIP_read_packet from Sip.pm
    
    Signed-off-by: Srdjan <srdjan@catalyst.net.nz>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    (cherry picked from commit 94441be435d9be074fc0f93bbec2c2e9122a5d67)
    Signed-off-by: Frédéric Demians <f.demians@tamil.fr>

--- C4/SIP/Sip.pm
+++ C4/SIP/Sip.pm
@@ -22,14 +22,13 @@ BEGIN {
 	@ISA = qw(Exporter);
 
 	@EXPORT_OK = qw(y_or_n timestamp add_field maybe_add add_count
-		    denied sipbool boolspace write_msg read_SIP_packet
+		    denied sipbool boolspace write_msg
 		    $error_detection $protocol_version $field_delimiter
 		    $last_response);
 
 	%EXPORT_TAGS = (
 		    all => [qw(y_or_n timestamp add_field maybe_add
 			       add_count denied sipbool boolspace write_msg
-			       read_SIP_packet
 			       $error_detection $protocol_version
 			       $field_delimiter $last_response)]);
 }
@@ -152,59 +151,6 @@ sub sipbool {
 sub boolspace {
     my $bool = shift;
     return $bool ? 'Y' : ' ';
-}
-
-
-# read_SIP_packet($file)
-#
-# Read a packet from $file, using the correct record separator
-#
-sub read_SIP_packet {
-    my $record;
-    my $fh = shift or syslog("LOG_ERR", "read_SIP_packet: no filehandle argument!");
-    my $len1 = 999;
-
-    # local $/ = "\r";      # don't need any of these here.  use whatever the prevailing $/ is.
-    local $/ = "\015";    # proper SPEC: (octal) \015 = (hex) x0D = (dec) 13 = (ascii) carriage return
-    {    # adapted from http://perldoc.perl.org/5.8.8/functions/readline.html
-            undef $!;
-            $record = readline($fh);
-            if ( defined($record) ) {
-                while ( chomp($record) ) { 1; }
-                $len1 = length($record);
-                syslog( "LOG_DEBUG", "read_SIP_packet, INPUT MSG: '$record'" );
-                $record =~ s/^\s*[^A-z0-9]+//s; # Every line must start with a "real" character.  Not whitespace, control chars, etc. 
-                $record =~ s/[^A-z0-9]+$//s;    # Same for the end.  Note this catches the problem some clients have sending empty fields at the end, like |||
-                $record =~ s/\015?\012//g;      # Extra line breaks must die
-                $record =~ s/\015?\012//s;      # Extra line breaks must die
-                $record =~ s/\015*\012*$//s;    # treat as one line to include the extra linebreaks we are trying to remove!
-                while ( chomp($record) ) { 1; }
-
-                $record and last;    # success
-            }
-    }
-    if ($record) {
-        my $len2 = length($record);
-        syslog("LOG_INFO", "read_SIP_packet, INPUT MSG: '$record'") if $record;
-        ($len1 != $len2) and syslog("LOG_DEBUG", "read_SIP_packet, trimmed %s character(s) (after chomps).", $len1-$len2);
-    } else {
-        syslog("LOG_WARNING", "read_SIP_packet input %s, end of input.", (defined($record) ? "empty ($record)" : 'undefined'));
-    }
-    #
-    # Cen-Tec self-check terminals transmit '\r\n' line terminators.
-    # This is actually very hard to deal with in perl in a reasonable
-    # since every OTHER piece of hardware out there gets the protocol
-    # right.
-    # 
-    # The incorrect line terminator presents as a \r at the end of the
-    # first record, and then a \n at the BEGINNING of the next record.
-    # So, the simplest thing to do is just throw away a leading newline
-    # on the input.
-    #  
-    # This is now handled by the vigorous cleansing above.
-    # syslog("LOG_INFO", encode_utf8("INPUT MSG: '$record'")) if $record;
-    syslog("LOG_INFO", "INPUT MSG: '$record'") if $record;
-    return $record;
 }
 
 #
