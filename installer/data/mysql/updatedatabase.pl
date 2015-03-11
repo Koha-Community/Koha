c9e4c6b84edffc725e04d25e3ce271e4ec1dc280
Tomas Cohen Arazi <tomascohen@gmail.com>
2015-03-11 11:59:34 -0300

    Bug 12648: DBRev 3.19.00.014
    Signed-off-by: Tomas Cohen Arazi <tomascohen@gmail.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -9804,7 +9804,7 @@ if ( CheckVersion($DBversion) ) {
     SetVersion($DBversion);
 }
 
-$DBversion = "3.19.00.XXX";
+$DBversion = "3.19.00.014";
 if ( CheckVersion($DBversion) ) {
     $dbh->do(q|
         CREATE TABLE aqorder_users (
