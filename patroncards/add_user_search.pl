f6c750165de6a96b058dccd77b0000848cb3ef96
Jonathan Druart <jonathan.druart@biblibre.com>
2015-03-20 17:48:43 +0100

    Bug 13891: DataTables server-side processing - patron cards
    This third patch removes the previous way to search for patron cards users.
    
    Test plan:
    1/ Add items to a patron card batch (patroncards/edit-batch.pl?op=edit&element_id=X)
    2/ Add 1+ patrons to the batch
    3/ Confirm there is no regression
    
    Tested together with other patches.
    Signed-off-by: Marc Veron <veron@veron.ch>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@gmail.com>

---
+++ patroncards/add_user_search.pl
@@ -0,0 +1,51 @@
+#!/usr/bin/perl
+
+# This file is part of Koha.
+#
+# Copyright 2014 BibLibre
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+use CGI qw ( -utf8 );
+use C4::Auth;
+use C4::Output;
+use C4::Members;
+
+my $input = new CGI;
+
+my $dbh = C4::Context->dbh;
+
+my ( $template, $loggedinuser, $cookie, $staff_flags ) = get_template_and_user(
+    {   template_name   => "common/patron_search.tt",
+        query           => $input,
+        type            => "intranet",
+        authnotrequired => 0,
+        flagsrequired => { serials => 'routing' },
+    }
+);
+
+my $q = $input->param('q') || '';
+my $op = $input->param('op') || '';
+
+my $referer = $input->referer();
+
+$template->param(
+    view => ( $input->request_method() eq "GET" ) ? "show_form" : "show_results",
+    columns => ['cardnumber', 'name', 'category', 'branch', 'dateexpiry', 'borrowernotes', 'action'],
+    json_template => 'patroncards/tables/members_results.tt',
+    selection_type => 'add',
+);
+output_html_with_http_headers( $input, $cookie, $template->output );
