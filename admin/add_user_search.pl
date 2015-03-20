26393577676fba7ee9291efffed94dd3854e9d04
Jonathan Druart <jonathan.druart@biblibre.com>
2015-03-20 13:13:25 +0100

    Bug 13891: DataTables server-side processing - budget users
    This first patch remove the previous way to search for users to link to
    budgets.
    
    Test plan:
    1/ Edit or create a fund
    2/ Edit the owner of this fund a select a patron
    3/ Add 1+ users to the user lists of this fund
    
    Tested together with other patches.
    Signed-off-by: Marc Veron <veron@veron.ch>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@gmail.com>

---
+++ admin/add_user_search.pl
@@ -0,0 +1,60 @@
+#!/usr/bin/perl
+
+# This file is part of Koha.
+#
+# Copyright 2014 BibLibre
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+use CGI qw ( -utf8 );
+use C4::Auth;
+use C4::Output;
+use C4::Members;
+
+my $input = new CGI;
+
+my $dbh = C4::Context->dbh;
+
+my ( $template, $loggedinuser, $cookie, $staff_flags ) = get_template_and_user(
+    {   template_name   => "common/patron_search.tt",
+        query           => $input,
+        type            => "intranet",
+        authnotrequired => 0,
+        flagsrequired   => { acquisition => 'budget_modify'  },
+    }
+);
+
+my $q = $input->param('q') || '';
+my $op = $input->param('op') || '';
+my $selection_type = $input->param('selection_type') || 'add';
+
+my $referer = $input->referer();
+
+# If this script is called by acqui/basket.pl
+# the patrons to return should be superlibrarian or have the order_manage
+# acquisition flag.
+my $search_patrons_with_acq_perm_only =
+    ( $referer =~ m|admin/aqbudgets.pl| )
+        ? 1 : 0;
+
+$template->param(
+    patrons_with_acq_perm_only => $search_patrons_with_acq_perm_only,
+    view => ( $input->request_method() eq "GET" ) ? "show_form" : "show_results",
+    columns => ['cardnumber', 'name', 'branch', 'category', 'action'],
+    json_template => 'acqui/tables/members_results.tt',
+    selection_type => $selection_type,
+);
+output_html_with_http_headers( $input, $cookie, $template->output );
