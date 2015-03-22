f851a35af6e47515b482cfd6aefcf5128b7325ff
Chris Cormack <chrisc@catalyst.net.nz>
2015-03-22 18:41:24 +1300

    Bumping db version for 3.18.5 release

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -9725,6 +9725,12 @@ if(CheckVersion($DBversion)) {
     SetVersion($DBversion);
 }
 
+$DBversion = "3.18.05.000";
+if ( CheckVersion($DBversion) ) {
+    print "Upgrade to $DBversion done (3.18.5 release)\n";
+    SetVersion($DBversion);
+}
+
 =head1 FUNCTIONS
 
 =head2 TableExists($table)
