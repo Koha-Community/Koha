7e87829166342d20d0828b157ac138a6b0220c87
Robin Sheat <robin@catalyst.net.nz>
2015-03-25 17:29:14 +1300

    Bug 12478: fix issues caused by rebasing
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    Signed-off-by: Jesse Weaver <jweaver@bywatersolutions.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    
    Signed-off-by: Brendan Gallagher <brendan@bywatersolutions.com>

---
+++ Koha/SearchEngine/Zebra/QueryBuilder.pm
@@ -0,0 +1,56 @@
+package Koha::SearchEngine::Zebra::QueryBuilder;
+
+# This file is part of Koha.
+#
+# Copyright 2012 BibLibre
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use base qw(Class::Accessor);
+use Modern::Perl;
+use Moose::Role;
+use C4::Search;
+use C4::AuthoritiesMarc;
+
+sub build_query {
+    shift;
+    C4::Search::buildQuery @_;
+}
+
+sub build_query_compat {
+    # Because this passes directly on to C4::Search, we have no trouble being
+    # compatible.
+    build_query(@_);
+}
+
+sub build_authorities_query {
+    shift;
+    C4::AuthoritiesMarc::SearchAuthorities(@_);
+    return {
+        marclist     => $_[0],
+        and_or       => $_[1],
+        excluding    => $_[2],
+        operator     => $_[3],
+        value        => $_[4],
+        authtypecode => $_[5],
+        orderby      => $_[6],
+    };
+}
+
+sub build_authorities_query_compat {
+    # Pass straight through as well
+    build_authorities_query(@_);
+}
+
+1;
