a6c9bd0eb55c32d5632625144775271f20aa15f7
Jonathan Druart <jonathan.druart@biblibre.com>
2015-04-08 17:09:04 +0200

    Bug 9978: Replace license header with the correct license (GPLv3+)
    Signed-off-by: Chris Nighswonger <cnighswonger@foundations.edu>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@gmail.com>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>
    
    http://bugs.koha-community.org/show_bug.cgi?id=9987
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@gmail.com>

--- serials/reorder_members.pl
+++ serials/reorder_members.pl
@@ -1,18 +1,18 @@
 #!/usr/bin/perl
 # This file is part of Koha.
 #
-# Koha is free software; you can redistribute it and/or modify it under the
-# terms of the GNU General Public License as published by the Free Software
-# Foundation; either version 2 of the License, or (at your option) any later
-# version.
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
 #
-# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License along
-# with Koha; if not, write to the Free Software Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
 
 # Routing.pl script used to create a routing list for a serial subscription
 # In this instance it is in fact a setting up of a list of reserves for the item
