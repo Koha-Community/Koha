487dd985eb86c5555c941c23e82049794aa2ee33
Mark Tompsett <mtompset@hotmail.com>
2015-04-15 12:33:29 -0400

    Bug 14001: Inventory has bad $_ references
    After receiving an error while attempt a simple inventory run,
    Two lines were changed from:
        ...$_->...
    to
        ...$item->...
    since the loop variable is $item. And $_ is not set to the
    expected hash reference, when there is a loop variable.
    
    This also helps explain the "Why are there blank dates on my
    last seen field?" problem that has been mentioned by users.
    
    TEST PLAN
    ---------
     1) Apply this patch after a reset to master.
     2) Log in to staff client
     3) Add one item via z39.50, setting barcode to a known value (BARCODE1)
     4) Wait for the reindex
     5) Home -> Tools -> Inventory/Stocktaking
     6) Browse for a file with the barcode in it
     7) Set the library dropdown to the library branch of the added item.
     8) Check 'Compare barcodes list to results:'
     9) Click 'Submit'
        -- This should not die under plack.
           This should not generate blank last seen dates.
           The last seen dates should be as expected.
    10) run koha qa test tools
    11) Confirm the two change point correspond to the two change points
        in the patch which shall not be pushed to master.
    
    The test result comply with expected outcome outlined in test plan.
    
    Signed-off-by: Indranil Das Gupta (L2C2 Technologies) <indradg@gmail.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@koha-community.org>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@unc.edu.ar>
    (cherry picked from commit 3ebc081962247ce0c598da810451c459909842bc)
    Signed-off-by: Chris Cormack <chrisc@catalyst.net.nz>

--- tools/inventory.pl
+++ tools/inventory.pl
@@ -279,7 +279,7 @@ if ( $compareinv2barcd ) {
     my $dls = output_pref( { dt => dt_from_string( $datelastseen ),
                              dateformat => 'iso' } );
     foreach my $item ( @$inventorylist ) {
-        my $cdls = output_pref( { dt => dt_from_string( $_->{datelastseen} ),
+        my $cdls = output_pref( { dt => dt_from_string( $item->{datelastseen} ),
                                   dateformat => 'iso' } );
         if ( $cdls lt $dls ) {
             $item->{problem} = 'missingitem';
@@ -326,7 +326,7 @@ foreach my $item ( @scanned_items ) {
     }
 
     # Modify date last seen for scanned items
-    ModDateLastSeen($_->{'itemnumber'});
+    ModDateLastSeen($item->{'itemnumber'});
     $moddatecount++;
 }
 
