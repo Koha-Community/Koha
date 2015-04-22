f4a46e8c3d90df38ef842b684d86ce4e7a0e1ce4
Fridolin Somers <fridolin.somers@biblibre.com>
2015-04-22 09:13:42 +0200

    Bug 13523 - DBrev 3.14.14.001

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -8083,7 +8083,7 @@ if ( CheckVersion($DBversion) ) {
     SetVersion($DBversion);
 }
 
-$DBversion = "3.19.00.XXX";
+$DBversion = "3.14.14.001";
 if(CheckVersion($DBversion)) {
     $dbh->do(q{
         ALTER TABLE biblioitems MODIFY COLUMN marcxml longtext
