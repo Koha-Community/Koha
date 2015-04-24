856861b47ee7e0658f790391fbf22af38e48616b
Tomas Cohen Arazi <tomascohen@gmail.com>
2015-04-24 10:03:17 -0300

    Bug 14030: DBRev 3.19.00.030
    Signed-off-by: Tomas Cohen Arazi <tomascohen@gmail.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -10192,6 +10192,71 @@ if ( CheckVersion($DBversion) ) {
     SetVersion($DBversion);
 }
 
+$DBversion = "3.19.00.030";
+if ( CheckVersion($DBversion) ) {
+    $dbh->do(q|
+UPDATE language_subtag_registry SET subtag = 'kn' WHERE subtag = 'ka' AND description = 'Kannada';
+    |);
+    $dbh->do(q|
+UPDATE language_rfc4646_to_iso639 SET rfc4646_subtag = 'kn' WHERE rfc4646_subtag = 'ka' AND iso639_2_code = 'kan';
+    |);
+    $dbh->do(q|
+UPDATE language_descriptions SET subtag = 'kn', lang = 'kn' WHERE subtag = 'ka' AND lang = 'ka' AND description = 'ಕನ್ನಡ';
+    |);
+    $dbh->do(q|
+UPDATE language_descriptions SET subtag = 'kn' WHERE subtag = 'ka' AND description = 'Kannada';
+    |);
+    $dbh->do(q|
+INSERT IGNORE INTO language_subtag_registry( subtag, type, description, added) VALUES ( 'ka', 'language', 'Georgian','2015-04-20');
+    |);
+    $dbh->do(q|
+DELETE FROM language_subtag_registry
+       WHERE NOT id IN
+         (SELECT id FROM
+           (SELECT MIN(id) as id,subtag,type,description,added
+            FROM language_subtag_registry
+            GROUP BY subtag,type,description,added)
+           AS subtable);
+    |);
+    $dbh->do(q|
+INSERT IGNORE INTO language_rfc4646_to_iso639(rfc4646_subtag,iso639_2_code) VALUES ( 'ka', 'geo');
+    |);
+    $dbh->do(q|
+DELETE FROM language_rfc4646_to_iso639
+       WHERE NOT id IN
+         (SELECT id FROM
+           (SELECT MIN(id) as id,rfc4646_subtag,iso639_2_code
+            FROM language_rfc4646_to_iso639
+            GROUP BY rfc4646_subtag,iso639_2_code)
+           AS subtable);
+    |);
+    $dbh->do(q|
+INSERT IGNORE INTO language_descriptions(subtag, type, lang, description) VALUES ( 'ka', 'language', 'ka', 'ქართული');
+    |);
+    $dbh->do(q|
+INSERT IGNORE INTO language_descriptions(subtag, type, lang, description) VALUES ( 'ka', 'language', 'en', 'Georgian');
+    |);
+    $dbh->do(q|
+INSERT IGNORE INTO language_descriptions(subtag, type, lang, description) VALUES ( 'ka', 'language', 'fr', 'Géorgien');
+    |);
+    $dbh->do(q|
+INSERT IGNORE INTO language_descriptions(subtag, type, lang, description) VALUES ( 'ka', 'language', 'de', 'Georgisch');
+    |);
+    $dbh->do(q|
+INSERT IGNORE INTO language_descriptions(subtag, type, lang, description) VALUES ( 'ka', 'language', 'es', 'Georgiano');
+    |);
+    $dbh->do(q|
+DELETE FROM language_descriptions
+       WHERE NOT id IN
+         (SELECT id FROM
+           (SELECT MIN(id) as id,subtag,type,lang,description
+            FROM language_descriptions GROUP BY subtag,type,lang,description)
+           AS subtable);
+    |);
+    print "Upgrade to $DBversion done (Bug 13606: Upgrade sessions.a_session to MEDIUMTEXT)\n";
+    SetVersion($DBversion);
+}
+
 # DEVELOPER PROCESS, search for anything to execute in the db_update directory
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
