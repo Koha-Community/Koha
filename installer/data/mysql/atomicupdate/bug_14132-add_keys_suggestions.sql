06017781ae19100673fb4f8ced6da793d424dcb4
Fridolin Somers <fridolin.somers@biblibre.com>
2015-05-04 13:49:53 +0200

    Bug 14132: suggestions table is missing indexes - atomicupdate
    Signed-off-by: Bernardo Gonzalez Kriegel <bgkriegel@gmail.com>
    Sorry but removed backticks
    No errors
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@gmail.com>

---
+++ installer/data/mysql/atomicupdate/bug_14132-add_keys_suggestions.sql
@@ -0,0 +1,3 @@
+ALTER IGNORE TABLE suggestions ADD KEY status (STATUS);
+ALTER IGNORE TABLE suggestions ADD KEY biblionumber (biblionumber);
+ALTER IGNORE TABLE suggestions ADD KEY branchcode (branchcode);
