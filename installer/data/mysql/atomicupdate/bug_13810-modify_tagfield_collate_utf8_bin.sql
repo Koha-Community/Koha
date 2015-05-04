0e74f0370624f9ab01fccfe27d37403ed1686ad9
Tomas Cohen Arazi <tomascohen@gmail.com>
2015-05-04 11:41:33 -0300

    Bug 13810: DBRev 3.19.00.037
    Signed-off-by: Tomas Cohen Arazi <tomascohen@gmail.com>

--- installer/data/mysql/atomicupdate/bug_13810-modify_tagfield_collate_utf8_bin.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE marc_subfield_structure MODIFY COLUMN tagsubfield varchar(1) COLLATE utf8_bin NOT NULL DEFAULT ''
--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -10363,6 +10363,16 @@ if ( CheckVersion($DBversion) ) {
     SetVersion ($DBversion);
 }
 
+$DBversion = "3.19.00.037";
+if ( CheckVersion($DBversion) ) {
+    $dbh->do(q|
+        ALTER TABLE marc_subfield_structure
+        MODIFY COLUMN tagsubfield varchar(1) COLLATE utf8_bin NOT NULL DEFAULT ''
+    |);
+    print "Upgrade to $DBversion done (Bug 13810: Change collate for tagsubfield (utf8_bin))\n";
+    SetVersion ($DBversion);
+}
+
 # DEVELOPER PROCESS, search for anything to execute in the db_update directory
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
