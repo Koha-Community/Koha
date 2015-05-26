0c58b58a313e81bf844685b62fa9997d43817816
Tomas Cohen Arazi <tomascohen@gmail.com>
2015-05-26 10:41:30 -0300

    DBRev 3.21.00.000: El tiempo vuela
    Signed-off-by: Tomas Cohen Arazi <tomascohen@gmail.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -10462,6 +10462,12 @@ if ( CheckVersion($DBversion) ) {
     SetVersion ($DBversion);
 }
 
+$DBversion = "3.21.00.000";
+if ( CheckVersion($DBversion) ) {
+    print "Upgrade to $DBversion done (El tiempo vuela, un nuevo ciclo comienza.)\n";
+    SetVersion ($DBversion);
+}
+
 # DEVELOPER PROCESS, search for anything to execute in the db_update directory
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
