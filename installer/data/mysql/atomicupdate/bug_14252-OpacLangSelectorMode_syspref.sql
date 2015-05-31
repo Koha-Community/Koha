8da9d099c6a3455edd3c0aee2253b68050d3c596
Indranil Das Gupta <indradg@gmail.com>
2015-06-01 05:26:31 +0530

    Bug 14252: (Followup) Fixed issues pointed to in comment #40
    Based on Mark's inputs in comment #40 -
    
    [1] selected 'mast' (short of 'masthead') and updated :
       (a) bug_14252-OpacLangSelectorMode_syspref.sql
       (b) sysprefs.sql
       (c) opac.pref
    [2] Reduced the calls to Koha.Preference() from :
       (a) masthead.inc
       (b) opac-bottom.inc
    
    After applying this patch, update the database once for it to pick
    up the new option values -'both|mast|foot'.
    
    Signed-off-by: Mark Tompsett <mtompset@hotmail.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@koha-community.org>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- installer/data/mysql/atomicupdate/bug_14252-OpacLangSelectorMode_syspref.sql
+++ installer/data/mysql/atomicupdate/bug_14252-OpacLangSelectorMode_syspref.sql
@@ -1,4 +1,4 @@
 --
 -- Adds OpacLangSelectorMode syspref for bug 14252
 --
-INSERT IGNORE INTO systempreferences (variable,value,options,explanation,type) VALUES ('OpacLangSelectorMode','both','both|head|foot','Select the location to display the language selector','Choice');
+INSERT IGNORE INTO systempreferences (variable,value,options,explanation,type) VALUES ('OpacLangSelectorMode','both','both|mast|foot','Select the location to display the language selector','Choice');
