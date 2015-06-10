bc3b0daaa42122045775c94b6fae59af63009465
Robin Sheat <robin@catalyst.net.nz>
2015-06-10 14:35:33 +1200

    Bug 12478: change the schema for storing mappings
    This makes the mapping schema a proper many-to-many relational
    structure, which is a little more fiddly to work with, but will make
    things like editors for it easier to write.
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    Signed-off-by: Jesse Weaver <jweaver@bywatersolutions.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    
    Signed-off-by: Brendan Gallagher <brendan@bywatersolutions.com>

---
+++ Koha/Schema/Result/SearchMarcToField.pm
@@ -0,0 +1,99 @@
+use utf8;
+package Koha::Schema::Result::SearchMarcToField;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+Koha::Schema::Result::SearchMarcToField
+
+=cut
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+=head1 TABLE: C<search_marc_to_field>
+
+=cut
+
+__PACKAGE__->table("search_marc_to_field");
+
+=head1 ACCESSORS
+
+=head2 search_marc_map_id
+
+  data_type: 'integer'
+  is_foreign_key: 1
+  is_nullable: 0
+
+=head2 search_field_id
+
+  data_type: 'integer'
+  is_foreign_key: 1
+  is_nullable: 0
+
+=cut
+
+__PACKAGE__->add_columns(
+  "search_marc_map_id",
+  { data_type => "integer", is_foreign_key => 1, is_nullable => 0 },
+  "search_field_id",
+  { data_type => "integer", is_foreign_key => 1, is_nullable => 0 },
+);
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</search_marc_map_id>
+
+=item * L</search_field_id>
+
+=back
+
+=cut
+
+__PACKAGE__->set_primary_key("search_marc_map_id", "search_field_id");
+
+=head1 RELATIONS
+
+=head2 search_field
+
+Type: belongs_to
+
+Related object: L<Koha::Schema::Result::SearchField>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "search_field",
+  "Koha::Schema::Result::SearchField",
+  { id => "search_field_id" },
+  { is_deferrable => 1, on_delete => "RESTRICT", on_update => "RESTRICT" },
+);
+
+=head2 search_marc_map
+
+Type: belongs_to
+
+Related object: L<Koha::Schema::Result::SearchMarcMap>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "search_marc_map",
+  "Koha::Schema::Result::SearchMarcMap",
+  { id => "search_marc_map_id" },
+  { is_deferrable => 1, on_delete => "RESTRICT", on_update => "RESTRICT" },
+);
+
+
+# Created by DBIx::Class::Schema::Loader v0.07042 @ 2015-06-10 14:32:07
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:ZNdjnjdGpK+5P478RkCWnA
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;
