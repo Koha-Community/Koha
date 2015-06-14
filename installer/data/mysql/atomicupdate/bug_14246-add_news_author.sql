b0f9b34a542ea1d8a339a9170bff78e080aac009
Indranil Das Gupta <indradg@gmail.com>
2015-06-14 17:39:52 +0530

    Bug 14246: (fix) correct ALTER TABLE mysql syntax
    Address bug 14246 comment 13 remark - correcting MySQL ALTER TABLE
    syntax.
    
    Signed-off-by: Nick Clemens <nick@quecheelibrary.org>
    
    Signed-off-by: Mark Tompsett <mtompset@hotmail.com>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- installer/data/mysql/atomicupdate/bug_14246-add_news_author.sql
+++ installer/data/mysql/atomicupdate/bug_14246-add_news_author.sql
@@ -1,2 +1,2 @@
-ALTER TABLE `opac_news` ADD `borrowernumber` int(11) AFTER `number` default NULL;
+ALTER TABLE `opac_news` ADD `borrowernumber` int(11) default NULL AFTER `number`;
 ALTER TABLE `opac_news` ADD CONSTRAINT `borrowernumber_fk` FOREIGN KEY (`borrowernumber`) REFERENCES `borrowers`(`borrowernumber`) ON DELETE SET NULL ON UPDATE CASCADE;
