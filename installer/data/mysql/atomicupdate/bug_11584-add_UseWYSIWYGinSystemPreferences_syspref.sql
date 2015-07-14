acc8de8214c6f8c342eb170c9ddd2be63c16354b
David Cook <dcook@prosentient.com.au>
2015-07-14 17:02:17 +1000

    Bug 11584: Introduce UseWYSIWYGinSystemPreferences syspref
    This patch adds a system preference "UseWYSIWYGinSystemPreferences" to the Staff Client tab.
    
    By default, it is off, which means that the TinyMCE WYSIWYG editor won't be shown for system
    preferences with a type of "htmlarea".
    
    However, when it's on, it will show the editor for "Local Use" preferences with a "htmlarea"
    type, and for other system preferences in the "Opac", "Circulation", and "Staff Client" tabs,
    which I have re-assigned. (Basically, I grepped for HTML and changed the type for all
    the system preferences I found except for "IntranetNav", "OpacCustomSearch", and "OPACSearchForTitleIn",
    as a WYSIWYG editor would potentially break the output for these system preferences or
    add no value to them...)
    
    _TEST PLAN_
    
    0) Run `perl installer/data/mysql/updatedatabase.pl` after setting your environmental variables
    1) Check the Opac tab to make sure that the WYSIWYG is nowhere to be seen
    2) Change the "UseWYSIWYGinSystemPreferences" preference in "Staff Client" to "Show"
    3) Refresh the Opac tab and notice that many system preferences now have a WYSIWYG editor
    
    4) Try typing some text into these fields
    5) Note that it gets marked as "modified"
    6) Save the preference, and refresh the page
    7) Note that the content has been saved
    8) Take a look at how it's rendered on the actual webpage!
    
    Signed-off-by: Martin Persson <xarragon@gmail.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

---
+++ installer/data/mysql/atomicupdate/bug_11584-add_UseWYSIWYGinSystemPreferences_syspref.sql
@@ -0,0 +1,1 @@
+INSERT IGNORE INTO systempreferences (variable,value,explanation,options,type) VALUES ('UseWYSIWYGinSystemPreferences','0','','Show WYSIWYG editor when editing certain HTML system preferences.','YesNo');
--- installer/data/mysql/sysprefs.sql
+++ installer/data/mysql/sysprefs.sql
@@ -474,6 +474,7 @@ INSERT INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `
 ('UseKohaPlugins','0','','Enable or disable the ability to use Koha Plugins.','YesNo'),
 ('UseQueryParser','0',NULL,'If enabled, try to use QueryParser for queries.','YesNo'),
 ('UseTransportCostMatrix','0','','Use Transport Cost Matrix when filling holds','YesNo'),
+('UseWYSIWYGinSystemPreferences','0','','Show WYSIWYG editor when editing certain HTML system preferences.','YesNo'),
 ('viewISBD','1','','Allow display of ISBD view of bibiographic records','YesNo'),
 ('viewLabeledMARC','0','','Allow display of labeled MARC view of bibiographic records','YesNo'),
 ('viewMARC','1','','Allow display of MARC view of bibiographic records','YesNo'),
