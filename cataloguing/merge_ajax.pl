a6539ea73d87fbfcd0343ec17e705e198ac24b93
Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
2015-07-22 14:19:09 +0200

    Bug 14588: Simplify merge_ajax.pl
    This script does not need a few modules:
        IO::File; CGI::Session; C4::UploadedFile.
    Warnings can be enabled (with Modern::Perl).
    Indirect object syntax replaced for CGI and CGI::Cookie.
    Moved the $reply line upwards (not needed twice anymore).
    
    Test plan:
    [1] Log in as staff user. Run the URL cataloguing/merge_ajax.pl.
        You should see the JSON reponse in the browser.
    [2] Logout. Run the URL again. Blank screen.
    [3] Go to addbooks.pl (Cataloging Search). Search something, select two
        biblios and click Merge selected, etc.
    
    Signed-off-by: Joonas Kylmala <j.kylmala@gmail.com>
    
    Signed-off-by: Mark Tompsett <mtompset@hotmail.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@unc.edu.ar>

--- cataloguing/merge_ajax.pl
+++ cataloguing/merge_ajax.pl
@@ -1,29 +1,25 @@
 #!/usr/bin/perl
 
-use strict;
-#use warnings; FIXME - Bug 2505
+use Modern::Perl;
 
-# standard or CPAN modules used
-use IO::File;
 use CGI qw ( -utf8 );
-use CGI::Session;
+use CGI::Cookie; # need to check cookies before CGI parses the POST request
+use JSON;
+
 use C4::Context;
 use C4::Biblio;
 use C4::Auth qw/check_cookie_auth/;
-use C4::UploadedFile;
-use JSON;
-use CGI::Cookie; # need to check cookies before
-                 # having CGI parse the POST request
 
-my %cookies = fetch CGI::Cookie;
-my ($auth_status, $sessionID) = check_cookie_auth($cookies{'CGISESSID'}->value, { editcatalogue => 'edit_catalogue' });
+my %cookies = CGI::Cookie->fetch;
+my ( $auth_status, $sessionID ) = check_cookie_auth(
+    $cookies{'CGISESSID'}->value, { editcatalogue => 'edit_catalogue' },
+);
+my $reply = CGI->new;
 if ($auth_status ne "ok") {
-    my $reply = CGI->new("");
     print $reply->header(-type => 'text/html');
     exit 0;
 } 
 
-my $reply = new CGI;
 my $framework = $reply->param('frameworkcode');
 my $tagslib = GetMarcStructure(1, $framework);
 print $reply->header(-type => 'text/html');
