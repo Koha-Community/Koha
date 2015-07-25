f9aacc69ad8cf778fd2b1c49384b01545bafb67b
Mark Tompsett <mtompset@hotmail.com>
2015-07-25 04:24:42 -0400

    Bug 4925: Remove Smithsonian as a delivered z39.50 target [3.16.x]
    Removes the Smithsonian as a target installed with the
    sample data during installation.
    
    Also adds the newer LOC authority targets to files where
    they were missing.
    
    To test:
    - Verify the Smithsonian has been removed from all
      translated installers
    - Verify the files are still valid SQL and install
      correctly
    
    NOTE: There was tiny scope creep which included ensuring
          there were two Authority z39.50 servers as well.
          Text files properly reflect the removal.
          SQL 'source' of SQL files worked properly.
          Was able to Z39.50 search for all of the 'en'.
    
    Signed-off-by: Mark Tompsett <mtompset@hotmail.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@koha-community.org>
    Signed-off-by: Mason James <mtj@kohaaloha.com>

--- installer/data/mysql/de-DE/optional/sample_z3950_servers.sql
+++ installer/data/mysql/de-DE/optional/sample_z3950_servers.sql
@@ -3,5 +3,4 @@ INSERT INTO z3950servers
 ('lx2.loc.gov',210,'LCDB','','','LIBRARY OF CONGRESS',1,1,'USMARC','utf8','biblio'),
 ('lx2.loc.gov',210,'NAF','','','LIBRARY OF CONGRESS NAMES',1,1,'USMARC','utf8','authority'),
 ('lx2.loc.gov',210,'SAF','','','LIBRARY OF CONGRESS SUBJECTS',1,2,'USMARC','utf8','authority'),
-('clio-db.cc.columbia.edu',7090,'voyager','','','COLUMBIA UNIVERSITY',0,0,'USMARC','MARC-8','biblio'),
-('siris-libraries.si.edu',210,'Default','','','SMITHSONIAN INSTITUTION LIBRARIES',0,0,'USMARC','MARC-8','biblio'); 
+('clio-db.cc.columbia.edu',7090,'voyager','','','COLUMBIA UNIVERSITY',0,0,'USMARC','MARC-8','biblio');
