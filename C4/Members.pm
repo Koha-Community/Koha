eea56d412461d9b673306697dcf5335d8686afc8
Joonas Kylmälä <j.kylmala@gmail.com>
2015-08-17 12:40:05 +0000

    Bug 14687: Patron's transaction history changes items' order after paying fines.
    Sorts Patron's accounting data consistently from newest to oldest.
    It doesn't depend on anymore to timestamp (which can be same for multiple
    entries) from database but instead uses accountline's id to sort.
    
    Signed-off-by: Liz Rea <liz@catalyst.net.nz>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    (cherry picked from commit 307fe0ac533c2dc8116a183d104c864717a91fc5)
    Signed-off-by: Frédéric Demians <f.demians@tamil.fr>

--- C4/Members.pm
+++ C4/Members.pm
@@ -1168,7 +1168,7 @@ sub GetMemberAccountRecords {
                         SELECT * 
                         FROM accountlines 
                         WHERE borrowernumber=?);
-    $strsth.=" ORDER BY date desc,timestamp DESC";
+    $strsth.=" ORDER BY accountlines_id desc";
     my $sth= $dbh->prepare( $strsth );
     $sth->execute( $borrowernumber );
 
