f4ba93afd2241bf7689199674c3596135996971f
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2015-08-18 18:40:31 +0100

    Bug 14584: Encoding broken with system preferences
    The pref are double encoded.
    
    From bug 12411:
    "This will fix the encoding issues existing in 3.14 and 3.16.
    The issue still appear in master but is fixed by bug 11944."
    
    They should not have been pushed together.
    
    Test plan:
    1) You need an accentuated language installed.
    2) Go to NoLoginInstructions pref, and add some accentuated message like
    "Hélo médames é mècieüx"
    3) refresh opac home page, message should be gibberish
    4) apply patch.  Message should be ok now.
    
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    Works as expected.
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@unc.edu.ar>
    (cherry picked from commit 813a047e236947a815c4be3c3dde38f19844b962)
    Signed-off-by: Chris Cormack <chris@bigballofwax.co.nz>
    (cherry picked from commit 270187404e87bdae985e70a590834ed2dc5d49c7)
    Signed-off-by: Liz Rea <wizzyrea@gmail.com>

--- Koha/Template/Plugin/Koha.pm
+++ Koha/Template/Plugin/Koha.pm
@@ -18,7 +18,6 @@ package Koha::Template::Plugin::Koha;
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 use Modern::Perl;
-use Encode qw( encode );
 
 use base qw( Template::Plugin );
 
@@ -42,7 +41,7 @@ is necessary.
 
 sub Preference {
     my ( $self, $pref ) = @_;
-    return encode('UTF-8', C4::Context->preference( $pref ) );
+    return C4::Context->preference( $pref );
 }
 
 1;
