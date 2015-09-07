8dfde8b85a6eeea4db4f79e35d6540d0bfc9cdf4
Tomas Cohen Arazi <tomascohen@theke.io>
2015-09-07 14:07:47 -0300

    Bug 14472: DBRev 3.21.00.026
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- installer/data/mysql/atomicupdate/bug_14472_Wrong_ISSN_search_index_in_record_matching_rules.sql
+++
@@ -1,1 +0,0 @@
-UPDATE matchpoints SET search_index ='issn' where matcher_id = (SELECT matcher_id FROM marc_matchers WHERE code = 'ISSN');
--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -10857,6 +10857,17 @@ if ( CheckVersion($DBversion) ) {
     SetVersion($DBversion);
 }
 
+$DBversion = "3.21.00.026";
+if ( CheckVersion($DBversion) ) {
+    $dbh->do(q{
+        UPDATE matchpoints
+        SET search_index='issn'
+        WHERE matcher_id=(SELECT matcher_id FROM marc_matchers WHERE code = 'ISSN')
+    });
+    print "Upgrade to $DBversion done (Bug 14472: Wrong ISSN search index in record matching rules)\n";
+    SetVersion($DBversion);
+}
+
 # DEVELOPER PROCESS, search for anything to execute in the db_update directory
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
