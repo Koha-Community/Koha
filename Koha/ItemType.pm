db2a1aa0dda1c786c34c3cf239fccfcaedefd094
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2015-09-15 17:02:28 +0100

    Bug 14828: Add Koha::ItemType[s] classes
    Signed-off-by: Marc Véron <veron@veron.ch>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    
    Signed-off-by: Brendan Gallagher brendan@bywatersolutions.com

---
+++ Koha/ItemType.pm
@@ -0,0 +1,90 @@
+package Koha::ItemType;
+
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use Modern::Perl;
+
+use Carp;
+
+use C4::Koha;
+use C4::Languages;
+use Koha::Database;
+use Koha::Localizations;
+
+use base qw(Koha::Object);
+
+=head1 NAME
+
+Koha::ItemType - Koha Item type Object class
+
+=head1 API
+
+=head2 Class Methods
+
+=cut
+
+=head3 image_location
+
+=cut
+
+sub image_location {
+    my ( $self, $interface ) = @_;
+    return C4::Koha::getitemtypeimagelocation( $interface, $self->SUPER::imageurl );
+}
+
+=head3 translated_description
+
+=cut
+
+sub translated_description {
+    my ( $self, $lang ) = @_;
+    $lang ||= C4::Languages::getlanguage;
+    my $translated_description = Koha::Localizations->search({
+        code => $self->itemtype,
+        entity => 'itemtypes',
+        lang => $lang
+    })->next;
+    return $translated_description || $self->description;
+}
+
+=head3 translated_descriptions
+
+=cut
+
+sub translated_descriptions {
+    my ( $self ) = @_;
+    my @translated_descriptions = Koha::Localizations->search(
+        {   entity => 'itemtypes',
+            code   => $self->itemtype,
+        }
+    );
+    return [ map {
+        {
+            lang => $_->lang,
+            translation => $_->translation,
+        }
+    } @translated_descriptions ];
+}
+
+=head3 type
+
+=cut
+
+sub type {
+    return 'Itemtype';
+}
+
+1;
