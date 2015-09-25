82539e86502fb618caf75281aed9fb4fb0f9a090
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2015-09-25 15:48:50 +0100

    Bug 12478: Add Koha::SearchField[s] and Koha::SearchMarcMap[s] classes
    This was initially submited on bug 14899, but we will need it here.
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    Signed-off-by: Jesse Weaver <jweaver@bywatersolutions.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

---
+++ Koha/SearchMarcMap.pm
@@ -0,0 +1,50 @@
+package Koha::SearchMarcMap;
+
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use Modern::Perl;
+
+use Carp;
+
+use Koha::Database;
+
+use base qw(Koha::Object);
+
+=head1 NAME
+
+Koha::SearchMarcMap - Koha SearchMarcMap Object class
+
+=head1 API
+
+=head2 Class Methods
+
+=cut
+
+sub add_to_search_fields {
+    my(  $self, $params) = @_;
+    $self->_result->add_to_search_fields($params);
+
+}
+
+=head3 type
+
+=cut
+
+sub type {
+    return 'SearchMarcMap';
+}
+
+1;
