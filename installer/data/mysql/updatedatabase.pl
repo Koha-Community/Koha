db687428a5b422d674b7cf7f6e92f1d22bffec74
Tomas Cohen Arazi <tomascohen@theke.io>
2015-10-02 14:30:14 -0300

    Bug 14820: DBRev 3.20.04.001
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    (cherry picked from commit b8f0a595344d28d85383007138715d7db131f136)
    Signed-off-by: Chris Cormack <chrisc@catalyst.net.nz>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -10652,6 +10652,18 @@ if ( CheckVersion($DBversion) ) {
     SetVersion($DBversion);
 }
 
+$DBversion = "3.20.04.001";
+if ( CheckVersion($DBversion) ) {
+    $dbh->do(q{
+        INSERT IGNORE INTO systempreferences (variable,value,options,explanation,type)
+        VALUES
+            ('SMSSendPassword', '', '', 'Password used to send SMS messages', 'free'),
+            ('SMSSendUsername', '', '', 'Username/Login used to send SMS messages', 'free')
+    });
+    print "Upgrade to $DBversion done (Bug 14820: SMSSendUsername and SMSSendPassword are not listed in the system preferences)\n";
+    SetVersion($DBversion);
+}
+
 # DEVELOPER PROCESS, search for anything to execute in the db_update directory
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
