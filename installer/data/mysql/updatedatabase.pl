6f81f8a0e2309447acc6e5bb74f444102d8adf56
Tomas Cohen Arazi <tomascohen@theke.io>
2015-10-02 15:22:10 -0300

    Bug 10855: DBRev 3.21.00.032
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -10957,6 +10957,35 @@ if ( CheckVersion($DBversion) ) {
     SetVersion($DBversion);
 }
 
+$DBversion = "3.21.00.032";
+if ( CheckVersion($DBversion) ) {
+    $dbh->do(q{
+        CREATE TABLE additional_fields (
+            id int(11) NOT NULL AUTO_INCREMENT,
+            tablename varchar(255) NOT NULL DEFAULT '',
+            name varchar(255) NOT NULL DEFAULT '',
+            authorised_value_category varchar(16) NOT NULL DEFAULT '',
+            marcfield varchar(16) NOT NULL DEFAULT '',
+            searchable tinyint(1) NOT NULL DEFAULT '0',
+            PRIMARY KEY (id),
+            UNIQUE KEY fields_uniq (tablename,name)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+    });
+    $dbh->do(q{
+        CREATE TABLE additional_field_values (
+            id int(11) NOT NULL AUTO_INCREMENT,
+            field_id int(11) NOT NULL,
+            record_id int(11) NOT NULL,
+            value varchar(255) NOT NULL DEFAULT '',
+            PRIMARY KEY (id),
+            UNIQUE KEY field_record (field_id,record_id),
+            CONSTRAINT afv_fk FOREIGN KEY (field_id) REFERENCES additional_fields (id) ON DELETE CASCADE ON UPDATE CASCADE
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+    });
+    print "Upgrade to $DBversion done (Bug 10855: Additional fields for subscriptions)\n";
+    SetVersion($DBversion);
+}
+
 # DEVELOPER PROCESS, search for anything to execute in the db_update directory
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
