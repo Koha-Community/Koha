b8c51bff2421528d8d01a011816b3c4e34f7a3fe
Tomas Cohen Arazi <tomascohen@theke.io>
2015-10-19 12:48:36 -0300

    Bug 13636: DBRev 3.21.00.036
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -11095,6 +11095,17 @@ if ( CheckVersion($DBversion) ) {
     SetVersion ($DBversion);
 }
 
+$DBversion = "3.21.00.036";
+if ( CheckVersion($DBversion) ) {
+   $dbh->do(q{
+       ALTER TABLE authorised_values_branches
+         CHANGE av_id av_id INT( 11 ) NOT NULL,
+         CHANGE branchcode branchcode VARCHAR( 10 ) NOT NULL
+   });
+   print "Upgrade to $DBversion done (Bug 10363: There is no package for authorised values)\n";
+   SetVersion($DBversion);
+}
+
 # DEVELOPER PROCESS, search for anything to execute in the db_update directory
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
