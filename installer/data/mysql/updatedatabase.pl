9bae51666478a7f224611f46c4708d3b7d8c6e9b
Tomas Cohen Arazi <tomascohen@theke.io>
2015-10-27 12:07:27 -0300

    Bug 10120: (QA followup) avoid raising warnings on upgrade
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -7019,13 +7019,11 @@ if(CheckVersion($DBversion)) {
 
 $DBversion = "3.13.00.006";
 if ( CheckVersion($DBversion) ) {
-    $dbh->do(
-        q{
-INSERT INTO systempreferences (variable,value,explanation,options,type) VALUES ('CalculateFinesOnReturn','1','Switch to control if overdue fines are calculated on return or not', '', 'YesNo');
-}
-    );
-    print
-"Upgrade to $DBversion done (Bug 10120 - Fines on item return controlled by a systempreference)\n";
+    $dbh->do(q{
+        INSERT IGNORE INTO systempreferences (variable,value,explanation,options,type)
+        VALUES ('CalculateFinesOnReturn','1','Switch to control if overdue fines are calculated on return or not', '', 'YesNo')
+    });
+    print "Upgrade to $DBversion done (Bug 10120: Fines on item return controlled by a systempreference)\n";
     SetVersion($DBversion);
 }
 
