c8afcd1311031b779337c30230280c67c4be9d1e
Frédéric Demians <f.demians@tamil.fr>
2015-12-05 08:29:57 +0100

    Revert "Bug 11190: sitemap.pl -- Generate a Catalog sitemap"
    This reverts commit e0a8173d10cc0f307dd95118e294c727c285e142.
    
    This feature introduces new dependencies, and make UT failing without them,
    which prevents package building.
    
    Signed-off-by: Frédéric Demians <f.demians@tamil.fr>

--- C4/Installer/PerlDependencies.pm
+++ C4/Installer/PerlDependencies.pm
@@ -747,11 +747,6 @@ our $PERL_DEPS = {
         'required' => '0',
         'min_ver'  => '0.614',
     },
-    'XML::Writer' => {
-        'usage'    => 'Command line scripts',
-        'required' => '0',
-        'min_ver'  => '0.614',
-    },
 };
 
 1;
