5fc9a42c7c7a89a7ddd91b73639654e16da29980
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2015-12-15 11:41:51 +0000

    Bug 15380: Use Koha::Authority::Type[s] in admin script
    This path rewrites the admin/authtypes.pl script to use the new modules
    instead of executing the SQL queries from the script.
    
    Test plan:
    1/ Create 1+ authority types
    2/ Update one
    3/ Delete it
    4/ Create an authority record using this authority type.
    5/ Try to delete the authority type
    Here there is a change in the behavior.
    Prior to this patch, the user was able to delete the authority type, now
    it's not.
    I think it was a bug, feel free to give your opinion if you disagree on
    this change.
    
    Signed-off-by: Marc Véron <veron@veron.ch>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    
    Signed-off-by: Brendan Gallagher brendan@bywatersolutions.com

--- admin/authtypes.pl
+++ admin/authtypes.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/perl
 
-# written 20/02/2002 by paul.poulain@free.fr
-
+# Copyright 2002 paul.poulain@biblibre.com
 # Copyright 2000-2002 Katipo Communications
+# Copyright 2015 Koha Development Team
 #
 # This file is part of Koha.
 #
@@ -25,91 +25,97 @@ use C4::Context;
 use C4::Auth;
 use C4::Output;
 
-sub StringSearch  {
-    my $string = shift || '';
-    my $dbh = C4::Context->dbh;
-    return $dbh->selectall_arrayref(q|
-        SELECT authtypecode, authtypetext, auth_tag_to_report, summary
-        FROM auth_types
-        WHERE (authtypecode like ?) ORDER BY authtypecode
-    |, { Slice => {} }, $string . "%" );
-}
+use Koha::Authorities;
+use Koha::Authority::Types;
 
-my $input = new CGI;
-my $script_name  = "/cgi-bin/koha/admin/authtypes.pl";
-my $searchfield  = $input->param('authtypecode');  # FIXME: Auth Type search not really implemented
+my $input        = new CGI;
 my $authtypecode = $input->param('authtypecode');
-my $op           = $input->param('op')     || '';
-my ($template, $borrowernumber, $cookie)
-    = get_template_and_user({template_name => "admin/authtypes.tt",
-                query => $input,
-                type => "intranet",
-                authnotrequired => 0,
-                flagsrequired => {parameters => 'parameters_remaining_permissions'},
-                debug => 1,
-                });
-
-$template->param(
-    script_name => $script_name,
-    ($op || 'else') => 1,
+my $op           = $input->param('op') || 'list';
+my @messages;
+my ( $template, $borrowernumber, $cookie ) = get_template_and_user(
+    {   template_name   => "admin/authtypes.tt",
+        query           => $input,
+        type            => "intranet",
+        authnotrequired => 0,
+        flagsrequired   => { parameters => 'parameters_remaining_permissions' },
+        debug           => 1,
+    }
 );
 
-my $dbh = C4::Context->dbh;
-
-# called by default. Used to create form to add or  modify a record
-if ($op eq 'add_form') {
-    #---- if primkey exists, it's a modify action, so read values to modify...
-    if ( defined $authtypecode) {
-        my $sth = $dbh->prepare("SELECT * FROM auth_types WHERE authtypecode=?");
-        $sth->execute($authtypecode);
-        my $data = $sth->fetchrow_hashref();
-        $template->param(
-            authtypecode       => $authtypecode,
-            authtypetext       => $data->{'authtypetext'},
-            auth_tag_to_report => $data->{'auth_tag_to_report'},
-            summary            => $data->{'summary'},
-        );
+if ( $op eq 'add_form' ) {
+    my $authority_type;
+    if ($authtypecode) {
+        $authority_type = Koha::Authority::Types->find($authtypecode);
     }
-                                                    # END $OP eq ADD_FORM
-################## ADD_VALIDATE ##################################
-# called by add_form, used to insert/modify data in DB
-} elsif ($op eq 'add_validate') {
-    my $sth = $input->param('modif') ? 
-            $dbh->prepare("UPDATE auth_types SET authtypetext=? ,auth_tag_to_report=?, summary=? WHERE authtypecode=?") :
-            $dbh->prepare("INSERT INTO auth_types SET authtypetext=?, auth_tag_to_report=?, summary=?, authtypecode=?") ;
-    $sth->execute($input->param('authtypetext'),$input->param('auth_tag_to_report'),$input->param('summary'),$input->param('authtypecode'));
-    print $input->redirect($script_name);    # FIXME: unnecessary redirect
-    exit;
-                                                    # END $OP eq ADD_VALIDATE
-################## DELETE_CONFIRM ##################################
-# called by default form, used to confirm deletion of data in DB
-} elsif ($op eq 'delete_confirm') {
-    #start the page and read in includes
-    my $sth=$dbh->prepare("SELECT count(*) AS total FROM auth_tag_structure WHERE authtypecode=?");
-    $sth->execute($authtypecode);
-    my $total = $sth->fetchrow_hashref->{total};
 
-    my $sth2 = $dbh->prepare("SELECT * FROM auth_types WHERE authtypecode=?");
-    $sth2->execute($authtypecode);
-    my $data = $sth2->fetchrow_hashref;
+    $template->param( authority_type => $authority_type );
+} elsif ( $op eq 'add_validate' ) {
+    my $authtypecode       = $input->param('authtypecode');
+    my $authtypetext       = $input->param('authtypetext');
+    my $auth_tag_to_report = $input->param('auth_tag_to_report');
+    my $summary            = $input->param('summary');
+    my $is_a_modif         = $input->param('is_a_modif');
 
-    $template->param(authtypecode => $authtypecode,
-                     authtypetext => $data->{'authtypetext'},
-                          summary => $data->{'summary'},
-                            total => $total);
-                                                    # END $OP eq DELETE_CONFIRM
-################## DELETE_CONFIRMED ##################################
-# called by delete_confirm, used to effectively confirm deletion of data in DB
-} elsif ($op eq 'delete_confirmed') {
-    #start the page and read in includes
-    my $sth=$dbh->prepare("DELETE FROM auth_types WHERE authtypecode=?");
-    $sth->execute(uc $input->param('authtypecode'));
-    print $input->redirect($script_name);   # FIXME: unnecessary redirect
-    exit;
-                                                    # END $OP eq DELETE_CONFIRMED
-################## DEFAULT ##################################
-} else { # DEFAULT
-    my $results = StringSearch($searchfield);
-    $template->param( loop => $results );
-} #---- END $OP eq DEFAULT
+    if ($is_a_modif) {
+        my $authority_type = Koha::Authority::Types->find($authtypecode);
+        $authority_type->authtypetext($authtypetext);
+        $authority_type->auth_tag_to_report($auth_tag_to_report);
+        $authority_type->summary($summary);
+        eval { $authority_type->store; };
+        if ($@) {
+            push @messages, { type => 'error', code => 'error_on_update' };
+        } else {
+            push @messages, { type => 'message', code => 'success_on_update' };
+        }
+    } else {
+        my $authority_type = Koha::Authority::Type->new(
+            {   authtypecode       => $authtypecode,
+                authtypetext       => $authtypetext,
+                auth_tag_to_report => $auth_tag_to_report,
+                summary            => $summary,
+            }
+        );
+        eval { $authority_type->store; };
+        if ($@) {
+            push @messages, { type => 'error', code => 'error_on_insert' };
+        } else {
+            push @messages, { type => 'message', code => 'success_on_insert' };
+        }
+    }
+    $op = 'list';
+
+} elsif ( $op eq 'delete_confirm' ) {
+    my $authority_type = Koha::Authority::Types->find($authtypecode);
+    my $authorities_using_it = Koha::Authorities->search( { authtypecode => $authtypecode } )->count;
+    $template->param(
+        authority_type       => $authority_type,
+        authorities_using_it => $authorities_using_it,
+    );
+} elsif ( $op eq 'delete_confirmed' ) {
+    my $authorities_using_it = Koha::Authorities->search( { authtypecode => $authtypecode } )->count;
+    if ( $authorities_using_it == 0 ) {
+        my $authority_type = Koha::Authority::Types->find($authtypecode);
+        my $deleted = eval { $authority_type->delete; };
+
+        if ( $@ or not $deleted ) {
+            push @messages, { type => 'error', code => 'error_on_delete' };
+        } else {
+            push @messages, { type => 'message', code => 'success_on_delete' };
+        }
+    } else {
+        push @messages, { type => 'error', code => 'error_on_delete' };
+    }
+    $op = 'list';
+}
+
+if ( $op eq 'list' ) {
+    my $authority_types = Koha::Authority::Types->search( {}, { order_by => ['authtypecode'] } );
+    $template->param( authority_types => $authority_types, );
+}
+
+$template->param(
+    messages => \@messages,
+    op       => $op,
+);
+
 output_html_with_http_headers $input, $cookie, $template->output;
