243662824ab6ab28aea269156f1e98bfaaf03e91
Bouzid Fergani <bouzid.fergani@inlibro.com>
2015-12-28 08:59:21 -0500

    Bug 12528 - fix problem when insert OPACEnhancedMessagingPreferences
    Signed-off-by: Aleisha <aleishaamohia@hotmail.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

--- installer/data/mysql/atomicupdate/sysprefs_Bug12528.sql
+++ installer/data/mysql/atomicupdate/sysprefs_Bug12528.sql
@@ -1,4 +1,2 @@
-INSERT INTO systempreferences ( variable, value, options, explanation,type ) VALUES
-('OPACEnhancedMessagingPreferences', '1', NULL, 'If ON, show patrons messaging setting on the OPAC.', 'YesNo') ON DUPLICATE KEY UPDATE value ='1';
-INSERT INTO systempreferences ( variable, value, options, explanation,type ) VALUES
-('EnhancedMessagingPreferences', '1', NULL, 'If ON, allows patrons to select to receive additional messages about items due or nearly due.', 'YesNo') ON DUPLICATE KEY UPDATE value ='1';
+INSERT INTO systempreferences ( value, variable, options, explanation,type )
+select value ,'OPACEnhancedMessagingPreferences', NULL, 'If ON, allows patrons to select to receive additional messages about items due or nearly due.', 'YesNo' from systempreferences where variable='EnhancedMessagingPreferences';
