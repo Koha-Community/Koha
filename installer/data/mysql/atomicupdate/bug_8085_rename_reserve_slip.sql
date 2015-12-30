a1c4b8b293d80fab7473168f2d3adbe65b4abcaf
Kyle M Hall <kyle@bywatersolutions.com>
2015-12-30 14:56:28 +0000

    Bug 8085 - DBrev 3.23.00.003
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

--- installer/data/mysql/atomicupdate/bug_8085_rename_reserve_slip.sql
+++
@@ -1,3 +0,0 @@
--- Rename 'Reserve slip' to 'Holds slip' to match Koha's terminology
-UPDATE letter SET name = "Hold Slip" WHERE name = "Reserve Slip";
-UPDATE letter SET title = "Hold Slip" WHERE title = "Reserve Slip";
--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -11416,6 +11416,19 @@ if(CheckVersion($DBversion)) {
     SetVersion($DBversion);
 }
 
+$DBversion = "3.23.00.003";
+if(CheckVersion($DBversion)) {
+    $dbh->do(q{
+        UPDATE letter SET name = "Hold Slip" WHERE name = "Reserve Slip"
+    });
+    $dbh->do(q{
+        UPDATE letter SET title = "Hold Slip" WHERE title = "Reserve Slip";
+    });
+
+    print "Upgrade to $DBversion done (Bug 8085 - Rename 'Reserve slip' to 'Hold slip')\n";
+    SetVersion($DBversion);
+}
+
 # DEVELOPER PROCESS, search for anything to execute in the db_update directory
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
