e71dd6fdc23205be59e0433970bb066353008d20
Kyle M Hall <kyle@bywatersolutions.com>
2016-01-04 09:21:07 +0000

    Bug 12528: Bug 9254: Followup - Rename pref to EnhancedMessagingPreferencesOPAC
    If the new pref is named EnhancedMessagingPreferencesOPAC, it will show
    up adjacent to EnhancedMessagingPreferences
    
    Signed-off-by: Marc Véron <veron@veron.ch>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

--- installer/data/mysql/atomicupdate/sysprefs_Bug12528.sql
+++ installer/data/mysql/atomicupdate/sysprefs_Bug12528.sql
@@ -1,2 +1,2 @@
 INSERT INTO systempreferences ( value, variable, options, explanation,type )
-select value ,'OPACEnhancedMessagingPreferences', NULL, 'If ON, allows patrons to select to receive additional messages about items due or nearly due.', 'YesNo' from systempreferences where variable='EnhancedMessagingPreferences';
+select value ,'EnhancedMessagingPreferencesOPAC', NULL, 'If ON, allows patrons to select to receive additional messages about items due or nearly due.', 'YesNo' from systempreferences where variable='EnhancedMessagingPreferences';
