e93ffac3f1dd92a1fa5e10dd52cffcaa5a1c2192
Aleisha <aleishaamohia@hotmail.com>
2016-01-19 03:44:31 +0000

    Bug 12051: Adds a Renew tab to the staff client
    UPDATE: Added to checkin-search.inc to show up on returns.pl
    
    To test:
    
    1) Apply patch
    2) Confirm that there is now a Renew tab on the pages where there is also a Check-in tab only
    3) Attempt to renew a book using this, confirm it works as expected
    4) Check if I have missed any pages
    
    Signed-off-by: Marc Véron <veron@veron.ch>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    
    Signed-off-by: Brendan Gallagher brendan@bywatersolutions.com

--- koha-tmpl/intranet-tmpl/prog/en/js/staff-global.js
+++ koha-tmpl/intranet-tmpl/prog/en/js/staff-global.js
@@ -32,6 +32,7 @@ $.fn.selectTabByID = function (tabID) {
     if($("#header_search #checkin_search").length > 0){ shortcut.add('Alt+r',function (){ $("#header_search").selectTabByID("#checkin_search"); $("#ret_barcode").focus(); }); } else { shortcut.add('Alt+r',function (){ location.href="/cgi-bin/koha/circ/returns.pl"; }); }
     if($("#header_search #circ_search").length > 0){ shortcut.add('Alt+u',function (){ $("#header_search").selectTabByID("#circ_search"); $("#findborrower").focus(); }); } else { shortcut.add('Alt+u',function(){ location.href="/cgi-bin/koha/circ/circulation.pl"; }); }
     if($("#header_search #catalog_search").length > 0){ shortcut.add('Alt+q',function (){ $("#header_search").selectTabByID("#catalog_search"); $("#search-form").focus(); }); } else { shortcut.add('Alt+q',function(){ location.href="/cgi-bin/koha/catalogue/search.pl"; }); }
+    if($("#header_search #renew_search").length > 0){ shortcut.add('Alt+y',function (){ $("#header_search").selectTabByID("#renew_search"); $("#ren_barcode").focus(); }); } else { shortcut.add('Alt+y',function(){ location.href="/cgi-bin/koha/circ/renew.pl"; }); }
 
     $(".focus").focus();
     $(".validated").each(function() {
