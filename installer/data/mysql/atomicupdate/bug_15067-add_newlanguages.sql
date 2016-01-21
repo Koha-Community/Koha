90822bc7104ae807828546582f6afe5d7fd6dffe
Gus <gel1163@stacmail.net>
2016-01-21 02:23:38 +0000

    Bug 15067: Add additional languages to advanced search language search
    run update database script, look for new laguages under 'Language:' in Advanced search.
    Languages that should be added are:
    1. Estonian
    2. Latvian
    3. Lithuanian
    4. Inuktitut
    5. Inupiaq
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    
    Signed-off-by: David Nind <david@davidnind.com>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

---
+++ installer/data/mysql/atomicupdate/bug_15067-add_newlanguages.sql
@@ -0,0 +1,15 @@
+INSERT INTO language_subtag_registry (subtag, type, description, added) VALUES ('et', 'language', 'Estonian', now());
+INSERT INTO language_descriptions (subtag, type, lang, description) VALUES ('et', 'language', 'en', 'Estonian');
+INSERT INTO language_rfc4646_to_iso639 (rfc4646_subtag, iso639_2_code) VALUES ('et', 'est');
+INSERT INTO language_subtag_registry (subtag, type, description, added) VALUES ('lv', 'language', 'Latvian', now());
+INSERT INTO language_descriptions (subtag, type, lang, description) VALUES ('lv', 'language', 'en', 'Latvian');
+INSERT INTO language_rfc4646_to_iso639 (rfc4646_subtag, iso639_2_code) VALUES ('lv', 'lav');
+INSERT INTO language_subtag_registry (subtag, type, description, added) VALUES ('lt', 'language', 'Lithuanian', now());
+INSERT INTO language_descriptions (subtag, type, lang, description) VALUES ('lt', 'language', 'en', 'Lithuanian');
+INSERT INTO language_rfc4646_to_iso639 (rfc4646_subtag, iso639_2_code) VALUES ('lt', 'lit');
+INSERT INTO language_subtag_registry (subtag, type, description, added) VALUES ('iu', 'language', 'Inuktitut', now());
+INSERT INTO language_descriptions (subtag, type, lang, description) VALUES ('iu', 'language', 'en', 'Inuktitut');
+INSERT INTO language_rfc4646_to_iso639 (rfc4646_subtag, iso639_2_code) VALUES ('iu', 'iku');
+INSERT INTO language_subtag_registry (subtag, type, description, added) VALUES ('ik', 'language', 'Inupiaq', now());
+INSERT INTO language_descriptions (subtag, type, lang, description) VALUES ('ik', 'language', 'en', 'Inupiaq');
+INSERT INTO language_rfc4646_to_iso639 (rfc4646_subtag, iso639_2_code) VALUES ('ik', 'ipk');
--- installer/data/mysql/mandatory/subtag_registry.sql
+++ installer/data/mysql/mandatory/subtag_registry.sql
@@ -1804,3 +1804,58 @@ VALUES ( 'VN', 'region', 'en', 'Vietnam');
 
 INSERT INTO language_descriptions(subtag, type, lang, description)
 VALUES ( 'VN', 'region', 'vi', 'Việt Nam');
+
+-- Estonian
+
+INSERT INTO language_descriptions (subtag, type, lang, description)
+VALUES ('et', 'language', 'en', 'Estonian');
+
+INSERT INTO language_subtag_registry (subtag, type, description, added)
+VALUES ('et', 'language', 'Estonian', now());
+
+INSERT INTO language_rfc4646_to_iso639 (rfc4646_subtag, iso639_2_code)
+VALUES ('et', 'est');
+
+-- Latvian
+
+INSERT INTO language_descriptions (subtag, type, lang, description)
+VALUES ('lv', 'language', 'en', 'Latvian');
+
+INSERT INTO language_subtag_registry (subtag, type, description, added)
+VALUES ('lv', 'language', 'Latvian', now());
+
+INSERT INTO language_rfc4646_to_iso639 (rfc4646_subtag, iso639_2_code)
+VALUES ('lv', 'lav');
+
+-- Lithianian
+
+INSERT INTO language_descriptions (subtag, type, lang, description)
+VALUES ('lt', 'language', 'en', 'Lithuanian');
+
+INSERT INTO language_subtag_registry (subtag, type, description, added)
+VALUES ('lt', 'language', 'Lithuanian', now());
+
+INSERT INTO language_rfc4646_to_iso639 (rfc4646_subtag, iso639_2_code)
+VALUES ('lt', 'lit');
+
+-- Inuktitut
+
+INSERT INTO language_descriptions (subtag, type, lang, description)
+VALUES ('iu', 'language', 'en', 'Inuktitut');
+
+INSERT INTO language_subtag_registry (subtag, type, description, added)
+VALUES ('iu', 'language', 'Inuktitut', now());
+
+INSERT INTO language_rfc4646_to_iso639 (rfc4646_subtag, iso639_2_code)
+VALUES ('iu', 'iku');
+
+--Inupiaq
+
+INSERT INTO language_descriptions (subtag, type, lang, description)
+VALUES ('ik', 'language', 'en', 'Inupiaq');
+
+INSERT INTO language_subtag_registry (subtag, type, description, added)
+VALUES ('ik', 'language', 'Inupiaq', now());
+
+INSERT INTO language_rfc4646_to_iso639 (rfc4646_subtag, iso639_2_code)
+VALUES ('ik', 'ipk');
