c76bcd508a0929609a7988275e05558003a3a23d
Brendan Gallagher <brendan@bywatersolutions.com>
2016-01-27 17:58:47 +0000

    DBRev for bug 8753  (removing the atomicupdate too).
    Signed-off-by: Brendan Gallagher brendan@bywatersolutions.com

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -11669,6 +11669,31 @@ if ( CheckVersion($DBversion) ) {
     SetVersion($DBversion);
 }
 
+$DBversion = "3.23.00.016";
+if ( CheckVersion($DBversion) ) {
+    $dbh->do(q{
+    INSERT IGNORE INTO systempreferences (variable,value,options,explanation,type)
+    VALUES ('OpacResetPassword',  '0','','Shows the ''Forgot your password?'' link in the OPAC','YesNo');
+});
+    $dbh->do(q{
+    CREATE TABLE IF NOT EXISTS borrower_password_recovery (
+      borrowernumber int(11) NOT NULL,
+      uuid varchar(128) NOT NULL,
+      valid_until timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+      PRIMARY KEY (borrowernumber),
+      KEY borrowernumber (borrowernumber)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+});
+    $dbh->do(q{
+    INSERT IGNORE INTO `letter` (module, code, branchcode, name, is_html, title, content, message_transport_type)
+    VALUES ('members','PASSWORD_RESET','','Online password reset',1,'Koha password recovery','<html>\r\n<p>This email has been sent in response to your password recovery request for the account <strong><<user>></strong>.\r\n</p>\r\n<p>\r\nYou can now create your new password using the following link:\r\n<br/><a href=\"<<passwordreseturl>>\"><<passwordreseturl>></a>\r\n</p>\r\n<p>This link will be valid for 2 days from this email\'s reception, then you must reapply if you do not change your password.</p>\r\n<p>Thank you.</p>\r\n</html>\r\n','email');
+
+    });
+
+    print "Upgrade to $DBversion done (Bug 8753 - Add forgot password link to OPAC)\n";
+    SetVersion($DBversion);
+}
+
 # DEVELOPER PROCESS, search for anything to execute in the db_update directory
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
