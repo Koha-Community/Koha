21ebaf949e0d3822e313ff76a32c23a97be0a78e
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2016-02-12 09:07:09 +0000

    Bug 15736: Add ShowAllCheckins pref to control the display of checked-in item list on checking
    Bug 14821 removed the items which were not checked out, but some
    libraries considered it as a bug.
    So let's add a new pref to control this behavior.
    
    Test plan:
    0/ Execute the updatedb entry and set ShowAllCheckins to "Do not show"
    (default)
    1/ Check an item in.
    If the item was not checked out, it won't be listed
    2/ Turn the pref to "Show"
    3/ Check an item in
    If the item was not checked out, it should be listed
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    
    Signed-off-by: Brendan Gallagher brendan@bywatersolutions.com
    (cherry picked from commit ce6c3bb88b426b0d0b945b9e71c695017dcb0fed)
    Signed-off-by: Julian Maurice <julian.maurice@biblibre.com>

---
+++ installer/data/mysql/atomicupdate/bug_15736.sql
@@ -0,0 +1,1 @@
+INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type` ) VALUES ('ShowAllCheckins', '0', '', 'Show all checkins', 'YesNo');
--- installer/data/mysql/sysprefs.sql
+++ installer/data/mysql/sysprefs.sql
@@ -404,6 +404,7 @@ INSERT INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `
 ('ShelfBrowserUsesCcode','1','0','Use the item collection code when finding items for the shelf browser.','YesNo'),
 ('ShelfBrowserUsesHomeBranch','1','1','Use the item home branch when finding items for the shelf browser.','YesNo'),
 ('ShelfBrowserUsesLocation','1','1','Use the item location when finding items for the shelf browser.','YesNo'),
+('ShowAllCheckins', '0', '', 'Show all checkins', 'YesNo'),
 ('ShowPatronImageInWebBasedSelfCheck','0','','If ON, displays patron image when a patron uses web-based self-checkout','YesNo'),
 ('ShowReviewer','full','none|full|first|surname|firstandinitial|username','Choose how a commenter\'s identity is presented alongside comments in the OPAC','Choice'),
 ('ShowReviewerPhoto','1','','If ON, photo of reviewer will be shown beside comments in OPAC','YesNo'),
