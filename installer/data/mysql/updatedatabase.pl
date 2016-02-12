d877c78fffc0dd06137f88e1dd99f526c0abd2fc
Jesse Weaver <jweaver@bywatersolutions.com>
2016-02-12 12:43:32 -0700

    Bug 15411: Update DB rev (3.20.08.002)
    Signed-off-by: Julian Maurice <julian.maurice@biblibre.com>
    (cherry picked from commit af61bfbcf827fc51e2f27f45afce9bec349a0bb4)
    Signed-off-by: Frédéric Demians <f.demians@tamil.fr>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -10724,6 +10724,16 @@ if ( CheckVersion($DBversion) ) {
     SetVersion($DBversion);
 }
 
+$DBversion = "3.20.08.002";
+if ( CheckVersion($DBversion) ) {
+    $dbh->do(q{
+        UPDATE `authorised_values` SET `lib`='Non-fiction' WHERE `lib`='Non Fiction';
+    });
+
+    print "Upgrade to $DBversion done (Bug 15411 - Change Non Fiction to Non-fiction in authorised_values)\n";
+    SetVersion($DBversion);
+}
+
 # DEVELOPER PROCESS, search for anything to execute in the db_update directory
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
