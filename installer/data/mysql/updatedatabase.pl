d409b437528748c41e108998231312d74e28d826
Jesse Weaver <jweaver@bywatersolutions.com>
2016-02-12 12:43:32 -0700

    Bug 15446: Update DB rev (3.22.04.003)
    Signed-off-by: Julian Maurice <julian.maurice@biblibre.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -11502,6 +11502,16 @@ if ( CheckVersion($DBversion) ) {
     SetVersion($DBversion);
 }
 
+$DBversion = "3.22.04.003";
+if ( CheckVersion($DBversion) ) {
+    $dbh->do(q{
+        UPDATE systempreferences SET value="0" where type="YesNo" and value="";
+    });
+
+    print "Upgrade to $DBversion done (Bug 15446 - Fix systempreferences rows where type=YesNo and value='')\n";
+    SetVersion($DBversion);
+}
+
 # DEVELOPER PROCESS, search for anything to execute in the db_update directory
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
