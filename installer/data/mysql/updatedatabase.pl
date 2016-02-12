e1966eec6567cb4106bbbc9e77985e73516ea5c1
Julian Maurice <julian.maurice@biblibre.com>
2016-02-12 11:00:56 +0100

    Increment version for 3.22.3 release
    Signed-off-by: Julian Maurice <julian.maurice@biblibre.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -11438,6 +11438,12 @@ if ( CheckVersion($DBversion) ) {
     SetVersion($DBversion);
 }
 
+$DBversion = "3.22.03.000";
+if ( CheckVersion($DBversion) ) {
+    print "Upgrade to $DBversion done (Koha 3.22.3)\n";
+    SetVersion($DBversion);
+}
+
 # DEVELOPER PROCESS, search for anything to execute in the db_update directory
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
