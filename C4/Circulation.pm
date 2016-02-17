5170c91f493aaa82d111a6a5215faf0ed53be72b
Colin Campbell <colin.campbell@ptfs-europe.com>
2016-02-17 12:47:56 +0000

    Bug 15841 Circulation module's true value misplaced
    1; was not at the end of the module as it should be
    to ensure compilation returns true
    return it to last line of code where it should be
    
    This patch fixes an ovious mistake.
    Signed-off-by: Marc Véron <veron@veron.ch>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    
    Signed-off-by: Brendan Gallagher brendan@bywatersolutions.com

--- C4/Circulation.pm
+++ C4/Circulation.pm
@@ -3942,7 +3942,6 @@ sub GetAgeRestriction {
     return ($restriction_year);
 }
 
-1;
 
 =head2 GetPendingOnSiteCheckouts
 
@@ -4035,6 +4034,7 @@ sub GetTopIssues {
     return @$rows;
 }
 
+1;
 __END__
 
 =head1 AUTHOR
