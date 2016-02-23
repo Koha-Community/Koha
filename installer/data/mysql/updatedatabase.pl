e061b2b65cd0cfa1f9e1b51e9d7ea0550c2b6a4f
Brendan Gallagher <brendan@bywatersolutions.com>
2016-02-23 23:13:08 +0000

    DBRev for Bug 15517 - Tables borrowers and deletedborrowers differ again

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -11795,6 +11795,18 @@ VALUES('OpenLibrarySearch','0','If Yes Open Library search results will show in 
     print "Upgrade to $DBversion done (Bug 6624 - Allow Koha to use the new read API from OpenLibrary)\n";
     SetVersion($DBversion);
 }
+
+$DBversion = "3.23.00.024";
+if ( CheckVersion($DBversion) ) {
+    $dbh->do(q{
+    ALTER TABLE deletedborrowers MODIFY COLUMN userid VARCHAR(75) DEFAULT NULL;
+});
+    $dbh->do(q{
+    ALTER TABLE deletedborrowers MODIFY COLUMN password VARCHAR(60) DEFAULT NULL;
+});
+    print "Upgrade to $DBversion done (Bug 15517 - Tables borrowers and deletedborrowers differ again)\n";
+    SetVersion($DBversion);
+}
 # DEVELOPER PROCESS, search for anything to execute in the db_update directory
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
