a66674a96484124b69d627f6e2e081debafa4a6c
Brendan Gallagher <brendan@bywatersolutions.com>
2016-02-23 15:33:41 +0000

    DBRev for Bug 13534 - Deleting staff patron will delete tags approved by this patron
    Signed-off-by: Brendan Gallagher brendan@bywatersolutions.com

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -11773,6 +11773,27 @@ if ( CheckVersion($DBversion) ) {
     SetVersion($DBversion);
 }
 
+$DBversion = "3.23.00.022";
+    if ( CheckVersion($DBversion) ) {
+    $dbh->do(q{
+	ALTER TABLE tags_all MODIFY COLUMN borrowernumber INT(11);
+	});
+    $dbh->do(q{
+	ALTER TABLE tags_all drop FOREIGN KEY tags_borrowers_fk_1;
+	});
+    $dbh->do(q{
+	ALTER TABLE tags_all ADD CONSTRAINT `tags_borrowers_fk_1` FOREIGN KEY (`borrowernumber`) REFERENCES `borrowers` (`borrowernumber`) ON DELETE SET NULL ON UPDATE CASCADE;
+	});
+    $dbh->do(q{
+	ALTER TABLE tags_approval DROP FOREIGN KEY tags_approval_borrowers_fk_1;
+	});
+    $dbh->do(q{
+	ALTER TABLE tags_approval ADD CONSTRAINT `tags_approval_borrowers_fk_1` FOREIGN KEY (`approved_by`) REFERENCES `borrowers` (`borrowernumber`) ON DELETE SET NULL ON UPDATE CASCADE;
+	});
+	print "Upgrade to $DBversion done (Bug 13534 - Deleting staff patron will delete tags approved by this patron)\n";
+    	SetVersion($DBversion);
+    }
+
 # DEVELOPER PROCESS, search for anything to execute in the db_update directory
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
