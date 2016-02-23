f528a68059e2b2ad18c242c4f55fbc9310af07f7
Brendan Gallagher <brendan@bywatersolutions.com>
2016-02-23 15:33:41 +0000

    Bug 13534: Update DB rev (3.20.08.003)
    Signed-off-by: Julian Maurice <julian.maurice@biblibre.com>
    (cherry picked from commit 9ad830d04b88e16c44824f3fd1052dd566df3345)
    Signed-off-by: Frédéric Demians <f.demians@tamil.fr>

--- installer/data/mysql/atomicupdate/bug_13534.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE tags_all MODIFY COLUMN borrowernumber INT(11);
-ALTER TABLE tags_all drop FOREIGN KEY tags_borrowers_fk_1;
-ALTER TABLE tags_all ADD CONSTRAINT `tags_borrowers_fk_1` FOREIGN KEY (`borrowernumber`) REFERENCES `borrowers` (`borrowernumber`) ON DELETE SET NULL ON UPDATE CASCADE;
-
-ALTER TABLE tags_approval DROP FOREIGN KEY tags_approval_borrowers_fk_1;
-ALTER TABLE tags_approval ADD CONSTRAINT `tags_approval_borrowers_fk_1` FOREIGN KEY (`approved_by`) REFERENCES `borrowers` (`borrowernumber`) ON DELETE SET NULL ON UPDATE CASCADE;
