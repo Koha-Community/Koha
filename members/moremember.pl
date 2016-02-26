e4d0363bc72403b4eea69cd7375e06ee2ecd729d
Nick Clemens <nick@bywatersolutions.com>
2016-02-26 21:08:55 +0000

    Bug 15928 - Show unlinked guarantor
    To test:
    1 - Add guarantor data to patron account by typing it in but do not 'Set to patron'
    2 - Note it is not displayed on patron details
    3 - Apply patch
    4 - Note the info is displayed
    5 - Test that linked guarantors show as expected
    
    Signed-off-by: Marc Véron <veron@veron.ch>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    Amended-patch: Correct some indents/spaces
    
    Signed-off-by: Brendan Gallagher brendan@bywatersolutions.com

--- members/moremember.pl
+++ members/moremember.pl
@@ -169,6 +169,13 @@ if ( my $guarantor = $patron->guarantor ) {
     $template->param( guarantor => $guarantor );
     push @relatives, $guarantor->borrowernumber;
     push @relatives, $_->borrowernumber for $patron->siblings;
+} elsif ( $patron->contactname || $patron->contactfirstname ) {
+    $template->param(
+        guarantor => {
+            firstname => $patron->contactfirstname,
+            surname   => $patron->contactname,
+        }
+    );
 } else {
     my @guarantees = $patron->guarantees;
     $template->param( guarantees => \@guarantees );
