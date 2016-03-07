72d13a26642d922628c6d589fbf65e5baaf66a9e
Julian Maurice <julian.maurice@biblibre.com>
2016-03-07 13:22:02 +0100

    Bug 15526: Update DB rev (3.22.04.002)

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -11493,6 +11493,15 @@ if ( CheckVersion($DBversion) ) {
     SetVersion($DBversion);
 }
 
+$DBversion = "3.22.04.002";
+if ( CheckVersion($DBversion) ) {
+    $dbh->do(q{
+        DROP TABLE IF EXISTS nozebra;
+    });
+    print "Upgrade to $DBversion done (Bug 15526 - Drop nozebra database table)\n";
+    SetVersion($DBversion);
+}
+
 # DEVELOPER PROCESS, search for anything to execute in the db_update directory
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
