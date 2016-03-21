9082762551429199eb911697ceb7d620436ef865
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2016-03-21 15:49:25 +0000

    Bug 16040: Update fnReloadAjax DT plugin to fix quotes deletion
    When deleting quotes, the table is not regenerated and a JS error is
    raised.
    That is because we are not using an up-to-date plugin
    
    Test plan:
    Delete a quote and confirm you do not get a JS error
    
    Works as expected.
    Signed-off-by: Marc Véron <veron@veron.ch>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Signed-off-by: Brendan A Gallagher <brendan@bywatersolutions.com>
    (cherry picked from commit 908a751e2cadcda0ceafa2efdd2cf0104a323467)
    Signed-off-by: Julian Maurice <julian.maurice@biblibre.com>
    (cherry picked from commit 9478a0bf1eb43e0b82fb7c8ea84a4b503b9f81ce)
    Signed-off-by: Frédéric Demians <f.demians@tamil.fr>

--- koha-tmpl/intranet-tmpl/lib/jquery/plugins/dataTables.fnReloadAjax.js
+++ koha-tmpl/intranet-tmpl/lib/jquery/plugins/dataTables.fnReloadAjax.js
@@ -1,50 +1,102 @@
-$.fn.dataTableExt.oApi.fnReloadAjax = function ( oSettings, sNewSource, fnCallback, bStandingRedraw )
+/**
+ * By default DataTables only uses the sAjaxSource variable at initialisation
+ * time, however it can be useful to re-read an Ajax source and have the table
+ * update. Typically you would need to use the `fnClearTable()` and
+ * `fnAddData()` functions, however this wraps it all up in a single function
+ * call.
+ *
+ * DataTables 1.10 provides the `dt-api ajax.url()` and `dt-api ajax.reload()`
+ * methods, built-in, to give the same functionality as this plug-in. As such
+ * this method is marked deprecated, but is available for use with legacy
+ * version of DataTables. Please use the new API if you are used DataTables 1.10
+ * or newer.
+ *
+ *  @name fnReloadAjax
+ *  @summary Reload the table's data from the Ajax source
+ *  @author [Allan Jardine](http://sprymedia.co.uk)
+ *  @deprecated
+ *
+ *  @param {string} [sNewSource] URL to get the data from. If not give, the
+ *    previously used URL is used.
+ *  @param {function} [fnCallback] Callback that is executed when the table has
+ *    redrawn with the new data
+ *  @param {boolean} [bStandingRedraw=false] Standing redraw (don't changing the
+ *      paging)
+ *
+ *  @example
+ *    var table = $('#example').dataTable();
+ *
+ *    // Example call to load a new file
+ *    table.fnReloadAjax( 'media/examples_support/json_source2.txt' );
+ *
+ *    // Example call to reload from original file
+ *    table.fnReloadAjax();
+ */
+
+jQuery.fn.dataTableExt.oApi.fnReloadAjax = function ( oSettings, sNewSource, fnCallback, bStandingRedraw )
 {
-    if ( typeof sNewSource != 'undefined' && sNewSource != null )
-    {
-        oSettings.sAjaxSource = sNewSource;
-    }
-    this.oApi._fnProcessingDisplay( oSettings, true );
-    var that = this;
-    var iStart = oSettings._iDisplayStart;
-    var aData = [];
+	// DataTables 1.10 compatibility - if 1.10 then `versionCheck` exists.
+	// 1.10's API has ajax reloading built in, so we use those abilities
+	// directly.
+	if ( jQuery.fn.dataTable.versionCheck ) {
+		var api = new jQuery.fn.dataTable.Api( oSettings );
 
-    this.oApi._fnServerParams( oSettings, aData );
+		if ( sNewSource ) {
+			api.ajax.url( sNewSource ).load( fnCallback, !bStandingRedraw );
+		}
+		else {
+			api.ajax.reload( fnCallback, !bStandingRedraw );
+		}
+		return;
+	}
 
-    oSettings.fnServerData( oSettings.sAjaxSource, aData, function(json) {
-        /* Clear the old information from the table */
-        that.oApi._fnClearTable( oSettings );
+	if ( sNewSource !== undefined && sNewSource !== null ) {
+		oSettings.sAjaxSource = sNewSource;
+	}
 
-        /* Got the data - add it to the table */
-        var aData =  (oSettings.sAjaxDataProp !== "") ?
-            that.oApi._fnGetObjectDataFn( oSettings.sAjaxDataProp )( json ) : json;
+	// Server-side processing should just call fnDraw
+	if ( oSettings.oFeatures.bServerSide ) {
+		this.fnDraw();
+		return;
+	}
 
-        for ( var i=0 ; i<aData.length ; i++ )
-        {
-            that.oApi._fnAddData( oSettings, aData[i] );
-        }
+	this.oApi._fnProcessingDisplay( oSettings, true );
+	var that = this;
+	var iStart = oSettings._iDisplayStart;
+	var aData = [];
 
-        oSettings.aiDisplay = oSettings.aiDisplayMaster.slice();
-        that.fnDraw();
+	this.oApi._fnServerParams( oSettings, aData );
 
-        if ( typeof bStandingRedraw != 'undefined' && bStandingRedraw === true )
-        {
-            oSettings._iDisplayStart = iStart;
-            that.fnDraw( false );
-        }
+	oSettings.fnServerData.call( oSettings.oInstance, oSettings.sAjaxSource, aData, function(json) {
+		/* Clear the old information from the table */
+		that.oApi._fnClearTable( oSettings );
 
-        that.oApi._fnProcessingDisplay( oSettings, false );
+		/* Got the data - add it to the table */
+		var aData =  (oSettings.sAjaxDataProp !== "") ?
+			that.oApi._fnGetObjectDataFn( oSettings.sAjaxDataProp )( json ) : json;
 
-        /* Callback user function - for event handlers etc */
-        if ( typeof fnCallback == 'function' && fnCallback != null )
-        {
-            fnCallback( oSettings );
-        }
-    }, oSettings );
-}
+		for ( var i=0 ; i<aData.length ; i++ )
+		{
+			that.oApi._fnAddData( oSettings, aData[i] );
+		}
 
-/* Example call to load a new file */
-//oTable.fnReloadAjax( 'media/examples_support/json_source2.txt' );
+		oSettings.aiDisplay = oSettings.aiDisplayMaster.slice();
 
-/* Example call to reload from original file */
-//oTable.fnReloadAjax();
+		that.fnDraw();
+
+		if ( bStandingRedraw === true )
+		{
+			oSettings._iDisplayStart = iStart;
+			that.oApi._fnCalculateEnd( oSettings );
+			that.fnDraw( false );
+		}
+
+		that.oApi._fnProcessingDisplay( oSettings, false );
+
+		/* Callback user function - for event handlers etc */
+		if ( typeof fnCallback == 'function' && fnCallback !== null )
+		{
+			fnCallback( oSettings );
+		}
+	}, oSettings );
+};
