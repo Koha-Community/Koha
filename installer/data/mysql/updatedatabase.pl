9e350eee19236e3a67d77f679512a970db8d7e76
Brendan Gallagher <brendan@bywatersolutions.com>
2016-04-22 16:27:13 -0700

    DBREV Bug 12267 - Allow password option in Patron Attribute non functional

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -12231,6 +12231,31 @@ if ( C4::Context->preference("Version") < TransformToNum($DBversion) ) {
 }
 
 
+
+DBversion = "3.23.00.049";
+if ( C4::Context->preference("Version") < TransformToNum($DBversion) ) {
+my $dbh = C4::Context->dbh;
+my ( $column_has_been_used ) = $dbh->selectrow_array(q|
+            SELECT COUNT(*)
+                FROM borrower_attributes
+                    WHERE password IS NOT NULL
+                    |);
+
+if ( $column_has_been_used ) {
+        print q|WARNING: The columns borrower_attribute_types.password_allowed and borrower_attributes.password have been removed from the Koha codebase. They were not used. However your installation has at least one borrower_attributes.password defined. In order not to alter your data, the columns have been kept, please save the information elsewhere and remove these columns manually.|;
+} else {
+        $dbh->do(q|
+        ALTER TABLE borrower_attribute_types DROP column password_allowed
+        |);
+        $dbh->do(q|
+        ALTER TABLE borrower_attributes DROP column password;
+        |);
+    }
+    print "Upgrade to $DBversion done (Bug 12267 - Allow password option in Patron Attribute non functional)\n";
+        SetVersion($DBversion);
+}
+
+
 # DEVELOPER PROCESS, search for anything to execute in the db_update directory
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
