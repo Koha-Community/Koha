4e6baf61858bd22d91d50ad5f176f04242a23370
Brendan Gallagher <brendan@bywatersolutions.com>
2016-04-27 02:38:35 +0000

    Forgot extra $dbh
    Signed-off-by: Brendan Gallagher <brendan@bywatersolutions.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -12350,7 +12350,9 @@ $dbh->do(q{
         DROP FOREIGN KEY emfk_vendor,
         DROP FOREIGN KEY emfk_edi_acct,
         DROP FOREIGN KEY emfk_basketno;
+        });
 
+$dbh->(q{
         ALTER TABLE edifact_messages
         ADD CONSTRAINT emfk_vendor FOREIGN KEY ( vendor_id ) REFERENCES aqbooksellers ( id ) ON DELETE CASCADE ON UPDATE CASCADE,
         ADD CONSTRAINT emfk_edi_acct FOREIGN KEY ( edi_acct ) REFERENCES vendor_edi_accounts ( id ) ON DELETE CASCADE ON UPDATE CASCADE,
