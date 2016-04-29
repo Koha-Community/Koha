7896665fc9a6ebff77658e58856f468cf26b4d61
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2016-04-29 15:52:36 +0100

    Bug 12528: Add IGNORE to the INSERT statement
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

--- installer/data/mysql/atomicupdate/sysprefs_Bug12528.sql
+++ installer/data/mysql/atomicupdate/sysprefs_Bug12528.sql
@@ -1,2 +1,2 @@
-INSERT INTO systempreferences ( value, variable, options, explanation,type )
+INSERT IGNORE INTO systempreferences ( value, variable, options, explanation,type )
 select value ,'EnhancedMessagingPreferencesOPAC', NULL, 'If ON, allows patrons to select to receive additional messages about items due or nearly due.', 'YesNo' from systempreferences where variable='EnhancedMessagingPreferences';
