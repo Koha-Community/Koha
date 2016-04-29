bywatersolutions/bywater-koha-devel

Bug 14577 - Allow restriction of checkouts based on fines of guarantor's guarantees
This enhancment allows a library to prevent patrons from checking out
items if his or her guarantees own too much.

Test Plan:
1) Apply this patch
2) Find or create a patron with a guarantor
3) Add a fine to the patron's account
4) Set the new system preference NoIssuesChargeGuarantees to be less
   than the amount owed by the patron
4) Attempt to check out an item to the guarantor, you will either
   be warned or prevented from checking out based on your system
   settings.

Koha/Patron.pm
@@ -22,6 +22,7 @@ use Modern::Perl;
 use Carp;
 
 use Koha::Database;
+use Koha::Patrons;
 use Koha::Patron::Images;
 
 use base qw(Koha::Object);
@@ -52,6 +53,12 @@ sub image {
     my ( $self ) = @_;
 
     return Koha::Patron::Images->find( $self->borrowernumber )
+}
+
+sub guarantees {
+    my ( $self ) = @_;
+
+    return Koha::Patrons->search( { guarantorid => $self->id } );
 }
 
 =head3 type

Koha-Community/Koha

Signed-off-by: Cathi Wiggin <CWIGGINS@arcadiaca.gov>
Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

Koha/Patron.pm
@@ -22,6 +22,7 @@ use Modern::Perl;
 use Carp;
 
 use Koha::Database;
+use Koha::Patrons;
 use Koha::Patron::Images;
 
 use base qw(Koha::Object);
@@ -65,7 +66,7 @@ Returns the guarantees (list of Koha::Patron) of this patron
 sub guarantees {
     my ( $self ) = @_;
 
-    return Koha::Patrons->search({ guarantorid => $self->borrowernumber });
+    return Koha::Patrons->search( { guarantorid => $self->borrowernumber } );
 }
 
 =head3 siblings