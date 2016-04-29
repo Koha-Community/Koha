c22934c3e6b7900f2400e13a073af4a7c123d635
Kyle M Hall <kyle@bywatersolutions.com>
2016-04-29 13:51:00 +0000

    Bug 16041 - DBRev 3.23.00.058
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -12492,6 +12492,39 @@ if ( CheckVersion($DBversion) ) {
     SetVersion($DBversion);
 }
 
+$DBversion = "3.23.00.058";
+if ( CheckVersion($DBversion) ) {
+
+    my ($count_imageurl) = $dbh->selectrow_array(q|
+        SELECT COUNT(*)
+        FROM authorised_values
+        WHERE imageurl IS NOT NULL
+            AND imageurl <> ""
+    |);
+
+    unless ($count_imageurl) {
+        if (   C4::Context->preference('AuthorisedValueImages')
+            or C4::Context->preference('StaffAuthorisedValueImages') )
+        {
+            $dbh->do(q|
+                UPDATE systempreferences
+                SET value = 0
+                WHERE variable = "AuthorisedValueImages"
+                   or variable = "StaffAuthorisedValueImages"
+            |);
+            warn "The system preferences AuthorisedValueImages and StaffAuthorisedValueImages have been turned off\n";
+            warn "authorised_values.imageurl is not populated, that means you are not using this feature\n";
+        }
+    }
+    else {
+        warn "At least one authorised value has an icon defined (imageurl)\n";
+        warn "The system preference AuthorisedValueImages or StaffAuthorisedValueImages could be turned off if you are not aware of this feature\n";
+    }
+
+    print "Upgrade to $DBversion done (Bug 15531 - Add support for standing orders)\n";
+    SetVersion($DBversion);
+}
+
 
 # DEVELOPER PROCESS, search for anything to execute in the db_update directory
 # SEE bug 13068
