24729ff8f99e3e96cb5fd1b3cd9af0616cc38e55
Owen Leonard <oleonard@myacpl.org>
2016-05-09 11:15:28 -0400

    Bug 16477 - Improve staff client cart JavaScript and template
    This patch makes several changes to the cart JavaScript and template. In
    the template:
    
    - Remove "onclick" attributes in favor of defining events in the
      JavaScript.
    - Add a [% BLOCK %] section for some repeated markup.
    - Add some Font Awesome icons (I didn't add icons to all controls
      because I thought it looked cluttered).
    - Move the batch modification control out of the toolbar and into the
      group of controls which affects selected records. I think this is a
      logical grouping, and makes more sense than having a drop-down menu in
      the toolbar with a single menu item.
    
    JavaScript:
    
    - Created separate "cart.js" file so that JS could be moved out of the
      template without loading up basket.js with event functions which are
      not needed on every page in the staff client.
    - Fix JSHint errors.
    
    To test, apply the patch and clear your browser cache if necessary.
    
    - Add multiple items to the cart in the staff client and open the cart.
    - Confirm correct functionality of these toolbar buttons:
      - "More details" (and the corresponding "Show less")
      - "Send"
      - "Print"
      - "Empty and close"
    - Confirm the correct functionality of all the selection controls:
      Select all, clear all, Remove, Add to a list, Place hold, Batch
      modify, and Batch delete.
    - Confirm that clicking any title in the cart opens the correct detail
      page in the parent window.
    
    Followed test plan, works as expected.
    Signed-off-by: Marc Véron <veron@veron.ch>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    
    https://bugs.koha-community.org/show_bug.cgi?id=1647
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

---
+++ koha-tmpl/intranet-tmpl/prog/js/cart.js
@@ -0,0 +1,156 @@
+
+function placeHold () {
+    var checkedItems = $("input:checkbox:checked");
+    if ($(checkedItems).size() === 0) {
+        alert(MSG_NO_RECORD_SELECTED);
+        return false;
+    }
+
+    var newloc;
+
+    if ($(checkedItems).size() > 1) {
+        var bibs = "";
+        $(checkedItems).each(function() {
+            var bib = $(this).val();
+            bibs += bib + "/";
+        });
+
+        newloc = "/cgi-bin/koha/reserve/request.pl?biblionumbers=" + bibs + "&multi_hold=1";
+    } else {
+        var bib = checkedItems[0].value;
+        newloc = "/cgi-bin/koha/reserve/request.pl?biblionumber=" + bib;
+    }
+
+    window.opener.location = newloc;
+    window.close();
+}
+
+function batchDelete(){
+    var checkedItems = $("input:checkbox:checked");
+    if ($(checkedItems).size() === 0) {
+        alert(MSG_NO_RECORD_SELECTED);
+        return false;
+    }
+    var newloc;
+
+    var bibs = "";
+    checkedItems.each(function() {
+        var bib = $(this).val();
+        bibs += bib + "/";
+    });
+
+    newloc = "/cgi-bin/koha/tools/batch_delete_records.pl?op=list&type=biblio&bib_list=" + bibs;
+
+    window.opener.location = newloc;
+    window.close();
+}
+
+function batchModify(){
+    var checkedItems = $("input:checkbox:checked");
+    if ($(checkedItems).size() === 0) {
+        alert(MSG_NO_RECORD_SELECTED);
+        return false;
+    }
+    var newloc;
+
+    var bibs = "";
+    $(checkedItems).each(function() {
+        var bib = $(this).val();
+        bibs += bib + "/";
+    });
+    newloc = "/cgi-bin/koha/tools/batch_record_modification.pl?op=list&amp;bib_list=" + bibs + "&type=biblio";
+
+    window.opener.location = newloc;
+    window.close();
+}
+
+$(document).ready(function(){
+    $("#items-popover").popover();
+    $("#CheckAll").click(function(){
+    var checked = [];
+    $("#bookbag_form").checkCheckboxes("*", true).each(
+        function() {
+            selRecord(this.value,true);
+        }
+    );
+        return false;
+    });
+    $("#CheckNone").click(function(){
+    var checked = [];
+    $("#bookbag_form").unCheckCheckboxes("*",true).each(
+        function() {
+            selRecord(this.value,false);
+        }
+    );
+        return false;
+    });
+    $(".holdsep").text("| ");
+    $(".hold").text(_("Place hold"));
+    $("#downloadcartc").empty();
+
+    $("#itemst").dataTable($.extend(true, {}, dataTablesDefaults, {
+        "sDom": 't',
+        "aoColumnDefs": [
+            { "bSortable": false, "bSearchable": false, 'aTargets': [ 'NoSort' ] },
+            { "sType": "anti-the", "aTargets" : [ "anti-the" ] },
+            { "sType": "callnumbers", "aTargets" : [ "callnumbers"] }
+        ],
+        "aaSorting": [[ 1, "asc" ]],
+        "bPaginate": false
+    }));
+
+    $(".showdetails").on("click",function(e){
+        e.preventDefault();
+        if( $(this).hasClass("showmore") ){
+            showMore();
+        } else {
+            showLess();
+        }
+    });
+
+    $("#batch_modify").on("click",function(e){
+        e.preventDefault();
+        batchModify();
+    });
+    $("#batch_delete").on("click",function(e){
+        e.preventDefault();
+        batchDelete();
+    });
+
+    $("#remove_from_cart").on("click",function(e){
+        e.preventDefault();
+        delSelRecords();
+    });
+
+    $("#add_to_list").on("click",function(e){
+        e.preventDefault();
+        addSelToShelf();
+    });
+
+    $("#place_hold").on("click",function(e){
+        e.preventDefault();
+        placeHold();
+    });
+
+    $("#send_cart").on("click",function(e){
+        e.preventDefault();
+        sendBasket();
+    });
+
+    $("#print_cart").on("click",function(e){
+        e.preventDefault();
+        printBasket();
+    });
+
+    $("#empty_cart").on("click",function(e){
+        e.preventDefault();
+        delBasket('popup');
+    });
+    $(".open_title").on("click",function(e){
+        e.preventDefault();
+        openBiblio( this.href );
+    });
+    $(".select_record").on("change",function(){
+        selRecord( this.value, this.checked );
+    });
+});
