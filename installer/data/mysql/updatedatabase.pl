85410cc9bc1cb6106ae8922e9698f6fb349b8b7d
Kyle M Hall <kyle@bywatersolutions.com>
2016-05-16 17:09:53 +0000

    Bug 16402 - DBRev 3.23.00.063
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -12576,6 +12576,18 @@ if ( CheckVersion($DBversion) ) {
 SetVersion($DBversion);
 }
 
+$DBversion = "3.23.00.063";
+if ( CheckVersion($DBversion) ) {
+    $dbh->do(q{
+        ALTER TABLE letter MODIFY COLUMN branchcode varchar(10) NOT NULL DEFAULT ''
+    });
+    $dbh->do(q{
+        ALTER TABLE permissions MODIFY COLUMN code varchar(64) NOT NULL DEFAULT '';
+    });
+    print "Upgrade to $DBversion done (Bug 16402: Fix DB structure to work on MySQL 5.7)\n";
+    SetVersion($DBversion);
+}
+
 
 # DEVELOPER PROCESS, search for anything to execute in the db_update directory
 # SEE bug 13068
