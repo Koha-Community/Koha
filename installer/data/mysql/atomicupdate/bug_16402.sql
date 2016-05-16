2b6664fd1cae92de24d14328d5c167a4e65b145f
Tomas Cohen Arazi <tomascohen@theke.io>
2016-05-16 12:36:56 -0300

    Bug 16402: (QA followup)
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    (cherry picked from commit 4e1b0e024297ecfe9b9ca91ea43e730db74a528a)
    Signed-off-by: Julian Maurice <julian.maurice@biblibre.com>

--- installer/data/mysql/atomicupdate/bug_16402.sql
+++ installer/data/mysql/atomicupdate/bug_16402.sql
@@ -1,2 +1,14 @@
-alter table letter modify column branchcode varchar(10) not null default '';
-alter table permissions modify column code varchar(64) not null default '';
+ALTER TABLE letter MODIFY COLUMN branchcode varchar(10) NOT NULL DEFAULT '';
+ALTER TABLE permissions MODIFY COLUMN code varchar(64) NOT NULL DEFAULT '';
+
+-- $DBversion = "3.23.00.XXX";
+-- if(CheckVersion($DBversion)) {
+--     $dbh->do(q{
+--         ALTER TABLE letter MODIFY COLUMN branchcode varchar(10) NOT NULL DEFAULT ''
+--     });
+--     $dbh->do(q{
+--         ALTER TABLE permissions MODIFY COLUMN code varchar(64) NOT NULL DEFAULT '';
+--     });
+--     print "Upgrade to $DBversion done (Bug 16402: Fix DB structure to work on MySQL 5.7)\n";
+--     SetVersion($DBversion);
+-- }
