bd7e4fb0d9fb90050a908f4162dfc0351aad1320
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2016-05-19 09:42:48 +0100

    Bug 16552: Add the ability to change the default holdings sort
    This new enhancement will add the ability to change the default holdings
    sort on OPAC displays to be by library, instead of item type.
    
    This patch adds a new pref OPACHoldingsDefaultSortField with 3 different
    possible values:
     - Item type
     - Home library
     - Holding library
    
    Note that if OpacLocationBranchToDisplay is set not to display home libraries,
    unexpected behaviors might happen if OPACHoldingsDefaultSortField is set to
    "Home library", same for "Holding library".
    
    Test plan:
    - Confirm that the default value for OPACHoldingsDefaultSortField is
      'first column' after executing the DB entry and that there is no
      change in the behavior (first column is used to sort the holdings
      table on the detail page).
    - Set OpacLocationBranchToDisplay to both and play with the different
      values of OPACHoldingsDefaultSortField
      => Confrm that the default column used to sort the table is correctly
      changed
    - Set the pref SeparateHoldings on
      => Confirm that both tables (Holdings and other holdings) are sorted using
      the OPACHoldingsDefaultSortField value.
    
    Sponsored-by: University of the Arts London
    
    Signed-off-by: Claire Gravely <c.gravely@arts.ac.uk>
    
    Signed-off-by: Katrin Fischer  <katrin.fischer@bsz-bw.de>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

---
+++ installer/data/mysql/atomicupdate/bug_16552.sql
@@ -0,0 +1,2 @@
+INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type` ) VALUES
+('OPACHoldingsDefaultSortField','first_column','first_column|homebranch|holdingbranch','Default sort field for the holdings table at the OPAC','choice');
