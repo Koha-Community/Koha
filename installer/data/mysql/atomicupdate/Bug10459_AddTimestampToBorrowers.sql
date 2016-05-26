947690e2324c72cba4ceca8790a98bcdf30752a8
Mark Tompsett <mtompset@hotmail.com>
2016-05-25 21:57:34 -0400

    Bug 10459 renamed timestamp to updated_on
    Signed-off-by: Joy Nelson <joy@bywatersolutions.com>
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    (cherry picked from commit f863e478b994f2631d79821473c773099dc78172)
    Signed-off-by: Frédéric Demians <f.demians@tamil.fr>

--- installer/data/mysql/atomicupdate/Bug10459_AddTimestampToBorrowers.sql
+++ installer/data/mysql/atomicupdate/Bug10459_AddTimestampToBorrowers.sql
@@ -1,8 +1,8 @@
 ALTER TABLE borrowers
-    ADD COLUMN timestamp timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+    ADD COLUMN updated_on timestamp NULL DEFAULT CURRENT_TIMESTAMP
     ON UPDATE CURRENT_TIMESTAMP
     AFTER privacy_guarantor_checkouts;
 ALTER TABLE deletedborrowers
-    ADD COLUMN timestamp timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+    ADD COLUMN updated_on timestamp NULL DEFAULT CURRENT_TIMESTAMP
     ON UPDATE CURRENT_TIMESTAMP
     AFTER privacy_guarantor_checkouts;
