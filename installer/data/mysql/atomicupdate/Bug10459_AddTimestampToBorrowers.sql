ac71c2e7da2777e3bb1cb4c457412dc7b0e94776
Frédéric Demians <f.demians@tamil.fr>
2016-06-10 17:15:21 +0000

    Bug 10459 - DBRev 16.05.00.002
    Signed-off-by: Frédéric Demians <f.demians@tamil.fr>

--- installer/data/mysql/atomicupdate/Bug10459_AddTimestampToBorrowers.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE borrowers
-    ADD COLUMN updated_on timestamp NULL DEFAULT CURRENT_TIMESTAMP
-    ON UPDATE CURRENT_TIMESTAMP
-    AFTER privacy_guarantor_checkouts;
-ALTER TABLE deletedborrowers
-    ADD COLUMN updated_on timestamp NULL DEFAULT CURRENT_TIMESTAMP
-    ON UPDATE CURRENT_TIMESTAMP
-    AFTER privacy_guarantor_checkouts;
