a0fb3fdf1a057dba56eb05ba6997d901bfcb9645
Kyle M Hall <kyle@bywatersolutions.com>
2016-06-14 16:35:49 +0000

    Bug 10459 - Update Schema
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    (cherry picked from commit 42cd9ca1447f4d576d9f1ad51e275e70c8bf9249)
    Signed-off-by: Frédéric Demians <f.demians@tamil.fr>

--- Koha/Schema/Result/Borrower.pm
+++ Koha/Schema/Result/Borrower.pm
@@ -407,6 +407,13 @@ __PACKAGE__->table("borrowers");
   default_value: 0
   is_nullable: 0
 
+=head2 updated_on
+
+  data_type: 'timestamp'
+  datetime_undef_if_invalid: 1
+  default_value: current_timestamp
+  is_nullable: 0
+
 =cut
 
 __PACKAGE__->add_columns(
@@ -576,6 +583,13 @@ __PACKAGE__->add_columns(
   { data_type => "integer", default_value => 1, is_nullable => 0 },
   "privacy_guarantor_checkouts",
   { data_type => "tinyint", default_value => 0, is_nullable => 0 },
+  "updated_on",
+  {
+    data_type => "timestamp",
+    datetime_undef_if_invalid => 1,
+    default_value => \"current_timestamp",
+    is_nullable => 0,
+  },
 );
 
 =head1 PRIMARY KEY
@@ -1204,8 +1218,8 @@ Composing rels: L</aqorder_users> -> ordernumber
 __PACKAGE__->many_to_many("ordernumbers", "aqorder_users", "ordernumber");
 
 
-# Created by DBIx::Class::Schema::Loader v0.07042 @ 2016-02-14 12:46:14
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:bI6qJYw+ulTUwA7XMCkkRw
+# Created by DBIx::Class::Schema::Loader v0.07042 @ 2016-06-14 16:35:12
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:Luq1YVrOwtdDvSDWgWNGUg
 
 __PACKAGE__->belongs_to(
     "guarantor",
