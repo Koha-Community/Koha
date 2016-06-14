3943b8ca4436762ed5ba9d2b51d6e1a4324792fb
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2016-06-14 10:43:14 +0100

    Bug 12748: Code tidy
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    (cherry picked from commit 44062c07b7703190b46701a3fa31665d998eadfb)
    Signed-off-by: Frédéric Demians <f.demians@tamil.fr>
    (cherry picked from commit 69e4c39c213192b46c4e231d40b7a34d676e6b4a)
    Signed-off-by: Julian Maurice <julian.maurice@biblibre.com>

--- C4/Serials.pm
+++ C4/Serials.pm
@@ -2679,9 +2679,9 @@ sub _can_do_on_subscription {
 
 =cut
 
-sub findSerialsByStatus{
-    my($status, $subscriptionid) = @_;
-    my $dbh = C4::Context->dbh;
+sub findSerialsByStatus {
+    my ( $status, $subscriptionid ) = @_;
+    my $dbh   = C4::Context->dbh;
     my $query = q| SELECT * from serial
                     WHERE status = ?
                     AND subscriptionid = ?
@@ -2689,6 +2689,7 @@ sub findSerialsByStatus{
     my $serials = $dbh->selectall_arrayref( $query, { Slice => {} }, $status, $subscriptionid );
     return @$serials;
 }
+
 1;
 __END__
 
