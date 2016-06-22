e3002eea202a50b68e96650f37bcacc37843872e
Marc Véron <veron@veron.ch>
2016-06-22 07:57:06 +0200

    Bug 16795 - Patron categories: Accept integers only for enrolment period and age limits
    To test:
    - Apply patch
    - Clear browser cache
    - Go to Home > Administration > Patron categories
    - Create or edit a category
    - Verify that field 'In months', 'Age required' and 'Upperage limit'
      accept digits only
    
    Signed-off-by: Owen Leonard <oleonard@myacpl.org>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

--- koha-tmpl/intranet-tmpl/prog/en/js/categories.js
+++ koha-tmpl/intranet-tmpl/prog/en/js/categories.js
@@ -59,7 +59,7 @@ $(document).ready(function() {
                 required: function(element){
                     return $("#enrolmentperioddate").val() === "";
                 },
-                number: true,
+                digits: true,
                 enrollment_period: true
             },
             enrolmentperioddate: {
@@ -69,10 +69,10 @@ $(document).ready(function() {
                 enrollment_period: true
             },
             dateofbirthrequired: {
-                number: true
+                digits: true
             },
             upperagelimit: {
-                number: true
+                digits: true
             },
             enrolmentfee: {
                 number: true
