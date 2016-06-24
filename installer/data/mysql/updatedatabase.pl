54451b66a15e7b1808001c8a8fdaef10194ed685
Kyle M Hall <kyle@bywatersolutions.com>
2016-06-24 13:32:35 +0000

    Bug 15485 - DBRev 16.06.00.004
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -12655,6 +12655,24 @@ if ( CheckVersion($DBversion) ) {
     SetVersion($DBversion);
 }
 
+$DBversion = '16.06.00.004';
+if ( CheckVersion($DBversion) ) {
+    $dbh->do(q{
+        INSERT IGNORE INTO systempreferences ( variable, value, options, explanation, type )
+         SELECT 'OPACXSLTListsDisplay', COALESCE(value,''), '', 'Enable XSLT stylesheet control over lists pages display on OPAC', 'Free'
+         FROM systempreferences WHERE variable='OPACXSLTResultsDisplay';
+    });
+
+    $dbh->do(q{
+        INSERT IGNORE INTO systempreferences ( variable, value, options, explanation, type )
+         SELECT 'XSLTListsDisplay', COALESCE(value,''), '', 'Enable XSLT stylesheet control over lists pages display on intranet', 'Free'
+         FROM systempreferences WHERE variable='XSLTResultsDisplay';
+    });
+
+    print "Upgrade to $DBversion done (Bug 15485: Allow choosing different XSLTs for lists)\n";
+    SetVersion($DBversion);
+}
+
 
 # DEVELOPER PROCESS, search for anything to execute in the db_update directory
 # SEE bug 13068
