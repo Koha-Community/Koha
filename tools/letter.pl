0527388924b20bb63f38a2a4eccf9cf794faaac7
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2016-06-28 14:37:57 +0100

    Bug 14757: Remove obsolete occurrence of is_tt
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

--- tools/letter.pl
+++ tools/letter.pl
@@ -259,7 +259,6 @@ sub add_validate {
     my @content       = $input->multi_param('content');
     for my $mtt ( @mtt ) {
         my $is_html = $input->param("is_html_$mtt");
-        my $is_tt = $input->param("is_tt_$mtt") ? 1 : 0;
         my $title   = shift @title;
         my $content = shift @content;
         my $letter = C4::Letters::getletter( $oldmodule, $code, $branchcode, $mtt);
