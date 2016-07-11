ae45243fae9e1ca584d72abaf050b1fc94742722
Mark Tompsett <mtompset@hotmail.com>
2016-07-11 00:42:40 -0400

    Bug 16892: Add automatic patron registration via OAuth2 login
    10988 added the ability to log into the OPAC authenticating with
    Google Open ID Connect. This extends it, by allowing an
    unregistered patron to have an account automatically created
    with default category code and branch.
    
    This is accomplished by adding 3 system preferences.
    - GoogleOpenIDConnectAutoRegister
          whether it will attempt to auto-register the patron.
    - GoogleOpenIDConnectDefaultCategory
          This category code will be used to create Google OpenID Connect patrons.
    - GoogleOpenIDConnectDefaultBranch'
          This branch code will be used to create Google OpenID Connect patrons.
    
    Sponsored-by: Tulong Aklatan
    
    Signed-off-by: Eugene Jose Espinoza <eugenegf@yahoo.com>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

---
+++ installer/data/mysql/atomicupdate/bug_16892_AutomaticPatronSystemPreferences.sql
@@ -0,0 +1,4 @@
+INSERT INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type` ) VALUES
+('GoogleOpenIDConnectAutoRegister',   '0',NULL,' Google OpenID Connect logins to auto-register patrons.','YesNo'),
+('GoogleOpenIDConnectDefaultCategory','','','This category code will be used to create Google OpenID Connect patrons.','Textarea'),
+('GoogleOpenIDConnectDefaultBranch',  '','','This branch code will be used to create Google OpenID Connect patrons.','Textarea');
