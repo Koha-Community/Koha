4ba478759e16fa210d61280bfe71be946483ff50
Nick Clemens <nick@bywatersolutions.com>
2016-07-14 11:45:54 -0400

    Bug 16681 - Allow update of opacnote via batch patron modification tool
    To test:
    1 - Select a batch of patrons (via list, saved file, and cardnumber
            entry)
    2 - Verify you cannot edit 'opacnotes' field, nor can you see
    'borrowernotes' field in table, though you can edit it
    3 - Verify editing works as expected
    4 - Apply patch
    5 - Select a batch via each method as above
    6 - Note both note columns display in table
    7 - Note that you can edit or clear the opacnotes field as expected
    8 - Verify other feautres work as previously
    
    Sponsored by:
       Lancaster Theological Seminary (https://lancasterseminary.edu/)
    
    Followed test plan, works as expected.
    Signed-off-by: Marc Véron <veron@veron.ch>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    (cherry picked from commit 81a9e7c88ee44fdfdd2f4f19b52150ce7176b1b7)
    Signed-off-by: Frédéric Demians <f.demians@tamil.fr>

--- tools/modborrowers.pl
+++ tools/modborrowers.pl
@@ -247,6 +247,12 @@ if ( $op eq 'show' ) {
             type => "text",
             mandatory => ( grep /borrowernotes/, @mandatoryFields ) ? 1 : 0,
         }
+        ,
+        {
+            name => "opacnote",
+            type => "text",
+            mandatory => ( grep /opacnote/, @mandatoryFields ) ? 1 : 0,
+        }
     );
 
     $template->param('patron_attributes_codes', \@patron_attributes_codes);
@@ -260,7 +266,7 @@ if ( $op eq 'do' ) {
 
     my @disabled = $input->multi_param('disable_input');
     my $infos;
-    for my $field ( qw/surname firstname branchcode categorycode city state zipcode country sort1 sort2 dateenrolled dateexpiry borrowernotes/ ) {
+    for my $field ( qw/surname firstname branchcode categorycode city state zipcode country sort1 sort2 dateenrolled dateexpiry borrowernotes opacnote/ ) {
         my $value = $input->param($field);
         $infos->{$field} = $value if $value;
         $infos->{$field} = "" if grep { /^$field$/ } @disabled;
