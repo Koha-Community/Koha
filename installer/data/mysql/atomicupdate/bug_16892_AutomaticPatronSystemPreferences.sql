b6e6817a284764ced35dc84a1ee3af461e2a9b0d
Mark Tompsett <mtompset@hotmail.com>
2016-07-19 10:48:35 -0400

    Bug 16892: Follow up with adding missing IGNORE
    Signed-off-by: Eugene Jose Espinoza <eugenegf@yahoo.com>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- installer/data/mysql/atomicupdate/bug_16892_AutomaticPatronSystemPreferences.sql
+++ installer/data/mysql/atomicupdate/bug_16892_AutomaticPatronSystemPreferences.sql
@@ -1,4 +1,4 @@
-INSERT INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type` ) VALUES
+INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type` ) VALUES
 ('GoogleOpenIDConnectAutoRegister',   '0',NULL,' Google OpenID Connect logins to auto-register patrons.','YesNo'),
 ('GoogleOpenIDConnectDefaultCategory','','','This category code will be used to create Google OpenID Connect patrons.','Textarea'),
 ('GoogleOpenIDConnectDefaultBranch',  '','','This branch code will be used to create Google OpenID Connect patrons.','Textarea');
