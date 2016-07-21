0fe7a4aa8e168f173835ddf54cddff898830a270
Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
2016-07-21 08:48:01 +0200

    Bug 16929: [QA Follow-up] Shortcut methods and use statements
    Resolves the following comments:
    
    I'd prefer to see a generate_csrf method than a CSRF flag.
    It'd be better to use instead of require the 2 modules.
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    
    Signed-off-by: Marc <veron@veron.ch>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

--- opac/opac-memberentry.pl
+++ opac/opac-memberentry.pl
@@ -183,8 +183,7 @@ elsif ( $action eq 'update' ) {
 
     my $borrower = GetMember( borrowernumber => $borrowernumber );
     die "Wrong CSRF token"
-        unless Koha::Token->new->check({
-            CSRF   => 1,
+        unless Koha::Token->new->check_csrf({
             id     => $borrower->{userid},
             secret => md5_base64( C4::Context->config('pass') ),
             token  => scalar $cgi->param('csrf_token'),
@@ -202,8 +201,7 @@ elsif ( $action eq 'update' ) {
             empty_mandatory_fields => \@empty_mandatory_fields,
             invalid_form_fields    => $invalidformfields,
             borrower               => \%borrower,
-            csrf_token             => Koha::Token->new->generate({
-                CSRF   => 1,
+            csrf_token             => Koha::Token->new->generate_csrf({
                 id     => $borrower->{userid},
                 secret => md5_base64( C4::Context->config('pass') ),
             }),
@@ -238,8 +236,7 @@ elsif ( $action eq 'update' ) {
                 action => 'edit',
                 nochanges => 1,
                 borrower => GetMember( borrowernumber => $borrowernumber ),
-                csrf_token => Koha::Token->new->generate({
-                    CSRF   => 1,
+                csrf_token => Koha::Token->new->generate_csrf({
                     id     => $borrower->{userid},
                     secret => md5_base64( C4::Context->config('pass') ),
                 }),
@@ -262,8 +259,7 @@ elsif ( $action eq 'edit' ) {    #Display logged in borrower's data
         borrower  => $borrower,
         guarantor => scalar Koha::Patrons->find($borrowernumber)->guarantor(),
         hidden => GetHiddenFields( $mandatory, 'modification' ),
-        csrf_token => Koha::Token->new->generate({
-            CSRF   => 1,
+        csrf_token => Koha::Token->new->generate_csrf({
             id     => $borrower->{userid},
             secret => md5_base64( C4::Context->config('pass') ),
         }),
