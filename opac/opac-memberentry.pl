a503a522c2bfa34f8f64af8dbf2ecba8010b5201
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2016-07-22 08:30:54 +0100

    Bug 16878: Fix XSS in opac-memberentry
    The vars are gotten from the url and sent to the template as it. They
    must be escaped.
    
    Test plan:
    I have not managed to create the original issue, so there is no test
    plan for the XSS fix, but you can confirm there is no regression.
    
    Signed-off-by: Chris Cormack <chris@bigballofwax.co.nz>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Signed-off-by: Brendan Gallagher <brendan@bywatersolutions.com>
    (cherry picked from commit 9bdea2e3691fd62e777cc974f89b867a69eec9a8)
    Signed-off-by: Frédéric Demians <f.demians@tamil.fr>
    (cherry picked from commit b505c6b7d636f262eadef82984b83b5194438724)
    Signed-off-by: Julian Maurice <julian.maurice@biblibre.com>

--- opac/opac-memberentry.pl
+++ opac/opac-memberentry.pl
@@ -20,7 +20,7 @@ use Modern::Perl;
 use CGI qw ( -utf8 );
 use Digest::MD5 qw( md5_base64 md5_hex );
 use String::Random qw( random_string );
-
+use HTML::Entities;
 use C4::Auth;
 use C4::Output;
 use C4::Members;
@@ -328,7 +328,7 @@ sub ParseCgiForBorrower {
     foreach ( $cgi->param ) {
         if ( $_ =~ '^borrower_' ) {
             my ($key) = substr( $_, 9 );
-            $borrower{$key} = $scrubber->scrub( $cgi->param($_) );
+            $borrower{$key} = HTML::Entities::encode( $scrubber->scrub( $cgi->param($_) ) );
         }
     }
 
