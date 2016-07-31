4c41ad91b5866de81fbc87cf196059cf47d4ea33
Julian Maurice <julian.maurice@biblibre.com>
2016-07-31 10:50:35 +0200

    Increment version for 3.22.9 release
    Signed-off-by: Julian Maurice <julian.maurice@biblibre.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -11667,6 +11667,12 @@ if ( CheckVersion($DBversion) ) {
     SetVersion($DBversion);
 }
 
+$DBversion = "3.22.09.000";
+if ( CheckVersion($DBversion) ) {
+    print "Upgrade to $DBversion done (Koha 3.22.9)\n";
+    SetVersion($DBversion);
+}
+
 
 # DEVELOPER PROCESS, search for anything to execute in the db_update directory
 # SEE bug 13068
