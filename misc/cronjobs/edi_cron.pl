af49b42219c6c2fb2f2a52cfb229d37b20b79e84
Colin Campbell <colin.campbell@ptfs-europe.com>
2016-08-17 17:30:55 +0100

    Bug 17141: Call config method to retrieve logdir
    Incorrect method call is causing runtime error and not
    retrieving the correct logdir value
    Change retrieves the value correctly
    
    To test:
    1) Run edi_cron.pl, notice error
    2) Apply patch and run edi_cron.pl again, should work as expected
    
    Signed-off-by: Aleisha Amohia <aleishaamohia@hotmail.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    Note: I did not test but changes make sense.
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

--- misc/cronjobs/edi_cron.pl
+++ misc/cronjobs/edi_cron.pl
@@ -36,7 +36,7 @@ use Koha::EDI qw( process_quote process_invoice process_ordrsp);
 use Koha::Edifact::Transport;
 use Fcntl qw( :DEFAULT :flock :seek );
 
-my $logdir = C4::Context->context('logdir');
+my $logdir = C4::Context->config('logdir');
 
 # logging set to trace as this may be what you
 # want on implementation
