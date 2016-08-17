090c0fc6c49e448b1ee506808e5f157c4ba88414
Kyle M Hall <kyle@bywatersolutions.com>
2016-08-17 18:40:27 +0000

    Bug 14642 - DBRev 16.06.00.015
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -12821,6 +12821,17 @@ if ( CheckVersion($DBversion) ) {
     SetVersion($DBversion);
 }
 
+$DBversion = "16.06.00.015";
+if ( CheckVersion($DBversion) ) {
+    $dbh->do(q{
+        INSERT IGNORE INTO systempreferences ( variable, value, options, explanation, type ) VALUES
+        ('HoldsLog','0',NULL,'If ON, log create/cancel/suspend/resume actions on holds.','YesNo');
+    });
+
+    print "Upgrade to $DBversion done (Bug 14642: Add logging of hold modifications)\n";
+    SetVersion($DBversion);
+}
+
 # DEVELOPER PROCESS, search for anything to execute in the db_update directory
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
