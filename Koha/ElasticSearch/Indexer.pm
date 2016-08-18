c4ab843429ae56df34c69b3ed76a77fe9b7d3fb2
Kyle M Hall <kyle@bywatersolutions.com>
2016-08-18 16:33:00 +0000

    Bug 16272 [QA Followup] - Indexer fails on indexing temparary testing records
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

--- Koha/ElasticSearch/Indexer.pm
+++ Koha/ElasticSearch/Indexer.pm
@@ -177,8 +177,10 @@ sub _sanitise_records {
         # the Tannhauser gate. MARC records where 999$c doesn't match the
         # biblionumber column. All those moments will be lost in time... like
         # tears in rain...
-        $rec->delete_fields($rec->field('999'));
-        $rec->append_fields(MARC::Field->new('999','','','c' => $bibnum, 'd' => $bibnum));
+        if ( $rec ) {
+            $rec->delete_fields($rec->field('999'));
+            $rec->append_fields(MARC::Field->new('999','','','c' => $bibnum, 'd' => $bibnum));
+        }
     }
 }
 
