4413708e97d412db8ab7e4b57f3baee69231d9f9
Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
2016-08-23 16:33:10 +0200

    Bug 14390: [Follow-up] Only update FU record in UpdateFine
    Exclude O, F and M when outstanding == 0.
    Check if the issue_id points to a FU record.
    
    Note: We only warn now when we see a second FU record with this issue id.
    That should be a rare exception. As before, we are just counting it in
    our total. Added a FIXME.
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Tested fine on overdue. Renewed and backdated for a second fine. The F
    and FU can be seen on the Fines tab and are totaled on Check out.
    
    Signed-off-by: Jacek Ablewicz <abl@biblos.pk.edu.pl>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    (cherry picked from commit fa48df281d68fe1d27cc6fb10b41e4504a70afb9)
    Signed-off-by: Frédéric Demians <f.demians@tamil.fr>
    (cherry picked from commit 0894b24c3d6e7d915871bd56a22ff9a10741b7b9)
    Signed-off-by: Julian Maurice <julian.maurice@biblibre.com>
    
    Conflicts:
    	C4/Overdues.pm

--- C4/Overdues.pm
+++ C4/Overdues.pm
@@ -523,8 +523,9 @@ sub UpdateFine {
 	#   "REF" is Cash Refund
     my $sth = $dbh->prepare(
         "SELECT * FROM accountlines
-        WHERE borrowernumber=?
-        AND   accounttype IN ('FU','O','F','M')"
+        WHERE borrowernumber=? AND
+        (( accounttype IN ('O','F','M') AND amountoutstanding<>0 ) OR
+           accounttype = 'FU' )"
     );
     $sth->execute( $borrowernumber );
     my $data;
@@ -535,9 +536,10 @@ sub UpdateFine {
     # - accumulate fines for other items
     # so we can update $itemnum fine taking in account fine caps
     while (my $rec = $sth->fetchrow_hashref) {
-        if ($rec->{itemnumber} == $itemnum && $rec->{description} =~ /$due_qr/) {
+        if ($rec->{itemnumber} == $itemnum && $rec->{description} =~ /$due_qr/ && $rec->{accounttype} eq 'FU') {
             if ($data) {
                 warn "Not a unique accountlines record for item $itemnum borrower $borrowernumber";
+                #FIXME Should we still count this one in total_amount ??
             } else {
                 $data = $rec;
                 next;
