1b43b3d7acaf04e1822cdb199946aa04215b84fb
Kyle M Hall <kyle@bywatersolutions.com>
2016-09-09 13:26:20 +0000

    Bug 16978 - DBRev 16.06.00.022
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

--- installer/data/mysql/atomicupdate/bug_16978.sql
+++
@@ -1,9 +0,0 @@
-INSERT IGNORE INTO `permissions`
-    (module_bit, code,             description) VALUES
-    (16,         'delete_reports', 'Delete SQL reports');
-
-INSERT IGNORE INTO user_permissions
-    (borrowernumber,      module_bit,code)
-    SELECT borrowernumber,module_bit,'delete_reports'
-        FROM user_permissions
-        WHERE module_bit=16 AND code='create_reports';
--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -12907,6 +12907,25 @@ if ( CheckVersion($DBversion) ) {
     SetVersion($DBversion);
 }
 
+$DBversion = "16.06.00.022";
+if ( CheckVersion($DBversion) ) {
+    $dbh->do(q{
+        INSERT IGNORE INTO `permissions`
+        (module_bit, code,             description) VALUES
+        (16,         'delete_reports', 'Delete SQL reports');
+    });
+    $dbh->do(q{
+        INSERT IGNORE INTO user_permissions
+        (borrowernumber,      module_bit,code)
+        SELECT borrowernumber,module_bit,'delete_reports'
+            FROM user_permissions
+            WHERE module_bit=16 AND code='create_reports';
+    });
+
+    print "Upgrade to $DBversion done (Bug 16978 - Add delete reports user permission)\n";
+    SetVersion($DBversion);
+}
+
 # DEVELOPER PROCESS, search for anything to execute in the db_update directory
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
