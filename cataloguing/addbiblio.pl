ba8ee98e1bb3b18f579c0b122b14d43e88cda2c8
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2016-09-13 15:03:55 +0100

    Bug 16358: Do not redirect to the editor when deleting records
    The "Delete record" button goes to addbiblio.pl?op=delete, which
    incorrectly redirects to Rancor when selected.
    
    Test plan:
    0/ Enable EnableAdvancedCatalogingEditor
    1/ Edit a record
    2/ Switch to the Advanced editor
    3/ click on "View the record"
    4/ Delete the record
    => Without this patch you are redirected to the editor
    => With this patch you are redirected to the advanced search
    
    Signed-off-by: Mirko Tietgen <mirko@abunchofthings.net>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    
    Signed-off-by: Brendan Gallagher <brendan@bywatersolutions.com>

--- cataloguing/addbiblio.pl
+++ cataloguing/addbiblio.pl
@@ -736,11 +736,12 @@ if ($frameworkcode eq 'FA'){
         'stickyduedate'      => $fa_stickyduedate,
         'duedatespec'        => $fa_duedatespec,
     );
-} elsif ( C4::Context->preference('EnableAdvancedCatalogingEditor') && $input->cookie( 'catalogue_editor_' . $loggedinuser ) eq 'advanced' && !$breedingid ) {
+} elsif ( $op ne "delete" && C4::Context->preference('EnableAdvancedCatalogingEditor') && $input->cookie( 'catalogue_editor_' . $loggedinuser ) eq 'advanced' && !$breedingid ) {
     # Only use the advanced editor for non-fast-cataloging.
     # breedingid is not handled because those would only come off a Z39.50
     # search initiated by the basic editor.
     print $input->redirect( '/cgi-bin/koha/cataloguing/editor.pl' . ( $biblionumber ? ( '#catalog/' . $biblionumber ) : '' ) );
+    exit;
 }
 
 my $frameworkcodeloop = Koha::BiblioFrameworks->search({}, { order_by => ['frameworktext'] });
