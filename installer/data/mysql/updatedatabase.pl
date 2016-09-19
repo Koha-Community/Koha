4ea484677e7049ddb835fcc6d6f24a801f2eb6ba
Mark Tompsett <mtompset@hotmail.com>
2016-09-19 14:27:14 -0400

    Bug 17234: Follow up to handle new problems
    Bug 16276 added two more ALTER IGNORES. This removes them.
    
    Signed-off-by: Mark Tompsett <mtompset@hotmail.com>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -12984,10 +12984,10 @@ if ( CheckVersion($DBversion) ) {
 $DBversion = "16.06.00.027";
 if ( CheckVersion($DBversion) ) {
     $dbh->do(q{
-        ALTER IGNORE TABLE borrowers ADD COLUMN lastseen datetime default NULL AFTER updated_on;
+        ALTER TABLE borrowers ADD COLUMN lastseen datetime default NULL AFTER updated_on;
     });
     $dbh->do(q{
-        ALTER IGNORE TABLE deletedborrowers ADD COLUMN lastseen datetime default NULL AFTER updated_on;
+        ALTER TABLE deletedborrowers ADD COLUMN lastseen datetime default NULL AFTER updated_on;
     });
     $dbh->do(q{
         INSERT IGNORE INTO systempreferences (variable,value,explanation,options,type) VALUES ('TrackLastPatronActivity', '0', 'If set, the field borrowers.lastseen will be updated everytime a patron is seen', NULL, 'YesNo');
