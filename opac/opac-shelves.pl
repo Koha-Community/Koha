db049cda6f01266a967b559f1ee506fb3800d024
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2016-09-20 09:55:25 +0100

    Bug 17316: Do not display the list's name if the user does not have permission - OPAC
    At the OPAC, if a user manipulate the URL to show a list (s)he is not
    allowed to view, the list's name will be displayed anyway.
    
    Test plan:
    - Create a private list with user A
    - Copy the op=view URL and access it with user B logged in
    => Without this patch, you will see the rss icon, the list's name and
    the "add list" button
    => Without this patch, only the "unauthorized" box will be displayed
    
    Followed test plan, works as expected.
    Signed-off-by: Marc Véron <veron@veron.ch>
    
    Signed-off-by: Katrin Fischer  <katrin.fischer@bsz-bw.de>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    (cherry picked from commit 31ca4849efd5539c6b287047ce2da560be7e1c53)
    Signed-off-by: Frédéric Demians <f.demians@tamil.fr>
    (cherry picked from commit d46322c7cd30f6be6c6589de79577b720a408d3b)
    Signed-off-by: Julian Maurice <julian.maurice@biblibre.com>

--- opac/opac-shelves.pl
+++ opac/opac-shelves.pl
@@ -324,6 +324,7 @@ if ( $op eq 'view' ) {
             }
         } else {
             push @messages, { type => 'error', code => 'unauthorized_on_view' };
+            undef $shelf;
         }
     } else {
         push @messages, { type => 'error', code => 'does_not_exist' };
