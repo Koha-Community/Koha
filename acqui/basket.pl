f3889059133d5e9933325c98e172d4d0b1e69c11
Marc <veron@veron.ch>
2016-10-06 14:37:14 +0200

    Bug 17411 - Change exit 1 to exit 0 in acqui/basket.pl to prevent Internal Server Error
    Note: Same situation as in Bug 17403
    
    To test:
    - Verifiy that code change makes sense.
    
    Note: Same situation as in Bug 17403
    Signed-off-by: David Cook <dcook@prosentient.com.au>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    Amended patch: Remove unecessary comment
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    (cherry picked from commit 01636dca2f1bd132fcc8d16f7a91fa4a45a90668)
    Signed-off-by: Frédéric Demians <f.demians@tamil.fr>

--- acqui/basket.pl
+++ acqui/basket.pl
@@ -233,7 +233,7 @@ elsif ( $op eq 'ediorder' ) {
               || ( $basket->{branch}  eq '' );
             unless ($validtest) {
                 print $query->redirect("../mainpage.pl");
-                exit 1;
+                exit 0;
             }
         }
         if (!defined $basket->{branch} or $basket->{branch} eq $userenv->{branch}) {
