a2d0b69bf6f1baf9c7248b6d36b64c25dbc0f3dd
Chris Cormack <chrisc@catalyst.net.nz>
2016-10-19 08:53:26 +1300

    Bug 17459: Follow up
    Adding POD and --userid and --password options
    
    1/ To test, use the same routine as before, with no options.
    2/ You should have a user with koha/koha as userid and passwords
    3/ Delete that user
    4/ Run the script with --userid <userid> --password <password>
    5/ You should have a user in koha with userid/password set
    
    Signed-off-by: Josef Moravec <josef.moravec@gmail.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    (cherry picked from commit f9da6f9739b334a1e14b75a1aa49b2f5569b032e)
    Signed-off-by: Frédéric Demians <f.demians@tamil.fr>

--- misc/devel/create_superlibrarian.pl
+++ misc/devel/create_superlibrarian.pl
@@ -17,8 +17,9 @@
 # You should have received a copy of the GNU General Public License
 # along with Koha; if not, see <http://www.gnu.org/licenses>.
 
-
 use Modern::Perl;
+use Getopt::Long;
+use Pod::Usage;
 
 use C4::Installer;
 use C4::Context;
@@ -32,18 +33,71 @@ use Koha::Patron::Categories;
 my $library         = Koha::Libraries->search->next;
 my $patron_category = Koha::Patron::Categories->search->next;
 
-die "Not enough data in the database, library and/or patron category does not exist"
+die
+"Not enough data in the database, library and/or patron category does not exist"
   unless $library and $patron_category;
 
-die "A patron with userid 'koha' already exists"   if Koha::Patrons->find( { userid     => 'koha' } );
-die "A patron with cardnumber '42' already exists" if Koha::Patrons->find( { cardnumber => 'koha' } );
+die "A patron with userid 'koha' already exists"
+  if Koha::Patrons->find( { userid => 'koha' } );
+die "A patron with cardnumber '42' already exists"
+  if Koha::Patrons->find( { cardnumber => 'koha' } );
+
+my $userid   = 'koha';
+my $password = 'koha';
+my $help;
+
+GetOptions(
+    'help|?'   => \$help,
+    'userid=s'   => \$userid,
+    'password=s' => \$password
+);
+
+pod2usage(1) if $help;
 
 AddMember(
     surname      => 'koha',
-    userid       => 'koha',
+    userid       => $userid,
     cardnumber   => 42,
     branchcode   => $library->branchcode,
     categorycode => $patron_category->categorycode,
-    password     => 'koha',
+    password     => $password,
     flags        => 1,
 );
+
+=head1 NAME
+
+create_superlibrarian.pl - create a user in Koha with superlibrarian permissions
+
+=head1 SYNOPSIS
+
+create_superlibrarian.pl
+  [ --userid <userid> ] [ --password <password> ]
+
+ Options:
+   -?|--help        brief help message
+   --userid         specify the userid to be set (defaults to koha)
+   --password       specify the password to be set (defaults to koha)
+
+=head1 OPTIONS
+
+=over 8
+
+=item B<--help|-?>
+
+Print a brief help message and exits
+
+=item B<--userid>
+
+Allows you to specify the userid to be set in the database
+
+=item B<--password>
+
+Allows you to specify the password to be set in the database
+
+=back
+
+=head1 DESCRIPTION
+
+A simple script to create a user in the Koha database with superlibrarian permissions
+
+=cut
