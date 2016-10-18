eb26c85b13f0db04693bea5161446ec060ad2016
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2016-10-18 13:06:55 +0100

    Bug 17453: Add pref OPACHoldsIfAvailableAtPickup
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    
    https://bugs.koha-community.org/show_bug.cgi?id=14753

---
+++ installer/data/mysql/atomicupdate/bug_17453.sql
@@ -0,0 +1,2 @@
+INSERT IGNORE INTO systempreferences (variable,value,explanation,options,type) VALUES
+('OPACHoldsIfAvailableAtPickup','1','','Allow to pickup up holds at libraries where the item is available','YesNo');
