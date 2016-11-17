1201f42e492dee0c788911499187f0793a1edfba
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2016-11-17 08:58:55 +0000

    Bug 17391: Add atomic update file
    Signed-off-by: Jonathan Druart <jonathan.druart@biblibre.com>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    (cherry picked from commit d785cd5b0a025dd2670c7259fd20038f450ab1ef)
    Signed-off-by: Julian Maurice <julian.maurice@biblibre.com>

---
+++ installer/data/mysql/atomicupdate/bug_17391.sql
@@ -0,0 +1,2 @@
+INSERT INTO systempreferences (variable,value,options,explanation,type) VALUES ('ReplytoDefault',  '',  NULL,  'The default email address to be set as replyto.',  'Free');
+INSERT INTO systempreferences (variable,value,options,explanation,type) VALUES ('ReturnpathDefault',  '',  NULL,  'The default email address to be set as return-path',  'Free');
