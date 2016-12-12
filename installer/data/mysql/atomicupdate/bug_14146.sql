66d322d9199782135895e7478b515c5c0eb8b9a9
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2016-12-12 16:03:01 +0100

    Bug 14146: Add the new pref CumulativeRestrictionPeriods
    Sponsored-by: Orex Digital
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

---
+++ installer/data/mysql/atomicupdate/bug_14146.sql
@@ -0,0 +1,2 @@
+INSERT IGNORE INTO systempreferences (variable,value,options,explanation,type)
+VALUES ('CumulativeRestrictionPeriods',  0,  NULL,  'Cumulate the restriction periods instead of keeping the highest',  'YesNo');
