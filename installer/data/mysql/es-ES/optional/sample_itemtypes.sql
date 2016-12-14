ab6f1dc9629cf0b2bfa843f83c6c9e4b3b9733ab
vinz3g <lvonti51@gmail.com>
2016-12-14 00:16:40 -0600

    Update sample_itemtypes.sql
    Actualización del término, materiales mezclados

--- installer/data/mysql/es-ES/optional/sample_itemtypes.sql
+++ installer/data/mysql/es-ES/optional/sample_itemtypes.sql
@@ -1,6 +1,6 @@
 INSERT INTO itemtypes (itemtype, description, rentalcharge, notforloan, imageurl, summary) VALUES
 ('BK', 'Libros',5,0,'bridge/book.gif',''),
-('MX', 'Mixed Materials',5,0,'bridge/kit.gif',''),
+('MX', 'Materiales mezclados',5,0,'bridge/kit.gif',''),
 ('CF', 'Archivos de computadoras',5,0,'bridge/computer_file.gif',''),
 ('MP', 'Mapas',5,0,'bridge/map.gif',''),
 ('VM', 'Materiales visuales',5,1,'bridge/dvd.gif',''),
