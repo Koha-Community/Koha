9185e2a9a9827a8831e5357704227dc91c16d3af
vinz3g <lvonti51@gmail.com>
2016-12-14 00:05:54 -0600

    Update patron_categories.sql
    Traducción al español

--- installer/data/mysql/es-ES/optional/patron_categories.sql
+++ installer/data/mysql/es-ES/optional/patron_categories.sql
@@ -1,23 +1,23 @@
 INSERT INTO `categories` (`categorycode`, `description`, `enrolmentperiod`, `upperagelimit`, `dateofbirthrequired`, `finetype`, `bulk`, `enrolmentfee`, `overduenoticerequired`, `issuelimit`, `reservefee`, `category_type`) VALUES 
 
 -- Adult Patrons
-('PT','Patron',99,999,18,NULL,NULL,'0.000000',1,NULL,'0.000000','A'),
-('ST','Student',99,999,18,NULL,NULL,'0.000000',1,NULL,'0.000000','A'),
-('HB','Home Bound',99,999,18,NULL,NULL,'0.000000',1,NULL,'0.000000','A'),
+('PT','Usuario',99,999,18,NULL,NULL,'0.000000',1,NULL,'0.000000','A'),
+('ST','Estudiante',99,999,18,NULL,NULL,'0.000000',1,NULL,'0.000000','A'),
+('HB','Domicilio',99,999,18,NULL,NULL,'0.000000',1,NULL,'0.000000','A'),
 
 -- Children
-('K','Kid',99,17,5,NULL,NULL,'0.000000',1,NULL,'0.000000','C'),
-('J','Juvenile',99,17,5,NULL,NULL,'0.000000',1,NULL,'0.000000','C'),
-('YA','Young Adult',99,17,5,NULL,NULL,'0.000000',1,NULL,'0.000000','C'),
+('K','Niño',99,17,5,NULL,NULL,'0.000000',1,NULL,'0.000000','C'),
+('J','Juvenil',99,17,5,NULL,NULL,'0.000000',1,NULL,'0.000000','C'),
+('YA','Adulto joven',99,17,5,NULL,NULL,'0.000000',1,NULL,'0.000000','C'),
 
 -- Professionals
-('T','Teacher',99,999,18,NULL,NULL,'0.000000',0,NULL,'0.000000','P'),
+('T','Maestro',99,999,18,NULL,NULL,'0.000000',0,NULL,'0.000000','P'),
 ('B','Board',99,17,5,NULL,NULL,'0.000000',1,NULL,'0.000000','P'),
 
 -- Institutional
-('IL','Inter-Library Loan',99,999,18,NULL,NULL,'0.000000',1,NULL,'0.000000','I'),
-('SC','School',99,999,18,NULL,NULL,'0.000000',1,NULL,'0.000000','I'),
-('L','Library',99,999,18,NULL,NULL,'0.000000',1,NULL,'0.000000','I'),
+('IL','Préstamo interbibliotecario',99,999,18,NULL,NULL,'0.000000',1,NULL,'0.000000','I'),
+('SC','Escuela',99,999,18,NULL,NULL,'0.000000',1,NULL,'0.000000','I'),
+('L','Biblioteca',99,999,18,NULL,NULL,'0.000000',1,NULL,'0.000000','I'),
 
 -- Staff
-('S','Staff',99,999,18,NULL,NULL,'0.000000',0,NULL,'0.000000','S');
+('S','Empleados',99,999,18,NULL,NULL,'0.000000',0,NULL,'0.000000','S');
