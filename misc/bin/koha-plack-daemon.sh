9a959c254872bb1e9c90b4cabc817cd72d8e8682
Olli-Antti Kivilahti <olli-antti.kivilahti@jns.fi>
2016-12-28 12:28:41 +0200

    KD-0 Plack for dev-install

---
+++ misc/bin/koha-plack-daemon.sh
@@ -0,0 +1,327 @@
+#!/bin/bash
+#
+# Copyright 2015 Theke Solutions
+#
+# This file is part of Koha.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+### BEGIN INIT INFO
+# Provides:          koha-plack-daemon
+# Required-Start:    $remote_fs
+# Required-Stop:     $remote_fs
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Plack server daemon for fast http-handling
+### END INIT INFO
+
+set -e
+
+. /lib/lsb/init-functions
+
+
+HELPER_FUNCTIONS="__PERL_MODULE_DIR__/debian/scripts/koha-functions.sh"
+
+RUNDIR="/var/run/koha"
+LOGDIR="__LOG_DIR__"
+PIDFILE="$RUNDIR/plack.pid"
+PLACKSOCKET="$RUNDIR/plack.sock"
+PSGIFILE="__PERL_MODULE_DIR__/misc/plack/plack.psgi"
+MODE="deployment" #development|deployment|test
+LISTEN=":5000"
+#User and group must be the same as for apache2, eg. www-data. This is to prevent nasty surprises with priviledge escalation.
+#And so programs can communicate via the same unix-socket
+USER="www-data"
+GROUP="www-data"
+
+
+#Set Environment for koha-plack-daemon.sh
+export KOHA_CONF="__KOHA_CONF_DIR__/koha-conf.xml"
+export INTRANET_CGI_DIR="__INTRANET_CGI_DIR__"
+export OPAC_CGI_DIR="__OPAC_CGI_DIR__/opac"
+export PERL_MODULE_DIR="__PERL_MODULE_DIR__"
+export PLACK_DEBUG #This is set if debug-parameter is given or if mode is development
+
+
+#Make sure the pid-dir and log-dir exists
+mkdir -p $RUNDIR
+mkdir -p $LOGDIR
+
+# include helper functions
+if [ -f "$HELPER_FUNCTIONS" ]; then
+    . "$HELPER_FUNCTIONS"
+else
+    echo "Error: $HELPER_FUNCTIONS not present." 1>&2
+    exit 1
+fi
+
+usage()
+{
+    local scriptname=$(basename $0)
+
+    cat <<EOF
+$scriptname
+
+This script lets you manage the plack daemon for your Koha instance.
+
+Usage:
+$scriptname start|stop|restart|reload [development|deployement|test] [debug] [debugger]
+$scriptname -h|--help
+
+    start               Start the plack daemon for the specified instances
+    stop                Stop the plack daemon for the specified instances
+    restart             Restart the plack daemon for the specified instances
+    reload              Reload the application without losing connections
+    debug               Trigger PLACK_DEBUG -environment variable
+    debugger            Start perl with the -d -flag
+    deployment          Set the plack mode
+    development         Set the plack mode
+    test                Set the plack mode
+    --help|-h           Display this help message
+
+EXAMPLE
+
+bash -x /etc/init.d/koha-plack-daemon start development debug debugger
+
+EOF
+}
+
+start_plack()
+{
+    _check_and_fix_perms
+
+    DEBUGGER_STR=""
+    test "$DEBUGGER" == "1" && DEBUGGER_STR="/usr/bin/perl -d "
+
+    STARMANOPTS="-M FindBin \
+                 --user=$USER --group=$GROUP \
+                 --pid ${PIDFILE} \
+                 -E $MODE \
+                 ${PSGIFILE} "
+
+    test "$MODE" != "deployment" && STARMANOPTS="$STARMANOPTS --workers 1"
+
+    test "$MODE" == "deployment" && STARMANOPTS="$STARMANOPTS --daemonize \
+                                                              --max-requests 50 \
+                                                              --workers 2 \
+                                                              --access-log $LOGDIR/plack.log \
+                                                              --error-log $LOGDIR/plack-error.log \
+                                                "
+
+    test -n "$PLACKSOCKET" && STARMANOPTS="$STARMANOPTS --listen ${PLACKSOCKET} "
+
+    test -n "$LISTEN" && STARMANOPTS="$STARMANOPTS --listen ${LISTEN} "
+
+
+
+    if ! is_plack_running; then
+        log_daemon_msg "Starting Plack daemon"
+
+        if ${DEBUGGER_STR} ${STARMAN} ${STARMANOPTS}; then
+            log_end_msg 0
+        else
+            log_end_msg 1
+        fi
+    else
+        log_daemon_msg "Error: Plack already running"
+        log_end_msg 1
+    fi
+}
+
+stop_plack()
+{
+    if is_plack_running; then
+
+        log_daemon_msg "Stopping Plack daemon"
+
+        if start-stop-daemon --pidfile ${PIDFILE} --stop; then
+            log_end_msg 0
+        else
+            log_end_msg 1
+        fi
+    else
+        log_daemon_msg "Error: Plack not running"
+        log_end_msg 1
+    fi
+}
+
+restart_plack()
+{
+    if is_plack_running; then
+
+        log_daemon_msg "Restarting Plack daemon"
+
+        if stop_plack && start_plack; then
+            log_end_msg 0
+        else
+            log_end_msg 1
+        fi
+    else
+        log_daemon_msg "Error: Plack not running"
+        log_end_msg 1
+    fi
+}
+
+#Starman says he supports hot-reload. Maybe others do, but I only love starman.
+reload_starman()
+{
+    if is_plack_running; then
+
+        log_daemon_msg "Hot-reloading starman daemon !!!"
+
+        if start-stop-daemon --pidfile ${PIDFILE} --stop --signal HUP; then
+            log_end_msg 0
+        else
+            log_end_msg 1
+        fi
+    else
+        log_daemon_msg "Error: starman not running"
+        log_end_msg 1
+    fi
+}
+
+check_env_and_warn()
+{
+    local apache_version_ok="no"
+    local required_modules="headers proxy_http"
+    local missing_modules=""
+
+    if /usr/sbin/apache2ctl -v | grep -q "Server version: Apache/2.4"; then
+        apache_version_ok="yes"
+    fi
+
+    for module in ${required_modules}; do
+        if ! /usr/sbin/apachectl -M 2> /dev/null | grep -q ${module}; then
+            missing_modules="${missing_modules}${module} "
+        fi
+    done
+
+    if [ "${apache_version_ok}" != "yes" ]; then
+        warn "WARNING: koha-plack requires Apache 2.4.x and you don't have that."
+    fi
+
+    if [ "${missing_modules}" != "" ]; then
+        cat 1>&2 <<EOM
+WARNING: koha-plack requires some Apache modules that you are missing.
+You can install them with:
+
+    sudo a2enmod ${missing_modules}
+
+EOM
+
+    fi
+}
+
+_check_and_fix_perms()
+{
+    local files="/var/log/koha/plack.log \
+                 /var/log/koha/plack-error.log"
+
+    for file in ${files}
+    do
+        if [ ! -e "${file}" ]; then
+            touch ${file}
+        fi
+        chown "koha":"koha" ${file}
+    done
+}
+
+set_action()
+{
+    if [ "$op" = "" ]; then
+        op=$1
+    else
+        die "Error: only one action can be specified."
+    fi
+}
+
+STARMAN=$(which starman)
+op=""
+quiet="no"
+
+# Read command line parameters
+while [ $# -gt 0 ]; do
+
+    case "$1" in
+        -h|--help)
+            usage ; exit 0 ;;
+        start)
+            set_action "start"
+            shift ;;
+        stop)
+            set_action "stop"
+            shift ;;
+        restart)
+            set_action "restart"
+            shift ;;
+        reload)
+            set_action "reload"
+            shift ;;
+        debug)
+            export PLACK_DEBUG=1
+            shift ;;
+        debugger)
+            DEBUGGER="1"
+            shift ;;
+        deployment)
+            MODE="deployment"
+            shift ;;
+        development)
+            MODE="development"
+            export PLACK_DEBUG=1
+            shift ;;
+        test)
+            MODE="test"
+            shift ;;
+        -*)
+            die "Error: invalid option switch ($1)" ;;
+        *)
+            # We expect the remaining stuff are the instance names
+            break ;;
+    esac
+
+done
+
+if [ -z $PERL5LIB ]; then
+    PERL5LIB="__INTRANET_CGI_DIR__"
+fi
+export PERL5LIB
+
+check_env_and_warn
+
+case $op in
+    "start")
+        start_plack
+        ;;
+    "stop")
+        stop_plack
+        ;;
+    "restart")
+        restart_plack
+        ;;
+    "reload")
+        reload_starman
+        ;;
+    "enable")
+        enable_plack
+        ;;
+    "disable")
+        disable_plack
+        ;;
+    *)
+        usage
+        ;;
+esac
+
+exit 0
---
+++ misc/plack/plack.psgi
@@ -0,0 +1,114 @@
+#!/usr/bin/perl
+
+# This file is part of Koha.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use Modern::Perl;
+
+use lib($ENV{PERL_MODULE_DIR});
+use lib($ENV{PERL_MODULE_DIR}.'/installer');
+
+use Plack::Builder;
+use Plack::App::CGIBin;
+use Plack::App::Directory;
+use Plack::App::URLMap;
+use Plack::Request;
+
+use Mojo::Server::PSGI;
+
+# Pre-load libraries
+use C4::Boolean;
+use C4::Koha;
+use C4::Languages;
+use C4::Letters;
+use C4::Members;
+use C4::XSLT;
+use Koha::Caches;
+use Koha::Cache::Memory::Lite;
+use Koha::Database;
+use Koha::DateUtils;
+
+use CGI qw(-utf8 ); # we will loose -utf8 under plack, otherwise
+{
+    no warnings 'redefine';
+    my $old_new = \&CGI::new;
+    *CGI::new = sub {
+        my $q = $old_new->( @_ );
+        $CGI::PARAM_UTF8 = 1;
+        Koha::Caches->flush_L1_caches();
+        Koha::Cache::Memory::Lite->flush();
+        return $q;
+    };
+}
+
+use Devel::Size 0.77; # 0.71 doesn't work for Koha
+my $watch_capture_regex = '(C4|Koha)';
+
+sub watch_for_size {
+        my @watch =
+        map { s/^.*$watch_capture_regex/$1/; s/\//::/g; s/\.pm$//; $_ } # fix paths
+        grep { /$watch_capture_regex/ }
+        keys %INC
+        ;
+        warn "# watch_for_size ",join(' ',@watch);
+        return @watch;
+};
+
+
+
+
+
+my $intranet = Plack::App::CGIBin->new(
+    root => $ENV{INTRANET_CGI_DIR}
+)->to_app;
+
+my $opac = Plack::App::CGIBin->new(
+    root => $ENV{OPAC_CGI_DIR}
+)->to_app;
+
+my $apiv1  = builder {
+    my $server = Mojo::Server::PSGI->new;
+    $server->load_app($ENV{PERL_MODULE_DIR}.'/api/v1/app.pl');
+    $server->to_psgi_app;
+};
+
+builder {
+    enable "ReverseProxy";
+    enable "Plack::Middleware::Static";
+    # + is required so Plack doesn't try to prefix Plack::Middleware::
+    enable "+Koha::Middleware::SetEnv";
+
+    ### INTRODUCING DEBUG PLUGINS ###
+    # don't enable this plugin in production, since stack traces reveal too much information
+    # about system to potential attackers!
+    enable_if { $ENV{PLACK_DEBUG} } 'StackTrace';
+    # please don't use plugins which are under enable_if $ENV{PLACK_DEBUG} in production!
+    # they are known to leek memory
+    enable_if { $ENV{PLACK_DEBUG} } 'Debug',  panels => [
+            qw(Environment Response Timer Memory),
+            # optional plugins (uncomment to enable) are sorted according to performance implact
+#           [ 'Devel::Size', for => \&watch_for_size ], # https://github.com/dpavlin/p5-plack-devel-debug-devel-size
+#           [ 'DBIProfile', profile => 2 ],
+#           [ 'DBITrace', level => 1 ], # a LOT of fine-graded SQL trace
+#           [ 'Profiler::NYTProf', exclude => [qw(.*\.css .*\.png .*\.ico .*\.js .*\.gif)] ],
+    ];
+
+
+    ##Mount apps to endpoints
+    mount '/opac'          => $opac;
+    mount '/intranet'      => $intranet;
+    mount '/api/v1/app.pl' => $apiv1;
+
+};
