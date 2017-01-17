a8ca6c089fec9b487ff322785e359f388059230a
Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
2017-01-17 16:49:40 +0100

    Bug 17925: Disable debugging in reports/bor_issues_top.pl
    Remove setting $debug to 1. This prevents creation of file
    tmp/bor_issues_top.debug.log.
    Enable warnings pragma.
    Resolve a uninitialized warning on $sep like:
    Use of uninitialized value $CGI::Compile::ROOT::usr_share_koha_masterclone_reports_bor_issues_top_2epl::sep in string eq at /usr/share/koha/masterclone/reports/bor_issues_top.pl line 66.
    
    Test plan:
    Restart Plack (as you always do when testing).
    Remove file /tmp/bor_issues_top.debug.log.
    Open Reports/Patrons with the most checkouts.
    Check if the file in /tmp has not been created again.
    Check the log for warnings after you restarted Plack.
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Signed-off-by: Mason James <mtj@kohaaloha.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    (cherry picked from commit e739fecbc83ff0ee32f155ae7e74c4780a5201c2)
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>

--- reports/bor_issues_top.pl
+++ reports/bor_issues_top.pl
@@ -17,8 +17,8 @@
 # You should have received a copy of the GNU General Public License
 # along with Koha; if not, see <http://www.gnu.org/licenses>.
 
-use strict;
-#use warnings; FIXME - Bug 2505
+use Modern::Perl;
+
 use CGI qw ( -utf8 );
 use C4::Auth;
 use C4::Output;
@@ -40,7 +40,6 @@ plugin that shows a stats on borrowers
 
 =cut
 
-$debug = 1;
 $debug and open DEBUG, ">/tmp/bor_issues_top.debug.log";
 
 my $input = new CGI;
@@ -62,7 +61,7 @@ my ($template, $borrowernumber, $cookie)
                 flagsrequired => {reports => '*'},
                 debug => 1,
                 });
-our $sep     = $input->param("sep");
+our $sep     = $input->param("sep") || C4::Context->preference('delimiter') || ',';
 $sep = "\t" if ($sep eq 'tabulation');
 $template->param(do_it => $do_it,
         );
