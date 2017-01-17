288304c25ee1354b6514b3911faaa8e8ca45a35b
Mark Tompsett <mtompset@hotmail.com>
2017-01-17 06:28:25 +0000

    Bug 17919: (bug 17466 follow-up) Koha::Issue is now called Koha::Checkout
    Ignoring the pluralization or lack thereof:
    $ git grep "Koha::Issue"
    -- This has results.
    But this was renamed by 17796, and supposedly still
    in discussion in 16870.
    
    apply this patch, and do the same git.
    There will be no results.
    
    This was found while running a prove t/db_dependent/00-strict.t
    
    Signed-off-by: Claire Gravely <claire_gravely@hotmail.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@biblibre.com>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

--- circ/returns.pl
+++ circ/returns.pl
@@ -50,7 +50,7 @@ use C4::RotatingCollections;
 use Koha::AuthorisedValues;
 use Koha::DateUtils;
 use Koha::Calendar;
-use Koha::Issues;
+use Koha::Checkouts;
 
 my $query = new CGI;
 
@@ -579,7 +579,7 @@ foreach ( sort { $a <=> $b } keys %returneditems ) {
             $ri{bortitle}       = $b->{'title'};
             $ri{bornote}        = $b->{'borrowernotes'};
             $ri{borcategorycode}= $b->{'categorycode'};
-            $ri{borissuescount} = Koha::Issues->count( { borrowernumber => $b->{'borrowernumber'} } );
+            $ri{borissuescount} = Koha::Checkouts->count( { borrowernumber => $b->{'borrowernumber'} } );
         }
         else {
             $ri{borrowernumber} = $riborrowernumber{$_};
