6abf904bffbf433b26a6da904fec2b1800274c20
Kyle M Hall <kyle@bywatersolutions.com>
2017-02-06 17:43:47 +0000

    Bug 17960 - DBRev 16.12.00.007
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -13861,6 +13861,25 @@ if ( CheckVersion($DBversion) ) {
     SetVersion($DBversion);
 }
 
+$DBversion = "16.12.00.007";
+if( CheckVersion( $DBversion ) ) {
+
+    if ( column_exists('opac_news', 'new' ) ) {
+        $dbh->do(q|ALTER TABLE opac_news CHANGE COLUMN new content text NOT NULL|);
+    }
+
+    my ( $used_in_templates ) = $dbh->selectrow_array(q|
+        SELECT COUNT(*) FROM letter WHERE content LIKE "%<<opac_news.new>>%";
+    |);
+    if ( $used_in_templates ) {
+        print "WARNING - It seems that you are using the opac_news.new column in your notice templates\n";
+        print "Since it has now been renamed with opac_news.content, you should update them.\n";
+    }
+
+    SetVersion( $DBversion );
+    print "Upgrade to $DBversion done (Bug 17960 - Rename opac_news with opac_news.content)\n";
+}
+
 # DEVELOPER PROCESS, search for anything to execute in the db_update directory
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
