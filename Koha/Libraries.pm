71495e2f6f633fe2c7bd3a74a70cfd13b0ec67c9
Lari Taskula <lari.taskula@jns.fi>
2017-02-09 19:21:32 +0200

    Bug 7614: Add a new method Koha::Libraries->pickup_locations
    This patch adds a new method, Koha::Libraries->pickup_locations. This method
    takes either an item or a biblio as a parameter, and returns the list of
    acceptable pickup locations by considering libraries' configuration to act
    as a pickup location as well as any branch transfer limits that may apply.
    
    To test:
    1. prove t/db_dependent/Koha/Libraries.t
    
    Signed-off-by: Bob Bennhoff <bbennhoff@clicweb.org>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    (cherry picked from commit 2101d0af0034bf8278893b4d4ca7d80c29b3bc75)
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- Koha/Libraries.pm
+++ Koha/Libraries.pm
@@ -23,7 +23,10 @@ use Carp;
 
 use C4::Context;
 
+use Koha::Biblios;
 use Koha::Database;
+use Koha::Item::Transfer::Limits;
+use Koha::Items;
 use Koha::Library;
 
 use base qw(Koha::Objects);
@@ -37,6 +40,75 @@ Koha::Libraries - Koha Library Object set class
 =head2 Class Methods
 
 =cut
+
+=head3 pickup_locations
+
+Returns available pickup locations for
+    A. a specific item
+    B. a biblio
+    C. none of the above, simply all libraries with pickup_location => 1
+
+This method determines the pickup location by two factors:
+    1. is the library configured as pickup location
+    2. can a specific item / at least one of the items of a biblio be transferred
+       into the library
+
+OPTIONAL PARAMETERS:
+    item   # Koha::Item object / itemnumber, find pickup locations for item
+    biblio # Koha::Biblio object / biblionumber, find pickup locations for biblio
+
+If no parameters are given, all libraries with pickup_location => 1 are returned.
+
+=cut
+
+sub pickup_locations {
+    my ($self, $params) = @_;
+
+    my $item = $params->{'item'};
+    my $biblio = $params->{'biblio'};
+    if ($biblio && $item) {
+        Koha::Exceptions::BadParameter->throw(
+            error => "Koha::Libraries->pickup_locations takes either 'biblio' or "
+            ." 'item' as parameter, but not both."
+        );
+    }
+
+    # Select libraries that are configured as pickup locations
+    my $libraries = $self->search({
+        pickup_location => 1
+    }, {
+        order_by => ['branchname']
+    });
+
+    return $libraries->unblessed unless $item or $biblio;
+    return $libraries->unblessed
+        unless C4::Context->preference('UseBranchTransferLimits');
+    my $limittype = C4::Context->preference('BranchTransferLimitsType');
+
+    my $items;
+    if ($item) {
+        unless (ref($item) eq 'Koha::Item') {
+            $item = Koha::Items->find($item);
+            return $libraries->unblessed unless $item;
+        }
+    } else {
+        unless (ref($biblio) eq 'Koha::Biblio') {
+            $biblio = Koha::Biblios->find($biblio);
+            return $libraries->unblessed unless $biblio;
+        }
+    }
+
+    my @pickup_locations;
+    foreach my $library ($libraries->as_list) {
+        if ($item && $item->can_be_transferred({ to => $library })) {
+            push @pickup_locations, $library->unblessed;
+        } elsif ($biblio && $biblio->can_be_transferred({ to => $library })) {
+            push @pickup_locations, $library->unblessed;
+        }
+    }
+
+    return wantarray ? @pickup_locations : \@pickup_locations;
+}
 
 =head3 search_filtered
 
