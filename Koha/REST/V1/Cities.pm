82a28e4c2d1c1948b2166b34bd9ed6b58b21741c
Lari Taskula <lari.taskula@jns.fi>
2017-02-20 19:58:28 +0200

    Bug 18137: Make /cities Mojolicious::Plugin::OpenAPI compatible
    Also:
    - adding some missing and new response definitions into Swagger spec.
    
    To test:
    1. prove t/db_dependent/api/v1/cities.t

--- Koha/REST/V1/Cities.pm
+++ Koha/REST/V1/Cities.pm
@@ -25,11 +25,11 @@ use Koha::Cities;
 use Try::Tiny;
 
 sub list {
-    my ( $c, $args, $cb ) = @_;
+    my $c = shift->openapi->valid_input or return;
 
     my $cities;
     my $filter;
-    $args //= {};
+    my $args = $c->req->params->to_hash;
 
     for my $filter_param ( keys %$args ) {
         $filter->{$filter_param} = { LIKE => $args->{$filter_param} . "%" };
@@ -37,96 +37,105 @@ sub list {
 
     return try {
         $cities = Koha::Cities->search($filter);
-        return $c->$cb( $cities, 200 );
+        return $c->render( status => 200, openapi => $cities );
     }
     catch {
         if ( $_->isa('DBIx::Class::Exception') ) {
-            return $c->$cb( { error => $_->{msg} }, 500 );
+            return $c->render( status  => 500,
+                               openapi => { error => $_->msg } );
         }
         else {
-            return $c->$cb(
-                { error => "Something went wrong, check the logs." }, 500 );
+            return $c->render( status => 500,
+                openapi => { error => "Something went wrong, check the logs."} );
         }
     };
+
 }
 
 sub get {
-    my ( $c, $args, $cb ) = @_;
+    my $c = shift->openapi->valid_input or return;
 
-    my $city = Koha::Cities->find( $args->{cityid} );
+    my $city = Koha::Cities->find( $c->validation->param('cityid') );
     unless ($city) {
-        return $c->$cb( { error => "City not found" }, 404 );
+        return $c->render( status  => 404,
+                           openapi => { error => "City not found" } );
     }
 
-    return $c->$cb( $city, 200 );
+    return $c->render( status => 200, openapi => $city );
 }
 
 sub add {
-    my ( $c, $args, $cb ) = @_;
+    my $c = shift->openapi->valid_input or return;
 
-    my $city = Koha::City->new( $args->{body} );
+    my $city = Koha::City->new( $c->validation->param('body') );
 
     return try {
         $city->store;
-        return $c->$cb( $city, 200 );
+        return $c->render( status => 200, openapi => $city );
     }
     catch {
         if ( $_->isa('DBIx::Class::Exception') ) {
-            return $c->$cb( { error => $_->msg }, 500 );
+            return $c->render( status  => 500,
+                               openapi => { error => $_->message } );
         }
         else {
-            return $c->$cb(
-                { error => "Something went wrong, check the logs." }, 500 );
+            return $c->render( status => 500,
+                openapi => { error => "Something went wrong, check the logs."} );
         }
     };
 }
 
 sub update {
-    my ( $c, $args, $cb ) = @_;
+    my $c = shift->openapi->valid_input or return;
 
     my $city;
 
     return try {
-        $city = Koha::Cities->find( $args->{cityid} );
-        $city->set( $args->{body} );
+        $city = Koha::Cities->find( $c->validation->param('cityid') );
+        my $params = $c->req->json;
+        $city->set( $params );
         $city->store();
-        return $c->$cb( $city, 200 );
+        return $c->render( status => 200, openapi => $city );
     }
     catch {
         if ( not defined $city ) {
-            return $c->$cb( { error => "Object not found" }, 404 );
+            return $c->render( status  => 404,
+                               openapi => { error => "Object not found" } );
         }
         elsif ( $_->isa('Koha::Exceptions::Object') ) {
-            return $c->$cb( { error => $_->message }, 500 );
+            return $c->render( status  => 500,
+                               openapi => { error => $_->message } );
         }
         else {
-            return $c->$cb(
-                { error => "Something went wrong, check the logs." }, 500 );
+            return $c->render( status => 500,
+                openapi => { error => "Something went wrong, check the logs."} );
         }
     };
 
 }
 
 sub delete {
-    my ( $c, $args, $cb ) = @_;
+    my $c = shift->openapi->valid_input or return;
 
     my $city;
 
     return try {
-        $city = Koha::Cities->find( $args->{cityid} );
+        $city = Koha::Cities->find( $c->validation->param('cityid') );
         $city->delete;
-        return $c->$cb( "", 200 );
+        return $c->render( status => 200, openapi => "" );
     }
     catch {
         if ( not defined $city ) {
-            return $c->$cb( { error => "Object not found" }, 404 );
+            return $c->render( status  => 404,
+                               openapi => { error => "Object not found" } );
         }
         elsif ( $_->isa('DBIx::Class::Exception') ) {
-            return $c->$cb( { error => $_->msg }, 500 );
+            return $c->render( status  => 500,
+                               openapi => { error => $_->msg } );
         }
         else {
-            return $c->$cb(
-                { error => "Something went wrong, check the logs." }, 500 );
+            return $c->render( status => 500,
+                openapi => { error => "Something went wrong, check the logs."} );
         }
     };
 
