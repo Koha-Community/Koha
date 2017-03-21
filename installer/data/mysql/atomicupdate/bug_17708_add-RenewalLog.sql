4c2b8358cb4974d1b6b708e1dfe6084cf4049457
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2017-03-21 16:00:19 -0300

    Bug 17708: Add the IGNORE clause
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

--- installer/data/mysql/atomicupdate/bug_17708_add-RenewalLog.sql
+++ installer/data/mysql/atomicupdate/bug_17708_add-RenewalLog.sql
@@ -1,2 +1,2 @@
-INSERT INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type` ) VALUES
+INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type` ) VALUES
 ('RenewalLog','0','','If ON, log information about renewals','YesNo');
