43784271459d3c94c523c74f30311923173e4d61
Brendan A Gallagher <brendan@bywatersolutions.com>
2017-03-23 00:18:10 +0000

    Bug 18066: Update DB rev (3.22.20.001)
    Signed-off-by: Julian Maurice <julian.maurice@biblibre.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -12005,6 +12005,38 @@ if ( CheckVersion($DBversion) ) {
     SetVersion($DBversion);
 }
 
+$DBversion = "3.22.20.001";
+if ( CheckVersion($DBversion) ) {
+    unless( column_exists( 'branches', 'geolocation' ) ) {
+        $dbh->do(q|
+            ALTER TABLE branches
+            ADD COLUMN geolocation VARCHAR(255) DEFAULT NULL after opac_info
+        |);
+    }
+
+    $dbh->do(q|
+        INSERT IGNORE INTO systempreferences
+            (variable, value, options, explanation, type)
+        VALUES
+            ('UsageStatsGeolocation', '', NULL, 'Geolocation of the main library', 'Free')
+    |);
+    $dbh->do(q|
+        INSERT IGNORE INTO systempreferences
+            (variable, value, options, explanation, type)
+        VALUES
+            ('UsageStatsLibrariesInfo', '', NULL, 'Share libraries information', 'YesNo')
+    |);
+    $dbh->do(q|
+        INSERT IGNORE INTO systempreferences
+            (variable, value, options, explanation, type)
+        VALUES
+            ('UsageStatsPublicID', '', NULL, 'Public ID for Hea website', 'Free')
+    |);
+
+    SetVersion( $DBversion );
+    print "Upgrade to $DBversion done (Bug 18066 - Hea version 2)\n";
+}
+
 # DEVELOPER PROCESS, search for anything to execute in the db_update directory
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
