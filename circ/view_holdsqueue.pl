ea15f3112b4de5d0fb76343d4545ccfd940d40fd
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2017-04-05 17:25:13 -0300

    Bug 18403: Use patron-title.inc when hidepatronname is used [SPECIFIC for view_holdsqueue]
    Same that the previous patch but for the holds queue
    
    Signed-off-by: Signed-off-by: Jon McGowan <jon.mcgowan@ptfs-europe.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- circ/view_holdsqueue.pl
+++ circ/view_holdsqueue.pl
@@ -53,6 +53,9 @@ my $itemtypeslimit = $params->{'itemtypeslimit'};
 if ( $run_report ) {
     # XXX GetHoldsQueueItems() does not support $itemtypeslimit!
     my $items = GetHoldsQueueItems($branchlimit, $itemtypeslimit);
+    for my $item ( @$items ) {
+        $item->{patron} = Koha::Patrons->find( $item->{borrowernumber} );
+    }
     $template->param(
         branchlimit     => $branchlimit,
         total      => scalar @$items,
