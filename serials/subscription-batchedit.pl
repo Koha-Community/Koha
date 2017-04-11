4e698f7b478a094d7b32ebfeaa917bca05ebcc5b
Julian Maurice <julian.maurice@biblibre.com>
2017-04-11 18:07:47 +0200

    Bug 18426: Allow to edit subscriptions in batch
    This adds a checkbox column in serials-search.pl tables that allow to edit
    selected subscriptions.
    The following fields can be modified:
     - Bookseller
     - Location
     - Library
     - Item type
     - Public note
     - Nonpublic note
     - "Create item when receiving" flag
     - Expiration date
    + the additional fields defined in serials/add_fields.pl
    
    Test plan:
    
    1. Go to Serials module
    2. If there is no additional fields defined, define some (at least one with an
       authorized value and one without)
    3. Start a subscription search
    4. Select some results using the checkboxes and click the "Edit" button above
       the table
    5. Select values for some fields (not all) and click "Start batch edit"
    6. Verify you are being redirected to the previous search results
    7. Verify that the fields for which you selected a value were modified and that
       the others fields weren't
    8. Repeat steps 4 to 7 but this time, modify the other fields.
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

---
+++ serials/subscription-batchedit.pl
@@ -0,0 +1,107 @@
+#!/usr/bin/perl
+
+# Copyright 2017 BibLibre
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+use CGI qw( -utf8 );
+
+use C4::Auth;
+use C4::Output;
+use Koha::Subscriptions;
+use Koha::Acquisition::Booksellers;
+use Koha::AdditionalField;
+use Koha::DateUtils;
+
+my $cgi = new CGI;
+
+my ($template, $loggedinuser, $cookie) = get_template_and_user({
+    template_name => 'serials/subscription-batchedit.tt',
+    query => $cgi,
+    type => 'intranet',
+    authnotrequired => 0,
+    flagsrequired => {serials => 'edit_subscription'},
+});
+
+my @subscriptionids = $cgi->multi_param('subscriptionid');
+
+my @subscriptions;
+foreach my $subscriptionid (@subscriptionids) {
+    my $subscription = Koha::Subscriptions->find($subscriptionid);
+
+    push @subscriptions, $subscription if $subscription;
+}
+
+my $additional_fields = Koha::AdditionalField->all({tablename => 'subscription'});
+
+my $batchedit = $cgi->param('batchedit');
+if ($batchedit) {
+    my %params = (
+        aqbooksellerid => scalar $cgi->param('booksellerid'),
+        location => scalar $cgi->param('location'),
+        branchcode => scalar $cgi->param('branchcode'),
+        itemtype => scalar $cgi->param('itemtype'),
+        notes => scalar $cgi->param('notes'),
+        internalnotes => scalar $cgi->param('internalnotes'),
+        serialsadditems => scalar $cgi->param('serialsadditems'),
+        enddate => dt_from_string(scalar $cgi->param('enddate')),
+    );
+
+    my $field_values = {};
+    foreach my $field (@$additional_fields) {
+        my $value = $cgi->param('field_' . $field->{id});
+        $field_values->{$field->{id}} = $value;
+    }
+
+    foreach my $subscription (@subscriptions) {
+        while (my ($key, $value) = each %params) {
+            if (defined $value and $value ne '') {
+                $subscription->$key($value);
+            }
+        }
+
+        foreach my $field (@$additional_fields) {
+            my $value = $field_values->{$field->{id}};
+            if (defined $value and $value ne '') {
+                $field->{values} //= {};
+                $field->{values}->{$subscription->subscriptionid} = $value;
+            }
+        }
+
+        $subscription->store;
+    }
+
+    foreach my $field (@$additional_fields) {
+        if (defined $field->{values}) {
+            $field->insert_values();
+        }
+    }
+
+    my $redirect_url = $cgi->param('referrer') // '/cgi-bin/koha/serials/serials-home.pl';
+    print $cgi->redirect($redirect_url);
+    exit;
+}
+
+$template->param(
+    subscriptions => \@subscriptions,
+    booksellers => [ Koha::Acquisition::Booksellers->search() ],
+    additional_fields => $additional_fields,
+    referrer => scalar $cgi->param('referrer'),
+);
+
+output_html_with_http_headers $cgi, $cookie, $template->output;
