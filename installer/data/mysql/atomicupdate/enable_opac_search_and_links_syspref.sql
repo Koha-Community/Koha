3191fb95b769918035a0bd19a1d20b82f0914798
Johan Larsson <johan.larsson@ub.gu.se>
2017-04-25 17:16:03 +0200

    added syspref for enabling and disabling search and external links in opac

---
+++ installer/data/mysql/atomicupdate/enable_opac_search_and_links_syspref.sql
@@ -0,0 +1,8 @@
+INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type`) VALUES
+('enableOpacCatalogBrowsing', '1', '', 'Toggle the search box visibility.', 'YesNo');
+
+INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type`) VALUES
+('linkToExternalSite', '0', '', 'Links to external site', 'YesNo');
+
+INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type`) VALUES
+('externalSiteURL', '', null, 'URL to external site', 'Free');
