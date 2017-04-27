6596513d4b5e0d20ee6b68fadbee2e5841997115
Owen Leonard <oleonard@myacpl.org>
2017-04-27 15:20:20 +0000

    Bug 17942 - Update style of the web installer with Bootstrap 3
    This patch makes style and markup changes to the web installer and new
    onboarding tool (Bug 17855).
    
    - Markup has been corrected, improved, and in many places reindented.
    - Some page titles have been made more specific.
    - Some JavaScript and CSS have been moved to separate files.
    
    In the onboarding tool I have removed form validation from the markup
    and JavaScript in many cases where the requirements were not matched
    elsewhere in Koha. For instance, we shouldn't limit item type
    descriptions to only letters because the database doesn't require such a
    limit.
    
    To test, apply the patch and run the web installer with an empty
    database. Confirm that the installation process completes correctly and
    that each page looks good and works correctly.
    
    Works as advertised
    Signed-off-by: Marc Véron <veron@veron.ch>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

---
+++ koha-tmpl/intranet-tmpl/prog/js/onboarding.js
@@ -0,0 +1,150 @@
+function _(s) { return s; } // dummy function for gettext
+
+jQuery.validator.addMethod( "category_code_check", function(value,element){
+    var patt = /^[A-Za-z]{0,10}$/g;
+    if (patt.test(element.value)) {
+        return true;
+    } else {
+        return false;
+    }
+    }, MSG_LETTERS_ONLY
+);
+
+jQuery.validator.addMethod( "letters_only", function(value,element){
+        var patt =/^[A-Za-z ]{0,30}$/g;
+        if (patt.test(element.value)){
+            return true;
+        } else {
+            return false;
+        }
+    }, MSG_LETTERS_ONLY
+);
+
+jQuery.validator.addMethod( "enrollment_period", function(){
+      enrolmentperiod = $("#enrolmentperiod").val();
+      enrolmentperioddate = $("#enrolmentperioddate").val();
+      if (( $("#enrolmentperiod").val() === "" && $("#enrolmentperioddate").val() === "") || ($("#enrolmentperiod").val() !== "" && $("#enrolmentperioddate").val() !== "")) {
+             return false;
+      } else {
+             return true;
+      }
+    }, MSG_ONLY_ONE_ENROLLMENTPERIOD
+);
+
+jQuery.validator.addMethod( "password_match", function(value,element){
+        var MSG_PASSWORD_MISMATCH = ( MSG_PASSWORD_MISMATCH );
+        var password = document.getElementById('password').value;
+        var confirmpassword = document.getElementById('password2').value;
+
+        if ( password != confirmpassword ){
+               return false;
+          }
+          else{
+               return true;
+          }
+    },  MSG_PASSWORD_MISMATCH
+);
+
+jQuery.validator.addMethod( "cardnumber_check", function(value,element){
+          var patt =/[A-Za-z1-9 ]+$/g;
+          if (patt.test(element.value)){
+              return true;
+          } else {
+              return false;
+          }
+     }, MSG_LETTERS_NUMBERS_ONLY
+);
+
+function toUC(f) {
+    var x=f.value.toUpperCase();
+    f.value=x;
+    return true;
+}
+
+$(document).ready(function() {
+    if ($("#branches option:selected").length < 1) {
+        $("#branches option:first").attr("selected", "selected");
+    }
+    $("#categorycode").on("blur",function(){
+         toUC(this);
+    });
+
+    $("#enrolmentperioddate").datepicker({
+        minDate: 1
+    }); // Require that "until date" be in the future
+
+    $("#category_form").validate({
+        rules: {
+            categorycode: {
+                    required: true,
+                    category_code_check: true
+            },
+            description: {
+                    required:true
+            },
+            enrolmentperiod: {
+                   required: function(element){
+                         return $("#enrolmentperioddate").val() === "";
+                   },
+                   digits: true,
+                   enrollment_period: true,
+            },
+            enrolmentperioddate: {
+                    required: function(element){
+                        return $("#enrolmentperiod").val() === "";
+                    },
+                    enrollment_period: true,
+                    // is_valid_date ($(#"enrolementperioddate").val());
+            },
+            dateofbirthrequired: {
+                    digits: true
+            },
+            upperagelimit: {
+                    digits: true
+            },
+            enrolmentfee: {
+                    number: true
+            },
+            reservefee: {
+                    number: true
+            },
+            category_type: {
+                    required: true
+            }
+        },
+        messages: {
+            enrolmentperiod: {
+                    required: MSG_ONE_ENROLLMENTPERIOD
+            },
+            enrolmentperioddate: {
+                    required: MSG_ONE_ENROLLMENTPERIOD
+            }
+        }
+    });
+
+    $("#createpatron").validate({
+        rules: {
+            surname: {
+                required: true
+            },
+            firstname: {
+                required: true
+            },
+            cardnumber: {
+                required: true
+            },
+            password: {
+                password_match: true
+            }
+        },
+        messages: {
+            password: {
+                required: MSG_PASSWORD_MISMATCH
+            },
+        }
+
+    });
+
+    $("#createitemform").validate();
+    $("#createcirculationrule").validate();
+});
