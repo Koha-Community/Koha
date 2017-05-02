71c75f4773015ffe36811b6d5de6b995ccf7a2f3
Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
2017-05-02 15:44:47 +0200

    Bug 18286: [Follow-up] Add t/etc/schema.pl
    We need this file here already.
    Copied from bug 18226 and adjusted in order to keep qa tools quiet.
    
    Test plan:
    Run t/db_dependent/01-test_dbic.t again.
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

---
+++ t/etc/schema.pl
@@ -0,0 +1,9 @@
+use Modern::Perl;
+
+return {
+    schema_class => 'Koha::Schema',
+#  This is the default option when nothing is defined
+#  connect_info => ['dbi:SQLite:dbname=:memory:','',''],
+    connect_opts => { name_sep => '.', quote_char => '`', sqlite_unicode => 1 },
+    fixture_class => '::Populate',
+};
