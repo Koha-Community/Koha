6f29c775d92d72bd03188efea4a41b3fbb49e928
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2017-05-23 16:16:17 -0300

    Bug 18663: Add pref ExportRemoveFields if missing
    Signed-off-by: Josef Moravec <josef.moravec@gmail.com>
    Signed-off-by: Julian Maurice <julian.maurice@biblibre.com>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

---
+++ installer/data/mysql/atomicupdate/bug_18663.sql
@@ -0,0 +1,2 @@
+INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type` ) VALUES
+('ExportRemoveFields','',NULL,'List of fields for non export in circulation.pl (separated by a space)','Free');
