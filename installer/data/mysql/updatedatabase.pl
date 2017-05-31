fe22e9d99c99881077cc5abf837125b8e1c107c3
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2017-05-31 15:35:15 -0300

    Koha 17.06 - unstable (start of a new dev cycle)

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -14522,6 +14522,12 @@ if( CheckVersion( $DBversion ) ) {
     print "Upgrade to $DBversion done (Koha 17.05)\n";
 }
 
+$DBversion = '17.06.00.000';
+if( CheckVersion( $DBversion ) ) {
+    SetVersion( $DBversion );
+    print "Upgrade to $DBversion done (He pai ake te iti i te kore)\n";
+}
+
 # DEVELOPER PROCESS, search for anything to execute in the db_update directory
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
