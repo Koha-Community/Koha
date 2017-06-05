5f6e1e00c265a552cf6faa7b7fa30db725204eb6
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2017-06-05 12:55:36 -0300

    Bug 8612: Update installer files for late_issues
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- installer/data/mysql/fr-CA/facultatif/csv_profiles.sql
+++ installer/data/mysql/fr-CA/facultatif/csv_profiles.sql
@@ -1,2 +1,2 @@
-INSERT IGNORE INTO export_format( profile, description, content, csv_separator, type )
-VALUES ( "Réclamation de numéros", "Export CSV par défaut pour la réclamation de numéros de périodiques", "FOURNISSEUR=aqbooksellers.name|TITRE=subscription.title|NUMÉRO=serial.serialseq|EN RETARD DEPUIS=serial.planneddate", ",", "sql" );
+INSERT IGNORE INTO export_format( profile, description, content, csv_separator, type, used_for )
+VALUES ( "Réclamation de numéros", "Export CSV par défaut pour la réclamation de numéros de périodiques", "FOURNISSEUR=aqbooksellers.name|TITRE=subscription.title|NUMÉRO=serial.serialseq|EN RETARD DEPUIS=serial.planneddate", ",", "sql", "late_issues" );
--- installer/data/mysql/it-IT/necessari/csv_profiles.sql
+++ installer/data/mysql/it-IT/necessari/csv_profiles.sql
@@ -1,2 +1,2 @@
-INSERT IGNORE INTO export_format( profile, description, content, csv_separator, type )
-VALUES ( "issues to claim", "CSV export per fascicoli in ritardo", "FORNITORE=aqbooksellers.name|TITOLO=subscription.title|NUMERO FASC=serial.serialseq|IN RITARDO DAL=serial.planneddate", ",", "sql" );
+INSERT IGNORE INTO export_format( profile, description, content, csv_separator, type, used_for )
+VALUES ( "issues to claim", "CSV export per fascicoli in ritardo", "FORNITORE=aqbooksellers.name|TITOLO=subscription.title|NUMERO FASC=serial.serialseq|IN RITARDO DAL=serial.planneddate", ",", "sql", "late_issues" );
