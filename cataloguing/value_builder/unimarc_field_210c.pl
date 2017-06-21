642355033e38bfb60144c4384f08e7a4b01b3e23
Fridolin Somers <fridolin.somers@biblibre.com>
2017-06-21 13:05:40 +0200

    Bug 18833: Plugin unimarc_field_210c pagination error
    In plugin unimarc_field_210c, the editors search results have pagination.
    First page works but not other pages.
    
    The main problem whas mainly because $startfrom var was missused. It is defined in C4::Output::pagination_bar as page number.
    This patch corrects by using a $offset var.
    Also removes dead code, obsoleted by using C4::Output::pagination_bar.
    Also changes hardcoded results per page from 19 to 20.
    
    Test plan :
    - Use UNIMARC catalogue
    - Define in framework value builder unimarc_field_210c on 210$c
    - Create autority type EDITORS with heading 200$b
    - Create 22 authorities of that type
    - Index those new authorities
    - Create a new biblio record
    - Click on 210$c plugin
    - Click on "Search"
    - You see 20 results
    - Click on page 2
    => Without patch you get empty table
    => With patch you get 2 results
    - Check that you see "Results 21 to 22 of 22"
    - Delete 2 autorities and retest
    
    Works as intended.
    
    Signed-off-by: Simon Pouchol <simon.pouchol@biblibre.com>
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    (cherry picked from commit 455d94eec9c41f7bad718ac62c2367bea6202452)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- cataloguing/value_builder/unimarc_field_210c.pl
+++ cataloguing/value_builder/unimarc_field_210c.pl
@@ -64,10 +64,10 @@ my ($input) = @_;
     my $resultstring = $query->param('result');
     my $dbh = C4::Context->dbh;
 
-    my $startfrom=$query->param('startfrom');
-    $startfrom=0 if(!defined $startfrom);
+    my $startfrom = $query->param('startfrom') // 1; # Page number starting at 1
     my ($template, $loggedinuser, $cookie);
-    my $resultsperpage;
+    my $resultsperpage = $query->param('resultsperpage') // 20; # TODO hardcoded
+    my $offset = ( $startfrom - 1 ) * $resultsperpage;
 
     if ($op eq "do_search") {
         my @marclist = $query->multi_param('marclist');
@@ -77,15 +77,12 @@ my ($input) = @_;
         my @value = $query->multi_param('value');
         my $orderby   = $query->param('orderby');
 
-        $resultsperpage= $query->param('resultsperpage');
-        $resultsperpage = 19 if(!defined $resultsperpage);
-
         # builds tag and subfield arrays
         my @tags;
 
         my ($results,$total) = SearchAuthorities( \@tags,\@and_or,
                                             \@excluding, \@operator, \@value,
-                                            $startfrom*$resultsperpage, $resultsperpage,$authtypecode, $orderby);
+                                            $offset, $resultsperpage,$authtypecode, $orderby);
 
 	# Getting the $b if it exists
 	for (@$results) {
@@ -104,49 +101,18 @@ my ($input) = @_;
                     debug => 1,
                     });
 
-        # multi page display gestion
-        my $displaynext=0;
-        my $displayprev=$startfrom;
-        if(($total - (($startfrom+1)*($resultsperpage))) > 0 ) {
-            $displaynext = 1;
-        }
+        # Results displayed in current page
+        my $from = $offset + 1;
+        my $to = ( $offset + $resultsperpage > $total ) ? $total : $offset + $resultsperpage;
 
-        my @numbers = ();
-
-        if ($total>$resultsperpage) {
-            for (my $i=1; $i<$total/$resultsperpage+1; $i++) {
-                if ($i<16) {
-                    my $highlight=0;
-                    ($startfrom==($i-1)) && ($highlight=1);
-                    push @numbers, { number => $i,
-                        highlight => $highlight ,
-                        startfrom => ($i-1)};
-                }
-            }
-        }
-
-        my $from = $startfrom*$resultsperpage+1;
-        my $to;
-
-        if($total < (($startfrom+1)*$resultsperpage)) {
-            $to = $total;
-        } else {
-            $to = (($startfrom+1)*$resultsperpage);
-        }
         my $link="../cataloguing/plugin_launcher.pl?plugin_name=unimarc_field_210c.pl&amp;authtypecode=EDITORS&amp;".join("&amp;",map {"value=".$_} @value)."&amp;op=do_search&amp;type=intranet&amp;index=$index";
 
         $template->param(result => $results) if $results;
         $template->param('index' => scalar $query->param('index'));
-        $template->param(startfrom=> $startfrom,
-                                displaynext=> $displaynext,
-                                displayprev=> $displayprev,
-                                resultsperpage => $resultsperpage,
-                                startfromnext => $startfrom+1,
-                                startfromprev => $startfrom-1,
+        $template->param(
                                 total=>$total,
                                 from=>$from,
                                 to=>$to,
-                                numbers=>\@numbers,
                                 authtypecode =>$authtypecode,
                                 resultstring =>$value[0],
                                 pagination_bar => pagination_bar(
