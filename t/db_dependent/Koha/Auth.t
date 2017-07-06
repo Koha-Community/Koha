KohaSuomi/Koha-3.x

Bug 14536 - PageObject-pattern base implementation
This patch introduces:

-PageObject base classes for OPAC and Intra, and example tests. More example tests
 in Buugg 14540, where setting test context is easier.
-a interactive debug script to drive PageObjects more easily.

------------------
PageObjects are used to make robust and reusable integration test components to
test various front-end features. PageObjects load a Selenium::Remote::Driver
implementation, phantomjs by default and use this to do scripted user actions
in the browser,
eg. clicking HTML-elements, accepting popup dialogs, entering text to input
fields.

PageObjects encapsulate those very low-level operations into clear and easily
usable actions or services, like doPasswordLogin().
PageObjects also seamlessly deal with navigation from one page to another, eg.
    my $mainpage = t::lib::Page::Mainpage->new();
    $mainpage->doPasswordLogin('admin', '1234')->gotoPatrons()->
               searchPatrons({keywordSearch => "Jane Doe"});

PageObjects make doing integration tests so great!
You can google for the specifics of PageObject patterns and there are a ton of
great tutorials explaining their function.

Now PageObjects also have a debugging system where you can get the server
response in a title+cookies+html-dump and a screenshot of the browser session.
see t::lib::Page for more info!

---------------------
Interactive debugger at misc/devel/interactiveWebDriverShell.pl loads the
desired PageObject with the needed parameters to the debugger context and makes
dynamic UserAgent scripting easy and fun!

Bug 14536: (follow-up) Add more functionalty for handling messaging preferences

- Fixes memberentry form submit to return Circulation PageObject.
- Prevents missing element errors at different steps of memberentry page. Patron
modification has multiple different steps (full edit, information edit, sms number edit,
library use edit, alternate address edit, alternative contact edit). Some of the input 

t/db_dependent/Koha/Auth.t
@@ -1,0 +1,72 @@
+#!/usr/bin/env perl
+
+# Copyright 2015 Open Source Freedom Fighters
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+use Test::More;
+use Try::Tiny; #Even Selenium::Remote::Driver uses Try::Tiny :)
+
+use t::lib::Page::Mainpage;
+
+use t::lib::TestObjects::BorrowerFactory;
+
+##Setting up the test context
+my $testContext = {};
+
+my $password = '1234';
+my $borrowerFactory = t::lib::TestObjects::BorrowerFactory->new();
+my $borrowers = $borrowerFactory->createTestGroup([
+            {firstname  => 'Olli-Antti',
+             surname    => 'Kivi',
+             cardnumber => '1A01',
+             branchcode => 'CPL',
+             flags      => '1', #superlibrarian, not exactly a very good way of doing permission testing?
+             userid     => 'mini_admin',
+             password   => $password,
+            },
+        ], undef, $testContext);
+
+##Test context set, starting testing:
+eval { #run in a eval-block so we don't die without tearing down the test context
+
+    testPasswordLogin();
+
+};
+if ($@) { #Catch all leaking errors and gracefully terminate.
+    warn $@;
+    tearDown();
+    exit 1;
+}
+
+##All tests done, tear down test context
+tearDown();
+done_testing;
+
+sub tearDown {
+    t::lib::TestObjects::ObjectFactory->tearDownTestContext($testContext);
+}
+
+######################################################
+    ###  STARTING TEST IMPLEMENTATIONS         ###
+######################################################
+
+sub testPasswordLogin {
+    my $mainpage = t::lib::Page::Mainpage->new();
+    $mainpage->isPasswordLoginAvailable()->doPasswordLogin($borrowers->{'1A01'}->userid(), $password)->quit();
+}

Koha-Community/Koha

t/db_dependent/Koha/Auth.t
@@ -1,0 +1,72 @@
+#!/usr/bin/env perl
+
+# Copyright 2015 Open Source Freedom Fighters
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+use Test::More;
+use Try::Tiny; #Even Selenium::Remote::Driver uses Try::Tiny :)
+
+use t::lib::Page::Mainpage;
+
+use t::lib::TestObjects::PatronFactory;
+
+##Setting up the test context
+my $testContext = {};
+
+my $password = '1234';
+my $borrowerFactory = t::lib::TestObjects::PatronFactory->new();
+my $borrowers = $borrowerFactory->createTestGroup([
+            {firstname  => 'Olli-Antti',
+             surname    => 'Kivi',
+             cardnumber => '1A01',
+             branchcode => 'CPL',
+             flags      => '1', #superlibrarian, not exactly a very good way of doing permission testing?
+             userid     => 'mini_admin',
+             password   => $password,
+            },
+        ], undef, $testContext);
+
+##Test context set, starting testing:
+eval { #run in a eval-block so we don't die without tearing down the test context
+
+    testPasswordLogin();
+
+};
+if ($@) { #Catch all leaking errors and gracefully terminate.
+    warn $@;
+    tearDown();
+    exit 1;
+}
+
+##All tests done, tear down test context
+tearDown();
+done_testing;
+
+sub tearDown {
+    t::lib::TestObjects::ObjectFactory->tearDownTestContext($testContext);
+}
+
+######################################################
+    ###  STARTING TEST IMPLEMENTATIONS         ###
+######################################################
+
+sub testPasswordLogin {
+    my $mainpage = t::lib::Page::Mainpage->new();
+    $mainpage->isPasswordLoginAvailable()->doPasswordLogin($borrowers->{'1A01'}->userid(), $password)->quit();
+}