KohaSuomi/Koha-3.x

Bug 13799: 4. API Authentication, part 1: API keys management in interface
Depends on Buugg 14539 and Buugg 7174.

This introduces the concept of API keys for use in the new REST API.
A key is a string of 32 alphanumerical characters (32 is purely
arbitrary, it can be changed easily).
A user can have multiple keys (unlimited at the moment)
Keys can be generated automatically, and then we have the possibility to
delete or revoke each one individually.

ApiKeys can be easily accessed using the Koha::ApiKeys-package.

Includes unit tests and selenium integration tests for Intra and OPAC.

Test plan:
1/ Go to staff interface
2/ Go to a borrower page
3/ In toolbar, click on More -> Manage API keys
4/ Click on "Generate new key" multiple times, check that they are
   correctly displayed under the button, and they are active by default
5/ Revoke some keys, check that they are not active anymore
6/ Delete some keys, check that they disappear from table
7/ Go to opac interface, log in
8/ In your user account pages, you now have a new tab to the left "your
   API keys". Click on it.
9/ Repeat steps 4-6

Koha/ApiKey.pm
@@ -1,0 +1,46 @@
+package Koha::ApiKey;
+
+# Copyright BibLibre 2015
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use Modern::Perl;
+
+use Carp;
+
+use Koha::Database;
+
+use base qw(Koha::Object);
+
+=head1 NAME
+
+Koha::ApiKey - Koha API Key Object class
+
+=head1 API
+
+=head2 Class Methods
+
+=cut
+
+=head3 type
+
+=cut
+
+sub type {
+    return 'ApiKey';
+}
+
+1;

Koha/ApiKeys.pm
@@ -1,0 +1,121 @@
+package Koha::ApiKeys;
+
+# Copyright BibLibre 2015
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use Modern::Perl;
+use Scalar::Util qw(blessed);
+
+use Koha::Borrowers;
+use Koha::ApiKey;
+
+use base qw(Koha::Objects);
+
+use Koha::Exception::BadParameter;
+use Koha::Exception::UnknownObject;
+
+=head1 NAME
+
+Koha::ApiKeys - Koha API Keys Object class
+
+=head1 API
+
+=head2 Class Methods
+
+=cut
+
+=head3 type
+
+=cut
+
+sub type {
+    return 'ApiKey';
+}
+
+sub object_class {
+    return 'Koha::ApiKey';
+}
+
+sub _get_castable_unique_columns {
+    return ['api_key_id', 'api_key'];
+}
+
+=head grant
+
+    my $apiKey = Koha::ApiKey->grant({borrower => $borrower,
+                                    apiKey => $apiKey
+                                });
+
+Granting an ApiKey should be easy. This creates a new ApiKey for the given Borrower,
+or sets the owner of an existing key.
+$PARAM1 HASHRef of params, {
+            borrower => MANDATORY, a Koha::Borrower or something castable to one.
+            apiKey   => OPTIONAL, an existing Koha::ApiKEy to give to somebody else.
+                                not sure why anybody would want to do that, but
+                                provided as a convenience for testing.
+}
+@THROWS Koha::Exception::BadParameter
+=cut
+
+sub grant {
+    my ($self, $borrower, $apiKey) = @_;
+    $borrower = Koha::Borrowers->cast($borrower);
+    if ($apiKey) {
+        $apiKey = Koha::ApiKeys->cast($apiKey);
+        $apiKey->borrowernumber($borrower->borrowernumber);
+    }
+    else {
+        $apiKey = new Koha::ApiKey;
+        $apiKey->borrowernumber($borrower->borrowernumber);
+        $apiKey->api_key(String::Random->new->randregex('[a-zA-Z0-9]{32}'));
+    }
+    $apiKey->store;
+    return $apiKey;
+}
+
+sub delete {
+    my ($self, $apiKey) = @_;
+    $apiKey = Koha::ApiKeys->cast($apiKey);
+
+    if ($apiKey) {
+        $apiKey->delete;
+    }
+}
+
+sub revoke {
+    my ($self, $apiKey) = @_;
+    $apiKey = Koha::ApiKeys->cast($apiKey);
+
+    if ($apiKey) {
+        $apiKey->active(0);
+        $apiKey->store;
+    }
+    return $apiKey;
+}
+
+sub activate {
+    my ($self, $apiKey) = @_;
+    $apiKey = Koha::ApiKeys->cast($apiKey);
+
+    if ($apiKey) {
+        $apiKey->active(1);
+        $apiKey->store;
+    }
+    return $apiKey;
+}
+
+1;

t/db_dependent/Koha/ApiKeys.t
@@ -1,0 +1,124 @@
+#!/usr/bin/perl
+
+# Copyright 2015 Vaara-kirjastot
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
+use Test::More;
+use Try::Tiny;
+use Scalar::Util qw(blessed);
+
+use Koha::Auth::PermissionManager;
+use Koha::ApiKeys;
+
+use t::lib::TestObjects::ObjectFactory;
+use t::lib::TestObjects::BorrowerFactory;
+use t::lib::Page::Members::Moremember;
+use t::lib::Page::Opac::OpacMain;
+
+##Setting up the test context
+my $testContext = {};
+
+my $password = '1234';
+my $borrowerFactory = t::lib::TestObjects::BorrowerFactory->new();
+my $borrowers = $borrowerFactory->createTestGroup([
+            {firstname  => 'Olli-Antti',
+             surname    => 'Kivi',
+             cardnumber => '1A01',
+             branchcode => 'CPL',
+             password   => $password,
+            },
+            {firstname  => 'Alli-Ontti',
+             surname    => 'Ivik',
+             cardnumber => '1A02',
+             branchcode => 'CPL',
+             password   => $password,
+            },
+        ], undef, $testContext);
+my $borrowerKivi = $borrowers->{'1A01'};
+my $borrowerIvik = $borrowers->{'1A02'};
+my $permissionManager = Koha::Auth::PermissionManager->new();
+$permissionManager->grantPermission($borrowerKivi, 'borrowers', 'manage_api_keys');
+
+
+##Test context set, starting testing:
+eval { #run in a eval-block so we don't die without tearing down the test context
+subtest "ApiKeys API Unit tests" => sub {
+    my $borrowerKivi = $borrowers->{'1A01'};
+    my $borrowerIvik = $borrowers->{'1A02'};
+
+
+    my $apiKey = Koha::ApiKeys->grant($borrowerKivi);
+    is($apiKey->borrowernumber, $borrowerKivi->borrowernumber, "ApiKey granted");
+
+    Koha::ApiKeys->revoke($apiKey);
+    is($apiKey->active, 0, "ApiKey revoked");
+
+    Koha::ApiKeys->activate($apiKey);
+    is($apiKey->active, 1, "ApiKey activated");
+
+    Koha::ApiKeys->grant($borrowerIvik, $apiKey);
+    is($apiKey->borrowernumber, $borrowerIvik->borrowernumber, "ApiKey granted to another Borrower");
+
+    Koha::ApiKeys->delete($apiKey);
+    $apiKey = Koha::ApiKeys->find({api_key_id => $apiKey->api_key_id});
+    ok(not($apiKey), "ApiKey deleted");
+}
+};
+if ($@) { #Catch all leaking errors and gracefully terminate.
+    warn $@;
+    tearDown();
+    exit 1;
+}
+
+eval {
+subtest "ApiKeys Intra Integration tests" => sub {
+    my $agent = t::lib::Page::Members::Moremember->new({borrowernumber => $borrowerKivi->borrowernumber});
+    $agent->doPasswordLogin($borrowerKivi->userid, $password)->navigateManageApiKeys()->generateNewApiKey();
+    my @apiKeys = Koha::ApiKeys->search({borrowernumber => $borrowerKivi->borrowernumber});
+    $agent->revokeApiKey($apiKeys[0]->api_key)->deleteApiKey($apiKeys[0]->api_key)
+               ->quit();
+}
+};
+if ($@) { #Catch all leaking errors and gracefully terminate.
+    warn $@;
+    tearDown();
+    exit 1;
+}
+
+eval {
+subtest "ApiKeys OPAC Integration tests" => sub {
+    my $agent = t::lib::Page::Opac::OpacMain->new();
+    $agent->doPasswordLogin($borrowerKivi->userid, $password)->navigateYourAPIKeys()->generateNewApiKey();
+    my @apiKeys = Koha::ApiKeys->search({borrowernumber => $borrowerKivi->borrowernumber});
+    $agent->revokeApiKey($apiKeys[0]->api_key)->deleteApiKey($apiKeys[0]->api_key)
+               ->quit();
+}
+};
+if ($@) { #Catch all leaking errors and gracefully terminate.
+    warn $@;
+    tearDown();
+    exit 1;
+}
+
+##All tests done, tear down test context
+tearDown();
+done_testing;
+
+sub tearDown {
+    t::lib::TestObjects::ObjectFactory->tearDownTestContext($testContext);
+}

Koha-Community/Koha

Koha/ApiKey.pm
@@ -1,0 +1,46 @@
+package Koha::ApiKey;
+
+# Copyright BibLibre 2015
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use Modern::Perl;
+
+use Carp;
+
+use Koha::Database;
+
+use base qw(Koha::Object);
+
+=head1 NAME
+
+Koha::ApiKey - Koha API Key Object class
+
+=head1 API
+
+=head2 Class Methods
+
+=cut
+
+=head3 _type
+
+=cut
+
+sub _type {
+    return 'ApiKey';
+}
+
+1;

Koha/ApiKeys.pm
@@ -1,0 +1,121 @@
+package Koha::ApiKeys;
+
+# Copyright BibLibre 2015
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use Modern::Perl;
+use Scalar::Util qw(blessed);
+
+use Koha::Patrons;
+use Koha::ApiKey;
+
+use base qw(Koha::Objects);
+
+use Koha::Exception::BadParameter;
+use Koha::Exception::UnknownObject;
+
+=head1 NAME
+
+Koha::ApiKeys - Koha API Keys Object class
+
+=head1 API
+
+=head2 Class Methods
+
+=cut
+
+=head3 _type
+
+=cut
+
+sub _type {
+    return 'ApiKey';
+}
+
+sub object_class {
+    return 'Koha::ApiKey';
+}
+
+sub _get_castable_unique_columns {
+    return ['api_key_id', 'api_key'];
+}
+
+=head grant
+
+    my $apiKey = Koha::ApiKey->grant({borrower => $borrower,
+                                    apiKey => $apiKey
+                                });
+
+Granting an ApiKey should be easy. This creates a new ApiKey for the given Borrower,
+or sets the owner of an existing key.
+$PARAM1 HASHRef of params, {
+            borrower => MANDATORY, a Koha::Patron or something castable to one.
+            apiKey   => OPTIONAL, an existing Koha::ApiKEy to give to somebody else.
+                                not sure why anybody would want to do that, but
+                                provided as a convenience for testing.
+}
+@THROWS Koha::Exception::BadParameter
+=cut
+
+sub grant {
+    my ($self, $borrower, $apiKey) = @_;
+    $borrower = Koha::Patrons->cast($borrower);
+    if ($apiKey) {
+        $apiKey = Koha::ApiKeys->cast($apiKey);
+        $apiKey->borrowernumber($borrower->borrowernumber);
+    }
+    else {
+        $apiKey = new Koha::ApiKey;
+        $apiKey->borrowernumber($borrower->borrowernumber);
+        $apiKey->api_key(String::Random->new->randregex('[a-zA-Z0-9]{32}'));
+    }
+    $apiKey->store;
+    return $apiKey;
+}
+
+sub delete {
+    my ($self, $apiKey) = @_;
+    $apiKey = Koha::ApiKeys->cast($apiKey);
+
+    if ($apiKey) {
+        $apiKey->delete;
+    }
+}
+
+sub revoke {
+    my ($self, $apiKey) = @_;
+    $apiKey = Koha::ApiKeys->cast($apiKey);
+
+    if ($apiKey) {
+        $apiKey->active(0);
+        $apiKey->store;
+    }
+    return $apiKey;
+}
+
+sub activate {
+    my ($self, $apiKey) = @_;
+    $apiKey = Koha::ApiKeys->cast($apiKey);
+
+    if ($apiKey) {
+        $apiKey->active(1);
+        $apiKey->store;
+    }
+    return $apiKey;
+}
+
+1;

t/db_dependent/Koha/ApiKeys.t
@@ -1,0 +1,124 @@
+#!/usr/bin/perl
+
+# Copyright 2015 Vaara-kirjastot
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
+use Test::More;
+use Try::Tiny;
+use Scalar::Util qw(blessed);
+
+use Koha::Auth::PermissionManager;
+use Koha::ApiKeys;
+
+use t::lib::TestObjects::ObjectFactory;
+use t::lib::TestObjects::PatronFactory;
+use t::lib::Page::Members::Moremember;
+use t::lib::Page::Opac::OpacMain;
+
+##Setting up the test context
+my $testContext = {};
+
+my $password = '1234';
+my $borrowerFactory = t::lib::TestObjects::PatronFactory->new();
+my $borrowers = $borrowerFactory->createTestGroup([
+            {firstname  => 'Olli-Antti',
+             surname    => 'Kivi',
+             cardnumber => '1A01',
+             branchcode => 'CPL',
+             password   => $password,
+            },
+            {firstname  => 'Alli-Ontti',
+             surname    => 'Ivik',
+             cardnumber => '1A02',
+             branchcode => 'CPL',
+             password   => $password,
+            },
+        ], undef, $testContext);
+my $borrowerKivi = $borrowers->{'1A01'};
+my $borrowerIvik = $borrowers->{'1A02'};
+my $permissionManager = Koha::Auth::PermissionManager->new();
+$permissionManager->grantPermission($borrowerKivi, 'borrowers', 'manage_api_keys');
+
+
+##Test context set, starting testing:
+eval { #run in a eval-block so we don't die without tearing down the test context
+subtest "ApiKeys API Unit tests" => sub {
+    my $borrowerKivi = $borrowers->{'1A01'};
+    my $borrowerIvik = $borrowers->{'1A02'};
+
+
+    my $apiKey = Koha::ApiKeys->grant($borrowerKivi);
+    is($apiKey->borrowernumber, $borrowerKivi->borrowernumber, "ApiKey granted");
+
+    Koha::ApiKeys->revoke($apiKey);
+    is($apiKey->active, 0, "ApiKey revoked");
+
+    Koha::ApiKeys->activate($apiKey);
+    is($apiKey->active, 1, "ApiKey activated");
+
+    Koha::ApiKeys->grant($borrowerIvik, $apiKey);
+    is($apiKey->borrowernumber, $borrowerIvik->borrowernumber, "ApiKey granted to another Borrower");
+
+    Koha::ApiKeys->delete($apiKey);
+    $apiKey = Koha::ApiKeys->find({api_key_id => $apiKey->api_key_id});
+    ok(not($apiKey), "ApiKey deleted");
+}
+};
+if ($@) { #Catch all leaking errors and gracefully terminate.
+    warn $@;
+    tearDown();
+    exit 1;
+}
+
+eval {
+subtest "ApiKeys Intra Integration tests" => sub {
+    my $agent = t::lib::Page::Members::Moremember->new({borrowernumber => $borrowerKivi->borrowernumber});
+    $agent->doPasswordLogin($borrowerKivi->userid, $password)->navigateManageApiKeys()->generateNewApiKey();
+    my @apiKeys = Koha::ApiKeys->search({borrowernumber => $borrowerKivi->borrowernumber});
+    $agent->revokeApiKey($apiKeys[0]->api_key)->deleteApiKey($apiKeys[0]->api_key)
+               ->quit();
+}
+};
+if ($@) { #Catch all leaking errors and gracefully terminate.
+    warn $@;
+    tearDown();
+    exit 1;
+}
+
+eval {
+subtest "ApiKeys OPAC Integration tests" => sub {
+    my $agent = t::lib::Page::Opac::OpacMain->new();
+    $agent->doPasswordLogin($borrowerKivi->userid, $password)->navigateYourAPIKeys()->generateNewApiKey();
+    my @apiKeys = Koha::ApiKeys->search({borrowernumber => $borrowerKivi->borrowernumber});
+    $agent->revokeApiKey($apiKeys[0]->api_key)->deleteApiKey($apiKeys[0]->api_key)
+               ->quit();
+}
+};
+if ($@) { #Catch all leaking errors and gracefully terminate.
+    warn $@;
+    tearDown();
+    exit 1;
+}
+
+##All tests done, tear down test context
+tearDown();
+done_testing;
+
+sub tearDown {
+    t::lib::TestObjects::ObjectFactory->tearDownTestContext($testContext);
+}