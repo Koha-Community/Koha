EreMaijala/KohaSuomi

Bug 13995 - Proper Exception handling

Koha/Exception/BatchOverlay/RemoteSearchAmbiguous.pm
@@ -1,0 +1,29 @@
+package Koha::Exception::BatchOverlay::RemoteSearchNoResults;
+
+# Copyright 2016 KohaSuomi
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use Modern::Perl;
+
+use Exception::Class (
+    'Koha::Exception::BatchOverlay::RemoteSearchNoResults' => {
+        isa => 'Koha::Exception::BatchOverlay',
+        description => 'Searching from the remote target returned no results.',
+    },
+);
+
+return 1;

Koha/Exception/BatchOverlay/RemoteSearchFailed.pm
@@ -1,0 +1,29 @@
+package Koha::Exception::BatchOverlay::RemoteSearchAmbiguous;
+
+# Copyright 2016 KohaSuomi
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use Modern::Perl;
+
+use Exception::Class (
+    'Koha::Exception::BatchOverlay::RemoteSearchAmbiguous' => {
+        isa => 'Koha::Exception::BatchOverlay',
+        description => 'Searching from the remote target returned more results than expected.',
+    },
+);
+
+return 1;

Koha/Exception/BatchOverlay/RemoteSearchNoResults.pm
@@ -1,0 +1,29 @@
+package Koha::Exception::BatchOverlay::RemoteSearchFailed;
+
+# Copyright 2016 KohaSuomi
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use Modern::Perl;
+
+use Exception::Class (
+    'Koha::Exception::BatchOverlay::RemoteSearchFailed' => {
+        isa => 'Koha::Exception::BatchOverlay',
+        description => 'Searching from the remote target failed.',
+    },
+);
+
+return 1;

Koha-Community/Koha

Koha/Exception/BatchOverlay/RemoteSearchAmbiguous.pm
@@ -1,0 +1,29 @@
+package Koha::Exception::BatchOverlay::RemoteSearchAmbiguous;
+
+# Copyright 2016 KohaSuomi
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use Modern::Perl;
+
+use Exception::Class (
+    'Koha::Exception::BatchOverlay::RemoteSearchAmbiguous' => {
+        isa => 'Koha::Exception::BatchOverlay',
+        description => 'Searching from the remote target returned more results than expected.',
+    },
+);
+
+return 1;

Koha/Exception/BatchOverlay/RemoteSearchFailed.pm
@@ -1,0 +1,29 @@
+package Koha::Exception::BatchOverlay::RemoteSearchFailed;
+
+# Copyright 2016 KohaSuomi
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use Modern::Perl;
+
+use Exception::Class (
+    'Koha::Exception::BatchOverlay::RemoteSearchFailed' => {
+        isa => 'Koha::Exception::BatchOverlay',
+        description => 'Searching from the remote target failed.',
+    },
+);
+
+return 1;

Koha/Exception/BatchOverlay/RemoteSearchNoResults.pm
@@ -1,0 +1,29 @@
+package Koha::Exception::BatchOverlay::RemoteSearchNoResults;
+
+# Copyright 2016 KohaSuomi
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use Modern::Perl;
+
+use Exception::Class (
+    'Koha::Exception::BatchOverlay::RemoteSearchNoResults' => {
+        isa => 'Koha::Exception::BatchOverlay',
+        description => 'Searching from the remote target returned no results.',
+    },
+);
+
+return 1;