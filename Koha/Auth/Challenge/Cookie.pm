KohaSuomi/Koha-3.x

Bug 7174 - Authentication Rewrite - Core
Depends heavily on Buugg 14540, which introduces many tests to spot regression
caused by this feature.

Introduces a new modular authentication system based on Exception signaling
and reusable authentication components/challenges.
This system is cross-framework -compatible, currently supporting Mojolicious
and CGI, but adding support for any other framework is straightforward.

Central idea is to provide a single authentication function to deal with all
types of authentication scenarios, REST API, Cookie, Password, LDAP...
Also it is important to make a system that is easy to extend and can deal with
many future authentication scenarios.

Currently only CGI password, LDAP and cookie login is tested. Legacy behaviour
is used to deal with other types of authentication.

See the attached schematic in Bugzilla for a more architectural overview.

Also fixes few bugs related to anonymous_search_history

The basic principle is:
1. We get an authentication request from any Web/GUI-framework, for ex CGI.
2. Request is normalized by extracting all necessary authentication data
   elements to a separate data structure.
3. Based on the found authentication data elements, system decides which
   authentication route to take.
4. Route implements all challenges needed to authenticate the request.
   Route returns the authenticated Koha::Borrower or an Exception if login
   failed.
5. The user environment/session is set/deleted based on the Route result.
6. a Koha::Borrower and the CGISESSID-cookie is returned to the calling
   framework in the format the framework needs.
7. Framework needs to catch possible exceptions and deal with them.
   Eg. login failed, no permission, under maintenance.
8. Authentication succeeds and session is set, or failure is reported to user.

Koha/Auth/Challenge/Cookie.pm
@@ -1,0 +1,83 @@
+package Koha::Auth::Challenge::Cookie;
+
+# Copyright 2015 Vaara-kirjastot
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+use C4::Context;
+use C4::Auth;
+use Koha::AuthUtils;
+use Koha::Borrowers;
+
+use Koha::Exception::LoginFailed;
+
+use base qw(Koha::Auth::Challenge);
+
+=head challenge
+STATIC
+
+    Koha::Auth::Challenge::Cookie::challenge($cookieValue);
+
+Checks if the given authentication cookie value matches a session, and checks if
+the session is still active.
+@PARAM1 String, hashed session key identifying a session in koha.sessions
+@RETURNS Koha::Borrower matching the verified and active session
+@THROWS Koha::Exception::LoginFailed, if no session is found,
+                                      if the session has expired,
+                                      if the session IP address changes,
+                                      if no borrower was found for the session
+=cut
+
+sub challenge {
+    my ($cookie, $originIps) = @_;
+
+    my $session = C4::Auth::get_session($cookie);
+    Koha::Exception::LoginFailed->throw(error => "No session matching the given session identifier '$session'.") unless $session;
+
+    # See if the given session is timed out
+    if ( ($session->param('lasttime') || 0) < (time()- C4::Auth::_timeout_syspref()) ) {
+        $session->delete();
+        $session->flush;
+        C4::Context::_unset_userenv($cookie);
+        Koha::Exception::LoginFailed->throw(error => "Session expired, please login again.");
+    }
+    # Check if we still access using the same IP than when the session was initialized.
+    elsif ( C4::Context->preference('SessionRestrictionByIP')) {
+
+        my $sameIpFound = grep {$session->param('ip') eq $_} @$originIps;
+
+        unless ($sameIpFound) {
+            $session->delete();
+            $session->flush;
+            C4::Context::_unset_userenv($cookie);
+            Koha::Exception::LoginFailed->throw(error => "Session's client address changed, please login again.");
+        }
+    }
+
+    #Get the Borrower-object
+    my $userid   = $session->param('id');
+    my $borrower = Koha::AuthUtils::checkKohaSuperuserFromUserid($userid);
+    $borrower = Koha::Borrowers->find({userid => $userid}) if not($borrower) && $userid;
+    Koha::Exception::LoginFailed->throw(error => "Cookie authentication succeeded, but no borrower found with userid '".($userid || '')."'.")
+            unless $borrower;
+
+    $session->param( 'lasttime', time() );
+    return $borrower;
+}
+
+1;

Koha/Auth/Challenge/Password.pm
@@ -1,0 +1,127 @@
+package Koha::Auth::Challenge::Password;
+
+# Copyright 2015 Vaara-kirjastot
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+use Koha::Borrowers;
+use Koha::AuthUtils;
+
+use base qw(Koha::Auth::Challenge);
+
+use Koha::Exception::LoginFailed;
+
+our @usernameAliasColumns = ('userid', 'cardnumber'); #Possible columns to treat as the username when authenticating. Must be UNIQUE in DB.
+
+=head NAME Koha::Auth::Challenge::Password
+
+=head SYNOPSIS
+
+This module implements the more specific behaviour of the password authentication component.
+
+=cut
+
+=head challenge
+STATIC
+
+    Koha::Auth::Challenge::Password::challenge();
+
+@RETURN Koha::Borrower-object if check succeedes, otherwise throws exceptions.
+@THROWS Koha::Exception::LoginFailed from Koha::AuthUtils password checks.
+=cut
+
+sub challenge {
+    my ($userid, $password) = @_;
+
+    my $borrower;
+    if (C4::Context->config('useldapserver')) {
+        $borrower = Koha::Auth::Challenge::Password::checkLDAPPassword($userid, $password);
+        return $borrower if $borrower;
+    }
+    if (C4::Context->preference('casAuthentication')) {
+        warn("Koha::Auth doesn't support CAS-authentication yet. Please refactor the CAS client implementation to work with Koha::Auth. It cant be too hard :)");
+    }
+    if (C4::Context->config('useshibboleth')) {
+        warn("Koha::Auth doesn't support Shibboleth-authentication yet. Please refactor the Shibboleth client implementation to work with Koha::Auth. It cant be too hard :)");
+    }
+
+    return Koha::Auth::Challenge::Password::checkKohaPassword($userid, $password);
+}
+
+=head checkKohaPassword
+
+    my $borrower = Koha::Auth::Challenge::Password::checkKohaPassword($userid, $password);
+
+Checks if the given username and password match anybody in the Koha DB
+@PARAM1 String, user identifier, either the koha.borrowers.userid, or koha.borrowers.cardnumber
+@PARAM2 String, clear text password from the authenticating user
+@RETURN Koha::Borrower, if login succeeded.
+                Sets Koha::Borrower->isSuperuser() if the user is a superuser.
+@THROWS Koha::Exception::LoginFailed, if no matching password was found for all username aliases in Koha.
+=cut
+
+sub checkKohaPassword {
+    my ($userid, $password) = @_;
+    my $borrower; #Find the borrower to return
+
+    $borrower = Koha::AuthUtils::checkKohaSuperuser($userid, $password);
+    return $borrower if $borrower;
+
+    my $usernameFound = 0; #Report to the user if userid/barcode was found, even if the login failed.
+    #Check for each username alias if we can confirm a login with that.
+    for my $unameAlias (@usernameAliasColumns) {
+        my $borrower = Koha::Borrowers->find({$unameAlias => $userid});
+        if ( $borrower ) {
+            $usernameFound = 1;
+            return $borrower if ( Koha::AuthUtils::checkHash( $password, $borrower->password ) );
+        }
+    }
+
+    Koha::Exception::LoginFailed->throw(error => "Password authentication failed for the given ".( ($usernameFound) ? "password" : "username and password").".");
+}
+
+=head checkLDAPPassword
+
+Checks if the given username and password match anybody in the LDAP service
+@PARAM1 String, user identifier
+@PARAM2 String, clear text password from the authenticating user
+@RETURN Koha::Borrower, or
+            undef if we couldn't reliably contact the LDAP server so we should
+            fallback to local Koha Password authentication.
+@THROWS Koha::Exception::LoginFailed, if LDAP login failed
+=cut
+
+sub checkLDAPPassword {
+    my ($userid, $password) = @_;
+
+    #Lazy load dependencies because somebody might never need them.
+    require C4::Auth_with_ldap;
+
+    my ($retval, $cardnumber, $local_userid) = C4::Auth_with_ldap::checkpw_ldap($userid, $password);    # EXTERNAL AUTH
+    if ($retval == -1) {
+        Koha::Exception::LoginFailed->throw(error => "LDAP authentication failed for the given username and password");
+    }
+
+    if ($retval) {
+        my $borrower = Koha::Borrowers->find({userid => $local_userid});
+        return $borrower;
+    }
+    return undef;
+}
+
+1;

t/db_dependent/Koha/Borrower.t
@@ -1,0 +1,55 @@
+#!/usr/bin/perl
+
+# Copyright 2015 Open Source Freedom Fighters
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Test::More; #Please don't set the test count here. It is nothing but trouble when rebasing against master and is of dubious help.
+
+use Koha::Borrower;
+
+
+
+testIsSuperuser();
+
+
+
+
+
+################################################################################
+#### Define test subroutines here ##############################################
+################################################################################
+
+=head testIsSuperuser
+@UNIT_TEST
+Tests Koha::Borrower->isSuperuser()
+=cut
+
+sub testIsSuperuser {
+    my $borrower = Koha::Borrower->new();
+    ok((not(defined($borrower->isSuperuser()))), "isSuperuser(): By default user is not defined as superuser.");
+    ok(($borrower->isSuperuser(1) == 1), "isSuperuser(): Setting user as superuser returns 1.");
+    ok(($borrower->isSuperuser() == 1), "isSuperuser(): Getting superuser status from a superuser returns 1.");
+    ok((not(defined($borrower->isSuperuser(0)))), "isSuperuser(): Removing superuser status from a superuser OK and returns undef");
+    ok((not(defined($borrower->isSuperuser()))), "isSuperuser(): Ex-superuser superuser status is undef");
+}
+
+
+
+
+#######################
+done_testing(); #YAY!!
+#######################

Koha-Community/Koha

Koha/Auth/Challenge/Cookie.pm
@@ -1,0 +1,83 @@
+package Koha::Auth::Challenge::Cookie;
+
+# Copyright 2015 Vaara-kirjastot
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+use C4::Context;
+use C4::Auth;
+use Koha::AuthUtils;
+use Koha::Patrons;
+
+use Koha::Exception::LoginFailed;
+
+use base qw(Koha::Auth::Challenge);
+
+=head challenge
+STATIC
+
+    Koha::Auth::Challenge::Cookie::challenge($cookieValue);
+
+Checks if the given authentication cookie value matches a session, and checks if
+the session is still active.
+@PARAM1 String, hashed session key identifying a session in koha.sessions
+@RETURNS Koha::Patron matching the verified and active session
+@THROWS Koha::Exception::LoginFailed, if no session is found,
+                                      if the session has expired,
+                                      if the session IP address changes,
+                                      if no borrower was found for the session
+=cut
+
+sub challenge {
+    my ($cookie, $originIps) = @_;
+
+    my $session = C4::Auth::get_session($cookie);
+    Koha::Exception::LoginFailed->throw(error => "No session matching the given session identifier '$session'.") unless $session;
+
+    # See if the given session is timed out
+    if ( ($session->param('lasttime') || 0) < (time()- C4::Auth::_timeout_syspref()) ) {
+        $session->delete();
+        $session->flush;
+        C4::Context::_unset_userenv($cookie);
+        Koha::Exception::LoginFailed->throw(error => "Session expired, please login again.");
+    }
+    # Check if we still access using the same IP than when the session was initialized.
+    elsif ( C4::Context->preference('SessionRestrictionByIP')) {
+
+        my $sameIpFound = grep {$session->param('ip') eq $_} @$originIps;
+
+        unless ($sameIpFound) {
+            $session->delete();
+            $session->flush;
+            C4::Context::_unset_userenv($cookie);
+            Koha::Exception::LoginFailed->throw(error => "Session's client address changed, please login again.");
+        }
+    }
+
+    #Get the Borrower-object
+    my $userid   = $session->param('id');
+    my $borrower = Koha::AuthUtils::checkKohaSuperuserFromUserid($userid);
+    $borrower = Koha::Patrons->find({userid => $userid}) if not($borrower) && $userid;
+    Koha::Exception::LoginFailed->throw(error => "Cookie authentication succeeded, but no borrower found with userid '".($userid || '')."'.")
+            unless $borrower;
+
+    $session->param( 'lasttime', time() );
+    return $borrower;
+}
+
+1;

Koha/Auth/Challenge/Password.pm
@@ -1,0 +1,127 @@
+package Koha::Auth::Challenge::Password;
+
+# Copyright 2015 Vaara-kirjastot
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+use Koha::Patrons;
+use Koha::AuthUtils;
+
+use base qw(Koha::Auth::Challenge);
+
+use Koha::Exception::LoginFailed;
+
+our @usernameAliasColumns = ('userid', 'cardnumber'); #Possible columns to treat as the username when authenticating. Must be UNIQUE in DB.
+
+=head NAME Koha::Auth::Challenge::Password
+
+=head SYNOPSIS
+
+This module implements the more specific behaviour of the password authentication component.
+
+=cut
+
+=head challenge
+STATIC
+
+    Koha::Auth::Challenge::Password::challenge();
+
+@RETURN Koha::Patron-object if check succeedes, otherwise throws exceptions.
+@THROWS Koha::Exception::LoginFailed from Koha::AuthUtils password checks.
+=cut
+
+sub challenge {
+    my ($userid, $password) = @_;
+
+    my $borrower;
+    if (C4::Context->config('useldapserver')) {
+        $borrower = Koha::Auth::Challenge::Password::checkLDAPPassword($userid, $password);
+        return $borrower if $borrower;
+    }
+    if (C4::Context->preference('casAuthentication')) {
+        warn("Koha::Auth doesn't support CAS-authentication yet. Please refactor the CAS client implementation to work with Koha::Auth. It cant be too hard :)");
+    }
+    if (C4::Context->config('useshibboleth')) {
+        warn("Koha::Auth doesn't support Shibboleth-authentication yet. Please refactor the Shibboleth client implementation to work with Koha::Auth. It cant be too hard :)");
+    }
+
+    return Koha::Auth::Challenge::Password::checkKohaPassword($userid, $password);
+}
+
+=head checkKohaPassword
+
+    my $borrower = Koha::Auth::Challenge::Password::checkKohaPassword($userid, $password);
+
+Checks if the given username and password match anybody in the Koha DB
+@PARAM1 String, user identifier, either the koha.borrowers.userid, or koha.borrowers.cardnumber
+@PARAM2 String, clear text password from the authenticating user
+@RETURN Koha::Patron, if login succeeded.
+                Sets Koha::Patron->isSuperuser() if the user is a superuser.
+@THROWS Koha::Exception::LoginFailed, if no matching password was found for all username aliases in Koha.
+=cut
+
+sub checkKohaPassword {
+    my ($userid, $password) = @_;
+    my $borrower; #Find the borrower to return
+
+    $borrower = Koha::AuthUtils::checkKohaSuperuser($userid, $password);
+    return $borrower if $borrower;
+
+    my $usernameFound = 0; #Report to the user if userid/barcode was found, even if the login failed.
+    #Check for each username alias if we can confirm a login with that.
+    for my $unameAlias (@usernameAliasColumns) {
+        my $borrower = Koha::Patrons->find({$unameAlias => $userid});
+        if ( $borrower ) {
+            $usernameFound = 1;
+            return $borrower if ( Koha::AuthUtils::checkHash( $password, $borrower->password ) );
+        }
+    }
+
+    Koha::Exception::LoginFailed->throw(error => "Password authentication failed for the given ".( ($usernameFound) ? "password" : "username and password").".");
+}
+
+=head checkLDAPPassword
+
+Checks if the given username and password match anybody in the LDAP service
+@PARAM1 String, user identifier
+@PARAM2 String, clear text password from the authenticating user
+@RETURN Koha::Patron, or
+            undef if we couldn't reliably contact the LDAP server so we should
+            fallback to local Koha Password authentication.
+@THROWS Koha::Exception::LoginFailed, if LDAP login failed
+=cut
+
+sub checkLDAPPassword {
+    my ($userid, $password) = @_;
+
+    #Lazy load dependencies because somebody might never need them.
+    require C4::Auth_with_ldap;
+
+    my ($retval, $cardnumber, $local_userid) = C4::Auth_with_ldap::checkpw_ldap($userid, $password);    # EXTERNAL AUTH
+    if ($retval == -1) {
+        Koha::Exception::LoginFailed->throw(error => "LDAP authentication failed for the given username and password");
+    }
+
+    if ($retval) {
+        my $borrower = Koha::Patrons->find({userid => $local_userid});
+        return $borrower;
+    }
+    return undef;
+}
+
+1;

t/db_dependent/Koha/Borrower.t
@@ -1,0 +1,55 @@
+#!/usr/bin/perl
+
+# Copyright 2015 Open Source Freedom Fighters
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Test::More; #Please don't set the test count here. It is nothing but trouble when rebasing against master and is of dubious help.
+
+use Koha::Patron;
+
+
+
+testIsSuperuser();
+
+
+
+
+
+################################################################################
+#### Define test subroutines here ##############################################
+################################################################################
+
+=head testIsSuperuser
+@UNIT_TEST
+Tests Koha::Borrower->isSuperuser()
+=cut
+
+sub testIsSuperuser {
+    my $borrower = Koha::Patron->new();
+    ok((not(defined($borrower->isSuperuser()))), "isSuperuser(): By default user is not defined as superuser.");
+    ok(($borrower->isSuperuser(1) == 1), "isSuperuser(): Setting user as superuser returns 1.");
+    ok(($borrower->isSuperuser() == 1), "isSuperuser(): Getting superuser status from a superuser returns 1.");
+    ok((not(defined($borrower->isSuperuser(0)))), "isSuperuser(): Removing superuser status from a superuser OK and returns undef");
+    ok((not(defined($borrower->isSuperuser()))), "isSuperuser(): Ex-superuser superuser status is undef");
+}
+
+
+
+
+#######################
+done_testing(); #YAY!!
+#######################