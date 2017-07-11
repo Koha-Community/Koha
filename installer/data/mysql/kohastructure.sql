d1a6502d342ac4fd37fec9e546602baf051deb47
Lari Taskula <lari.taskula@jns.fi>
2017-07-11 14:46:59 +0300

    KD#1446 (squashable) Vetuma - add tables to kohastructure.sql
    Adds Vetuma online payment integration tables to kohastructure.sql. Otherwise
    causing warnings when running unit tests.

--- installer/data/mysql/kohastructure.sql
+++ installer/data/mysql/kohastructure.sql
@@ -4285,6 +4285,42 @@ CREATE TABLE `payments_transactions_accountlines` ( -- related accountlines for 
     ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Table structure for table `vetuma_transaction`
+--
+
+DROP TABLE IF EXISTS `vetuma_transaction`;
+CREATE TABLE `vetuma_transaction` (
+    transaction_id  BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    amount  DECIMAL(28,6) NULL,
+    request_timestamp  BIGINT UNSIGNED NULL,
+    response_timestamp  BIGINT UNSIGNED NULL,
+    ref VARCHAR(255) NULL,
+    trid VARCHAR(255) NULL,
+    response_so VARCHAR(255) NULL,
+    payid VARCHAR(255) NULL,
+    paid VARCHAR(255) NULL,
+    status VARCHAR(255) NULL,
+    PRIMARY KEY (transaction_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Table structure for table `vetuma_transaction_accountlines_link`
+--
+
+DROP TABLE IF EXISTS `vetuma_transaction_accountlines_link`;
+CREATE TABLE `vetuma_transaction_accountlines_link` (
+    accountlines_id  INT(11) NOT NULL,
+    transaction_id  BIGINT UNSIGNED NOT NULL,
+    PRIMARY KEY (accountlines_id, transaction_id),
+    KEY ix_vetuma_transaction_link_accountlines_id (accountlines_id),
+    KEY ix_vetuma_transaction_link_transaction_id (transaction_id),
+    FOREIGN KEY (accountlines_id) REFERENCES accountlines(accountlines_id)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (transaction_id) REFERENCES vetuma_transaction(transaction_id)
+        ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
