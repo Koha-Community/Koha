506d669424fd85edb7986fd045bde0821964bc1f
Aleisha Amohia <aleishaamohia@hotmail.com>
2017-08-02 23:25:35 +0000

    Bug 2093: (follow-up) Add OPAC dashboard for logged-in users
    This patch:
    - hides the dashboard if there is no dashboard information to display
    - changes '5.00 due' to '5.00 due in fines and charges' for translation
    - uses Koha::Holds in place of deprecated C4::Reserves methods
    
    To test, confirm all the right information for holds still shows, and
    confirm the dashboard is hidden if there are no checkouts, holds, fines
    or overdues.
    
    Signed-off-by: Hugo Agud <hagud@orex.es>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- opac/opac-main.pl
+++ opac/opac-main.pl
@@ -27,8 +27,8 @@ use C4::Languages qw(getTranslatedLanguages accept_language);
 use C4::Koha qw( GetDailyQuote );
 use C4::Members;
 use C4::Overdues;
-use C4::Reserves;
 use Koha::Checkouts;
+use Koha::Holds;
 
 my $input = new CGI;
 my $dbh   = C4::Context->dbh;
@@ -66,23 +66,22 @@ my $koha_news_count = scalar @$all_koha_news;
 
 my $quote = GetDailyQuote();   # other options are to pass in an exact quote id or select a random quote each pass... see perldoc C4::Koha
 
+# For dashboard
 my $checkouts = Koha::Checkouts->search({ borrowernumber => $borrowernumber })->count;
 my ( $overdues_count, $overdues ) = checkoverdues($borrowernumber);
-my @holds = GetReservesFromBorrowernumber($borrowernumber);
-my $holds_pending = 0;
-foreach my $hold (@holds) {
-    if (not defined($hold->{found})){
-        $holds_pending++;
-    }
-}
-my @holds_waiting = GetReservesFromBorrowernumber($borrowernumber, 'W');
+my $holds_pending = Koha::Holds->search({ borrowernumber => $borrowernumber, found => undef })->count;
+my $holds_waiting = Koha::Holds->search({ borrowernumber => $borrowernumber })->waiting->count;
 my ( $total , $accts, $numaccts) = GetMemberAccountRecords( $borrowernumber );
+
+if  ( $checkouts > 0 || $overdues_count > 0 || $holds_pending > 0 || $holds_waiting > 0 || $total > 0 ) {
+    $template->param( dashboard_info => 1 );
+}
 
 $template->param(
     checkouts           => $checkouts,
     overdues            => $overdues_count,
     holds_pending       => $holds_pending,
-    holds_waiting       => scalar @holds_waiting,
+    holds_waiting       => $holds_waiting,
     total_owing         => $total,
     koha_news           => $all_koha_news,
     koha_news_count     => $koha_news_count,
