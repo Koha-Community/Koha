319f5f67aaa056952ecc9a4500fa52627db4fdcc
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2017-08-07 17:25:15 -0300

    Bug 19055: Remove C4::Reserves::GetReservesToBranch
    This subroutine is no longer in used and can be removed
    
    Test plan:
      git grep GetReservesToBranch
    must not return any results
    
    Signed-off-by: Marc Véron <veron@veron.ch>
    Signed-off-by: Julian Maurice <julian.maurice@biblibre.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    (cherry picked from commit 9cc7268362219e39b22aebb03a83e013f467d93a)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- C4/Reserves.pm
+++ C4/Reserves.pm
@@ -106,7 +106,6 @@ BEGIN {
         &GetReserve
         &GetReservesFromBorrowernumber
         &GetReservesForBranch
-        &GetReservesToBranch
         &GetReserveCount
         &GetReserveInfo
         &GetReserveStatus
@@ -641,33 +640,6 @@ SELECT COUNT(*) FROM reserves WHERE biblionumber=? AND borrowernumber<>?
         }
     }
     return $fee;
-}
-
-=head2 GetReservesToBranch
-
-  @transreserv = GetReservesToBranch( $frombranch );
-
-Get reserve list for a given branch
-
-=cut
-
-sub GetReservesToBranch {
-    my ( $frombranch ) = @_;
-    my $dbh = C4::Context->dbh;
-    my $sth = $dbh->prepare(
-        "SELECT reserve_id,borrowernumber,reservedate,itemnumber,timestamp
-         FROM reserves
-         WHERE priority='0'
-           AND branchcode=?"
-    );
-    $sth->execute( $frombranch );
-    my @transreserv;
-    my $i = 0;
-    while ( my $data = $sth->fetchrow_hashref ) {
-        $transreserv[$i] = $data;
-        $i++;
-    }
-    return (@transreserv);
 }
 
 =head2 GetReservesForBranch
