7f6f98e00c319e60e9296b96913c8321366a7d12
Josef Moravec <josef.moravec@gmail.com>
2017-08-08 15:03:19 +0200

    Bug 19043: Z39.50 target clio-db.cc.columbia.edu:7090 is no longer publicly available
    Test plan:
    0) Apply the patch
    1) Install all affected languages: de-DE, es-ES, fr-CA and pl-PL
    2) Remove and recreate Koha database
    3) Go through web installer in English and don't forgot to load sample Z39.50 servers
    4) Go to Administration -> Z39.50/SRU Servers and confirm the sample
    servers are here, but columbia.edu is not here
    5) Repeat 2-4 for other languages: de-DE, es-ES, fr-CA and pl-PL
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- installer/data/mysql/en/optional/sample_z3950_servers.sql
+++ installer/data/mysql/en/optional/sample_z3950_servers.sql
@@ -2,8 +2,7 @@ INSERT INTO z3950servers
 (host, port, db, userid, password, servername, checked, rank, syntax, encoding,recordtype) VALUES
 ('lx2.loc.gov',210,'LCDB','','','LIBRARY OF CONGRESS',1,1,'USMARC','utf8','biblio'),
 ('lx2.loc.gov',210,'NAF','','','LIBRARY OF CONGRESS NAMES',1,1,'USMARC','utf8','authority'),
-('lx2.loc.gov',210,'SAF','','','LIBRARY OF CONGRESS SUBJECTS',1,2,'USMARC','utf8','authority'),
-('clio-db.cc.columbia.edu',7090,'voyager','','','COLUMBIA UNIVERSITY',0,0,'USMARC','MARC-8','biblio');
+('lx2.loc.gov',210,'SAF','','','LIBRARY OF CONGRESS SUBJECTS',1,2,'USMARC','utf8','authority');
 
 #Insert SRU server
 INSERT INTO z3950servers
--- installer/data/mysql/pl-PL/optional/sample_z3950_servers.sql
+++ installer/data/mysql/pl-PL/optional/sample_z3950_servers.sql
@@ -3,8 +3,7 @@ INSERT INTO z3950servers
 ('alpha.bn.org.pl',210,'INNOPAC','','','BIBLIOTEKA NARODOWA',1,1,'MARC21','utf8', 'biblio'),
 ('lx2.loc.gov',210,'LCDB','','','LIBRARY OF CONGRESS',1,1,'USMARC','utf8','biblio'),
 ('lx2.loc.gov',210,'NAF','','','LIBRARY OF CONGRESS NAMES',1,1,'USMARC','utf8','authority'),
-('lx2.loc.gov',210,'SAF','','','LIBRARY OF CONGRESS SUBJECTS',1,2,'USMARC','utf8','authority'),
-('clio-db.cc.columbia.edu',7090,'voyager','','','COLUMBIA UNIVERSITY',0,0,'USMARC','MARC-8','biblio');
+('lx2.loc.gov',210,'SAF','','','LIBRARY OF CONGRESS SUBJECTS',1,2,'USMARC','utf8','authority');
 
 #Insert SRU server
 INSERT INTO z3950servers
