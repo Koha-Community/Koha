933dd039790734024d84d85e5778f8a23051de37
Mason James <mtj@kohaaloha.com>
2017-08-24 19:22:22 +1200

    rm test files

--- installer/data/mysql/atomicupdate/bug_18663.sql
+++
@@ -1,2 +0,0 @@
-INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type` ) VALUES
-('ExportRemoveFields','',NULL,'List of fields for non export in circulation.pl (separated by a space)','Free');
