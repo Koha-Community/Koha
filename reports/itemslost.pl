abbd0eeea9b686584c3f2c5d7201b43a4e76ae76
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2017-10-05 17:38:34 -0300

    Bug 9573: Lost items report - Add the CSV export ability
    Finally we add the ability to export the list of lost items.
    The items will be export in CSV format using a CSV profile defined.
    
    Test plan:
    0/ Apply all the patches from this patch set
    1/ Define a CSV profile (type=SQL, Usage=Export lost items in report)
    Try something like that to get the same columns as the default table:
    Title=biblio.title
    |Author=biblio.author
    |Lost status=items.itemlost
    |Lost on=items.itemlost_on
    |Barcode=items.barcode
    |Call number=items.itemcallnumber
    |Date last seen=items.datelastseen
    |Price=items.price
    |Rep. price=items.replacementprice
    |Library=items.homebranch
    |item type=items.itype
    |Current location=items.holdingbranch
    |Location=items.location
    |Not for loan status=items.notforloan
    |Notes=items.itemnotes
    2/ Use the filters and select items to export
    3/ Export the list of items you want and make sure the CSV is correctly
    formatted and contains the items you selected
    
    QA Notes:
    - I think we should add default CSV profiles for the different "usage",
    but I would consider it as a separate enhancement since none of them is
    defined yet
    - Most of the code to export CSV is no reusable. We should make
    Koha::Exporter::Record support CSV export for type=SQL (it only
    supports MARC type so far).
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Signed-off-by: Séverine QUEUNE <severine.queune@bulac.fr>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- reports/itemslost.pl
+++ reports/itemslost.pl
@@ -34,6 +34,7 @@ use C4::Biblio;
 use C4::Items;
 
 use Koha::AuthorisedValues;
+use Koha::CsvProfiles;
 use Koha::DateUtils;
 
 my $query = new CGI;
@@ -50,8 +51,62 @@ my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
 
 my $params = $query->Vars;
 my $get_items = $params->{'get_items'};
+my $op = $query->param('op') || '';
 
-if ($get_items) {
+if ( $op eq 'export' ) {
+    my @itemnumbers = $query->multi_param('itemnumber');
+    my $csv_profile_id = $query->param('csv_profile_id');
+    my @rows;
+    if ($csv_profile_id) {
+        # FIXME This following code has the same logic as GetBasketAsCSV
+        # We should refactor all the CSV export code
+        # Note: For MARC it is already done in Koha::Exporter::Record but not for SQL CSV profiles type
+        my $csv_profile = Koha::CsvProfiles->find( $csv_profile_id );
+        die "There is no valid csv profile given" unless $csv_profile;
+
+        my $csv = Text::CSV_XS->new({'quote_char'=>'"','escape_char'=>'"','sep_char'=>$csv_profile->csv_separator,'binary'=>1});
+        my $csv_profile_content = $csv_profile->content;
+        my ( @headers, @fields );
+        while ( $csv_profile_content =~ /
+            ([^=\|]+) # header
+            =?
+            ([^\|]*) # fieldname (table.row or row)
+            \|? /gxms
+        ) {
+            my $header = $1;
+            my $field = ($2 eq '') ? $1 : $2;
+
+            $header =~ s/^\s+|\s+$//g; # Trim whitespaces
+            push @headers, $header;
+
+            $field =~ s/[^\.]*\.{1}//; # Remove the table name if exists.
+            $field =~ s/^\s+|\s+$//g; # Trim whitespaces
+            push @fields, $field;
+        }
+        my $items = Koha::Items->search({ itemnumber => { -in => \@itemnumbers } });
+        while ( my $item = $items->next ) {
+            my @row;
+            my $all_fields = $item->unblessed;
+            $all_fields = { %$all_fields, %{$item->biblio->unblessed}, %{$item->biblioitem->unblessed} };
+            for my $field (@fields) {
+                push @row, $all_fields->{$field};
+            }
+            push @rows, \@row;
+        }
+        my $content = join( $csv_profile->csv_separator, @headers ) . "\n";
+        for my $row ( @rows ) {
+            $csv->combine(@$row);
+            my $string = $csv->string;
+            $content .= $string . "\n";
+        }
+        print $query->header(
+            -type       => 'text/csv',
+            -attachment => 'lost_items.csv',
+        );
+        print $content;
+        exit;
+    }
+} elsif ( $get_items ) {
     my $branchfilter     = $params->{'branchfilter'}     || undef;
     my $barcodefilter    = $params->{'barcodefilter'}    || undef;
     my $itemtypesfilter  = $params->{'itemtypesfilter'}  || undef;
@@ -96,8 +151,11 @@ if ($get_items) {
 # getting all itemtypes
 my $itemtypes = Koha::ItemTypes->search_with_localization;
 
+my $csv_profiles = Koha::CsvProfiles->search({ type => 'sql', used_for => 'export_lost_items' });
+
 $template->param(
     itemtypes => $itemtypes,
+    csv_profiles => $csv_profiles,
 );
 
 # writing the template
