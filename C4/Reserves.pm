745aee7c8f74208bda07742935df52c24c1c99ae
Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
2017-10-06 11:05:49 +0200

    Bug 19260: [QA Follow-up] Remove obsolete $dbh
    The variable is no longer used.
    Removed a few empty lines on the way.
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- C4/Reserves.pm
+++ C4/Reserves.pm
@@ -876,17 +876,12 @@ Cancels all reserves with an expiration date from before today.
 =cut
 
 sub CancelExpiredReserves {
-
     my $today = dt_from_string();
     my $cancel_on_holidays = C4::Context->preference('ExpireReservesOnHolidays');
     my $expireWaiting = C4::Context->preference('ExpireReservesMaxPickUpDelay');
 
-    my $dbh = C4::Context->dbh;
-
     my $dtf = Koha::Database->new->schema->storage->datetime_parser;
-
     my $params = { expirationdate => { '<', $dtf->format_date($today) } };
-
     $params->{found} = undef unless $expireWaiting;
 
     # FIXME To move to Koha::Holds->search_expired (?)
