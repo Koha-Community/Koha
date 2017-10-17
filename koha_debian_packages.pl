a455b82bedf9718f0c6561c6d8675cda9d8c37da
Johanna Raisa <johanna.raisa@gmail.com>
2017-10-17 11:47:04 +0000

    KD-1628, (squashable) adding install option

--- koha_debian_packages.pl
+++ koha_debian_packages.pl
@@ -9,9 +9,11 @@ use Getopt::Long;
 use C4::KohaSuomi::DebianPackages;
 
 my $help;
+my $install;
 
 GetOptions(
     'h|help'             => \$help,
+    'i|install'          => \$install
 );
 
 my $usage = << 'ENDUSAGE';
@@ -23,7 +25,8 @@ All packages which are not core-Koha are excluded.
 
 Such as apache2, idzebra2, mysql-*, memcached.
 
-These are intended to be installed in separate servers or by using specific Ansible roles.
+These are intended to be installed in separate servers or by using specific Ansible roles 
+or script can be used to install them to current server with sudo and option -i or -install.
 
 ENDUSAGE
 
@@ -33,4 +36,13 @@ if ($help) {
 }
 
 my $packageNames = C4::KohaSuomi::DebianPackages::getKohaSuomiDebianPackageNames();
-print join("\n", @$packageNames);
+if ($install) {
+	foreach my $package (@$packageNames) {
+		my $cmd = "/usr/bin/apt-get install $package";
+      		my $output = system($cmd);
+		print "$output\n";
+		print "--------------------------------------------------------------------------------------------------------\n";
+	}
+} else {
+	print join("\n", @$packageNames);
+}
