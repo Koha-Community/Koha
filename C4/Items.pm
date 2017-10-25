b85a24d6debdbea7bb5786a1470b5143ade3cfca
Kyle M Hall <kyle@bywatersolutions.com>
2017-10-25 14:24:18 -0400

    Bug 19530: Prevent multiple transfers from existing for one item
    We have found multiple open branch transfers for a given item. While I'm not sure how it is happening at this point, it seems that it would make sense to cancel any existing branch transfers when initiating a new one.
    
    Test Plan:
    1) prove t/db_dependent/Circulation/transfers.t
    
    Signed-off-by: Mark Tompsett <mtompset@hotmail.com>
    
    Signed-off-by: Josef Moravec <josef.moravec@gmail.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- C4/Items.pm
+++ C4/Items.pm
@@ -622,6 +622,8 @@ sub ModItemTransfer {
     # Remove the 'shelving cart' location status if it is being used.
     CartToShelf( $itemnumber ) if ( C4::Context->preference("ReturnToShelvingCart") );
 
+    $dbh->do("UPDATE branchtransfers SET datearrived = NOW(), comments = ? WHERE itemnumber = ?", undef, "Canceled, new transfer from $frombranch to $tobranch created", $itemnumber);
+
     #new entry in branchtransfers....
     my $sth = $dbh->prepare(
         "INSERT INTO branchtransfers (itemnumber, frombranch, datesent, tobranch)
