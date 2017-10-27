09a49bc96734af8d7615a1c7b024ff69ee2df04c
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2017-10-27 15:33:14 -0300

    Bug 19377: (follow-up) Remove charge for 'books' in ru-RU sample data
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- installer/data/mysql/ru-RU/optional/library_item_types.sql
+++ installer/data/mysql/ru-RU/optional/library_item_types.sql
@@ -2,7 +2,7 @@ SET foreign_key_checks = 0;
 TRUNCATE itemtypes;
 SET foreign_key_checks = 1;
 INSERT INTO itemtypes (itemtype, description, rentalcharge, notforloan, imageurl, summary) VALUES
-('BK', ' Книги',5,0,'bridge/book.gif',''),
+('BK', ' Книги',0,0,'bridge/book.gif',''),
 ('MX', 'Смешанные атериалы',5,0,'bridge/kit.gif',''),
 ('CF', 'Компьютерные файлы',5,0,'bridge/computer_file.gif',''),
 ('MP', 'Карты',5,0,'bridge/map.gif',''),
