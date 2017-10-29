c1d72e60df9ac1378ed7a8d0be903ab8be34c86a
Katrin Fischer <katrin.fischer.83@web.de>
2017-10-29 18:42:12 +0100

    Bug 18801: DBRev 16.11.13.001

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -13890,6 +13890,23 @@ if ( CheckVersion($DBversion) ) {
     SetVersion($DBversion);
 }
 
+$DBversion = "16.11.13.001";
+if( CheckVersion( $DBversion ) ) {
+    # Fetch all auth types
+    my $authtypes = $dbh->selectcol_arrayref( q|SELECT authtypecode FROM auth_types| );
+
+    if( grep { $_ eq 'Default' } @$authtypes ) {
+        # If this exists as an authtypecode, we don't do anything
+    } else {
+        # Replace the incorrect Default by empty string
+        $dbh->do( q|UPDATE auth_header SET authtypecode='' WHERE authtypecode='Default'| );
+    }
+
+    SetVersion( $DBversion );
+    print "Upgrade to $DBversion done (Bug 18801 - Update incorrect Default auth type codes)\n";
+}
+
+
 # DEVELOPER PROCESS, search for anything to execute in the db_update directory
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
