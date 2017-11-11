cf5835fc5286c7e087da2eeec06cf0417a2502d3
Magnus Enger <magnus@libriotech.no>
2017-11-11 14:02:22 +0000

    Bug 18591: Database updates
    Signed-off-by: andrew.isherwood@ptfs-europe.com
    
    Bug 18591: (QA follow-up) Fix merge error in kohastructure.sql
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>

--- installer/data/mysql/kohastructure.sql
+++ installer/data/mysql/kohastructure.sql
@@ -4223,6 +4223,28 @@ CREATE TABLE `oauth_access_tokens` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
+-- Table structure for table illcomments
+--
+
+DROP TABLE IF EXISTS `illcomments`;
+CREATE TABLE illcomments (
+    illcomment_id int(11) NOT NULL AUTO_INCREMENT, -- Unique ID of the comment
+    illrequest_id bigint(20) unsigned NOT NULL,    -- ILL request number
+    borrowernumber integer DEFAULT NULL,           -- Link to the user who made the comment (could be librarian, patron or ILL partner library)
+    comment text DEFAULT NULL,                     -- The text of the comment
+    timestamp timestamp DEFAULT CURRENT_TIMESTAMP, -- Date and time when the comment was made
+    PRIMARY KEY  ( illcomment_id ),
+    CONSTRAINT illcomments_bnfk
+      FOREIGN KEY ( borrowernumber )
+      REFERENCES  borrowers  ( borrowernumber )
+      ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT illcomments_ifk
+      FOREIGN KEY (illrequest_id)
+      REFERENCES illrequests ( illrequest_id )
+      ON UPDATE CASCADE ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
 -- Table structure for table `circulation_rules`
 --
 
