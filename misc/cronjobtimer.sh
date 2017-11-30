6109ecb6014beae3905d71c2f4606e25686af949
janPasi <pasi.korkalo@ouka.fi>
2017-11-30 14:53:42 +0200

    KD-2612 Cronjobtimer fixes and cleanup
    Fix cronjobtimer.sh locking and convert the script to POSIX sh

--- misc/cronjobtimer.sh
+++ misc/cronjobtimer.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 ## SYNOPSIS ##
 # This script takes another script and measures it's runtime, appending it to the same log that the script's stderr and stdout go.
@@ -8,48 +8,47 @@
 # This script also locks the given script to prevent cronjobs from going to a repeating loop.
 #
 
-logdirectory=$(grep -Po '(?<=<logdir>).*?(?=</logdir>)' $KOHA_CONF)/
-logfile=$1".log"
-cronjobdir=$(dirname "$logfile")
-logpath=$logdirectory$logfile
+logdirectory=$(grep -Po '(?<=<logdir>).*?(?=</logdir>)' $KOHA_CONF)
+logfile=$logdirectory/${1#*/}.log
 croncommand=$KOHA_PATH"/misc/$@"
-lockfile=$KOHA_PATH"/misc/$1.lock"
+lockdirectory="${KOHA_CONF%/etc/koha-conf.xml}/var/lock"
+lockfile="$lockdirectory/${1##*/}.lock"
 disableCronjobsFlag=$KOHA_PATH"/misc/cronjobs/disableCronjobs.flag"
 
-if [ ! -d "$logdirectory/$cronjobdir" ]; then
-        #Make sure that the cronjob folders exists
-        mkdir -p "$logdirectory/$cronjobdir"
+if [ -z "$1" ]; then
+  printf "You need to provide a cronscript name and it's parameters as arguments."
+  exit 1
 fi
 
+# Create required directories
+mkdir -p "$logdirectory/$cronjobdir" "$lockdirectory"
 
 starttime=$(date +%s)
 startMsg='Start: '$(date --date="@$starttime" "+%Y-%m-%d %H:%M:%S")
-printf "$startMsg\n" >> $logpath
+printf "$startMsg\n" >> $logfile
 
 #Check if cronjobs are disabled by the preproduction to production migration process
 if [ -e $disableCronjobsFlag ]; then
-	echo "Disabled by disableCronjobsFlag" >> $logpath
-        exit 0;
+    printf "Disabled by disableCronjobsFlag\n" >> $logfile
+    exit 0
 fi
 
 #Lock the cronjob we are running!
-if [ -e $lockfile ]; then
-	echo "Lockfile present" >> $logpath
-	exit 0;
+if kill -0 $(cat $lockfile 2> /dev/null) 2> /dev/null; then
+   printf "$1 already locked and running!\n"
+   exit 1
 fi
-touch $lockfile
-
+echo $$ > $lockfile
 
 starttime=$(date +%s)
 
-
-$croncommand 2&>> $logpath
-
+$croncommand 1> $logfile 2>&1
+wait # We need to wait here so that lockfile doesn't get released too early!
 
 endtime=$(date +%s)
 runtime=$((endtime - starttime))
 timelog='End: '$(date --date="@$endtime" "+%Y-%m-%d %H:%M:%S")"\n"'Runtime: '$(($runtime/60/60))':'$(($runtime/60%60))':'$(($runtime%60))
 
-printf "$timelog\n" >> $logpath
+printf "$timelog\n" >> $logfile
 
-rm $lockfile
+rm -f $lockfile
