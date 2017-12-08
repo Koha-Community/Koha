04d79ba063bafcb15ae9c2fbb33bd7c7c96b4e60
Owen Leonard <oleonard@myacpl.org>
2017-12-08 16:04:15 +0000

    Bug 19778: Move template JavaScript to the footer: Serials, part 4
    This patch modifies more and more staff client serials templates so
    that JavaScript is included in the footer instead of the header.
    
    This patch adds a new JavaScript include, showpredictionpattern.js,
    which is used by subscription-add.tt and subscription-numberpatterns.tt.
    it also adds subscription-add.js, moving most of the JS embedded in
    subscription-add.tt into an external file.
    
    To test, apply the patch and test the JavaScript-driven features of the
    modified templates: All button controls, DataTables functionality, tabs,
    etc.
    
     - Serials -> New subscription
       - Date pickers
       - Popup search windows for vendors and bibliographic
         records
       - Next / Previous buttons
       - Form validation
       - Prediction pattern operations
    
       Test with new subscriptions, editing existing subscriptions, and
       duplicating subscriptions.
    
     - Serials -> Manage Numbering Patterns
       - Datatable
       - Delete confirmation
       -> Edit
          - Datepicker, test pattern
     - Serials -> Manage frequencies
       - Delete confirmation
       -> Edit
          - Form validation
     - Serials -> Subscription details -> Planning tab
       -> Edit history
          -> Datepicker
     - Serials -> Subscription details -> Renew
       - Datepicker
    
    Signed-off-by: Dominic Pichette <dominic@inlibro.com>
    
    Signed-off-by: Josef Moravec <josef.moravec@gmail.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

---
+++ koha-tmpl/intranet-tmpl/prog/js/showpredictionpattern.js
@@ -0,0 +1,16 @@
+function Check_boxes(dow) {
+    if($(":checkbox[data-dow='"+dow+"']:first").is(':checked')) {
+        $("#predictionst :checkbox[data-dow='"+dow+"']").each(function(){
+            $(this).prop('checked', true);
+        });
+    } else {
+        $("#predictionst :checkbox[data-dow='"+dow+"']").each(function(){
+            $(this).prop('checked', false);
+        });
+    }
+}
+$(document).ready(function(){
+    $("#displayexample").on("change",".skipday",function(){
+        Check_boxes( $(this).data("dow"));
+    });
+});
