d2a045b9d508d56e556ecbbb11238d8108ce4512
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2017-12-20 20:29:29 -0300

    Bug 15770: Do not format numbers if too big
    At several places we got the following error if we use numbers too big
    for Number::Format
    Template process failed: undef error - round() overflow. Try smaller
    precision or use Math::BigFloat at /home/koha/src/Koha/Number/Price.pm line 44
    
    It make the app explodes.
    The goal here is to handle these errors gracefully and easily.
    
    Test plan:
    - Add a manual fine to a patron of 100000000000000
    - Create a patron category with an enrolment fee of 123456789012345
    
    Signed-off-by: Mark Tompsett <mtompset@hotmail.com>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- Koha/Number/Price.pm
+++ Koha/Number/Price.pm
@@ -41,6 +41,11 @@ sub format {
 
     my $format_params = $self->_format_params( $params );
 
+    # To avoid the system to crash, we will not format big number
+    # We divide per 100 because we want to keep the default DECIMAL_DIGITS (2)
+    # error - round() overflow. Try smaller precision or use Math::BigFloat
+    return $self->value if $self->value > Number::Format::MAX_INT/100;
+
     return Number::Format->new(%$format_params)->format_price($self->value);
 }
 
