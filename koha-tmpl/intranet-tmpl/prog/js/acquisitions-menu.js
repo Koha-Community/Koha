6dd0ff217460375bb01e192f642c592912c6fcbb
Josef Moravec <josef.moravec@gmail.com>
2017-12-21 18:12:38 +0000

    Bug 19753: (QA follow-up) Style invoices menu item when on individual invoice page
    Signed-off-by: Josef Moravec <josef.moravec@gmail.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- koha-tmpl/intranet-tmpl/prog/js/acquisitions-menu.js
+++ koha-tmpl/intranet-tmpl/prog/js/acquisitions-menu.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
     var path = location.pathname.substring(1);
-    $('#navmenulist a[href$="/' + path + '"]').css('font-weight','bold');
+    if (path.indexOf("invoice") >= 0) {
+        $('#navmenulist a[href$="/cgi-bin/koha/acqui/invoices.pl"]').css('font-weight','bold');
+    } else {
+        $('#navmenulist a[href$="/' + path + '"]').css('font-weight','bold');
+    }
 });
