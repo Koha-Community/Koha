4180999aa58ab2530b377ba11567aff6832c766b
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2017-12-21 15:59:24 -0300

    Bug 18474: Restore multiple holds when patron is searched for
    We lost the ability to place multiple holds when we are searching for
    patrons. The multi_holds parameter is lost and not handled correctly in
    the template.
    
    Test plan:
    - Make sure you can place multiple holds for a patron you will search for
    - Same for simple hold
    
    TODO the multiple holds view should not be displayed if only 1 record
    has been selected from the search result.
    
    Signed-off-by: claude brayer <claude.brayer@cea.fr>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    (cherry picked from commit dc008b90cd50b80c78520de6379c61e534efce75)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- reserve/request.pl
+++ reserve/request.pl
@@ -61,8 +61,6 @@ my ( $template, $borrowernumber, $cookie, $flags ) = get_template_and_user(
     }
 );
 
-my $multihold = $input->param('multi_hold');
-$template->param(multi_hold => $multihold);
 my $showallitems = $input->param('showallitems');
 
 my $itemtypes = { map { $_->{itemtype} => $_ } @{ Koha::ItemTypes->search_with_localization->unblessed } };
@@ -122,13 +120,16 @@ if ($findborrower) {
 }
 
 my @biblionumbers = ();
+my $biblionumber = $input->param('biblionumber');
 my $biblionumbers = $input->param('biblionumbers');
-if ($multihold) {
+if ( $biblionumbers ) {
     @biblionumbers = split '/', $biblionumbers;
 } else {
     push @biblionumbers, $input->multi_param('biblionumber');
 }
 
+# FIXME multi_hold should not be a variable but depends on the number of elements in @biblionumbers
+$template->param(multi_hold => scalar $input->param('multi_hold'));
 
 # If we have the borrowernumber because we've performed an action, then we
 # don't want to try to place another reserve.
@@ -631,10 +632,6 @@ $template->param( biblioloop => \@biblioloop );
 $template->param( biblionumbers => $biblionumbers );
 $template->param( exceeded_maxreserves => $exceeded_maxreserves );
 $template->param( exceeded_holds_per_record => $exceeded_holds_per_record );
-
-if ($multihold) {
-    $template->param( multi_hold => 1 );
-}
 
 if ( C4::Context->preference( 'AllowHoldDateInFuture' ) ) {
     $template->param( reserve_in_future => 1 );
