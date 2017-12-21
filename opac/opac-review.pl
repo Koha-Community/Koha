Koha-Community/Koha

commit 092ae340cef375cc66c8511fd34c1a4eec9c7ba6

Bug 15839: Koha::Reviews - Remove savereview
Signed-off-by: Marc Veron <veron@veron.ch>
Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>

Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

opac/opac-review.pl
@@ -22,11 +22,11 @@ use CGI qw ( -utf8 );
 use C4::Auth;
 use C4::Koha;
 use C4::Output;
-use C4::Review;
 use C4::Biblio;
 use C4::Scrubber;
 use C4::Debug;
 use Koha::DateUtils;
+use Koha::Review;
 use Koha::Reviews;
 
 my $query        = new CGI;
@@ -69,9 +69,14 @@ if (defined $review) {
                         datereviewed  => dt_from_string
                     }
                 )->store;
-			} else {
-    			savereview($biblionumber, $borrowernumber, $clean);
-			}
+            } else {
+                Koha::Review->new(
+                    {   biblionumber   => $biblionumber,
+                        borrowernumber => $borrowernumber,
+                        review         => $clean,
+                    }
+                )->store;
+            }
 			unless (@errors){ $template->param(WINDOW_CLOSE=>1); }
 		}
 	}

C4/Review.pm
@@ -47,24 +47,6 @@ Review.pm provides many routines for manipulating reviews.
 
 =head1 FUNCTIONS
 
-=head2 savereview
-
-  savereview($biblionumber,$borrowernumber, $review);
-
-Save a review in the 'reviews' database
-
-=cut
-
-sub savereview {
-    my ( $biblionumber, $borrowernumber, $review ) = @_;
-    my $dbh   = C4::Context->dbh;
-    my $query = "INSERT INTO reviews (borrowernumber,biblionumber,
-  review,approved,datereviewed) VALUES
-  (?,?,?,0,now())";
-    my $sth = $dbh->prepare($query);
-    $sth->execute( $borrowernumber, $biblionumber, $review );
-}
-
 =head2 numberofreviewsbybiblionumber
 
   my $count=numberofreviewsbybiblionumber($biblionumber);

commit 11b562e0d1dbca1243e7ab9305741360b2114db6

Bug 19843: (bug 15839 follow-up) Set reviews.datereviewed when the review is made
Caused by
  commit 092ae340cef375cc66c8511fd34c1a4eec9c7ba6
    Bug 15839: Koha::Reviews - Remove savereview

the datereviewed=now() part of the query has not been translated.

Test plan:
Add a review from the OPAC
Confirm that the reviews.datereviewd has been correctly set. Without
this patch it is set to NULL.

I have no idea how we could update existing data :-/

Signed-off-by: Owen Leonard <oleonard@myacpl.org>

Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

opac/opac-review.pl
@@ -87,6 +87,7 @@ if( !@errors && defined $review ) {
                     {   biblionumber   => $biblionumber,
                         borrowernumber => $borrowernumber,
                         review         => $clean,
+                        datereviewed   => dt_from_string
                     }
                 )->store->reviewid;
             }