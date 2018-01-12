49d353d615cb86cd0d7a7b7478080619e162bb97
Johanna Raisa <johanna.raisa@gmail.com>
2018-01-12 13:05:04 +0200

    KD-1530 (squashable) fixes for new BTJ order

--- Koha/Procurement/EditX/LibraryShipNotice/ItemDetail/CopyDetail/Btj.pm
+++ Koha/Procurement/EditX/LibraryShipNotice/ItemDetail/CopyDetail/Btj.pm
@@ -6,40 +6,6 @@ use Moose;
 
 use Data::Dumper;
 
-extends "Koha::Procurement::EditX::LibraryShipNotice::ItemDetail::CopyDetail";
-
-
-sub getEditionStatement {
-    my $self = shift;
-    my $marcRecord = $self->getMarcData();
-    my $editionStatement;
-
-    if($marcRecord){
-        $editionStatement = $marcRecord->subfield('250','a');
-    }
-    if(!$editionStatement){
-       my $xmlData = $self->getItemDetail()->getXmlData();
-       my $result = $xmlData->find('ItemDescription/EditionStatement')->string_value;
-    }
-    if(!$editionStatement){
-       $editionStatement = '';
-    }
-    return $editionStatement;
-}
-
-# sub getDeliverToLocation {
-#     my $self = shift;
-#     return $self->getFundNumber();
-# }
-
-# sub getDestinationLocation {
-#     my $self = shift;
-#     return $self->getFundNumber();
-# }
-
-sub getFundMonetaryAmount {
-    my $self = shift;
-    return $self->getItemDetail()->getPriceSRPExcludingTax();
-}
+extends "Koha::Procurement::EditX::LibraryShipNotice::ItemDetail::CopyDetail::Booky";
 
 1;
