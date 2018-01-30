5741383cf3ad60efb7dd742334ef51966e2dbfce
Kyle M Hall <kyle@bywatetsolutions.com>
2018-01-30 11:10:07 -0500

    Bug 20101: Cronjob automatic_item_modification_by_age.pl does not log run in action logs
    C4::Log::cronlogaction() was never implemented in automatic_item_modification_by_age.pl.
    
    Test Plan:
    1) Apply this patch
    2) Enable CronjobLog
    3) Run automatic_item_modification_by_age.pl succesfully
    4) Note the run is noted in the action logs
    
    Signed-off-by: Mark Tompsett <mtompset@hotmail.com>
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    
    Signed-off-by: Joy Nelson <joy@bywatersolutions.com>
    (cherry picked from commit 8ed0d57c95f36cfb960a998dc5b2fda6cfa44ef4)
    
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

--- misc/cronjobs/automatic_item_modification_by_age.pl
+++ misc/cronjobs/automatic_item_modification_by_age.pl
@@ -9,6 +9,7 @@ use JSON;
 use Koha::Script -cron;
 use C4::Context;
 use C4::Items;
+use C4::Log;
 
 # Getting options
 my ( $verbose, $help, $confirm );
@@ -26,6 +27,8 @@ my $syspref_content = C4::Context->preference('automatic_item_modification_by_ag
 my $rules = eval { JSON::from_json( $syspref_content ) };
 pod2usage({ -message => "Unable to load the configuration : $@", -exitval => 1 })
     if $@;
+
+cronlogaction();
 
 my $report = C4::Items::ToggleNewStatus( { rules => $rules, report_only => not $confirm } );
 
