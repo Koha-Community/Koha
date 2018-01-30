a5685e8a0179a5dffa20079c91454d1a060af595
Johanna Raisa <johanna.raisa@gmail.com>
2018-01-30 10:26:56 +0200

    Bug 19208 - Pay select option doesn't pay the selected fine
    The "Pay selected" option on the Fines tab in the borrower account page doesn't work as intended.
    The fine on top of the list gets the amount deducted, even if another fine is choosen from the list.
    
    Test Plan:
    1) Create two or three fines, using the Create manual invoice function.
    2) Choose one of the fines (not the one on the top) and click Pay selected
    3) Pay a partial amount
    4) Go back to the Pay fines tab an notice that the fine you selected has not changed. Instead, either the top fine or the total (see attachment) has ben affected.
    5) Apply this patch
    6) Repeat steps 1-3
    7) Note the correct fine is paid
    
    Signed-off-by: Andreas Hedström Mace <andreas.hedstrom.mace@sub.su.se>
    Signed-off-by: Julian Maurice <julian.maurice@biblibre.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- members/paycollect.pl
+++ members/paycollect.pl
@@ -64,7 +64,6 @@ my $writeoff     = $input->param('writeoff_individual');
 my $select_lines = $input->param('selected');
 my $select       = $input->param('selected_accts');
 my $payment_note = uri_unescape $input->param('payment_note');
-my $accountno;
 my $accountlines_id;
 
 my $pos;
@@ -83,7 +82,6 @@ if ( $individual || $writeoff ) {
     $accountlines_id       = $input->param('accountlines_id');
     my $amount            = $input->param('amount');
     my $amountoutstanding = $input->param('amountoutstanding');
-    $accountno = $input->param('accountno');
     my $itemnumber  = $input->param('itemnumber');
     my $description  = $input->param('description');
     my $title        = $input->param('title');
@@ -93,7 +91,6 @@ if ( $individual || $writeoff ) {
     $template->param(
         accounttype       => $accounttype,
         accountlines_id    => $accountlines_id,
-        accountno         => $accountno,
         amount            => $amount,
         amountoutstanding => $amountoutstanding,
         title             => $title,
@@ -178,7 +175,7 @@ if ( $total_paid and $total_paid ne '0.00' ) {
                     {
                         borrowernumber    => $borrowernumber,
                         amountoutstanding => { '<>' => 0 },
-                        accountno         => { 'IN' => \@acc },
+                        accountlines_id   => { 'IN' => \@acc },
                     },
                     { order_by => 'date' }
                 );
