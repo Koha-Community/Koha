f9efe84a2d382df3091f5b6eea76c2fdab143fbc
Fridolin Somers <fridolin.somers@biblibre.com>
2018-01-30 09:26:39 +0100

    Bug 18915: DBRev 17.05.08.001
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- installer/data/mysql/atomicupdate/bug_18915_-_modifying_patron_note_notice.sql
+++
@@ -1,4 +0,0 @@
--- For installations having the note already
-UPDATE letter SET code = 'CHECKOUT_NOTE', name = 'Checkout note on item set by patron', title = 'Checkout note', content = REPLACE(content, "<<biblio.item>>", "<<biblio.title>>") WHERE code = 'PATRON_NOTE';
--- For installations coming from 17.11
-INSERT IGNORE INTO `letter` (`module`, `code`, `branchcode`, `name`, `is_html`, `title`, `content`, `message_transport_type`) VALUES ('circulation', 'CHECKOUT_NOTE', '', 'Checkout note on item set by patron', '0', 'Checkout note', '<<borrowers.firstname>> <<borrowers.surname>> has added a note to the item <<biblio.title>> - <<biblio.author>> (<<biblio.biblionumber>>).','email');
