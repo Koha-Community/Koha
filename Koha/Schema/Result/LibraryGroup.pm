1240cd140890f9698202ac7682fae6864296e105
Kyle M Hall <kyle@bywatersolutions.com>
2018-02-07 19:48:56 +0000

    Bug 20157: DBIC Schema changes
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- Koha/Schema/Result/LibraryGroup.pm
+++ Koha/Schema/Result/LibraryGroup.pm
@@ -59,6 +59,18 @@ __PACKAGE__->table("library_groups");
   default_value: 0
   is_nullable: 0
 
+=head2 ft_search_groups_opac
+
+  data_type: 'tinyint'
+  default_value: 0
+  is_nullable: 0
+
+=head2 ft_search_groups_staff
+
+  data_type: 'tinyint'
+  default_value: 0
+  is_nullable: 0
+
 =head2 created_on
 
   data_type: 'timestamp'
@@ -86,6 +98,10 @@ __PACKAGE__->add_columns(
   "description",
   { data_type => "text", is_nullable => 1 },
   "ft_hide_patron_info",
+  { data_type => "tinyint", default_value => 0, is_nullable => 0 },
+  "ft_search_groups_opac",
+  { data_type => "tinyint", default_value => 0, is_nullable => 0 },
+  "ft_search_groups_staff",
   { data_type => "tinyint", default_value => 0, is_nullable => 0 },
   "created_on",
   {
@@ -186,8 +202,8 @@ __PACKAGE__->belongs_to(
 );
 
 
-# Created by DBIx::Class::Schema::Loader v0.07042 @ 2018-02-09 16:32:32
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:4nPUJUWLIqttCGWm7N0txg
+# Created by DBIx::Class::Schema::Loader v0.07042 @ 2018-02-09 16:35:09
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:Hy7zXPiHGs832yvEBIO6Uw
 
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
