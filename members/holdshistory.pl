0b2c4ff551bb29db15813d91cfb5e74de1061683
Josef Moravec <josef.moravec@gmail.com>
2018-02-16 10:58:42 +0000

    Bug 18789: (QA follow-up) Update holdshistory.pl, fix is_child, is_adult, patron->image, clear template params
    Signed-off-by: Josef Moravec <josef.moravec@gmail.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- members/holdshistory.pl
+++ members/holdshistory.pl
@@ -74,21 +74,14 @@ if ( $borrowernumber eq C4::Context->preference('AnonymousPatron') ){
     }
 }
 
-if ( $patron->category->category_type eq 'C') {
+if ( $patron->is_child) {
     my $patron_categories = Koha::Patron::Categories->search_limited({ category_type => 'A' }, {order_by => ['categorycode']});
     $template->param( 'CATCODE_MULTI' => 1) if $patron_categories->count > 1;
     $template->param( 'catcode' => $patron_categories->next->categorycode )  if $patron_categories->count == 1;
 }
 
-$template->param( adultborrower => 1 ) if ( $patron->category->category_type eq 'A' || $patron->category->category_type eq 'I' );
-
-$template->param( picture => 1 ) if $patron->image;
-
-$template->param(%{ $patron->unblessed });
-
 $template->param(
     holdshistoryview => 1,
-    borrowernumber   => $borrowernumber,
     patron           => $patron,
     holds            => \@all_holds,
 );
