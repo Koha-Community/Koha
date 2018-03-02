6db09ad3b070845539994ff4f1f8b9f7d6d2dfc8
Tomas Cohen Arazi <tomascohen@gmail.com>
2018-03-02 11:51:20 -0300

    Bug 17717: Make cronjobs using koha-foreach use --chdir
    In order to patch production sites we need to adjust the shipped
    cronjobs so they are called with the --chdir option switch.
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>

--- debian/koha-common.cron.d
+++ debian/koha-common.cron.d
@@ -9,4 +9,4 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 # Uncomment the following line if you do not want to use the koha-index-daemon integration
 # */5 * * * * root test -x /usr/sbin/koha-rebuild-zebra && koha-rebuild-zebra -q $(koha-list --enabled)
 
-*/15 * * * * root koha-foreach --enabled --email /usr/share/koha/bin/cronjobs/process_message_queue.pl
+*/15 * * * * root koha-foreach --chdir --enabled --email /usr/share/koha/bin/cronjobs/process_message_queue.pl
