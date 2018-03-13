5b35c97a841bd2a8a199dc81b70c12d1c1beb0a4
Owen Leonard <oleonard@myacpl.org>
2018-03-13 13:57:36 +0000

    Bug 19474: Convert staff client CSS to SCSS
    This patch converts staff-global.css to Sass, using SCSS syntax. This
    changes the build process for Koha to include installation and execution
    of yarn to install npm dependencies and run SCSS -> CSS conversion.
    
    To test, apply the patch and run the following:
    
    $ sudo apt-get install nodejs npm [not necessary in kohadevbox]
    $ sudo npm install -g yarn
    $ yarn install
    $ yarn build
    
    Clear your browser cache if necessary and confirm that CSS styling
    throughout the staff client looks correct.
    
    The "yarn build" command triggers a gulp process which compiles SCSS to
    CSS, adds automatic vendor-prefixing, and minifies the resulting CSS
    file.
    
    There is also a "yarn css" command available which might be used by
    developers who are making changes to SCSS. This command does two things
    differently:
    
    1. Adds .css.map files which aid CSS debugging using in-browser
       inspector tools.
    2. Compiles staff-global.css without minification. It can be useful to
       see unminified CSS during development, especially to see how SCSS
       compiles.
    
    This patch adds a configuration file for sass-lint, .sass-lint.yml.
    Currently this configuration is not used during the build process but
    can be used in a code editor which supports linting.
    
    Signed-off-by: Claire Gravely <claire.gravely@bsz-bw.de>
    
    Signed-off-by: Josef Moravec <josef.moravec@gmail.com>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+/* eslint-env node */
+/* eslint no-console:"off" */
+
+let gulp;
+
+try {
+    gulp = require( "gulp" );
+} catch(e) {
+    console.error("You are missing required Node modules; run `npm install`.");
+    process.exit(1);
+}
+
+const gutil = require( "gulp-util" );
+const sass = require("gulp-sass");
+const cssnano = require("gulp-cssnano");
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+
+const STAFF_JS_BASE = "koha-tmpl/intranet-tmpl/prog/js";
+const STAFF_CSS_BASE = "koha-tmpl/intranet-tmpl/prog/css";
+const OPAC_JS_BASE = "koha-tmpl/opac-tmpl/bootstrap/js";
+const OPAC_CSS_BASE = "koha-tmpl/opac-tmpl/bootstrap/css";
+
+var sassOptions = {
+    errLogToConsole: true,
+    precision: 3
+}
+
+gulp.task( "default", ['watch'] );
+
+// CSS processing for development
+gulp.task('css', function() {
+    return gulp.src( STAFF_CSS_BASE + "/src/**/*.scss" )
+      .pipe(sourcemaps.init())
+      .pipe(sass( sassOptions ).on('error', sass.logError))
+      .pipe(autoprefixer())
+      .pipe(sourcemaps.write('./maps'))
+      .pipe(gulp.dest( STAFF_CSS_BASE ));
+});
+
+// CSS processing for production
+
+gulp.task('build', function() {
+    return gulp.src( STAFF_CSS_BASE + "/src/**/*.scss" )
+      .pipe(sass( sassOptions ).on('error', sass.logError))
+      .pipe(autoprefixer())
+      .pipe(cssnano())
+      .pipe(gulp.dest( STAFF_CSS_BASE ));
+});
+
+gulp.task('watch', function(){
+    gulp.watch( STAFF_CSS_BASE + "/src/**/*.scss", ['css'] );
+});
