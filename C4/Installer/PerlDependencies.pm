aaea46594215c88b4459cd882f1d869faeb2e2d8
Tomas Cohen Arazi <tomascohen@theke.io>
2018-04-05 10:06:37 -0300

    Bug 20191: (QA follow-up) Require Jessie's Test::MockTime
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- C4/Installer/PerlDependencies.pm
+++ C4/Installer/PerlDependencies.pm
@@ -592,6 +592,11 @@ our $PERL_DEPS = {
         'required' => '0',
         'min_ver'  => '1.09',
     },
+    'Test::MockTime' => {
+        usage      => 'Testing suite',
+        required   => 0,
+        min_ver    => '0.13',
+    },
     'Test::More' => {
         'usage'    => 'Core',
         'required' => '1',
@@ -882,11 +887,6 @@ our $PERL_DEPS = {
         usage      => 'Testing suite',
         required   => 0,
         min_ver    => '0.52',
-    },
-    'Test::MockTime' => {
-        usage      => 'Testing suite',
-        required   => 0,
-        min_ver    => '0.17',
     },
 };
 
