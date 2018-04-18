50c68497a26f50a66c59b182cae50bade708faf4
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2018-04-18 14:01:18 -0300

    Bug 7910: Add the ability to renew several subscriptions at once
    This patch adds a "Renew all selected subscriptions" action link on top
    of the table of the "Check expiration" page.
    
    It will allow to auto-renew several subscriptions.
    
    Test plan:
    Make sure this new link renew the selected subscriptions as expected.
    
    Sponsored-by: BULAC - http://www.bulac.fr/
    
    Signed-off-by: Séverine QUEUNE <severine.queune@bulac.fr>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- serials/subscription-renew.pl
+++ serials/subscription-renew.pl
@@ -60,7 +60,7 @@ my $dbh   = C4::Context->dbh;
 
 my $mode           = $query->param('mode') || q{};
 my $op             = $query->param('op') || 'display';
-my $subscriptionid = $query->param('subscriptionid');
+my @subscriptionids = $query->multi_param('subscriptionid');
 my $done = 0;    # for after form has been submitted
 my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
     {
@@ -73,34 +73,44 @@ my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
     }
 );
 if ( $op eq "renew" ) {
+    # Do not use this script with op=renew and @subscriptionids > 1!
+    my $subscriptionid = $subscriptionids[0];
     my $startdate = output_pref( { str => scalar $query->param('startdate'), dateonly => 1, dateformat => 'iso' } );
     ReNewSubscription(
         $subscriptionid, $loggedinuser,
-        $startdate, $query->param('numberlength'),
-        $query->param('weeklength'), $query->param('monthlength'),
-        $query->param('note')
+        $startdate, scalar $query->param('numberlength'),
+        scalar $query->param('weeklength'), scalar $query->param('monthlength'),
+        scalar $query->param('note')
+    );
+} elsif ( $op eq 'multi_renew' ) {
+    for my $subscriptionid ( @subscriptionids ) {
+        my $subscription = GetSubscription( $subscriptionid );
+        next unless $subscription;
+        ReNewSubscription(
+            $subscriptionid, $loggedinuser,
+            $subscription->{enddate}, $subscription->{numberlength},
+            $subscription->{weeklength}, $subscription->{monthlength},
+        );
+    }
+} else {
+    my $subscriptionid = $subscriptionids[0];
+    my $subscription = GetSubscription($subscriptionid);
+    if ($subscription->{'cannotedit'}){
+      carp "Attempt to renew subscription $subscriptionid by ".C4::Context->userenv->{'id'}." not allowed";
+      print $query->redirect("/cgi-bin/koha/serials/subscription-detail.pl?subscriptionid=$subscriptionid");
+    }
+
+    my $newstartdate = output_pref( { str => $subscription->{enddate}, dateonly => 1 } )
+        or output_pref( { dt => dt_from_string, dateonly => 1 } );
+
+    $template->param(
+        startdate      => $newstartdate,
+        subscription   => $subscription,
     );
 }
 
-my $subscription = GetSubscription($subscriptionid);
-if ($subscription->{'cannotedit'}){
-  carp "Attempt to renew subscription $subscriptionid by ".C4::Context->userenv->{'id'}." not allowed";
-  print $query->redirect("/cgi-bin/koha/serials/subscription-detail.pl?subscriptionid=$subscriptionid");
-}
-
-my $newstartdate = output_pref( { str => $subscription->{enddate}, dateonly => 1 } )
-    or output_pref( { dt => dt_from_string, dateonly => 1 } );
-
 $template->param(
-    startdate      => $newstartdate,
-    numberlength   => $subscription->{numberlength},
-    weeklength     => $subscription->{weeklength},
-    monthlength    => $subscription->{monthlength},
-    subscriptionid => $subscriptionid,
-    bibliotitle    => $subscription->{bibliotitle},
-    $op            => 1,
-    popup          => ($mode eq 'popup'),
+    op => $op,
 );
 
-# Print the page
 output_html_with_http_headers $query, $cookie, $template->output;
