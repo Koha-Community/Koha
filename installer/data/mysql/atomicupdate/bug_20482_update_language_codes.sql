3d5271d315d401a2d1269cf8e815413de7452e99
Nick Clemens <nick@bywatersolutions.com>
2018-04-20 17:36:15 +0000

    Bug 20482: (QA follow-up) Correct code in atomic update
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- installer/data/mysql/atomicupdate/bug_20482_update_language_codes.sql
+++ installer/data/mysql/atomicupdate/bug_20482_update_language_codes.sql
@@ -1,3 +1,3 @@
 UPDATE language_rfc4646_to_iso639 SET iso639_2_code = 'baq' WHERE iso639_2_code = 'eus' AND rfc4646_subtag = 'eu';
-UPDATE language_rfc4646_to_iso639 SET iso639_2_code = 'mri' WHERE iso639_2_code = 'mri' AND rfc4646_subtag = 'mi';
+UPDATE language_rfc4646_to_iso639 SET iso639_2_code = 'mao' WHERE iso639_2_code = 'mri' AND rfc4646_subtag = 'mi';
 UPDATE language_rfc4646_to_iso639 SET iso639_2_code = 'alb' WHERE iso639_2_code = 'sqi' AND rfc4646_subtag = 'sq';
