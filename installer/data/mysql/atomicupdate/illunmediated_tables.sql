b1e076fab0da49d7480d22b8c440342af0bbc652
Andrew Isherwood <andrew.isherwood@ptfs-europe.com>
2018-04-26 11:56:19 +0100

    Bug 18837: Add ILL Unmediated syspref stuff
    This patch adds the syspref requirements for unmediated ILL requests. It
    is a recreation of
    https://bugs.koha-community.org/bugzilla3/attachment.cgi?id=66628&action=diff
    which had diverged sufficiently as to make it impossible to apply.
    
    * installer/data/mysql/atomicupdate/illunmediated_tables.sql: New
      file.
    * installer/data/mysql/sysprefs.sql: Add `ILLModuleUnmediated`.
    * koha-tmpl/intranet-tmpl/prog/en/modules/admin/preferences/admin.pref:
        Add `ILLModuleUnmediated` handling.
    
    Signed-off-by: Stephen Graham <s.graham4@herts.ac.uk>
    Signed-off-by: David Peacock <d.m.peacock@herts.ac.uk>
    Signed-off-by: Jayne Maisey <j.maisey@herts.ac.uk>
    
    Signed-off-by: Josef Moravec <josef.moravec@gmail.com>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>

---
+++ installer/data/mysql/atomicupdate/illunmediated_tables.sql
@@ -0,0 +1,4 @@
+-- System preferences
+
+INSERT IGNORE INTO systempreferences (variable,value,explanation,options,type) VALUES
+       ('ILLModuleUnmediated','0','','If enabled, try to immediately progress newly placed ILL requests.','YesNo');
