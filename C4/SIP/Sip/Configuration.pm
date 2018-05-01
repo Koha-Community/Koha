315f544cdc4b17912a12e41b90615d54babba705
Kyle M Hall <kyle@bywatersolutions.com>
2018-05-01 11:47:12 +0000

    Bug 15253: Add Koha::Logger based logging for SIP2
    Now that we have Koha::Logger, we should use it in our SIP server. This
    has the potential to make debugging SIP issue much easier. We should add
    the userid for the sipuser to the namespace so we can allow for separate
    files per sip user if wanted.
    
    Test Plan:
    1) Apply this patch set
    2) Update the modififed log4perl.conf to your system
    3) Restart your sip server
    4) Tail your sip2.log, run some queries
    5) Note you still get the same output messages as before, with the
       addition of the ip address and username ( if available )
       prefixing the message.
    
    Signed-off-by: Liz Rea <wizzyrea@gmail.com>
    Signed-off-by: Chris Cormack <chris@bigballofwax.co.nz>
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- C4/SIP/Sip/Configuration.pm
+++ C4/SIP/Sip/Configuration.pm
@@ -10,6 +10,8 @@ use strict;
 use warnings;
 use XML::Simple qw(:strict);
 
+use C4::SIP::Sip qw(syslog);
+
 my $parser = new XML::Simple(
     KeyAttr => {
         login       => '+id',
@@ -63,7 +65,7 @@ sub find_service {
     my $portstr;
     foreach my $addr ( '', '*:', "$sockaddr:", "[$sockaddr]:" ) {
         $portstr = sprintf( "%s%s/%s", $addr, $port, lc $proto );
-        Sys::Syslog::syslog( "LOG_DEBUG",
+        syslog( "LOG_DEBUG",
             "Configuration::find_service: Trying $portstr" );
         last if ( exists( ( $self->{listeners} )->{$portstr} ) );
     }
