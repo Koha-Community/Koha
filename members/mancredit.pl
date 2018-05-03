fb083c95a2582904d633b84e4eddabce08baf99c
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2018-05-03 09:09:19 -0300

    Bug 20701: Add csrf protection to mancredit.pl
    Works OK.
    
    Signed-off-by: Amit Gupta <amit.gupta@informaticsglobal.com>
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>

--- members/mancredit.pl
+++ members/mancredit.pl
@@ -36,6 +36,7 @@ use C4::Members::Attributes qw(GetBorrowerAttributes);
 use Koha::Patrons;
 
 use Koha::Patron::Categories;
+use Koha::Token;
 
 my $input=new CGI;
 my $flagsrequired = { borrowers => 1, updatecharges => 1 };
@@ -51,6 +52,15 @@ my $add=$input->param('add');
 
 if ($add){
     if ( checkauth( $input, 0, $flagsrequired, 'intranet' ) ) {
+
+        die "Wrong CSRF token"
+            unless Koha::Token->new->check_csrf( {
+                session_id => $input->cookie('CGISESSID'),
+                token  => scalar $input->param('csrf_token'),
+            });
+
+        # Note: If the logged in user is not allowed to see this patron an invoice can be forced
+        # Here we are trusting librarians not to hack the system
         my $barcode = $input->param('barcode');
         my $itemnum;
         if ($barcode) {
@@ -101,6 +111,9 @@ if ($add){
         borrowernumber => $borrowernumber,
         categoryname   => $patron->category->description,
         is_child       => ($patron->category->category_type eq 'C'), # FIXME is_child should be a Koha::Patron method
+        csrf_token => Koha::Token->new->generate_csrf(
+            { session_id => scalar $input->cookie('CGISESSID') }
+        ),
         );
     output_html_with_http_headers $input, $cookie, $template->output;
 }
