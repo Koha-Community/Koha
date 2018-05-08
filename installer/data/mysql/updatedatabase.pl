d012552ab636b23227990e3992e9dfe34733fcab
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2018-05-08 16:05:04 -0300

    Bug 20402: DBRev 17.12.00.042
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -15992,6 +15992,24 @@ if( CheckVersion( $DBversion ) ) {
     print "Upgrade to $DBversion done (Bug 11317 - Add a new permission to access files stored on the server)\n";
 }
 
+$DBversion = '17.12.00.042';
+if( CheckVersion( $DBversion ) ) {
+
+    if (!TableExists('oauth_access_tokens')) {
+        $dbh->do(q{
+            CREATE TABLE oauth_access_tokens (
+                `access_token` VARCHAR(191) NOT NULL,
+                `client_id`    VARCHAR(191) NOT NULL,
+                `expires`      INT NOT NULL,
+                PRIMARY KEY (`access_token`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+        });
+    }
+
+    SetVersion( $DBversion );
+    print "Upgrade to $DBversion done (Bug 20402 - Implement OAuth2 authentication for REST API)\n";
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 
