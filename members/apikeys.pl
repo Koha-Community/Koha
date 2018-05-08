d2454d6868c357ee65dd683d80ab16f2cc2366fc
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2018-05-08 16:15:54 -0300

    Bug 20568: Fix bad resolution conflict with bug 18403
    borrowers module permission has now several subpermissions
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- members/apikeys.pl
+++ members/apikeys.pl
@@ -35,7 +35,7 @@ my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
         query           => $cgi,
         type            => 'intranet',
         authnotrequired => 0,
-        flagsrequired   => { borrowers => 1 },
+        flagsrequired   => { borrowers => 'edit_borrowers' },
     }
 );
 
