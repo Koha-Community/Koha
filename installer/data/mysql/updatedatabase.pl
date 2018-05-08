ef7a900742cf8c7bc063cc6df74b922db235b8b6
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2018-05-08 16:06:22 -0300

    Bug 20568: DBRev 17.12.00.043
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -16010,6 +16010,32 @@ if( CheckVersion( $DBversion ) ) {
     print "Upgrade to $DBversion done (Bug 20402 - Implement OAuth2 authentication for REST API)\n";
 }
 
+$DBversion = '17.12.00.043';
+if(CheckVersion($DBversion)) {
+
+    if (!TableExists('api_keys')) {
+        $dbh->do(q{
+            CREATE TABLE `api_keys` (
+                `client_id`   VARCHAR(191) NOT NULL,
+                `secret`      VARCHAR(191) NOT NULL,
+                `description` VARCHAR(255) NOT NULL,
+                `patron_id`   INT(11) NOT NULL,
+                `active`      TINYINT(1) DEFAULT 1 NOT NULL,
+                PRIMARY KEY `client_id` (`client_id`),
+                UNIQUE KEY `secret` (`secret`),
+                KEY `patron_id` (`patron_id`),
+                CONSTRAINT `api_keys_fk_patron_id`
+                  FOREIGN KEY (`patron_id`)
+                  REFERENCES `borrowers` (`borrowernumber`)
+                  ON DELETE CASCADE ON UPDATE CASCADE
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+        });
+    }
+
+    print "Upgrade to $DBversion done (Bug 20568 - Add API key management interface for patrons)\n";
+    SetVersion($DBversion);
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 
