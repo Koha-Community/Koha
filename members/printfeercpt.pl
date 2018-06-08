a09643ca23348009e409ae42057792224e7cbd0c
Josef Moravec <josef.moravec@gmail.com>
2018-06-08 09:43:13 +0000

    Bug 20903: Remove unnecessary category code parameters
    Test plan:
    0) Do no apply the patch
    1) Have only one adults patrons category defined
    2) Add a fine to child patron and pay it
    3) Try to print the payment receipt (file printfeercpt.pl)
    4) You see only ISE
    5) Apply the patch
    6) Try the printing again
    7) Now you should see right receipt
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>

--- members/printfeercpt.pl
+++ members/printfeercpt.pl
@@ -58,12 +58,6 @@ if ( $action eq 'print' ) {
 #  ReversePayment( $borrowernumber, $input->param('accountno') );
 }
 
-if ( $patron->is_child ) {
-    my $patron_categories = Koha::Patron::Categories->search_limited({ category_type => 'A' }, {order_by => ['categorycode']});
-    $template->param( 'CATCODE_MULTI' => 1) if $patron_categories->count > 1;
-    $template->param( 'catcode' => $patron_categories->next->categorycde )  if $patron_categories->count == 1;
-}
-
 #get account details
 my $total = $patron->account->balance;
 
