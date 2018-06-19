18e122a78cb7f0ac84161cd908b4e03ec6401c48
Katrin Fischer <katrin.fischer.83@web.de>
2018-06-20 00:16:38 +0200

    Bug 19719: (follow-up) Add database update to keep collection column hidden
    For existing installations we want to keep the current behaviour.
    This patch adds the column configuration to hide the collection
    column from the details and checkouts tabs until toggled.
    
    Signed-off-by: Julian Maurice <julian.maurice@biblibre.com>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>

---
+++ installer/data/mysql/atomicupdate/bug_19719_add_collection_column.sql
@@ -0,0 +1,5 @@
+INSERT INTO columns_settings (module, page, tablename, columnname, cannot_be_toggled, is_hidden) VALUES
+("circ", "circulation", "issues-table", "collection", 0, 1),
+("members", "moremember", "issues-table", "collection", 0, 1);
+
+-- Bug 19719: Add collection column to issues table
