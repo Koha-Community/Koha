549148d74b6368697e216a31c39296016668a8d1
xljoha <johan.larsson@ub.gu.se>
2018-06-21 09:58:55 +0200

    Bug 20972: If ISBN has 10 numbers only the first 9 numbers are used
    If ISBN has 10 numbers only the first 9 numbers are being added to the Edifact-message (PIA+5+3540556753:IB')
    
    This is caused by wrong positioning of capturing parentheses in a regular expression.
    
    Sponsored-by: Gothenburg University Library
    Signed-off-by: Colin Campbell <colin.campbell@ptfs-europe.com>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    (cherry picked from commit b9bd93e9205d4b8344a77214fefb8efe2c84792e)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>
    (cherry picked from commit 7a14e9d6e477c0c3904df099e11afbf15fdee701)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- Koha/Edifact/Order.pm
+++ Koha/Edifact/Order.pm
@@ -580,7 +580,7 @@ sub additional_product_id {
         $product_id   = $1;
         $product_code = 'EN';
     }
-    elsif ( $isbn_field =~ m/(\d{9})[Xx\d]/ ) {
+    elsif ( $isbn_field =~ m/(\d{9}[Xx\d])/ ) {
         $product_id   = $1;
         $product_code = 'IB';
     }
