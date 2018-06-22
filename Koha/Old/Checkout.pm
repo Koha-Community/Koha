Koha-Community/Koha

commit fa54100dffe092e606f79b15692eedaf78f42e45

Bug 18403: Use patron-title.inc when hidepatronname is used [SPECIFIC for issuehistory]
On this page we do not have the patron object sent to the template,
let's pass it!

Test plan:
Go on the checkout history of a bibliographic record
(catalogue/issuehistory.pl)
You should not see patron's information that are not part of your group
if you
are not allowed to see them.

Signed-off-by: Signed-off-by: Jon McGowan <jon.mcgowan@ptfs-europe.com>

Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

catalogue/issuehistory.pl
@@ -22,9 +22,9 @@ use CGI qw ( -utf8 );
 use C4::Auth;
 use C4::Output;
 
-use C4::Circulation;    # GetBiblioIssues
 use C4::Biblio;    # GetBiblio
 use C4::Search;		# enabled_staff_search_views
+use Koha::Checkouts;
 
 use Koha::Biblios;
 
@@ -41,18 +41,18 @@ my ( $template, $borrowernumber, $cookie ) = get_template_and_user(
 
 my $biblionumber = $query->param('biblionumber');
 
-if (C4::Context->preference("HidePatronName")) {
-   $template->param(HidePatronName => 1);
-}
-
-my $issues = GetBiblioIssues($biblionumber);
+my $checkouts = Koha::Checkouts->search(
+    { biblionumber => $biblionumber },
+    {
+        join       => 'item',
+        order_by   => 'timestamp',
+    }
+);
 my $biblio = Koha::Biblios->find( $biblionumber );
 
 $template->param(
-    biblionumber => $biblionumber, # required for left-side navigation
-    biblio       => $biblio,
-    total        => scalar @$issues,
-    issues       => $issues,
+    checkouts => $checkouts,
+    biblio    => $biblio,
 	issuehistoryview => 1,
 	C4::Search::enabled_staff_search_views,
 );

koha-tmpl/intranet-tmpl/prog/en/modules/catalogue/issuehistory.tt
@@ -25,8 +25,8 @@
 [% IF biblio.author %]<h3>by [% biblio.author %]</h3>[% END %]
 
 <div class="searchresults">
-    [% IF ( issues ) %]
-        <h4>Checked out [% total %] times</h4>
+    [% IF checkouts.count %]
+        <h4>Checked out [% checkouts.count %] times</h4>
         <table id="table_issues">
             <thead><tr>
             [% IF Koha.Preference('intranetreadinghistory') AND CAN_user_circulate_circulate_remaining_permissions %]
@@ -40,39 +40,40 @@
             <th class='title-string'>Checkin on</th>
             </tr></thead>
             <tbody>
-        [% FOREACH issue IN issues %]
+        [% FOREACH checkout IN checkouts %]
             <tr>
                 [% IF Koha.Preference('intranetreadinghistory') AND CAN_user_circulate_circulate_remaining_permissions %]
                 <td><a href="/cgi-bin/koha/members/moremember.pl?borrowernumber=[% issue.borrowernumber %]">[% IF HidePatronName %][% issue.cardnumber %][% ELSE %][% issue.surname %][% IF ( issue.firstname ) %], [% issue.firstname %][% END %][% END %]</a></td>
                 [% END %]
-                <td>[% IF ( issue.barcode ) %]
-                        <a href="/cgi-bin/koha/catalogue/moredetail.pl?biblionumber=[% issue.biblionumber %]&amp;itemnumber=[% issue.itemnumber %]">[% issue.barcode %]</a>
+                <td>
+                    [% IF checkout.item.barcode %] [%# FIXME This test is not mandatory I think %]
+                        <a href="/cgi-bin/koha/catalogue/moredetail.pl?biblionumber=[% checkout.item.biblionumber %]&amp;itemnumber=[% checkout.item.itemnumber %]">[% checkout.item.barcode %]</a>
                     [% ELSE %]
                         &nbsp;
                     [% END %]</td>
-                <td>[% IF ( issue.branchcode ) %]
-                        [% Branches.GetName( issue.branchcode ) %]
+                <td>[% IF checkout.branchcode %]
+                        [% Branches.GetName( checkout.branchcode ) %]
                     [% ELSE %]
                         &nbsp;
                     [% END %]</td>
-                <td>[% IF ( issue.renewals ) %]
-                        Yes[% IF ( issue.lastreneweddate ) %], <small>last on: [% issue.lastreneweddate |$KohaDates with_hours => 1  %]</small>
+                <td>[% IF checkout.renewals %]
+                        Yes[% IF checkout.lastreneweddate %], <small>last on: [% checkout.lastreneweddate |$KohaDates with_hours => 1  %]</small>
                             [% END %]
                     [% ELSE %]
                         No
                     [% END %]</td>
-                <td>[% IF ( issue.issuedate ) %]
-                        <span title="[% issue.issuedate %]">[% issue.issuedate |$KohaDates with_hours => 1  %]</span>
+                <td>[% IF checkout.issuedate %]
+                        <span title="[% checkout.issuedate %]">[% checkout.issuedate |$KohaDates with_hours => 1  %]</span>
                     [% ELSE %]
                         <span title="0000-00-00"></span>
                     [% END %]</td>
-                <td>[% IF ( issue.date_due ) %]
-                        <span title="[% issue.date_due %]">[% issue.date_due |$KohaDates with_hours => 1  %]</span>
+                <td>[% IF checkout.date_due %]
+                        <span title="[% checkout.date_due %]">[% checkout.date_due |$KohaDates with_hours => 1  %]</span>
                     [% ELSE %]
                         <span title="0000-00-00"></span>
                     [% END %]</td>
-                <td>[% IF ( issue.returndate ) %]
-                        <span title="[% issue.returndate %]">[% issue.returndate |$KohaDates with_hours => 1  %]</span>
+                <td>[% IF checkout.returndate %]
+                        <span title="[% checkout.returndate %]">[% checkout.returndate |$KohaDates with_hours => 1  %]</span>
                     [% ELSE %]
                         <span title="Checked out"><small>Checked out</small></span>
                     [% END %]</td>

commit 117e3a18f5b7c24e6c9fb7eabb78ab1b5320cd14

Bug 20934: Fix display of old checkouts in the checkout history page
Again a regression caused by
  commit fa54100dffe092e606f79b15692eedaf78f42e45
  Bug 18403: Use patron-title.inc when hidepatronname is used [SPECIFIC for issuehistory]

GetBiblioIssues does a union all with issues and old_issues, so we
should old_issues as well.
To make the join on the items table we need to define the item and
patron methods. For consistency the relationships have been redefined
(item instead of itemnumber, borrower instead of borrowernumber) in the
DBIx::Class definition.

This is not perfect but I think the best way to provide an easy to
backport patch.

It highlights that we need improvements in this area.

Signed-off-by: Maryse Simard <maryse.simard@inlibro.com>
The patch works as expected.

Signed-off-by: Josef Moravec <josef.moravec@gmail.com>

Signed-off-by: Nick Clemens <nick@bywatersolutions.com>

Koha/Old/Checkout.pm
@@ -21,6 +21,38 @@ use Koha::Database;
 
 use base qw(Koha::Object);
 
+=head3 item
+
+my $item = $checkout->item;
+
+Return the checked out item
+
+=cut
+
+sub item {
+    my ( $self ) = @_;
+    my $item_rs = $self->_result->item;
+    return Koha::Item->_new_from_dbic( $item_rs );
+}
+
+=head3 patron
+
+my $patron = $checkout->patron
+
+Return the patron for who the checkout has been done
+
+=cut
+
+sub patron {
+    my ( $self ) = @_;
+    my $patron_rs = $self->_result->borrower;
+    return Koha::Patron->_new_from_dbic( $patron_rs );
+}
+
+
+
+
+
 sub _type {
     return 'OldIssue';
 }

Koha/Schema/Result/OldIssue.pm
@@ -229,6 +229,25 @@ __PACKAGE__->belongs_to(
 # Created by DBIx::Class::Schema::Loader v0.07042 @ 2018-02-16 17:54:54
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:RKLeDDEz22G5BU/ZAl7QLA
 
+__PACKAGE__->belongs_to(
+    "borrower",
+    "Koha::Schema::Result::Borrower",
+    { borrowernumber => "borrowernumber" },
+    { join_type => "LEFT", on_delete => "CASCADE", on_update => "CASCADE" },
+);
+
+__PACKAGE__->belongs_to(
+  "item",
+  "Koha::Schema::Result::Item",
+  { itemnumber => "itemnumber" },
+  {
+    is_deferrable => 1,
+    join_type     => "LEFT",
+    on_delete     => "CASCADE",
+    on_update     => "CASCADE",
+  },
+);
+
 sub koha_objects_class {
     'Koha::Old::Checkouts';
 }

catalogue/issuehistory.pl
@@ -25,6 +25,7 @@ use C4::Output;
 use C4::Biblio;    # GetBiblio
 use C4::Search;		# enabled_staff_search_views
 use Koha::Checkouts;
+use Koha::Old::Checkouts;
 
 use Koha::Biblios;
 
@@ -41,17 +42,25 @@ my ( $template, $borrowernumber, $cookie ) = get_template_and_user(
 
 my $biblionumber = $query->param('biblionumber');
 
-my $checkouts = Koha::Checkouts->search(
+my @checkouts = Koha::Checkouts->search(
     { biblionumber => $biblionumber },
     {
         join       => 'item',
         order_by   => 'timestamp',
     }
 );
+my @old_checkouts = Koha::Old::Checkouts->search(
+    { biblionumber => $biblionumber },
+    {
+        join       => 'item',
+        order_by   => 'timestamp',
+    }
+);
+
 my $biblio = Koha::Biblios->find( $biblionumber );
 
 $template->param(
-    checkouts => $checkouts,
+    checkouts => [ @checkouts, @old_checkouts ],
     biblio    => $biblio,
 	issuehistoryview => 1,
 	C4::Search::enabled_staff_search_views,

koha-tmpl/intranet-tmpl/prog/en/modules/catalogue/issuehistory.tt
@@ -26,8 +26,8 @@
 [% IF biblio.author %]<h3>by [% biblio.author %]</h3>[% END %]
 
 <div class="searchresults">
-    [% IF checkouts.count %]
-        <h4>Checked out [% checkouts.count %] times</h4>
+    [% IF checkouts %]
+        <h4>Checked out [% checkouts %] times</h4>
         <table id="table_issues">
             <thead><tr>
             [% IF Koha.Preference('intranetreadinghistory') AND CAN_user_circulate_circulate_remaining_permissions %]