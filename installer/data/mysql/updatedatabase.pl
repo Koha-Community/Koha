4126e687e4c1b271ad75f8285b34a445870cb6d2
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2018-06-22 08:54:26 +0100

    Increment version for 18.05.01 release
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -16052,6 +16052,13 @@ if( CheckVersion( $DBversion ) ) {
     print "Upgrade to $DBversion done (Koha 18.05)\n";
 }
 
+$DBversion = "18.05.01.000";
+if ( CheckVersion($DBversion) ) {
+    SetVersion ($DBversion);
+    print "Upgrade to $DBversion done (18.05.01 release)\n";
+}
+
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 
