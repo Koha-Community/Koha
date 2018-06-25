a1e61cefc29ba5b082ece063ab84cbb756f0c64f
Fridolin Somers <fridolin.somers@biblibre.com>
2018-06-25 14:22:08 +0200

    Increment version for 17.11.07 release
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -15132,6 +15132,12 @@ if( CheckVersion( $DBversion ) ) {
     print "Upgrade to $DBversion done (17.11.06 release)\n";
 }
 
+$DBversion = '17.11.07.000';
+if( CheckVersion( $DBversion ) ) {
+    SetVersion( $DBversion );
+    print "Upgrade to $DBversion done (17.11.07 release)\n";
+}
+
 # DEVELOPER PROCESS, search for anything to execute in the db_update directory
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
