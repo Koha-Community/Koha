addbff779ca6e5be867c83e9c0d03e5f82ad821c
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2018-06-25 16:48:21 -0300

    Bug 20921: Fix opac_auth for selenium
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>

--- t/lib/Selenium.pm
+++ t/lib/Selenium.pm
@@ -79,12 +79,11 @@ sub opac_auth {
 
     $login ||= $self->login;
     $password ||= $self->password;
-    my $mainpage = $self->base_url . 'opac-main.pl';
+    my $mainpage = $self->opac_base_url . 'opac-main.pl';
 
     $self->driver->get($mainpage);
     $self->fill_form( { userid => $login, password => $password } );
-    my $login_button = $self->driver->find_element('//input[@id="submit"]');
-    $login_button->submit();
+    $self->submit_form;
 }
 
 sub fill_form {
