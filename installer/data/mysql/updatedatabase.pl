1f7c3db303f60537aebcad0e9d9bdd5abbb10045
Fridolin Somers <fridolin.somers@biblibre.com>
2018-06-26 10:09:10 +0200

    Increment version for 17.05.13 release
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -14689,6 +14689,12 @@ if ( CheckVersion($DBversion) ) {
     print "Upgrade to $DBversion done (Koha 17.05.12)\n";
 }
 
+$DBversion = "17.05.13.000";
+if ( CheckVersion($DBversion) ) {
+    SetVersion($DBversion);
+    print "Upgrade to $DBversion done (Koha 17.05.13)\n";
+}
+
 # DEVELOPER PROCESS, search for anything to execute in the db_update directory
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
