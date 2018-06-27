bee15e627f4d68c177fbe9f4e6774d679df6c346
Owen Leonard <oleonard@myacpl.org>
2018-06-27 17:01:02 +0000

    Bug 21870: Convert browser alerts to modals: OPAC user summary
    This patch updates the user summary page in the OPAC so that JavaScript
    alerts are replaced with Bootstrap modals. This provides us the ability
    to more carefully control the content of confirmation dialogs and their
    control buttons.
    
    A global function for defining a modal confirmation boxes has been
    added, to which one can pass:
    
     - The modal title
     - Body
     - Text for submit and cancel buttons
     - A callback function to trigger on submit
    
    To test, apply the patch and clear your browser cache if necessary.
    
     - Log in to the OPAC as a user who has holds and article requests.
     - Test the following interactions, both the confirmation and cancel
       options for each:
         - Cancel a hold
         - Suspend all holds
         - Resume all holds
         - Cancel an article request
    
    Signed-off-by: Jose-Mario Monteiro-Santos <jose-mario.monteiro-santos@inLibro.com>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>

--- koha-tmpl/opac-tmpl/bootstrap/js/global.js
+++ koha-tmpl/opac-tmpl/bootstrap/js/global.js
@@ -51,3 +51,39 @@ function suffixOf (s, tok) {
     var index = s.indexOf(tok);
     return s.substring(index + 1);
 }
+
+// Adapted from https://gist.github.com/jnormore/7418776
+function confirmModal(message, title, yes_label, no_label, callback) {
+    $("#bootstrap-confirm-box-modal").data('confirm-yes', false);
+    if($("#bootstrap-confirm-box-modal").length == 0) {
+        $("body").append('<div id="bootstrap-confirm-box-modal" class="modal">\
+            <div class="modal-dialog">\
+                <div class="modal-content">\
+                    <div class="modal-header" style="min-height:40px;">\
+                        <button type="button" class="closebtn" data-dismiss="modal" aria-hidden="true">&times;</button>\
+                        <h4 class="modal-title"></h4>\
+                    </div>\
+                    <div class="modal-body"><p></p></div>\
+                    <div class="modal-footer">\
+                        <a href="#" id="bootstrap-confirm-box-modal-submit" class="btn btn-danger"><i class="fa fa-check"></i></a>\
+                        <a href="#" id="bootstrap-confirm-box-modal-cancel" data-dismiss="modal" class="btn btn-default"><i class="fa fa-remove"></i></a>\
+                    </div>\
+                </div>\
+            </div>\
+        </div>');
+        $("#bootstrap-confirm-box-modal-submit").on('click', function () {
+            $("#bootstrap-confirm-box-modal").data('confirm-yes', true);
+            $("#bootstrap-confirm-box-modal").modal('hide');
+            return false;
+        });
+        $("#bootstrap-confirm-box-modal").on('hide.bs.modal', function () {
+            if(callback) callback($("#bootstrap-confirm-box-modal").data('confirm-yes'));
+        });
+    }
+
+    $("#bootstrap-confirm-box-modal .modal-header h4").text( title || "" );
+    $("#bootstrap-confirm-box-modal .modal-body p").text( message || "" );
+    $("#bootstrap-confirm-box-modal-submit").text( yes_label || 'Confirm' );
+    $("#bootstrap-confirm-box-modal-cancel").text( no_label || 'Cancel' );
+    $("#bootstrap-confirm-box-modal").modal('show');
+}
