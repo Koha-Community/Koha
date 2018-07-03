Koha-Community/Koha

commit 2b90ea2cb0e5e976de7ddef0151ae83d8ac578e6

Bug 17829: Move GetMember to Koha::Patron
GetMember returned a patron given a borrowernumber, cardnumber or
userid.
All of these 3 attributes are defined as a unique key at the DB level
and so we can use Koha::Patrons->find to replace this subroutine.
Additionaly GetMember set category_type and description.

Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

circ/returns.pl
@@ -30,6 +30,8 @@ script to execute returns of books
 use strict;
 use warnings;
 
+# FIXME There are weird things going on with $patron and $borrowernumber in this script
+
 use Carp 'verbose';
 $SIG{ __DIE__ } = sub { Carp::confess( @_ ) };
 
@@ -167,8 +169,8 @@ if ( $query->param('reserve_id') ) {
 #   check if we have other reserves for this document, if we have a return send the message of transfer
     my ( $messages, $nextreservinfo ) = GetOtherReserves($itemnumber);
 
-    my $borr = GetMember( borrowernumber => $nextreservinfo );
-    my $name   = $borr->{'surname'} . ", " . $borr->{'title'} . " " . $borr->{'firstname'};
+    my $patron = Koha::Patrons->find( $nextreservinfo );
+    my $name   = $patron->surname . ", " . $patron->title . " " . $patron->firstname;
     if ( $messages->{'transfert'} ) {
         $template->param(
             itemtitle      => $biblio->title,
@@ -177,10 +179,10 @@ if ( $query->param('reserve_id') ) {
             iteminfo       => $biblio->author,
             name           => $name,
             borrowernumber => $borrowernumber,
-            borcnum        => $borr->{'cardnumber'},
-            borfirstname   => $borr->{'firstname'},
-            borsurname     => $borr->{'surname'},
-            borcategory    => $borr->{'description'},
+            borcnum        => $patron->cardnumber,
+            borfirstname   => $patron->firstname,
+            borsurname     => $patron->surname,
+            borcategory    => $patron->category->description,
             diffbranch     => 1,
         );
     }
@@ -399,27 +401,31 @@ if ( $messages->{'WrongTransfer'} and not $messages->{'WasTransfered'}) {
     );
 
     my $reserve    = $messages->{'ResFound'};
-    my $borr = C4::Members::GetMember( borrowernumber => $reserve->{'borrowernumber'} );
-    my $name = $borr->{'surname'} . ", " . $borr->{'title'} . " " . $borr->{'firstname'};
+    if ( $reserve ) {
+        my $patron = Koha::Patrons->find( $reserve->{'borrowernumber'} );
+        my $name = $patron->surname . ", " . $patron->title . " " . $patron->firstname;
+        $template->param(
+            # FIXME The full patron object should be passed to the template
+                wname           => $name,
+                wborfirstname   => $patron->firstname,
+                wborsurname     => $patron->surname,
+                wborcategory    => $patron->category->description,
+                wbortitle       => $patron->title,
+                wborphone       => $patron->phone,
+                wboremail       => $patron->email,
+                streetnumber    => $patron->streetnumber,
+                address         => $patron->address,
+                address2        => $patron->address2,
+                city            => $patron->city,
+                zipcode         => $patron->zipcode,
+                state           => $patron->state,
+                country         => $patron->country,
+                wborrowernumber => $reserve->{'borrowernumber'},
+                wborcnum        => $patron->cardnumber,
+        );
+    }
     $template->param(
-            wname           => $name,
-            wborfirstname   => $borr->{'firstname'},
-            wborsurname     => $borr->{'surname'},
-            wborcategory    => $borr->{'description'},
-            wbortitle       => $borr->{'title'},
-            wborphone       => $borr->{'phone'},
-            wboremail       => $borr->{'email'},
-            streetnumber    => $borr->{streetnumber},
-            streettype      => $borr->{streettype},
-            address         => $borr->{'address'},
-            address2        => $borr->{'address2'},
-            city            => $borr->{'city'},
-            zipcode         => $borr->{'zipcode'},
-            state           => $borr->{'state'},
-            country         => $borr->{'country'},
-            wborrowernumber => $reserve->{'borrowernumber'},
-            wborcnum        => $borr->{'cardnumber'},
-            wtransfertFrom  => $userenv_branch,
+        wtransfertFrom  => $userenv_branch,
     );
 }
 
@@ -428,7 +434,7 @@ if ( $messages->{'WrongTransfer'} and not $messages->{'WasTransfered'}) {
 #
 if ( $messages->{'ResFound'}) {
     my $reserve    = $messages->{'ResFound'};
-    my $borr = C4::Members::GetMember( borrowernumber => $reserve->{'borrowernumber'} );
+    my $patron = Koha::Patrons->find( $reserve->{borrowernumber} );
     my $holdmsgpreferences =  C4::Members::Messaging::GetMessagingPreferences( { borrowernumber => $reserve->{'borrowernumber'}, message_name   => 'Hold_Filled' } );
     if ( $reserve->{'ResFound'} eq "Waiting" or $reserve->{'ResFound'} eq "Reserved" ) {
         if ( $reserve->{'ResFound'} eq "Waiting" ) {
@@ -446,25 +452,25 @@ if ( $messages->{'ResFound'}) {
 
         # same params for Waiting or Reserved
         $template->param(
+            # FIXME The full patron object should be passed to the template
             found          => 1,
-            name           => $borr->{'surname'} . ", " . $borr->{'title'} . " " . $borr->{'firstname'},
-            borfirstname   => $borr->{'firstname'},
-            borsurname     => $borr->{'surname'},
-            borcategory    => $borr->{'description'},
-            bortitle       => $borr->{'title'},
-            borphone       => $borr->{'phone'},
-            boremail       => $borr->{'email'},
-            streetnumber   => $borr->{streetnumber},
-            streettype     => $borr->{streettype},
-            address        => $borr->{'address'},
-            address2       => $borr->{'address2'},
-            city           => $borr->{'city'},
-            zipcode        => $borr->{'zipcode'},
-            state          => $borr->{'state'},
-            country        => $borr->{'country'},
-            borcnum        => $borr->{'cardnumber'},
-            debarred       => $borr->{'debarred'},
-            gonenoaddress  => $borr->{'gonenoaddress'},
+            name           => $patron->surname . ", " . $patron->title . " " . $patron->firstname,
+            borfirstname   => $patron->firstname,
+            borsurname     => $patron->surname,
+            borcategory    => $patron->category->description,
+            bortitle       => $patron->title,
+            borphone       => $patron->phone,
+            boremail       => $patron->email,
+            boraddress     => $patron->address,
+            boraddress2    => $patron->address2,
+            streetnumber   => $patron->streetnumber,
+            city           => $patron->city,
+            zipcode        => $patron->zipcode,
+            state          => $patron->state,
+            country        => $patron->country,
+            borcnum        => $patron->cardnumber,
+            debarred       => $patron->debarred,
+            gonenoaddress  => $patron->gonenoaddress,
             barcode        => $barcode,
             destbranch     => $reserve->{'branchcode'},
             borrowernumber => $reserve->{'borrowernumber'},
@@ -572,19 +578,19 @@ foreach ( sort { $a <=> $b } keys %returneditems ) {
             $ri{hour}   = $duedate->hour();
             $ri{minute}   = $duedate->minute();
             $ri{duedate} = output_pref($duedate);
-            my $b      = C4::Members::GetMember( borrowernumber => $riborrowernumber{$_} );
+            my $patron = Koha::Patrons->find( $riborrowernumber{$_} );
             unless ( $dropboxmode ) {
                 $ri{return_overdue} = 1 if (DateTime->compare($duedate, DateTime->now()) == -1);
             } else {
                 $ri{return_overdue} = 1 if (DateTime->compare($duedate, $dropboxdate) == -1);
             }
-            $ri{borrowernumber} = $b->{'borrowernumber'};
-            $ri{borcnum}        = $b->{'cardnumber'};
-            $ri{borfirstname}   = $b->{'firstname'};
-            $ri{borsurname}     = $b->{'surname'};
-            $ri{bortitle}       = $b->{'title'};
-            $ri{bornote}        = $b->{'borrowernotes'};
-            $ri{borcategorycode}= $b->{'categorycode'};
+            $ri{borrowernumber} = $patron->borrowernumber;
+            $ri{borcnum}        = $patron->cardnumber;
+            $ri{borfirstname}   = $patron->firstname;
+            $ri{borsurname}     = $patron->surname;
+            $ri{bortitle}       = $patron->title;
+            $ri{bornote}        = $patron->borrowernotes;
+            $ri{borcategorycode}= $patron->categorycode;
             $ri{borissuescount} = Koha::Checkouts->count( { borrowernumber => $b->{'borrowernumber'} } );
         }
         else {

commit 16add1adb392389c87b30446cc13183d43aae167

Bug 21019: Fix display of patron's address on reserve pop-up on returns.pl
Caused by
  commit 2b90ea2cb0e5e976de7ddef0151ae83d8ac578e6
  Bug 17829: Move GetMember to Koha::Patron

address variable has been replaced by boraddress (wrong resolution
conflict?)

It does not affect master (thanks bug 18789)

Test plan:
Confirm the address of the patron with a hold placed on the item is now
displayed when this item is checked in

Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

circ/returns.pl
@@ -462,8 +462,8 @@ if ( $messages->{'ResFound'}) {
             bortitle       => $patron->title,
             borphone       => $patron->phone,
             boremail       => $patron->email,
-            boraddress     => $patron->address,
-            boraddress2    => $patron->address2,
+            address        => $patron->address,
+            address2       => $patron->address2,
             streetnumber   => $patron->streetnumber,
             city           => $patron->city,
             zipcode        => $patron->zipcode,