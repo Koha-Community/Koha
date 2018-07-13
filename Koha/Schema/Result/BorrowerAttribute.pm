956c1a2a26f6663e1620492e67c6a9b20ef3fe94
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2018-07-13 10:41:15 -0300

    Bug 20443: Revert some stuffs from 12159
    These methods have been added to Koha::Patron::Attribute but are wrong, see bug 18339
    
    We should use ->type->$method instead
    
    Moreover the tests exist in another subtest, we do not need them.
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- Koha/Schema/Result/BorrowerAttribute.pm
+++ Koha/Schema/Result/BorrowerAttribute.pm
@@ -109,6 +109,21 @@ __PACKAGE__->belongs_to(
 # Created by DBIx::Class::Schema::Loader v0.07042 @ 2016-12-08 04:41:27
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:EarETnedFsmRmAAJAUrKGg
 
+=head2 borrower_attribute_types
+
+Type: belongs_to
+
+Related object: L<Koha::Schema::Result::BorrowerAttributeType>
+
+=cut
+
+__PACKAGE__->belongs_to(
+    "borrower_attribute_types",
+    "Koha::Schema::Result::BorrowerAttributeType",
+    { "foreign.code" => "self.code" },
+    { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },);
+
+
 sub koha_object_class {
     'Koha::Patron::Attribute';
 }
