0b89cbf6a473e71597bc051d88c2152531648ad8
Katrin Fischer <katrin.fischer.83@web.de>
2018-07-15 19:27:03 +0200

    Bug 21077: Fix database documentation for statistics.type
    There's a typo for 'locause', this should be 'localuse'.
    Also, 'Credit*' is never used.
    
    Signed-off-by: Maryse Simard <maryse.simard@inlibro.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- installer/data/mysql/kohastructure.sql
+++ installer/data/mysql/kohastructure.sql
@@ -1996,7 +1996,7 @@ CREATE TABLE `statistics` ( -- information related to transactions (circulation 
   `branch` varchar(10) default NULL, -- foreign key, branch where the transaction occurred
   `proccode` varchar(4) default NULL, -- type of procedure used when making payments (does not appear in the code)
   `value` double(16,4) default NULL, -- monetary value associated with the transaction
-  `type` varchar(16) default NULL, -- transaction type (locause, issue, return, renew, writeoff, payment, Credit*)
+  `type` varchar(16) default NULL, -- transaction type (localuse, issue, return, renew, writeoff, payment)
   `other` LONGTEXT, -- used by SIP
   `usercode` varchar(10) default NULL, -- unused in Koha
   `itemnumber` int(11) default NULL, -- foreign key from the items table, links transaction to a specific item
