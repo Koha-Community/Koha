82cede1cc202e8e8e6b8d49357400da3343318ca
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2018-07-19 17:56:34 -0300

    Bug 13618: Add the raw TT filter
    Signed-off-by: Owen Leonard <oleonard@myacpl.org>
    
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>

---
+++ Koha/Template/Plugin/raw.pm
@@ -0,0 +1,30 @@
+package Koha::Template::Plugin::raw;
+
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+use Template::Plugin::Filter;
+use base qw( Template::Plugin::Filter );
+
+our $DYNAMIC = 1;
+
+sub filter {
+    my ( $self, $text ) = @_;
+    return $text;
+}
+
+1;
