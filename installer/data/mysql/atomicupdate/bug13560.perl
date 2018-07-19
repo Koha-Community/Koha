3286bead8e60fbcf8b2a76685b03c7cf6712da5e
Nick Clemens <nick@bywatersolutions.com>
2018-07-19 17:19:38 +0000

    Bug 13560: DBRev 18.06.00.008
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>

--- installer/data/mysql/atomicupdate/bug13560.perl
+++
@@ -1,6 +0,0 @@
-$DBversion = 'XXX';  # will be replaced by the RM
-if( CheckVersion( $DBversion ) ) {
-    $dbh->do( "ALTER TABLE marc_modification_template_actions CHANGE action action ENUM('delete_field','add_field','update_field','move_field','copy_field','copy_and_replace_field')" );
-    SetVersion( $DBversion );
-    print "Upgrade to $DBversion done (Bug 13560 - need an add option in marc modification templates)\n";
-}
--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -16155,6 +16155,13 @@ if( CheckVersion( $DBversion ) ) {
     print "Upgrade to $DBversion done (Bug 7651 - Add separate permission for managing currencies and exchange rates)\n";
 }
 
+$DBversion = '18.06.00.008';  # will be replaced by the RM
+if( CheckVersion( $DBversion ) ) {
+    $dbh->do( "ALTER IGNORE TABLE marc_modification_template_actions CHANGE action action ENUM('delete_field','add_field','update_field','move_field','copy_field','copy_and_replace_field')" );
+    SetVersion( $DBversion );
+    print "Upgrade to $DBversion done (Bug 13560 - need an add option in marc modification templates)\n";
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 
