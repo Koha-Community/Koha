1ee0bafc739f9286bfd129fbbd694bcd7c8ec0c2
Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
2018-07-27 08:29:03 +0200

    Bug 20661: (QA follow-up) Add POD for output_and_exit
    Resolve:
     FAIL   C4/Output.pm
       FAIL   pod
                    *** ERROR: Apparent command =cut not preceded by blank line  in file C4/Output.pm
       FAIL   pod coverage
                    POD is missing for output_and_exit
    
    Test plan:
    Run QA tools again.
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    (cherry picked from commit ef58eff737b2fdcb4f97c8667e2f83e7f7aa70c7)
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- C4/Output.pm
+++ C4/Output.pm
@@ -62,7 +62,6 @@ C4::Output - Functions for managing output, is slowly being deprecated
 =head1 FUNCTIONS
 
 =over 2
-=cut
 
 =item pagination_bar
 
@@ -341,6 +340,15 @@ sub output_and_exit_if_error {
     output_and_exit( $query, $cookie, $template, $error ) if $error;
     return;
 }
+
+=item output_and_exit
+
+    output_and_exit( $query, $cookie, $template, $error );
+
+    $error is a blocking error like biblionumber not found or so.
+    We should output the error and exit.
+
+=cut
 
 sub output_and_exit {
     my ( $query, $cookie, $template, $error ) = @_;
