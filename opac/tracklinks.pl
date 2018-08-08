Koha-Community/Koha

commit 51b6cf2aafbfb45b9fcb072403b986fe3c2add53

Bug 19847: Track links within the records and 404 for others
Test plan:
Behave like a robot, you will get 404
Be a human, you will be tracked

Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>

Signed-off-by: Nick Clemens <nick@bywatersolutions.com>

Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

opac/tracklinks.pl
@@ -21,6 +21,7 @@
 use Modern::Perl;
 use C4::Context;
 use C4::Auth qw(checkauth);
+use C4::Biblio;
 use Koha::Linktracker;
 use CGI qw ( -utf8 );
 
@@ -54,25 +55,28 @@ if ($uri) {
         my $biblionumber = $cgi->param('biblionumber') || 0;
         my $itemnumber   = $cgi->param('itemnumber')   || 0;
 
-        $tracker->trackclick(
-            {
-                uri            => $uri,
-                biblionumber   => $biblionumber,
-                borrowernumber => $borrowernumber,
-                itemnumber     => $itemnumber
-            }
-        );
-        print $cgi->redirect($uri);
+        my $record = C4::Biblio::GetMarcBiblio({ biblionumber => $biblionumber });
+        my $marc_urls = C4::Biblio::GetMarcUrls($record, C4::Context->preference('marcflavour'));
+        if ( grep { /^$uri$/ } map { $_->{MARCURL} } @$marc_urls ) {
+            $tracker->trackclick(
+                {
+                    uri            => $uri,
+                    biblionumber   => $biblionumber,
+                    borrowernumber => $borrowernumber,
+                    itemnumber     => $itemnumber
+                }
+            );
+            print $cgi->redirect($uri);
+            exit;
+        }
     }
     else {
 
         # We have a valid url, but we shouldn't track it, just redirect
         print $cgi->redirect($uri);
+        exit;
     }
 }
-else {
 
-    # we shouldn't be here, bail out
-    print $cgi->redirect("/cgi-bin/koha/errors/404.pl");    # escape early
-    exit;
-}
+print $cgi->redirect("/cgi-bin/koha/errors/404.pl");    # escape early
+exit;

commit d14ed73744fa23d4ed649da6d847f4b37cdfce92

Bug 21018: Fix TrackClicks for 952$u
Caused by
  commit 51b6cf2aafbfb45b9fcb072403b986fe3c2add53
  Bug 19847: Track links within the records and 404 for others

We should also accept uri stored in items.uri (952$u)

Test plan:
- Turn on TrackClicks
- Defined uri for some items
- At the OPAC click on the link (from opac-detail.pl)
=> Without this patch you will get 404 (redirected from tracklinks.pl)
=> With this patch applied you will be redirected correctly and the
linktracker will be filled correctly

Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>

Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

Signed-off-by: Nick Clemens <nick@bywatersolutions.com>

opac/tracklinks.pl
@@ -22,6 +22,7 @@ use Modern::Perl;
 use C4::Context;
 use C4::Auth qw(checkauth);
 use C4::Biblio;
+use Koha::Items;
 use Koha::Linktracker;
 use CGI qw ( -utf8 );
 
@@ -57,7 +58,9 @@ if ($uri) {
 
         my $record = C4::Biblio::GetMarcBiblio({ biblionumber => $biblionumber });
         my $marc_urls = C4::Biblio::GetMarcUrls($record, C4::Context->preference('marcflavour'));
-        if ( grep { $_ eq $uri } map { $_->{MARCURL} } @$marc_urls ) {
+        if ( ( grep { $_ eq $uri } map { $_->{MARCURL} } @$marc_urls )
+            || Koha::Items->search( { uri => $uri } )->count )
+        {
             $tracker->trackclick(
                 {
                     uri            => $uri,