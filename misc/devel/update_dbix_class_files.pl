1bcc2159a5c137c216f6afa759cb7da5f2f4a15d
Julian Maurice <julian.maurice@biblibre.com>
2018-08-08 15:40:09 +0200

    Bug 21177: Use koha-conf.xml in misc/devel/update_dbix_class_files.pl
    It is annoying to have to specify database connection parameters each
    time DBIx::Class files need to be updated.
    
    This patch adds a new option --koha-conf that takes an optional <path>
    which defaults to the value of KOHA_CONF environment variable, and use
    the database connection parameters found in that file.
    --db_* options override values from $KOHA_CONF
    
    Test plan:
    1. Run the script with the same parameters as before the patch and see
       that it still works.
       Example:
         misc/devel/update_dbix_class_files.pl --db_name koha_dev \
         --db_user koha --db_pass koha
    
    2. Verify that KOHA_CONF is set and execute:
         misc/devel/update_dbix_class_files.pl --koha-conf
       Verify that Koha/Schema files were updated accordingly
    
    3. Execute:
        misc/devel/update_dbix_class_files.pl --koha-conf \
        /path/to/another/koha-conf.xml
       Verify that Koha/Schema files were updated accordingly
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- misc/devel/update_dbix_class_files.pl
+++ misc/devel/update_dbix_class_files.pl
@@ -25,13 +25,20 @@ use DBIx::Class::Schema::Loader qw/ make_schema_at /;
 use Getopt::Long;
 use Pod::Usage;
 
+my %db_defaults = (
+    driver => 'mysql',
+    host => 'localhost',
+    port => '3306',
+);
+
 my $path = "./";
-my $db_driver = 'mysql';
-my $db_host = 'localhost';
-my $db_port = '3306';
+my $db_driver;
+my $db_host;
+my $db_port;
 my $db_name;
 my $db_user;
 my $db_passwd;
+my $koha_conf;
 my $help;
 
 GetOptions(
@@ -42,11 +49,47 @@ GetOptions(
     "db_name=s"   => \$db_name,
     "db_user=s"   => \$db_user,
     "db_passwd=s" => \$db_passwd,
+    "koha-conf:s" => \$koha_conf,
     "h|help"      => \$help
 );
 
 # If we were asked for usage instructions, do it
 pod2usage(1) if defined $help;
+
+if (defined $koha_conf) {
+    if ($koha_conf eq '' and not defined $ENV{KOHA_CONF}) {
+        print STDERR "Error: KOHA_CONF is not defined\n";
+        exit(1);
+    }
+
+    $koha_conf ||= $ENV{KOHA_CONF};
+    unless (-r $koha_conf) {
+        print STDERR "Error: File $koha_conf does not exist or is not readable\n";
+        exit(1);
+    }
+
+    require C4::Context;
+    my $context = C4::Context->new($koha_conf);
+    unless ($context) {
+        print STDERR "Error: Koha context creation failed. Please check that $koha_conf is correct\n";
+        exit(1);
+    }
+
+    $context->set_context;
+    $db_defaults{driver} = $context->config('db_scheme');
+    $db_defaults{host} = $context->config('hostname');
+    $db_defaults{port} = $context->config('port');
+    $db_defaults{name} = $context->config('database');
+    $db_defaults{user} = $context->config('user');
+    $db_defaults{passwd} = $context->config('pass');
+}
+
+$db_driver //= $db_defaults{driver};
+$db_host //= $db_defaults{host};
+$db_port //= $db_defaults{port};
+$db_name //= $db_defaults{name};
+$db_user //= $db_defaults{user};
+$db_passwd //= $db_defaults{passwd};
 
 if (! defined $db_name ) {
     print "Error: \'db_name\' parameter is mandatory.\n";
@@ -68,8 +111,8 @@ misc/devel/update_dbix_class_files.pl
 
 =head1 SYNOPSIS
 
- update_dbix_class_files.pl --db_name=db-name --db_user=db-user \
-                            --db_passwd=db-pass ...
+ update_dbix_class_files.pl [--koha-conf <path>] --db_name=db-name \
+                            --db_user=db-user --db_passwd=db-pass ...
 
 The command in usually called from the root directory for the Koha source tree.
 If you are running from another directory, use the --path switch to specify
@@ -78,6 +121,15 @@ a different path.
 =head1 OPTIONS
 
 =over 8
+
+=item B<--koha-conf> <path>
+
+Path to koha-conf.xml from which DB connection params will be retrieved.
+
+<path> is optional and defaults to the value of environment variable KOHA_CONF,
+if set. It is an error to omit the <path> if KOHA_CONF is not set.
+
+Any B<--db_*> options will override values retrieved from <path>.
 
 =item B<--db_name>
 
