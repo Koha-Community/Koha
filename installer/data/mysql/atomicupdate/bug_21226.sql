28ee71e6849e2be21f24626aad6530b55f514419
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2018-08-15 11:36:21 -0300

    Bug 21226: DB Changes
    Remove prefs OCLCAffiliateID, XISBN and XISBNDailyLimit
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    (cherry picked from commit b19090c4505c0f407a7f3cbfe3c14da5cfecc657)
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    (cherry picked from commit 8111e0c380c9d2c61a2b6b6665b5a7bad3a3e646)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

---
+++ installer/data/mysql/atomicupdate/bug_21226.sql
@@ -0,0 +1,3 @@
+DELETE FROM systempreferences where variable="OCLCAffiliateID";
+DELETE FROM systempreferences where variable="XISBN";
+DELETE FROM systempreferences where variable="XISBNDailyLimit";
