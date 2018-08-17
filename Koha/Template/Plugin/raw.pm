9e895e581288e4299e28d6c542ac3f66f80d6f3d
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2018-08-17 13:11:28 -0300

    Bug 13618: Add POD to raw.pm
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>

--- Koha/Template/Plugin/raw.pm
+++ Koha/Template/Plugin/raw.pm
@@ -28,3 +28,31 @@ sub filter {
 }
 
 1;
+
+=head1 NAME
+
+Koha::Template::Plugin::raw - TT Plugin for filtering variables as raw
+
+=head1 SYNOPSIS
+
+[% USE raw %]
+
+[% my_var | $raw %]
+
+The variable will not be modified and display at it.
+
+It is required to use a filter to display any variables in .tt or .inc
+
+In most of the case, you need to use the html filter instead.
+
+=head1 METHODS
+
+=head2 filter
+
+Will return the variable as it. Nothing is changed.
+
+=head1 AUTHOR
+
+Jonathan Druart <jonathan.druart@biblibre.com>
+
+=cut
