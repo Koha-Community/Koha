9659b2f447ef21a71b3d3698fba126be526a8849
Nick Clemens <nick@bywatersolutions.com>
2018-09-15 21:52:47 +0000

    Bug 20777: DBRev 18.05.03.003
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    (cherry picked from commit 2847ac4b4de239a430e6e9d20f6e895435c56d7b)
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -16166,6 +16166,18 @@ if( CheckVersion( $DBversion ) ) {
     print "Upgrade to $DBversion done (Bug 21288: Slowness in acquisition caused by GetInvoices\n";
 }
 
+$DBversion = '18.05.03.003';
+if( CheckVersion( $DBversion ) ) {
+    if ( column_exists( 'accountlines', 'dispute' ) ) {
+        $dbh->do(q{
+            ALTER TABLE `accountlines`
+                DROP COLUMN `dispute`
+        });
+    }
+    SetVersion( $DBversion );
+    print "Upgrade to $DBversion done (Bug 20777 - Remove unused field accountlines.dispute)\n";
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 
