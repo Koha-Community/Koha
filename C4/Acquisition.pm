ab4cf23286219b473820b61bca82c884de274e80
Nick Clemens <nick@bywatersolutions.com>
2018-09-16 01:21:16 +0000

    Bug 18736: (follow-up) cast as UNSIGNED instead of INTEGER
    More recent MySql versions require the use of SIGNED or UNSIGNED rather
    than INTEGER. UNSIGNED will still work in older installs
    
    Signed-off-by: Julian Maurice <julian.maurice@biblibre.com>
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>

--- C4/Acquisition.pm
+++ C4/Acquisition.pm
@@ -2011,7 +2011,7 @@ returns the correct SQL routine based on OrderPriceRounding system preference.
 sub _get_rounding_sql {
     my ( $round_string ) = @_;
     my $rounding_pref = C4::Context->preference('OrderPriceRounding');
-    if ( $rounding_pref eq "nearest_cent"  ) { return ("CAST($round_string*100 AS INTEGER)/100"); }
+    if ( $rounding_pref eq "nearest_cent"  ) { return ("CAST($round_string*100 AS UNSIGNED)/100"); }
     else                                     { return ("$round_string"); }
 }
 
