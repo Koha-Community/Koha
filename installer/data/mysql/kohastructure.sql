3b489bc24e2b9c39110ed0ec7dc4f9ff28f12966
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2018-09-29 17:29:44 -0300

    Bug 18887: Add foreign keys
    Signed-off-by: Josef Moravec <josef.moravec@gmail.com>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>

--- installer/data/mysql/kohastructure.sql
+++ installer/data/mysql/kohastructure.sql
@@ -4219,9 +4219,9 @@ CREATE TABLE `circulation_rules` (
   `rule_name` varchar(32) NOT NULL,
   `rule_value` varchar(32) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `branchcode` (`branchcode`),
-  KEY `categorycode` (`categorycode`),
-  KEY `itemtype` (`itemtype`),
+  CONSTRAINT `circ_rules_ibfk_1` FOREIGN KEY (`branchcode`) REFERENCES `branches` (`branchcode`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `circ_rules_ibfk_2` FOREIGN KEY (`categorycode`) REFERENCES `categories` (`categorycode`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `circ_rules_ibfk_3` FOREIGN KEY (`itemtype`) REFERENCES `itemtypes` (`itemtype`) ON DELETE CASCADE ON UPDATE CASCADE,
   KEY `rule_name` (`rule_name`),
   UNIQUE (`branchcode`,`categorycode`,`itemtype`,`rule_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
