c6e44c9c6bb78bfb3edeacf877c5edb114aab558
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2018-10-01 12:14:17 -0300

    Bug 21403: Do not overwrite the options if modified locally
    Just in case...
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>

--- installer/data/mysql/atomicupdate/bug_21403-add_indian_amazon_domain.sql
+++ installer/data/mysql/atomicupdate/bug_21403-add_indian_amazon_domain.sql
@@ -1,1 +1,1 @@
-UPDATE `systempreferences` SET options = 'US|CA|DE|FR|IN|JP|UK' WHERE variable = 'AmazonLocale';
+UPDATE `systempreferences` SET options = 'US|CA|DE|FR|IN|JP|UK' WHERE variable = 'AmazonLocale' AND options='US|CA|DE|FR|JP|UK';
