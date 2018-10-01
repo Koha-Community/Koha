Koha-Community/Koha

commit a7df1f9f8eb9ed16246964d94dd8a69b756b6551

Bug 18904: (follow-up) Retrieve subfield in the order they are stored
Use subfields in the order they are stored.

This patch removes use of an intermediate hash for storing values. Order
of subfields as obtained from authority is now preserved.

Also removes useless code that was intended to set $2 subfields

Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>

Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>

Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

authorities/blinddetail-biblio-search.pl
@@ -86,20 +86,16 @@ if ($authid) {
     my $repet = ($query->param('repet') || 1) - 1;
     my $field = $fields[$repet];
 
-    # Get all values for each distinct subfield
-    my %subfields;
+    # Get all values for each distinct subfield and add to subfield loop
+    my %done_subfields;
     for ( $field->subfields ) {
         next if $_->[0] eq '9'; # $9 will be set with authid value
         my $letter = $_->[0];
-        next if defined $subfields{$letter};
+        $letter ||= '@';
+        next if defined $done_subfields{$letter};
         my @values = $field->subfield($letter);
-        $subfields{$letter} = \@values;
-    }
-
-    # Add all subfields to the subfield_loop
-    for( keys %subfields ) {
-        my $letter = $_ || '@';
-        push( @subfield_loop, {marc_subfield => $letter, marc_values => $subfields{$_}} );
+        push @subfield_loop, {marc_subfield => $letter, marc_values => @values };
+        $done_subfields{$letter} = 1;
     }
 
     push( @subfield_loop, { marc_subfield => 'w', marc_values => $relationship } ) if ( $relationship );

commit 08386cd3f05c5bc4dc6306e3a1d6c4878cce7c86

Bug 21448: Fix copy repeated subfields when linking an authority with 6xx
The reported issue was:
On field 606, if I use an Authority with multiple "x" subfields, it will
add just the first "x"

Error in the logs:
Odd number of elements in anonymous hash at /home/vagrant/kohaclone/authorities/blinddetail-biblio-search.pl line 97.

We should pass a ref (!), caused by
  commit a7df1f9f8eb9ed16246964d94dd8a69b756b6551
  Bug 18904: (follow-up) Retrieve subfield in the order they are stored

Test plan:
Edit an authority and add several $x to 600
Link this authority to a bibliographic record
=> Without this patch only the first subfield is copied
=> With this patch applied they are all copied

Signed-off-by: José Anjos <joseanjos@gmail.com>
Signed-off-by: Chris Cormack <chrisc@catalyst.net.nz>

Signed-off-by: Nick Clemens <nick@bywatersolutions.com>

authorities/blinddetail-biblio-search.pl
@@ -94,7 +94,7 @@ if ($authid) {
         $letter ||= '@';
         next if defined $done_subfields{$letter};
         my @values = $field->subfield($letter);
-        push @subfield_loop, {marc_subfield => $letter, marc_values => @values };
+        push @subfield_loop, {marc_subfield => $letter, marc_values => \@values };
         $done_subfields{$letter} = 1;
     }
 