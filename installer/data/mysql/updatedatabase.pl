25b5759f3a9c43bd40eb283544f9af345046226f
Fridolin Somers <fridolin.somers@biblibre.com>
2018-10-05 09:08:30 +0200

    Increment version for 17.05.14 release
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -14715,6 +14715,12 @@ if( CheckVersion( $DBversion ) ) {
     print "Upgrade to $DBversion done (Bug 21226 - Remove prefs OCLCAffiliateID, XISBN and XISBNDailyLimit)\n";
 }
 
+$DBversion = "17.05.14.000";
+if ( CheckVersion($DBversion) ) {
+    SetVersion($DBversion);
+    print "Upgrade to $DBversion done (Koha 17.05.14)\n";
+}
+
 # DEVELOPER PROCESS, search for anything to execute in the db_update directory
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
