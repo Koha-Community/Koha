91790fdd90c8dc59b70f6defd63b088febf5b39d
Nick Clemens <nick@bywatersolutions.com>
2018-10-09 10:41:03 +0000

    Bug 21082: DBRev 18.06.00.037
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>

--- Koha/Schema/Result/Branch.pm
+++ Koha/Schema/Result/Branch.pm
@@ -316,6 +316,21 @@ __PACKAGE__->has_many(
   { cascade_copy => 0, cascade_delete => 0 },
 );
 
+=head2 branches_overdrive
+
+Type: might_have
+
+Related object: L<Koha::Schema::Result::BranchesOverdrive>
+
+=cut
+
+__PACKAGE__->might_have(
+  "branches_overdrive",
+  "Koha::Schema::Result::BranchesOverdrive",
+  { "foreign.branchcode" => "self.branchcode" },
+  { cascade_copy => 0, cascade_delete => 0 },
+);
+
 =head2 branchtransfers_frombranches
 
 Type: has_many
@@ -632,8 +647,8 @@ __PACKAGE__->has_many(
 );
 
 
-# Created by DBIx::Class::Schema::Loader v0.07042 @ 2018-10-05 14:29:17
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:L/zUeW5lNf5oJcZ/bES7rw
+# Created by DBIx::Class::Schema::Loader v0.07042 @ 2018-10-09 10:38:33
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:nKWRDScCAwZUDrRhm1kYMA
 
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
