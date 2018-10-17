60e00a332e2a500746f12908042dd61250d1f79a
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2018-10-17 19:31:14 -0300

    Bug 21598: 'koha_kohadev.aqbudgets.budget_parent_id' isn't in GROUP BY
    DBD::mysql::db selectall_hashref failed:
    'koha_kohadev.aqbudgets.budget_parent_id' isn't in GROUP BY [for
    Statement "
    
        at /home/vagrant/kohaclone/C4/Budgets.pm line 558.
    and
        at /home/vagrant/kohaclone/C4/Budgets.pm line 565.
    
    Test plan:
    hit /cgi-bin/koha/admin/aqbudgets.pl
    
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>

--- C4/Budgets.pm
+++ C4/Budgets.pm
@@ -560,14 +560,14 @@ sub GetBudgetHierarchy {
                SUM( COALESCE(unitprice_tax_included, ecost_tax_included) * quantity ) AS budget_spent
         FROM aqorders JOIN aqbudgets USING (budget_id)
         WHERE quantityreceived > 0 AND datecancellationprinted IS NULL
-        GROUP BY budget_id
+        GROUP BY budget_id, budget_parent_id
         |, 'budget_id');
     my $hr_budget_ordered = $dbh->selectall_hashref(q|
         SELECT aqorders.budget_id, aqbudgets.budget_parent_id,
                SUM(ecost_tax_included *  quantity) AS budget_ordered
         FROM aqorders JOIN aqbudgets USING (budget_id)
         WHERE quantityreceived = 0 AND datecancellationprinted IS NULL
-        GROUP BY budget_id
+        GROUP BY budget_id, budget_parent_id
         |, 'budget_id');
     my $hr_budget_spent_shipment = $dbh->selectall_hashref(q|
         SELECT shipmentcost_budgetid as budget_id,
