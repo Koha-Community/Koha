a23f610fc2a8283b12cd8af4832dc34eb493004b
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2018-10-18 17:37:35 -0300

    Bug 21612: Remove incorrect GROUP BY's from Koha::VirtualShelves
    'koha_kohadev.me.shelfname' isn't in GROUP BY
    
    Same as
      commit 7e220e0d8f107cce63f32bfaa1919c04b4dea6e2
      Bug 20179: [sql_modes] Remove GROUP BY in get_shelves_containing_record
    
    Test plan:
    - Add public and privates shelves
    - Shares some of them
    - List them
    => The correct numbers of shelves must be displayed
    
    Signed-off-by: Andrew Isherwood <andrew.isherwood@ptfs-europe.com>
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    (cherry picked from commit b106ddb96b21a78559d7a60057e94024e7db4a16)
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- Koha/Virtualshelves.pm
+++ Koha/Virtualshelves.pm
@@ -55,7 +55,7 @@ sub get_private_shelves {
         },
         {
             join => [ 'virtualshelfshares' ],
-            group_by => 'shelfnumber',
+            distinct => 'shelfnumber',
             order_by => 'shelfname',
             ( ( $page and $rows ) ? ( page => $page, rows => $rows ) : () ),
         }
@@ -73,7 +73,7 @@ sub get_public_shelves {
             category => 2,
         },
         {
-            group_by => 'shelfnumber',
+            distinct => 'shelfnumber',
             order_by => 'shelfname',
             ( ( $page and $rows ) ? ( page => $page, rows => $rows ) : () ),
         }
@@ -116,7 +116,7 @@ sub get_some_shelves {
         },
         {
             join => [ 'virtualshelfshares' ],
-            group_by => 'shelfnumber',
+            distinct => 'shelfnumber',
             order_by => { -desc => 'lastmodified' },
         }
     );
