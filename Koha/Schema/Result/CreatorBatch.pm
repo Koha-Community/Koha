2e3eb44319db63f33ac05a51782c0692aa9a95d8
Nick Clemens <nick@bywatersolutions.com>
2018-10-24 17:35:18 +0000

    Bug 15766: (RM follow-up) Schema updates
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>

--- Koha/Schema/Result/CreatorBatch.pm
+++ Koha/Schema/Result/CreatorBatch.pm
@@ -35,6 +35,11 @@ __PACKAGE__->table("creator_batches");
   default_value: 1
   is_nullable: 0
 
+=head2 description
+
+  data_type: 'mediumtext'
+  is_nullable: 1
+
 =head2 item_number
 
   data_type: 'integer'
@@ -76,6 +81,8 @@ __PACKAGE__->add_columns(
   { data_type => "integer", is_auto_increment => 1, is_nullable => 0 },
   "batch_id",
   { data_type => "integer", default_value => 1, is_nullable => 0 },
+  "description",
+  { data_type => "mediumtext", is_nullable => 1 },
   "item_number",
   { data_type => "integer", is_foreign_key => 1, is_nullable => 1 },
   "borrower_number",
@@ -174,8 +181,8 @@ __PACKAGE__->belongs_to(
 );
 
 
-# Created by DBIx::Class::Schema::Loader v0.07039 @ 2014-07-11 09:26:55
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:x9PaYKzfg8jT6zFP4IwhBA
+# Created by DBIx::Class::Schema::Loader v0.07042 @ 2018-10-24 17:34:29
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:5SOMkaJ6IxGtnqtPVFpNtg
 
 
 # You can replace this text with custom content, and it will be preserved on regeneration
