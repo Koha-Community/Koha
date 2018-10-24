f4b0e3a980239c97244d3bab97cc177949ddd0e7
Nick Clemens <nick@bywatersolutions.com>
2018-10-24 19:43:18 +0000

    Bug 21639: Adjust the other languages
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    (cherry picked from commit 7c25be79a00b6b7b8b2f4b38eb2dd9f3f6cb502e)
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- installer/data/mysql/uk-UA/mandatory/sample_notices_message_transports.sql
+++ installer/data/mysql/uk-UA/mandatory/sample_notices_message_transports.sql
@@ -8,19 +8,22 @@
 SET FOREIGN_KEY_CHECKS=0;
 insert into message_transports
  (message_attribute_id, message_transport_type, is_digest, letter_module, letter_code, branchcode) values
- (1,                   'email',                 '0',       'circulation', 'DUE',       ''),
- (1,                   'email',                 '1',       'circulation', 'DUEDGST',   ''),
- (2,                   'email',                 '0',       'circulation', 'PREDUE',    ''),
- (2,                   'email',                 '1',       'circulation', 'PREDUEDGST',''),
- (4,                   'email',                 '0',       'reserves',    'HOLD',      ''),
- (1,                   'sms',                   '0',       'circulation', 'DUE',       ''),
- (1,                   'sms',                   '1',       'circulation', 'DUEDGST',   ''),
- (2,                   'sms',                   '0',       'circulation', 'PREDUE',    ''),
- (2,                   'sms',                   '1',       'circulation', 'PREDUEDGST',''),
- (4,                   'sms',                   '0',       'reserves',    'HOLD',      ''),
- (5,                   'email',                 '0',       'circulation', 'CHECKIN',   ''),
- (5,                   'sms',                   '0',       'circulation', 'CHECKIN',   ''),
- (6,                   'email',                 '0',       'circulation', 'CHECKOUT',  ''),
- (6,                   'sms',                   '0',       'circulation', 'CHECKOUT',  '');
+(1, 'email', 0, 'circulation', 'DUE'),
+(1, 'email', 1, 'circulation', 'DUEDGST'),
+(1, 'sms',   0, 'circulation', 'DUE'),
+(1, 'sms',   1, 'circulation', 'DUEDGST'),
+(2, 'email', 0, 'circulation', 'PREDUE'),
+(2, 'email', 1, 'circulation', 'PREDUEDGST'),
+(2, 'sms',   0, 'circulation', 'PREDUE'),
+(2, 'sms',   1, 'circulation', 'PREDUEDGST'),
+(2, 'phone', 0, 'circulation', 'PREDUE'),
+(2, 'phone', 1, 'circulation', 'PREDUEDGST'),
+(4, 'email', 0, 'reserves',    'HOLD'),
+(4, 'sms',   0, 'reserves',    'HOLD'),
+(4, 'phone', 0, 'reserves',    'HOLD'),
+(5, 'email', 0, 'circulation', 'CHECKIN'),
+(5, 'sms',   0, 'circulation', 'CHECKIN'),
+(6, 'email', 0, 'circulation', 'CHECKOUT'),
+(6, 'sms',   0, 'circulation', 'CHECKOUT');
 SET FOREIGN_KEY_CHECKS=1;
 
