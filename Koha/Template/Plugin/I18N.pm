d1c6e70b446ff0f099f42e29352c5b718b36b0b1
Julian Maurice <julian.maurice@biblibre.com>
2018-10-25 14:06:03 +0200

    Bug 15395: Make QA test script happy
    It is not totally happy because of the template file used in tests.
    It has line breaks inside TT directives and QA test script complains
    because line breaks in TT directives confuse the current translatable
    strings extractor.
    However this patchset (along with bug 20988) will hopefully make the
    current extractor obsolete and thus make the 'no-line-breaks' rule
    useless
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>

--- Koha/Template/Plugin/I18N.pm
+++ Koha/Template/Plugin/I18N.pm
@@ -24,45 +24,113 @@ use base qw( Template::Plugin );
 use C4::Context;
 use Koha::I18N;
 
+=head1 NAME
+
+Koha::Template::Plugin::I18N - Translate strings in templates
+
+=head1 SYNOPSIS
+
+Do not use this plugin directly. Add the following directive
+
+    [% PROCESS 'i18n.inc' %]
+
+and use the macros defined here
+
+=head1 METHODS
+
+=head2 t
+
+    [% I18N.t("hello") %]
+
+=cut
+
 sub t {
     my ($self, $msgid) = @_;
     return __($msgid);
 }
+
+=head2 tx
+
+    [% I18N.tx("hello {name}", { name = name }) %]
+
+=cut
 
 sub tx {
     my ($self, $msgid, $vars) = @_;
     return __x($msgid, %$vars);
 }
 
+=head2 tn
+
+    [% I18N.tn("item", "items", count) %]
+
+=cut
+
 sub tn {
     my ($self, $msgid, $msgid_plural, $count) = @_;
     return __n($msgid, $msgid_plural, $count);
 }
+
+=head2 tnx
+
+    [% I18N.tnx("{count} item", "{count} items", count, { count = count }) %]
+
+=cut
 
 sub tnx {
     my ($self, $msgid, $msgid_plural, $count, $vars) = @_;
     return __nx($msgid, $msgid_plural, $count, %$vars);
 }
 
+=head2 txn
+
+Alias of tnx
+
+=cut
+
 sub txn {
     my ($self, $msgid, $msgid_plural, $count, $vars) = @_;
     return __xn($msgid, $msgid_plural, $count, %$vars);
 }
+
+=head2 tp
+
+    [% I18N.tp("context", "hello") %]
+
+=cut
 
 sub tp {
     my ($self, $msgctxt, $msgid) = @_;
     return __p($msgctxt, $msgid);
 }
 
+=head2 tpx
+
+    [% I18N.tpx("context", "hello {name}", { name = name }) %]
+
+=cut
+
 sub tpx {
     my ($self, $msgctxt, $msgid, $vars) = @_;
     return __px($msgctxt, $msgid, %$vars);
 }
 
+=head2 tnp
+
+    [% I18N.tnp("context", "item", "items") %]
+
+=cut
+
 sub tnp {
     my ($self, $msgctxt, $msgid, $msgid_plural, $count) = @_;
     return __np($msgctxt, $msgid, $msgid_plural, $count);
 }
+
+=head2 tnpx
+
+    [% I18N.tnpx("context", "{count} item", "{count} items", { count = count }) %]
+
+=cut
 
 sub tnpx {
     my ($self, $msgctxt, $msgid, $msgid_plural, $count, $vars) = @_;
