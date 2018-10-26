44e01fc3ca245ba2a3aefc408b9f155a66a5f289
Nick Clemens <nick@bywatersolutions.com>
2018-10-26 17:09:18 +0000

    Bug 21639: DBRev 18.05.05.003
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    (cherry picked from commit 22d817ddc863354b0a6316c6ddb91e2cc86cf603)
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -16234,6 +16234,20 @@ if( CheckVersion( $DBversion ) ) {
     print "Upgrade to $DBversion done (Bug 21617: Make statistics.ccode longer)\n";
 }
 
+$DBversion = '18.05.05.003';
+if( CheckVersion( $DBversion ) ) {
+    $dbh->do(q(
+        INSERT IGNORE INTO message_transports
+        (message_attribute_id,message_transport_type,is_digest,letter_module,letter_code)
+        VALUES
+        (2, 'phone', 0, 'circulation', 'PREDUE'),
+        (2, 'phone', 1, 'circulation', 'PREDUEDGST'),
+        (4, 'phone', 0, 'reserves',    'HOLD')
+        ));
+    SetVersion( $DBversion );
+    print "Upgrade to $DBversion done (Bug 21639 - Add phone transports by default)\n";
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 
