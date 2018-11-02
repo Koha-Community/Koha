ebf0336059635f2ea13c7f626b794e92e4980f46
Nick Clemens <nick@bywatersolutions.com>
2018-11-02 10:01:16 +0000

    Bug 21656: DBRev 18.06.00.049
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>

--- installer/data/mysql/atomicupdate/bug_21656.perl
+++
@@ -1,9 +0,0 @@
-$DBversion = 'XXX';  # will be replaced by the RM
-if( CheckVersion( $DBversion ) ) {
-    $dbh->do(q{
-        UPDATE letter SET content = REPLACE(content,"item.reason ne \'in-demand\'","item.reason != \'in-demand\'")
-        WHERE code="SR_SLIP";
-    });
-    print "Upgrade to $DBversion done (Bug 21656 - Stock Rotation Notice, Template Toolkit Syntax Correction)\n";
-    SetVersion( $DBversion );
-}
--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -16756,6 +16756,16 @@ if( CheckVersion( $DBversion ) ) {
     print "Upgrade to $DBversion done (Bug 21682 - Remove default on stockrotationrotas.description)\n";
 }
 
+$DBversion = '18.06.00.049';
+if( CheckVersion( $DBversion ) ) {
+    $dbh->do(q{
+        UPDATE letter SET content = REPLACE(content,"item.reason ne \'in-demand\'","item.reason != \'in-demand\'")
+        WHERE code="SR_SLIP";
+    });
+    print "Upgrade to $DBversion done (Bug 21656 - Stock Rotation Notice, Template Toolkit Syntax Correction)\n";
+    SetVersion( $DBversion );
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 
