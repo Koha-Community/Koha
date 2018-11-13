a28fc694fd09968cab7d2f8403e8afdfe35eac74
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2018-11-13 11:23:11 +0000

    Bug 19893: (QA follow-up) Convert atomicupdate to .perl
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>

--- installer/data/mysql/atomicupdate/bug_19893_elasticsearch_index_status_sysprefs.sql
+++
@@ -1,2 +0,0 @@
-INSERT IGNORE INTO systempreferences (variable, value, explanation, options, type) VALUES ('ElasticsearchIndexStatus_biblios', '0', 'Biblios index status', NULL, NULL);
-INSERT IGNORE INTO systempreferences (variable, value, explanation, options, type) VALUES ('ElasticsearchIndexStatus_authorities', '0', 'Authorities index status', NULL, NULL);
