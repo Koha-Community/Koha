ec1145c148aefc5ea37a9dfbb5827da14e1d839d
Fridolin Somers <fridolin.somers@biblibre.com>
2018-11-14 11:18:58 +0100

    Bug 21832: Restore is_expired in ILS-DI GetPatronInfo service
    Since Bug 17578 removed C4::Member::GetMemberDetails, the information "is_expired" disappeared from ILS-DI service GetPatronInfo.
    
    I propose to restore in by adding the code that exisited in C4::Member::GetMemberDetails directly in C4::ILSDI::Services.
    
    Test plan :
    1) Enable ILS-DI webservice
    2) Choose a patron not expired
    3) Call web-service (replace X with patron's borrowernumber) :
       /cgi-bin/koha/ilsdi.pl?service=GetPatronInfo&patron_id=X
    4) You see : <is_expired>0</is_expired>
    5) Choose a patron expired
    6) Call web-service (replace X with patron's borrowernumber) :
       /cgi-bin/koha/ilsdi.pl?service=GetPatronInfo&patron_id=X
    7) You see : <is_expired>1</is_expired>
    
    Signed-off-by: Maryse Simard <maryse.simard@inlibro.com>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Signed-off-by: Josef Moravec <josef.moravec@gmail.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    (cherry picked from commit 0748911eb26f2a35d8ba881dcb92e37a979fc9a9)
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    (cherry picked from commit 531040897b2c02114fe51a6c8de338f8bb61c3cb)
    
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

--- C4/ILSDI/Services.pm
+++ C4/ILSDI/Services.pm
@@ -482,6 +482,9 @@ sub GetPatronInfo {
         $borrower->{'attributes'} = $attrs;
     }
 
+    # Add is expired information
+    $borrower->{'is_expired'} = $patron->is_expired ? 1 : 0;
+
     return $borrower;
 }
 
