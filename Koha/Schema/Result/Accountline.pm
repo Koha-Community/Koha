4335e35f0ca5f65b340ef783bcd7d197af40e6d9
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2018-12-14 12:48:09 -0300

    Bug 21065: Add koha_object(s)_class methods to accountline
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    (cherry picked from commit 457e560a06e00d861ec88bc7354969aac7ab9099)
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    (cherry picked from commit f899e440146baadc6e268d00ef9188de8963af19)
    
    Conflicts:
    	Koha/Schema/Result/Accountline.pm
    
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

--- Koha/Schema/Result/Accountline.pm
+++ Koha/Schema/Result/Accountline.pm
@@ -240,5 +240,11 @@ __PACKAGE__->belongs_to(
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:pygYYKxFDRLX97PyeUeLvg
 
 
-# You can replace this text with custom content, and it will be preserved on regeneration
+sub koha_objects_class {
+    'Koha::Account::Lines';
+}
+sub koha_object_class {
+    'Koha::Account::Line';
+}
+
 1;
