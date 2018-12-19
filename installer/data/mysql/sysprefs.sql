274271b59d5491dc44ebe7bbfb12da13e96d2ceb
Nick Clemens <nick@bywatersolutions.com>
2018-12-19 17:53:08 +0000

    Bug 22030: Use preference to determine username sent to overdrive
    Overdrive configuration generally defaults to cardnumber, however, they
    have confirmed that some libraries use usernames. We need to allow for
    both scenarios.
    
    To test:
    1 - Have an OverDrive account setup with SIP authentication
        Note: You can apply for a testing account at developer.overdrive.com
        and setup an environment
    2 - Fill in all your OverDrive system preferences
    3 - Test with a patron whose username is their cardnumber
    4 - Confirm their overdrive account tab on opac loads
    5 - Change the username to be another value like "borked_wont_work"
    6 - Note the overdrive account tab won't load
    7 - Apply patch, update database, not new system preference
    OverDriveUsername (default to 'cardnumber)
    8 - Note the OD account loads successfully
    9 - Change the system preference to 'user name' - the account load fails
    
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    (cherry picked from commit 96adab7af8999da7f351169e1265d4be5e59742f)
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- installer/data/mysql/sysprefs.sql
+++ installer/data/mysql/sysprefs.sql
@@ -431,6 +431,7 @@ INSERT INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `
 ('OverDriveClientSecret','','Client key for OverDrive integration','30','YesNo'),
 ('OverDriveLibraryID','','Library ID for OverDrive integration','','Integer'),
 ('OverDrivePasswordRequired','',NULL,'Does the library require passwords for OverDrive SIP authentication','YesNo'),
+('OverDriveUsername','cardnumber','cardnumber|userid','Which patron information should be passed as OverDrive username','Choice'),
 ('OverdueNoticeCalendar',0,NULL,'Take the calendar into consideration when generating overdue notices','YesNo'),
 ('OverduesBlockCirc','noblock','noblock|confirmation|block','When checking out an item should overdues block checkout, generate a confirmation dialogue, or allow checkout','Choice'),
 ('OverduesBlockRenewing','allow','allow|blockitem|block','If any of patron checked out documents is late, should renewal be allowed, blocked only on overdue items or blocked on whatever checked out document','Choice'),
