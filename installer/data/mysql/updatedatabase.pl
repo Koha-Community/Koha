256253daed7af636ef229145e55e65e6d39e670e
Fridolin Somers <fridolin.somers@biblibre.com>
2018-12-21 07:50:04 +0100

    Increment version for 17.11.13 release
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -15251,6 +15251,12 @@ if( CheckVersion( $DBversion ) ) {
     print "Upgrade to $DBversion done (Bug 21730: Add new authorised value category PA_CLASS)\n";
 }
 
+$DBversion = '17.11.13.000';
+if( CheckVersion( $DBversion ) ) {
+    SetVersion( $DBversion );
+    print "Upgrade to $DBversion done (17.11.13 release)\n";
+}
+
 # DEVELOPER PROCESS, search for anything to execute in the db_update directory
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
