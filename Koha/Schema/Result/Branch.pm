92cd2f8ca6ee06a05d2b6511ad75533bbf619773
Tomas Cohen Arazi <tomascohen@theke.io>
2019-01-14 08:04:51 -0300

    Bug 22125: branches.pickup_location should be flagged as boolean
    This trivial patch just marks the mentioned column as a boolean.
    
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    
    Signed-off-by: Josef Moravec <josef.moravec@gmail.com>
    
    https://bugs.koha-community.org/show_bug.cgi?id=16497
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    (cherry picked from commit 95ce61fd3e2c7f41d14e570da3bb8bf05373c65e)
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- Koha/Schema/Result/Branch.pm
+++ Koha/Schema/Result/Branch.pm
@@ -665,8 +665,9 @@ __PACKAGE__->has_many(
 # Created by DBIx::Class::Schema::Loader v0.07042 @ 2018-10-09 15:50:42
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:0yMUX1UukdV7eMol06JXTQ
 
-
-# You can replace this text with custom code or comments, and it will be preserved on regeneration
+__PACKAGE__->add_columns(
+    '+pickup_location' => { is_boolean => 1 }
+);
 
 sub koha_objects_class {
     'Koha::Libraries';
