58497e1bc5de57fa1a9c20a7502714cc9e2bd3fd
Matthias Meusburger <matthias.meusburger@biblibre.com>
2019-01-15 10:46:04 +0100

    Bug 19661: Add and change properties and parameters to conform to the funds RFC.
    Also, perform an exact search when searching on the fund owner id.
    
    Signed-off-by: Josef Moravec <josef.moravec@gmail.com>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    (cherry picked from commit 6e98cc4503cedba411371d4005be41b5822569c8)
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- Koha/REST/V1/Acquisitions/Funds.pm
+++ Koha/REST/V1/Acquisitions/Funds.pm
@@ -20,7 +20,6 @@ use Modern::Perl;
 use Mojo::Base 'Mojolicious::Controller';
 
 use Koha::Acquisition::Funds;
-use JSON qw(to_json);
 
 use Try::Tiny;
 
@@ -45,8 +44,15 @@ sub list_funds {
     my $filter;
 
     for my $filter_param ( keys %$args ) {
-        $filter->{$filter_param} = { LIKE => $args->{$filter_param} . "%" }
-            if $args->{$filter_param};
+        if ($args->{$filter_param}) {
+            if ($filter_param eq "budget_owner_id") {
+                # Perform an exact search on the borrowernumber
+                $filter->{$filter_param} = { "=" => $args->{$filter_param} }
+            } else {
+                # And a "start with" search on the budget name
+                $filter->{$filter_param} = { LIKE => $args->{$filter_param} . "%" }
+            }
+        }
     }
 
     return try {
@@ -77,9 +83,20 @@ the attribute names the exposed REST api spec.
 sub _to_api {
     my $fund = shift;
     my $returnfund;
-    $returnfund->{id} = delete $fund->{budget_id};
+    $returnfund->{fund_id} = delete $fund->{budget_id};
     $returnfund->{code} = delete $fund->{budget_code};
     $returnfund->{name} = delete $fund->{budget_name};
+    $returnfund->{library_id} = delete $fund->{budget_branchcode};
+    $returnfund->{total_amount} = delete $fund->{budget_amount};
+    $returnfund->{warn_at_percentage} = delete $fund->{budget_encumb};
+    $returnfund->{warn_at_amount} = delete $fund->{budget_expend};
+    $returnfund->{notes} = delete $fund->{budget_notes};
+    $returnfund->{budget_id} = delete $fund->{budget_period_id};
+    $returnfund->{timestamp} = delete $fund->{timestamp};
+    $returnfund->{fund_owner_id} = delete $fund->{budget_owner_id};
+    $returnfund->{fund_access} = delete $fund->{budget_permission};
+    $returnfund->{statistic1_auth_value_category} = delete $fund->{sort1_authcat};
+    $returnfund->{statistic2_auth_value_category} = delete $fund->{sort2_authcat};
 
     return $returnfund;
 }
@@ -93,13 +110,25 @@ attribute names.
 
 sub _to_model {
     my $fund = shift;
+    my $returnfund;
 
     # Rename back
-    $fund->{budget_id}     = delete $fund->{id};
-    $fund->{budget_code}   = delete $fund->{code};
-    $fund->{budget_name}   = delete $fund->{name};
+    $returnfund->{budget_id} = delete $fund->{fund_id};
+    $returnfund->{budget_code} = delete $fund->{code};
+    $returnfund->{budget_name} = delete $fund->{name};
+    $returnfund->{budget_branchcode} = delete $fund->{library_id};
+    $returnfund->{budget_amount} = delete $fund->{total_amount};
+    $returnfund->{budget_encumb} = delete $fund->{warn_at_percentage};
+    $returnfund->{budget_expend} = delete $fund->{warn_at_amount};
+    $returnfund->{budget_notes} = delete $fund->{notes};
+    $returnfund->{budget_period_id} = delete $fund->{budget_id};
+    $returnfund->{budget_owner_id} = delete $fund->{fund_owner_id};
+    $returnfund->{timestamp} = delete $fund->{timestamp};
+    $returnfund->{budget_permission} = delete $fund->{fund_access};
+    $returnfund->{sort1_authcat} = delete $fund->{statistic1_auth_value_category};
+    $returnfund->{sort2_authcat} = delete $fund->{statistic2_auth_value_category};
 
-    return $fund;
+    return $returnfund;
 }
 
 1;
