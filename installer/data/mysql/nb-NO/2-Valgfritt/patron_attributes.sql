495619649b329ce52b86998668461ca72827c6ab
Jasmine Amohia <jasmineamohia.student@wegc.school.nz>
2019-01-17 02:32:08 +0000

    Bug 11922: Add SHOW_BCODE patron attribute for Norwegian web installer
    Test plan:
    1. Notice that /patron_attributes.sql and /patron_attributes.txt can be
    located in 2-Valgfritt
    2. Confirm that the SHOW_BCODE patron attribute in the English web
    installers .sql and .txt file is also in the Norwegian web installers
    .sql and .txt file
    
    Signed-off-by: David Nind <david@davidnind.com>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>

---
+++ installer/data/mysql/nb-NO/2-Valgfritt/patron_attributes.sql
@@ -0,0 +1,2 @@
+INSERT INTO `borrower_attribute_types` (`code`, `description`, `repeatable`, `unique_id`, `opac_display`, `staff_searchable`, `authorised_value_category`)
+VALUES ('SHOW_BCODE',  'Show barcode on the summary screen items listings', 0, 0, 1, 0, 'YES_NO');
