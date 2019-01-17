5907fd59df85479f6db8f38ec6afef2ba56dc4bd
Ethan Amohia <ethan.amohia@gmail.com>
2019-01-17 02:42:32 +0000

    Bug 11996: Made RUB default currency for ru-RU
    Didn't know how to change the uk-UA default currency.
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    (cherry picked from commit 6bb8065af69438673ca5ed0bca0664c366a87e02)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>
    (cherry picked from commit e001395c1a3afa53ea2df7cdd34738dd3ea6f0da)
    
    Signed-off-by: Andrew Fuerste-Henry <andrew@bywatersolutions.com>

--- installer/data/mysql/ru-RU/optional/sample_currency.sql
+++ installer/data/mysql/ru-RU/optional/sample_currency.sql
@@ -4,8 +4,8 @@ INSERT INTO `currency` (`currency`, `symbol`, `timestamp`, `rate`, active) VALUE
 
 INSERT INTO `currency` (`currency`, `symbol`, `timestamp`, `rate`, active) VALUES ('GPB','£','2009-01-23 08:15:20',11.317390, 0);
 
-INSERT INTO `currency` (`currency`, `symbol`, `timestamp`, `rate`, active) VALUES ('GRN','грн.','2009-01-28 00:00:00',1.0, 1);
+INSERT INTO `currency` (`currency`, `symbol`, `timestamp`, `rate`, active) VALUES ('GRN','грн.','2009-01-28 00:00:00',1.0, 0);
 
-INSERT INTO `currency` (`currency`, `symbol`, `timestamp`, `rate`, active) VALUES ('RUB','руб.','2009-01-28 00:00:00',21.166, 0);
+INSERT INTO `currency` (`currency`, `symbol`, `timestamp`, `rate`, active) VALUES ('RUB','руб.','2009-01-28 00:00:00',21.166, 1);
 
 INSERT INTO `currency` (`currency`, `symbol`, `timestamp`, `rate`, active) VALUES ('USD','$','2009-01-23 08:15:20',7.7, 0);
