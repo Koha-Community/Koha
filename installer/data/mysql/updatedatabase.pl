7b94dfaa2d82d961d4a1e48a8c82f967f75779d5
Lucas Gass <lucas@bywatersolutions.com>
2019-02-25 22:59:29 +0000

    Bug: 13515 DBRev 18.05.09.001
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -16308,6 +16308,28 @@ if ( CheckVersion($DBversion) ) {
                     SetVersion ($DBversion);
 }
 
+
+$DBversion = '18.05.09.001';
+if( CheckVersion( $DBversion ) ) {
+
+    unless( foreign_key_exists( 'messages', 'messages_borrowernumber' ) ) {
+        $dbh->do(q|
+            DELETE m FROM messages m
+            LEFT JOIN borrowers b ON m.borrowernumber=b.borrowernumber
+            WHERE b.borrowernumber IS NULL
+        |);
+
+        $dbh->do(q|
+            ALTER TABLE messages
+            ADD CONSTRAINT messages_borrowernumber
+            FOREIGN KEY (borrowernumber) REFERENCES borrowers (borrowernumber) ON DELETE CASCADE ON UPDATE CASCADE
+        |);
+    }
+
+    SetVersion( $DBversion );
+    print "Upgrade to $DBversion done (Bug 13515 - Add a FOREIGN KEY constaint on messages.borrowernumber)\n";
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 
