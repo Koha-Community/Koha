6e7dee0f1d371989c5928c3ca11527021ced67f0
Fridolin Somers <fridolin.somers@biblibre.com>
2019-03-05 15:51:15 +0100

    Bug 21560: (follow-up) move use at the top
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    (cherry picked from commit 639ad97d844f2774f49881174b559293edd6baa4)
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    (cherry picked from commit 30c9167dd48d1631f2554fa7897638ae4f4f5186)
    
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>
    (cherry picked from commit 9918ef23a39ff5f69ba60c4fcacc5b830a00773c)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- reports/guided_reports.pl
+++ reports/guided_reports.pl
@@ -36,6 +36,7 @@ use Koha::AuthorisedValues;
 use Koha::BiblioFrameworks;
 use Koha::Libraries;
 use Koha::Patron::Categories;
+use Koha::Util::OpenDocument;
 
 =head1 NAME
 
@@ -886,7 +887,6 @@ elsif ($phase eq 'Export'){
                 }
 
                 # Process
-                use Koha::Util::OpenDocument;
                 generate_ods($ods_filepath, $ods_content);
 
                 # Output
