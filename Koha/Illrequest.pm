5e20e438e80465fcd6f1c2af5e6138a639dec5f3
Andrew Isherwood <andrew.isherwood@ptfs-europe.com>
2019-03-06 14:07:50 +0000

    Bug 22464: Pass forward parameters
    This patch ensures that parameters that are present are passed forward
    when accepting the copyright notice.
    
    Test plan:
    
    - Clone the latest version of the FreeForm backend from:
      https://github.com/PTFS-Europe/koha-ill-freeform
    - Ensure the ILLModuleCopyrightClearance syspref is populated
    - Create an OpenURL in order to pre-populate the FreeForm form, a suitable URL would be:
    
    <your_koha_instance>/cgi-bin/koha/opac-illrequests.pl?openurl=true&backend=FreeForm&method=create&opac=1&atitle=Theater%20careers.%20%5Belectronic%20resource%5D%20%3A%20a%20realistic%20guide.&aulast=Donahue%2C%20Tim&date=2012&genre=article&isbn=978123123123&issn=1234-5678&issue=34&pages=12-34&sid=EBSCO%3AAthlone%20Institute%20of%20Technology%3Aait.101326&title=article%20title%20here&volume=12
    
    - Log in
    - Accept the copyright notice
    => TEST: Observe that the FreeForm form is pre-filled with the metadata
    supplied in the OpenURL
    
    Signed-off-by: Liz Rea <wizzyrea@gmail.com>
    
    Signed-off-by: Josef Moravec <josef.moravec@gmail.com>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>

--- Koha/Illrequest.pm
+++ Koha/Illrequest.pm
@@ -785,6 +785,7 @@ sub backend_create {
                 method  => 'create',
                 stage   => 'copyrightclearance',
                 value   => {
+                    other   => $params,
                     backend => $self->_backend->name
                 }
             };
