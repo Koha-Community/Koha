a2b057758d83c3530c38ced02921c82185f3251f
Nick Clemens <nick@bywatersolutions.com>
2019-03-06 12:21:38 -0500

    Bug 20128: Add permission for advanced cataloging editor
    To test:
    1 - Have a borrower with edit_catalogue permission and one with
    editcatalogue top level
    2 - Confirm they can both access the advanced editor from the
    'Cataloguing' home page or from the basic editor
    3 - Apply patch
    4 - Update database
    5 - Confirm borrower with 'edit_catalogue' has 'advanced_editor'
    permission
    6 - Confirm borrowers can access advanced editor as above
    7 - Remove 'advanced_editor' permission from borrower
    8 - Ensure they cannot access the advanced editor
    9 - Ensure links to 'Edit record' from search results go to basic editor
    
    Signed-off-by: Liz Rea <wizzyrea@gmail.com>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>

--- cataloguing/editor.pl
+++ cataloguing/editor.pl
@@ -40,7 +40,12 @@ my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
         query           => $input,
         type            => 'intranet',
         authnotrequired => 0,
-        flagsrequired   => { editcatalogue => 'edit_catalogue' },
+        flagsrequired   => {
+            editcatalogue => {
+                'edit_catalogue'  => 1,
+                'advanced_editor' => 1
+            },
+        }
     }
 );
 
