def667f7fc1b64171bec937861e34c5a94ead0d7
Josef Moravec <josef.moravec@gmail.com>
2019-04-01 15:08:26 +0000

    Bug 22615: Add /ill_backends endpoint
    Signed-off-by: Andrew Isherwood <andrew.isherwood@ptfs-europe.com>
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

---
+++ Koha/REST/V1/Illbackends.pm
@@ -0,0 +1,54 @@
+package Koha::REST::V1::Illbackends;
+
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use Modern::Perl;
+
+use Mojo::Base 'Mojolicious::Controller';
+
+use Koha::Illrequest::Config;
+use Koha::Illrequests;
+
+=head1 NAME
+
+Koha::REST::V1::Illbackends
+
+=head2 Operations
+
+=head3 list
+
+Return a list of available ILL backends and its capabilities
+
+=cut
+
+sub list {
+    my $c = shift->openapi->valid_input;
+
+    my $config = Koha::Illrequest::Config->new;
+    my $backends = $config->available_backends;
+
+    my @data;
+    foreach $b ( @$backends ) {
+        my $backend = Koha::Illrequest->new->load_backend( $b );
+        push @data, {
+            ill_backend_id => $b,
+            capabilities => $backend->capabilities,
+        };
+    }
+    return $c->render( status => 200, openapi => \@data );
+}
+
+1;
