359984abb341b6b6501cbd07a9f14da1b95c2ae4
janPasi <pasi.korkalo@ouka.fi>
2019-04-03 12:12:31 +0300

    KD-2571 (squashable) Use addSSN instead or findSSN for adding ssn-numbers
    By definition user adding ssns to ssn-service via Koha SC shouldn't
    have find permission to ssn-service. Autologin to SSN-service, however,
    was using findSSN username and password to communicate with the
    ssn-service, which was a bad idea.
    
    This commit changes the autologin to use addSSN username and password
    instead.

--- members/memberentry.pl
+++ members/memberentry.pl
@@ -787,8 +787,8 @@ $template->param(
   hetula_url => C4::Context->config('hetula')->{'url'},
   hetula_organization => C4::Context->config('hetula')->{'organization'},
   ssn_url => C4::Context->config('ssnProvider')->{'url'} . '/ssn/addssn?ajax=1', #Feature #1132, config for ssn url + KD#1844, changes in koha-conf.xml
-  ssn_user => C4::Context->config('ssnProvider')->{'findSSN'}->{'user'}, # for KD-2571 (autologin)
-  ssn_pass => C4::Context->config('ssnProvider')->{'findSSN'}->{'password'}, # for KD-2571 (autologin)
+  ssn_user => C4::Context->config('ssnProvider')->{'addSSN'}->{'user'}, # for KD-2571 (autologin)
+  ssn_pass => C4::Context->config('ssnProvider')->{'addSSN'}->{'password'}, # for KD-2571 (autologin)
   );
 
 # Generate CSRF token
