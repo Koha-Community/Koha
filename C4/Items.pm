9df6a7d14f3dd869c00e514d7c3c45238aaaccc0
Fridolin Somers <fridolin.somers@biblibre.com>
2019-04-12 11:51:52 +0200

    Bug 21975: (follow-up) avoid undef in eq
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>

--- C4/Items.pm
+++ C4/Items.pm
@@ -2643,7 +2643,7 @@ sub ToggleNewStatus {
             my $itemnumber = $values->{itemnumber};
             for my $substitution ( @$substitutions ) {
                 next unless $substitution->{field};
-                next if ( $values->{ $substitution->{item_field} } eq $substitution->{value} );
+                next if ( defined $values->{ $substitution->{item_field} } and $values->{ $substitution->{item_field} } eq $substitution->{value} );
                 C4::Items::ModItem( { $substitution->{item_field} => $substitution->{value} }, $biblionumber, $itemnumber )
                     unless $report_only;
                 push @{ $report->{$itemnumber} }, $substitution;
