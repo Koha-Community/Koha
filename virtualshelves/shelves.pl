d8e0930549e3578155acd7db533f0a42d605190a
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2019-05-02 20:49:45 -0400

    Revert "Bug 22478: (QA follow-up) Handle category in opac-shelves like a boolean"
    This reverts commit 375dd35d5354d46c4c16f534494b123ce370c3f4.
    
    https://bugs.koha-community.org/show_bug.cgi?id=22836
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    (cherry picked from commit 7bed2a3fe61ff605b636e292e73b96947de23b29)
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- virtualshelves/shelves.pl
+++ virtualshelves/shelves.pl
@@ -50,7 +50,6 @@ my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
 my $op       = $query->param('op')       || 'list';
 my $referer  = $query->param('referer')  || $op;
 my $category = $query->param('category') || 1;
-$category = 2 if $category ne "1";
 my ( $shelf, $shelfnumber, @messages );
 
 if ( $op eq 'add_form' ) {
