f4f559023970e0eba0715e4e01989d91d1a8ceff
Andrew Isherwood <andrew.isherwood@ptfs-europe.com>
2019-05-07 17:37:02 +0100

    Bug 22818: Add support for ILL notices
    Here we're just adding ILL notices to the existing "letters"
    infrastructure so notices can be displayed and edited
    
    Sponsored-by: PTFS Europe
    Signed-off-by: Niamh Walker-Headon <Niamh.Walker-Headon@it-tallaght.ie>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- C4/Letters.pm
+++ C4/Letters.pm
@@ -750,6 +750,7 @@ sub _parseletter_sth {
     ($table eq 'suggestions'  )    ? "SELECT * FROM $table WHERE   suggestionid = ?"                                  :
     ($table eq 'aqbooksellers')    ? "SELECT * FROM $table WHERE             id = ?"                                  :
     ($table eq 'aqorders'     )    ? "SELECT * FROM $table WHERE    ordernumber = ?"                                  :
+    ($table eq 'illrequests'  )    ? "SELECT * FROM $table WHERE  illrequest_id = ?"                                  :
     ($table eq 'opac_news'    )    ? "SELECT * FROM $table WHERE          idnew = ?"                                  :
     ($table eq 'article_requests') ? "SELECT * FROM $table WHERE             id = ?"                                  :
     ($table eq 'borrower_modifications') ? "SELECT * FROM $table WHERE verification_token = ?" :
