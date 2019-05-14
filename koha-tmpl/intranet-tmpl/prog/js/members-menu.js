9c411575078b529e57e98c808ab7eb2facef1e65
Owen Leonard <oleonard@myacpl.org>
2019-05-14 13:43:12 +0000

    Bug 22904: Untranslatable strings in members-menu.js
    This patch moves several English strings out of members-menu.js and into
    str/members-menu.inc where they can be picked up by the translation
    process. The following strings are affected:
    
    "Are you sure you want to renew this patron's registration?"
    "Are you sure you want to update this child to an Adult category?  This
    cannot be undone."
    "Dates of birth should be entered in the format 'MM/DD/YYYY'"
    "Dates of birth should be entered in the format 'YYYY-MM-DD'"
    "Dates of birth should be entered in the format 'DD/MM/YYYY'"
    "Dates of birth should be entered in the format 'DD.MM.YYYY'"
    
    This patch also makes a correction to members-toolbar.inc in order to
    enable the presence of the "Update child to Adult" menu item.
    
    To test:
    
     - Install and update a language.
     - Check the po-file for that language. You should find no msgid for
       the strings listed above.
     - Apply the patch.
     - Update the language you installed.
     - You should find entries for all the strings above.
     - Translate those messages and update the translated templates.
     - Reinstall the translation.
    
    In the staff client:
    
     - Select your updated translation.
     - Open an expired patron's account and choose "Renew patron" from the
       "More" button in the toolbar. You should be prompted for confirmation
       using the translation you provided.
     - To test the "Update child" confirmation you should have only one
       patron category in the "Adult" category.
       - Open a patron record with a child category and choose "Update
         child" from the "More" menu in the toolbar. You should be prompted
         for confirmation using the translation you provided.
     - To test the date format messages: From the patrons home page, expand
       the advanced search options in the header search form. Select "Date
       of birth" under "Search fields." A correctly-translated tooltip
       should appear above the search field.
    
    Signed-off-by: Michal Denar <black23@gmail.com>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    (cherry picked from commit f5894d41d21b3d57217397f7d8de500082e66157)
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- koha-tmpl/intranet-tmpl/prog/js/members-menu.js
+++ koha-tmpl/intranet-tmpl/prog/js/members-menu.js
@@ -1,4 +1,4 @@
-/* global borrowernumber advsearch dateformat _ CAN_user_borrowers_edit_borrowers number_of_adult_categories destination */
+/* global borrowernumber advsearch dateformat _ CAN_user_borrowers_edit_borrowers number_of_adult_categories destination Sticky MSG_DATE_FORMAT_US MSG_DATE_FORMAT_ISO MSG_DATE_FORMAT_METRIC MSG_DATE_FORMAT_DMYDOT MSG_CONFIRM_UPDATE_CHILD MSG_CONFIRM_RENEW_PATRON */
 
 $(document).ready(function(){
     $("#filteraction_off, #filteraction_on").on('click', function(e) {
@@ -16,13 +16,13 @@ $(document).ready(function(){
         var MSG_DATE_FORMAT = "";
         if ( $(this).val() == 'dateofbirth' ) {
             if( dateformat == 'us' ){
-                MSG_DATE_FORMAT = _("Dates of birth should be entered in the format 'MM/DD/YYYY'");
+                MSG_DATE_FORMAT = MSG_DATE_FORMAT_US;
             } else if( dateformat == 'iso' ){
-                MSG_DATE_FORMAT = _("Dates of birth should be entered in the format 'YYYY-MM-DD'");
+                MSG_DATE_FORMAT = MSG_DATE_FORMAT_ISO;
             } else if( dateformat == 'metric' ){
-                MSG_DATE_FORMAT = _("Dates of birth should be entered in the format 'DD/MM/YYYY'");
+                MSG_DATE_FORMAT = MSG_DATE_FORMAT_METRIC;
             } else if( dateformat == 'dmydot' ){
-                MSG_DATE_FORMAT = _("Dates of birth should be entered in the format 'DD.MM.YYYY'");
+                MSG_DATE_FORMAT = MSG_DATE_FORMAT_DMYDOT;
             }
             $('#searchmember').attr("title", MSG_DATE_FORMAT).tooltip('show');
         } else {
@@ -85,7 +85,7 @@ $(document).ready(function(){
 });
 
 function confirm_updatechild() {
-    var is_confirmed = window.confirm(_("Are you sure you want to update this child to an Adult category?  This cannot be undone."));
+    var is_confirmed = window.confirm( MSG_CONFIRM_UPDATE_CHILD );
     if (is_confirmed) {
         window.location='/cgi-bin/koha/members/update-child.pl?op=update&borrowernumber=' + borrowernumber;
     }
@@ -100,7 +100,7 @@ function update_child() {
 }
 
 function confirm_reregistration() {
-    var is_confirmed = window.confirm(_("Are you sure you want to renew this patron's registration?"));
+    var is_confirmed = window.confirm( MSG_CONFIRM_RENEW_PATRON );
     if (is_confirmed) {
         window.location = '/cgi-bin/koha/members/setstatus.pl?borrowernumber=' + borrowernumber + '&amp;destination=' + destination + '&amp;reregistration=y';
     }
