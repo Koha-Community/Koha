6b03674954ee409bd73c1960903c37fe12ec3967
Pasi Kallinen <pasi.kallinen@koha-suomi.fi>
2019-05-15 12:07:16 +0300

    KD-413 - Do not copy unique patron attributes to duplicated patron

--- members/memberentry.pl
+++ members/memberentry.pl
@@ -894,6 +894,7 @@ sub patron_attributes_form {
                     $newentry->{auth_val_loop} = GetAuthorisedValues($attr_type->authorised_value_category(), $attr->{value});
                 }
                 $i++;
+                undef $newentry->{value} if ($attr_type->unique_id() && $op eq 'duplicate');
                 $newentry->{form_id} = "patron_attr_$i";
                 push @{$items_by_class{$attr_type->class()}}, $newentry;
             }
