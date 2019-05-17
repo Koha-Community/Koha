4469b4e52d632f2661cc9472ef651887c91b8e36
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2019-05-17 09:06:04 -0500

    Bug 22844: Add the new DB column borrower_attribute_types.mandatory
    Signed-off-by: David Nind <david@davidnind.com>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    
    Signed-off-by: Victor Grousset/tuxayo <victor@tuxayo.net>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    https://bugs.koha-community.org/show_bug.cgi?id=23391
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- installer/data/mysql/kohastructure.sql
+++ installer/data/mysql/kohastructure.sql
@@ -229,6 +229,7 @@ CREATE TABLE `borrower_attribute_types` ( -- definitions for custom patron field
   `category_code` VARCHAR(10) NULL DEFAULT NULL,-- defines a category for an attribute_type
   `class` VARCHAR(255) NOT NULL DEFAULT '',-- defines a class for an attribute_type
   `keep_for_pseudonymization` tinyint(1) NOT NULL default 0, -- defines if this field is copied to anonymized_borrower_attributes (1 for yes, 0 for no)
+  `mandatory` tinyint(1) NOT NULL DEFAULT 0, -- defines if the attribute is mandatory or not
   PRIMARY KEY  (`code`),
   KEY `auth_val_cat_idx` (`authorised_value_category`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
