ce8169fc214f68f46c30dbd7ea3fbd3950a68bc7
Petter von Krogh <petter@libriotech.no>
2019-05-22 15:38:21 +0000

    Bug 22966: Add Norwegian library and patron names for installer
     THe Norwegian installer had reused english smaple atrons and libraries,
     this patch fixes this.
    
     test plan:
     empty database
     install norwegian translation
     run the web-installer and choose norwegian language
     select sample libraries and patrons
     make sure there are no errors
    
    Signed-off-by: Chris Cormack <chrisc@catalyst.net.nz>
    I trust Petter with Norwegian names, so I didn't spell check them :)
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- installer/data/mysql/nb-NO/2-Valgfritt/sample_libraries.sql
+++ installer/data/mysql/nb-NO/2-Valgfritt/sample_libraries.sql
@@ -1,15 +1,15 @@
 INSERT INTO branches ( `branchcode`,`branchname`,`branchaddress1`) VALUES
 
-('MPL','Midway','372 Forest Street'),
-('SPL','Springfield','Highland Boulevard'),
-('RPL','Riverside','Johnson Terrace'),
-('CPL','Centerville','Jefferson Summit'),
-('FPL','Fairview','Hickory Squere'),
-('FFL','Fairfield','River Station'),
-('IPT','Institut Protestant de Théologie',''),
-('PVL','Pleasant Valley','Meadow Grove'),
-('TPL','Troy','Valley Way'),
-('FRL','Franklin','Smith Heights'),
-('LPL','Liberty','East Hills'),
-('UPL','Union','Chestnut Hollow')
+('ENK','Enebakk',' Atomveien 1'),
+('TON','Toten','Bringsletta 2'),
+('TRE','Trengereid','Rådyrfaret 3'),
+('FIR','Firda','Fjordvegen 4'),
+('FEM','Femund','Markveien 5'),
+('SKS','Sekkestad','Ryggjakyrkja 6'),
+('SJU','Sjukeheimen',''),
+('OTT','Otta','Dalstrøket 8'),
+('NIF','Norsk interesseforening','Landskapet 9'),
+('TIG','Tigerstaden','Centralgaragen 10'),
+('ELV','Elvebyen','Drammensveien 11'),
+('TLV','Tolvskillingen','Nygårdshøyden 12')
 ;
