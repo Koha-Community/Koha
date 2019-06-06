a249ed65075958772f28b0fb7f9f0b2755e41f5d
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2019-06-06 15:32:14 +0100

    Bug 10215: DBRev 19.05.00.002
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    (cherry picked from commit c3346b50bd00778c2e8364b40a7dd7a891c713b4)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -18693,6 +18693,19 @@ if( CheckVersion( $DBversion ) ) {
     print "Upgrade to $DBversion done (Bug 22960: Fix typo in syspref description)\n";
 }
 
+$DBversion = '19.05.00.002';
+if ( CheckVersion($DBversion) ) {
+
+    $dbh->do(q{ALTER TABLE subscriptionhistory CHANGE opacnote opacnote LONGTEXT NULL});
+    $dbh->do(q{ALTER TABLE subscriptionhistory CHANGE librariannote librariannote LONGTEXT NULL});
+
+    $dbh->do(q{UPDATE subscriptionhistory SET opacnote = NULL WHERE opacnote = ''});
+    $dbh->do(q{UPDATE subscriptionhistory SET librariannote = NULL WHERE librariannote = ''});
+
+    SetVersion ($DBversion);
+    print "Upgrade to $DBversion done (Bug 10215 - Increase the size of opacnote and librariannote for table subscriptionhistory)\n";
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 
