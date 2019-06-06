33660ea283a007b668d9f230994be1869b0ac629
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2019-06-06 16:07:39 +0100

    Bug 22867: DBRev 19.06.00.003
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -18712,9 +18712,16 @@ if ( CheckVersion($DBversion) ) {
     print "Upgrade to $DBversion done (Bug 10215 - Increase the size of opacnote and librariannote for table subscriptionhistory)\n";
 }
 
+$DBversion = '19.06.00.003';
+if( CheckVersion( $DBversion ) ) {
+    $dbh->do(q{UPDATE systempreferences SET value = REPLACE( value, ' ', '|' ) WHERE variable = 'UniqueItemFields'; });
+
+    SetVersion( $DBversion );
+    print "Upgrade to $DBversion done (Bug 22867 - UniqueItemFields preference value should be pipe-delimited)\n";
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
-
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
 opendir( my $dirh, $update_dir );
 foreach my $file ( sort readdir $dirh ) {
