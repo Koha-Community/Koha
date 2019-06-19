e6e6867ed67d794d334933a13575734c72d0c58f
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2019-06-19 13:07:23 +0100

    Bug 22653: DBRev 19.06.00.007
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- installer/data/mysql/atomicupdate/bug_22653_remove_RotationPreventTransfers.perl
+++
@@ -1,6 +0,0 @@
-$DBversion = 'XXX';
-if( CheckVersion( $DBversion ) ) {
-    $dbh->do( "DELETE FROM systempreferences WHERE variable = 'RotationPreventTransfers'" );
-    SetVersion( $DBversion );
-    print "Upgrade to $DBversion done (Bug 22653 - Remove unimplemented RotationPreventTransfers system preference)\n";
-}
--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -18761,6 +18761,13 @@ if( CheckVersion( $DBversion ) ) {
     print "Upgrade to $DBversion done (Bug 21073 - Improve plugin performance)\n";
 }
 
+$DBversion = '19.06.00.007';
+if( CheckVersion( $DBversion ) ) {
+    $dbh->do( "DELETE FROM systempreferences WHERE variable = 'RotationPreventTransfers'" );
+    SetVersion( $DBversion );
+    print "Upgrade to $DBversion done (Bug 22653 - Remove unimplemented RotationPreventTransfers system preference)\n";
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
