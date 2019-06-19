502565a21cc71d13711d0194f809ac6062c9009b
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2019-06-19 14:49:15 -0500

    Bug 23150: Do not consider gdpr_proc_consent a mandatory field
    It introduced a regression in the patron's self modification feature.
    Patron's details are no longer modificable, the screen displays
    "You have not filled out all required fields. Please fill in all missing
    fields and resubmit."
    
    Test plan:
    - Set GDPR_Policy to either Enforced or Permissive
    - Set PatronSelfRegistrationBorrowerMandatoryField to empty
    - Set OPACPatronDetails to Allow
    - Log into the OPAC
    - Accept the GDPR screen
    - Go to opac-memberentry.pl, change something, submit modification
    request
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    (cherry picked from commit 3b14f7d636fea4425b2f6eab922be9aa2a9eed1d)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- opac/opac-memberentry.pl
+++ opac/opac-memberentry.pl
@@ -366,7 +366,6 @@ sub GetMandatoryFields {
       C4::Context->preference("PatronSelfRegistrationBorrowerMandatoryField");
 
     my @fields = split( /\|/, $BorrowerMandatoryField );
-    push @fields, 'gdpr_proc_consent' if C4::Context->preference('GDPR_Policy');
 
     foreach (@fields) {
         $mandatory_fields{$_} = 1;
