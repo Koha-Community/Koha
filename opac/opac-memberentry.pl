b3319eec6c3ebc4d2cef01e6bc87c1d5a2accf62
Mark Tompsett <mtompset@hotmail.com>
2019-06-19 16:29:59 +0000

    Bug 23151: Tweak to use the new database structure
    This will use changed_fields to know whether a
    borrower field is actually modified.
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    (cherry picked from commit 30fd4eb1a59fd480406261f30daa40692f16bc10)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>
    (cherry picked from commit 9ca074496d30f4164679bba2c7458ccefe4fa254)
    
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

--- opac/opac-memberentry.pl
+++ opac/opac-memberentry.pl
@@ -250,9 +250,8 @@ elsif ( $action eq 'update' ) {
     my %borrower = ParseCgiForBorrower($cgi);
     $borrower{borrowernumber} = $borrowernumber;
 
-    my %borrower_changes = DelEmptyFields(%borrower);
     my @empty_mandatory_fields =
-      CheckMandatoryFields( \%borrower_changes, $action );
+      CheckMandatoryFields( \%borrower, $action );
     my $invalidformfields = CheckForInvalidFields(\%borrower);
 
     # Send back the data to the template
@@ -273,6 +272,7 @@ elsif ( $action eq 'update' ) {
     }
     else {
         my %borrower_changes = DelUnchangedFields( $borrowernumber, %borrower );
+        $borrower_changes{'changed_fields'} = join ',', keys %borrower_changes;
         my $extended_attributes_changes = FilterUnchangedAttributes( $borrowernumber, $attributes );
 
         if ( %borrower_changes || scalar @{$extended_attributes_changes} > 0 ) {
