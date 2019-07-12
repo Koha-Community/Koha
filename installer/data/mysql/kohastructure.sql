849275341067b25bcb193c64b1ae14814bc3104d
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2019-07-12 14:08:56 +0100

    Bug 18930: (RM follow-up) Drop refund_lost_item_fee_rules
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- installer/data/mysql/kohastructure.sql
+++ installer/data/mysql/kohastructure.sql
@@ -837,17 +837,6 @@ CREATE TABLE `issuingrules` ( -- circulation and fine rules
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Table structure for table `refund_lost_item_fee_rules`
---
-
-DROP TABLE IF EXISTS `refund_lost_item_fee_rules`;
-CREATE TABLE `refund_lost_item_fee_rules` ( -- refund lost item fee rules tbale
-  `branchcode` varchar(10) NOT NULL default '', -- the branch this rule is for (branches.branchcode)
-  `refund` tinyint(1) NOT NULL default 0, -- control wether to refund lost item fees on return
-  PRIMARY KEY  (`branchcode`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
 -- Table structure for table `items`
 --
 
