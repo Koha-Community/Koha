b72d9da369efc394e4a0b53a831364cd6802301d
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2019-07-19 10:07:17 +0100

    Bug 23151: DBRev 19.05.01.001
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    (cherry picked from commit cca82cc6d04a7070688fe7a3d7d55cf385ae2502)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- installer/data/mysql/atomicupdate/bug_23151.perl
+++
@@ -1,8 +0,0 @@
-$DBversion = 'XXX';
-if ( CheckVersion( $DBversion ) ) {
-    unless ( column_exists( 'borrower_modifications', 'changed_fields' ) ) {
-        $dbh->do("ALTER TABLE borrower_modifications ADD changed_fields MEDIUMTEXT AFTER verification_token;");
-    }
-    SetVersion( $DBversion );
-    print "Upgrade to $DBversion done (Bug 23151 - Add borrower_modifications.changed_fields column)\n";
-}
--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -18720,6 +18720,15 @@ if( CheckVersion( $DBversion ) ) {
     print "Upgrade to $DBversion done (19.05.01 release)\n";
 }
 
+$DBversion = '19.05.01.001';
+if ( CheckVersion( $DBversion ) ) {
+    unless ( column_exists( 'borrower_modifications', 'changed_fields' ) ) {
+        $dbh->do("ALTER TABLE borrower_modifications ADD changed_fields MEDIUMTEXT AFTER verification_token;");
+    }
+    SetVersion( $DBversion );
+    print "Upgrade to $DBversion done (Bug 23151 - Add borrower_modifications.changed_fields column)\n";
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 
