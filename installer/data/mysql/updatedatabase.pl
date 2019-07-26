f292b4ff29a0aa1a109c3f1af9c2f5412dd1a5e8
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2019-07-26 16:21:58 +0100

    Bug 22524: DBRev 19.06.00.015
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -19038,6 +19038,14 @@ if ( CheckVersion($DBversion) ) {
     print "Upgrade to $DBversion done (Bug 11573 - Fix accounttypes for 'Rent')\n";
 }
 
+$DBversion = '19.06.00.015';
+if( CheckVersion( $DBversion ) ) {
+    $dbh->do( "UPDATE `search_field` SET `name` = 'date-time-last-modified', `label` = 'date-time-last-modified' WHERE `name` = 'date/time-last-modified'" );
+
+    SetVersion( $DBversion );
+    print "Upgrade to $DBversion done (Bug 22524 - Fix date/time-last-modified search with Elasticsearch)\n";
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
