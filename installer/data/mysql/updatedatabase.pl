3eadb87fd86b2331d4ea58aa86f1810bbf952c89
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2019-08-05 11:36:40 +0100

    Bug 23396: DBRev 19.06.00.016
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -19046,6 +19046,29 @@ if( CheckVersion( $DBversion ) ) {
     print "Upgrade to $DBversion done (Bug 22524 - Fix date/time-last-modified search with Elasticsearch)\n";
 }
 
+$DBversion = '19.06.00.016';
+if( CheckVersion( $DBversion ) ) {
+
+    $dbh->do(q|
+        INSERT IGNORE INTO keyboard_shortcuts (shortcut_name, shortcut_keys) VALUES
+            ("insert_copyright","Alt-C"),
+            ("insert_copyright_sound","Alt-P"),
+            ("insert_delimiter","Ctrl-D"),
+            ("subfield_help","Ctrl-H"),
+            ("link_authorities","Shift-Ctrl-L"),
+            ("delete_field","Ctrl-X"),
+            ("delete_subfield","Shift-Ctrl-X"),
+            ("new_line","Enter"),
+            ("line_break","Shift-Enter"),
+            ("next_position","Tab"),
+            ("prev_position","Shift-Tab"),
+            ("toggle_keyboard", "Shift-Ctrl-K")
+    ;|);
+
+    SetVersion( $DBversion );
+    print "Upgrade to $DBversion done (Bug 23396 - Fix missing keyboard_shortcuts table)\n";
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
