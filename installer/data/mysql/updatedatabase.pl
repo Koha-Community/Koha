d82e31ab13d658dae7b1385692064d570752be42
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2019-08-05 13:29:10 +0100

    Bug 22610: DBRev 19.06.017
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -19069,6 +19069,81 @@ if( CheckVersion( $DBversion ) ) {
     print "Upgrade to $DBversion done (Bug 23396 - Fix missing keyboard_shortcuts table)\n";
 }
 
+$DBversion = '19.06.00.017';
+if ( CheckVersion($DBversion) ) {
+
+    $dbh->do(qq{
+        INSERT INTO
+          authorised_values (category,authorised_value,lib)
+        VALUES
+          ('PAYMENT_TYPE','SIP00','Cash via SIP2'),
+          ('PAYMENT_TYPE','SIP01','VISA via SIP2'),
+          ('PAYMENT_TYPE','SIP02','Creditcard via SIP2')
+    });
+
+    $dbh->do(qq{
+        UPDATE
+          accountlines
+        SET
+          accounttype  = 'Pay',
+          payment_type = 'SIP00'
+        WHERE
+          accounttype = 'Pay00';
+    });
+
+    $dbh->do(qq{
+        UPDATE
+          accountlines
+        SET
+          accounttype  = 'Pay',
+          payment_type = 'SIP01'
+        WHERE
+          accounttype = 'Pay01';
+    });
+
+    $dbh->do(qq{
+        UPDATE
+          accountlines
+        SET
+          accounttype  = 'Pay',
+          payment_type = 'SIP02'
+        WHERE
+          accounttype = 'Pay02';
+    });
+
+    my $sth = $dbh->prepare( q{SELECT * FROM accountlines WHERE accounttype REGEXP '^Pay[[:digit:]]{2}$' } );
+    $sth->execute();
+    my $seen = {};
+    while (my $row = $sth->fetchrow_hashref) {
+        my $type = $row->{accounttype};
+        my $sipcode = $type;
+        $sipcode =~ s/Pay/SIP/g;
+        unless ($seen->{$sipcode}) {
+            $dbh->do(qq{
+                INSERT INTO
+                  authorised_values (category,authorised_value,lib)
+                VALUES
+                  ('PAYMENT_TYPE',"$sipcode",'Unrecognised SIP2 payment type')
+            });
+
+             $dbh->do(qq{
+                UPDATE
+                  accountlines
+                SET
+                  accounttype  = 'Pay',
+                  payment_type = "$sipcode"
+                WHERE
+                  accounttype = "$type";
+            });
+
+            $seen->{$sipcode} = 1;
+        }
+    }
+
+    SetVersion($DBversion);
+    print "Upgrade to $DBversion done (Bug 22610 - Fix accounttypes for SIP2 payments)\n";
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
