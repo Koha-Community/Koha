d48c2496514ef9ee3db67aa8d1785dfcd7fb7e68
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2019-08-08 15:30:14 +0100

    Bug 23228: DBRev 19.06.00.019
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -19220,6 +19220,25 @@ if( CheckVersion( $DBversion ) ) {
     print "Upgrade to $DBversion done (Bug 11529 - Add medium, subtitle and part information to biblio table)\n";
 }
 
+$DBversion = '19.06.00.019';
+if ( CheckVersion($DBversion) ) {
+    $dbh->do(q{
+        INSERT IGNORE INTO systempreferences (variable, value, options, explanation, type)
+        VALUES
+          (
+            'FinePaymentAutoPopup',
+            '0',
+            NULL,
+            'If enabled, automatically display a print dialog for a payment receipt when making a payment.',
+            'YesNo'
+          )
+    });
+
+    SetVersion($DBversion);
+    print
+"Upgrade to $DBversion done (Bug 23228 - Add option to automatically display payment receipt for printing after making a payment)\n";
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
