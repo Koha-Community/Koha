bb9dd99b278974bf7ca07cd00070cce5266942d6
Lucas Gass <lucas@bywatersolutions.com>
2019-08-31 03:47:42 +0000

    Bug 22524: DBRev 18.11.09.001
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -17525,6 +17525,16 @@ if ( CheckVersion($DBversion) ) {
             SetVersion ($DBversion);
 }
 
+
+$DBversion = '18.11.09.001';
+if( CheckVersion( $DBversion ) ) {
+    $dbh->do( "UPDATE `search_field` SET `name` = 'date-time-last-modified', `label` = 'date-time-last-modified' WHERE `name` = 'date/time-last-modified'" );
+
+    # Always end with this (adjust the bug info)
+    SetVersion( $DBversion );
+    print "Upgrade to $DBversion done (Bug 22524 - Fix date/time-last-modified search with Elasticsearch)\n";
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 
