16399fd2363ff4bacd2ab4ffe042d211a10e071d
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2019-09-11 08:57:42 +0100

    Bug 20691: DBRev 19.06.00.026
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -19386,6 +19386,38 @@ if( CheckVersion( $DBversion ) ) {
     print "Upgrade to $DBversion done (Bug 22996 - Add pref BarcodeSeparators)\n";
 }
 
+$DBversion = '19.06.00.026';
+if( CheckVersion( $DBversion ) ) {
+
+    unless ( column_exists( 'borrowers', 'privacy_guarantor_fines' ) ) {
+        $dbh->do(q{
+            ALTER TABLE borrowers
+                ADD privacy_guarantor_fines TINYINT(1) NOT NULL DEFAULT '0' AFTER privacy;
+        });
+    }
+
+    unless ( column_exists( 'deletedborrowers', 'privacy_guarantor_fines' ) ) {
+        $dbh->do(q{
+            ALTER TABLE deletedborrowers
+                ADD privacy_guarantor_fines TINYINT(1) NOT NULL DEFAULT '0' AFTER privacy;
+        });
+    }
+
+    $dbh->do(q{
+        INSERT IGNORE INTO systempreferences (variable, value, options, explanation, type )
+        VALUES (
+            'AllowStaffToSetFinesVisibilityForGuarantor',  '0', NULL,
+            'If enabled, library staff can set a patron''s fines to be visible to linked patrons from the opac.',  'YesNo'
+        ), (
+            'AllowPatronToSetFinesVisibilityForGuarantor',  '0', NULL,
+            'If enabled, the patron can set fines to be visible to  his or her guarantor',  'YesNo'
+        )
+    });
+
+    SetVersion( $DBversion );
+    print "Upgrade to $DBversion done (Bug 20691 - Add ability for guarantors to view guarantee's fines in OPAC)\n";
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
