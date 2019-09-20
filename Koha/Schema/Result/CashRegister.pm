306fcf6bce7ac26b498d1b2fda93760ae447dc97
Tomas Cohen Arazi <tomascohen@theke.io>
2019-09-20 15:26:08 -0300

    Bug 23321: (QA follow-up) Booleans
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- Koha/Schema/Result/CashRegister.pm
+++ Koha/Schema/Result/CashRegister.pm
@@ -148,6 +148,10 @@ __PACKAGE__->belongs_to(
 # Created by DBIx::Class::Schema::Loader v0.07046 @ 2019-07-23 13:14:31
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:TfGf0U/vWS7IviRlvDdE1w
 
+__PACKAGE__->add_columns(
+    '+archived'       => { is_boolean => 1 },
+    '+branch_default' => { is_boolean => 1 },
+);
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
 1;
