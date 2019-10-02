c5f4a558c6cfe054de3b5d624431444d2cfd2c46
Nicolas Legrand <nicolas.legrand@bulac.fr>
2019-10-02 14:09:21 +0200

    Bug 13881: Add desk management
    Add a desk and linked it to a library (branch). That's it. In the
    future it'll have super features like being able to link waiting
    reserve to a specific desk.
    
    Test plan:
    
    1. go to the administration page and notice there isn't any mention of
    desk whatsoever
    2. apply patches
    3. ./installer/data/mysql/updatedatabase.pl
    4. prove t/db_dependent/Koha/Desks.t
    5. you now have desks links in admin-home and admin-menu
    6. click the link
    7. add a desk
    8. add another one
    9. delete one
    10. you should have a fair list of all current desks defined
    11. Done
    
    Signed-off-by: Séverine QUEUNE <severine.queune@bulac.fr>
    Signed-off-by: Owen Leonard <oleonard@myacpl.org>
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

---
+++ admin/desks.pl
@@ -0,0 +1,114 @@
+#! /usr/bin/perl
+
+# Copyright (C) 2020 BULAC
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+
+use Modern::Perl;
+use CGI qw ( -utf8 );
+use C4::Context;
+use C4::Auth;
+use C4::Output;
+
+use Koha::Desks;
+
+my $input       = new CGI;
+my $searchfield = $input->param('desk_name') // q||;
+my $desk_id      = $input->param('desk_id') || '';
+my $op          = $input->param('op') || 'list';
+my @messages;
+
+my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
+    {   template_name   => "admin/desks.tt",
+        query           => $input,
+        type            => "intranet",
+        authnotrequired => 0,
+        flagsrequired   => { parameters => 'manage_desks' },
+        debug           => 1,
+    }
+);
+
+my $branches = Koha::Libraries->search( {}, { order_by => ['branchname'] } )->unblessed;
+
+if ( $op eq 'add_form' ) {
+    my $desk;
+    if ($desk_id) {
+        $desk = Koha::Desks->find($desk_id);
+    }
+
+    $template->param( desk => $desk, );
+} elsif ( $op eq 'add_validate' ) {
+    my $desk_id       = $input->param('desk_id');
+    my $desk_name    = $input->param('desk_name');
+    my $branchcode   = $input->param('branchcode');
+
+    if (Koha::Desks->find($desk_id)) {
+        my $desk = Koha::Desks->find($desk_id);
+        $desk->desk_name($desk_name);
+        $desk->branchcode($branchcode);
+        eval { $desk->store; };
+        if ($@) {
+            push @messages, { type => 'error', code => 'error_on_update' };
+        } else {
+            push @messages, { type => 'message', code => 'success_on_update' };
+        }
+    } else {
+        my $desk = Koha::Desk->new(
+            {
+                desk_id       => $desk_id,
+                desk_name    => $desk_name,
+                branchcode   => $branchcode,
+            }
+        );
+        eval { $desk->store; };
+        if ($@) {
+            push @messages, { type => 'error', code => 'error_on_insert' };
+        } else {
+            push @messages, { type => 'message', code => 'success_on_insert' };
+        }
+    }
+    $searchfield = q||;
+    $op          = 'list';
+} elsif ( $op eq 'delete_confirm' ) {
+    my $desk = Koha::Desks->find($desk_id);
+    $template->param( desk => $desk, );
+} elsif ( $op eq 'delete_confirmed' ) {
+    my $desk = Koha::Desks->find($desk_id);
+    my $deleted = eval { $desk->delete; };
+
+    if ( $@ or not $deleted ) {
+        push @messages, { type => 'error', code => 'error_on_delete' };
+    } else {
+        push @messages, { type => 'message', code => 'success_on_delete' };
+    }
+    $op = 'list';
+}
+
+if ( $op eq 'list' || ! $op) {
+    my $desks = Koha::Desks->search( { desk_name => { -like => "%$searchfield%" } } );
+    $template->param( desks => $desks, );
+}
+
+$template->param(
+    desk_id      => $desk_id,
+    searchfield => $searchfield,
+    messages    => \@messages,
+    op          => $op,
+    branches    => $branches,
+);
+
+output_html_with_http_headers $input, $cookie, $template->output;
---
+++ t/db_dependent/Koha/Desks.t
@@ -0,0 +1,60 @@
+#!/usr/bin/perl
+
+# Copyright 2015 Koha Development team
+# Copyright 2020 BULAC
+#
+# This file is part of Koha
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+use Test::More tests => 4;
+
+use Koha::Desk;
+use Koha::Desks;
+use Koha::Database;
+use Koha::Libraries;
+
+use t::lib::TestBuilder;
+
+my $schema = Koha::Database->new->schema;
+$schema->storage->txn_begin;
+
+# Add CPL if missing.
+if (not defined Koha::Libraries->find('CPL')) {
+    Koha::Library->new({ branchcode => 'CPL', branchname => 'Centerville' })->store;
+}
+
+my $builder = t::lib::TestBuilder->new;
+my $nb_of_desks = Koha::Desks->search->count;
+my $new_desk_1 = Koha::Desk->new({
+    desk_name  => 'my_desk_name_for_test_1',
+    branchcode => 'CPL',
+})->store;
+my $new_desk_2 = Koha::Desk->new({
+    desk_name  => 'my_desk_name_for_test_2',
+    branchcode => 'CPL',
+})->store;
+
+like( $new_desk_1->desk_id, qr|^\d+$|, 'Adding a new desk should have set the desk_id');
+is( Koha::Desks->search->count, $nb_of_desks + 2, 'The 2 desks should have been added' );
+
+my $retrieved_desk_1 = Koha::Desks->find( $new_desk_1->desk_id );
+is( $retrieved_desk_1->desk_name, $new_desk_1->desk_name, 'Find a desk by id should return the correct desk' );
+
+$retrieved_desk_1->delete;
+is( Koha::Desks->search->count, $nb_of_desks + 1, 'Delete should have deleted the desk' );
+
+$schema->storage->txn_rollback;
