a013f2bbdd9cbf0c560a855e66d665857f8b8db0
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2019-10-09 14:37:39 +0100

    Bug 17179: DBRev 19.06.00.039
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -19649,6 +19649,20 @@ if( CheckVersion( $DBversion ) ) {
     print "Upgrade to $DBversion done (Bug 23697 - Rename CircAutocompl system preference to PatronAutoComplete)\n";
 }
 
+$DBversion = '19.06.00.039';
+if( CheckVersion( $DBversion ) ) {
+    $dbh->do(q|
+        INSERT IGNORE INTO keyboard_shortcuts (shortcut_name, shortcut_keys) VALUES
+        ("copy_line","Ctrl-C"),
+        ("copy_subfield","Shift-Ctrl-C"),
+        ("paste_line","Ctrl-P"),
+        ("insert_line","Ctrl-I")
+        ;
+    |);
+    SetVersion( $DBversion );
+    print "Upgrade to $DBversion done (Bug 17179 - Add additional keyboard_shortcuts)\n";
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
