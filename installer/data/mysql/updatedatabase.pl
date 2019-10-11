9d9bb45865c9d6c88952f47f52f0e92ddcce0054
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2019-10-11 10:57:30 +0100

    Bug 17140: DBRev 19.06.00.040
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -19663,6 +19663,19 @@ if( CheckVersion( $DBversion ) ) {
     print "Upgrade to $DBversion done (Bug 17179 - Add additional keyboard_shortcuts)\n";
 }
 
+$DBversion = '19.06.00.040';
+if( CheckVersion( $DBversion ) ) {
+    $dbh->do(q|
+        INSERT IGNORE INTO systempreferences
+        (variable,value,explanation,options,type)
+        VALUES
+        ('RoundFinesAtPayment','0','If enabled any fines with fractions of a cent will be rounded to the nearest cent when payments are collected. e.g. 1.004 will be paid off by a 1.00 payment','0','YesNo')
+    |);
+
+    SetVersion( $DBversion );
+    print "Upgrade to $DBversion done (Bug 17140 - Add pref to allow rounding fines at payment)\n";
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
