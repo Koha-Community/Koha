a230878b6f3d3afa5d7f33c9d816bd6272c5b1a1
Owen Leonard <oleonard@myacpl.org>
2019-11-20 20:04:10 +0000

    Bug 21296: suspend hold ignores system preference on intranet
    This patch modifies the DataTable configuration for the table of holds
    which displays under the "Holds" tab on the patron's checkout and
    details pages. A variable is now defined in the JavaScript with the
    value of the "SuspendHoldsIntranet" system preference. This variable is
    passed to the DataTable configuration to determine whether the column is
    visible or not.
    
    One of the changes made in this patch moves a <script> block so that it
    appears before some of the JS includes on the page. This helps keep the
    string-defining JS together in one block.
    
    To test, apply the patch and set the "SuspendHoldsIntranet" system
    preference to "Allow."
    
     - Open the checkout page for a patron with one or more holds.
     - Under the "Holds" tab, the table of holds should have a "Suspend"
       column with controls for suspending or resuming holds.
     - Check that the same is true on the patron detail page.
     - Set the "SuspendHoldsIntranet" system preference to "Don't allow."
     - Test the checkout and detail pages again and confirm that the
       "Suspend" column does not appear.
    
    Signed-off-by: Kelly McElligott <kelly@bywatersolutions.com>
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    
    Signed-off-by: Joy Nelson <joy@bywatersolutions.com>
    (cherry picked from commit 541dfa7ddeec9fc72dd764165087dcfb601ddf07)
    
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>
    (cherry picked from commit cc87e68e0d1b90eed67ca2f614ca0f6a93b57054)
    Signed-off-by: Hayley Mapley <hayleymapley@catalyst.net.nz>

--- koha-tmpl/intranet-tmpl/prog/js/holds.js
+++ koha-tmpl/intranet-tmpl/prog/js/holds.js
@@ -146,6 +146,7 @@ $(document).ready(function() {
                     },
                     {
                         "bSortable": false,
+                        "visible": SuspendHoldsIntranet,
                         "mDataProp": function( oObj ) {
                             holds[oObj.reserve_id] = oObj; //Store holds for later use
 
