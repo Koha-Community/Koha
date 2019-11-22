f1196b9f523a097bbb00fb94179f1d1c90bac002
Lucas Gass <lucas@bywatersolutions.com>
2019-11-22 17:42:29 +0000

    Increment version for 18.11.11 release
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -17560,6 +17560,12 @@ if ( CheckVersion($DBversion) ) {
             SetVersion ($DBversion);
 }
 
+$DBversion = "18.11.11.000";
+if ( CheckVersion($DBversion) ) {
+        print "Upgrade to $DBversion done (18.11.11 release)\n";
+            SetVersion ($DBversion);
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 
