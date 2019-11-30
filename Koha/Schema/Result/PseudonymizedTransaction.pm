03334a5a901c4942ac2516c3148710206be38497
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2019-11-30 13:35:06 +0100

    Bug 24151: Add boolean flag for has_cardnumber
    Signed-off-by: Signed-off-by: Sonia Bouis <sonia.bouis@univ-lyon3.fr>
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- Koha/Schema/Result/PseudonymizedTransaction.pm
+++ Koha/Schema/Result/PseudonymizedTransaction.pm
@@ -311,6 +311,8 @@ __PACKAGE__->belongs_to(
 # Created by DBIx::Class::Schema::Loader v0.07046 @ 2020-01-11 17:12:17
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:bzHjcKCHYf+bixeA1dhKIQ
 
+__PACKAGE__->add_columns(
+    '+has_cardnumber' => { is_boolean => 1 },
+);
 
-# You can replace this text with custom code or comments, and it will be preserved on regeneration
 1;
