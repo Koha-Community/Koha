ff6951e83bd9eeb2bfd2ec4794ce6c8b3e72a33c
Nick <nick@bywatersolutions.com>
2019-12-19 03:20:36 +0000

    Bug 24272: add check_sysprefs_cache.pl
    This script ensure that no sysprefs have been changed directly in the database and/or
    that the cache has not become corrupted. We have occasionally seen this happen on production sites
    
    To test:
    1 - In the staff interface go to Administration
    2 - Search for system preference 'IntranetUserJS'
    3 - Add content to the syspref:
        console.log('Hi!');
    4 - On the command line launch mysql
        sudo koha-mysql kohadev
    5 - Alter the syspref directly
        UPDATE systempreferences SET value = "console.log('Bye!');" WHERE variable = 'IntranetUserJS';
    6 - run the script
        perl misc/maintenance/check_syspref_cache.pl
    7 - You are warned about the altered system preference
    
    Signed-off-by: David Nind <david@davidnind.com>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    (cherry picked from commit de5527f37d898e756cac4d8ed9de63c6576c1241)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>
    (cherry picked from commit 5a1e18efd87c34ef9f158c4a6f663d30ab3a4e43)
    
    Signed-off-by: Andrew Fuerste-Henry <andrew@bywatersolutions.com>

---
+++ misc/maintenance/check_syspref_cache.pl
@@ -0,0 +1,48 @@
+#! /usr/bin/perl
+
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+use Koha::Script;
+use Koha::Caches;
+use Koha::Config::SysPrefs;
+use C4::Context;
+
+=head1 NAME
+
+check_syspref_cache.pl
+
+=head1 SYNOPSIS
+
+    perl check_syspref_cache.pl
+
+=head1 DESCRIPTION
+
+Catch data inconsistencies in cached sysprefs vs those in the database
+
+=cut
+
+
+my $syspref_cache = Koha::Caches->get_instance('syspref');
+my $prefs = Koha::Config::SysPrefs->search();
+
+while  (my $pref = $prefs->next) {
+    my $var = lc $pref->variable;
+    my $cached_var = $syspref_cache->get_from_cache("syspref_$var");
+    next unless $cached_var;
+    print "$var: value in cache is $cached_var and value in db is ".$pref->value,"\n" unless $cached_var eq $pref->value;
+}
