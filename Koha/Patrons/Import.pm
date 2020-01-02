6d456df81a03096342dd72df99284e23603a7bbc
Kyle M Hall <kyle@bywatersolutions.com>
2020-01-02 10:30:54 -0500

    Bug 24330: When importing patrons from CSV, automatically strip BOM from file if it exists
    We have a partner that exports UTF-8 CSV files, and is experiencing the same thing as the author of this article: https://www.freecodecamp.org/news/a-quick-tale-about-feff-the-invisible-character-cd25cd4630e7/
    
    In short, Excel is inserting an invisible UTF-8 character at the start of the file, so that the column name "cardnumber" is actually named "\x{feff}cardnumber", causing "cardnumber" to be blank.
    
    A simple solution is provided here: https://stackoverflow.com/questions/24390034/remove-bom-from-string-with-perl
    
    Test Plan:
    1) Download the example.csv file
    2) Attempt to import it using the patron import tool
    3) Note the invalid column name error
    4) Apply this patch, restart all the things!
    5) Attempt the import again
    6) Assuming you have a branchcode MPL and a cataegory code S, the patron should import!
    
    Signed-off-by: David Nind <david@davidnind.com>
    Signed-off-by: Maggie Wong <maggie.wong@yccece.edu.hk>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- Koha/Patrons/Import.pm
+++ Koha/Patrons/Import.pm
@@ -404,6 +404,7 @@ sub prepare_columns {
     foreach my $keycol (@csvcolumns) {
         # columnkeys don't contain whitespace, but some stupid tools add it
         $keycol =~ s/ +//g;
+        $keycol =~ s/^\N{BOM}//; # Strip BOM if exists, otherwise it will be part of first column key
         $params->{keycol}->{$keycol} = $col++;
     }
 
