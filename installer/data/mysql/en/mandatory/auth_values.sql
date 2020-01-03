9c8a182c024600cae9c9664304cdb1120d8d8139
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2020-01-03 21:50:51 +0000

    Bug 24339: Add SIP2 payment types for installation
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    
    Signed-off-by: Joy Nelson <joy@bywatersolutions.com>

--- installer/data/mysql/en/mandatory/auth_values.sql
+++ installer/data/mysql/en/mandatory/auth_values.sql
@@ -1,4 +1,7 @@
 INSERT INTO authorised_values (category,authorised_value,lib,lib_opac) VALUES ('YES_NO','0','No','No');
 INSERT INTO authorised_values (category,authorised_value,lib,lib_opac) VALUES ('YES_NO','1','Yes','Yes');
 INSERT INTO authorised_values (category,authorised_value,lib,lib_opac) VALUES ('PAYMENT_TYPE','CASH','Cash','Cash');
+INSERT INTO authorised_values (category,authorised_value,lib,lib_opac) VALUES ('PAYMENT_TYPE','SIP00','Cash via SIP2','Cash');
+INSERT INTO authorised_values (category,authorised_value,lib,lib_opac) VALUES ('PAYMENT_TYPE','SIP01','Visa via SIP2','Visa');
+INSERT INTO authorised_values (category,authorised_value,lib,lib_opac) VALUES ('PAYMENT_TYPE','SIP02','Creditcard via SIP2','Creditcard');
 INSERT INTO authorised_values (category, authorised_value, lib) VALUES ('HSBND_FREQ','EW','Every week');
