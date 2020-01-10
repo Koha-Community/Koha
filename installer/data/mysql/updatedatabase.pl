d3bdb77d9a3be685fea2164da808d5ef963c114a
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2020-01-10 08:43:30 +0000

    Bug 23442: DBRev 19.12.00.007
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -20323,6 +20323,33 @@ if( CheckVersion( $DBversion ) ) {
     print "Upgrade to $DBversion done (Bug 24263 - Replace relationship with NULL when empty string)\n";
 }
 
+$DBversion = '19.12.00.007';
+if ( CheckVersion($DBversion) ) {
+
+    $dbh->do(
+        qq{
+            INSERT IGNORE INTO account_credit_types (code, description, can_be_added_manually, is_system)
+            VALUES
+              ('REFUND', 'A refund applied to a patrons fine', 0, 1)
+        }
+    );
+
+    $dbh->do(qq{
+        INSERT IGNORE INTO account_offset_types ( type ) VALUES ('REFUND');
+    });
+
+    $dbh->do(qq{
+        INSERT IGNORE permissions (module_bit, code, description)
+        VALUES
+        (10, 'refund', 'Perform account refund action')
+    });
+
+    SetVersion($DBversion);
+    print "Upgrade to $DBversion done (Bug 23442 - Add REFUND to account_credit_types)\n";
+    print "Upgrade to $DBversion done (Bug 23442 - Add REFUND to account_offset_types)\n";
+    print "Upgrade to $DBversion done (Bug 23442 - Add accounts refund permission)\n";
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
