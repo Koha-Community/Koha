462db680242b4a6cbfb82b3469ebec8912e69af3
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2020-01-14 10:02:11 +0100

    Bug 22868: Move suggestions_manage subperm out of acquisition perm
    Bug 11911 replaced the permission of suggestions.pl (create a purchase
    suggestion) from catalogue => 1 to acquisition => 'suggestions_manage'.
    However we have a lot of acquisition scripts that have lax permissions
    (acquisition => '*' which means any sub permissions of acquisition is
    enough).
    
    That causes problem when a circulation staff can create purchase
    suggestions but not access acquisition information.
    
    One solution is to move the suggestions_manage subpermission out of the
    acquisition permission and create a new suggestion permission.
    
    Test plan:
    0. Setup
    * Create a patron with several permission (and full acquisition
    permission)
    * Create another patron with several permission, and suggestions_manage
    permission
    * Create another patron without the suggestions_manage permission
    1. Apply the patch and execute the update database entry
    2. Note that the third patron you create still does not have
    suggestions_manage
    3. Confirm that you can create a purchase suggestion if you have
    suggestions_manage, but cannot access acquisition pages if you do not
    have any subpermissions of the acquisition permission
    
    Signed-off-by: Hayley Mapley <hayleymapley@catalyst.net.nz>
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    
    Signed-off-by: Joy Nelson <joy@bywatersolutions.com>

--- members/purchase-suggestions.pl
+++ members/purchase-suggestions.pl
@@ -34,7 +34,7 @@ my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
         query           => $input,
         type            => "intranet",
         authnotrequired => 0,
-        flagsrequired   => { acquisition => 'suggestions_manage' },
+        flagsrequired   => { suggestions => 'suggestions_manage' },
         debug           => 1,
     }
 );
