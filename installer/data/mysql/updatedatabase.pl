d6f70a76192182f30627e86405eca69655ec6397
Joy Nelson <joy@bywatersolutions.com>
2020-01-15 00:18:59 +0000

    Bug 23233: DBRev 19.11.01.001
    Signed-off-by: Joy Nelson <joy@bywatersolutions.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -20245,6 +20245,13 @@ if ( CheckVersion($DBversion) ) {
     SetVersion ($DBversion);
 }
 
+$DBversion = '19.11.01.001';
+if( CheckVersion( $DBversion ) ) {
+    $dbh->do("UPDATE systempreferences SET variable = 'AllowItemsOnHoldCheckoutSIP', explanation = 'Do not generate RESERVE_WAITING and RESERVED warning when checking out items reserved to someone else via SIP. This allows self checkouts for those items.' WHERE variable = 'AllowItemsOnHoldCheckout';" );
+    SetVersion( $DBversion );
+    print "Upgrade to $DBversion done (Bug 23233 - Rename AllowItemsOnHoldCheckout syspref)\n";
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
