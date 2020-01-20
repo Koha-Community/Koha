0d103621ada02496c1aa2ee4e6300b683b5a105f
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2020-01-20 13:56:04 +0000

    Bug 24289: DBRev 19.12.00.011
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- Koha/Schema/Result/SpecialHoliday.pm
+++ Koha/Schema/Result/SpecialHoliday.pm
@@ -32,7 +32,7 @@ __PACKAGE__->table("special_holidays");
 =head2 branchcode
 
   data_type: 'varchar'
-  default_value: (empty string)
+  is_foreign_key: 1
   is_nullable: 0
   size: 10
 
@@ -78,7 +78,7 @@ __PACKAGE__->add_columns(
   "id",
   { data_type => "integer", is_auto_increment => 1, is_nullable => 0 },
   "branchcode",
-  { data_type => "varchar", default_value => "", is_nullable => 0, size => 10 },
+  { data_type => "varchar", is_foreign_key => 1, is_nullable => 0, size => 10 },
   "day",
   { data_type => "smallint", default_value => 0, is_nullable => 0 },
   "month",
@@ -105,9 +105,26 @@ __PACKAGE__->add_columns(
 
 __PACKAGE__->set_primary_key("id");
 
+=head1 RELATIONS
 
-# Created by DBIx::Class::Schema::Loader v0.07042 @ 2018-02-16 17:54:54
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:9Qc6VbOUqX83upThUC//oA
+=head2 branchcode
+
+Type: belongs_to
+
+Related object: L<Koha::Schema::Result::Branch>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "branchcode",
+  "Koha::Schema::Result::Branch",
+  { branchcode => "branchcode" },
+  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
+);
+
+
+# Created by DBIx::Class::Schema::Loader v0.07046 @ 2020-01-20 13:57:27
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:cj8Oveni0l6kAR/azmw4SA
 
 
 # You can replace this text with custom content, and it will be preserved on regeneration
