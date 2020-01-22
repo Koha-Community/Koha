49bd9bae3e0ac6ab0488f25e75d4eea416676e58
Amit Gupta <amit.gupta@informaticsglobal.com>
2020-01-22 21:37:22 +0530

    Bug 22990: Add CSRF protection to boraccount, pay and suggestion
    Signed-off-by: David Cook <dcook@prosentient.com.au>
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Test plan would have been nioe.
    Tested by changing MAX_AGE with suggestions.
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    (cherry picked from commit 833d1dc8b082cc742b88e358edef77960b5ffc2f)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- suggestion/suggestion.pl
+++ suggestion/suggestion.pl
@@ -21,7 +21,7 @@ use Modern::Perl;
 require Exporter;
 use CGI qw ( -utf8 );
 use C4::Auth qw( get_template_and_user );
-use C4::Output qw( output_html_with_http_headers );
+use C4::Output qw( output_html_with_http_headers output_and_exit_if_error );
 use C4::Suggestions;
 use C4::Koha qw( GetAuthorisedValues );
 use C4::Budgets qw( GetBudget GetBudgets GetBudgetHierarchy CanUserUseBudget );
@@ -134,6 +134,7 @@ my $branchfilter = $input->param('branchcode') || C4::Context->userenv->{'branch
 ##
 
 if ( $op =~ /save/i ) {
+    output_and_exit_if_error($input, $cookie, $template, { check => 'csrf_token' });
     my @messages;
     my $biblio = MarcRecordFromNewSuggestion({
             title => $suggestion_only->{title},
@@ -251,7 +252,8 @@ elsif ($op=~/add/) {
     $op ='save';
 }
 elsif ($op=~/edit/) {
-    #Edit suggestion  
+    #Edit suggestion
+    output_and_exit_if_error($input, $cookie, $template, { check => 'csrf_token' });
     $suggestion_ref=&GetSuggestion($$suggestion_ref{'suggestionid'});
     $suggestion_ref->{reasonsloop} = $reasonsloop;
     my $other_reason = 1;
@@ -266,7 +268,7 @@ elsif ($op=~/edit/) {
     $op ='save';
 }  
 elsif ($op eq "update_status" ) {
-
+    output_and_exit_if_error($input, $cookie, $template, { check => 'csrf_token' });
     my $suggestion;
     # set accepted/rejected/managed informations if applicable
     # ie= if the librarian has chosen some action on the suggestions
@@ -303,6 +305,7 @@ elsif ($op eq "update_status" ) {
     }
     redirect_with_params($input);
 }elsif ($op eq "delete" ) {
+    output_and_exit_if_error($input, $cookie, $template, { check => 'csrf_token' });
     foreach my $delete_field (@editsuggestions) {
         &DelSuggestion( $borrowernumber, $delete_field,'intranet' );
     }
