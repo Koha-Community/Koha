122945827d6d958e533b913e10f47c13e3c34f65
Joy Nelson <joy@bywatersolutions.com>
2020-02-06 21:33:28 +0000

    Bug 24338: DBREV 19.11.02.002
    Signed-off-by: Joy Nelson <joy@bywatersolutions.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -20281,6 +20281,22 @@ if( CheckVersion( $DBversion ) ) {
     print "Upgrade to $DBversion done (Bug 24206 - Update OpacSearchForTitleIn system preference)\n";
 }
 
+$DBversion = '19.11.02.002';
+if( CheckVersion( $DBversion ) ) {
+    my $exists;
+    eval { $exists = $dbh->do("SELECT 1 FROM authorised_values WHERE category = 'PAYMENT_TYPE' AND authorised_value = 'CASH'"); };
+    if ( $exists == 0 ) {
+       $dbh->do(qq{
+           INSERT INTO
+             authorised_values (category,authorised_value,lib, lib_opac)
+           VALUES
+             ('PAYMENT_TYPE','CASH','Cash','Cash')
+       });
+    }
+    SetVersion( $DBversion );
+    print "Upgrade to $DBversion done (Bug 24338 - Add 'CASH' to default 'PAYMENT_TYPE')\n";
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
