cea8fccd11b822874374e9c15f0f6691c9850787
Hayley Mapley <hayleymapley@catalyst.net.nz>
2020-02-18 10:19:08 +1300

    Bug 22868: DBRev 18.11.14.001
    Signed-off-by: Hayley Mapley <hayleymapley@catalyst.net.nz>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -17578,6 +17578,26 @@ if ( CheckVersion($DBversion) ) {
             SetVersion ($DBversion);
 }
 
+$DBversion = "18.11.14.001";
+if( CheckVersion( $DBversion ) ) {
+    $dbh->do(q{
+        INSERT IGNORE INTO `userflags` (`bit`, `flag`, `flagdesc`, `defaulton`)
+        VALUES (12, 'suggestions', 'Suggestion management', 0)
+    });
+
+    $dbh->do(q{
+        UPDATE permissions SET module_bit=12
+        WHERE code="suggestions_manage"
+    });
+
+    $dbh->do(q{
+        UPDATE borrowers SET flags = flags + (1<<12) WHERE flags & (1 << 11)
+    });
+
+    SetVersion( $DBversion );
+    print "Upgrade to $DBversion done (Bug 22868 - Move suggestions_manage subpermission out of acquisition permission)\n";
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 
