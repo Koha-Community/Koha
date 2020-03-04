95b1b6662deabfe6062e79b7d318f96da448d506
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2020-03-04 16:05:28 +0000

    Bug 24082: DBRev 19.12.00.035
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -21004,7 +21004,7 @@ if ( CheckVersion($DBversion) ) {
     print "Upgrade to $DBversion done (Bug 23355 - Add cash register cashup permissions)\n";
 }
 
-$DBversion = '19.12.00.033';
+$DBversion = '19.12.00.034';
 if ( CheckVersion($DBversion) ) {
 
     $dbh->do(
@@ -21031,6 +21031,19 @@ if ( CheckVersion($DBversion) ) {
 
     SetVersion($DBversion);
     print "Upgrade to $DBversion done (Bug 24081 - Add DISCOUNT to account_credit_types and account_offset_types, Add accounts discount permission)\n";
+}
+
+$DBversion = '19.12.00.035';
+if ( CheckVersion($DBversion) ) {
+
+    $dbh->do(qq{
+        INSERT IGNORE permissions (module_bit, code, description)
+        VALUES
+        (25, 'anonymous_refund', 'Perform refund actions from cash registers')
+    });
+
+    SetVersion($DBversion);
+    print "Upgrade to $DBversion done (Bug 23442 - Add a refund option to the point of sale system)\n";
 }
 
 # SEE bug 13068
