fa580344912781d8b010f81fab8e1e4298c493e7
Matthias Meusburger <matthias.meusburger@biblibre.com>
2020-03-05 16:41:25 +0100

    Bug 24819: Allow the librarian to choose a patron when entering a purchase suggestion
    Sometimes librarians are creating purchase suggestions that came from patrons
    which didn't use the opac (but sent an email, or told the librarian verbally...)
    
    This patch allows the librarian to change the creator of the purchase suggestion
    when entering it.
    
    This way, the patron will be able to receive notifications during the purchase
    suggestion workflow.
    
    Test plan:
    
     - Apply the patch
    
     - Check that you can change the default creator of the purchase suggestion when
       creating a new suggestion by clicking on 'Set to patron'
       (Home > Acquisitions > Suggestions management > New purchase suggestion)
    
     - Check that you can also change the creator of the purchase suggestion when
       editing an existing suggestion
    
    Signed-off-by: Owen Leonard <oleonard@myacpl.org>
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

---
+++ koha-tmpl/intranet-tmpl/prog/js/suggestions.js
@@ -0,0 +1,17 @@
+function select_user(borrowernumber, borrower) {
+    var suggested = '<input type="hidden" id="suggestedby" name="suggestedby" value="' + borrowernumber + '" />';
+    suggested += '<a href="/cgi-bin/koha/members/moremember.pl?borrowernumber=' + borrowernumber + '">';
+    suggested += borrower.surname + ', ' + borrower.firstname + ' (' + borrower.cardnumber + ')';
+    suggested += '</a> ';
+    suggested += borrower.branchname + ' (' + borrower.category_description + ')';
+    $("#tdsuggestedby").html(suggested);
+    return 0;
+}
+
+$(document).ready(function(){
+    $('body').on('click', '#suggestion_search', function(e) {
+        e.preventDefault();
+        var newin = window.open('suggestion_search.pl','popup','width=600,height=400,resizable=no,toolbar=false,scrollbars=yes,top');
+    });
+
+});
