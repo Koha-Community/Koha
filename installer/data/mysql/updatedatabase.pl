aad473ff26ffb3e5bef6f3330c828a29690d8a2b
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2020-03-10 15:19:34 +0000

    Bug 17374: DBRev 19.12.040
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -21092,6 +21092,14 @@ if( CheckVersion( $DBversion ) ) {
     print "Upgrade to $DBversion done (Bug 17845 - Drop unused table printers and branchprinter column)\n";
 }
 
+$DBversion = '19.12.00.040';
+if( CheckVersion( $DBversion ) ) {
+    $dbh->do( "UPDATE systempreferences SET  explanation = 'Comma separated list defining the default fields to be used during a patron search using the \"standard\" option. If empty Koha will default to \"surname,firstname,othernames,cardnumber,userid\". Additional fields added to this preference will be added as search options in the dropdown menu on the patron search page.' WHERE variable='DefaultPatronSearchFields' " );
+
+    SetVersion( $DBversion );
+    print "Upgrade to $DBversion done (Bug 17374 - update description of DefaultPatronSearchFields)\n";
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
