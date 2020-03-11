185adc167433133626710edd296555db2d27865c
Aleisha Amohia <aleishaamohia@hotmail.com>
2020-03-11 01:52:27 +0000

    Bug 4461: Manage problem reports on the staff client
    Test plan:
    
    - Update database and upgrade schema files (if you haven't already).
    Restart memcached
    - Check your user's permissions and ensure the 'problem_reports'
    permission is ticked. Confirm the OPACReportProblem syspref is enabled
    - Log into the OPAC and submit a problem report
    - Log into the staff client
    - You should see a box at the bottom of the main page showing your
    pending problem report
    - Click the link and confirm it takes you to the new page for managing
    problem reports
    - Go to Administration
    - Confirm you can see a link to 'OPAC problem reports' under the
    'Additional parameters' heading
    - Click 'OPAC problem reports'
    - Confirm your problem report is showing in the table
    - Open the OPAC in another tab and submit at least two more problem
    reports (so you should have at least three in the table after
    refreshing)
    - Try the different buttons
        - selecting multiple problem reports and using the big 'mark
        viewed', 'mark closed', 'mark new' buttons. Confirm there are no
        failures and that the number of selected problem reports is correct
        - select all, clear all, hide viewed, hide closed, hide new, show
        all
        - individual 'mark viewed', 'mark closed', 'mark new' buttons for
        each problem report. Confirm the status shows and the correct button
        is disabled while others are enabled
    - Confirm the problem page link works as expected
    
    Sponsored-by: Catalyst IT
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: David Nind <david@davidnind.com>
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

---
+++ admin/problem-reports.pl
@@ -0,0 +1,73 @@
+#!/usr/bin/perl
+
+# Copyright 2020 Aleisha Amohia <aleisha@catalyst.net.nz>
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+use CGI qw ( -utf8 );
+use C4::Context;
+use C4::Output;
+use C4::Auth;
+use Koha::ProblemReports;
+
+my $query = new CGI;
+
+my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
+    {
+        template_name   => "admin/problem-reports.tt",
+        query           => $query,
+        type            => "intranet",
+        authnotrequired => 0,
+        flagsrequired   => { problem_reports => 1 },
+    }
+);
+
+my $action;
+foreach (qw( viewed closed new )) {
+    $action = $_ if ( $query->param("mark_selected-$_") );
+}
+$action ||= 'none';
+
+my @report_ids = $query->multi_param('report_ids');
+
+if ( $action eq 'viewed' ) {
+    foreach my $report_id ( @report_ids ) {
+        my $report = Koha::ProblemReports->find($report_id);
+        $report->set({ status => 'V' })->store;
+                                }
+} elsif ( $action eq 'closed' ) {
+    foreach my $report_id ( @report_ids ) {
+        my $report = Koha::ProblemReports->find($report_id);
+        $report->set({ status => 'C' })->store;
+    }
+
+} elsif ( $action eq 'new' ) {
+    foreach my $report_id ( @report_ids ) {
+        my $report = Koha::ProblemReports->find($report_id);
+        $report->set({ status => 'N' })->store;
+    }
+}
+
+my $problem_reports = Koha::ProblemReports->search();
+$template->param(
+    selected_count  => scalar(@report_ids),
+    action          => $action,
+    problem_reports => $problem_reports,
+);
+
+output_html_with_http_headers $query, $cookie, $template->output;
