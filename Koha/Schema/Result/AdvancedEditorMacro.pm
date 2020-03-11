b76fb4a6c5b6eec9d9ed7a6656bca28e013b6ca9
Nick Clemens <nick@bywatersolutions.com>
2020-03-11 09:39:25 -0400

    Bug 17268: Set boolean for shared column in schema
    Signed-off-by: Andrew Fuerste-Henry <andrew@bywatersolutions.com>
    Signed-off-by: Heather Hernandez <Heather_Hernandez@nps.gov>
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- Koha/Schema/Result/AdvancedEditorMacro.pm
+++ Koha/Schema/Result/AdvancedEditorMacro.pm
@@ -105,6 +105,9 @@ __PACKAGE__->belongs_to(
 # Created by DBIx::Class::Schema::Loader v0.07046 @ 2020-01-02 15:01:04
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:Wx556VlgXYqdP2XyQ87Jzg
 
+__PACKAGE__->add_columns(
+            '+shared' => { is_boolean => 1 },
+        );
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
 1;
