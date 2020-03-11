1270e14df5f4ea1c9a3d553c35af20c735a6508e
Tomas Cohen Arazi <tomascohen@theke.io>
2020-03-11 17:10:10 -0300

    Bug 20212: Clean parcel.pl
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    Signed-off-by: Séverine QUEUNE <severine.queune@bulac.fr>
    Signed-off-by: Andrew Fuerste-Henry <andrew@bywatersolutions.com>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Andrew Fuerste-Henry <andrew@bywatersolutions.com>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- acqui/parcel.pl
+++ acqui/parcel.pl
@@ -180,105 +180,6 @@ for my $order ( @orders ) {
 }
 push @book_foot_loop, map { $_ } values %foot;
 
-my @loop_orders = ();
-unless( defined $invoice->{closedate} ) {
-    my $pendingorders;
-    if ( $op eq "search" or $sticky_filters ) {
-        my ( $search, $ean, $basketname, $orderno, $basketgroupname );
-        if ( $sticky_filters ) {
-            $search = $input->cookie("filter_parcel_summary");
-            $ean = $input->cookie("filter_parcel_ean");
-            $basketname = $input->cookie("filter_parcel_basketname");
-            $orderno = $input->cookie("filter_parcel_orderno");
-            $basketgroupname = $input->cookie("filter_parcel_basketgroupname");
-        } else {
-            $search   = $input->param('summaryfilter') || '';
-            $ean      = $input->param('eanfilter') || '';
-            $basketname = $input->param('basketfilter') || '';
-            $orderno  = $input->param('orderfilter') || '';
-            $basketgroupname = $input->param('basketgroupnamefilter') || '';
-        }
-        $pendingorders = SearchOrders({
-            booksellerid => $booksellerid,
-            basketname => $basketname,
-            ordernumber => $orderno,
-            search => $search,
-            ean => $ean,
-            basketgroupname => $basketgroupname,
-            pending => 1,
-            ordered => 1,
-        });
-        $template->param(
-            summaryfilter => $search,
-            eanfilter => $ean,
-            basketfilter => $basketname,
-            orderfilter => $orderno,
-            basketgroupnamefilter => $basketgroupname,
-        );
-    }else{
-        $pendingorders = SearchOrders({
-            booksellerid => $booksellerid,
-            ordered => 1
-        });
-    }
-    my $countpendings = scalar @$pendingorders;
-
-    for (my $i = 0 ; $i < $countpendings ; $i++) {
-        my $order = $pendingorders->[$i];
-
-        if ( $bookseller->invoiceincgst ) {
-            $order->{ecost} = $order->{ecost_tax_included};
-        } else {
-            $order->{ecost} = $order->{ecost_tax_excluded};
-        }
-        $order->{total} = get_rounded_price($order->{ecost}) * $order->{quantity};
-
-        my %line = %$order;
-
-        $line{invoice} = $invoice;
-        $line{booksellerid} = $booksellerid;
-
-        my $biblionumber = $line{'biblionumber'};
-        my $biblio = Koha::Biblios->find( $biblionumber );
-        my $countbiblio = $biblio ? $biblio->active_orders->count : 0;
-        my $ordernumber = $line{'ordernumber'};
-        my $order_object = Koha::Acquisition::Orders->find($ordernumber);
-        my $cnt_subscriptions = $biblio ? $biblio->subscriptions->count: 0;
-        my $itemcount   = $biblio ? $biblio->items->count : 0;
-        my $holds_count = $biblio ? $biblio->holds->count : 0;
-        my @itemnumbers = $order_object->items->get_column('itemnumber');
-        my $itemholds = $biblio ? $biblio->holds->search({ itemnumber => { -in => \@itemnumbers } })->count : 0;
-
-        my $suggestion   = GetSuggestionInfoFromBiblionumber($line{biblionumber});
-        $line{suggestionid}         = $suggestion->{suggestionid};
-        $line{surnamesuggestedby}   = $suggestion->{surnamesuggestedby};
-        $line{firstnamesuggestedby} = $suggestion->{firstnamesuggestedby};
-
-        # if the biblio is not in other orders and if there is no items elsewhere and no subscriptions and no holds we can then show the link "Delete order and Biblio" see bug 5680
-        $line{can_del_bib}          = 1 if $countbiblio <= 1 && $itemcount == scalar @itemnumbers && !($cnt_subscriptions) && !($holds_count);
-        $line{items}                = ($itemcount) - (scalar @itemnumbers);
-        $line{left_item}            = 1 if $line{items} >= 1;
-        $line{left_biblio}          = 1 if $countbiblio > 1;
-        $line{biblios}              = $countbiblio - 1;
-        $line{left_subscription}    = 1 if $cnt_subscriptions;
-        $line{subscriptions}        = $cnt_subscriptions;
-        $line{left_holds}           = ($holds_count >= 1) ? 1 : 0;
-        $line{left_holds_on_order}  = 1 if $line{left_holds}==1 && ($line{items} == 0 || $itemholds );
-        $line{holds}                = $holds_count;
-        $line{holds_on_order}       = $itemholds?$itemholds:$holds_count if $line{left_holds_on_order};
-        $line{basket}               = Koha::Acquisition::Baskets->find( $line{basketno} );
-
-        my $budget_name = GetBudgetName( $line{budget_id} );
-        $line{budget_name} = $budget_name;
-
-        push @loop_orders, \%line;
-    }
-
-    $template->param(
-        loop_orders  => \@loop_orders,
-    );
-}
-
 $template->param(
     invoiceid             => $invoice->{invoiceid},
     invoice               => $invoice->{invoicenumber},
@@ -287,7 +188,6 @@ $template->param(
     name                  => $bookseller->name,
     booksellerid          => $bookseller->id,
     loop_received         => \@loop_received,
-    loop_orders           => \@loop_orders,
     book_foot_loop        => \@book_foot_loop,
     (uc(C4::Context->preference("marcflavour"))) => 1,
     total_tax_excluded    => $total_tax_excluded,
