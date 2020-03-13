48f4383bda8c9236ea130280cfb5e5cbf766a7e5
Joy Nelson <joy@bywatersolutions.com>
2020-03-13 00:29:45 +0000

    Bug 21633: DBRev 19.11.03.003
    Signed-off-by: Joy Nelson <joy@bywatersolutions.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -20372,6 +20372,20 @@ if( CheckVersion( $DBversion ) ) {
     print "Upgrade to $DBversion done (Bug 24640 - Allow quotes.timestamp to be NULL)\n";
 }
 
+$DBversion = '19.11.03.003';
+if( CheckVersion( $DBversion ) ) {
+    $dbh->do(q{
+        UPDATE systempreferences SET value = 'off'
+        WHERE variable = 'finesMode' AND (value <> 'production' OR value IS NULL)
+    });
+    $dbh->do(q{
+        UPDATE systempreferences SET options = 'off|production',
+        explanation = "Choose the fines mode, 'off' (do not accrue fines) or 'production' (accrue overdue fines).  Requires accruefines cronjob or CalculateFinesOnReturn system preference."
+        WHERE variable = 'finesMode'
+    });
+    SetVersion( $DBversion );
+    print "Upgrade to $DBversion done (Bug 21633  - Remove finesMode 'test')\n";
+}
 
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
