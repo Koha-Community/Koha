f46e3fafe63b51747e0d56efeb7edf7d3097117a
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2020-03-24 10:40:34 +0000

    Bug 24696: (RM follow-up) Fix spaceing on command line
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -21316,7 +21316,7 @@ sub NewVersion {
                     say sprintf "Upgrade to %s done [%s]: Bug %5s - %s", $DBversion, $time, $bug_number, $description;
                 }
             }
-            say sprintf "\t\t\t\t\t- %s", $description;
+            say sprintf "\t\t\t\t\t\t   - %s", $description;
         } else {
             unless ( $bug_number ) {
                 say sprintf "Upgrade to %s done [%s]: %s", $DBversion, $time, $description;
