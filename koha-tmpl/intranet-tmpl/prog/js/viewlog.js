b81d95f280259fe1de7775a0dbac52929b10bcb0
Aleisha Amohia <aleishaamohia@hotmail.com>
2020-03-24 16:00:41 +0000

    Bug 24982: Use checkboxes in Log Viewer
    So we can see all options at once, and more easily select more than one
    option.
    
    Test plan:
    1) Go to Tools -> Log viewer
    2) Notice the new checkboxes under Modules, Actions and Interface. 'All'
    should be selected by default for all three options
    3) Confirm you can deselect 'All' and the other options are enabled.
    Confirm selection all of the other options automatically selects 'All'
    and disables the options again.
    4) Confirm the results still work as expected when clicking 'Submit'
    button
    
    Sponsored-by: Catalyst IT
    Signed-off-by: Sally <sally.healey@cheshiresharedservices.gov.uk>
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

---
+++ koha-tmpl/intranet-tmpl/prog/js/viewlog.js
@@ -0,0 +1,58 @@
+function tickAll(section){
+    $("input[name='" + section + "']").prop("checked", true);
+    $("#" + section.slice(0,-1) + "ALL").prop("checked", true);
+    $("input[name='" + section + "']").prop("disabled", true);
+    $("#" + section.slice(0,-1) + "ALL").prop("disabled", false);
+}
+
+function untickAll(section){
+    $("input[name='" + section + "']").prop("checked", false);
+    $("input[name='" + section + "']").prop("disabled", false);
+}
+
+$(document).ready(function(){
+    tickAll('modules');
+    $("#moduleALL").change(function(){
+        if ( this.checked == true ){
+            tickAll('modules');
+        } else {
+            untickAll('modules');
+        }
+
+    });
+    $("input[name='modules']").change(function(){
+        if ( $("input[name='modules']:checked").length == $("input[name='modules']").length - 1 ){
+            tickAll('modules');
+        }
+    });
+
+    tickAll('actions');
+    $("#actionALL").change(function(){
+        if ( this.checked == true ){
+            tickAll('actions');
+        } else {
+            untickAll('actions');
+        }
+
+    });
+    $("input[name='actions']").change(function(){
+        if ( $("input[name='actions']:checked").length == $("input[name='actions']").length - 1 ){
+            tickAll('actions');
+        }
+    });
+
+    tickAll('interfaces');
+    $("#interfaceALL").change(function(){
+        if ( this.checked == true ){
+            tickAll('interfaces');
+        } else {
+            untickAll('interfaces');
+        }
+
+    });
+    $("input[name='interfaces']").change(function(){
+        if ( $("input[name='interfaces']:checked").length == $("input[name='interfaces']").length - 1 ){
+            tickAll('interfaces');
+        }
+    });
+});
