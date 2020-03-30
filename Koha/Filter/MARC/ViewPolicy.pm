df58c3e2c9af7b0218f6db957dbb781f0cb1f278
Tomas Cohen Arazi <tomascohen@theke.io>
2020-03-30 09:39:13 -0300

    Bug 25019: Make the ViewPolicy filter initialization standard
    This patch makes the ViewPolicy filter use the 'params' accessor instead
    of relying of ->{options} which has no accessor. This will allow
    interacting with the filter object be similar through all the filters in
    the chain.
    
    To test, we just need to verify no behaviour change takes place:
    1. Run:
       $ kshell
      k$ prove t/db_dependent/Filter_MARC_ViewPolicy.t
    => SUCCESS: Tests pass
    2. Apply this patch
    3. Repeat 1
    => SUCCESS: Tests still pass!
    4. Sign off :-D
    
    Signed-off-by: David Nind <david@davidnind.com>
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- Koha/Filter/MARC/ViewPolicy.pm
+++ Koha/Filter/MARC/ViewPolicy.pm
@@ -78,10 +78,12 @@ sub filter {
         push @records, $precord;
     }
 
+    my $params = $self->params;
+    my $interface     = $params->{options}->{interface} // 'opac';
+    my $frameworkcode = $params->{options}->{frameworkcode} // q{};
+
     foreach my $current_record (@records) {
         my $result        = $current_record;
-        my $interface     = $self->{options}->{interface} // 'opac';
-        my $frameworkcode = $self->{options}->{frameworkcode} // q{};
         my $hide          = _should_hide_on_interface();
 
         my $marcsubfieldstructure = GetMarcStructure( 0, $frameworkcode, { unsafe => 1 } );
@@ -150,16 +152,6 @@ sub _filter_field {
         }
 
     }
-    return;
-}
-
-sub initialize {
-    my $self  = shift;
-    my $param = shift;
-
-    my $options = $param->{options};
-    $self->{options} = $options;
-    $self->Koha::RecordProcessor::Base::initialize($param);
     return;
 }
 
