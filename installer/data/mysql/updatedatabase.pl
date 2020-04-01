8386f5f5e828565457ea4421773b0d0fe5168483
Joy Nelson <joy@bywatersolutions.com>
2020-04-01 20:52:41 +0000

    Bug 22273: DBRev 19.11.04.002
    Signed-off-by: Joy Nelson <joy@bywatersolutions.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -20463,9 +20463,7 @@ if( CheckVersion( $DBversion ) ) {
 $DBversion = '19.11.04.002';
 if( CheckVersion( $DBversion ) ) {
     $dbh->do( q|
-        ALTER TABLE article_requests 
-        MODIFY COLUMN created_on timestamp NULL, 
-        MODIFY COLUMN updated_on timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+        ALTER TABLE article_requests MODIFY COLUMN created_on timestamp NULL, MODIFY COLUMN updated_on timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
     |);
     SetVersion( $DBversion );
     print "Upgrade to $DBversion done (Bug 22273: Column article_requests.created_on should not be updated)\n";
