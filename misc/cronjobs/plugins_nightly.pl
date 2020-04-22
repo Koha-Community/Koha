b7e748e886b726a568a225a3c62787ff527e425d
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2020-04-22 12:04:37 +0100

    Bug 25245: Add plugins_nightly.pl cronjob script
    This script simply iterates through installed plugins that impliment a
    cronjob_nightly method and runs said method.
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    (cherry picked from commit 63b926b28b7cb8905fbc12ff3c016231bcc05e7d)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

---
+++ misc/cronjobs/plugins_nightly.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/perl
+
+use Modern::Perl;
+
+use Koha::Script -cron;
+cronlogaction();
+
+if ( C4::Context->config("enable_plugins") ) {
+    my @plugins = Koha::Plugins->new->GetPlugins(
+        {
+            method => 'cronjob_nightly',
+        }
+    );
+
+    foreach my $plugin (@plugins) {
+        try {
+            $plugin->cronjob_nightly();
+        }
+        catch {
+            warn "$_";
+        };
+    }
+}
+
+=head1 NAME
+
+plugins_nightly.pl - Run nightly tasks specified by plugins
+
+=head1 SYNOPSIS
+
+plugins_nightly.pl
+
+=head1 AUTHOR
+
+Martin Renvoize <martin.renvoize@ptfs-europe.com>
+
+=head1 LICENSE
+
+This file is part of Koha.
+
+Koha is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+Koha is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+=cut
