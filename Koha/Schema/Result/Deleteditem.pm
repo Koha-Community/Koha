6bef3c3c6987b8ebdc2820a9fdabfb5b34ba2b4b
Andrew Fuerste-Henry <andrew@bywatersolutions.com>
2020-04-26 23:51:25 +0000

    Bug 23081: set default to 0 for items.issues and deleteditems.issues
    To test:
    - save and run this sql query in reports: select sum(if(issues is null,1,0)),sum(if(issues=0,1,0)) from items
    - you should see a lot of nulls and no zeros
    - apply patch
    - updatedatabase
    - re-run your query and see that your nulls have changed to zeros
    - create a new item
    - rerun your query and see your new item is counted in the zeros, not the nulls
    
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    
    Signed-off-by: Joy Nelson <joy@bywatersolutions.com>
    (cherry picked from commit 02896efdf347adbfa0cb262c9b7d46e11dbe4866)
    
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

--- Koha/Schema/Result/Deleteditem.pm
+++ Koha/Schema/Result/Deleteditem.pm
@@ -156,7 +156,8 @@ __PACKAGE__->table("deleteditems");
 =head2 issues
 
   data_type: 'smallint'
-  is_nullable: 1
+  is_nullable: 0
+  default_value: 0
 
 =head2 renewals
 
@@ -341,7 +342,10 @@ __PACKAGE__->add_columns(
   "coded_location_qualifier",
   { data_type => "varchar", is_nullable => 1, size => 10 },
   "issues",
-  { data_type => "smallint", is_nullable => 1 },
+   { data_type => "smallint",
+    is_nullable => 0,
+    default_value => 0,
+  },
   "renewals",
   { data_type => "smallint", is_nullable => 1 },
   "reserves",
