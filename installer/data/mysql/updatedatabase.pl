bdb8edb5b51e11f10e06ddbe4a9fc48d90ddd0db
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2020-04-27 11:51:08 +0100

    Bug 25045: (RM follow-up) Add the RESTPublicAnonymousRequests syspref
    The atomic update alos included a quoting mistake
    
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    (cherry picked from commit 19a241c131f03a2640044ac3280759b2e6fd35bc)
    
    Signed-off-by: Aleisha Amohia <aleishaamohia@hotmail.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -20596,7 +20596,7 @@ if( CheckVersion( $DBversion ) ) {
         INSERT IGNORE INTO `systempreferences`
             (`variable`,`value`,`explanation`,`options`,`type`)
         VALUES
-            ('RESTPublicAnonymousRequests', $opac_public, NULL,'If enabled, the API will allow anonymous access to public routes that don\'t require authenticated access'.','YesNo');
+            ('RESTPublicAnonymousRequests', $opac_public, NULL,'If enabled, the API will allow anonymous access to public routes that don\'t require authenticated access.','YesNo');
     });
 
     NewVersion( $DBversion, 25045, "Add a way to restrict anonymous access to public routes (OpacPublic behaviour)");
