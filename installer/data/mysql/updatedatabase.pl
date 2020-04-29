d1cb5f562e47be367bf17a8a689a378c88da954e
Joy Nelson <joy@bywatersolutions.com>
2020-04-29 20:43:09 +0000

    Bug 20754: DBRev 19.11.05.002
    Signed-off-by: Joy Nelson <joy@bywatersolutions.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -20482,6 +20482,18 @@ if( CheckVersion( $DBversion ) ) {
     print "Upgrade to $DBversion done (Bug 25010: Fix typo in account_debit_type description)\n";
 }
 
+$DBversion = '19.11.05.002';
+if( CheckVersion( $DBversion ) ) {
+    # From: https://stackoverflow.com/questions/3311903/remove-duplicate-rows-in-mysql
+    $dbh->do(q|
+            DELETE a
+            FROM virtualshelfshares as a, virtualshelfshares as b
+            WHERE a.id < b.id AND a.borrowernumber IS NOT NULL AND a.borrowernumber=b.borrowernumber AND a.shelfnumber=b.shelfnumber
+             |);
+    SetVersion( $DBversion );
+    print "Upgrade to $DBversion done (Bug 20754: Remove double accepted list shares)\n";
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
