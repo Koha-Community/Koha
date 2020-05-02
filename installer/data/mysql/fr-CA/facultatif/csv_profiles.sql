2042cb65310072a5c00205fe1d632deac00e639e
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2020-05-02 15:46:13 +0200

    Bug 24163: Add the new CSV profile to other installer files
    export_format is not populated for other languages apparently...
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- installer/data/mysql/fr-CA/facultatif/csv_profiles.sql
+++ installer/data/mysql/fr-CA/facultatif/csv_profiles.sql
@@ -1,2 +1,12 @@
 INSERT IGNORE INTO export_format( profile, description, content, csv_separator, type, used_for )
-VALUES ( "Réclamation de numéros", "Export CSV par défaut pour la réclamation de numéros de périodiques", "FOURNISSEUR=aqbooksellers.name|TITRE=subscription.title|NUMÉRO=serial.serialseq|EN RETARD DEPUIS=serial.planneddate", ",", "sql", "late_issues" );
+VALUES ( "Réclamation de numéros", "Export CSV par défaut pour la réclamation de numéros de périodiques", "FOURNISSEUR=aqbooksellers.name|TITRE=subscription.title|NUMÉRO=serial.serialseq|EN RETARD DEPUIS=serial.planneddate", ",", "sql", "late_issues" ),
+("Late orders (CSV profile)", "Default CSV export for late orders", 'Title[% separator %]Author[% separator %]Publication year[% separator %]ISBN[% separator %]Quantity[% separator %]Number of claims
+[% FOR order IN orders ~%]
+[%~ SET biblio = order.biblio ~%]
+"[% biblio.title %]"[% separator ~%]
+"[% biblio.author %]"[% separator ~%]
+"[% bibio.biblioitem.publicationyear %]"[% separator ~%]
+"[% biblio.biblioitem.isbn %]"[% separator ~%]
+"[% order.quantity%]"[% separator ~%]
+"[% order.claims.count%][% IF order.claims.count %]([% FOR c IN order.claims %][% c.claimed_on | $KohaDates %][% UNLESS loop.last %], [% END %][% END %])[% END %]"
+[% END %]', ",", "sql", "late_orders");
