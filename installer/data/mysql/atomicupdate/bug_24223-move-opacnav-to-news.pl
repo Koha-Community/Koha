4577950900e0738f3e3cf09b1ae5ebf364e8d8be
Owen Leonard <oleonard@myacpl.org>
2020-05-04 19:33:33 +0000

    Bug 24223: Convert OpacNav system preference to additional content
    This patch builds on Bug 22318 to move the OpacNav system
    preference into the "Additional contents" system, making it possible to
    have language- and library-specific content.
    
    To test you should have some content in the OpacNav system
    preference. To make testing easier it's also a good idea to have content
    in the OpacNavBottom preference.
    
     - Apply the patch and run the database update process.
     - In the staff client, go to Tools -> HTML customizations and verify
       that the content from OpacNav is now stored there. When you
       edit the entry you should see content for each of the enabled
       translations in your system under each tab.
       - Confirm that the previous contents of OpacNav were added
         correctly.
     - Go to Administration -> System preferences -> OPAC and verify that
       the OpacNav preference has been removed.
     - Update and reinstall active translations (for instance fr-FR):
       - cd misc/translator/
       - perl translate update fr-FR
       - perl translate install fr-FR
     - View the OPAC and confirm that the contents of the OpacNav system
       preference displays in the left-hand sidebar.
     - Test as many pages as possible to confirm that wherever the
       OpacNavBottom content is displayed, OpacNav is also displayed.
     - Test also using the updated translation.
    
    Signed-off-by: David Nind <david@davidnind.com>
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    
    Bug 24223: (follow-up) Add date to OpacNav additional content entries
    
    This follow-up modifies the atomic update so that it inserts today's
    date as the "Published on" date. Without that information the system
    considers the item unpublished and will not display the content.
    
    Signed-off-by: David Nind <david@davidnind.com>
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    
    Bug 24223: (follow-up) Make logged-in user's branchcode available to template
    
    This patch makes a couple of corrections for problems I discovered
    while working on Bug 24224: The variables required for correct display
    of news items by branch and language were not consistently available to
    the templates.
    
    In opac-readingrecord.pl, the "lang" variable was being overwritten. On
    this and other pages the logged-in patron's brachcode was not made
    available. I've corrected instances of KohaNews.get() to use the
    globally-available "logged_in_user.branchcode" instead.
    
    To test, apply the patch and follow the original test plan, with the
    addition of instances of the OpacNav additional contents item which
    have a specific library selected.
    
    Confirm that such items are correctly displayed according to the
    logged-in user's home library. If there is no logged-in user the
    library-specific OpacNav should not display.
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    
    Bug 24223: (follow-up) Updates to conform to new practices
    
    - The patch now works with new "Additional contents" system
    - The database update follows new skeleton.pl
    - Add "OpacNav" option to the additional contents template
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    
    Bug 24223: (follow-up) Add missing "return" to atomicupdate
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    
    Bug 24223: (QA follow-up) Link OpacNav to HTML customizations now
    
    In the pref description of OpacNavBottom. (Before obsoleting that
    one too somewhere soon.)
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

---
+++ installer/data/mysql/atomicupdate/bug_24223-move-opacnav-to-news.pl
@@ -0,0 +1,49 @@
+use Modern::Perl;
+
+return {
+    bug_number => "24223",
+    description => "Move contents of OpacNav system preference into additional contents",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+
+        # get list of installed translations
+        require C4::Languages;
+        my @langs;
+        my $tlangs = C4::Languages::getTranslatedLanguages();
+
+        foreach my $language ( @$tlangs ) {
+            foreach my $sublanguage ( @{$language->{'sublanguages_loop'}} ) {
+                push @langs, $sublanguage->{'rfc4646_subtag'};
+            }
+        }
+
+        # There must be a "default" entry in addition to language-specific ones
+        push @langs, "default";
+
+        # Get any existing value from the OpacNav system preference
+        my ($opacnav) = $dbh->selectrow_array( q|
+            SELECT value FROM systempreferences WHERE variable='OpacNav';
+        |);
+        if( $opacnav ){
+            # If there is a value in the OpacNav preference, insert it into additional_contents
+            my $code = '';
+            foreach my $lang ( @langs ) {
+                say $out "Inserting OpacNav contents into $lang news item...";
+                $dbh->do( "INSERT INTO additional_contents ( category, code, location, branchcode, title, content, lang, published_on ) VALUES ('html_customizations', '', 'OpacNav', NULL, ?, ?, ?, CAST(NOW() AS date) )", undef, "OpacNav $lang", $opacnav, $lang );
+                my $idnew = $dbh->last_insert_id(undef, undef, 'additional_contents', undef);
+                if( $code eq '' ){
+                    $code = "News_$idnew";
+                }
+                $dbh->do(q|UPDATE additional_contents SET code=? WHERE idnew = ?|, undef, $code, $idnew);
+            }
+
+            # Remove the OpacNav system preference
+            $dbh->do("DELETE FROM systempreferences WHERE variable='OpacNav'");
+            say $out "Bug 24223 update done";
+        } else {
+            say $out "No OpacNav preference found. Update has already been run.";
+        }
+
+    },
+}
