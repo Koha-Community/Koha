a18e944400efd354f2b755459f45e249f8a24468
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2020-05-04 09:23:19 +0100

    Bug 24161: DBRev 19.12.00.085
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -21993,6 +21993,41 @@ if( CheckVersion( $DBversion ) ) {
     NewVersion( $DBversion, 17682, "Add macros db table and permissions");
 }
 
+$DBversion = '19.12.00.085';
+if( CheckVersion( $DBversion ) ) {
+    unless ( TableExists( 'aqorders_claims' ) ) {
+        $dbh->do(q|
+            CREATE TABLE aqorders_claims (
+                id int(11) AUTO_INCREMENT,
+                ordernumber INT(11) NOT NULL,
+                claimed_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                PRIMARY KEY (id),
+                CONSTRAINT aqorders_claims_ibfk_1 FOREIGN KEY (ordernumber) REFERENCES aqorders (ordernumber) ON DELETE CASCADE ON UPDATE CASCADE
+            ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci
+        |);
+
+        my $orders = $dbh->selectall_arrayref(q|
+            SELECT ordernumber, claims_count, claimed_date
+            FROM aqorders
+            WHERE claims_count > 0
+        |, { Slice => {} });
+        my $insert_claim_sth = $dbh->prepare(q|
+            INSERT INTO aqorders_claims (ordernumber, claimed_on)
+            VALUES (?,?)
+        |);
+
+        for my $order ( @$orders ) {
+            for my $claim (1..$order->{claims_count}) {
+                $insert_claim_sth->execute($order->{ordernumber}, $order->{claimed_on});
+            }
+        }
+
+        $dbh->do(q|ALTER TABLE aqorders DROP COLUMN claims_count, DROP COLUMN claimed_date|);
+    }
+
+    NewVersion( $DBversion, 24161, "Add new join table aqorders_claims to keep track of claims");
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
