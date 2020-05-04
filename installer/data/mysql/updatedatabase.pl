39138e860ce8e3eb54d2788ba32728baf03122c0
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2020-05-04 08:35:59 +0100

    Bug 17268: DBRev 19.12.00.084
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -21963,8 +21963,34 @@ if( CheckVersion( $DBversion ) ) {
         WHERE category_name IN ('branches', 'itemtypes', 'cn_source')
     |);
 
-    # Always end with this (adjust the bug info)
     NewVersion( $DBversion, 17355, "Add is_system to authorised_value_categories table");
+}
+
+$DBversion = '19.12.00.084';
+if( CheckVersion( $DBversion ) ) {
+    unless ( TableExists('advanced_editor_macros') ) {
+        $dbh->do(q|
+            CREATE TABLE advanced_editor_macros (
+            id INT(11) NOT NULL AUTO_INCREMENT,
+            name varchar(80) NOT NULL,
+            macro longtext NULL,
+            borrowernumber INT(11) default NULL,
+            shared TINYINT(1) default 0,
+            PRIMARY KEY (id),
+            CONSTRAINT borrower_macro_fk FOREIGN KEY ( borrowernumber ) REFERENCES borrowers ( borrowernumber ) ON UPDATE CASCADE ON DELETE CASCADE
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;|
+        );
+    }
+    $dbh->do(q|
+        INSERT IGNORE INTO permissions (module_bit, code, description)
+        VALUES (9, 'create_shared_macros', 'Create public macros')
+    |);
+    $dbh->do(q|
+        INSERT IGNORE INTO permissions (module_bit, code, description)
+        VALUES (9, 'delete_shared_macros', 'Delete public macros')
+    |);
+
+    NewVersion( $DBversion, 17682, "Add macros db table and permissions");
 }
 
 # SEE bug 13068
