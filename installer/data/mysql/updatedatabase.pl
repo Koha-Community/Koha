219f000069377f8a6b2790e42870083236b66bd0
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2020-05-04 07:46:00 +0100

    Bug 17355: DBRev 19.12.00.083
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -21941,6 +21941,32 @@ if( CheckVersion( $DBversion ) ) {
     NewVersion( $DBversion, 23794, \@detail);
 }
 
+$DBversion = '19.12.00.083';
+if( CheckVersion( $DBversion ) ) {
+
+    unless ( column_exists( 'authorised_value_categories', 'is_system' ) ) {
+        $dbh->do(q|
+            ALTER TABLE authorised_value_categories
+            ADD COLUMN is_system TINYINT(1) DEFAULT 0 AFTER category_name
+        |);
+    }
+
+    $dbh->do(q|
+        UPDATE authorised_value_categories
+        SET is_system = 1
+        WHERE category_name IN ('LOC', 'LOST', 'WITHDRAWN', 'Bsort1', 'Bsort2', 'Asort1', 'Asort2', 'SUGGEST', 'DAMAGED', 'LOST', 'BOR_NOTES', 'CCODE', 'NOT_LOAN')
+    |);
+
+    $dbh->do(q|
+        UPDATE authorised_value_categories
+        SET is_system = 1
+        WHERE category_name IN ('branches', 'itemtypes', 'cn_source')
+    |);
+
+    # Always end with this (adjust the bug info)
+    NewVersion( $DBversion, 17355, "Add is_system to authorised_value_categories table");
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
