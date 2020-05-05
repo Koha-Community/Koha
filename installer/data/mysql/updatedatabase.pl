cdf786b0aa0dc1d9fed489335fcfb0bf2cf9ea43
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2020-05-05 11:14:49 +0100

    Bug 24163: DBRev 19.12.00.086
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -22028,6 +22028,25 @@ if( CheckVersion( $DBversion ) ) {
     NewVersion( $DBversion, 24161, "Add new join table aqorders_claims to keep track of claims");
 }
 
+$DBversion = '19.12.00.086';
+if( CheckVersion( $DBversion ) ) {
+    $dbh->do(q{
+        INSERT IGNORE INTO export_format( profile, description, content, csv_separator, type, used_for ) VALUES
+        ("Late orders (CSV profile)", "Default CSV export for late orders", 'Title[% separator %]Author[% separator %]Publication year[% separator %]ISBN[% separator %]Quantity[% separator %]Number of claims
+        [% FOR order IN orders ~%]
+        [%~ SET biblio = order.biblio ~%]
+        "[% biblio.title %]"[% separator ~%]
+        "[% biblio.author %]"[% separator ~%]
+        "[% bibio.biblioitem.publicationyear %]"[% separator ~%]
+        "[% biblio.biblioitem.isbn %]"[% separator ~%]
+        "[% order.quantity%]"[% separator ~%]
+        "[% order.claims.count%][% IF order.claims.count %]([% FOR c IN order.claims %][% c.claimed_on | $KohaDates %][% UNLESS loop.last %], [% END %][% END %])[% END %]"
+        [% END %]', ",", "sql", "late_orders")
+    });
+
+    NewVersion( $DBversion, 24163, "Define a default CSV profile for late orders");
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
