575878f0fbfbd4f222cd8a9d5f9b4704ca1ad19d
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2020-05-11 11:10:41 +0100

    Bug 25184: (RM follow-up) Syntax error correction
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -22051,7 +22051,7 @@ $DBversion = '19.12.00.087';
 if( CheckVersion( $DBversion ) ) {
     $dbh->do(q{
         INSERT INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type` ) VALUES
-        ('TrapHoldsOnOrder','1',NULL,'If enabled, Koha will trap holds for on order items ( notforloan < 0 )','YesNo't c)
+        ('TrapHoldsOnOrder','1',NULL,'If enabled, Koha will trap holds for on order items ( notforloan < 0 )','YesNo')
     });
 
     NewVersion( $DBversion, 25184, "Items with a negative notforloan status should not be captured for holds");
