0f293662c37f76408ac2c4b952c6160dbdb9f014
Lucas Gass <lucas@bywatersolutions.com>
2020-05-11 21:27:29 +0000

    Bug 20754: DBRev 19.05.11.001
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -19029,6 +19029,18 @@ if ( CheckVersion($DBversion) ) {
             SetVersion ($DBversion);
 }
 
+$DBversion = '19.05.10.001';
+if( CheckVersion( $DBversion ) ) {
+    # From: https://stackoverflow.com/questions/3311903/remove-duplicate-rows-in-mysql
+    $dbh->do(q|
+DELETE a
+FROM virtualshelfshares as a, virtualshelfshares as b
+WHERE a.id < b.id AND a.borrowernumber IS NOT NULL AND a.borrowernumber=b.borrowernumber AND a.shelfnumber=b.shelfnumber
+    |);
+    SetVersion( $DBversion );
+    print "Upgrade to $DBversion done (Bug 20754: Remove double accepted list shares)\n";
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 
