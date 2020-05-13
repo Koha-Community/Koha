1e2cb846ecab70a6bdad0d960e4609c954f6583e
Joy Nelson <joy@bywatersolutions.com>
2020-05-13 18:54:56 +0000

    Bug 25478: DBRev 19.11.05.006
    Signed-off-by: Joy Nelson <joy@bywatersolutions.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -20529,6 +20529,25 @@ if( CheckVersion( $DBversion ) ) {
     print "Upgrade to $DBversion done (Bug 25184: Items with a negative notforloan status should not be captured for holds)\n";
 }
 
+$DBversion = '19.11.05.006';
+if( CheckVersion( $DBversion ) ) {
+    # Migrate LOST_RETURNED to LOST_RETURN
+    $dbh->do(qq{
+            UPDATE
+            accountlines
+            SET
+            credit_type_code = 'LOST_RETURN'
+            WHERE
+            credit_type_code = 'LOST_RETURNED'
+            });
+
+    # Drop LOST_RETURNED credit type
+    $dbh->do(qq{
+            DELETE FROM account_credit_types WHERE code = 'LOST_RETURNED'
+            });
+    SetVersion( $DBversion );
+    print "Upgrade to $DBversion done (Bug 25389: Catch errant cases of LOST_RETURNED)\n";
+}
 
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
