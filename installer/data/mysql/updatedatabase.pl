4429b42036061d6cf5662f12e390c521eef65771
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2020-05-13 10:34:10 +0100

    Bug 25389: DBRev 19.12.00.089
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -22107,6 +22107,27 @@ if( CheckVersion( $DBversion ) ) {
     NewVersion( $DBversion, 24378, "Fix some grammatical errors in default auto renewal notice");
 }
 
+$DBversion = '19.12.00.089';
+if( CheckVersion( $DBversion ) ) {
+
+    # Migrate LOST_RETURNED to LOST_FOUND
+    $dbh->do(qq{
+        UPDATE
+          accountlines
+        SET
+          credit_type_code = 'LOST_FOUND'
+        WHERE
+          credit_type_code = 'LOST_RETURNED'
+    });
+
+    # Drop LOST_RETURNED credit type
+    $dbh->do(qq{
+        DELETE FROM account_credit_types WHERE code = 'LOST_RETURNED'
+    });
+
+    NewVersion( $DBversion, 25389, "Catch errant cases of LOST_RETURNED");
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
