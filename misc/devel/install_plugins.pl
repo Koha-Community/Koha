7ce97d4b17482b865f51a2e6fb0db0c1821cadff
Nick Clemens <nick@bywatersolutions.com>
2020-05-20 11:12:12 +0000

    Bug 25549: Remove plugin methods for broken plugins
    To test:
     1 - Enable plugins in the koha-conf
     2 - Install the kitchen sink plugin
     3 - Your staff client should be orange now :-)
     4 - edit the plugin module
         /var/lib/koha/kohadev/plugins/Koha/Plugin/Com/ByWaterSolutions/KitchenSink.pm
     5 - Add a line to break compilation, like:
         this won't compile
     6 - Restart all
     7 - Your koha is now broken
     8 - kshell
     9 - perl misc/devel/install_plugins.pl
    10 - Restart all
    11 - Koha remains broken
    12 - Apply patch
    13 - kshell
    14 - perl misc/devel/install_plugins.pl
    15 - Koha now works!
    16 - Koha is not orange because the plugin methods are removed
    
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    (cherry picked from commit 0eae966d257fa2ecc6a6506e2515691e092a701c)
    
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>
    (cherry picked from commit bd2347fbf85083fe6204dcaaf1cc98826219eaed)
    
    Signed-off-by: Aleisha Amohia <aleishaamohia@hotmail.com>

--- misc/devel/install_plugins.pl
+++ misc/devel/install_plugins.pl
@@ -39,14 +39,18 @@ unless ($plugins_enabled) {
     exit 1;
 }
 
-
 my @existing_plugins = Koha::Plugins->new()->GetPlugins({
     all    => 1,
+    errors => 1,
 });
 my $existing_plugins;
 for my $existing_plugin (@existing_plugins) {
-    $existing_plugins->{ $existing_plugin->{metadata}->{name} } =
-      $existing_plugin->{metadata}->{version};
+    if( defined $existing_plugin->{error} ){
+        print "Could not load: ".$existing_plugin->{name}." any associated method will be removed\n";
+    } else {
+        $existing_plugins->{ $existing_plugin->{metadata}->{name} } =
+          $existing_plugin->{metadata}->{version};
+    }
 }
 
 my @installed_plugins = Koha::Plugins->new()->InstallPlugins();
