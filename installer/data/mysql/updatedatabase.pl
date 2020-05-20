a17ea63d028fe500f80ff75b42a4fa1a511fe708
Joy Nelson <joy@bywatersolutions.com>
2020-05-20 14:59:21 +0000

    Bug 24854: DBRev 19.11.05.007
    Signed-off-by: Joy Nelson <joy@bywatersolutions.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -20561,6 +20561,23 @@ if( CheckVersion( $DBversion ) ) {
     print "Upgrade to $DBversion done (Bug 25389: Catch errant cases of LOST_RETURNED)\n";
 }
 
+$DBversion = 'XXX'; # will be replaced by the RM
+if( CheckVersion( $DBversion ) ) {
+    my $ft_enabled = $dbh->selectrow_array(q|
+        SELECT COUNT(*) FROM systempreferences WHERE variable like "IDreamBooks%" and value="1"
+    |);
+    if ( $ft_enabled ) {
+        $dbh->do(q|
+            UPDATE systempreferences
+            SET value="0"
+            WHERE variable like "IDreamBooks%"
+        |);
+    }
+
+    SetVersion( $DBversion );
+    print "Upgrade to $DBversion done (Bug 24854 - Disable IDreamBooks)\n";
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
