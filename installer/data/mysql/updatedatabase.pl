3bbc5ca0ecb7d6f86bd4b59827d7f8e098d2bcea
Joy Nelson <joy@bywatersolutions.com>
2020-05-20 14:59:21 +0000

    Bug 24854: DBRev 19.05.11.001
    Signed-off-by: Joy Nelson <joy@bywatersolutions.com>
    
    (cherry picked from commit a17ea63d028fe500f80ff75b42a4fa1a511fe708)
    Signed-off-by: Victor Grousset/tuxayo <victor@tuxayo.net>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -19067,6 +19067,23 @@ if ( CheckVersion($DBversion) ) {
             SetVersion ($DBversion);
 }
 
+$DBversion = "19.05.11.001";
+if( CheckVersion( $DBversion ) ) {
+    my $ft_enabled = $dbh->selectrow_array(q|
+        SELECT COUNT(*) FROM systempreferences WHERE variable like "IDreamBooks%" and value="1"
+    |);
+    if ( $ft_enabled ) {
+        $dbh->do(q|
+            UPDATE systempreferences
+            SET value="0"
+            WHERE variable like "IDreamBooks%"
+        |);
+    }
+
+    SetVersion( $DBversion );
+    print "Upgrade to $DBversion done (Bug 24854 - Disable IDreamBooks)\n";
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 
