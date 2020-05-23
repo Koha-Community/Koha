ef3b7eedc0efc726329f2f608c0f676e93fd5566
Katrin Fischer <katrin.fischer.83@web.de>
2020-05-24 01:27:49 +0200

    Bug 25129: Update German (de-DE) web installer sample files for 20.05
    This translates the new notices and CSV profie for late orders,
    makes some minor changes to the patron category descriptions
    and numbering patterin labelling.
    
    To test:
    - Make sure German language is installed, see:
      https://wiki.koha-community.org/wiki/Installation_of_additional_languages_for_OPAC_and_INTRANET_staff_client
    - Run the web installer and choose de-DE
    - Select all sample files
    - Make sure they all install without any errors
    
    Bonus:
    - If you can speak German, you can check the translations as well :)
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- installer/data/mysql/de-DE/optional/patron_categories.sql
+++ installer/data/mysql/de-DE/optional/patron_categories.sql
@@ -1,17 +1,17 @@
 INSERT INTO `categories` (`categorycode`, `description`, `enrolmentperiod`, `upperagelimit`, `dateofbirthrequired`, `finetype`, `bulk`, `enrolmentfee`, `overduenoticerequired`, `issuelimit`, `reservefee`, `category_type`) VALUES 
 
 -- Adult Patrons
-('PT','Erwachsener',99,999,18,NULL,NULL,'0.000000',1,NULL,'0.000000','A'),
-('ST','Student',99,999,18,NULL,NULL,'0.000000',1,NULL,'0.000000','A'),
+('PT','Erwachsene/r',99,999,18,NULL,NULL,'0.000000',1,NULL,'0.000000','A'),
+('ST','Studierende/r',99,999,18,NULL,NULL,'0.000000',1,NULL,'0.000000','A'),
 ('HB','Ans Haus gebunden',99,999,18,NULL,NULL,'0.000000',1,NULL,'0.000000','A'),
 
 -- Children
 ('K','Kind',99,17,5,NULL,NULL,'0.000000',1,NULL,'0.000000','C'),
-('J','Jugendlicher',99,17,5,NULL,NULL,'0.000000',1,NULL,'0.000000','C'),
+('J','Jugendliche/r',99,17,5,NULL,NULL,'0.000000',1,NULL,'0.000000','C'),
 ('YA','Junger Erwachsener',99,17,5,NULL,NULL,'0.000000',1,NULL,'0.000000','C'),
 
 -- Professionals
-('T','Lehrer',99,999,18,NULL,NULL,'0.000000',0,NULL,'0.000000','P'),
+('T','Lehrkraft',99,999,18,NULL,NULL,'0.000000',0,NULL,'0.000000','P'),
 ('B','Gremienmitglied',99,17,5,NULL,NULL,'0.000000',1,NULL,'0.000000','P'),
 
 -- Institutional
@@ -20,4 +20,4 @@ INSERT INTO `categories` (`categorycode`, `description`, `enrolmentperiod`, `upp
 ('L','Bibliothek',99,999,18,NULL,NULL,'0.000000',1,NULL,'0.000000','I'),
 
 -- Staff
-('S','Bibliotheksmitarbeiter',99,999,18,NULL,NULL,'0.000000',0,NULL,'0.000000','S');
+('S','Bibliothekspersonal',99,999,18,NULL,NULL,'0.000000',0,NULL,'0.000000','S');
