5b263c80dc647778bd0a4e23dbfd65167cf4c336
Owen Leonard <oleonard@myacpl.org>
2020-05-23 14:35:45 +0000

    Bug 25492: Your Account Menu button does nothing on mobile devices
    This patch removes some redundant code from the OPAC JavaScript which
    was causing problems with the show/hide action on two controls which are
    shown only at narrower browser widths: The "Your account" menu shown
    when the user is on a user-related page, and the "Refine your search"
    menu shown on the search results page.
    
    To test, apply the patch and go to the OPAC. Narrow your browser width
    to below 768 pixels.
    
     - Perform a search.
     - On the search results page you should see a "Refine your search"
       link at the top of the results. Clicking it should expand and
       collapse the facets menu.
     - Log in to the OPAC.
     - On the "Your summary" page there should be a "Your account menu" link
       at the top of the main part of the page. Clicking it should expand
       and collapse the account menu.
     - Verify that these menus display correctly when you widen and narrow
       the browser window.
    
    Signed-off-by: David Nind <david@davidnind.com>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    (cherry picked from commit 9ac43fd97b58120560a33e26509d5acc712c97fb)
    
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

--- koha-tmpl/opac-tmpl/bootstrap/js/script.js
+++ koha-tmpl/opac-tmpl/bootstrap/js/script.js
@@ -62,12 +62,6 @@ $(document).ready(function(){
         }
     });
 
-    $(".menu-collapse-toggle").on("click",function(e){
-        e.preventDefault();
-        $(this).toggleClass("menu-open");
-        $(".menu-collapse").toggle();
-    });
-
     $(".loginModal-trigger").on("click",function(e){
         e.preventDefault();
         $("#loginModal").modal("show");
