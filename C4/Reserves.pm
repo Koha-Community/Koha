a831c288da603f022a9894d560ed542e69feb5e3
Andrew Nugged <nugged@gmail.com>
2020-06-05 09:59:48 +0300

    Bug 24683: Subroutine name changed (fix), no code logic changed This is the intermediate refactor: renamed subroutine only.
    Naming mistake came because this sub is used to detect if anything
    available for hold, but it used in "if ANY UNAVAILABLE rule", so actually
    results of this sub negated (see below "return" in the code).
    
    In details:
    
    when previous refactor was done, name for subroutine was chosen
    wrongly in "opposite" direction from what it actually does:
    
    it was named "ItemsAnyAvailableForHold", but this subroutine gave
    truth (1) if at least one of the items available on shelf, not lost,
    not on loan, not held, and not restricted by smart rules and damaged
    status. So, if this sub says that item is still "available", this
    actually PREVENTS item from hold in parent sub (see negated return):
    
        sub IsAvailableForItemLevelRequest {
            ...
            my $any_available = ItemsAnyAvailableAndNotRestricted...
            return $any_available ? 0 : 1;
                 # ^^^ if any available and not restricted - we don't allow
                 #     on-shelf holds
            ...
    
    I.e. like it named now: "ItemsAnyAvailableAndNotRestricted".
    
    Small aside fix: white space for '&&' inside brackets added to join
    operation by priority visually.
    
    Testing plan not needed: all places where sub used it just renamed.
    More: all this places/code was introduced in one older commit so there
    is also no overlaps or other calls/uses for this subroutine.
    
    Signed-off-by: Agustin Moyano <agustinmoyano@theke.io>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    (cherry picked from commit 618cf80df9b0b668174b28ff61cad8a938704ec3)
    
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

--- C4/Reserves.pm
+++ C4/Reserves.pm
@@ -122,7 +122,7 @@ BEGIN {
         &AutoUnsuspendReserves
 
         &IsAvailableForItemLevelRequest
-        ItemsAnyAvailableForHold
+        ItemsAnyAvailableAndNotRestricted
 
         &AlterPriority
         &ToggleLowestPriority
@@ -1299,24 +1299,25 @@ sub IsAvailableForItemLevelRequest {
         return  $items_any_available ? 0 : 1
             if defined $items_any_available;
 
-        my $any_available = ItemsAnyAvailableForHold( { biblionumber => $item->biblionumber, patron => $patron });
+        my $any_available = ItemsAnyAvailableAndNotRestricted( { biblionumber => $item->biblionumber, patron => $patron });
         return $any_available ? 0 : 1;
     } else { # on_shelf_holds == 0 "If any unavailable" (the description is rather cryptic and could still be improved)
         return $item->onloan || IsItemOnHoldAndFound( $item->itemnumber );
     }
 }
 
-=head2 ItemsAnyAvailableForHold
+=head2 ItemsAnyAvailableAndNotRestricted
 
-  ItemsAnyAvailableForHold( { biblionumber => $biblionumber, patron => $patron });
+  ItemsAnyAvailableAndNotRestricted( { biblionumber => $biblionumber, patron => $patron });
 
-This function checks all items for specified biblionumber (num) / patron (object)
-and returns true (1) or false (0) depending if any of rules allows at least of
-one item to be available for hold including lots of parameters/logic
+This function checks all items for specified biblionumber (numeric) against patron (object)
+and returns true (1) if at least one item available for loan/check out/present/not held
+and also checks other parameters logic which not restricts item for hold at all (for ex.
+AllowHoldsOnDamagedItems or 'holdallowed' own/sibling library)
 
 =cut
 
-sub ItemsAnyAvailableForHold {
+sub ItemsAnyAvailableAndNotRestricted {
     my $param = shift;
 
     my @items = Koha::Items->search( { biblionumber => $param->{biblionumber} } );
@@ -1337,7 +1338,7 @@ sub ItemsAnyAvailableForHold {
             || $i->onloan
             || IsItemOnHoldAndFound( $i->id )
             || ( $i->damaged
-            && ! C4::Context->preference('AllowHoldsOnDamagedItems') )
+                 && ! C4::Context->preference('AllowHoldsOnDamagedItems') )
             || Koha::ItemTypes->find( $i->effective_itemtype() )->notforloan
             || $branchitemrule->{holdallowed} == 1 && $param->{patron}->branchcode ne $i->homebranch
             || $branchitemrule->{holdallowed} == 3 && ! $item_library->validate_hold_sibling( { branchcode => $param->{patron}->branchcode } );
