8b315919124b6caa4ba0a5ad918da723cf7d317e
Aleisha Amohia <aleishaamohia@hotmail.com>
2020-06-22 10:09:42 +1200

    Bug 25045: DBRev 19.11.06.001
    Signed-off-by: Aleisha Amohia <aleishaamohia@hotmail.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -20586,6 +20586,22 @@ if ( CheckVersion($DBversion) ) {
     SetVersion ($DBversion);
 }
 
+$DBversion = '19.11.06.001';
+if( CheckVersion( $DBversion ) ) {
+
+    # Default to the homologous OpacPublic syspref
+    my $opac_public = C4::Context->preference('OpacPublic') ? 1 : 0;
+
+    $dbh->do(qq{
+        INSERT IGNORE INTO `systempreferences`
+            (`variable`,`value`,`explanation`,`options`,`type`)
+        VALUES
+            ('RESTPublicAnonymousRequests', $opac_public, NULL,'If enabled, the API will allow anonymous access to public routes that don\'t require authenticated access'.','YesNo');
+    });
+
+    NewVersion( $DBversion, 25045, "Add a way to restrict anonymous access to public routes (OpacPublic behaviour)");
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
