93ad4e2d52290607b0fbd023e41e1ccc42c950c4
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2020-06-29 09:54:58 +0100

    Bug 25850: (QA follow-up) Match logic in is_holiday
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    (cherry picked from commit afd88e1d21a5cb5f2198c058144065b64098c124)
    
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>
    (cherry picked from commit 4c0d7b14bebcedb802488ee1fb200a2c7513ca91)
    
    Signed-off-by: Aleisha Amohia <aleishaamohia@hotmail.com>

--- Koha/Calendar.pm
+++ Koha/Calendar.pm
@@ -236,11 +236,18 @@ sub get_push_amt {
     my ( $self, $base_date) = @_;
 
     my $dow = $base_date->day_of_week;
+    # Representation fix
+    # DateTime object dow (1-7) where Monday is 1
+    # Arrays are 0-based where 0 = Sunday, not 7.
+    if ( $dow == 7 ) {
+        $dow = 0;
+    }
+
     return (
         # We're using Dayweek useDaysMode option
         $self->{days_mode} eq 'Dayweek' &&
         # It's not a permanently closed day
-        !$self->{weekly_closed_days}->[$dow % 7]
+        !$self->{weekly_closed_days}->[$dow]
     ) ? 7 : 1;
 }
 
