e68fe2a928bec05b9602fa28dfbc86e04657ce81
Owen Leonard <oleonard@myacpl.org>
2020-07-10 13:19:14 +0000

    Bug 25968: Make logs sort by date descending as a default
    This patch adds DataTables with columns configuration to the log viewer.
    The table of log entries is now sorted by default by date descending.
    
    To test, apply the patch and go to Administration -> Table settings.
    
     - Expand the "Tools" section and confirm that there is a section for
       "logviewer."
     - Go to Tools -> Log viewer.
     - Perform a search which will return multiple results.
     - Confirm that the results are sorted by date descending.
     - Test that the table controls work correctly: Paging, columns, export.
     - Return to the Table settings administration page and test that
       changes to those settings are correctly reflected in the log viewer
       table.
    
    Signed-off-by: barbara <barbara.johnson@bedfordtx.gov>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    JD amended patch: Fix indentation in .yml to match other entries
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- koha-tmpl/intranet-tmpl/prog/js/viewlog.js
+++ koha-tmpl/intranet-tmpl/prog/js/viewlog.js
@@ -61,4 +61,10 @@ $(document).ready(function(){
             tickAll('interfaces');
         }
     });
+
+    var logst = KohaTable("logst", {
+        "autoWidth": false,
+        "order": [[0, "desc"]],
+        "pagingType" : "full"
+    }, columns_settings);
 });
