7efb135dfb3d16c8046877de1a47cd1a835bf5d9
Aleisha Amohia <aleishaamohia@hotmail.com>
2020-07-16 16:14:40 +1200

    Bug 24986: DBRev 19.11.07.001
    Signed-off-by: Aleisha Amohia <aleishaamohia@hotmail.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -20609,6 +20609,30 @@ if ( CheckVersion($DBversion) ) {
     SetVersion ($DBversion);
 }
 
+$DBversion = '19.11.07.001';
+if( CheckVersion( $DBversion ) ) {
+
+    for my $f (qw( streetnumber streettype zipcode mobile B_streetnumber B_streettype B_zipcode ) ) {
+        $dbh->do(qq|
+            ALTER TABLE borrowers MODIFY $f TINYTEXT DEFAULT NULL
+        |);
+        $dbh->do(qq|
+            ALTER TABLE deletedborrowers MODIFY $f TINYTEXT DEFAULT NULL
+        |);
+    }
+    for my $f ( qw( B_address altcontactfirstname altcontactsurname altcontactaddress1 altcontactaddress2 altcontactaddress3 altcontactzipcode altcontactphone ) ) {
+        $dbh->do(qq|
+            ALTER TABLE borrowers MODIFY $f MEDIUMTEXT DEFAULT NULL
+        |);
+        $dbh->do(qq|
+            ALTER TABLE deletedborrowers MODIFY $f MEDIUMTEXT DEFAULT NULL
+        |);
+    }
+
+    SetVersion( $DBversion );
+    print "Upgrade to $DBversion done (Bug 24986 - Switch borrowers address related fields to TINYTEXT or MEDIUMTEXT)\n";
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
