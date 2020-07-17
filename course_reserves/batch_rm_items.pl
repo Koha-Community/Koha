3bf7430d1c41bf9acb363f4fff9476f6ee653fdd
Joseph Sikowitz <joe@flo.org>
2020-07-17 11:34:30 -0400

    Bug 14648: Batch remove reserve items
    Adds "Batch remove items" button to course-reserves.tt. Creates batch_rm_items.pl and batch_rm_items.tt. Allows user to enter a list of
    barcode numbers so that they will be removed from all course reserves to which they are attached. A dialog box will provide a warning
    before the final deletion is made.
    
    This patch also now moves barcodes that are invalid or not attached to a course reserve to the invalid barcodes output. It also provides
    a count of the courses that the deleted items were attached to.
    
    Test Plan
    1. Log in as a user with permissions to add and delete course reserves
    2. Add the same items to multiple course reserves
    3. Go to the main course reserves page (i.e. /cgi-bin/koha/course_reserves/course-reserves.pl)
    4. Click the "- Batch remove items" button
    5. Enter the barcodes that you added to multiple reserves in step 1
    6. Click submit
    7. Click Ok if you are ready to delete all of these items
    8. Check each course reserve that you added the items to in step 1 to make sure the items you entered were deleted but nothing else
    9. Repeat 2-5
    10. Add an invalid barcode and a valid barcode that is not attached to a course reserve
    11. Repeat 6-7
    12. You should see that the items attached to course reserves were removed while the invalid and unattached barcodes were rejected
    13. Log out
    14. Log in as a user without permission to delete course reserves
    15. Go to the main course reserves page (i.e. /cgi-bin/koha/course_reserves/course-reserves.pl)
    16. You should not longer see the "- Batch remove items" button
    
    Signed-off-by: David Nind <david@davidnind.com>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

---
+++ course_reserves/batch_rm_items.pl
@@ -0,0 +1,90 @@
+#!/usr/bin/perl
+
+# This file is part of Koha.
+#
+# Copyright (C) 2020  Fenway Library Organization
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+use CGI qw( -utf8 );
+use List::MoreUtils qw( uniq );
+
+use C4::Auth;
+use C4::Output;
+use C4::CourseReserves qw(GetItemCourseReservesInfo DelCourseReserve GetCourseItem);
+
+use Koha::Items;
+
+my $cgi = new CGI;
+
+my $action    = $cgi->param('action')    || q{};
+my $barcodes  = $cgi->param('barcodes')  || q{};
+
+my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
+    {
+        template_name   => "course_reserves/batch_rm_items.tt",
+        query           => $cgi,
+        type            => "intranet",
+        flagsrequired   => { coursereserves => 'delete_reserves' },
+    }
+);
+
+if ( !$action ) {
+    $template->param( action => 'display_form' );
+}
+
+elsif ( $action eq 'batch_rm' ) {
+    my @barcodes = uniq( split (/\s\n/, $barcodes ) );
+    my @invalid_barcodes;
+    my @item_and_count;
+
+    foreach my $bar (@barcodes) {
+        my $item = Koha::Items->find( { barcode => $bar } );
+        if($item) {
+            my $courseitem = GetCourseItem(itemnumber => $item->id);
+            if($courseitem) {
+
+                my $res_info = GetItemCourseReservesInfo(itemnumber => $item->id);
+
+                my $no_of_res = @$res_info;
+
+                my $delitemcount = {'delitem' => $item, 'delcount' => $no_of_res};
+                push ( @item_and_count, $delitemcount );
+
+                foreach my $cr (@$res_info) {
+                    if($cr->{cr_id}) {
+                        DelCourseReserve('cr_id' => $cr->{cr_id});
+                    }
+                }
+            }
+            else {
+                push( @invalid_barcodes, $bar);
+            }
+        }
+        else {
+            push( @invalid_barcodes, $bar );
+        }
+
+    }
+
+    $template->param(
+        action => 'display_results',
+        invalid_barcodes => \@invalid_barcodes,
+        item_and_count => \@item_and_count,
+    );
+}
+
+output_html_with_http_headers $cgi, $cookie, $template->output;
