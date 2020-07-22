68af6695b028dbdbc2a9ac035bd1097a90c7c3e3
Aleisha Amohia <aleishaamohia@hotmail.com>
2020-07-22 12:53:18 +1200

    Increment version for 19.11.08 release
    Signed-off-by: Aleisha Amohia <aleishaamohia@hotmail.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -20633,6 +20633,12 @@ if( CheckVersion( $DBversion ) ) {
     print "Upgrade to $DBversion done (Bug 24986 - Switch borrowers address related fields to TINYTEXT or MEDIUMTEXT)\n";
 }
 
+$DBversion = "19.11.08.000";
+if ( CheckVersion($DBversion) ) {
+    print "Upgrade to $DBversion done (19.11.08 release)\n";
+    SetVersion ($DBversion);
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
