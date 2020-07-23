8f819ac9b97bd6c9872259f2b614543724fefbbf
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2020-07-23 10:35:13 +0200

    Bug 23795: DBRev 20.06.00.012
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -22376,6 +22376,36 @@ if( CheckVersion( $DBversion ) ) {
     NewVersion( $DBversion, 5087, "Add export_format.staff_only" );
 }
 
+$DBversion = '20.06.00.012';
+if( CheckVersion( $DBversion ) ) {
+
+    # get list of installed translations
+    require C4::Languages;
+    my @langs;
+    my $tlangs = C4::Languages::getTranslatedLanguages('opac','bootstrap');
+
+    foreach my $language ( @$tlangs ) {
+        foreach my $sublanguage ( @{$language->{'sublanguages_loop'}} ) {
+            push @langs, $sublanguage->{'rfc4646_subtag'};
+        }
+    }
+
+    # Get any existing value from the opaccredits system preference
+    my ($opaccredits) = $dbh->selectrow_array( q|
+        SELECT value FROM systempreferences WHERE variable='opaccredits';
+    |);
+    if( $opaccredits ){
+        foreach my $lang ( @langs ) {
+            # If there is a value in the opaccredits preference, insert it into opac_news
+            $dbh->do("INSERT IGNORE INTO opac_news (branchcode, lang, title, content ) VALUES (NULL, ?, '', ?)", undef, "opaccredits_$lang", $opaccredits);
+        }
+    }
+    # Remove the opaccredits system preference
+    $dbh->do("DELETE FROM systempreferences WHERE variable='opaccredits'");
+
+    NewVersion( $DBversion, 23795, "Convert OpacCredits system preference to news block" );
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
