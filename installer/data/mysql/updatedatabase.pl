f0a0f630bb523cfcda6efa82049608474e09fb15
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2020-07-23 10:36:54 +0200

    Bug 23796: DBRev 20.06.00.013
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -22406,6 +22406,37 @@ if( CheckVersion( $DBversion ) ) {
     NewVersion( $DBversion, 23795, "Convert OpacCredits system preference to news block" );
 }
 
+$DBversion = '20.06.00.013';
+if( CheckVersion( $DBversion ) ) {
+
+    # get list of installed translations
+    require C4::Languages;
+    my @langs;
+    my $tlangs = C4::Languages::getTranslatedLanguages('opac','bootstrap');
+
+    foreach my $language ( @$tlangs ) {
+        foreach my $sublanguage ( @{$language->{'sublanguages_loop'}} ) {
+            push @langs, $sublanguage->{'rfc4646_subtag'};
+        }
+    }
+
+    # Get any existing value from the OpacCustomSearch system preference
+    my ($OpacCustomSearch) = $dbh->selectrow_array( q|
+        SELECT value FROM systempreferences WHERE variable='OpacCustomSearch';
+    |);
+    if( $OpacCustomSearch ){
+        foreach my $lang ( @langs ) {
+            # If there is a value in the OpacCustomSearch preference, insert it into opac_news
+            $dbh->do("INSERT INTO opac_news (branchcode, lang, title, content ) VALUES (NULL, ?, '', ?)", undef, "OpacCustomSearch_$lang", $OpacCustomSearch);
+        }
+    }
+    # Remove the OpacCustomSearch system preference
+    $dbh->do("DELETE FROM systempreferences WHERE variable='OpacCustomSearch'");
+
+    NewVersion( $DBversion, 23795, "Convert OpacCustomSearch system preference to news block" );
+}
+
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
