adf8e12eb132208d1401d150f1f0b77fca912c32
Aleisha Amohia <aleishaamohia@hotmail.com>
2020-08-05 11:46:48 +1200

    Bug 25709: DBRev 19.11.08.001 - fixing output message

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -20642,7 +20642,7 @@ if ( CheckVersion($DBversion) ) {
 $DBversion = "19.11.08.001";
 if( CheckVersion( $DBversion ) ) {
     $dbh->do( "UPDATE systempreferences SET variable='NotesToHide' WHERE variable = 'NotesBlacklist'" );
-    print "Bug 25709: Rename systempreference to NotesToHide\n";
+    print "Upgrade to $DBversion done (Bug 25709 - Rename systempreference to NotesToHide)\n";
     SetVersion( $DBversion );
 }
 
