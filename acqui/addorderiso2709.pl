12e1b2faccf6396d3c4b5327c881e78f177f135b
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2020-08-06 12:50:36 +0200

    Bug 26134: (bug 23463 follow-up) Fix add to basket from staged file
    There is a ->store call missing when we attempt to create a new item.
    
    Test plan:
    - set at least one value in MARCItemFieldsToOrder
    - add to a basket via staged file with at least one value matching your MARCItemFields mappings
    - set required fields for your order and click Save
    => Without this patch you got:
    receive error: "DBIx::Class::Storage::DBI::_dbh_execute(): Column 'itemnumber' cannot be null at /kohadevbox/koha/Koha/Acquisition/Order.pm line 113
     at /usr/share/perl5/DBIx/Class/Exception.pm line 77"
    => With this patch applied the order has been created successfully
    
    Signed-off-by: Andrew Fuerste-Henry <andrew@bywatersolutions.com>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- acqui/addorderiso2709.pl
+++ acqui/addorderiso2709.pl
@@ -237,7 +237,7 @@ if ($op eq ""){
                     replacementprice    => $replacementprices[$i],
                     itemcallnumber      => $itemcallnumbers[$i],
                 }
-            );
+            )->store;
             push( @itemnumbers, $item->itemnumber );
         }
         if ($itemcreation == 1) {
