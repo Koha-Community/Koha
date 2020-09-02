72deefe8cf4f2d1cc24e7577ef2d2b8a44945f18
Nick Clemens <nick@bywatersolutions.com>
2020-09-02 17:32:42 +0000

    Bug 24197: (QA follow-up) Shorten syspref name to AddressForFailedEmailNotices
    If you define the address we use it, if not we fallback,
    it's not really a redirect, and that just makes the name longer.
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- misc/cronjobs/overdue_notices.pl
+++ misc/cronjobs/overdue_notices.pl
@@ -455,7 +455,7 @@ foreach my $branchcode (@branches) {
     my $library             = Koha::Libraries->find($branchcode);
     my $admin_email_address = $library->branchemail
       || C4::Context->preference('KohaAdminEmailAddress');
-    my $branch_email_address = C4::Context->preference('RedirectAddressForFailedOverdueNotices')
+    my $branch_email_address = C4::Context->preference('AddressForFailedOverdueNotices')
       || $library->inbound_email_address;
     my @output_chunks;    # may be sent to mail or stdout or csv file.
 
