f9a2be7ef7c274c24bf5aa5bf40c6d61387c257d
Fridolin Somers <fridolin.somers@biblibre.com>
2020-09-04 14:49:40 +0200

    Bug 18989: Add atomic update
    Signed-off-by: Kelly McElligott <kelly@bywatersolutions.com>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

---
+++ installer/data/mysql/atomicupdate/bug_18989.sql
@@ -0,0 +1,2 @@
+INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type` ) VALUES
+('OpacHiddenItemsHidesRecord','0','','Hide biblio record when all its items are hidden because of OpacHiddenItems','YesNo');
