debf8f804c6bc47777f1a239e6b20f78c847e0b2
Timothy Alexis Vass <timothy_alexis.vass@ub.lu.se>
2020-09-16 08:35:15 +0200

    Bug 26454: Add system preference to set meta description for the OPAC
    Websites should have a meta description tag that shows in search engine results.
    The content should also be available to set as a system preference.
    
    To test:
    1) Look at opac-main source (in the browser) and see that meta name="description" is missing.
    2) Apply patch.
    3) Run: perl installer/data/mysql/updatedatabase.pl
    4) Go to system preferences > OPAC and add a description to OpacMetaDescription.
    5) Look at opac-main source and confirm that meta name="description" now has content equal to the text set in the OpacMetaDescription system preference.
    6) Sign off.
    
    Sponsored by: Lunds University Libraries
    Signed-off-by: Magnus Enger <magnus@libriotech.no>
    Works as advertised. Newlines are preserved. Double quote is rendered
    as &quot;. Nice enhancement!
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- installer/data/mysql/sysprefs.sql
+++ installer/data/mysql/sysprefs.sql
@@ -414,6 +414,7 @@ INSERT INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `
 ('OpacMaintenance','0','','If ON, enables maintenance warning in OPAC','YesNo'),
 ('OpacMaintenanceNotice','','','A user-defined block of HTML to appear on screen when OpacMaintenace is enabled','Textarea'),
 ('OpacMaxItemsToDisplay','50','','Max items to display at the OPAC on a biblio detail','Integer'),
+('OpacMetaDescription','','','This description will show in search engine results (160 characters).','Textarea'),
 ('OpacMoreSearches', '', NULL, 'Add additional elements to the OPAC more searches bar', 'Textarea'),
 ('OPACMySummaryHTML','','70|10','Enter the HTML that will appear in a column on the \'my summary\' and \'my reading history\' tabs when a user is logged in to the OPAC. Enter {BIBLIONUMBER}, {TITLE}, {AUTHOR}, or {ISBN} in place of their respective variables in the HTML. Leave blank to disable.','Textarea'),
 ('OPACMySummaryNote','','','Note to display on the patron summary page. This note only appears if the patron is connected.','Free'),
