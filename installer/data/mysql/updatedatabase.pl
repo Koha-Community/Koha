b9a937d7856c2be915d43d5ecbb29a5e686af96c
Victor Grousset/tuxayo <victor@tuxayo.net>
2020-09-22 15:35:30 +0200

    Increment version for 19.05.15 release
    Signed-off-by: Victor Grousset/tuxayo <victor@tuxayo.net>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -19153,6 +19153,12 @@ if( CheckVersion( $DBversion ) ) {
     SetVersion( $DBversion );
 }
 
+$DBversion = "19.05.15.000";
+if ( CheckVersion($DBversion) ) {
+    print "Upgrade to $DBversion done (19.05.15 release)\n";
+    SetVersion ($DBversion);
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 
