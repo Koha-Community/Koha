ae21c3adb16c0604c995a08a4082c3d1d60ab822
Owen Leonard <oleonard@myacpl.org>
2020-09-22 11:01:30 +0000

    Bug 26505: Suspend hold modal broken in the OPAC
    This patch makes corrections to the markup for the suspend hold modal in
    the OPAC. It hadn't been updated during the upgrade to Bootstrap 4.
    
    The JavaScript controlling the "suspend until" datepicker has been
    modified because the datepicker pop-up was not positioned correctly in
    this new version.
    
    Unrelated: The markup for confirmation modals has been updated to
    replace Bootstrap 3's "btn-default" with Bootstrap 4's "btn-secondary."
    
    To test, apply the patch and make sure the SuspendHoldsOpac and
    AutoResumeSuspendedHolds system preferences are enabled.
    
    - Log in to the OPAC as a user who has holds.
    - On the "Your summary" page open the "Holds" tab.
    - In the list of holds, click the "Suspend" button.
    - The modal should appear and look correct.
    - Test the "Suspend until" field: Clicking in the form field should
      trigger the datepicker. It should be positioned correctly under the
      form field.
    - Confirm that the datepicker populates the field.
    - Submit the suspension and confirm that the hold is suspended.
    - Click the "Cancel" button for a hold. Confirm that the confirmation
      dialog appears correctly.
    
    Edit: Updated class of hidden submit button. Test the page with JS
    disabled to test that the "Suspend until" form works correctly.
    
    Signed-off-by: David Nind <david@davidnind.com>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- koha-tmpl/opac-tmpl/bootstrap/js/global.js
+++ koha-tmpl/opac-tmpl/bootstrap/js/global.js
@@ -90,7 +90,7 @@ function confirmModal(message, title, yes_label, no_label, callback) {
                     <div class="modal-body"><p></p></div>\
                     <div class="modal-footer">\
                         <a href="#" id="bootstrap-confirm-box-modal-submit" class="btn btn-danger"><i class="fa fa-check" aria-hidden="true"></i></a>\
-                        <a href="#" id="bootstrap-confirm-box-modal-cancel" data-dismiss="modal" class="btn btn-default"><i class="fa fa-remove" aria-hidden="true"></i></a>\
+                        <a href="#" id="bootstrap-confirm-box-modal-cancel" data-dismiss="modal" class="btn btn-secondary"><i class="fa fa-remove" aria-hidden="true"></i></a>\
                     </div>\
                 </div>\
             </div>\
