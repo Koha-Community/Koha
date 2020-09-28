b1f2c0ca9f43a4d61f0f995f966eae84ae8018e2
Lucas Gass <lucas@bywatersolutions.com>
2020-09-28 15:18:15 +0000

    Bug 26548: Fix typo in updatedatabase
    Test plan:
    1. On 20.05.x run updatedatbase and see an error
    2. Apply patch
    3. Re-run updatedatabase without an error
    
    https://bugs.koha-community.org/show_bug.cgi?id=25958
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>
    
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -22387,7 +22387,7 @@ $DBversion = '20.05.03.001';
 if( CheckVersion( $DBversion ) ) {
     $dbh->do(q{
         INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type` ) VALUES
-        ('DefaultLongOverdueSkipLostStatuses', '', NULL, 'Skip these lost statuses by default in longoverdue.pl', 'Free'),
+        ('DefaultLongOverdueSkipLostStatuses', '', NULL, 'Skip these lost statuses by default in longoverdue.pl', 'Free')
     });
 
     NewVersion( $DBversion, 25958, "Allow LongOverdue cron to exclude specified lost values");
