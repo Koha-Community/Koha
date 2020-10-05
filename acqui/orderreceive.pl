81e94d71102bc80881539ac122f94ac8fb7de8c4
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2020-10-05 16:47:00 +0100

    Bug 23376: (QA follow-up) Remove authnotrequired
    The rebased patchset subsequnetly re-introduced the now deprecated use
    of authnotrequired on staff client controllers.
    
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- acqui/orderreceive.pl
+++ acqui/orderreceive.pl
@@ -96,7 +96,6 @@ my ( $template, $loggedinuser, $cookie, $userflags ) = get_template_and_user(
         template_name   => "acqui/orderreceive.tt",
         query           => $input,
         type            => "intranet",
-        authnotrequired => 0,
         flagsrequired   => {acquisition => 'order_receive'},
         debug           => 1,
     }
