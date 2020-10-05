2e08fa0b1877a2c0d68ff98f7713cac697e99c41
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2020-10-05 13:27:52 +0000

    Bug 15032: DBRev 20.06.00.045
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -22923,6 +22923,36 @@ if( CheckVersion( $DBversion ) ) {
     NewVersion( $DBversion, 22343, "Add SMTP configuration options");
 }
 
+$DBversion = '20.06.00.045';
+if( CheckVersion( $DBversion ) ) {
+
+    unless ( TableExists('background_jobs') ) {
+        $dbh->do(q|
+            CREATE TABLE background_jobs (
+                id INT(11) NOT NULL AUTO_INCREMENT,
+                status VARCHAR(32),
+                progress INT(11),
+                size INT(11),
+                borrowernumber INT(11),
+                type VARCHAR(64),
+                data TEXT,
+                enqueued_on DATETIME DEFAULT NULL,
+                started_on DATETIME DEFAULT NULL,
+                ended_on DATETIME DEFAULT NULL,
+                PRIMARY KEY (id)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+        |);
+    }
+
+    $dbh->do(qq{
+        INSERT IGNORE permissions (module_bit, code, description)
+        VALUES
+        (3, 'manage_background_jobs', 'Manage background jobs')
+    });
+
+    NewVersion( $DBversion, 15032, "Add new table background_jobs");
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
