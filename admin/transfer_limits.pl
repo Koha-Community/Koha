fd575181480dc822e1903959962c2a248e147584
Kyle M Hall <kyle@bywatersolutions.com>
2020-10-08 11:15:44 -0400

    Bug 26633: Add advanced editor for transfer limits
    The current transfer limits editor works great for Koha instances with
    small numbers of branches. However, for consortiums with dozens or even
    hundreds of libraries, the editor does not work well or effectively.
    
    We should provide an "advanced" editor displays all to/from library
    combinations in a grid and allows them to be edited in a manner somewhat
    similar to the transport cost matrix editor.
    
    Test Plan:
    1) Apply this patch
    2) Browse to the transfer limits editor
    3) Click the new "Switch to advanced editor" link
    4) Select a collection code/item type to edit limits for
    5) Test the "Check all" function
    6) Test the "Uncheck all" function
    7) Test the "Check" column function
    8) Test the "Uncheck" column function
    9) Test the "Check" row function
    10) Test the "Uncheck" row function
    11) Test individual checkboxes/table cells
    
    Signed-off-by: Lisette Scheer <lisettes@latahlibrary.org>
    
    Signed-off-by: Lisette Scheer <lisettes@latahlibrary.org>
    
    Signed-off-by: Lisette Scheer <lisettes@latahlibrary.org>
    
    Signed-off-by: Lisette Scheer <lisettes@latahlibrary.org>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

---
+++ admin/transfer_limits.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+
+# copyright 2020 ByWater Solutions
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+use CGI qw ( -utf8 );
+use C4::Auth;
+use C4::Context;
+use C4::Output;
+use C4::Koha;
+use C4::Circulation
+  qw{ IsBranchTransferAllowed DeleteBranchTransferLimits CreateBranchTransferLimit };
+
+my $input = new CGI;
+
+my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
+    {
+        template_name => "admin/transfer_limits.tt",
+        query         => $input,
+        type          => "intranet",
+        flagsrequired => { parameters => 'manage_transfers' },
+        debug         => 1,
+    }
+);
+
+output_html_with_http_headers $input, $cookie, $template->output;
