ab6494fc3de5fc4f325941baf1c65f67634b1c1b
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2020-10-09 11:04:19 +0000

    Bug 23420: DBRev 20.06.00.047
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -22969,6 +22969,36 @@ if( CheckVersion( $DBversion ) ) {
     NewVersion( $DBversion, 13535, "Add FK constraint on borrowernumber to alert table" );
 }
 
+$DBversion = '20.06.00.047';
+if ( CheckVersion($DBversion) ) {
+
+    #Get value from AllowPurchaseSuggestionBranchChoice system preference
+    my ($allowpurchasesuggestionbranchchoice) =
+      C4::Context->preference('AllowPurchaseSuggestionBranchChoice');
+    if ($allowpurchasesuggestionbranchchoice) {
+        $dbh->do(q{
+            INSERT IGNORE INTO systempreferences
+            (`variable`, `value`, `options`, `explanation`, `type`)
+            VALUES
+            ('OPACSuggestionUnwantedFields','branch', NULL,'Define the hidden fields for a patron purchase suggestions made via OPAC.','multiple');
+        });
+    }
+    else {
+        $dbh->do(q{
+            INSERT IGNORE INTO systempreferences
+            (`variable`, `value`, `options`, `explanation`, `type`)
+            VALUES
+            ('OPACSuggestionUnwantedFields','', NULL,'Define the hidden fields for a patron purchase suggestions made via OPAC.','multiple');
+        });
+    }
+
+    #Remove the  AllowPurchaseSuggestionBranchChoice system preference
+    $dbh->do(
+        "DELETE FROM systempreferences WHERE variable='AllowPurchaseSuggestionBranchChoice'"
+    );
+    NewVersion( $DBversion, 23420, "Allow configuration of hidden fields on the suggestion form in OPAC" );
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
