3a5d5367f10fa1101e99748aadf0c076ebd9dea7
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2020-10-25 23:12:15 +0000

    Bug 25596: DBRev 20.06.00.054
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -23081,6 +23081,32 @@ if( CheckVersion( $DBversion ) ) {
     NewVersion( $DBversion, 26569, "Use gender neutral pronouns in system preference explanations" );
 }
 
+$DBversion = '20.06.00.054';
+if ( CheckVersion($DBversion) ) {
+
+    $dbh->do(
+        qq{
+            INSERT IGNORE INTO account_credit_types (code, description, can_be_added_manually, is_system)
+            VALUES
+              ('OVERPAYMENT', 'Overpayment refund', 0, 1)
+        }
+    );
+
+    $dbh->do(
+        qq{
+            INSERT IGNORE INTO account_offset_types ( type ) VALUES ('Overpayment');
+        }
+    );
+
+    $dbh->do(
+        qq{
+            UPDATE accountlines SET credit_type_code = 'OVERPAYMENT' WHERE credit_type_code = 'CREDIT' AND description = 'Overpayment refund'
+        }
+    );
+
+    NewVersion( $DBversion, 25596, "Add OVERPAYMENT credit type" );
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
