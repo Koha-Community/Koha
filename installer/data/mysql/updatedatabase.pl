476865d0fe77d4294f0efee803a8f2959e5446ea
Aleisha Amohia <aleishaamohia@hotmail.com>
2020-10-27 18:19:24 +1300

    Increment version for 19.11.11 release
    Signed-off-by: Aleisha Amohia <aleishaamohia@hotmail.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -20697,6 +20697,12 @@ if( CheckVersion( $DBversion ) ) {
     SetVersion( $DBversion );
 }
 
+$DBversion = "19.11.11.000";
+if ( CheckVersion( $DBversion ) ) {
+    print "Upgrade to $DBversion done (19.11.11 release)\n";
+    SetVersion( $DBversion );
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
