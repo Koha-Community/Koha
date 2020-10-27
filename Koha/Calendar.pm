Koha-Community/Koha

commit a693c7243c23f888e2fad38a4fff9f37ff4a9301

Bug 23382: hours_between should match the logic of days_between
The loops for subtraction holiday dates in hours_between and
days_between differed and as such their handling of start and end
boundaries for days also differed.  This patch makes them handle the
boundary days consistently.

Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

Koha/Calendar.pm
@@ -344,21 +344,17 @@ sub hours_between {
     # However for hourly loans the logic should be expanded to
     # take into account open/close times then it would be a duration
     # of library open hours
+    # start and end should not be closed days
     my $skipped_days = 0;
-    for (my $dt = $start_dt->clone();
-        $dt <= $end_dt;
-        $dt->add(days => 1)
-    ) {
-        if ($self->is_holiday($dt)) {
-            ++$skipped_days;
-        }
+    while( $start_dt->compare($end_dt) < 1 ) {
+        $start_dt->add( days => 1 );
+        $skipped_days++ if $self->is_holiday($start_dt);
     }
     if ($skipped_days) {
         $duration->subtract_duration(DateTime::Duration->new( hours => 24 * $skipped_days));
     }
 
     return $duration;
-
 }
 
 sub set_daysmode {

commit 0d3bb28015cdd17d40f1fb613f58cafea20a81a0

Bug 25665: (Bug 23382: (RM follow-up) Revert "hours_between should match...")
This reverts commit a693c7243c23f888e2fad38a4fff9f37ff4a9301 which
caused regressions.

The original loop compared start date to end date and iterated all the
way to start date equals end date. The alternate implimentation
inadvertantly looped from start date, skipped the first day then
iterated up to one day beyond end date.

Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
Signed-off-by: Victor Grousset/tuxayo <victor@tuxayo.net>

Koha/Calendar.pm
@@ -345,17 +345,21 @@ sub hours_between {
     # However for hourly loans the logic should be expanded to
     # take into account open/close times then it would be a duration
     # of library open hours
-    # start and end should not be closed days
     my $skipped_days = 0;
-    while( $start_dt->compare($end_dt) < 1 ) {
-        $start_dt->add( days => 1 );
-        $skipped_days++ if $self->is_holiday($start_dt);
+    for (my $dt = $start_dt->clone();
+        $dt <= $end_dt;
+        $dt->add(days => 1)
+    ) {
+        if ($self->is_holiday($dt)) {
+            ++$skipped_days;
+        }
     }
     if ($skipped_days) {
         $duration->subtract_duration(DateTime::Duration->new( hours => 24 * $skipped_days));
     }
 
     return $duration;
+
 }
 
 sub set_daysmode {