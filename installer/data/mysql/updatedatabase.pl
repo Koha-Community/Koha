76e4790d26b7bae6777f4ba33cc520518f0f02f0
Lucas Gass <lucas@bywatersolutions.com>
2020-10-27 20:37:08 +0000

    Increment version for 20.05.05 release
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>
    (cherry picked from commit d59ec16fd07aa95bc870ad06b7b568c38fb4bcc0)
    
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -22419,6 +22419,11 @@ if( CheckVersion( $DBversion ) ) {
     NewVersion( $DBversion, 18958, "Add reserve_id to hold_fill_targets");
 }
 
+$DBversion = '20.05.05.000';
+if( CheckVersion( $DBversion ) ) {
+    NewVersion( $DBversion, undef, '20.05.05 release' );
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
