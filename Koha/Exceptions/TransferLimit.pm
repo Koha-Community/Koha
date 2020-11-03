82de600f6dfd9bbe5e272ee25b54fb2b4c519a35
Tomas Cohen Arazi <tomascohen@theke.io>
2020-11-03 11:14:26 -0300

    Bug 26633: POD for the exceptions file
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- Koha/Exceptions/TransferLimit.pm
+++ Koha/Exceptions/TransferLimit.pm
@@ -1,5 +1,20 @@
 package Koha::Exceptions::TransferLimit;
 
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
 use Modern::Perl;
 
 use Exception::Class (
@@ -13,5 +28,21 @@ use Exception::Class (
         description => 'A transfer limit with the given parameters already exists!',
     },
 );
+
+=head1 NAME
+
+Koha::Exceptions::TransferLimit - Base class for transfer limits exceptions
+
+=head1 Exceptions
+
+=head2 Koha::Exceptions::TransferLimit
+
+Generic transfer limit exception
+
+=head2 Koha::Exceptions::TransferLimit::Duplicate
+
+Exception to be used when trying to store an already existing transfer limit.
+
+=cut
 
 1;
