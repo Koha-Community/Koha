Koha-Community/Koha

commit a2449a81be55aa5b3710ca2c6df7884513f89540

Bug 23051: (follow-up) Add renewal feedback and move code to subroutines and test
Rebasing was a nightmare, so I'm squashing the sign off follow-ups to
ease the pain with any future rebases

Includes:

Bug 23051: (follow-up) Refactor renewal code
As per Nick's first point in comment #20, the code that tests for
renewability and renews items has been refactored into it's own
function.

Bug 23051: (follow-up) Provide feedback
For renewals that fail when a fine is being paid off, this patch causes
any errors to be passed back to the template for display.
Addresses the second point in Nick's comment #20

Bug 23051: (follow-up) Fix unit tests
As raised by Nick in comment #35

Bug 23051: (follow-up) Fix/improve feedback
This follow up patch addresses the following parts of Nick's feedback in
comment #35:
- it would be nice to get feedback on what was successfully renewed as well
- In general I think I would prefer to see 'ok' and 'not_ok' returned as
a single 'renewal_results' array
- There is no listing of errors if I use the 'pay' button on an
individual fine

Bug 23051: (follow-up) Refactor methods
This follow up patch addresses the following parts of Nick's feedback in
comment #35:
- I don't really like that the functions are internal functions and then
exported
- I think the pref description should highlight that if 'RenewalPeriodBase'
is set to due date, there may be doubled charges

Bug 23051: (follow-up) Add SIP summary
This follow up patch addresses the following parts of Nick's feedback in
comment #35:
- Ideally SIP would get feedback in a screen message

Bug 23051: (follow-up) Renewing in OPAC
This follow up patch addresses the following parts of Nick's feedback in
comment #35:
- I am also not sure about the code path if a patron paid fines on the
opac (via paypal etc.) but renewals are not allowed on the opac.

We've introduced the syspref RenewAccruingItemInOpac (default is off)
which, when enabled, will cause items attached to fines that are paid

members/pay.pl
@@ -39,6 +39,7 @@ use C4::Stats;
 use C4::Koha;
 use C4::Overdues;
 use Koha::Patrons;
+use Koha::Items;
 
 use Koha::Patron::Categories;
 use URI::Escape;
@@ -65,6 +66,7 @@ if ( !$borrowernumber ) {
 
 my $payment_id = $input->param('payment_id');
 our $change_given = $input->param('change_given');
+our @renew_results = $input->multi_param('renew_result');
 
 # get borrower details
 my $logged_in_user = Koha::Patrons->find( $loggedinuser );
@@ -135,10 +137,27 @@ for (@names) {
     }
 }
 
+# Populate an arrayref with everything we need to display any
+# renew results that occurred based on what we were passed
+my $renew_results_display = [];
+foreach my $renew_result(@renew_results) {
+    my ($itemnumber, $success, $info) = split(/,/, $renew_result);
+    my $item = Koha::Items->find($itemnumber);
+    if ($success) {
+        $info = uri_unescape($info);
+    }
+    push @{$renew_results_display}, {
+        item => $item,
+        success => $success,
+        info => $info
+    };
+}
+
 $template->param(
     finesview  => 1,
     payment_id => $payment_id,
     change_given => $change_given,
+    renew_results => $renew_results_display
 );
 
 add_accounts_to_template();

C4/SIP/ILS.pm
@@ -271,10 +271,14 @@ sub pay_fee {
         $trans->screen_msg('Invalid patron barcode.');
         return $trans;
     }
-    my $ok = $trans->pay( $patron->{borrowernumber}, $fee_amt, $pay_type, $fee_id, $is_writeoff, $disallow_overpayment );
+    my $trans_result = $trans->pay( $patron->{borrowernumber}, $fee_amt, $pay_type, $fee_id, $is_writeoff, $disallow_overpayment );
+    my $ok = $trans_result->{ok};
     $trans->ok($ok);
 
-    return $trans;
+    return {
+        status       => $trans,
+        pay_response => $trans_result->{pay_response}
+    };
 }
 
 sub add_hold {

C4/SIP/ILS/Transaction/FeePayment.pm
@@ -57,13 +57,13 @@ sub pay {
     my $account = Koha::Account->new( { patron_id => $borrowernumber } );
 
     if ($disallow_overpayment) {
-        return 0 if $account->balance < $amt;
+        return { ok => 0 } if $account->balance < $amt;
     }
 
     if ($fee_id) {
         my $fee = Koha::Account::Lines->find($fee_id);
         if ( $fee ) {
-            $account->pay(
+            my $pay_response = $account->pay(
                 {
                     amount       => $amt,
                     type         => $type,
@@ -72,14 +72,19 @@ sub pay {
                     interface    => C4::Context->interface
                 }
             );
-            return 1;
+            return {
+                ok           => 1,
+                pay_response => $pay_response
+            };
         }
         else {
-            return 0;
+            return {
+                ok => 0
+            };
         }
     }
     else {
-        $account->pay(
+        my $pay_response = $account->pay(
             {
                 amount       => $amt,
                 type         => $type,
@@ -87,7 +92,10 @@ sub pay {
                 interface    => C4::Context->interface
             }
         );
-        return 1;
+        return {
+            ok           => 1,
+            pay_response => $pay_response
+        };
     }
 }
 

C4/SIP/Sip/MsgType.pm
@@ -20,6 +20,7 @@ use CGI qw ( -utf8 );
 use C4::Auth qw(&check_api_auth);
 
 use Koha::Patron::Attributes;
+use Koha::Items;
 
 use UNIVERSAL::can;
 
@@ -1103,7 +1104,46 @@ sub handle_fee_paid {
 
     $ils->check_inst_id( $inst_id, "handle_fee_paid" );
 
-    $status = $ils->pay_fee( $patron_id, $patron_pwd, $fee_amt, $fee_type, $pay_type, $fee_id, $trans_id, $currency, $is_writeoff, $disallow_overpayment );
+    my $pay_result = $ils->pay_fee( $patron_id, $patron_pwd, $fee_amt, $fee_type, $pay_type, $fee_id, $trans_id, $currency, $is_writeoff, $disallow_overpayment );
+    $status = $pay_result->{status};
+    my $pay_response = $pay_result->{pay_response};
+
+    my $failmap = {
+        "no_item" => "No matching item could be found",
+        "no_checkout" => "Item is not checked out",
+        "too_soon" => "Cannot yet be renewed",
+        "too_many" => "Renewed the maximum number of times",
+        "auto_too_soon" => "Scheduled for automatic renewal and cannot yet be renewed",
+        "auto_too_late" => "Scheduled for automatic renewal and cannot yet be any more",
+        "auto_account_expired" => "Scheduled for automatic renewal and cannot be renewed because the patron's account has expired",
+        "auto_renew" => "Scheduled for automatic renewal",
+        "auto_too_much_oweing" => "Scheduled for automatic renewal",
+        "on_reserve" => "On hold for another patron",
+        "patron_restricted" => "Patron is currently restricted",
+        "item_denied_renewal" => "Item is not allowed renewal",
+        "onsite_checkout" => "Item is an onsite checkout"
+    };
+    my @success = ();
+    my @fail = ();
+    foreach my $result( @{$pay_response->{renew_result}} ) {
+        my $item = Koha::Items->find({ itemnumber => $result->{itemnumber} });
+        if ($result->{success}) {
+            push @success, '"' . $item->biblio->title . '"';
+        } else {
+            push @fail, '"' . $item->biblio->title . '" : ' . $failmap->{$result->{error}};
+        }
+    }
+
+    my $msg = "";
+    if (scalar @success > 0) {
+        $msg.="The following items were renewed: " . join(", ", @success) . ". ";
+    }
+    if (scalar @fail > 0) {
+        $msg.="The following items were not renewed: " . join(", ", @fail) . ".";
+    }
+    if (length $msg > 0) {
+        $status->screen_msg($status->screen_msg . " $msg");
+    }
 
     $resp .= ( $status->ok ? 'Y' : 'N' ) . timestamp;
     $resp .= add_field( FID_INST_ID,   $inst_id, $server );

Koha/Account.pm
@@ -97,10 +97,10 @@ sub pay {
         && !defined($cash_register) );
 
     my @fines_paid; # List of account lines paid on with this payment
-    # Item numbers that have had a fine paid where the line has a accounttype
-    # of OVERDUE and a status of UNRETURNED. We might want to try and renew
-    # these items.
-    my $overdue_unreturned = {};
+
+    # The outcome of any attempted item renewals as a result of fines being
+    # paid off
+    my $renew_outcomes = [];
 
     my $balance_remaining = $amount; # Set it now so we can adjust the amount if necessary
     $balance_remaining ||= 0;
@@ -119,16 +119,14 @@ sub pay {
         $fine->amountoutstanding($new_amountoutstanding)->store();
         $balance_remaining = $balance_remaining - $amount_to_pay;
 
-        # If we need to make a note of the item associated with this line,
-        # in order that we can potentially renew it, do so.
-        if (
-            $new_amountoutstanding == 0 &&
-            $fine->accounttype &&
-            $fine->accounttype eq 'OVERDUE' &&
-            $fine->status &&
-            $fine->status eq 'UNRETURNED'
-        ) {
-            $overdue_unreturned->{$fine->itemnumber} = $fine;
+        # Attempt to renew the item associated with this debit if
+        # appropriate
+        if ($fine->renewable) {
+            # We're ignoring the definition of $interface above, by all
+            # accounts we can't rely on C4::Context::interface, so here
+            # we're only using what we've been explicitly passed
+            my $outcome = $fine->renew_item($params->{interface});
+            push @{$renew_outcomes}, $outcome if $outcome;
         }
 
         # Same logic exists in Koha::Account::Line::apply
@@ -193,14 +191,10 @@ sub pay {
 
         # If we need to make a note of the item associated with this line,
         # in order that we can potentially renew it, do so.
-        if (
-            $old_amountoutstanding - $amount_to_pay == 0 &&
-            $fine->accounttype &&
-            $fine->accounttype eq 'OVERDUE' &&
-            $fine->status &&
-            $fine->status eq 'UNRETURNED'
-        ) {
-            $overdue_unreturned->{$fine->itemnumber} = $fine;
+        my $amt = $old_amountoutstanding - $amount_to_pay;
+        if ($fine->renewable) {
+            my $outcome = $fine->renew_item;
+            push @{$renew_outcomes}, $outcome;
         }
 
         if (   $fine->amountoutstanding == 0
@@ -283,36 +277,6 @@ sub pay {
         }
     );
 
-    # If we have overdue unreturned items that have had payments made
-    # against them, check whether the balance on those items is now zero
-    # and, if the syspref is set, renew them
-    # Same logic exists in Koha::Account::Line::apply
-    if (
-        C4::Context->preference('RenewAccruingItemWhenPaid') &&
-        keys %{$overdue_unreturned}
-    ) {
-        foreach my $itemnumber (keys %{$overdue_unreturned}) {
-            # Only do something if this item has no fines left on it
-            my $fine = C4::Overdues::GetFine( $itemnumber, $self->{patron_id} );
-            next if $fine && $fine > 0;
-
-            my ( $renew_ok, $error ) =
-                C4::Circulation::CanBookBeRenewed(
-                    $self->{patron_id}, $itemnumber
-                );
-            if ( $renew_ok ) {
-                C4::Circulation::AddRenewal(
-                    $self->{patron_id},
-                    $itemnumber,
-                    $library_id,
-                    undef,
-                    undef,
-                    1
-                );
-            }
-        }
-    }
-
     if ( C4::Context->preference("FinesLog") ) {
         logaction(
             "FINES", 'CREATE',
@@ -360,7 +324,7 @@ sub pay {
         }
     }
 
-    return $payment->id;
+    return { payment_id => $payment->id, renew_result => $renew_outcomes };
 }
 
 =head3 add_credit

Koha/Account/Line.pm
@@ -453,11 +453,6 @@ sub apply {
 
     my $schema = Koha::Database->new->schema;
 
-    # Item numbers that have had a fine paid where the line has a accounttype
-    # of OVERDUE and a status of UNRETURNED. We might want to try and renew
-    # these items.
-    my $overdue_unreturned = {};
-
     $schema->txn_do( sub {
         for my $debit ( @{$debits} ) {
 
@@ -490,17 +485,10 @@ sub apply {
             $self->amountoutstanding( $available_credit * -1 )->store;
             $debit->amountoutstanding( $owed - $amount_to_cancel )->store;
 
-            # If we need to make a note of the item associated with this line,
-            # in order that we can potentially renew it, do so.
-            # Same logic existing in Koha::Account::pay
-            if (
-                $debit->amountoutstanding == 0 &&
-                $debit->accounttype &&
-                $debit->accounttype eq 'OVERDUE' &&
-                $debit->status &&
-                $debit->status eq 'UNRETURNED'
-            ) {
-                $overdue_unreturned->{$debit->itemnumber} = $debit;
+            # Attempt to renew the item associated with this debit if
+            # appropriate
+            if ($debit->renewable) {
+                $debit->renew_item($params->{interface});
             }
 
             # Same logic exists in Koha::Account::pay
@@ -514,36 +502,6 @@ sub apply {
 
         }
     });
-
-    # If we have overdue unreturned items that have had payments made
-    # against them, check whether the balance on those items is now zero
-    # and, if the syspref is set, renew them
-    # Same logic existing in Koha::Account::pay
-    if (
-        C4::Context->preference('RenewAccruingItemWhenPaid') &&
-        keys %{$overdue_unreturned}
-    ) {
-        foreach my $itemnumber (keys %{$overdue_unreturned}) {
-            # Only do something if this item has no fines left on it
-            my $fine = C4::Overdues::GetFine( $itemnumber, $self->borrowernumber );
-            next if $fine && $fine > 0;
-
-            my ( $renew_ok, $error ) =
-                C4::Circulation::CanBookBeRenewed(
-                    $self->borrowernumber, $itemnumber
-                );
-            if ( $renew_ok ) {
-                C4::Circulation::AddRenewal(
-                    $self->borrowernumber,
-                    $itemnumber,
-                    $overdue_unreturned->{$itemnumber}->{branchcode},
-                    undef,
-                    undef,
-                    1
-                );
-            }
-        }
-    }
 
     return $available_credit;
 }
@@ -810,6 +768,98 @@ sub to_api_mapping {
         manager_id        => 'user_id',
         note              => 'internal_note',
     };
+
+=head3 renewable
+
+    my $bool = $line->renewable;
+
+=cut
+
+sub renewable {
+    my ($self) = @_;
+
+    return (
+        $self->amountoutstanding == 0 &&
+        $self->accounttype &&
+        $self->accounttype eq 'OVERDUE' &&
+        $self->status &&
+        $self->status eq 'UNRETURNED'
+    ) ? 1 : 0;
+}
+
+=head3 renew_item
+
+    my $renew_result = $line->renew_item;
+
+Conditionally attempt to renew an item and return the outcome. This is
+as a consequence of the fine on an item being fully paid off
+
+=cut
+
+sub renew_item {
+    my ($self, $params) = @_;
+
+    my $outcome = {};
+
+    # We want to reject the call to renew if any of these apply:
+    # - The RenewAccruingItemWhenPaid syspref is off
+    # - The line item doesn't have an item attached to it
+    # - The line item doesn't have a patron attached to it
+    #
+    # - The RenewAccruingItemInOpac syspref is off
+    # AND
+    # - There is an interface param passed and it's value is 'opac'
+
+    if (
+        !C4::Context->preference('RenewAccruingItemWhenPaid') ||
+        !$self->item ||
+        !$self->patron ||
+        (
+            !C4::Context->preference('RenewAccruingItemInOpac') &&
+            $params->{interface} &&
+            $params->{interface} eq 'opac'
+        )
+    ) {
+        return;
+    }
+
+    my $itemnumber = $self->item->itemnumber;
+    my $borrowernumber = $self->patron->borrowernumber;
+    # Only do something if this item has no fines left on it
+    my $fine = C4::Overdues::GetFine($itemnumber, $borrowernumber);
+    if ($fine && $fine > 0) {
+        return {
+            itemnumber => $itemnumber,
+            error      => 'has_fine',
+            success    => 0
+        };
+    }
+    my ( $can_renew, $error ) = C4::Circulation::CanBookBeRenewed(
+        $borrowernumber,
+        $itemnumber
+    );
+    if ( $can_renew ) {
+        my $due_date = C4::Circulation::AddRenewal(
+            $borrowernumber,
+            $itemnumber,
+            $self->{branchcode},
+            undef,
+            undef,
+            1
+        );
+        return {
+            itemnumber => $itemnumber,
+            due_date   => $due_date,
+            success    => 1
+        };
+    } else {
+        return {
+            itemnumber => $itemnumber,
+            error      => $error,
+            success    => 0
+        };
+    }
+
 }
 
 =head2 Internal methods

Koha/REST/V1/Patrons/Account.pm
@@ -130,7 +130,7 @@ sub add_credit {
         my $outstanding_credit = $credit->amountoutstanding;
         if ($debits) {
             # pay them!
-            $outstanding_credit = $credit->apply({ debits => [ $debits->as_list ], offset_type => 'payment' });
+            $outstanding_credit = $credit->apply({ debits => [ $debits->as_list ], offset_type => 'payment' })->{outstanding_amount};
         }
 
         if ($outstanding_credit) {

installer/data/mysql/atomicupdate/bug_23051_add_RenewAccruingItemInOpac_syspref.perl
@@ -1,0 +1,8 @@
+$DBversion = 'XXX'; # will be replaced by the RM
+if( CheckVersion( $DBversion ) ) {
+
+    $dbh->do( q| INSERT IGNORE INTO systempreferences (variable, value, explanation, options, type) VALUES ('RenewAccruingItemInOpac', '0', 'If enabled, when the fines on an item accruing is paid off in the OPAC via a payment plugin, attempt to renew that item. If the syspref "RenewalPeriodBase" is set to "due date", renewed items may still be overdue', '', 'YesNo'); | );
+
+    SetVersion( $DBversion );
+    print "Upgrade to $DBversion done (Bug 23051 - Add RenewAccruingItemInOpac syspref)\n";
+}

commit 42c0475a948577442e44b11dbb12b45b25171418

Bug 26915: Fix FinePaymentAutoPopup behaviour
The behaviour of the syspref FinePaymentAutoPopup is broken, the popup
explodes with
    Can't call method "unblessed" on an undefined value at /home/vagrant/kohaclone/members/printfeercpt.pl line 64

It has been broken by
  commit a2449a81be55aa5b3710ca2c6df7884513f89540
  Bug 23051: (follow-up) Add renewal feedback and move code to subroutines and test
which modified the return value of Koha::Account->pay (from $payment_id
to a hashref)

Test plan:
- To test activate FinePaymentAutoPopup
- Create a manual fee
- Write it off using the write off button on the account line (not below table)
=> Without the patch you get the error
=> With the patch applied the popup is correctly displayed

Signed-off-by: David Nind <david@davidnind.com>
Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

members/pay.pl
@@ -122,7 +122,7 @@ elsif ( $input->param('confirm_writeoff') ) {
                 interface  => C4::Context->interface,
                 library_id => $branch,
             }
-        );
+        )->{payment_id};
     }
 }
 