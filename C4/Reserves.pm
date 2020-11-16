4fdad9b2a547f0ddadb3420d49184fbc4c741b8b
Josef Moravec <josef.moravec@gmail.com>
2020-11-16 09:43:03 +0000

    Bug 27030: Add missing perldoc for Processing hold status to C4::Reserves
    Test plan:
    
    Look into diff and confirm the comment does make sense
    
    Signed-off-by: David Nind <david@davidnind.com>
    Signed-off-by: Joonas Kylmälä <joonas.kylmala@helsinki.fi>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    (cherry picked from commit 2c4cd7f3f260d44801e36a7582c8dc384afb93e7)

--- C4/Reserves.pm
+++ C4/Reserves.pm
@@ -69,10 +69,13 @@ This modules provides somes functions to deal with reservations.
   The following columns contains important values :
   - priority >0      : then the reserve is at 1st stage, and not yet affected to any item.
              =0      : then the reserve is being dealed
-  - found : NULL       : means the patron requested the 1st available, and we haven't chosen the item
-            T(ransit)  : the reserve is linked to an item but is in transit to the pickup branch
-            W(aiting)  : the reserve is linked to an item, is at the pickup branch, and is waiting on the hold shelf
-            F(inished) : the reserve has been completed, and is done
+  - found : NULL         : means the patron requested the 1st available, and we haven't chosen the item
+            T(ransit)    : the reserve is linked to an item but is in transit to the pickup branch
+            W(aiting)    : the reserve is linked to an item, is at the pickup branch, and is waiting on the hold shelf
+            F(inished)   : the reserve has been completed, and is done
+            P(rocessing) : reserved item has been returned using self-check machine and reserve needs to be confirmed
+                           by librarian before notice is send and status changed to waiting.
+                           Applicable only if HoldsNeedProcessingSIP system preference is set.
   - itemnumber : empty : the reserve is still unaffected to an item
                  filled: the reserve is attached to an item
   The complete workflow is :
