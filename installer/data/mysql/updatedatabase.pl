23970975e358ea93624ca5c8559475e3a01bb1d6
Aleisha Amohia <aleishaamohia@hotmail.com>
2020-11-17 17:56:18 +1300

    Bug 18050: DBRev 19.05.16.002
    Signed-off-by: Aleisha Amohia <aleishaamohia@hotmail.com>
    
    (cherry picked from commit 026316d5515621aa3a096850754d2fae5b6f01dc)
    Signed-off-by: Victor Grousset/tuxayo <victor@tuxayo.net>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -19177,6 +19177,46 @@ if( CheckVersion( $DBversion ) ) {
     SetVersion( $DBversion );
 }
 
+$DBversion = '19.05.16.002';
+if( CheckVersion( $DBversion ) ) {
+    my $count_missing_budget = $dbh->selectrow_arrayref(q|
+        SELECT COUNT(*) FROM aqbudgets ab WHERE NOT EXISTS
+            (SELECT * FROM aqbudgetperiods abp WHERE abp.budget_period_id = ab.budget_period_id)
+            AND budget_period_id IS NOT NULL;
+
+    |);
+
+    my $message = "";
+    if($count_missing_budget->[0] > 0) {
+        $dbh->do(q|
+            CREATE TABLE _bug_18050_aqbudgets AS
+            SELECT * FROM aqbudgets ab WHERE NOT EXISTS
+                (SELECT * FROM aqbudgetperiods abp WHERE abp.budget_period_id = ab.budget_period_id)
+        |);
+
+        $dbh->do(q|
+            UPDATE aqbudgets ab SET budget_period_id = NULL
+            WHERE NOT EXISTS
+                (SELECT * FROM aqbudgetperiods abp WHERE abp.budget_period_id = ab.budget_period_id)
+        |);
+        $message = ". There are $count_missing_budget->[0] funds in your database that are not linked
+        to a valid budget. Setting invalid budget id (budget_period_id) to null. The table _bug_18050_aqbudgets
+        was created with original data. Please check that table and place valid ids in aqbudget table as soon as possible."
+
+    }
+
+    if ( !foreign_key_exists( 'aqbudgets', 'aqbudgetperiods_ibfk_1' ) ) {
+        $dbh->do(q|
+            ALTER TABLE aqbudgets ADD CONSTRAINT `aqbudgetperiods_ibfk_1` FOREIGN KEY (`budget_period_id`) REFERENCES `aqbudgetperiods` (`budget_period_id`) ON UPDATE CASCADE ON DELETE CASCADE
+        |);
+        print "Upgrade to $DBversion done (Bug 18050 - Add FK constraint on aqbudgets.budget_period_id$message)\n";
+        SetVersion( $DBversion );
+    } else {
+        print "Upgrade to $DBversion done (Bug 18050 - FK constraint on aqbudgets.budget already exists)\n";
+        SetVersion( $DBversion );
+    }
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 
