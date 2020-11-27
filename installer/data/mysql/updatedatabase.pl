0f47e16ef6008e6494128fa400fae61bc3adbef3
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2020-11-27 09:24:46 +0000

    Bug 27103: Remove HOLD_CANCELLATION notice
    We are really close to release, and the 'proper' fix to make the sending
    of hold cancellation notices optional will introduce string changes.
    
    This patch takes the alternative approach of removing the default notice
    template there by disabling the feature for 20.11.00 release and giving
    us time to properly impliment a solution and re-instate the default
    notice template for the 20.11.01 release.
    
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -22677,10 +22677,6 @@ if ( CheckVersion( $DBversion ) ) {
         INSERT IGNORE INTO authorised_value_categories( category_name, is_system ) VALUES ('HOLD_CANCELLATION', 0);
     });
 
-    $dbh->do(q{
-INSERT IGNORE INTO `letter` VALUES ('reserves','HOLD_CANCELLATION','','Hold Cancellation',0,'Your hold was canceled.','[%- USE AuthorisedValues -%]\r\nDear [% borrower.firstname %] [% borrower.surname %],\r\n\r\nYour hold for [% biblio.title %] was canceled for the following reason: [% AuthorisedValues.GetByCode( \'HOLD_CANCELLATION\', hold.cancellation_reason ) %]','email','default');
-    });
-
     if ( !column_exists( 'reserves', 'cancellation_reason' ) ) {
         $dbh->do(q{
             ALTER TABLE reserves ADD COLUMN `cancellation_reason` varchar(80) default NULL AFTER cancellationdate;
