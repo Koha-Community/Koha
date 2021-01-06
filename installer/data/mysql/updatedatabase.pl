265e5341f58de941b2643ddd06b140022ac9744a
Andrew Fuerste-Henry <andrew@bywatersolutions.com>
2021-01-06 21:53:59 +0000

    Increment version for 20.05.07 release
    Signed-off-by: Andrew Fuerste-Henry <andrew@bywatersolutions.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -22513,6 +22513,12 @@ if ( CheckVersion($DBversion) ) {
     SetVersion ($DBversion);
 }
 
+$DBversion = "20.05.07.000";
+if ( CheckVersion($DBversion) ) {
+    print "Upgrade to $DBversion done (20.05.07 release)\n";
+    SetVersion ($DBversion);
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
