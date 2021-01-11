0b1f162dccfe0f7edc1505fc0648dfa7f7792ca8
Nick Clemens <nick@bywatersolutions.com>
2021-01-11 14:08:35 +0000

    Bug 24108: (QA follow-up) Adjust wording and use perl for update file
    Signed-off-by: David Nind <david@davidnind.com>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- installer/data/mysql/atomicupdate/bug_24108-add_SaveRecordbyControlNumber_syspref.sql
+++ installer/data/mysql/atomicupdate/bug_24108-add_SaveRecordbyControlNumber_syspref.sql
@@ -1,1 +1,9 @@
-NSERT IGNORE INTO systempreferences (variable, value, explanation, options, type) VALUES ('SaveRecordbyControlNumber', '0', 'If set, advanced editor (Rancor) will use the control number field to populate the name of the save file, otherwise it uses the biblionumber.', NULL, 'YesNo');
+$DBversion = 'XXX';
+if( CheckVersion( $DBversion ) ) {
+    $dbh->do(q{
+         INSERT IGNORE INTO systempreferences
+         (variable, value, explanation, options, type) VALUES
+         ('SaveRecordbyControlNumber', '0', 'If set, advanced cataloging editor will use the control number field to populate the name of the save file, otherwise it uses the biblionumber.', NULL, 'YesNo')
+    });
+    NewVersion( $DBversion, 24108, "Add system preference SaveRecordbyControlNumber");
+}
