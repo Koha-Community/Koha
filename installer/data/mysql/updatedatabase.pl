1eb0f4fcf99979ed926e641cb35bbfb476940884
Andrew Fuerste-Henry <andrew@bywatersolutions.com>
2021-01-15 23:06:00 +0000

    DBRev 20.05.07.001
    Signed-off-by: Andrew Fuerste-Henry <andrew@bywatersolutions.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -22529,6 +22529,12 @@ if ( CheckVersion($DBversion) ) {
     SetVersion ($DBversion);
 }
 
+$DBversion = '20.05.07.001';
+if( CheckVersion( $DBversion ) ) {
+    $dbh->do(q{UPDATE systempreferences SET `type` = 'Choice' WHERE `variable` = 'UsageStatsCountry'});
+    NewVersion( $DBversion, 27351, "Set type for UsageStatsCountry to Choice");
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
