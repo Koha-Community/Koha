d43d8c905cc518170fe858f717feab44597cd17a
Katrin Fischer <katrin.fischer.83@web.de>
2021-01-29 00:00:34 +0100

    Bug 18989: (QA follow-up) Have new preference default to current behaviour
    At the moments the records are hidden when all items are hidden, so
    we need to make sure that we keep this behaviour when the preference
    is added.
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- installer/data/mysql/atomicupdate/bug_18989.sql
+++ installer/data/mysql/atomicupdate/bug_18989.sql
@@ -1,2 +1,2 @@
 INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type` ) VALUES
-('OpacHiddenItemsHidesRecord','0','','Hide biblio record when all its items are hidden because of OpacHiddenItems','YesNo');
+('OpacHiddenItemsHidesRecord','1','','Hide bibliographic record when all its items are hidden because of OpacHiddenItems','YesNo');
