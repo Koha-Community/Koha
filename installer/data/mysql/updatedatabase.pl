5c7a1e9716b03cafb55c657e9bcf7069b6b9d86b
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2021-03-01 14:10:40 +0000

    Bug 27808: DBRev 20.12.00.018
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    (cherry picked from commit aa41987dbfebcd61e1a7b5847095f55261c55d5e)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -23484,6 +23484,19 @@ if( CheckVersion( $DBversion ) ) {
     NewVersion( $DBversion, 27316, "In Elastisearch mappings convert NULL (Undef) for sort to 1 (Yes)");
 }
 
+$DBversion = '20.11.03.004';
+if( CheckVersion( $DBversion ) ) {
+
+    $dbh->do(q|
+        UPDATE items
+        LEFT JOIN issues ON issues.itemnumber=items.itemnumber
+        SET items.onloan=CAST(issues.date_due AS DATE)
+        WHERE items.onloan IS NULL
+    |);
+
+    NewVersion( $DBversion, 27808, "Adjust items.onloan if needed" );
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
