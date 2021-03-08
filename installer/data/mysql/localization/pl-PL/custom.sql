444dd28b02d3bd65dc03eb4b2ce7ec8925a14712
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2021-03-08 11:32:06 +0100

    Bug 27623: Add PLN currency to custom.sql
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    
    Signed-off-by: Victor Grousset/tuxayo <victor@tuxayo.net>
    
    Signed-off-by: Joonas Kylmälä <joonas.kylmala@helsinki.fi>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

---
+++ installer/data/mysql/localization/pl-PL/custom.sql
@@ -0,0 +1,20 @@
+--
+-- System preferences that differ from the global defaults
+--
+-- This file is part of Koha.
+--
+-- Koha is free software; you can redistribute it and/or modify it
+-- under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- Koha is distributed in the hope that it will be useful, but
+-- WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+UPDATE `currency` SET `active` = 0;
+INSERT INTO `currency` (`currency`, `rate`, `symbol`, `active`) VALUES ('PLN', 1.0, 'zł', '1');
