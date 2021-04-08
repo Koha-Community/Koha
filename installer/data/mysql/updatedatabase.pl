4bfc2584e02f3233fed8c8f329205d712f3bfe0b
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2021-04-08 10:15:04 +0000

    Bug 27069: DBRev 20.12.00.027
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -23749,6 +23749,24 @@ if( CheckVersion( $DBversion ) ) {
     NewVersion( $DBversion, 26498, "Bug 26498 - Add option to set a default expire date for holds at reservation time");
 }
 
+$DBversion = '20.12.00.027';
+if( CheckVersion( $DBversion ) ) {
+
+    $dbh->do(q{
+        UPDATE circulation_rules
+        SET
+            rule_value = CASE
+                WHEN rule_value='0' THEN 'not_allowed'
+                WHEN rule_value='1' THEN 'from_home_library'
+                WHEN rule_value='2' THEN 'from_any_library'
+                WHEN rule_value='3' THEN 'from_local_hold_group'
+            END
+        WHERE rule_name='holdallowed' AND rule_value >= 0 AND rule_value <= 3;
+    });
+
+    NewVersion( $DBversion, 27069, "Change holdallowed values from numbers to strings");
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
