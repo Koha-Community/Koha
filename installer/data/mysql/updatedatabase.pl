aad8699b015024a2cb1fb63b149ea803a542013b
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2021-04-09 14:04:23 +0000

    Bug 14233: DBRev 20.12.00.028
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -23767,6 +23767,29 @@ if( CheckVersion( $DBversion ) ) {
     NewVersion( $DBversion, 27069, "Change holdallowed values from numbers to strings");
 }
 
+$DBversion = '20.12.00.028';
+if ( CheckVersion($DBversion) ) {
+
+    if ( !column_exists( 'letter', 'id' ) ) {
+        $dbh->do(q{
+            ALTER TABLE letter DROP PRIMARY KEY
+        });
+        $dbh->do(q{
+            ALTER TABLE letter ADD COLUMN `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST
+        });
+        $dbh->do(q{
+            ALTER TABLE letter ADD UNIQUE KEY letter_uniq_1 (`module`,`code`,`branchcode`,`message_transport_type`,`lang`)
+        });
+    }
+
+    $dbh->do(q{
+        INSERT IGNORE INTO systempreferences (variable, value, explanation, options, type)
+        VALUES ('NoticesLog','',NULL,'If enabled, log changes to notice templates','YesNo')
+    });
+
+    NewVersion( $DBversion, 14233, "Add id field to letter table" );
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
