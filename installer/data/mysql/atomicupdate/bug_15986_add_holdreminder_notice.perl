051a9d740f8eb3260e814284d80c63ee21beaf9b
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2021-04-16 12:16:47 +0000

    Bug 15986: DBRev 20.12.00.032
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- installer/data/mysql/atomicupdate/bug_15986_add_holdreminder_notice.perl
+++
@@ -1,9 +0,0 @@
-$DBversion = 'XXX';
-if( CheckVersion( $DBversion ) ) {
-    $dbh->do(q{
-        INSERT IGNORE INTO letter
-        (module,code,branchcode,name,is_html,title,content,message_transport_type,lang)
-        VALUES ('reserves','HOLD_REMINDER','','Waiting hold reminder',0,'You have waiting holds.','Dear [% borrower.firstname %] [% borrower.surname %],\r\n\r\nThe following holds are waiting at [% branch.branchname %]:\r\n\\r\n[% FOREACH hold IN holds %]\r\n    [% hold.biblio.title %] : waiting since [% hold.waitingdate | $KohaDates %]\r\n[% END %]','email','default')
-    });
-    NewVersion( $DBversion, 15986, "Add sample HOLD_REMINDER notice");
-}
--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -23817,6 +23817,17 @@ if( CheckVersion( $DBversion ) ) {
     NewVersion( $DBversion, 21260, "Add new system preference Reference_NFL_Statuses");
 }
 
+$DBversion = '20.12.00.032';
+if( CheckVersion( $DBversion ) ) {
+    $dbh->do(q{
+        INSERT IGNORE INTO letter
+        (module,code,branchcode,name,is_html,title,content,message_transport_type,lang)
+        VALUES ('reserves','HOLD_REMINDER','','Waiting hold reminder',0,'You have waiting holds.','Dear [% borrower.firstname %] [% borrower.surname %],\r\n\r\nThe following holds are waiting at [% branch.branchname %]:\r\n\\r\n[% FOREACH hold IN holds %]\r\n    [% hold.biblio.title %] : waiting since [% hold.waitingdate | $KohaDates %]\r\n[% END %]','email','default')
+    });
+
+    NewVersion( $DBversion, 15986, "Add sample HOLD_REMINDER notice");
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
