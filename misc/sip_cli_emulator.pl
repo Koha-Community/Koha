a4ae376b4e29499c0a35065ca3c2a35e1ef3eca5
Nick Clemens <nick@bywatersolutions.com>
2021-05-11 13:43:51 +0000

    Bug 28320: Add DB connection check to the SIP SC status message
    This patch adds a lookup of the sip user during an SC status, confirming that
    our DB ocnnection is working, and that our user is still valid
    
    Additionally, it adds support for SC status to the sip_cli_emulator and adds basic
    test coverage for the SC status message
    
    To test:
    1 - Apply patch
    2 - Restart SP server
    3 - perl misc/sip_cli_emulator.pl -a localhost -p 6001 -l CPL -m sc_status_request -su term1 -sp term1
    4 - prove -v t/db_dependent/SIP/Message.t
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    (cherry picked from commit d85ae826c64ef43d1f606aceb6eaa6d3174cba52)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>
    (cherry picked from commit 06a99ac9bbf163d9186e743e03533bd0ecd74614)
    
    Signed-off-by: Andrew Fuerste-Henry <andrew@bywatersolutions.com>

--- misc/sip_cli_emulator.pl
+++ misc/sip_cli_emulator.pl
@@ -125,6 +125,12 @@ my $handlers = {
             location_code  => $location_code,
         },
     },
+    sc_status_request => {
+        name       => 'SC Status',
+        subroutine => \&build_sc_status_command_message,
+        parameters => {
+        },
+    },
     patron_status_request => {
         name       => 'Patron Status Request',
         subroutine => \&build_patron_status_request_command_message,
@@ -358,6 +364,12 @@ sub build_login_command_message {
       . build_field( FID_LOGIN_UID,     $login_user_id )
       . build_field( FID_LOGIN_PWD,     $login_password )
       . build_field( FID_LOCATION_CODE, $location_code );
+}
+
+sub build_sc_status_command_message {
+    my ($params) = @_;
+
+    return SC_STATUS . "0" . "030" . "2.00";
 }
 
 sub build_patron_status_request_command_message {
@@ -650,6 +662,7 @@ Options:
     item_information
     patron_information
     patron_status_request
+    sc_status_request
     renew
 /
 }
