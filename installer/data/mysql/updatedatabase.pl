b532cd2cf8b857cf8ad08db422cd5d53f80225b1
Andrew Fuerste-Henry <andrew@bywatersolutions.com>
2021-05-24 15:39:09 +0000

    Bug 26940: DBRev 20.05.11.001
    Signed-off-by: Andrew Fuerste-Henry <andrew@bywatersolutions.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -22595,6 +22595,29 @@ if ( CheckVersion($DBversion) ) {
     SetVersion ($DBversion);
 }
 
+$DBversion = '20.05.11.001'; # will be replaced by the RM
+if( CheckVersion( $DBversion ) ) {
+    my $debar = $dbh->selectall_arrayref(q|
+        SELECT d.borrowernumber, GROUP_CONCAT(comment SEPARATOR '\n') AS comment
+        FROM borrower_debarments d
+        LEFT JOIN borrowers b ON b.borrowernumber=d.borrowernumber
+        WHERE ( b.debarredcomment IS NULL OR b.debarredcomment = "" ) AND ( expiration > CURRENT_DATE() OR expiration IS NULL )
+        GROUP BY d.borrowernumber
+    |, { Slice => {} });
+
+
+    my $update_sth = $dbh->prepare(q|
+        UPDATE borrowers
+        SET debarredcomment=?
+        WHERE borrowernumber=?
+    |);
+    for my $d ( @$debar ) {
+        $update_sth->execute($d->{comment}, $d->{borrowernumber});
+    }
+
+    NewVersion( $DBversion, 26940, "Put in sync borrowers.debarredcomment with comments from borrower_debarments");
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
