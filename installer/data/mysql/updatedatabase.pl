9284d09eec529ff0efed99b68228dd991d2bfd0c
Victor Grousset/tuxayo <victor@tuxayo.net>
2021-05-25 17:17:01 +0200

    Increment version for 19.11.18 release
    Signed-off-by: Victor Grousset/tuxayo <victor@tuxayo.net>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -20807,6 +20807,12 @@ if ( CheckVersion($DBversion) ) {
     SetVersion ($DBversion);
 }
 
+$DBversion = "19.11.18.000";
+if ( CheckVersion($DBversion) ) {
+    print "Upgrade to $DBversion done (19.11.18 release)\n";
+    SetVersion ($DBversion);
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
