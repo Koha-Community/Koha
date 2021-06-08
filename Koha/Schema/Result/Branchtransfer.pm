28a154a02cacf964312ff68f74dd9e92ddf29bde
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2021-06-08 09:27:50 +0100

    Bug 24434: DBIC Update
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- Koha/Schema/Result/Branchtransfer.pm
+++ Koha/Schema/Result/Branchtransfer.pm
@@ -111,7 +111,7 @@ what triggered the transfer
 =head2 cancellation_reason
 
   data_type: 'enum'
-  extra: {list => ["Manual","StockrotationAdvance","StockrotationRepatriation","ReturnToHome","ReturnToHolding","RotatingCollection","Reserve","LostReserve","CancelReserve","ItemLost"]}
+  extra: {list => ["Manual","StockrotationAdvance","StockrotationRepatriation","ReturnToHome","ReturnToHolding","RotatingCollection","Reserve","LostReserve","CancelReserve","ItemLost","WrongTransfer"]}
   is_nullable: 1
 
 what triggered the transfer cancellation
@@ -205,6 +205,7 @@ __PACKAGE__->add_columns(
         "LostReserve",
         "CancelReserve",
         "ItemLost",
+        "WrongTransfer",
       ],
     },
     is_nullable => 1,
@@ -271,8 +272,8 @@ __PACKAGE__->belongs_to(
 );
 
 
-# Created by DBIx::Class::Schema::Loader v0.07049 @ 2021-04-23 09:48:46
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:7iCCrApSJnoleyL9p3odQg
+# Created by DBIx::Class::Schema::Loader v0.07049 @ 2021-06-08 08:13:08
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:+ABUZOo6IHbiRH1LTy3t+A
 
 sub koha_object_class {
     'Koha::Item::Transfer';
