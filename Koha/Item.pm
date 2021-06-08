cea528dbf36c6967b7e364e11ab304fa94718bd4
Joonas Kylmälä <joonas.kylmala@helsinki.fi>
2021-06-08 07:47:32 +0000

    Bug 28520: Revert "Bug 12362: (QA follow-up) Fix ModItemTransfer cancellation handling"
    This reverts commit fbba09f63f200d3e57a8612554984d807867cf84.
    
    This commit was part of a series adding adding reverse transfers, i.e.
    transfers that were created for transfers that were cancelled and
    we wanted the item to return back to its sending library. This however
    hid the information about transfer happening and we need another approach
    to the problem.
    
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- Koha/Item.pm
+++ Koha/Item.pm
@@ -456,6 +456,9 @@ sub request_transfer {
     Koha::Exceptions::Item::Transfer::InQueue->throw( transfer => $request )
       if ( $request && !$params->{enqueue} && !$params->{replace} );
 
+    $request->cancel( { reason => $params->{reason}, force => 1 } )
+      if ( defined($request) && $params->{replace} );
+
     my $transfer = Koha::Item::Transfer->new(
         {
             itemnumber    => $self->itemnumber,
@@ -466,9 +469,6 @@ sub request_transfer {
             comments      => $params->{comment}
         }
     )->store();
-
-    $request->cancel( { reason => $params->{reason}, force => 1, replace => 1 } )
-      if ( defined($request) && $params->{replace} );
 
     return $transfer;
 }
