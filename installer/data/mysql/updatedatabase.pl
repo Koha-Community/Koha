01734a9ca38b698c449b967b2ceddc3153bf10a1
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2021-06-14 14:37:03 +0000

    Bug 28490: DBRev 21.06.00.002
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -24296,6 +24296,29 @@ if ( CheckVersion($DBversion) ) {
     NewVersion( $DBversion, '28489', 'Modify sessions.a_session from longtext to longblob' );
 }
 
+$DBversion = '21.06.00.002';
+if( CheckVersion( $DBversion ) ) {
+    if( !column_exists( 'borrower_modifications', 'relationship' ) ) {
+      $dbh->do(q{
+          ALTER TABLE borrower_modifications ADD COLUMN `relationship` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL AFTER `borrowernotes`
+      });
+    }
+
+    if( !column_exists( 'borrowers', 'relationship' ) ) {
+      $dbh->do(q{
+          ALTER TABLE borrowers ADD COLUMN `relationship` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'used for children to include the relationship to their guarantor' AFTER `borrowernotes`
+      });
+    }
+
+    if( !column_exists( 'deletedborrowers', 'relationship' ) ) {
+      $dbh->do(q{
+          ALTER TABLE deletedborrowers ADD COLUMN `relationship` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'used for children to include the relationship to their guarantor' AFTER `borrowernotes`
+      });
+    }
+
+    NewVersion( $DBversion, 28490, "Bring back accidentally deleted relationship columns");
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
