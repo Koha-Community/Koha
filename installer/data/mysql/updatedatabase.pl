f2cc49863dbac1adfb355fab1a848b823ca7fd72
Wainui Witika-Park <wainuiwitikapark@catalyst.net.nz>
2021-06-21 23:30:11 +0000

    Increment version for 19.11.19 release
    Signed-off-by: Wainui Witika-Park <wainuiwitikapark@catalyst.net.nz>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -20813,6 +20813,12 @@ if ( CheckVersion($DBversion) ) {
     SetVersion ($DBversion);
 }
 
+$DBversion = "19.11.19.000";
+if ( CheckVersion($DBversion) ) {
+    print "Upgrade to $DBversion done (19.11.19 release)\n";
+    SetVersion ($DBversion);
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
