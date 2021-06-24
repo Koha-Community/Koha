df597c1aca8cb8e480344bce641be596c64c2603
Nick Clemens <nick@bywatersolutions.com>
2021-06-24 06:20:23 +0000

    Bug 27981: DB update
    Signed-off-by: Thomas Klausner <domm@plix.at>
    
    Signed-off-by: Andrew Nugged <nugged@gmail.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

---
+++ installer/data/mysql/atomicupdate/bug_27981.pl
@@ -0,0 +1,17 @@
+use Modern::Perl;
+
+return {
+    bug_number => "27981",
+    description => "Make it possible to force 001 = biblionumber",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+        # Do you stuffs here
+        $dbh->do(q{
+            INSERT IGNORE INTO systempreferences (variable, value, options, explanation, type) VALUES ('autoControlNumber','OFF','biblionumber|OFF',
+            'Used to autogenerate a Control Number: biblionumber will be as biblionumber; OFF will leave it as is','Choice');
+        });
+        # Print useful stuff here
+        say $out "Added system preference autoControlNumber";
+    },
+};
