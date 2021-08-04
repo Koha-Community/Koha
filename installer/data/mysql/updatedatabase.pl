9abe5530f0548d46d6828507f5c580101ad35f88
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2021-08-04 09:28:52 +0000

    Bug 15067: DBRev 21.06.00.012
    JD Amended DB rev: no need to display a warning, the creation of the
    unique key will fail with an appropriate error.
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -24481,6 +24481,67 @@ if( CheckVersion( $DBversion ) ) {
     NewVersion( $DBversion, 28567, "Set to NULL empty branches fields");
 }
 
+$DBversion = '21.06.00.012';
+if( CheckVersion( $DBversion ) ) {
+    if( !unique_key_exists( 'language_subtag_registry', 'uniq_lang' ) ) {
+        $dbh->do(q{
+            ALTER TABLE language_subtag_registry
+            ADD UNIQUE KEY uniq_lang (subtag, type)
+        });
+    };
+
+    if( !unique_key_exists( 'language_descriptions', 'uniq_desc' ) ) {
+        $dbh->do(q{
+            ALTER TABLE language_descriptions
+            ADD UNIQUE KEY uniq_desc (subtag, type, lang)
+        });
+    };
+
+    if( !unique_key_exists( 'language_rfc4646_to_iso639', 'uniq_code' ) ) {
+        $dbh->do(q{
+            ALTER TABLE language_rfc4646_to_iso639
+            ADD UNIQUE KEY uniq_code (rfc4646_subtag, iso639_2_code)
+        });
+    };
+
+    $dbh->do(q{
+        INSERT IGNORE INTO language_subtag_registry (subtag, type, description, added)
+        VALUES
+        ('et', 'language', 'Estonian', now()),
+        ('lv', 'language', 'Latvian', now()),
+        ('lt', 'language', 'Lithuanian', now()),
+        ('iu', 'language', 'Inuktitut', now()),
+        ('ik', 'language', 'Inupiaq', now())
+    });
+
+    $dbh->do(q{
+        INSERT IGNORE INTO language_descriptions (subtag, type, lang, description)
+        VALUES
+        ('et', 'language', 'en', 'Estonian'),
+        ('et', 'language', 'et', 'Eesti'),
+        ('lv', 'language', 'en', 'Latvian'),
+        ('lv', 'language', 'lv', 'Latvija'),
+        ('lt', 'language', 'en', 'Lithuanian'),
+        ('lt', 'language', 'lt', 'Lietuvių'),
+        ('iu', 'language', 'en', 'Inuktitut'),
+        ('iu', 'language', 'iu', 'ᐃᓄᒃᑎᑐᑦ'),
+        ('ik', 'language', 'en', 'Inupiaq'),
+        ('ik', 'language', 'ik', 'Iñupiaq')
+    });
+
+    $dbh->do(q{
+        INSERT IGNORE INTO language_rfc4646_to_iso639 (rfc4646_subtag, iso639_2_code)
+        VALUES
+        ('et', 'est'),
+        ('lv', 'lav'),
+        ('lt', 'lit'),
+        ('iu', 'iku'),
+        ('ik', 'ipk')
+    });
+
+    NewVersion( $DBversion, 15067, "Add missing languages" );
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
