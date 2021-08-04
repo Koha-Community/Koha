90bcfaff9a30e92bccf55699b7cd0aec0f16595a
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2021-08-04 09:28:52 +0000

    Bug 22435: DBRev 21.06.00.013
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -24542,6 +24542,44 @@ if( CheckVersion( $DBversion ) ) {
     NewVersion( $DBversion, 15067, "Add missing languages" );
 }
 
+$DBversion = '21.06.00.013';
+if( CheckVersion( $DBversion ) ) {
+    # Remove foreign key for offset types
+    if ( foreign_key_exists( 'account_offsets', 'account_offsets_ibfk_t' ) ) {
+        $dbh->do( "ALTER TABLE account_offsets DROP FOREIGN KEY account_offsets_ibfk_t" );
+    }
+
+    # Drop account_offset_types table
+    $dbh->do( "DROP TABLE IF EXISTS account_offset_types" );
+
+    # Update offset_types to 'CREATE' where appropriate
+    $dbh->do( "UPDATE account_offsets SET type = 'CREATE' WHERE type != 'OVERDUE_INCREASE' AND type != 'OVERDUE_DECREASE' AND ( debit_id IS NULL OR credit_id IS NULL)" );
+    $dbh->do( "UPDATE account_offsets SET amount = ABS(amount) WHERE type = 'CREATE'" );
+
+    # Update offset_types to 'APPLY' where appropriate
+    $dbh->do( "UPDATE account_offsets SET type = 'APPLY' WHERE type != 'OVERDUE_INCREASE' AND type != 'OVERDUE_DECREASE' AND type != 'CREATE' AND type != 'VOID'" );
+
+    # Update table to ENUM
+    $dbh->do(
+        q{
+            ALTER TABLE
+                `account_offsets`
+            MODIFY COLUMN
+                `type` enum(
+                    'CREATE',
+                    'APPLY',
+                    'VOID',
+                    'OVERDUE_INCREASE',
+                    'OVERDUE_DECREASE'
+                )
+            AFTER `debit_id`
+          }
+    );
+
+
+    NewVersion( $DBversion, 22435, "Update existing offsets");
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
