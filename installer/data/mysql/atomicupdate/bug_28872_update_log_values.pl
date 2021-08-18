ccb09890d16b4d3c2cdad56e78a8e832439d91d9
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2021-08-18 13:03:09 +0200

    Bug 28872: Fix QA issues on atomicupdate
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

--- installer/data/mysql/atomicupdate/bug_28872_update_log_values.pl
+++ installer/data/mysql/atomicupdate/bug_28872_update_log_values.pl
@@ -1,16 +1,12 @@
 use Modern::Perl;
 
-{
+return {
     bug_number => "28872",
-    description => "update values from on and off to 1 and 0",
+    description => "Update syspref values from on and off to 1 and 0",
     up => sub {
         my ($args) = @_;
         my ($dbh, $out) = @$args{qw(dbh out)};
-        # Do you stuffs here
         $dbh->do(q{update systempreferences set value=1 where variable in ('AcquisitionLog', 'NewsLog', 'NoticesLog') and value='on'});
         $dbh->do(q{update systempreferences set value=0 where variable in ('AcquisitionLog', 'NewsLog', 'NoticesLog') and value='off'});
-
-        # Print useful stuff here
-        say $out "Update is going well so far";
     },
 }
