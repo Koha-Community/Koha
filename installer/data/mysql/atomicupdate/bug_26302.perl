e52cc698fadf12670f493990042533f3b4bc3cd8
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2021-08-30 17:08:58 +0200

    Bug 26302: DBRev 21.06.00.019
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- installer/data/mysql/atomicupdate/bug_26302.perl
+++
@@ -1,9 +0,0 @@
-$DBversion = 'XXX'; # will be replaced by the RM
-if( CheckVersion( $DBversion ) ) {
-    $dbh->do(q|
-        INSERT IGNORE INTO systempreferences (variable, value, options, explanation, type) VALUES
-            ('OPACResultsMaxItems','1','','Maximum number of available items displayed in search results','Integer'),
-            ('OPACResultsMaxItemsUnavailable','0','','Maximum number of unavailable items displayed in search results','Integer')
-    |);
-    NewVersion( $DBversion, 26302, "Add preferences OPACResultsMaxItems and OPACResultsMaxItemsUnavailable");
-}
---
+++ installer/data/mysql/db_revs/210600019.pl
@@ -0,0 +1,15 @@
+use Modern::Perl;
+
+return {
+    bug_number => "26302",
+    description => "Add system preferences OPACResultsMaxItems and OPACResultsMaxItemsUnavailable");
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+        $dbh->do(q|
+            INSERT IGNORE INTO systempreferences (variable, value, options, explanation, type) VALUES
+                ('OPACResultsMaxItems','1','','Maximum number of available items displayed in search results','Integer'),
+                ('OPACResultsMaxItemsUnavailable','0','','Maximum number of unavailable items displayed in search results','Integer')
+        |);
+    },
+}
