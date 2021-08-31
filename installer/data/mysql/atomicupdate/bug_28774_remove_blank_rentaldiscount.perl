1fc2e3f7a547a0b703c7e359d9095b00316c4c94
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2021-08-31 10:16:49 +0200

    Bug 28774: DBRev 21.06.00.021
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- installer/data/mysql/atomicupdate/bug_28774_remove_blank_rentaldiscount.perl
+++
@@ -1,8 +0,0 @@
-$DBversion = 'XXX';
-if( CheckVersion( $DBversion ) ) {
-    $dbh->do(q{
-        DELETE FROM circulation_rules
-        WHERE rule_name = 'rentaldiscount' AND rule_value=''
-    });
-    NewVersion( $DBversion, 28774, "Delete blank rental discounts");
-}
---
+++ installer/data/mysql/db_revs/210600021.pl
@@ -0,0 +1,14 @@
+use Modern::Perl;
+
+return {
+    bug_number => "28774",
+    description => "Delete blank rental discounts",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+        $dbh->do(q{
+            DELETE FROM circulation_rules
+            WHERE rule_name = 'rentaldiscount' AND rule_value=''
+        });
+    },
+}
