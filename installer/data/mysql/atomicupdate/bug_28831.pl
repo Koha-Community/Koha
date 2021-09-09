b34bb8fc62d4a46fae3dd1337224492ac539d44f
Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
2021-09-09 09:37:50 +0000

    Bug 28831: Database revision, add new preference
    The preference OPACResultsUnavailableGroupingBy allows you to
    group unavailable items by substatus, only showing item counts, on
    the OPAC XSLT results.
    
    This is meant to be useful for larger consortia.
    
    Test plan:
    Run dbrev.
    Check new pref on Admin, preferences, OPAC tab.
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    
    Signed-off-by: David Nind <david@davidnind.com>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

---
+++ installer/data/mysql/atomicupdate/bug_28831.pl
@@ -0,0 +1,14 @@
+use Modern::Perl;
+
+return {
+    bug_number => "28831",
+    description => "Add system preferences OPACResultsUnavailableGroupingBy",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+        $dbh->do(q|
+            INSERT IGNORE INTO systempreferences (variable, value, options, explanation, type) VALUES
+                ('OPACResultsUnavailableGroupingBy','branch','branch\|substatus','Group OPAC XSLT results by branch or substatus','Choice')
+        |);
+    },
+}
