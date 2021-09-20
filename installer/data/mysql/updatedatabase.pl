bded25d262d59742bccbe2ec7fd8fe416f564a90
Victor Grousset/tuxayo <victor@tuxayo.net>
2021-09-20 08:44:59 +0200

    Bug 28772: DBRev 20.11.09.002

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -24436,6 +24436,36 @@ if( CheckVersion( $DBversion ) ) {
     NewVersion( $DBversion, "28774", "Delete blank rental discounts" );
 }
 
+$DBversion = '21.05.03.002';
+if ( CheckVersion( $DBversion ) ) {
+
+    use Koha::AuthUtils qw(hash_password);
+
+    my $sth = $dbh->prepare(q{
+        SELECT client_id, secret
+        FROM api_keys
+    });
+    $sth->execute;
+    my $results = $sth->fetchall_arrayref({});
+
+    $sth = $dbh->prepare(q{
+        UPDATE api_keys
+        SET
+            secret = ?
+        WHERE
+            client_id = ?
+    });
+
+    foreach my $api_key (@$results) {
+        unless ( $api_key->{secret} =~ m/^\$2a\$08\$/ ) {
+            my $digest = Koha::AuthUtils::hash_password( $api_key->{secret} );
+            $sth->execute( $digest, $api_key->{client_id} );
+        }
+    }
+
+    NewVersion( $DBversion, 28772, "Store hashed API key secrets" );
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
