36a37b245d8debe63e39c9eb551f3c169e68eada
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2021-09-20 13:57:06 +0200

    Bug 28972: Make DB rev idempotent
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- installer/data/mysql/atomicupdate/bug_28972.pl
+++ installer/data/mysql/atomicupdate/bug_28972.pl
@@ -6,10 +6,17 @@ return {
     up => sub {
         my ($args) = @_;
         my ($dbh, $out) = @$args{qw(dbh out)};
-        $dbh->do(q{
-            ALTER TABLE tmp_holdsqueue
-            ADD CONSTRAINT `tmp_holdsqueue_ibfk_2` FOREIGN KEY (`biblionumber`) REFERENCES `biblio` (`biblionumber`) ON DELETE CASCADE ON UPDATE CASCADE,
-            ADD CONSTRAINT `tmp_holdsqueue_ibfk_3` FOREIGN KEY (`borrowernumber`) REFERENCES `borrowers` (`borrowernumber`) ON DELETE CASCADE ON UPDATE CASCADE
-        });
+        unless ( foreign_key_exists('tmp_holdsqueue', 'tmp_holdsqueue_ibfk_2') ) {
+            $dbh->do(q{
+                ALTER TABLE tmp_holdsqueue
+                ADD CONSTRAINT `tmp_holdsqueue_ibfk_2` FOREIGN KEY (`biblionumber`) REFERENCES `biblio` (`biblionumber`) ON DELETE CASCADE ON UPDATE CASCADE
+            });
+        }
+        unless ( foreign_key_exists('tmp_holdsqueue', 'tmp_holdsqueue_ibfk_2') ) {
+            $dbh->do(q{
+                ALTER TABLE tmp_holdsqueue
+                ADD CONSTRAINT `tmp_holdsqueue_ibfk_3` FOREIGN KEY (`borrowernumber`) REFERENCES `borrowers` (`borrowernumber`) ON DELETE CASCADE ON UPDATE CASCADE
+            });
+        }
     },
 }
