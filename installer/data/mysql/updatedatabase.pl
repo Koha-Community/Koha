b17cf65633107306bd5d423998a50b65aaecce65
Kyle Hall <kyle@bywatersolutions.com>
2021-09-23 09:46:58 -0400

    Increment version for 21.05.04 release
    Signed-off-by: Kyle Hall <kyle@bywatersolutions.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -24466,6 +24466,11 @@ if ( CheckVersion( $DBversion ) ) {
     NewVersion( $DBversion, 28772, "Store hashed API key secrets" );
 }
 
+$DBversion = '21.05.04.000';
+if( CheckVersion( $DBversion ) ) {
+    NewVersion( $DBversion, "", "Koha 21.05.04 release" );
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
