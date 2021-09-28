d8ff6293e859a8f19580c9cf6f7e73e73a70e4c8
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2021-09-28 12:43:07 +0200

    Bug 29019: Restore eval call
    But fix it (pass an arrayref). We need the eval to catch possible errors.
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- tools/additional-contents.pl
+++ tools/additional-contents.pl
@@ -174,7 +174,7 @@ elsif ( $op eq 'add_validate' ) {
 elsif ( $op eq 'delete_confirmed' ) {
     my @ids = $cgi->multi_param('ids');
     my $deleted =
-        map { Koha::AdditionalContents->find($_)->delete() } @ids;
+        eval { Koha::AdditionalContents->search( { idnew => \@ids } )->delete; };
 
     if ( $@ or not $deleted ) {
         push @messages, { type => 'error', code => 'error_on_delete' };
