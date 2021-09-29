6ee9dcff441f8afbbafaf14b8c282b0968e3956b
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2021-09-29 12:46:54 +0200

    Bug 28826: DBRev 21.06.00.025
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

---
+++ installer/data/mysql/db_revs/210600025.pl
@@ -0,0 +1,16 @@
+use Modern::Perl;
+
+return {
+    bug_number => "28826",
+    description => "A system preference FacetOrder",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+
+        $dbh->do(q{
+            INSERT IGNORE INTO systempreferences
+            ( variable, value, options, explanation, type ) VALUES
+            ('FacetOrder','Alphabetical','Alphabetical|Usage','Specify the order of facets within each category','Choice')
+        });
+    },
+}
