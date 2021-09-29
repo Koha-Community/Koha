bb0328218974f59b8d952438a3cdd202adb13a0d
Aleisha Amohia <aleishaamohia@hotmail.com>
2021-09-30 11:31:45 +1300

    Bug 27266: (follow-up) Remove get_marc_authors from opac-detail
    There's no way to use 'no' XSLT now, so don't need to use this to show
    authors on the detail page.
    
    Signed-off-by: David Nind <david@davidnind.com>
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- opac/opac-detail.pl
+++ opac/opac-detail.pl
@@ -783,7 +783,6 @@ if (scalar(@itemloop) == 0 || scalar(@otheritemloop) == 0) {
 }
 
 my $marcnotesarray = $biblio->get_marc_notes({ marcflavour => $marcflavour, opac => 1 });
-my $marcauthorsarray = $biblio->get_marc_authors;
 
 if( C4::Context->preference('ArticleRequests') ) {
     my $patron = $borrowernumber ? Koha::Patrons->find($borrowernumber) : undef;
@@ -799,7 +798,6 @@ if( C4::Context->preference('ArticleRequests') ) {
 my $norequests = ! $biblio->items->filter_by_for_hold->count;
     $template->param(
                      MARCNOTES               => $marcnotesarray,
-                     MARCAUTHORS             => $marcauthorsarray,
                      norequests              => $norequests,
                      itemdata_ccode          => $itemfields{ccode},
                      itemdata_materials      => $itemfields{materials},
