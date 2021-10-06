a5e0c1a3e985f21f3585c7ce6f7367696e054b32
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2021-10-06 17:00:07 +0200

    Bug 18984: DB changes
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

---
+++ installer/data/mysql/atomicupdate/bug_18984.pl
@@ -0,0 +1,12 @@
+use Modern::Perl;
+
+return {
+    bug_number => "18984",
+    description => "Remove NORMARC from search_marc_map.marc_type",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+        $dbh->do(q{DELETE FROM search_marc_map WHERE marc_type='normarc'});
+        $dbh->do(q{ALTER TABLE search_marc_map MODIFY `marc_type` enum('marc21','unimarc') COMMENT 'what MARC type this map is for'});
+    },
+}
