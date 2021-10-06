7ce74ab1330b4eedfa3502c85a4a6b8bdb009bd9
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2021-10-06 14:55:45 +0200

    Bug 25429: DBRev 21.06.00.030
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

---
+++ installer/data/mysql/db_revs/210600030.pl
@@ -0,0 +1,14 @@
+use Modern::Perl;
+
+return {
+    bug_number => "25429",
+    description => "Add new system preference CleanUpDatabaseReturnClaims",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+        $dbh->do(q{
+            INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type` ) VALUES
+            ('CleanUpDatabaseReturnClaims', '', '', 'Sets the age of resolved return claims to delete from the database for cleanup_database.pl', 'Integer' );
+        });
+    },
+}
