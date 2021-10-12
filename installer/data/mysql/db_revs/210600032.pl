ccaa25a70d164e05084150cb025bff56af457e0d
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2021-10-12 11:14:38 +0200

    Bug 29138: DBRev 21.06.00.032
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- installer/data/mysql/db_revs/210600032.pl
+++ installer/data/mysql/db_revs/210600032.pl
@@ -2,13 +2,13 @@ use Modern::Perl;
 
 return {
     bug_number => "29138",
-    description => "Use a zero instead of a no in LoadSearchHistoryToTheFirstLoggedUser",
+    description => "Use a zero instead of a 'no' in LoadSearchHistoryToTheFirstLoggedUser",
     up => sub {
         my ($args) = @_;
         my ($dbh, $out) = @$args{qw(dbh out)};
         $dbh->do(q{
             UPDATE systempreferences SET value='0'
-            WHERE variable='LoadSearchHistoryToTheFirstLoggedUser' AND COALESCE(value,'0')<>'1';
+            WHERE variable='LoadSearchHistoryToTheFirstLoggedUser' AND value='no';
         });
     },
 }
