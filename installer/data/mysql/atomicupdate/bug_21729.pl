ba5f0d39bddc9be0a84af31c3e628f75b6736860
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2021-10-13 11:02:24 +0200

    Bug 21729: DB changes
    Signed-off-by: Andrew Fuerste-Henry <andrew@bywatersolutions.com>
    Signed-off-by: Florian Bontemps <florian.bontemps@biblibre.com>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

---
+++ installer/data/mysql/atomicupdate/bug_21729.pl
@@ -0,0 +1,25 @@
+use Modern::Perl;
+
+return {
+    bug_number => "21729",
+    description => "Add new column reserves.patron_expiration_date",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+        unless( column_exists('reserves', 'patron_expiration_date') ) {
+            $dbh->do(q{
+                ALTER TABLE reserves
+                ADD COlUMN patron_expiration_date date DEFAULT NULL COMMENT 'the date the hold expires - usually the date entered by the patron to say they don''t need the hold after a certain date'
+                AFTER expirationdate
+            });
+        }
+        unless( column_exists('old_reserves', 'patron_expiration_date') ) {
+            $dbh->do(q{
+                ALTER TABLE old_reserves
+                ADD COlUMN patron_expiration_date date DEFAULT NULL COMMENT 'the date the hold expires - usually the date entered by the patron to say they don''t need the hold after a certain date'
+                AFTER expirationdate
+            });
+        }
+
+    },
+}
