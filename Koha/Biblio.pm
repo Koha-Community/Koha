ca6ef8ddaf732587ad4293b7d5bd2122b7ca685c
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2021-10-18 16:14:07 +0100

    Bug 11175: (QA follow-up) Be more specific with title fields
    We were using the MARC::Record title accessor, but that just stringifies
    all the subfields of field 245. The indexer, and the XSLT, specifically
    use 245a so we need to do the same.
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- Koha/Biblio.pm
+++ Koha/Biblio.pm
@@ -543,7 +543,7 @@ sub get_components_query {
         }
     }
     else {
-        my $cleaned_title = $marc->title;
+        my $cleaned_title = $marc->subfield('245', "a");
         $cleaned_title =~ tr|/||;
         $cleaned_title = $builder->clean_search_term($cleaned_title);
         $searchstr = "Host-item:($cleaned_title)";
