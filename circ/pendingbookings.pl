1f7038d11a8fcd3013fc0b25a1a3a253db037b71
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2021-10-19 13:57:00 +0100

    Bug 33736: Add pendingbookings report
    This patch adds a 'Bookings to collect' page to circulation that allows
    for a workflow similar to 'Holds to pull', but for bookings.
    
    Sponsored-by: PTFS Europe Ltd
    Signed-off-by: Esther Melander <esther@bywatersolutions.com>
    Signed-off-by: Paul Derscheid <paul.derscheid@lmscloud.de>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

---
+++ circ/pendingbookings.pl
@@ -0,0 +1,49 @@
+#!/usr/bin/perl
+
+# Copyright PTFS Europe 2021
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+use CGI qw ( -utf8 );
+
+use C4::Output qw( output_html_with_http_headers );
+use C4::Auth qw( get_template_and_user );
+
+use Koha::DateUtils qw(dt_from_string);
+
+my $input = CGI->new;
+my ( $template, $borrowernumber, $cookie, $flags ) = get_template_and_user(
+    {
+        template_name => "circ/pendingbookings.tt",
+        query         => $input,
+        type          => "intranet",
+        flagsrequired => { circulate => 'manage_bookings' },
+    }
+);
+
+my $today = dt_from_string();
+my $startdate = $today->truncate( to => 'day' );
+my $enddate = $startdate->clone->add( days => 1 );
+
+$template->param(
+    todaysdate          => $today,
+    from                => $startdate,
+    to                  => $enddate
+);
+
+output_html_with_http_headers $input, $cookie, $template->output;
