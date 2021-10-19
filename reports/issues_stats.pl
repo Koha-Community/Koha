70f0d50d0721f6692453176f1d901f638d165092
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2021-10-19 11:08:29 +0200

    Bug 29204: Fix reports/issues_stats.pl for MySQL 8
    Error was: Incorrect DATETIME value: ''
    
    Test plan
    Go to Reports -> Circulation
    On Period select "Row" or "Column" and choose a date
    Submit
    
    Signed-off-by: Joonas Kylmälä <joonas.kylmala@iki.fi>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- reports/issues_stats.pl
+++ reports/issues_stats.pl
@@ -312,7 +312,8 @@ sub calculate {
           ( $linesource eq 'items' )
           ? " LEFT JOIN items ON (statistics.itemnumber = items.itemnumber) "
           : " LEFT JOIN borrowers ON (statistics.borrowernumber = borrowers.borrowernumber) ";
-        $strsth .= " WHERE $line is not null AND $line != '' ";
+        $strsth .= " WHERE $line IS NOT NULL ";
+        $strsth .= " AND $line != '' " if $line ne "datetime";
     }
 
     if ( $line =~ /datetime/ ) {
@@ -400,7 +401,8 @@ sub calculate {
           ( $colsource eq 'items' )
           ? "LEFT JOIN items ON (statistics.itemnumber = items.itemnumber) "
           : "LEFT JOIN borrowers ON (statistics.borrowernumber = borrowers.borrowernumber) ";
-        $strsth2 .= " WHERE $column IS NOT NULL AND $column != '' ";
+        $strsth .= " WHERE $column IS NOT NULL";
+        $strsth .= " AND $column != '' " if $column ne "datetime";
     }
 
     if ( $column =~ /datetime/ ) {
