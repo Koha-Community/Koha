ab13f33eaee2599beb376366524d92afa79df034
Nick Clemens <nick@bywatersolutions.com>
2021-10-20 12:46:07 +0000

    Bug 29284: Don't die on analytics searching error
    This patch adds an eval around the call to search for analytic records
    It pases a value to the template on the staff side, but logs the warning on
    the opac
    
    This seems similar to 'decoding_error' which is noted on staff side, but absent on OPAC
    
    The eval follows the patter used during searching
    
    To test:
    1 - Add a title to catalog, with 245a:
        Digger does it all (not really!)
    2 - Set searchEngine preference to: Elasticsearch
    3 - The record does not load
    4 - Apply patch
    5 - The record loads, there is a note about analytics at the top fo the record
    6 - View record in opac, no note
    7 - Check logs on intranet and opac, searching error is logged
    
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

--- catalogue/detail.pl
+++ catalogue/detail.pl
@@ -139,10 +139,16 @@ if ( $xslfile ) {
       ( C4::Context->preference('UseControlNumber') and $record->field('001') )
       ? 'rcn:'. $record->field('001')->data . ' AND (bib-level:a OR bib-level:b)'
       : "Host-item:($cleaned_title)";
-    my ( $err, $result, $count ) = $searcher->simple_search_compat( $query, 0, 0 );
+    my ( $err, $result, $count );
+    eval {
+        ( $err, $result, $count ) =
+          $searcher->simple_search_compat( $query, 0, 0 );
 
-    warn "Warning from simple_search_compat: $err"
-        if $err;
+    };
+    if ($err || $@){
+        warn "Warning from simple_search_compat: $err.$@";
+        $template->param( analytics_error => 1 );
+    }
 
     my $variables = {
         show_analytics_link => $count > 0 ? 1 : 0
