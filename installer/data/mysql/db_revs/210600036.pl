e88ee4c1c021fc40ead2fdb5b92f346a7512ef9c
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2021-10-21 12:20:03 +0200

    Bug 29200: DBRev 21.06.00.036
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- installer/data/mysql/db_revs/210600036.pl
+++ installer/data/mysql/db_revs/210600036.pl
@@ -6,9 +6,7 @@ return {
     up => sub {
         my ($args) = @_;
         my ($dbh, $out) = @$args{qw(dbh out)};
-        # Do you stuffs here
         $dbh->do(q{DELETE FROM systempreferences WHERE variable LIKE 'AdlibrisCovers%'});
-        # Print useful stuff here
         say $out "AdlibrisCoversEnabled and AdlibrisCoversURL preferences removed.";
     },
 }
