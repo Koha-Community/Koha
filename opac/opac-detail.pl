471712625b3f5c727333409f9589a0b936d26cc0
Nick Clemens <nick@bywatersolutions.com>
2021-10-21 10:26:12 +0000

    Bug 29284: (follow-up) Improve error logging to prevent warns
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    (cherry picked from commit cf29caf39fed2d81262735fe7893a1ac3a8d59b1)
    Signed-off-by: Victor Grousset/tuxayo <victor@tuxayo.net>

--- opac/opac-detail.pl
+++ opac/opac-detail.pl
@@ -196,12 +196,15 @@ if ( $xslfile ) {
           $searcher->simple_search_compat( $query, 0, 0 );
     };
     if ($err || $@){
-        warn "Warning from simple_search_compat: $err.$@";
+        my $error = q{};
+        $error .= $err if $err;
+        $error .= $@ if $@;
+        warn "Warning from simple_search_compat: $error";
     }
 
     my $variables = {
         anonymous_session   => ($borrowernumber) ? 0 : 1,
-        show_analytics_link => $count > 0 ? 1 : 0
+        show_analytics_link => defined $count && $count > 0 ? 1 : 0
     };
 
     my @plugin_responses = Koha::Plugins->call(
