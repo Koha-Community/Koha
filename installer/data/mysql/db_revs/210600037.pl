c0a03d34cafd28eaa07e565109b448d09c04d398
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2021-10-26 15:44:19 +0200

    Bug 11175: DBRev 21.06.00.037
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

---
+++ installer/data/mysql/db_revs/210600037.pl
@@ -0,0 +1,19 @@
+use Modern::Perl;
+
+return {
+    bug_number => "11175",
+    description => "Show component records in detail views",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+        $dbh->do(q{
+            INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type` )
+            VALUES ('ShowComponentRecords', 'nowhere', 'nowhere|staff|opac|both','In which record detail pages to show list of the component records, as linked via 773','Choice')
+        });
+
+        $dbh->do(q{
+            INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type` )
+            VALUES ('MaxComponentRecords', '300', '','Max number of component records to display','Integer')
+        });
+    },
+}
