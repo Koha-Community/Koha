f05ae32c34c1a8c41a34838c2a71f949d6ef8b08
Fridolin Somers <fridolin.somers@biblibre.com>
2021-10-28 10:29:13 -1000

    Increment version for 20.11.11 release
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -23711,6 +23711,11 @@ if( CheckVersion( $DBversion ) ) {
     NewVersion( $DBversion, 29137, "Add system preference CreateAVFromCataloguing");
 }
 
+$DBversion = '20.11.11.000';
+if( CheckVersion( $DBversion ) ) {
+    NewVersion( $DBversion, "", "Koha 20.11.11 release" );
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
