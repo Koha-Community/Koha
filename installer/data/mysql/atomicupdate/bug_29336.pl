2652f299987879c6a71d65bcaaf1a5101008ddfb
Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
2021-10-28 12:11:07 +0000

    Bug 29336: Database revision
    Resizes the six fields identified earlier:
    
    additional_fields.authorised_value_category 16=>32
    auth_subfield_structure.authorised_value 10=>32
    auth_tag_structure.authorised_value 10=>32
    club_template_enrollment_fields.authorised_value_category 16=>32
    club_template_fields.authorised_value_category 16=>32
    marc_tag_structure.authorised_value 10=>32
    
    Test plan:
    Run dbrev, check database.
    Bonus: Connect an AV category with a long name to a authority subfield.
    Check if you see its values in the authority editor.
    (This was the way I found the problem.)
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    
    Signed-off-by: David Nind <david@davidnind.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

---
+++ installer/data/mysql/atomicupdate/bug_29336.pl
@@ -0,0 +1,21 @@
+use Modern::Perl;
+
+return {
+    bug_number => 29336,
+    description => "Resize authorised value category fields to 32 chars",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+        my @sql_statements = (
+            q|ALTER TABLE additional_fields CHANGE authorised_value_category authorised_value_category varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ''|,
+            q|ALTER TABLE auth_subfield_structure CHANGE authorised_value authorised_value varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL|,
+            q|ALTER TABLE auth_tag_structure CHANGE authorised_value authorised_value varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL|,
+            q|ALTER TABLE club_template_enrollment_fields CHANGE authorised_value_category authorised_value_category varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL|,
+            q|ALTER TABLE club_template_fields CHANGE authorised_value_category authorised_value_category varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL|,
+            q|ALTER TABLE marc_tag_structure CHANGE authorised_value authorised_value varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL|,
+        );
+        foreach my $sql ( @sql_statements ) {
+            $dbh->do( $sql );
+        }
+    },
+}
