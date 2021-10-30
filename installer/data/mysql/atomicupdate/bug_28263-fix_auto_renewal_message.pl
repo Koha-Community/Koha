f831175338d16719e6202b42cb12afa7660877c9
Joonas Kylmälä <joonas.kylmala@iki.fi>
2021-10-30 20:19:39 +0000

    Bug 28263: (QA follow-up) Use new atomicupdate code style
    Signed-off-by: Joonas Kylmälä <joonas.kylmala@iki.fi>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

---
+++ installer/data/mysql/atomicupdate/bug_28263-fix_auto_renewal_message.pl
@@ -0,0 +1,15 @@
+use Modern::Perl;
+
+return {
+    bug_number => "28263",
+    description => "Update AUTO_RENEWAL too_many message",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh) = @$args{qw(dbh)};
+        $dbh->do(q{
+            UPDATE letter SET
+            content = REPLACE(content, "You have reached the maximum number of checkouts possible." , "You have reached the maximum number of renewals possible.")
+            WHERE ( code = 'AUTO_RENEWALS' OR code = 'AUTO_RENEWALS_DGST' );
+        });
+    },
+}
