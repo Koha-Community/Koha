f2febaeac4d4567fd3d5dbaf64d69b7226378e0f
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2021-11-03 15:07:07 +0100

    Bug 26326: DBRev 21.06.00.042
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

---
+++ installer/data/mysql/db_revs/210600042.pl
@@ -0,0 +1,23 @@
+use Modern::Perl;
+
+return {
+    bug_number => "26326",
+    description => "Add primary key to import_record_matches",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+        unless(
+            primary_key_exists('import_record_matches','import_record_id') &&
+            primary_key_exists('import_record_matches','candidate_match_id')
+        ){
+            if( primary_key_exists('import_record_matches' ) ){
+                $dbh->do(q|
+                    ALTER TABLE import_record_matches DROP PRIMARY KEY
+                |);
+            }
+            $dbh->do(q|
+                ALTER TABLE import_record_matches ADD PRIMARY KEY (import_record_id,candidate_match_id)
+            |);
+        }
+    },
+}
