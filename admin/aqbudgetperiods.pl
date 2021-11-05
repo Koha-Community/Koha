5324962a8beb4d85619d00d06be443f0a67b6629
Florian Bontemps <florian.bontemps@biblibre.com>
2021-11-05 16:28:37 +0000

    Bug 29429: (bug 17600 follow-up) Fix import in aqbudgetperiods.pl
    Adds GetBudgetHierarchy to the functions called by C4::Budgets
    
    Plan :
    1. Create a budget with a random amount, save it.
    2. Try to close the budget.
    3. You should get an Error Trace.
    
    4. Apply the patch
    5. Close the budget again
    6. It should work.
    
    Signed-off-by: Joonas Kylmälä <joonas.kylmala@iki.fi>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- admin/aqbudgetperiods.pl
+++ admin/aqbudgetperiods.pl
@@ -55,7 +55,7 @@ use C4::Context;
 use C4::Auth qw( get_template_and_user );
 use C4::Output qw( output_html_with_http_headers );
 use C4::Acquisition;
-use C4::Budgets qw( GetBudgetPeriod GetBudgetPeriods ModBudgetPeriod AddBudgetPeriod GetBudgets DelBudgetPeriod CloneBudgetPeriod MoveOrders );
+use C4::Budgets qw( GetBudgetPeriod GetBudgetHierarchy GetBudgetPeriods ModBudgetPeriod AddBudgetPeriod GetBudgets DelBudgetPeriod CloneBudgetPeriod MoveOrders );
 use C4::Log qw(logaction);
 use Koha::Acquisition::Currencies;
 
