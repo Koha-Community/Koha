78532c6f49f6daa67d714ddb78c3cce5b6cf4141
Tomas Cohen Arazi <tomascohen@theke.io>
2021-11-11 15:23:27 -0300

    Bug 27779: New atomic update syntax
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- installer/data/mysql/atomicupdate/bug_27779.perl
+++
@@ -1,8 +0,0 @@
-$DBversion = 'XXX'; # will be replaced by the RM
-if( CheckVersion( $DBversion ) ) {
-
-    # REFUND
-    $dbh->do( "UPDATE account_credit_types SET description = 'Refund' WHERE code = 'REFUND'" );
-
-    NewVersion( $DBversion, 27779, "Simplify credit descriptions");
-}
---
+++ installer/data/mysql/atomicupdate/bug_27779.pl
@@ -0,0 +1,15 @@
+use Modern::Perl;
+
+return {
+    bug_number  => "27779",
+    description => "Simplify credit descriptions",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+
+        $dbh->do(q{
+            UPDATE account_credit_types
+            SET description = 'Refund' WHERE code = 'REFUND'
+        });
+    },
+}
