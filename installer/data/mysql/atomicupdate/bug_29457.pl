1dec6d946875b7151278fab9d6da65995f37cdab
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2021-11-17 16:49:27 +0000

    Bug 29457: Generic warning at upgrade
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Amended: Adding exec flag and two dots.
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>
    
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

---
+++ installer/data/mysql/atomicupdate/bug_29457.pl
@@ -0,0 +1,12 @@
+use Modern::Perl;
+
+return {
+    bug_number => "29457",
+    description => "Fee Cancellation records the wrong manager_id",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+        say $out "WARNING: You may have some incorrect manager_id's recorded against account cancellation lines, please see bugzilla for details.";
+        say $out "NOTE: You may already have this bugfix applied at an earlier upgrade.";
+    },
+}
