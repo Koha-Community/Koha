9e09581995ca155b1aae3d62ddf17bff41f54bcb
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2021-11-17 16:07:40 +0000

    Bug 29519: Allow resolution of claims return at checkin
    Add a 'Resolve' button in the alert dialogue that is displayed when a
    lost item with a return claim is checked in. The button will trigger the
    usual resolution modal allowing the user to pick their resolution.
    
    This patch splits the resolution modal out of checkouts.js and
    checkouts-table.inc so it can be used outside of the checkouts table.
    We then reload it, optionally based upon the presence of the claims
    preference, where needed.  This has the added benefit that it saves a
    little bit of page load data in cases where the feature is not enabled.
    
    Test plan
    1. As we alter the file locations of the resolution handling code we
       need to test that normal claims functionality continue to work as
       expected.
    2. Test the new functoinality by checking in an item that has been
       claimed as returned (but not yet resolved). The dialogue box should
       now contain a 'resolve' button next to each claimant and clicking
       upon it should trigger the resolution modal where the librarian can
       subsequently pick the resolution and submit it.
    
    Signed-off-by: Andrew Fuerste-Henry <andrew@bywatersolutions.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

---
+++ koha-tmpl/intranet-tmpl/prog/js/resolve_claim_modal.js
@@ -0,0 +1,40 @@
+$('body').on('click', '.return-claim-tools-resolve', function() {
+    let id = $(this).data('return-claim-id');
+    let current_lost_status = $(this).data('current-lost-status');
+
+    $('#claims-returned-resolved-modal-id').val(id);
+    $("#new_lost_status").val(current_lost_status);
+    let selected_option = $("#new_lost_status option:selected");
+    $(selected_option).text(_("%s (current status)").format($(selected_option).text()));
+    $('#claims-returned-resolved-modal').modal()
+});
+
+$(document).on('click', '#claims-returned-resolved-modal-btn-submit', function(e) {
+    let resolution = $('#claims-returned-resolved-modal-resolved-code').val();
+    let new_lost_status = $('#new_lost_status').val();
+    let id = $('#claims-returned-resolved-modal-id').val();
+
+    $('#claims-returned-resolved-modal-btn-submit-spinner').show();
+    $('#claims-returned-resolved-modal-btn-submit-icon').hide();
+
+    params = {
+        resolution: resolution,
+        resolved_by: logged_in_user_borrowernumber,
+        new_lost_status: new_lost_status
+    };
+
+    $.ajax({
+        url: '/api/v1/return_claims/' + id + '/resolve',
+        type: 'PUT',
+        data: JSON.stringify(params),
+        success: function(data) {
+            $('#claims-returned-resolved-modal-btn-submit-spinner').hide();
+            $('#claims-returned-resolved-modal-btn-submit-icon').show();
+            $('#claims-returned-resolved-modal').modal('hide')
+
+            refreshReturnClaimsTable();
+        },
+        contentType: "json"
+    });
+
+});
