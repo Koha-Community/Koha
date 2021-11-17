7beddcb7a9347e280dee0a2a43bcad5a5405e9bc
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2021-11-17 09:56:48 +0000

    Bug 29495: Drop issue_id constraint from return_claims
    This patch removes the issue_id constraint from return_claims.
    
    Due to the nature of our dual table approach to checkouts/old_checkouts
    we can't safely hae this constraint and not lose data.  Prior to this
    commit, when an item is checked in we move the checkout from checkouts
    to old_checkouts.. this therefore triggers the delete of the issue_id
    from the return_claims table as described by the foreign key constraint.
    
    Signed-off-by: Andrew Fuerste-Henry <andrew@bywatersolutions.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

---
+++ installer/data/mysql/atomicupdate/bug_29495.pl
@@ -0,0 +1,15 @@
+use Modern::Perl;
+
+return {
+    bug_number => "29495",
+    description => "Drop issue_id constraint from return_claims table",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+        if ( foreign_key_exists( 'return_claims', 'issue_id' ) ) {
+            $dbh->do(q{
+                ALTER TABLE return_claims DROP FOREIGN KEY issue_id
+            });
+        }
+    },
+};
