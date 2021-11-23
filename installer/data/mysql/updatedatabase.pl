29be0a38bffa3f1062092911e8ad4a1f257ac7a2
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2021-11-23 12:12:02 +0100

    Bug 28772: DBRev 19.11.23.001
    Note that this added back the following patch: "Bug 28772: Do not hash secrets twice"
    
    https://bugs.koha-community.org/show_bug.cgi?id=29132
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    
    Signed-off-by: Wainui Witika-Park <wainuiwitikapark@catalyst.net.nz>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -20843,6 +20843,37 @@ if ( CheckVersion($DBversion) ) {
     SetVersion ($DBversion);
 }
 
+$DBversion = "19.11.23.001";
+if ( CheckVersion($DBversion) ) {
+
+    require Koha::AuthUtils;
+
+    my $sth = $dbh->prepare(q{
+        SELECT client_id, secret
+        FROM api_keys
+    });
+    $sth->execute;
+    my $results = $sth->fetchall_arrayref({});
+
+    $sth = $dbh->prepare(q{
+        UPDATE api_keys
+        SET
+            secret = ?
+        WHERE
+            client_id = ?
+    });
+
+    foreach my $api_key (@$results) {
+        unless ( $api_key->{secret} =~ m/^\$2a\$08\$/ ) {
+            my $digest = Koha::AuthUtils::hash_password( $api_key->{secret} );
+            $sth->execute( $digest, $api_key->{client_id} );
+        }
+    }
+
+    print "Upgrade to $DBversion done (Bug 28772 - Store hashed API key secrets)\n";
+    SetVersion ($DBversion);
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
