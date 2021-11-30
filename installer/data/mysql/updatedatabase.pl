40281bc6bcee802c0e8034c8a65dd4305d07d9be
Fridolin Somers <fridolin.somers@biblibre.com>
2021-11-29 20:47:14 -1000

    Increment version for 20.11.12 release
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -23737,6 +23737,11 @@ if( CheckVersion( $DBversion ) ) {
     NewVersion( $DBversion, "29341", "Remove foreign keys on pseudonymized_transactions" );
 }
 
+$DBversion = '20.11.12.000';
+if( CheckVersion( $DBversion ) ) {
+    NewVersion( $DBversion, "", "Koha 20.11.12 release" );
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
