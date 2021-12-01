352843bd61006761fb269608c398f395c763df21
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2021-12-01 10:21:59 +0100

    Bug 29586: Add atomic update for existing intallations
    In case an installation was created on 21.11.00
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    
    Signed-off-by: David Nind <david@davidnind.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

---
+++ installer/data/mysql/atomicupdate/bug_29586.pl
@@ -0,0 +1,23 @@
+use Modern::Perl;
+
+return {
+    bug_number => "29586",
+    description => "Add Hold Reminder messaging preference if missing",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+        $dbh->do(q{
+            INSERT IGNORE INTO `message_attributes`
+                (message_attribute_id, message_name, `takes_days`)
+            VALUES (10, 'Hold_Reminder', 0)
+        });
+        $dbh->do(q{
+            INSERT IGNORE INTO `message_transports`
+                (`message_attribute_id`, `message_transport_type`, `is_digest`, `letter_module`, `letter_code`)
+            VALUES  (10, 'email', 0, 'circulation', 'HOLD_REMINDER'),
+                    (10, 'sms', 0, 'circulation', 'HOLD_REMINDER'),
+                    (10, 'phone', 0, 'circulation', 'HOLD_REMINDER'),
+                    (10, 'itiva', 0, 'circulation', 'HOLD_REMINDER')
+        });
+    },
+}
