9c15ad121f44ca821ffc70a88d02b05cb7a46a2d
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2021-12-02 12:12:10 +0100

    Bug 29605: Missing FK on tmp_holdsqueue.borrowernumber (tmp_holdsqueue_ibfk_3)
    Bad copy paste on the foreign_key_exists condition
    
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

--- installer/data/mysql/atomicupdate/bug_29605.pl
+++ installer/data/mysql/atomicupdate/bug_29605.pl
@@ -2,10 +2,10 @@ use Modern::Perl;
 
 return {
     bug_number  => "29605",
-    description => "Add language_script_mapping primary key",
+    description => "Resync DB structure for existing installations",
     up => sub {
         my ($args) = @_;
-        my ($dbh) = @$args{qw(dbh)};
+        my ($dbh, $out) = @$args{qw(dbh out)};
 
         if (   !primary_key_exists( 'language_script_mapping', 'language_subtag' )
             and index_exists( 'language_script_mapping', 'language_subtag' ) ) {
@@ -22,6 +22,18 @@ return {
                 ALTER TABLE language_script_mapping
                     ADD PRIMARY KEY `language_subtag` (`language_subtag`);
             });
+
+            say $out "Added missing primary key on language_script_mapping"
         }
+
+        unless ( foreign_key_exists('tmp_holdsqueue', 'tmp_holdsqueue_ibfk_3') ) {
+            $dbh->do(q{
+                ALTER TABLE tmp_holdsqueue
+                ADD CONSTRAINT `tmp_holdsqueue_ibfk_3` FOREIGN KEY (`borrowernumber`) REFERENCES `borrowers` (`borrowernumber`) ON DELETE CASCADE ON UPDATE CASCADE
+            });
+
+            say $out "Added missing foreign key on tmp_holdsqueue"
+        }
+
     },
 }
