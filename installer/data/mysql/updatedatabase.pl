c2eb9ee3ce1c60e18c4035b0bd2d0a44f1662507
Victor Grousset/tuxayo <victor@tuxayo.net>
2021-12-23 23:08:42 +0100

    Increment version for 20.11.13 release
    Signed-off-by: Victor Grousset/tuxayo <victor@tuxayo.net>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -23742,6 +23742,12 @@ if( CheckVersion( $DBversion ) ) {
     NewVersion( $DBversion, "", "Koha 20.11.12 release" );
 }
 
+
+$DBversion = '20.11.13.000';
+if ( CheckVersion( $DBversion ) ) {
+    NewVersion( $DBversion, "", "Koha 20.11.13 release" );
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
