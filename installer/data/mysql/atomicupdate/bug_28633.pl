131ff0f4025eec5ac7d4b72c9add0e73d76cd7eb
Nick Clemens <nick@bywatersolutions.com>
2022-01-03 15:21:52 +0000

    Bug 28633: DB update
    This adds the field to necessary tables and updates API and columns def
    and sets preferred name to firstname
    
    Signed-off-by: Emily Lamancusa <emily.lamancusa@montgomerycountymd.gov>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

---
+++ installer/data/mysql/atomicupdate/bug_28633.pl
@@ -0,0 +1,61 @@
+use Modern::Perl;
+
+return {
+    bug_number  => "28633",
+    description => "Add preferred_name to borrowers table",
+    up          => sub {
+        my ($args) = @_;
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
+        if ( !column_exists( 'borrowers', 'preferred_name' ) ) {
+            $dbh->do(
+                q{
+                ALTER TABLE borrowers
+                ADD COLUMN preferred_name longtext NULL DEFAULT NULL
+                COMMENT "patron/borrower's preferred name"
+                AFTER firstname
+            }
+            );
+            say $out "Added column 'borrowers.preferred_name'";
+        }
+        if ( !column_exists( 'deletedborrowers', 'preferred_name' ) ) {
+            $dbh->do(
+                q{
+                ALTER TABLE deletedborrowers
+                ADD COLUMN preferred_name longtext NULL DEFAULT NULL
+                COMMENT "patron/borrower's preferred name"
+                AFTER firstname
+            }
+            );
+            say $out "Added column 'deletedborrowers.preferred_name'";
+        }
+        if ( !column_exists( 'borrower_modifications', 'preferred_name' ) ) {
+            $dbh->do(
+                q{
+                ALTER TABLE borrower_modifications
+                ADD COLUMN preferred_name longtext NULL DEFAULT NULL
+                COMMENT "patron/borrower's preferred name"
+                AFTER firstname
+            }
+            );
+            say $out "Added column 'borrower_modifications.preferred_name'";
+        }
+        my @default_patron_search_fields = split( '\|', C4::Context->preference('DefaultPatronSearchFields') );
+        unless ( grep /preferred_name/, @default_patron_search_fields ) {
+            if ( grep /firstname/, @default_patron_search_fields ) {
+                push @default_patron_search_fields, 'preferred_name';
+                C4::Context->set_preference( 'DefaultPatronSearchFields', join( '|', @default_patron_search_fields ) );
+                say $out "Added 'preferred_name' to DefaultPatronSearchFields";
+            } else {
+                say $out "Please add 'preferred_name' to DefaultPatronSearchFields if you want it searched by default";
+            }
+        }
+        $dbh->do(
+            q{
+            UPDATE borrowers
+            SET preferred_name = firstname
+            WHERE preferred_name IS NULL
+        }
+        );
+        say $out "Initially set preferred_name to firstname";
+    },
+    }
