235e73e908a602b58565ce6fa37368b9336e5275
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2022-01-03 11:31:54 +0100

    Bug 29778: Prevent orphan additional contents
    When deleting a content, only the main one (lang="default") is removed,
    which leads to orphan contents in the DB that are still displayed on the
    UI.
    
    Test plan:
    0. Don't apply this patch
    1. Create some contents, translate them in different languages
    2. Delete some of them
    => Note that they are still displayed on the UI and that the entries
    with lang!="default" are still in the DB
    3. Apply this patch
    4. Repeat 1
    5. Run updatedatabase
    6. Delete from of the contents
    => Note that the orphan entries created before you applied the patch
    have been removed and that the "delete" behaviour is now working
    correnctly.
    
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

---
+++ installer/data/mysql/atomicupdate/bug_29778.pl
@@ -0,0 +1,18 @@
+use Modern::Perl;
+
+return {
+    bug_number => "29778",
+    description => "Delete orphan additional contents",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+        $dbh->do(q{
+            DELETE FROM additional_contents
+            WHERE code NOT IN (
+                SELECT code
+                FROM additional_contents
+                WHERE lang = "default"
+            );
+        });
+    },
+}
