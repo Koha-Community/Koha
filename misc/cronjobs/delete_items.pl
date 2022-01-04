e4f4a7ea98c64b7726784d58b174eb010ae9d4f2
Tomas Cohen Arazi <tomascohen@theke.io>
2022-01-04 16:55:05 -0300

    Bug 29794: (follow-up) Add license
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- misc/cronjobs/delete_items.pl
+++ misc/cronjobs/delete_items.pl
@@ -1,12 +1,28 @@
 #! /usr/bin/perl
 
-use Getopt::Long qw( GetOptions );
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
 
-use Koha::Script -cron;
-use C4::Context;
 use Modern::Perl;
+
+use Getopt::Long qw( GetOptions );
 use Pod::Usage qw( pod2usage );
 
+use Koha::Script -cron;
+
+use C4::Context;
 use Koha::Items;
 
 my $dbh = C4::Context->dbh();
