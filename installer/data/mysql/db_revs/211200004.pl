3eb7480e0c58f095a138d3efc16746699db3ccf9
Fridolin Somers <fridolin.somers@biblibre.com>
2022-01-09 20:21:54 -1000

    Bug 26296: DBrev 21.12.00.004
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

---
+++ installer/data/mysql/db_revs/211200004.pl
@@ -0,0 +1,15 @@
+use Modern::Perl;
+
+return {
+    bug_number => "26296",
+    description => "Replace comma with pipe in OPACSuggestion field preferences",
+    up => sub {
+        my ($args) = @_;
+        my $dbh = $args->{dbh};
+
+        $dbh->do(q{
+            UPDATE systempreferences SET value = REPLACE(value, ',', '|')
+            WHERE variable IN ('OPACSuggestionMandatoryFields','OPACSuggestionUnwantedFields')
+        });
+    },
+}
