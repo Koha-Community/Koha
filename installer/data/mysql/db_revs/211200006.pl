42491ed1c38de87fdd5ffe2c0015e06132ddc783
Fridolin Somers <fridolin.somers@biblibre.com>
2022-01-11 11:45:26 -1000

    Bug 29557: DBRev 21.12.00.006
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>
    
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- installer/data/mysql/db_revs/211200006.pl
+++ installer/data/mysql/db_revs/211200006.pl
@@ -2,7 +2,7 @@ use Modern::Perl;
 
 return {
     bug_number => "29557",
-    description => "Add auto_account_expired to AUTO_RENEWALS notice. Please update your AUTO_RENEWALS notice manually if you have changed or translated it.",
+    description => "Add auto_account_expired to AUTO_RENEWALS notice",
     up => sub {
         my ($args) = @_;
         my ($dbh, $out) = @$args{qw(dbh out)};
@@ -11,5 +11,6 @@ return {
             SET content=REPLACE(content, "[% ELSIF checkout.auto_renew_error == 'too_unseen' %]\r\nThis item must be renewed at the library.\r\n[% END %]", "[% ELSIF checkout.auto_renew_error == 'too_unseen' %]\r\nThis item must be renewed at the library.\r\n[% ELSIF checkout.auto_renew_error == 'auto_account_expired' %]\r\nYour account has expired.\r\n[% END %]")
             WHERE code="AUTO_RENEWALS"
         });
+        say $out "Please update your AUTO_RENEWALS notice manually if you have changed or translated it"
     },
 }
