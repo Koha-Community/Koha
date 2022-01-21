7be31a6f2abd9ef1412ef63347ce147f41efb838
Owen Leonard <oleonard@myacpl.org>
2022-01-21 13:05:40 +0000

    Bug 29932: Use localStorage instead of cookie for bibs_selected
    This patch replaces the use of the jquery.cookie.js plugin in the
    "Browse selected records" feature. The feature is modified to use
    localStorage instead of cookies.
    
    To test, apply the patch and go to Administration -> System preferences.
    
     - Enable the "BrowseResultSelection" system preference.
     - Perform a catalog search which will return multiple results.
     - On the search results page, check the checkbox for a number of
       titles.
     - Click the "Browse selected biblios" button in the toolbar.
     - You should be directed to the detail page for the first title you
       checked.
     - Clicking the right-arrow in the sidebar should take you
       through each one of your selected titles.
     - When you click the "Results" link in the sidebar you should be
       returned to the same search results set and the same titles should be
       checked.
     _ If you uncheck those titles and reload the page the checkboxes should
       remain unchecked.
     - If you log out of the staff interface the "bibs_selected" item in
       localStorage should be deleted. See, for example,
       https://developer.mozilla.org/en-US/docs/Tools/Storage_Inspector for
       information about viewing local storage.
    
    Signed-off-by: David Nind <david@davidnind.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    (cherry picked from commit 6c8752cac4bd7888c8181271a32b88d2f97a1d80)
    
    Signed-off-by: Andrew Fuerste-Henry <andrew@bywatersolutions.com>

--- koha-tmpl/intranet-tmpl/prog/js/staff-global.js
+++ koha-tmpl/intranet-tmpl/prog/js/staff-global.js
@@ -205,6 +205,7 @@ function logOut(){
     removeLastBorrower();
     localStorage.removeItem("sql_reports_activetab");
     localStorage.removeItem("searches");
+    localStorage.removeItem("bibs_selected");
 }
 
 function openHelp(){
@@ -320,11 +321,11 @@ function delBibToContext( bibnum ) {
 }
 
 function setContextBiblioNumbers( bibnums ) {
-    $.cookie('bibs_selected', JSON.stringify( bibnums ));
+    localStorage.setItem('bibs_selected', JSON.stringify( bibnums ) );
 }
 
 function getContextBiblioNumbers() {
-    var r = $.cookie('bibs_selected');
+    var r = localStorage.getItem('bibs_selected');
     if ( r ) {
         return JSON.parse(r);
     }
