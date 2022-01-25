37fcf837ce221f6bf1bcbf48fb29b09a3fd6a1b9
Kyle Hall <kyle@bywatersolutions.com>
2022-01-25 14:06:41 -0500

    Bug 27424: Update database
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

---
+++ installer/data/mysql/atomicupdate/bug_27424.pl
@@ -0,0 +1,11 @@
+use Modern::Perl;
+
+return {
+    bug_number => "27424",
+    description => "Add column to specify an SMTP server as the default",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+        $dbh->do(q{ALTER TABLE smtp_servers ADD COLUMN `is_default` tinyint(1) NOT NULL DEFAULT 0 AFTER debug});
+    },
+};
