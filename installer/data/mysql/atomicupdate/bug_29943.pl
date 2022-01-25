4406fa359bef7778cbd4a6ec32acab37024d66ad
Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
2022-01-25 15:35:05 +0000

    Bug 29943: Db rev: replace typo in existing notices
    Test plan:
    Run updatedatabase.
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    (cherry picked from commit b5a8b8925854be3efe3135d2ca94f1ced42edd79)
    
    Signed-off-by: Andrew Fuerste-Henry <andrew@bywatersolutions.com>

---
+++ installer/data/mysql/atomicupdate/bug_29943.pl
@@ -0,0 +1,16 @@
+use Modern::Perl;
+
+return {
+    bug_number => 29943,
+    description => "Fix typo in NOTIFY_MANAGER notice",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+
+        # Correct "[% borrowers." (where present and restricted to specific notice)
+        $dbh->do(q|
+UPDATE letter SET content = REPLACE(content, '[% borrowers.', '[% borrower.')
+WHERE code = 'NOTIFY_MANAGER'
+        |);
+    },
+};
