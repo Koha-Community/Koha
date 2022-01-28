ddfc5304b3410098266fe27fb6c39b616840e91a
Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
2022-01-28 09:59:15 +0000

    Bug 29966: Dont pass authnotrequired true and flags together
    Test plan:
    Logout in OPAC.
    Goto sco help page.
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

--- opac/sco/help.pl
+++ opac/sco/help.pl
@@ -39,7 +39,6 @@ my ( $template, $borrowernumber, $cookie ) = get_template_and_user(
         query           => $query,
         type            => "opac",
         authnotrequired => 1,
-        flagsrequired   => { self_check => "self_checkout_module" },
     }
 );
 
