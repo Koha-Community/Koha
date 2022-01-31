f6138634f5b53b4121a7731cc518fd4313ceceaa
Aleisha Amohia <aleishaamohia@hotmail.com>
2022-02-01 00:11:11 +1300

    Bug 29990: Add ShowHeadingUse system preference
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

---
+++ installer/data/mysql/atomicupdate/bug_29990_-_add_ShowHeadingUse_syspref.pl
@@ -0,0 +1,12 @@
+use Modern::Perl;
+
+return {
+    bug_number => "29990",
+    description => "Add new system preference ShowHeadingUse",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+
+        $dbh->do(q{INSERT IGNORE INTO systempreferences (variable,value,options,explanation,type) VALUES ('ShowHeadingUse', '0', NULL, 'Show whether authority record contains an established heading that conforms to descriptive cataloguing rules, and can therefore be used as a main/added entry, or subject, or series title', 'YesNo') });
+    },
+};
