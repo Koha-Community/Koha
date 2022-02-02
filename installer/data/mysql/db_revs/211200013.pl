6935a30cfce7044fd6246dbd9d6ca5d6380772c7
Fridolin Somers <fridolin.somers@biblibre.com>
2022-02-01 21:04:59 -1000

    Bug 29605: DBRev 21.12.00.013
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- installer/data/mysql/db_revs/211200013.pl
+++ installer/data/mysql/db_revs/211200013.pl
@@ -12,7 +12,7 @@ return {
 
             $dbh->do(q{
                 ALTER TABLE language_script_mapping
-                    DROP KEY `language_subtag`;
+                DROP KEY `language_subtag`;
             });
         }
 
@@ -20,7 +20,7 @@ return {
 
             $dbh->do(q{
                 ALTER TABLE language_script_mapping
-                    ADD PRIMARY KEY `language_subtag` (`language_subtag`);
+                ADD PRIMARY KEY `language_subtag` (`language_subtag`);
             });
 
             say $out "Added missing primary key on language_script_mapping"
@@ -29,61 +29,56 @@ return {
         unless ( foreign_key_exists('tmp_holdsqueue', 'tmp_holdsqueue_ibfk_3') ) {
             $dbh->do(q{
                 ALTER TABLE tmp_holdsqueue
-                ADD CONSTRAINT `tmp_holdsqueue_ibfk_3` FOREIGN KEY (`borrowernumber`) REFERENCES `borrowers` (`borrowernumber`) ON DELETE CASCADE ON UPDATE CASCADE
+                ADD CONSTRAINT `tmp_holdsqueue_ibfk_3` FOREIGN KEY (`borrowernumber`)
+                REFERENCES `borrowers` (`borrowernumber`) ON DELETE CASCADE ON UPDATE CASCADE
             });
 
             say $out "Added missing foreign key on tmp_holdsqueue";
         }
 
-        $dbh->do(
-            q{
+        $dbh->do(q{
                 ALTER TABLE `account_offsets`
                 MODIFY COLUMN `type` enum( 'CREATE', 'APPLY', 'VOID', 'OVERDUE_INCREASE', 'OVERDUE_DECREASE' ) NOT NULL
-            }
-        );
+        });
         say $out "Ensure NOT NULL on account_offsets.type";
 
-        $dbh->do(
-            q{
+        $dbh->do(q{
                 ALTER TABLE `additional_contents`
                 MODIFY COLUMN `code` VARCHAR(100) NOT NULL
-            }
-        );
+        });
         say $out "Ensure additional_contents.code is VARCHAR(100)";
 
-        $dbh->do(
-            q{
+        $dbh->do(q{
                 ALTER TABLE `additional_contents`
                 MODIFY COLUMN `lang` VARCHAR(50) NOT NULL DEFAULT ''
-            }
-        );
+        });
         say $out "Ensure additional_contents.lang is VARCHAR(50)";
 
-        $dbh->do(q{ALTER TABLE search_marc_map MODIFY `marc_type` enum('marc21','unimarc') NOT NULL COMMENT 'what MARC type this map is for'});
+        $dbh->do(q{
+            ALTER TABLE search_marc_map MODIFY `marc_type` enum('marc21','unimarc') NOT NULL COMMENT 'what MARC type this map is for'
+        });
         say $out "Ensure NOT NULL on search_marc_map.marc_type";
 
-        $dbh->do(
-            q{
-                alter table
-                    `branchtransfers`
-                modify column
-                    `cancellation_reason` enum(
-                        'Manual',
-                        'StockrotationAdvance',
-                        'StockrotationRepatriation',
-                        'ReturnToHome',
-                        'ReturnToHolding',
-                        'RotatingCollection',
-                        'Reserve',
-                        'LostReserve',
-                        'CancelReserve',
-                        'ItemLost',
-                        'WrongTransfer'
-                    ) DEFAULT NULL
-                after `reason`
-              }
-        );
+        $dbh->do(q{
+            alter table
+                `branchtransfers`
+            modify column
+                `cancellation_reason` enum(
+                    'Manual',
+                    'StockrotationAdvance',
+                    'StockrotationRepatriation',
+                    'ReturnToHome',
+                    'ReturnToHolding',
+                    'RotatingCollection',
+                    'Reserve',
+                    'LostReserve',
+                    'CancelReserve',
+                    'ItemLost',
+                    'WrongTransfer'
+                ) DEFAULT NULL
+            after `reason`
+        });
         say $out "Ensure branchtransfers.cancellation_reason enum values are uppercase";
     },
 
-}
+};
