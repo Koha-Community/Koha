4bd5d0f0bf5e7ace0636967ec65a05ecbc13316f
Fridolin Somers <fridolin.somers@biblibre.com>
2022-02-17 20:37:13 -1000

    Bug 30128: Change language_subtag_registry.description to varchar(255)
    In database, language_descriptions.description is VARCHAR(255).
    language_subtag_registry.description is VARCH(25), it should have same size.
    
    You can see in https://www.loc.gov/standards/iso639-2/php/code_list.php there is for example 'South American Indian languages'
    
    Test plan :
    1) Run atomic update
    2) Check language_subtag_registry.description is now varchar(255)
    
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

---
+++ installer/data/mysql/atomicupdate/bug_30128.pl
@@ -0,0 +1,14 @@
+use Modern::Perl;
+
+return {
+    bug_number => "30128",
+    description => "Change language_subtag_registry.description to varchar(255)",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+        # Do you stuffs here
+        $dbh->do(q{
+            ALTER TABLE `language_subtag_registry` MODIFY COLUMN `description` VARCHAR(255) DEFAULT NULL
+        });
+    },
+};
