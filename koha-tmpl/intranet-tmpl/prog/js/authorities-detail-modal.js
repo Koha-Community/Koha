e155dd70a6f5593679ef38145e6bad18a8690500
Nick Clemens <nick@bywatersolutions.com>
2022-02-18 17:15:07 +0000

    Bug 29965: (follow-up) Move repeated JS to its own file
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

---
+++ koha-tmpl/intranet-tmpl/prog/js/authorities-detail-modal.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $(".authorizedheading a").on("click", function(e){
+        e.preventDefault();
+        var authid = $(this).data("authid");
+
+        $.get("/cgi-bin/koha/authorities/detail.pl", { authid : authid }, function( data ){
+            var auth_detail = $(data).find("#authoritiestabs");
+            auth_detail.find("ul").remove();
+            $("#authorityDetail .modal-title").html(_("Authority") + " " + authid );
+            $("#authorityDetail .modal-body").html( auth_detail );
+        });
+
+        $("#authorityDetail").modal("show");
+    });
+    $("#authorityDetail").on("hidden.bs.modal", function(){
+        $("#authorityDetail .modal-body, #authorityDetail .modal-title").html("");
+        $("#authorityDetail .modal-body").html("<div id=\"loading\"><img src=\"[% interface | html %]/[% theme | html %]/img/spinner-small.gif\" alt=\"\" /></div>");
+    });
+});
