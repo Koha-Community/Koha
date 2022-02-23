8c33eac0a8c1d19dee1547c628f608c7d459b9af
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2022-02-23 12:32:09 +0100

    Bug 32030: ERM - Home view
    Signed-off-by: Jonathan Field <jonathan.field@ptfs-europe.com>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

---
+++ erm/erm-home.pl
@@ -0,0 +1,38 @@
+#! /usr/bin/perl
+
+# Copyright 2022 Koha Development Team
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
+use CGI qw ( -utf8 );
+use C4::Context;
+use C4::Auth qw( get_template_and_user );
+use C4::Output qw( output_html_with_http_headers );
+
+use Koha::Cities;
+
+my $input = CGI->new;
+
+my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
+    {   template_name   => "erm/erm-home.tt",
+        query           => $input,
+        type            => "intranet",
+        flagsrequired   => { erm => '*' },
+    }
+);
+
+output_html_with_http_headers $input, $cookie, $template->output;
