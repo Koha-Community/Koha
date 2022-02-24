9cef83726c839c1b508037b5ca7c8dd4bbaf3894
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2022-02-24 16:52:22 +0100

    Bug 32030: ERM - Koha::ERM::Agreement[s] objects
    Signed-off-by: Jonathan Field <jonathan.field@ptfs-europe.com>
    
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

---
+++ Koha/ERM/Agreement.pm
@@ -0,0 +1,44 @@
+package Koha::ERM::Agreement;
+
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+use Koha::Database;
+
+use base qw(Koha::Object);
+
+=head1 NAME
+
+Koha::ERM::Agreement - Koha ErmAgreement Object class
+
+=head1 API
+
+=head2 Class Methods
+
+=cut
+
+=head2 Internal methods
+
+=head3 _type
+
+=cut
+
+sub _type {
+    return 'ErmAgreement';
+}
+
+1;
