1c006d0e55cf9a78aacc6e7848d9e12bc8e277c1
Kyle Hall <kyle@bywatersolutions.com>
2022-02-28 08:30:07 -0500

    Bug 27812: Add DBRev to warn of the change to the account details notice
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

---
+++ installer/data/mysql/atomicupdate/bug_27812.pl
@@ -0,0 +1,10 @@
+use Modern::Perl;
+
+return {
+    bug_number => "27812",
+    description => "Remove the ability to transmit a patron's plain text password over email",
+    up => sub {
+        my ($args) = @_;
+        say $out "The notice template ACCTDETAILS no longer has access to the patron's plain text password. Please update the language of your ACCTDETAILS as needed.";
+    },
+};
