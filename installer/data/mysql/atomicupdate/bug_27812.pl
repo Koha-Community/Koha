0b3febb279ed2ed58422b41531dc41f82393c66c
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2022-03-01 16:00:10 +0000

    Bug 27812: (QA follow-up) Fix atomicupdate
    We removed a bit too much from the skeliton here ;)
    
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- installer/data/mysql/atomicupdate/bug_27812.pl
+++ installer/data/mysql/atomicupdate/bug_27812.pl
@@ -5,6 +5,7 @@ return {
     description => "Remove the ability to transmit a patron's plain text password over email",
     up => sub {
         my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
         say $out "The notice template ACCTDETAILS no longer has access to the patron's plain text password. Please update the language of your ACCTDETAILS as needed.";
     },
 };
