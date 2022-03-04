94423e6d631bed3292fd05a1ad8eaf7f011b9285
jeremy breuillard <jeremy.breuillard@biblibre.com>
2022-03-04 14:16:33 +0100

    Bug 30506: Value builder for field 182
    This patch provides values builders for the subfields $a, $c and $2. Those are unimarc official codes who help to
    identify the kind of items
    Test plan:
    1)Go to Home > Cataloging > New record
    2)Section 1 - field 182 : it contains subfields $a, $c & $2
    3)Inputs are writable but notice that no plugins are suggested
    4)Apply patch and repeat 1) to 3)
    5)Values builders plugins are available for $a, $c and $2
    
    Signed-off-by: David Nind <david@davidnind.com>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

---
+++ cataloguing/value_builder/unimarc_field_182a.pl
@@ -0,0 +1,71 @@
+#!/usr/bin/perl
+
+# Copyright 2014 Rijksmuseum
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+use Date::Calc;
+
+use Koha::Util::FrameworkPlugin qw(wrapper);
+use C4::Auth qw( get_template_and_user );
+use CGI qw ( -utf8 );
+use C4::Context;
+use C4::Output qw( output_html_with_http_headers );
+
+my $builder= sub {
+    my $params = shift;
+    my $id = $params->{id};
+
+    return qq|
+<script>
+
+function Click$id(event) {
+    var fieldvalue=\$('#'+event.data.id).val();
+    window.open(\"../cataloguing/plugin_launcher.pl?plugin_name=unimarc_field_182a.pl&index=\"+event.data.id+\"&result=\"+fieldvalue,\"tag_editor\",'width=700,height=700,toolbar=false,scrollbars=yes');
+    return false; /* prevents scrolling */
+}
+</script>|;
+};
+
+my $launcher= sub {
+    my $params = shift;
+    my $cgi = $params->{cgi};
+    my ( $template, $loggedinuser, $cookie ) = get_template_and_user({
+        template_name => "cataloguing/value_builder/unimarc_field_182a.tt",
+        query => $cgi,
+        type => "intranet",
+        flagsrequired => {editcatalogue => '*'},
+    });
+    my $results = scalar $cgi->param('result');
+
+    $template->param(
+        index => scalar $cgi->param('index'),
+        result => $results,
+    );
+
+    # Return the result of the position in the string, ex: abcde = 1=a, 2=b, 3=c...
+    my @x = split(//, $results);
+    my $i = 1;
+    for my $fresult (@x) {
+        $template->param("f$i" => $fresult);
+        ++$i;
+    }
+    output_html_with_http_headers $cgi, $cookie, $template->output;
+};
+
+return { builder => $builder, launcher => $launcher };
