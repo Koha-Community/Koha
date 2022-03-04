b5bcdaf2cb52902067e91590a3fce5a7d65bf5cb
Andrew Fuerste-Henry <andrew@bywatersolutions.com>
2022-03-04 15:28:35 +0000

    Bug 29596: DBRev 20.11.16.001
    Signed-off-by: Andrew Fuerste-Henry <andrew@bywatersolutions.com>
    (cherry picked from commit e6faadbc8647cbe667a38c47d265c6b6d301880d)
    Signed-off-by: Victor Grousset/tuxayo <victor@tuxayo.net>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -23778,6 +23778,50 @@ if ( CheckVersion( $DBversion ) ) {
     NewVersion( $DBversion, "", "Koha 20.11.16 release" );
 }
 
+$DBversion = '20.11.16.001';
+if( CheckVersion( $DBversion ) ) {
+    $dbh->do(q{
+            INSERT IGNORE INTO language_subtag_registry( subtag, type, description, added)
+            VALUES ( 'yi', 'language', 'Yiddish', NOW() );
+        });
+
+        $dbh->do(q{
+            INSERT IGNORE INTO language_rfc4646_to_iso639(rfc4646_subtag,iso639_2_code)
+            VALUES ( 'yi','yid');
+        });
+
+        $dbh->do(q{
+            INSERT IGNORE INTO language_descriptions(subtag, type, lang, description)
+            VALUES ( 'yi', 'language', 'de', 'Jiddisch');
+        });
+
+        $dbh->do(q{
+            INSERT IGNORE INTO language_descriptions(subtag, type, lang, description)
+            VALUES ( 'yi', 'language', 'en', 'Yiddish');
+        });
+
+        $dbh->do(q{
+            INSERT IGNORE INTO language_descriptions(subtag, type, lang, description)
+            VALUES ( 'yi', 'language', 'es', 'Yidis');
+        });
+
+        $dbh->do(q{
+            INSERT IGNORE INTO language_descriptions(subtag, type, lang, description)
+            VALUES ( 'yi', 'language', 'fr', 'Yiddish');
+        });
+
+        $dbh->do(q{
+            INSERT IGNORE INTO language_descriptions(subtag, type, lang, description)
+            VALUES ( 'yi', 'language', 'yi', 'יידיש');
+        });
+
+        $dbh->do(q{
+            INSERT IGNORE INTO language_script_mapping(language_subtag,script_subtag)
+            VALUES ( 'yi', 'Hebr');
+        });
+    NewVersion( $DBversion, "29596", "Add Yiddish language" );
+}
+
 
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
