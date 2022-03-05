0a6c8afd77b9d3204d949d3d2f0c95ede13f5990
Mason James <mtj@kohaaloha.com>
2022-03-05 15:30:21 +1300

    Bug 19169: Add a test to detect unneeded 'atomicupdate' files
    to test...
    
    1/ set git repo
        $ git reset --hard v21.11.03
    
    2/ run test
        $ prove ./t
        OK
    
    3/ apply patch
    
    4/ run test again, observe FAIL
    
        $ prove ./t/00-check-atomic-updates.pl
        ./t/00-check-atomic-updates.pl .. 1/?
        #   Failed test 'check for unhandled atomic updates: bug_29596.pl'
        #   at ./t/00-check-atomic-updates.pl line 34.
        #                   'bug_29596.pl'
        #           matches '(?^u:.*pl$)'
        # Looks like you failed 1 test of 3.
        ./t/00-check-atomic-updates.pl .. Dubious, test returned 1 (wstat 256, 0x100)
        Failed 1/3 subtests
    
    JD Amended patch: fix copyright year
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

---
+++ t/00-check-atomic-updates.pl
@@ -0,0 +1,37 @@
+# Copyright 2022 Mason James
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
+use Test::More;
+use File::Find;
+
+my $dir = ('installer/data/mysql/atomicupdate');
+my @files;
+
+find( \&wanted, $dir );
+
+sub wanted {
+    push @files, $_;
+    return;
+}
+
+foreach my $f (@files) {
+    next if $f eq 'skeleton.pl';
+    unlike( $f, qr/.*pl$/, "check for unhandled atomic updates: $f" );
+}
+
+done_testing();
