b4764daae3bf9c2505720cb53ad34da2dabfd475
Aleisha Amohia <aleishaamohia@hotmail.com>
2022-03-09 01:13:03 +0000

    Bug 30250: Add ApplyFrameworkDefaults system preference
    (cherry picked from commit 684a04deb6ec7e94ca60f5cb38811176fbb9c230)
    
    Signed-off-by: David Nind <david@davidnind.com>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    (cherry picked from commit d42de2c657b6432c3223291f663eac5fc25b8706)
    
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

---
+++ installer/data/mysql/atomicupdate/bug_30250_-_add_ApplyFrameworkDefaults_syspref.pl
@@ -0,0 +1,12 @@
+use Modern::Perl;
+
+return {
+    bug_number => "30250",
+    description => "Add new system preference ApplyFrameworkDefaults",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+
+        $dbh->do(q{INSERT IGNORE INTO systempreferences (variable,value,options,explanation,type) VALUES ('ApplyFrameworkDefaults', 'new', "new|duplicate|changed", "Configure when to apply framework default values - when cataloguing a new record, or when editing a record as new (duplicating), or when changing framework", 'multiple') });
+    },
+};
