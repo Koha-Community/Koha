7dd67c9c99572e0bd71e6f4461bb1958fdae54f1
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2022-03-11 13:04:24 +0000

    Bug 30275: Generated kohastructure.sql
    This kohastructure update was generated using `koha-dump --schema-only`
    in koha-testing-docker once the atomicupdate had been run against the
    previous atomicupdate patch.
    
    Signed-off-by: Owen Leonard <oleonard@myacpl.org>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- installer/data/mysql/kohastructure.sql
+++ installer/data/mysql/kohastructure.sql
@@ -1696,6 +1696,26 @@ CREATE TABLE `categories_branches` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Table structure for table `checkout_renewals`
+--
+
+DROP TABLE IF EXISTS `checkout_renewals`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `checkout_renewals` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `issue_id` int(11) DEFAULT NULL COMMENT 'the id of the issue this renewal pertains to',
+  `renewer_id` int(11) DEFAULT NULL COMMENT 'the id of the user who processed the renewal',
+  `seen` tinyint(1) DEFAULT 0 COMMENT 'boolean denoting whether the item was present or not',
+  `interface` varchar(16) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'the interface this renewal took place on',
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp() COMMENT 'the date and time the renewal took place',
+  PRIMARY KEY (`id`),
+  KEY `renewer_id` (`renewer_id`),
+  CONSTRAINT `renewals_renewer_id` FOREIGN KEY (`renewer_id`) REFERENCES `borrowers` (`borrowernumber`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `circulation_rules`
 --
 
