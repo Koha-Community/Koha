effefacf302271128b36337ba11c5c416189683e
Fridolin Somers <fridolin.somers@biblibre.com>
2022-03-11 13:06:45 -1000

    Bug 28138: (follow-up) New atomic update
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- installer/data/mysql/atomicupdate/add-system-preference-RequirePaymentType.perl
+++
@@ -1,9 +0,0 @@
-$DBversion = 'XXX';
-if ( CheckVersion($DBversion) ) {
-    $dbh->do(q{
-        INSERT IGNORE INTO systempreferences (`variable`,`value`,`options`,`explanation`,`type`)
-        VALUES ('RequirePaymentType','0','','Require staff to select a payment type when a payment is made','YesNo')
-    });
-
-    NewVersion( $DBversion, '28138', 'Add system preference RequirePaymentType');
-}
---
+++ installer/data/mysql/atomicupdate/bug_28138.pl
@@ -0,0 +1,15 @@
+use Modern::Perl;
+
+return {
+    bug_number => "28138",
+    description => "Add system preference RequirePaymentType",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+        # Do you stuffs here
+        $dbh->do(q{
+            INSERT IGNORE INTO systempreferences (`variable`,`value`,`options`,`explanation`,`type`)
+            VALUES ('RequirePaymentType','0','','Require staff to select a payment type when a payment is made','YesNo')
+        });
+    },
+};
