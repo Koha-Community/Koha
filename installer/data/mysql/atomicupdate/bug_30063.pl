7d2d53793ba7b01991dda669696576cff42ab5fa
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2022-03-30 10:28:34 +0200

    Bug 30063: Sort by "Name" firstname,surname
    As it's how it's displayed
    
    Signed-off-by: Séverine Queune <severine.queune@bulac.fr>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

---
+++ installer/data/mysql/atomicupdate/bug_30063.pl
@@ -0,0 +1,14 @@
+use Modern::Perl;
+
+return {
+    bug_number => "30063",
+    description => "Fix DefaultPatronSearchFields order",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+        $dbh->do(q{
+            UPDATE systempreferences SET value="firstname,surname,othernames,cardnumber,userid"
+            WHERE variable="DefaultPatronSearchFields" AND value="surname,firstname,othernames,cardnumber,userid"
+        });
+    },
+};
