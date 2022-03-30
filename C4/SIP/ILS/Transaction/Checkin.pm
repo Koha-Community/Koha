94f9ab623a0a53d4126c02a81feaf475591fae7b
Kyle M Hall <kyle@bywatersolutions.com>
2022-03-30 16:34:55 +0000

    Bug 30118: (QA follow-up) Fix Perl Critic issues
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    (cherry picked from commit 61468ffe6d201cdb1257923d9fed575ffb5911cb)
    
    Signed-off-by: Andrew Fuerste-Henry <andrew@bywatersolutions.com>

--- C4/SIP/ILS/Transaction/Checkin.pm
+++ C4/SIP/ILS/Transaction/Checkin.pm
@@ -90,7 +90,7 @@ sub do_checkin {
 
     my $reserved;
     my $lookahead = C4::Context->preference('ConfirmFutureHolds'); #number of days to look for future holds
-    my ($resfound, $resrec, undef) = C4::Reserves::CheckReserves( $item->itemnumber, undef, $lookahead ) unless ( $item->withdrawn );
+    my ($resfound) = $item->withdrawn ? q{} : C4::Reserves::CheckReserves( $item->itemnumber, undef, $lookahead );
     if ( $resfound eq "Reserved") {
         $reserved = 1;
     }
