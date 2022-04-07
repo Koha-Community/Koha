b025a7934557298a5b39428241f40329d6fab686
Fridolin Somers <fridolin.somers@biblibre.com>
2022-04-07 10:34:40 +0200

    Bug 11083: DBRev 21.12.00.022
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

---
+++ installer/data/mysql/db_revs/211200022.pl
@@ -0,0 +1,16 @@
+use Modern::Perl;
+
+return {
+    bug_number => "11083",
+    description => "Add syspref AuthorityXSLTResultsDisplay",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+        # Do you stuffs here
+        $dbh->do(q{
+            INSERT IGNORE INTO systempreferences (`variable`, `value`, `options`, `explanation`, `type`) VALUES
+            ('AuthorityXSLTResultsDisplay','','','Enable XSL stylesheet control over authority results page display on intranet','Free')
+        });
+    },
+};
+
