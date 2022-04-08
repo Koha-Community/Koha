18939987f553c1325d86dfb9602c35b8c2e543f2
Fridolin Somers <fridolin.somers@biblibre.com>
2022-04-08 15:10:36 +0200

    Bug 20398: DBRev 21.12.00.024
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- installer/data/mysql/atomicupdate/add-system-preference-StaffHighlightedWords.perl
+++
@@ -1,8 +0,0 @@
-$DBversion = 'XXX';
-if ( CheckVersion($DBversion) ) {
-    $dbh->do(q{
-        INSERT IGNORE INTO systempreferences (`variable`,`value`,`options`,`explanation`,`type`)
-        VALUES ('StaffHighlightedWords','1','','Activate or not highlighting of search terms for staff interface','YesNo ')
-    });
-    NewVersion( $DBversion, '20398', 'Add system preference StaffHighlightedWords');
-}
---
+++ installer/data/mysql/db_revs/211200024.pl
@@ -0,0 +1,15 @@
+use Modern::Perl;
+
+return {
+    bug_number => "20398",
+    description => "Add system preference StaffHighlightedWords",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+        # Do you stuffs here
+        $dbh->do(q{
+            INSERT IGNORE INTO systempreferences (`variable`,`value`,`options`,`explanation`,`type`)
+            VALUES ('StaffHighlightedWords','1','','Activate or not highlighting of search terms for staff interface','YesNo')
+	});
+    },
+};
