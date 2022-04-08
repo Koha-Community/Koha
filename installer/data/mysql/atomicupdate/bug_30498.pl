ccc218b04dee67ca11a7b2a177c76799e397844e
Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
2022-04-08 09:19:31 +0000

    Bug 30498: Fix enum search_field.type
    Resolves sync issue with upgrades.
    
    Test plan:
    Run dbrev.
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Signed-off-by: Victor Grousset/tuxayo <victor@tuxayo.net>
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

---
+++ installer/data/mysql/atomicupdate/bug_30498.pl
@@ -0,0 +1,11 @@
+use Modern::Perl;
+
+return {
+    bug_number => 30498,
+    description => "Correct enum search_field.type",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+        $dbh->do( q|ALTER TABLE search_field MODIFY COLUMN `type` enum('','string','date','number','boolean','sum','isbn','stdno','year') NOT NULL COMMENT 'what type of data this holds, relevant when storing it in the search engine'| );
+    },
+};
