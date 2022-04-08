075cd4b9378facce1d3919fb5ed0c44c29ee4d46
Fridolin Somers <fridolin.somers@biblibre.com>
2022-04-08 15:06:03 +0200

    Bug 20517: DBRev 21.12.00.023
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

---
+++ installer/data/mysql/db_revs/211200023.pl
@@ -0,0 +1,15 @@
+use Modern::Perl;
+
+return {
+    bug_number => "20517",
+    description => "Add SIP2SortBinMapping system preference",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+        # Do you stuffs here
+        $dbh->do(q{
+            INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type` ) VALUES
+            ('SIP2SortBinMapping','',NULL,'Use the following mappings to determine the sort_bin of a returned item. The mapping should be on the form \"branchcode:item field:item field value:sort bin number\", with one mapping per line.','free');
+	});
+    },
+};
