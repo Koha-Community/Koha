14b9274135f32c92c655fc070eb912c4f39b4eca
Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
2022-04-08 08:23:56 +0000

    Bug 30497: Recreate old_reserves_ibfk_4 when it cascades
    Test plan:
    Drop old_reserves_ibfk_4.
    Add back with:
        alter table old_reserves ADD CONSTRAINT `old_reserves_ibfk_4` FOREIGN KEY (`itemtype`) REFERENCES `itemtypes` (`itemtype`) ON DELETE CASCADE ON UPDATE CASCADE;
    Run dbrev. Check that constraint has been replaced by SET NULL.
    Run dbrev again. No changes.
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    [EDIT 23-08-22 Replaced DROP CONSTRAINT]
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

---
+++ installer/data/mysql/atomicupdate/bug_30497.pl
@@ -0,0 +1,16 @@
+use Modern::Perl;
+
+return {
+    bug_number => 30497,
+    description => "Recreate old_reserves_ibfk_4 if cascading",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+
+        my @info = $dbh->selectrow_array( q|SHOW CREATE TABLE old_reserves| );
+        if( $info[1] =~ /^\s*CONSTRAINT .old_reserves_ibfk_4.*CASCADE$/m ) {
+            $dbh->do( q|ALTER TABLE old_reserves DROP FOREIGN KEY old_reserves_ibfk_4| );
+            $dbh->do( q|ALTER TABLE old_reserves ADD FOREIGN KEY old_reserves_ibfk_4 (itemtype) REFERENCES itemtypes (itemtype) ON DELETE SET NULL ON UPDATE SET NULL| );
+        }
+    },
+};
