2aaa0b87cfc59e45660d2ff7e73c8f8d82fbf2e8
Fridolin Somers <fridolin.somers@biblibre.com>
2022-04-12 11:51:22 +0200

    Bug 17018: DBRev 21.12.00.026
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

---
+++ installer/data/mysql/db_revs/211200026.pl
@@ -0,0 +1,13 @@
+use Modern::Perl;
+
+return {
+    bug_number => "17018",
+    description => "Split AdvancedSearchTypes for staff and OPAC",
+    up => sub {
+        my ($args) = @_;
+        my $dbh = $args->{dbh};
+        $dbh->do(q{
+            INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type` ) SELECT 'OpacAdvancedSearchTypes', `value`, `options`, 'Select which set of fields are available as limits on the OPAC advanced search page', `type` FROM systempreferences WHERE variable = 'AdvancedSearchTypes'
+        });
+    },
+};
