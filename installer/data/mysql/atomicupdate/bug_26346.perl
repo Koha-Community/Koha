6d7aa694a92781e7df61d9dd60d0ff14f891a52b
Fridolin Somers <fridolin.somers@biblibre.com>
2022-04-12 12:09:05 +0200

    Bug 26346: DBRev 21.12.00.027
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- installer/data/mysql/atomicupdate/bug_26346.perl
+++
@@ -1,9 +0,0 @@
-$DBversion = 'XXX';
-if ( CheckVersion($DBversion) ) {
-    if ( !column_exists( 'virtualshelves', 'allow_change_from_staff' ) ) {
-        $dbh->do(q{ALTER TABLE virtualshelves ADD COLUMN `allow_change_from_staff` tinyint(1) DEFAULT '0' COMMENT 'can staff change contents?'});
-    }
-
-    $dbh->do(q{ INSERT IGNORE INTO permissions (module_bit, code, description) VALUES (20, 'edit_public_lists', 'Edit public lists') });
-    NewVersion( $DBversion, 26346, "Add allow_change_from_staff to virtualshelves table" );
-}
---
+++ installer/data/mysql/db_revs/211200027.pl
@@ -0,0 +1,18 @@
+use Modern::Perl;
+
+return {
+    bug_number => "26346",
+    description => "Add allow_change_from_staff to virtualshelves table",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+        if ( !column_exists( 'virtualshelves', 'allow_change_from_staff' ) ) {
+            $dbh->do(q{
+                ALTER TABLE virtualshelves ADD COLUMN `allow_change_from_staff` tinyint(1) DEFAULT '0' COMMENT 'can staff change contents?'
+            });
+        }
+        $dbh->do(q{
+            INSERT IGNORE INTO permissions (module_bit, code, description) VALUES (20, 'edit_public_lists', 'Edit public lists')
+        });
+    },
+};
