77cd5d301dc03dde41ceef5161df29e79d25461a
Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
2022-04-15 09:43:26 +0000

    Bug 30226: (QA follow-up) Sync atomicupdate with sysprefs.sql
    Description and remove choices.
    Set items => flag items (as in circulation.pref)
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- installer/data/mysql/atomicupdate/bug_30226.pl
+++ installer/data/mysql/atomicupdate/bug_30226.pl
@@ -8,7 +8,7 @@ return {
         my ($dbh, $out) = @$args{qw(dbh out)};
         # Add system preference AllowSetAutomaticRenewal
         $dbh->do(q{
-            INSERT IGNORE INTO systempreferences (`variable`, `value`, `options`, `explanation`, `type` ) VALUES('AllowSetAutomaticRenewal', '1', '|yes|no', 'Allow or Prevent staff from flagging items for autorenewal on the checkout page', 'YesNo')
+            INSERT IGNORE INTO systempreferences (`variable`, `value`, `options`, `explanation`, `type` ) VALUES('AllowSetAutomaticRenewal', '1', NULL, 'If ON, allows staff to flag items for automatic renewal on the check out page', 'YesNo')
         });
         # Finished adding system preference AllowSetAutomaticRenewal
         say $out "Added system preference AllowSetAutomaticRenewal";
