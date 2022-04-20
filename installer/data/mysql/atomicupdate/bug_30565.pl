bb210393522abd23e0b9821c5bd34ea176279faa
Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
2022-04-20 11:29:58 +0000

    Bug 30565: Database revision for stockrotationrotas
    Add unique key if needed.
    Adjust description column to make it NOT NULL.
    
    Test plan:
    (Optionally:) Drop unique key, make description nullable.
    Run dbrev.
    Check if unique key present, and description not-nullable.
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

---
+++ installer/data/mysql/atomicupdate/bug_30565.pl
@@ -0,0 +1,19 @@
+use Modern::Perl;
+
+return {
+    bug_number => 30565,
+    description => "Update table stockrotationrotas",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+        if( !unique_key_exists('stockrotationrotas', 'stockrotationrotas_title') ) {
+            $dbh->do( q|
+ALTER TABLE stockrotationrotas ADD CONSTRAINT UNIQUE KEY stockrotationrotas_title (title)
+            |);
+        }
+        # Make sure that description is NOT NULL
+        $dbh->do( q|
+ALTER TABLE stockrotationrotas MODIFY COLUMN description text NOT NULL COMMENT 'Description for this rota'
+        |);
+    },
+};
