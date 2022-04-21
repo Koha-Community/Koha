794a6d4530da7df8a73add7a173f7716fc5da7ab
Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
2022-04-21 12:42:53 +0000

    Bug 30572: Dbrev for search_marc_to_field.sort
    Test plan:
    Run updatedatabase.
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

---
+++ installer/data/mysql/atomicupdate/bug_30572.pl
@@ -0,0 +1,14 @@
+use Modern::Perl;
+
+return {
+    bug_number => 30572,
+    description => "Adjust search_marc_to_field.sort",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+        # Do you stuffs here
+        $dbh->do(q|
+ALTER TABLE search_marc_to_field MODIFY COLUMN sort tinyint(1) DEFAULT 1 NOT NULL COMMENT 'Sort defaults to 1 (Yes) and creates sort fields in the index, 0 (no) will prevent this';
+        |);
+    },
+};
