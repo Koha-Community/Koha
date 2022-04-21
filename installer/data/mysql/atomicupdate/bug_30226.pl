45e8a00ee98840f4c88520a025dc2da472ba1587
Fridolin Somers <fridolin.somers@biblibre.com>
2022-04-20 14:56:16 -1000

    Bug 30226: (follow-up) Remove text from database update
    The text displayed by database update is not really helpful.
    And it will be incorrect if update is run several times.
    
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- installer/data/mysql/atomicupdate/bug_30226.pl
+++ installer/data/mysql/atomicupdate/bug_30226.pl
@@ -6,11 +6,8 @@ return {
     up => sub {
         my ($args) = @_;
         my ($dbh, $out) = @$args{qw(dbh out)};
-        # Add system preference AllowSetAutomaticRenewal
         $dbh->do(q{
             INSERT IGNORE INTO systempreferences (`variable`, `value`, `options`, `explanation`, `type` ) VALUES('AllowSetAutomaticRenewal', '1', NULL, 'If ON, allows staff to flag items for automatic renewal on the check out page', 'YesNo')
         });
-        # Finished adding system preference AllowSetAutomaticRenewal
-        say $out "Added system preference AllowSetAutomaticRenewal";
     },
 };
