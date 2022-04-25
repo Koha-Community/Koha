de350e0bf225ebcdf35ec73b1906a2cc4f8cfa24
Katrin Fischer <katrin.fischer.83@web.de>
2022-04-25 10:30:36 +0000

    Bug 30290: (QA follow-up) Sample notices/database update: Change phrasing to Yes/No
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- installer/data/mysql/atomicupdate/bug_30290.pl
+++ installer/data/mysql/atomicupdate/bug_30290.pl
@@ -8,7 +8,7 @@ return {
         my ($dbh, $out) = @$args{qw(dbh out)};
         my $sql = q|
 UPDATE letter
-SET content=REPLACE(content, '\nPages:', '\nTOC: [% IF article_request.toc_request %]Include TOC[% ELSE %]No[% END %]\nPages:')
+SET content=REPLACE(content, '\nPages:', '\nTOC: [% IF article_request.toc_request %]Yes[% ELSE %]No[% END %]\nPages:')
 WHERE code RLIKE '^AR_'  AND module='circulation' AND content NOT RLIKE '\nTOC:';
         |;
         $dbh->do( $sql );
