f637c68cbae6d3c5c8ae720e2cef5296e3c41070
Andrew Fuerste-Henry <andrew@bywatersolutions.com>
2022-04-26 12:39:40 +0000

    Increment version for 21.05.14 release
    Signed-off-by: Andrew Fuerste-Henry <andrew@bywatersolutions.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -24671,6 +24671,11 @@ if( CheckVersion( $DBversion ) ) {
     NewVersion( $DBversion, "", "Koha 21.05.13 release" );
 }
 
+$DBversion = '21.05.14.000';
+if( CheckVersion( $DBversion ) ) {
+    NewVersion( $DBversion, "", "Koha 21.05.14 release" );
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
