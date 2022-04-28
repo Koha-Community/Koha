cdb6f28139b01b98397107f9f919b818f775763e
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2022-04-28 12:04:11 +0200

    Bug 27619: Remove fr-FR installer data
    Signed-off-by: Arthur Suzuki <arthur.suzuki@biblibre.com>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- installer/data/mysql/fr-FR/2-Optionel/z3950_BNF.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `z3950servers`
-(`host`, `port`, `db`, `userid`, `password`, `servername`, `id`, `checked`, `rank`, `syntax`, `servertype`, `encoding`) VALUES
-('z3950.bnf.fr', 2211, 'TOUT-UTF8', 'Z3950', 'Z3950_BNF', 'BNF2', 2, 1, 2, 'UNIMARC', 'zed', 'utf8');
