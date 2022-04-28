38db9932d2df0895b0f542525e539f95c4dc7903
Katrin Fischer <katrin.fischer.83@web.de>
2022-04-28 14:01:52 +0000

    Bug 13142: (QA follow-up) Convert database update to current format
    https://bugs.koha-community.org/show_bug.cgi?id=13412
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

---
+++ installer/data/mysql/atomicupdate/bug_13412-add_GenerateAuthorityField_syspref.pl
@@ -0,0 +1,16 @@
+use Modern::Perl;
+ 
+return {
+    bug_number => "13412",
+    description => "Add GenerateAuthorityField667 and GenerateAuthorityField670 system preferences",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+ 
+        $dbh->do(q{
+            INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type` ) VALUES
+            ( 'GenerateAuthorityField667', 'Machine generated authority record', NULL, 'When BiblioAddsAuthorities and AutoCreateAuthorities are enabled, use this as a default value for the 667$a field of MARC21 records', 'free' ),
+            ( 'GenerateAuthorityField670', 'Work cat.', NULL, 'When BiblioAddsAuthorities and AutoCreateAuthorities are enabled, use this as a default value for the 670$a field of MARC21 records', 'free' )
+         });
+    },
+};
--- installer/data/mysql/atomicupdate/bug_13412-add_GenerateAuthorityField_syspref.sql
+++
@@ -1,3 +0,0 @@
-INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type` ) VALUES
-( 'GenerateAuthorityField667', 'Machine generated authority record', NULL, 'When BiblioAddsAuthorities and AutoCreateAuthorities are enabled, use this as a default value for the 667$a field of MARC21 records', 'free' ),
-( 'GenerateAuthorityField670', 'Work cat.', NULL, 'When BiblioAddsAuthorities and AutoCreateAuthorities are enabled, use this as a default value for the 670$a field of MARC21 records', 'free' )
