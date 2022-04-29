e86c1e67362270e365c537b7d93844c94bf27420
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2022-04-29 12:14:38 +0100

    Bug 30611: Add STAFF_PASSWORD_RESET notice
    This patch adds a new STAFF_PASSWORD_RESET notice template for password
    recoveries initiated via the staff client as aposed for those initiated
    by the end user.
    
    Signed-off-by: Sally <sally.healey@cheshiresharedservices.gov.uk>
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Chmod 755 on atomic update file.
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

---
+++ installer/data/mysql/atomicupdate/bug_30611.pl
@@ -0,0 +1,14 @@
+use Modern::Perl;
+
+return {
+    bug_number => "30611",
+    description => "Add STAFF_PASSWORD_RESET notice",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+        # Add STAFF_PASSWORD_RESET notice
+        $dbh->do(q{
+            INSERT IGNORE INTO letter (module, code, name, is_html, title, content, message_transport_type) VALUES ('members', 'STAFF_PASSWORD_RESET', 'Online password reset', 1, "Koha password reset", "<html>\r\n<p>A librarian has reset the password for the account <strong><<user>></strong>.\r\n</p>\r\n<p>\r\nPlease create your new password using the following link:\r\n<br/><a href=\"<<passwordreseturl>>\"><<passwordreseturl>></a>\r\n</p>\r\n<p>This link will be valid for 5 days from this email's reception, then you must reapply if you do not change your password.</p>\r\n<p>Thank you.</p>\r\n</html>", 'email');
+        });
+    },
+};
