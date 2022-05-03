b0c98b13cc1a3e13a29dc2e3cf968306b0e4610a
Fridolin Somers <fridolin.somers@biblibre.com>
2022-05-03 10:10:39 -1000

    Bug 14242: DBRev 21.12.00.045
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- installer/data/mysql/atomicupdate/bug_14242-autofill_syspref.perl
+++
@@ -1,6 +0,0 @@
-$DBversion = 'XXX';
-if( CheckVersion( $DBversion ) ) {
-    $dbh->do(q{ INSERT IGNORE INTO systempreferences (`variable`, `value`, `options`, `explanation`,`type`) VALUES ('OPACSuggestionAutoFill', '0', NULL, 'Automatically fill OPAC suggestion form with data from Google Books API','YesNo') });
-
-    NewVersion( $DBversion, 14242, "Add OPACSuggestionAutoFill system preference");
-}
---
+++ installer/data/mysql/db_revs/211200045.pl
@@ -0,0 +1,14 @@
+use Modern::Perl;
+
+return {
+    bug_number => "14242",
+    description => "Add OPACSuggestionAutoFill system preference",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+        $dbh->do(q{
+            INSERT IGNORE INTO systempreferences (`variable`, `value`, `options`, `explanation`,`type`)
+            VALUES ('OPACSuggestionAutoFill', '0', NULL, 'Automatically fill OPAC suggestion form with data from Google Books API', 'YesNo')
+        });
+    },
+};
