36e60f52acefe2768f7237ba7ca49b59180f10b9
Fridolin Somers <fridolin.somers@biblibre.com>
2022-05-06 09:49:42 -1000

    Bug 29925: DBRev 21.12.00.051
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- installer/data/mysql/db_revs/211200051.pl
+++ installer/data/mysql/db_revs/211200051.pl
@@ -10,6 +10,5 @@ return {
             INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type` ) VALUES
             ('EnableExpiredPasswordReset', '0', NULL, 'Enable ability for patrons with expired password to reset their password directly', 'YesNo')
         });
-        say $out "Added EnableExpirePasswordReset system preference";
     },
 };
