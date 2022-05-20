d9ebb06539dbbd07c34ca78fbf37add62e21818d
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2022-05-20 11:27:25 +0200

    Bug 32030: ERM - Package - Koha classes
    Signed-off-by: Jonathan Field <jonathan.field@ptfs-europe.com>
    
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

---
+++ Koha/ERM/Packages.pm
@@ -0,0 +1,49 @@
+package Koha::ERM::Packages;
+
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+
+use Koha::Database;
+
+use Koha::ERM::Package;
+
+use base qw(Koha::Objects);
+
+=head1 NAME
+
+Koha::ERM::Packages - Koha ERM Package Object set class
+
+=head1 API
+
+=head2 Class Methods
+
+=cut
+
+=head3 type
+
+=cut
+
+sub _type {
+    return 'ErmPackage';
+}
+
+sub object_class {
+    return 'Koha::ERM::Package';
+}
+
+1;
