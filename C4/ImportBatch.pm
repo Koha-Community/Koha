ef845aa682f69a4fce3e1ad099a2cd60a4299cf8
Nick Clemens <nick@bywatersolutions.com>
2022-05-20 11:26:29 +0000

    Bug 30813: (follow-up) ImportBatch updates
    Missed these in the original commits
    
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    
    Signed-off-by: Joonas Kylmälä <joonas.kylmala@iki.fi>
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- C4/ImportBatch.pm
+++ C4/ImportBatch.pm
@@ -736,7 +736,7 @@ sub BatchCommitItems {
         my ( $MARCfield, $MARCsubfield ) = GetMarcFromKohaField( 'items.onloan' );
         $item_marc->field($MARCfield)->delete_subfield( code => $MARCsubfield );
 
-        my $item = TransformMarcToKoha( $item_marc );
+        my $item = TransformMarcToKoha({ record => $item_marc });
 
         my $duplicate_barcode = exists( $item->{'barcode'} ) && Koha::Items->find({ barcode => $item->{'barcode'} });
         my $duplicate_itemnumber = exists( $item->{'itemnumber'} );
@@ -1685,7 +1685,7 @@ sub _parse_biblio_fields {
     my ($marc_record) = @_;
 
     my $dbh = C4::Context->dbh;
-    my $bibliofields = TransformMarcToKoha($marc_record, '');
+    my $bibliofields = TransformMarcToKoha({ record => $marc_record });
     return ($bibliofields->{'title'}, $bibliofields->{'author'}, $bibliofields->{'isbn'}, $bibliofields->{'issn'});
 
 }
