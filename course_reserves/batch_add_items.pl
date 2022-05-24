4855d41952e3737f92f57a1ad2197472d47681b6
Nick Clemens <nick@bywatersolutions.com>
2022-05-24 18:27:30 +0000

    Bug 30840: Add support for barcode filters to course reserves
    This bug adds support for barcode filters on the inputs in the course reserves module.
    
    To test:
    1 - Set itemBarcodeInputFilter to 'Remove spaces from'
    2 - Find a barcode in your system and add spaces between each characters
        e.g. 3 9 9 9 9 0 0 0 0 0 0 8 1 8
    3 - Past that barcode into the checkin box, success
    4 - Browse to course reserves, ensure you have a course or create on
    5 - Test adding a reserve, batch adding a reserve, and batch removing reserves
    6 - The pasted barcode does not in any
    7 - Apply patch and restart
    8 - Pasted barcode with spaces can now successfuly be added, batch added, and batch removed from courses
    
    Signed-off-by: Jason Robb <jrobb@sekls.org>
    
    Signed-off-by: Joonas Kylmälä <joonas.kylmala@iki.fi>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    (cherry picked from commit b8a18702d291210f9d62ca9daa747ec06f2cc8f6)
    
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>
    (cherry picked from commit 93cd916cc18e9180943cbb76ea3ba6740318a549)
    Signed-off-by: Arthur Suzuki <arthur.suzuki@biblibre.com>

--- course_reserves/batch_add_items.pl
+++ course_reserves/batch_add_items.pl
@@ -24,6 +24,7 @@ use CGI qw( -utf8 );
 use List::MoreUtils qw( uniq );
 
 use C4::Auth qw( get_template_and_user );
+use C4::Circulation qw( barcodedecode );
 use C4::Output qw( output_html_with_http_headers );
 use C4::CourseReserves qw( GetCourse ModCourse ModCourseItem ModCourseReserve );
 
@@ -73,6 +74,7 @@ if ( $course_id && $course ) {
             my @items;
             my @invalid_barcodes;
             for my $b (@barcodes) {
+                $b = barcodedecode($b) if $b;
                 my $item = Koha::Items->find( { barcode => $b } );
 
                 if ($item) {
