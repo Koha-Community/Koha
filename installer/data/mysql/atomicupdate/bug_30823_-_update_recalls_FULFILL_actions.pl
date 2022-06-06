ec40a9c8262fce1f34ffa362fe24204389109f53
Aleisha Amohia <aleishaamohia@hotmail.com>
2022-06-06 23:52:59 +0000

    Bug 30823: Filling recalls uses 'FILL' action in action logs
    This enhancement changes recall fulfillment actions to log with the
    FILL action. It will also update existing recalls FULFILL actions in
    the database to use the FILL action.
    
    To test:
    1) Enable the UseRecalls system preference and set up your
    recalls-related circulation rules. Confirm RecallsLog is enabled.
    2) Check out an item to Patron B.
    3) Log into the OPAC as Patron A and search for the item.
    4) Place a recall on that item.
    5) Go back to the staff client and check the item in. Confirm the recall
    as waiting for Patron A.
    6) Check out the item for Patron A to fill the recall.
    7) Go to Tools -> Log Viewer. Confirm there is a FULFILL action. Choose
    the following search params to browse system logs:
    - modules: recalls
    - actions: fill
    8) Submit the search and confirm the recall DOES NOT show.
    9) Apply the patch, update database, restart services.
    10) Refresh the log viewer and repeat step 7. Submit the search and
    confirm the recall DOES show. Confirm there is no longer a FULFILL
    action as both holds and recalls will use FILL.
    11) Check in the item.
    12) Repeat steps 2-6. We are ensuring that future recalls are logged
    using the FILL action.
    13) Repeat step 7. Confirm all test recalls are now showing in search
    results.
    
    Sponsored-by: Catalyst IT
    
    Signed-off-by: David Nind <david@davidnind.com>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    (cherry picked from commit 7b8939b2ec823afb4b008dbba81276e259037ce5)
    
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

---
+++ installer/data/mysql/atomicupdate/bug_30823_-_update_recalls_FULFILL_actions.pl
@@ -0,0 +1,12 @@
+use Modern::Perl;
+
+return {
+    bug_number => "30823",
+    description => "Replace recalls FULFILL actions with FILL in action logs",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+
+        $dbh->do(q{ UPDATE action_logs SET action = 'FILL' WHERE action = 'FULFILL' AND module = 'RECALLS' });
+    },
+};
