1ae7b9055ac9b1e3252f9e75813b7a8536382d64
Tomas Cohen Arazi <tomascohen@theke.io>
2022-06-08 17:22:51 -0300

    Bug 30500: Add OPACInTransitHoldPickupLocationChange syspref
    Sponsored-by: Montgomery County Public Libraries
    
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

---
+++ installer/data/mysql/atomicupdate/bug_30500.pl
@@ -0,0 +1,18 @@
+use Modern::Perl;
+
+return {
+    bug_number  => "30500",
+    description => "Add option to allow user to change the pickup location while a hold is in transit",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+
+        $dbh->do(q{
+            INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type` )
+            VALUES
+            ('OPACInTransitHoldPickupLocationChange','0',NULL,'Allow user to change the pickup location while a hold is in transit','YesNo')
+        });
+
+        say $out "Added new system preference 'OPACInTransitHoldPickupLocationChange'";
+    },
+  }
