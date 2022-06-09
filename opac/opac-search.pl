55fc22117fdb6f22e934327bab85dc2981009d5c
Kevin Carnes <kevin.carnes@ub.lu.se>
2022-06-09 08:12:56 +0200

    Bug 30844: Ensure that allow_nonref is enabled when using older versions of JSON
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    (cherry picked from commit cc40bbf1952f67b2e46bae0cf71b8d4fbe0bea3d)
    
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

--- opac/opac-search.pl
+++ opac/opac-search.pl
@@ -561,7 +561,8 @@ if ($tag) {
     #        displays search results which should be hidden.
     # FIXME: No facets for tags search.
 } else {
-    $pasarParams .= '&amp;query=' . uri_escape_utf8(encode_json($query));
+    my $json = JSON->new->utf8->allow_nonref(1);
+    $pasarParams .= '&amp;query=' . uri_escape_utf8($json->encode($query));
     $pasarParams .= '&amp;count=' . uri_escape_utf8($results_per_page);
     $pasarParams .= '&amp;simple_query=' . uri_escape_utf8($simple_query);
     $pasarParams .= '&amp;query_type=' . uri_escape_utf8($query_type) if ($query_type);
