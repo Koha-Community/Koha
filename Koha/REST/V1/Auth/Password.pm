26061075deee2e7e08ba307515956ec8794cfafa
David Cook <dcook@prosentient.com.au>
2022-06-17 04:43:44 +0000

    Bug 30962: REST API: Add endpoint /auth/password/validation
    This patch adds an endpoint for /auth/password/validation
    
    This allows a third-party, using an authenticated and authorized Koha
    API user, to check if the username and password given by a user is
    correct in Koha.
    
    For example, a Keycloak extension can be created using its
    User Storage SPI to use Koha as the user database for Keycloak. This
    API allows us to authenticate the user as a particular Koha user - without
    creating a Koha user session for them.
    
    Test plan:
    0. Apply patch and koha-plack --restart kohadev
    1. Go to http://localhost:8081/cgi-bin/koha/admin/preferences.pl?op=search&searchfield=RESTBasicAuth
    2. Enable "RESTBasicAuth"
    3. Run the following commands while substituting correct values for <koha_user> and <koha_password>
    3. curl -XPOST -H "Content-Type: application/json" -u <koha_user>:<koha_password> http://localhost:8081/api/v1/auth/password/validation -d '{ "username": "<koha_username">, "password": "<koha_password>" }' -v
    4. Note "204 No Content" response
    5. curl -XPOST -H "Content-Type: application/json" -u <koha_user>:<koha_password> http://localhost:8081/api/v1/auth/password/validation -d '{ "username": "<koha_username">, "password": "this is definitely not the password" }' -v
    6. Note "400 Bad Request" response and error message {"error":"Validation failed"}
    
    Signed-off-by: David Nind <david@davidnind.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    (cherry picked from commit a8a356404c197e45eedc6c01847cf1811a1cc037)
    Signed-off-by: Jacob O'Mara <jacob.omara@ptfs-europe.com>

---
+++ Koha/REST/V1/Auth/Password.pm
@@ -0,0 +1,76 @@
+package Koha::REST::V1::Auth::Password;
+
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+use Mojo::Base 'Mojolicious::Controller';
+
+use C4::Auth qw/checkpw/;
+use Koha::Patrons;
+
+=head1 NAME
+
+Koha::REST::V1::Auth::Password - Controller library for handling
+validation of username and password.
+
+Intended use case is authenticating Koha patrons in external
+applications via Koha's REST API.
+
+=head2 Operations
+
+=head3 validate
+
+Controller method that checks a patron's password
+
+=cut
+
+sub validate {
+    my $c = shift->openapi->valid_input or return;
+    my $body   = $c->validation->param('body');
+    my $username = $body->{username} // '';
+    my $patron = Koha::Patrons->find({ userid => $username });
+
+    unless ($patron) {
+        return $c->render( status => 400, openapi => { error => "Validation failed" } );
+    }
+
+    my $password   = $body->{password}   // "";
+
+    return try {
+        my ($status, $cardnumber, $userid) = C4::Auth::checkpw($patron->userid, $password );
+        unless ( $status ) {
+            return $c->render(
+                status  => 400,
+                openapi => { error => "Validation failed" }
+            );
+        }
+
+        return $c->render( status => 204, openapi => '' );
+    }
+    catch {
+        if ( blessed $_ and $_->isa('Koha::Exceptions::Password') ) {
+            return $c->render(
+                status  => 400,
+                openapi => { error => "$_" }
+            );
+        }
+
+        $c->unhandled_exception($_);
+    };
+}
+
+1;
