18fd0c89cf7f288ad5ff4527484d9e989ed3e5f7
Kyle Hall <kyle@bywatersolutions.com>
2022-06-17 06:59:11 -0400

    Bug 30984: Action logs should log the cronjob script name that generated the given log
    When something is changed by a cronjob, and that entity is logged via action logs, we can know what changed, and that it was via a cronjob, but we cannot necessarily know which cronjob made that change. The closest we can come is to find the action logs for the cronjob module which ran before the change which is by no means reliable assuming the CronLog is even enabled.
    
    We should add a new column to action logs to store the name of the script ran for any action logs where the interface is "cron".
    
    Test plan:
    1) Apply this patch
    2) Run updatedatabase.pl
    3) Enable all the Log/Logging sysprefs
    4) Run some cronjobs that will generate action logs
    5) Note the new action_logs column "script" contains the filename of the
       cronjob that caused the change.
    
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

---
+++ installer/data/mysql/atomicupdate/bug_30984.pl
@@ -0,0 +1,17 @@
+use Modern::Perl;
+
+return {
+    bug_number => "30984",
+    description => "Log the cron script that generated an action log if there is one",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+        if( !column_exists( 'action_logs', 'script' ) ) {
+            $dbh->do(q{
+                ALTER TABLE action_logs
+                ADD COLUMN script mediumtext COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'the name of the cron script that caused this change'
+                AFTER interface
+            });
+        }
+    },
+};
