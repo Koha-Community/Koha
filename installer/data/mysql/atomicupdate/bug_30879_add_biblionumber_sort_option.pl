905fef6ca2b5844e713864c90ad1ad1ecf3498be
Nick Clemens <nick@bywatersolutions.com>
2022-06-22 14:55:03 +0000

    Bug 30879: Add option to syspref and update display
    To test:
    1 - Update ComponentSortField system preference and verify biblionumber is an option
    2 - Repeat test plan from 30327 and confirm sorting with biblionumber works
    
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    (cherry picked from commit 505e12970d81fe30436a6a86189c17c9c242a729)
    
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

---
+++ installer/data/mysql/atomicupdate/bug_30879_add_biblionumber_sort_option.pl
@@ -0,0 +1,15 @@
+use Modern::Perl;
+
+return {
+    bug_number => "30327",
+    description => "Add biblionumber to ComponentSortField",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+        $dbh->do(q{
+            UPDATE systempreferences set options="call_number|pubdate|acqdate|title|author|biblionumber"
+            WHERE variable = 'ComponentSortField'
+        });
+        say $out "Added biblionumber option to ComponentSortField";
+    },
+};
