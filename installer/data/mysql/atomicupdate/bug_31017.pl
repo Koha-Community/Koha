f0010cb39d400cad6942832f4ccaff1151c98446
Nick Clemens <nick@bywatersolutions.com>
2022-06-23 10:57:31 +0000

    Bug 31017: DB Updates
    Signed-off-by: KIT Library Germany <michaela.sieber@kit.edu>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

---
+++ installer/data/mysql/atomicupdate/bug_31017.pl
@@ -0,0 +1,20 @@
+use Modern::Perl;
+
+return {
+    bug_number => "31017",
+    description => "Add type option to vendors",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+        unless( column_exists('aqbooksellers','vendor_type') ){
+            $dbh->do(q{
+                ALTER TABLE aqbooksellers ADD COLUMN vendor_type varchar(255) DEFAULT NULL AFTER accountnumber
+            });
+            say $out "Added vendor_type column";
+        }
+        $dbh->do(q{
+            INSERT IGNORE INTO authorised_value_categories( category_name, is_system ) VALUES ('VENDOR_TYPE', 1);
+        });
+        say $out "Added VENDOR_TYPE authorised value category";
+    },
+};
