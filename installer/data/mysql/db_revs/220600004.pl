d1fbd8b3c63b136b1a98edd9babc7d9aa0287c1c
Tomas Cohen Arazi <tomascohen@theke.io>
2022-06-23 12:00:36 -0300

    Bug 12446: DBRev 22.06.00.004
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

---
+++ installer/data/mysql/db_revs/220600004.pl
@@ -0,0 +1,23 @@
+use Modern::Perl;
+
+return {
+    bug_number  => "12446",
+    description => "Ability to allow guarantor relationship for all patron category types",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh) = @$args{qw(dbh)};
+
+        unless ( column_exists( 'categories', 'canbeguarantee' ) ) {
+            $dbh->do(q{
+                ALTER TABLE categories
+                    ADD COLUMN `canbeguarantee` tinyint(1) NOT NULL default '0' AFTER `checkprevcheckout`
+            });
+        }
+
+        $dbh->do(q{
+            UPDATE categories
+            SET canbeguarantee = 1
+            WHERE category_type = 'P' OR category_type = 'C'
+        });
+    },
+};
