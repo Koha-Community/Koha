efc9f9e8823ba89e9e8b6276efd8f888b9a95cd3
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2022-06-24 14:30:53 +0100

    Bug 27779: (QA follow-up) Fix database update
    Don't overwrite peoples translations if they've already changed the
    description.
    
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- installer/data/mysql/atomicupdate/bug_27779.pl
+++ installer/data/mysql/atomicupdate/bug_27779.pl
@@ -9,7 +9,7 @@ return {
 
         $dbh->do(q{
             UPDATE account_credit_types
-            SET description = 'Refund' WHERE code = 'REFUND'
+            SET description = 'Refund' WHERE code = 'REFUND' AND description = 'A refund applied to a patrons fine';
         });
     },
 }
