8757023b2167bfec92a494873232f1aaa76ab6be
Nick Clemens <nick@bywatersolutions.com>
2022-06-25 11:18:42 +0000

    Bug 30327: (follow-up) Fix inconsistencies in syspref names and supply defaults
    This patch fetches the new sysprefs into variables, providing default title ascending if
    they are not set to avoid an undefined concatenation warning
    
    I also make the update idempotent and fix confusion of plural/singular names
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- installer/data/mysql/atomicupdate/bug_30327_add_sortComponents_syspref.pl
+++ installer/data/mysql/atomicupdate/bug_30327_add_sortComponents_syspref.pl
@@ -2,15 +2,15 @@ use Modern::Perl;
 
 return {
     bug_number => "30327",
-    description => "Add ComponentsSortField and ComponentsSortOrder sysprefs",
+    description => "Add ComponentSortField and ComponentSortOrder sysprefs",
     up => sub {
         my ($args) = @_;
         my ($dbh, $out) = @$args{qw(dbh out)};
         $dbh->do(q{
-            INSERT INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type` ) VALUES
+            INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type` ) VALUES
             ('ComponentSortField','title','call_number|pubdate|acqdate|title|author','Specify the default field used for sorting','Choice'),
             ('ComponentSortOrder','asc','asc|dsc|az|za','Specify the default sort order','Choice')
         });
-        say $out "Added ComponentsSortField and ComponentsSortOrder sysprefs";
+        say $out "Added ComponentSortField and ComponentSortOrder sysprefs";
     },
 };
