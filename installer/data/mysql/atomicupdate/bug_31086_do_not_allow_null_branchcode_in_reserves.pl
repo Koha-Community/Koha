1565efe8f3e4c782e4d94a6c06cc71d2ac460d43
Nick Clemens <nick@bywatersolutions.com>
2022-07-01 14:43:09 +0000

    Bug 31086: DB update
    This update sets branchcode as NOT NULL
    default set to 0 as borrowernumber is
    
    Signed-off-by: David Nind <david@davidnind.com>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

---
+++ installer/data/mysql/atomicupdate/bug_31086_do_not_allow_null_branchcode_in_reserves.pl
@@ -0,0 +1,16 @@
+use Modern::Perl;
+
+return {
+    bug_number => "31086",
+    description => "Do not allow null values in branchcodes for reserves",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+        $dbh->do(q{
+            ALTER TABLE reserves
+            MODIFY COLUMN `branchcode` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 0 COMMENT 'foreign key from the branches table defining which branch the patron wishes to pick this hold up at'
+        });
+        # Print useful stuff here
+        say $out "Removed NULL option from branchcode for reserves";
+    },
+};
