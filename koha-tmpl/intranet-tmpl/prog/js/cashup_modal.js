4d48a9af0b0c2e83c51a616978a3abbf22f5997b
Owen Leonard <oleonard@myacpl.org>
2022-07-11 15:03:44 +0100

    Bug 31039: (follow-up) Wrap jQuery in $(document).ready()
    cashup_modal.js consists only of jQuery code, so the whole thing should
    be contained in a $(document).ready() function. This may or may not be
    contributing to the behavior this bug is trying to fix.
    
    Please note that this patch contains whitespace changes, so diff
    accordingly.
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    (cherry picked from commit 743731949db14eac858c6cdced4bb471d1da879b)
    
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>
    (cherry picked from commit b5d2c7d621749eba2faabeb72d3baee993fd0a2f)
    Signed-off-by: Arthur Suzuki <arthur.suzuki@biblibre.com>

--- koha-tmpl/intranet-tmpl/prog/js/cashup_modal.js
+++ koha-tmpl/intranet-tmpl/prog/js/cashup_modal.js
@@ -1,52 +1,54 @@
-$('#cashupSummaryModal').on('show.bs.modal', function(e) {
-    var button = $(e.relatedTarget);
-    var cashup = button.data('cashup');
-    var description = button.data('register');
-    var summary_modal = $(this);
-    summary_modal.find('#register_description').text(description);
-    $.ajax({
-        url: '/api/v1/cashups/' + cashup,
-        headers: {
-            'x-koha-embed': 'summary'
-        },
-        async: false,
-        success: function(data) {
-            summary_modal.find('#from_date').text(data.summary.from_date);
-            summary_modal.find('#to_date').text(data.summary.to_date);
-            var tbody = summary_modal.find('tbody')
-            tbody.empty();
-            for (out of data.summary.payout_grouped) {
-                tbody.append('<tr><td>' + out.credit_type.description + '</td><td>- ' + out.total.format_price() + '</td></tr>');
-            }
+$(document).ready(function() {
+    $('#cashupSummaryModal').on('show.bs.modal', function(e) {
+        var button = $(e.relatedTarget);
+        var cashup = button.data('cashup');
+        var description = button.data('register');
+        var summary_modal = $(this);
+        summary_modal.find('#register_description').text(description);
+        $.ajax({
+            url: '/api/v1/cashups/' + cashup,
+            headers: {
+                'x-koha-embed': 'summary'
+            },
+            async: false,
+            success: function(data) {
+                summary_modal.find('#from_date').text(data.summary.from_date);
+                summary_modal.find('#to_date').text(data.summary.to_date);
+                var tbody = summary_modal.find('tbody')
+                tbody.empty();
+                for (out of data.summary.payout_grouped) {
+                    tbody.append('<tr><td>' + out.credit_type.description + '</td><td>- ' + out.total.format_price() + '</td></tr>');
+                }
 
-            for (income of data.summary.income_grouped) {
-                tbody.append('<tr><td>' + income.debit_type.description + '</td><td>' + income.total.format_price() + '</td></tr>');
-            }
+                for (income of data.summary.income_grouped) {
+                    tbody.append('<tr><td>' + income.debit_type.description + '</td><td>' + income.total.format_price() + '</td></tr>');
+                }
 
-            var tfoot = summary_modal.find('tfoot');
-            tfoot.empty();
-            tfoot.append('<tr><td>Total</td><td>' + data.summary.total.format_price() + '</td></tr>');
-            for (type of data.summary.total_grouped) {
-                if (type.total !== 0) {
-                    tfoot.append('<tr><td>' + type.payment_type + '</td><td>' + type.total.format_price() + '</td></tr>');
+                var tfoot = summary_modal.find('tfoot');
+                tfoot.empty();
+                tfoot.append('<tr><td>Total</td><td>' + data.summary.total.format_price() + '</td></tr>');
+                for (type of data.summary.total_grouped) {
+                    if (type.total !== 0) {
+                        tfoot.append('<tr><td>' + type.payment_type + '</td><td>' + type.total.format_price() + '</td></tr>');
+                    }
                 }
             }
-        }
+        });
     });
-});
 
-$('.modal.printable').on('shown.bs.modal', function() {
-    $('.modal-dialog', this).addClass('focused');
-    $('body').addClass('modalprinter');
+    $('.modal.printable').on('shown.bs.modal', function() {
+        $('.modal-dialog', this).addClass('focused');
+        $('body').addClass('modalprinter');
 
-    if ($(this).hasClass('autoprint')) {
+        if ($(this).hasClass('autoprint')) {
+            window.print();
+        }
+    }).on('hidden.bs.modal', function() {
+        $('.modal-dialog', this).removeClass('focused');
+        $('body').removeClass('modalprinter');
+    });
+
+    $('.printModal').click(function() {
         window.print();
-    }
-}).on('hidden.bs.modal', function() {
-    $('.modal-dialog', this).removeClass('focused');
-    $('body').removeClass('modalprinter');
-});
-
-$('.printModal').click(function() {
-    window.print();
+    });
 });
