c98b623313122618e53f9a2c77701d0a602d0c4f
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2022-07-28 10:05:03 +0200

    Bug 30588: Add missing atomic update
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

---
+++ installer/data/mysql/atomicupdate/bug_30588.pl
@@ -0,0 +1,17 @@
+use Modern::Perl;
+
+return {
+    bug_number => "30588",
+    description => "Add an 'enforce' option for TwoFactorAuthentication",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+        $dbh->do(q{
+            UPDATE systempreferences
+            SET options="enforced|enabled|disabled",
+                value=CASE value WHEN '1' THEN 'enabled' ELSE 'disabled' END,
+                type="Choice"
+            WHERE variable="TwoFactorAuthentication"
+        });
+    },
+};
