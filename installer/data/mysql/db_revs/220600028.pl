35e03400495924088081e825349261a3c2b54ac8
Tomas Cohen Arazi <tomascohen@theke.io>
2022-08-01 14:05:45 -0300

    Bug 30077: DBRev 22.06.00.028
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

---
+++ installer/data/mysql/db_revs/220600028.pl
@@ -0,0 +1,14 @@
+use Modern::Perl;
+
+return {
+    bug_number => "30077",
+    description => "Add new system preference IntranetAddMastheadLibraryPulldown",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+        $dbh->do(q{
+            INSERT IGNORE INTO systempreferences (`variable`,`value`,`options`,`explanation`,`type`)
+            VALUES ('IntranetAddMastheadLibraryPulldown','0','','Add a library select pulldown menu on the staff header search','YesNo ')
+        });
+    },
+};
