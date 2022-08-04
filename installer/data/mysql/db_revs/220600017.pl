e544ff4345498902dad76acfc7077b473870a74a
Tomas Cohen Arazi <tomascohen@theke.io>
2022-08-04 09:55:16 -0300

    Bug 29632: (QA follow-up) Fix COMMENT discrepancy on upgrade
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- installer/data/mysql/db_revs/220600017.pl
+++ installer/data/mysql/db_revs/220600017.pl
@@ -1,7 +1,7 @@
 use Modern::Perl;
 
 return {
-    bug_number => "29632    ",
+    bug_number => "29632",
     description => "Add callnumber type to allow sorting",
     up => sub {
         my ($args) = @_;
@@ -9,6 +9,7 @@ return {
         $dbh->do(q{
             ALTER TABLE `search_field` MODIFY COLUMN `type`
             enum('','string','date','number','boolean','sum','isbn','stdno','year','callnumber') NOT NULL
+            COMMENT 'what type of data this holds, relevant when storing it in the search engine'
         });
         say $out "Add callnumber to search_field type enum";
     },
