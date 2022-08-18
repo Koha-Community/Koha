cb5049a7bc2306863abcffbbbcdd3d4c95b30c63
Katrin Fischer <katrin.fischer.83@web.de>
2022-08-18 21:26:54 +0000

    Bug 31401: Update administration sidebar to match entries on administration start page
    This makes sure that all entries on the administration start page
    have a matching entry in the administration sidebar that shows
    on the left side if you are on any of the administration sub pages.
    
    Changes made:
    * Rename 'Classification sources' to 'Classification configuration'
    * Make Plugins entry show and appear in correct spot
      This relied on the variable plugins_enabled that wasn't available
      in all the different templates. I therefore moved it to Auth.pm
      and cleaned up the code for the admin start page.
    * Move 'MARC overlay rules' and rename to 'Record overlay rules'
    
    To test:
    * Make sure plugins are enabled and visible on admin start page
    * Compare admin start page and sidebar
      * Sequence should be the same
      * All entries should appear on both pages
      * Naming should be the same
    
    Signed-off-by: David Nind <david@davidnind.com>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    (cherry picked from commit aa90b60ea2979279a5e134eb9cbc7cb3d6075417)
    
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

--- C4/Auth.pm
+++ C4/Auth.pm
@@ -524,7 +524,7 @@ sub get_template_and_user {
         $template->param(
             AmazonCoverImages                                                          => C4::Context->preference("AmazonCoverImages"),
             AutoLocation                                                               => C4::Context->preference("AutoLocation"),
-            PatronAutoComplete                                                       => C4::Context->preference("PatronAutoComplete"),
+            PatronAutoComplete                                                         => C4::Context->preference("PatronAutoComplete"),
             FRBRizeEditions                                                            => C4::Context->preference("FRBRizeEditions"),
             IndependentBranches                                                        => C4::Context->preference("IndependentBranches"),
             IntranetNav                                                                => C4::Context->preference("IntranetNav"),
@@ -549,6 +549,7 @@ sub get_template_and_user {
             UseCourseReserves                                                          => C4::Context->preference("UseCourseReserves"),
             useDischarge                                                               => C4::Context->preference('useDischarge'),
             pending_checkout_notes                                                     => Koha::Checkouts->search({ noteseen => 0 }),
+            plugins_enabled                                                            => C4::Context->config("enable_plugins"),
         );
     }
     else {
