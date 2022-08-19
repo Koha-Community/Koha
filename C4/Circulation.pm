4676ce1a03fee089c4aa51a49ecc646afb91450a
Nick Clemens <nick@bywatersolutions.com>
2022-08-19 13:11:09 +0000

    Bug 31112: (QA follow-up) Restore check to avoid extra processing
    We now count all holds for all patrons, we can still eject if we have more
    holds than we do items
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- C4/Circulation.pm
+++ C4/Circulation.pm
@@ -2959,6 +2959,8 @@ sub CanBookBeRenewed {
             notforloan   => 0,
             -not         => { itemnumber => $itemnumber } })->as_list;
 
+        return ( 0, "on_reserve" ) if scalar @other_items < scalar @possible_holds;
+
         my %matched_items;
         foreach my $possible_hold (@possible_holds) {
             my $fillable = 0;
