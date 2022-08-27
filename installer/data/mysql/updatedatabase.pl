3903d9fed4b52ef588ddaa5d524bac18930903fc
Victor Grousset/tuxayo <victor@tuxayo.net>
2022-08-27 22:51:21 +0200

    Increment version for 21.05.18 release
    Signed-off-by: Victor Grousset/tuxayo <victor@tuxayo.net>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -24691,6 +24691,11 @@ if( CheckVersion( $DBversion ) ) {
     NewVersion( $DBversion, "", "Koha 21.05.17 release" );
 }
 
+$DBversion = '21.05.18.000';
+if ( CheckVersion( $DBversion ) ) {
+    NewVersion( $DBversion, "", "Koha 21.05.18 release" );
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
