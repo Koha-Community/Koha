e110534e1ce398e23867608497bc3ff9f784edff
Katrin Fischer <katrin.fischer.83@web.de>
2022-09-07 13:17:53 +0000

    Bug 31170: (QA follow-up) Fix capitalization in database update
    As this has not been released yet, we can also fix the database
    update to not introduce the wrong capitalization in the first
    place.
    
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- installer/data/mysql/db_revs/220600013.pl
+++ installer/data/mysql/db_revs/220600013.pl
@@ -9,7 +9,7 @@ return {
         $dbh->do(q{
             INSERT IGNORE INTO letter
             (module,code,branchcode,name,is_html,title,content,message_transport_type,lang)
-            VALUES ('circulation','OVERDUE_FINE_DESC','','Overdue Item Fine Description',0,'Overdue Item Fine Description','[% item.biblio.title %] [% checkout.date_due | $KohaDates %]','print','default')
+            VALUES ('circulation','OVERDUE_FINE_DESC','','Overdue item fine description',0,'Overdue item fine description','[% item.biblio.title %] [% checkout.date_due | $KohaDates %]','print','default')
         });
     },
 };
