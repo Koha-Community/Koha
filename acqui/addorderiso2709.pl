a4d60e2be5fe111926bc497aebf6acea85cd4858
Nick Clemens <nick@bywatersolutions.com>
2022-09-15 13:50:35 +0000

    Bug 31569: Remove unecessary imports
    By using objects a number of other routines are not needed:
    GetImportRecordMarc SetImportRecordStatus SetMatchedBiblionumber
    
    To test:
    Grep for routines above in addorderiso2709 and confirm they are not used
    
    Signed-off-by: Andrew Fuerste-Henry <andrewfh@dubcolib.org>
    
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- acqui/addorderiso2709.pl
+++ acqui/addorderiso2709.pl
@@ -30,7 +30,7 @@ use Encode;
 use C4::Context;
 use C4::Auth qw( get_template_and_user );
 use C4::Output qw( output_html_with_http_headers );
-use C4::ImportBatch qw( GetImportRecordMarc GetImportRecordMatches SetImportRecordStatus SetMatchedBiblionumber SetImportBatchStatus GetImportBatch GetImportBatchRangeDesc GetNumberOfNonZ3950ImportBatches GetImportBatchOverlayAction GetImportBatchNoMatchAction GetImportBatchItemAction );
+use C4::ImportBatch qw( GetImportRecordMatches SetImportBatchStatus GetImportBatch GetImportBatchRangeDesc GetNumberOfNonZ3950ImportBatches GetImportBatchOverlayAction GetImportBatchNoMatchAction GetImportBatchItemAction );
 use C4::Matcher;
 use C4::Search qw( FindDuplicate );
 use C4::Biblio qw(
