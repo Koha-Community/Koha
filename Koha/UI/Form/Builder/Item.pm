eb292431401a7ed0782427412eb1208fa7716e4c
Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
2022-09-16 08:58:51 +0000

    Bug 31154: (QA follow-up) Fix UI form builder
    The change in batchMod.pl is not needed when we fix the builder.
    But we could at least add a comment there!
    
    Note that passing the biblionumber of the first item does not
    make sense since we are modifying items from several biblio
    records normally. We most probably do not want this MARC
    record's itemcallnumber (via the syspref) in all our items.
    
    Test plan:
    Test batchMod with and without the Populate fields checkbox.
    Fill syspref 'itemcallnumber' with e.g. 084a.
    Check Populate, and try again.
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- Koha/UI/Form/Builder/Item.pm
+++ Koha/UI/Form/Builder/Item.pm
@@ -162,7 +162,7 @@ sub generate_subfield_form {
             my $CNsubfields =
               substr( $pref_itemcallnumber_part, 3 );    # Any and all subfields
             $CNsubfields = undef if $CNsubfields eq '';
-            my $temp2 = $marc_record->field($CNtag);
+            my $temp2 = $marc_record ? $marc_record->field($CNtag) : undef;
 
             next unless $temp2;
             $value = $temp2->as_string( $CNsubfields, ' ' );
@@ -314,7 +314,7 @@ sub generate_subfield_form {
         );
         my $pars = {
             dbh     => $dbh,
-            record  => $marc_record,
+            record  => $marc_record, #Note: could be undefined
             tagslib => $tagslib,
             id      => $subfield_data{id},
         };
@@ -564,7 +564,7 @@ sub edit_form {
                         value              => $value,
                         tagslib            => $tagslib,
                         libraries          => $libraries,
-                        marc_record        => $marc_record,
+                        marc_record        => $marc_record, #Note: could be undefined
                         restricted_edition => $restricted_edition,
                         prefill_with_default_values => $prefill_with_default_values,
                         branch_limit       => $branch_limit,
