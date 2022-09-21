c9e44c6e0b0241bb786b91ce2c8833afe31a14fe
Nick Clemens <nick@bywatersolutions.com>
2022-09-21 16:10:34 +0000

    Bug 31595: Don't process extended attributes if none are inlcuded in the file
    This patch simply adds a check to the import process to only process attrbutes if
    they have been found in the data
    
    To test:
    1 - Import some patrons without extended attributes
    2 - Import patrons including extended attributes
    3 - Confirm both work unchanged
    
    Signed-off-by: Chris Cormack <chris@bigballofwax.co.nz>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    (cherry picked from commit 40cff1a774a3a9deea2effb375b3e7272003444c)
    
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>
    (cherry picked from commit 4e4fac93144c174003d49a9d049a52960f34c6e1)
    Signed-off-by: Arthur Suzuki <arthur.suzuki@biblibre.com>

--- Koha/Patrons/Import.pm
+++ Koha/Patrons/Import.pm
@@ -326,7 +326,7 @@ sub import_patrons {
                             }
                         }
                     }
-                    if ($extended) {
+                    if ($extended && @$patron_attributes) {
                         if ($ext_preserve) {
                             $patron_attributes = $patron->extended_attributes->merge_and_replace_with( $patron_attributes );
                         }
@@ -386,7 +386,7 @@ sub import_patrons {
                         );
                     }
 
-                    if ($extended) {
+                    if ($extended && @$patron_attributes) {
                         # FIXME Hum, we did not filter earlier and now we do?
                         $patron->extended_attributes->filter_by_branch_limitations->delete;
                         $patron->extended_attributes($patron_attributes);
