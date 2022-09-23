c86d7f632fdcfb309151e094e5794bd9c09be290
Tomas Cohen Arazi <tomascohen@theke.io>
2022-09-23 09:03:50 -0300

    Bug 25375: New atomicupdate syntax
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

---
+++ installer/data/mysql/atomicupdate/bug_25375.pl
@@ -0,0 +1,15 @@
+use Modern::Perl;
+
+return {
+    bug_number  => "25735",
+    description => "Add Elasticsearch field 'available'",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+
+        $dbh->do(qq{
+            INSERT IGNORE INTO search_field (name, label, type)
+            VALUES ('available', 'available', 'boolean')
+        });
+    },
+};
