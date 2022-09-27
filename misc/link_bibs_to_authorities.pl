4310e13a6416d680e825621278859e3b91b1942a
Tomas Cohen Arazi <tomascohen@theke.io>
2022-09-27 16:10:51 -0300

    Bug 30024: Make link_bibs_to_authorities.pl rely on LinkerRelink
    This patch makes the trivial change of making the linker script rely on
    LinkerRelink instead of CatalogModuleRelink, which is confusing and
    incorrect (according to both sysprefs descriptions).
    
    To test:
    1. Verify that relinking is now tied to the right syspref
    
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    Signed-off-by: Phil Ringnalda <phil@chetcolibrary.org>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- misc/link_bibs_to_authorities.pl
+++ misc/link_bibs_to_authorities.pl
@@ -1,7 +1,21 @@
 #!/usr/bin/perl
 
-use strict;
-use warnings;
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
 
 use Koha::Script;
 use C4::Context;
@@ -37,7 +51,7 @@ my $auth_limit;
 my $bib_limit;
 my $commit = 100;
 my $tagtolink;
-my $allowrelink = C4::Context->preference("CatalogModuleRelink") || '';
+my $allowrelink = C4::Context->preference("LinkerRelink") // '';
 
 my $result = GetOptions(
     'v|verbose'      => \$verbose,
