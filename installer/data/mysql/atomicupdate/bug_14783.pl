3da8a25253248802e65e6d69af4b2bce4d2d9dee
Tomas Cohen Arazi <tomascohen@theke.io>
2022-10-17 15:59:07 -0300

    Bug 14783: Update atomicupdate syntax
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

---
+++ installer/data/mysql/atomicupdate/bug_14783.pl
@@ -0,0 +1,30 @@
+use Modern::Perl;
+
+return {
+    bug_number  => "14783",
+    description => "Allow patrons to change pickup location for non-waiting holds",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+
+        $dbh->do(q{
+            INSERT IGNORE INTO systempreferences (variable, value, options, explanation, type)
+            VALUES ('OPACAllowUserToChangeBranch','','Pending, In-Transit, Suspended','Allow users to change the library to pick up a hold for these statuses:','multiple');
+        });
+
+        $dbh->do(q{
+            UPDATE systempreferences SET value = (SELECT CASE WHEN value = 1 THEN 'intransit' ELSE '' END
+            FROM systempreferences
+            WHERE variable = 'OPACInTransitHoldPickupLocationChange')
+        });
+
+        say $out "Added new system preference 'OPACAllowUserToChangeBranch'";
+
+        $dbh->do(q{
+            DELETE FROM systempreferences
+            WHERE variable = 'OPACInTransitHoldPickupLocationChange'
+        });
+
+        say $out "Removed system preference 'OPACInTransitHoldPickupLocationChange'";
+    },
+};
