87391f3e400365cf237024e73e8319709c6b8158
Tomas Cohen Arazi <tomascohen@theke.io>
2022-11-14 12:01:10 -0300

    Bug 32191: Add sample output for copying and pasting to skeleton.pl
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- installer/data/mysql/atomicupdate/skeleton.pl
+++ installer/data/mysql/atomicupdate/skeleton.pl
@@ -9,6 +9,16 @@ return {
         # Do you stuffs here
         $dbh->do(q{});
         # Print useful stuff here
-        say $out "Update is going well so far";
+        # tables
+        say $out "Added new table 'XXX'";
+        say $out "Added column 'XXX.YYY'";
+        # sysprefs
+        say $out "Added new system preference 'XXX'";
+        say $out "Updated system preference 'XXX'";
+        say $out "Removed system preference 'XXX'";
+        # permissions
+        say $out "Added new permission 'XXX'";
+        # letters
+        say $out "Added new letter 'XXX' (TRANSPORT)";
     },
 };
