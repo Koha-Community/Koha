6072492693a9c0cb784224f284733011a2f092f9
Tomas Cohen Arazi <tomascohen@theke.io>
2022-11-28 15:26:01 -0300

    Koha 22.12 - start of a new dev cycle
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

---
+++ installer/data/mysql/db_revs/221200000.pl
@@ -0,0 +1,16 @@
+use Modern::Perl;
+use utf8;
+use Encode qw( encode_utf8 );
+
+return {
+    bug_number => undef,
+    description => 'Increase DBRev for 22.12',
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+
+        say $out encode_utf8 '📜 Deep into that darkness peering, long I stood there wondering, fearing,';
+        say $out encode_utf8 '📜 Doubting, dreaming dreams no mortal ever dared to dream before;';
+        say $out encode_utf8 '📜 But the silence was unbroken, and the stillness gave no token';
+    },
+}
