bywatersolutions/bywater-koha-devel

Bug 20457: Overdue and pre-overdue cronjobs not skipping phone notices
The overdue and pre-overdue cron scripts are not skipping the generation of phone notices. This causes many phone notices to be created that will always be left at 'pending' as the Talking Tech outbound script creates its own phone notices and puts them in the message queue.

Test Plan:
1) Enable Talking Tech
2) Enable predue and overdue notice phone transports for a patron
3) Generate overdues and predues, notice phone notices are generated
4) Apply the patch
5) Repeat steps 2-3
6) Note phone notices are not generated

misc/cronjobs/advance_notices.pl
@@ -285,6 +285,7 @@ UPCOMINGITEM: foreach my $upcoming ( @$upcoming_dues ) {
 
             ## Get branch info for borrowers home library.
             foreach my $transport ( keys %{$borrower_preferences->{'transports'}} ) {
+                next if $transport eq 'phone';
                 my $letter = parse_letter( { letter_code    => $letter_type,
                                       borrowernumber => $upcoming->{'borrowernumber'},
                                       branchcode     => $upcoming->{'branchcode'},
@@ -323,6 +324,7 @@ UPCOMINGITEM: foreach my $upcoming ( @$upcoming_dues ) {
 
             ## Get branch info for borrowers home library.
             foreach my $transport ( keys %{$borrower_preferences->{'transports'}} ) {
+                next if $transport eq 'phone';
                 my $letter = parse_letter( { letter_code    => $letter_type,
                                       borrowernumber => $upcoming->{'borrowernumber'},
                                       branchcode     => $upcoming->{'branchcode'},
@@ -566,6 +568,7 @@ sub send_digests {
         }
 
         foreach my $transport ( keys %{ $borrower_preferences->{'transports'} } ) {
+            next if $transport eq 'phone';
             my $letter = parse_letter(
                 {
                     letter_code    => $params->{letter_code},

misc/cronjobs/overdue_notices.pl
@@ -678,6 +678,7 @@ END_SQL
 
                 my $print_sent = 0; # A print notice is not yet sent for this patron
                 for my $mtt ( @message_transport_types ) {
+                    next if $mtt eq 'phone';
                     my $effective_mtt = $mtt;
                     if ( ($mtt eq 'email' and not scalar @emails_to_use) or ($mtt eq 'sms' and not $data->{smsalertnumber}) ) {
                         # email or sms is requested but not exist, do a print.

Koha-Community/Koha

Signed-off-by: Joonas Kylmälä <joonas.kylmala@iki.fi>
Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
(cherry picked from commit bf6bbc29a0c84581a59624b070cb5747f195ea43)
Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

misc/cronjobs/advance_notices.pl
@@ -298,6 +298,7 @@ UPCOMINGITEM: foreach my $upcoming ( @$upcoming_dues ) {
 
             ## Get branch info for borrowers home library.
             foreach my $transport ( keys %{$borrower_preferences->{'transports'}} ) {
+                next if $transport eq 'itiva';
                 my $letter = parse_letter( { letter_code    => $letter_type,
                                       borrowernumber => $upcoming->{'borrowernumber'},
                                       branchcode     => $branchcode,
@@ -345,6 +346,7 @@ UPCOMINGITEM: foreach my $upcoming ( @$upcoming_dues ) {
 
             ## Get branch info for borrowers home library.
             foreach my $transport ( keys %{$borrower_preferences->{'transports'}} ) {
+                next if $transport eq 'itiva';
                 my $letter = parse_letter( { letter_code    => $letter_type,
                                       borrowernumber => $upcoming->{'borrowernumber'},
                                       branchcode     => $branchcode,
@@ -595,6 +597,7 @@ sub send_digests {
         }
 
         foreach my $transport ( keys %{ $borrower_preferences->{'transports'} } ) {
+            next if $transport eq 'itiva';
             my $letter = parse_letter(
                 {
                     letter_code    => $params->{letter_code},

misc/cronjobs/overdue_notices.pl
@@ -704,6 +704,7 @@ END_SQL
 
                 my $print_sent = 0; # A print notice is not yet sent for this patron
                 for my $mtt ( @message_transport_types ) {
+                    next if $mtt eq 'itiva';
                     my $effective_mtt = $mtt;
                     if ( ($mtt eq 'email' and not scalar @emails_to_use) or ($mtt eq 'sms' and not $data->{smsalertnumber}) ) {
                         # email or sms is requested but not exist, do a print.