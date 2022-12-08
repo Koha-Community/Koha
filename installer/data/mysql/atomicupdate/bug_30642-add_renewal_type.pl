7fe9e76af85f3f063ac870d17b10c5e18f95c48f
Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>
2022-12-08 14:28:49 +0000

    Bug 30642: (follow-up) Change sql and adapt for translations
    New column has now been changed to an enum in line with comments and the strings have been amended to be picked up for translation. The file koha-tmpl/intranet-tmpl/prog/en/includes/str/checkout_renewals.inc has been removed as the variables can be included within the javascript file.
    
    Test plan:
    1) In the database shell run "show columns from checkout_renewals;" and observe that there is currently no column for recording the type of renewal
    2) Apply patch
    3) In the shell run "dbic" and "perl installer/data/mysql/updatedatabase.pl" to update the database schema with the new column.
    4) Create some checkouts
    5) Renew some checkouts manually and observe in the database that there is now a column called "renewal_type" that will have recorded these as "Manual"
    6) Create some checkouts that can be automatically renewed
    7) Run the cron script in automatic_renewals.pl and observe that there are now also entries with a renewal_type of "Automatic"
    8) Send a GET request to http://localhost:8081/api/v1/checkouts/1/renewals and observe that the renewal_type is now returned in the response
    9) In the Item Details tab for a record, there is the "Current renewals" option which has a button to view renewals. Click on this and observe that the modal now displays the new information.
    
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- installer/data/mysql/atomicupdate/bug_30642-add_renewal_type.pl
+++ installer/data/mysql/atomicupdate/bug_30642-add_renewal_type.pl
@@ -1,7 +1,7 @@
 use Modern::Perl;
 
 return {
-    bug_number => "BUG_30642",
+    bug_number => "30642",
     description => "Record whether a renewal has been done manually or automatically.",
     up => sub {
         my ($args) = @_;
@@ -9,7 +9,7 @@ return {
 
         if( !column_exists( 'checkout_renewals', 'renewal_type' ) ) {
           $dbh->do(q{
-              ALTER TABLE checkout_renewals ADD COLUMN `renewal_type` varchar(9) NOT NULL AFTER `timestamp`
+              ALTER TABLE checkout_renewals ADD COLUMN `renewal_type` enum('Automatic', 'Manual') NOT NULL DEFAULT 'Manual' AFTER `timestamp`
           });
 
           say $out "Added column 'checkout_renewals.column_name'";
