ecc92f92ca1569f20abbc4aa0cea3944d1cb4973
Nick Clemens <nick@bywatersolutions.com>
2022-12-12 18:13:25 +0000

    Bug 32437: Add replace method to Koha::Import::Record objects
    This patch adds a replace routine to Koha::Import::Record - largely
    copying and updating code from C4::ImportBatch
    
    To test:
    prove t/db_dependent/Koha/Import/Records.t
    
    Signed-off-by: Andrew Fuerste-Henry <andrewfh@dubcolib.org>
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- Koha/Import/Record.pm
+++ Koha/Import/Record.pm
@@ -21,8 +21,11 @@ use Carp;
 use MARC::Record;
 
 use C4::Context;
+use C4::Biblio qw(ModBiblio);
+use C4::AuthoritiesMarc qw(GuessAuthTypeCode ModAuthority);
 use Koha::Database;
 use Koha::Import::Record::Biblios;
+use Koha::Import::Record::Auths;
 use Koha::Import::Record::Matches;
 
 use base qw(Koha::Object);
@@ -72,6 +75,20 @@ sub import_biblio {
     return Koha::Import::Record::Biblio->_new_from_dbic( $import_biblio_rs );
 }
 
+=head3 import_auth
+
+Returns the import auth object for this import record
+
+    my $import_auth = $import_record->import_auth()
+
+=cut
+
+sub import_auth {
+    my ( $self ) = @_;
+    my $import_auth_rs = $self->_result->import_auth;
+    return Koha::Import::Record::Auth->_new_from_dbic( $import_auth_rs );
+}
+
 =head3 get_import_record_matches
 
 Returns the Import::Record::Matches for the record
@@ -91,6 +108,62 @@ sub get_import_record_matches {
     return $matches->filter_by_chosen() if $chosen;
 
     return $matches->search({},{ order_by => { -desc => ['score','candidate_match_id'] } });
+}
+
+=head3 replace
+
+Import the record to replace an existing record which is passed to this sub
+
+    $import_record->replace({ biblio => $biblio_object });
+
+=cut
+
+sub replace {
+    my ($self, $params) = @_;
+    my $biblio = $params->{biblio};
+    my $authority = $params->{authority};
+
+    my $userenv = C4::Context->userenv;
+    my $logged_in_patron = Koha::Patrons->find( $userenv->{number} );
+
+    my $marc_record = $self->get_marc_record;
+    my $xmlrecord;
+    if( $biblio ){
+        my $record = $biblio->metadata->record;
+        $xmlrecord = $record->as_xml;
+        my $context = { source => 'batchimport' };
+        if ($logged_in_patron) {
+            $context->{categorycode} = $logged_in_patron->categorycode;
+            $context->{userid} = $logged_in_patron->userid;
+        }
+        ModBiblio(
+            $marc_record,
+            $biblio->id,
+            $biblio->frameworkcode,
+            {
+                overlay_context   => $context,
+                skip_record_index => 1
+            }
+        );
+        $self->import_biblio->matched_biblionumber( $biblio->id )->store;
+    } elsif( $authority ) {
+        $xmlrecord = $authority->marcxml;
+        ModAuthority(
+            $authority->id,
+            $marc_record,
+            GuessAuthTypeCode($marc_record)
+        );
+        $self->import_auth->matched_authid( $authority->id )->store;
+    } else {
+        # We could also throw an exception
+        return;
+    }
+    $self->marcxml_old( $xmlrecord );
+    $self->status('imported');
+    $self->overlay_status('match_applied');
+    $self->store;
+
+    return 1;
 }
 
 =head2 Internal methods
