8f91d48b6ba6f9bc5fe0026f4b81e3885371dbf1
Wainui Witika-Park <wainuiwitikapark@catalyst.net.nz>
2022-12-23 14:43:09 +1300

    Increment version for 21.05.22 release
    Signed-off-by: Wainui Witika-Park <wainuiwitikapark@catalyst.net.nz>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -24711,6 +24711,11 @@ if ( CheckVersion( $DBversion ) ) {
     NewVersion( $DBversion, "", "Koha 21.05.21 release" );
 }
 
+$DBversion = '21.05.22.000';
+if ( CheckVersion( $DBversion ) ) {
+    NewVersion( $DBversion, "", "Koha 21.05.22 release" );
+}
+
 # SEE bug 13068
 # if there is anything in the atomicupdate, read and execute it.
 my $update_dir = C4::Context->config('intranetdir') . '/installer/data/mysql/atomicupdate/';
