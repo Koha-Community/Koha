caef730218b3f5fbf6211a1ce890e7875cdd39b8
Aleisha Amohia <aleishaamohia@hotmail.com>
2023-01-13 14:39:50 +1300

    Bug 8367: (follow-up) possible to delete circ rule
    Signed-off-by: Emmi Takkinen <emmi.takkinen@koha-suomi.fi>
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    Signed-off-by: Kelly <kelly@bywatersolutions.com>
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    Signed-off-by: Kelly <kelly@bywatersolutions.com>
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- admin/smart-rules.pl
+++ admin/smart-rules.pl
@@ -116,6 +116,7 @@ if ($op eq 'delete') {
                 recall_overdue_fine              => undef,
                 recall_shelf_time                => undef,
                 decreaseloanholds                => undef,
+                holds_pickup_period              => undef,
             }
         }
     );
