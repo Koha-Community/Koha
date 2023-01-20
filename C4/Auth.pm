b0437dc643652bf97f43952540f4304e4d0d5eec
Wainui Witika-Park <wainuiwitikapark@catalyst.net.nz>
2023-01-20 15:44:32 +1300

    Revert "Bug 31908: Replace an exit by a safe_exit in Auth.pm L1314"
    This reverts commit b160979234babc22bf6670149a09a10015a2419e.

--- C4/Auth.pm
+++ C4/Auth.pm
@@ -1320,7 +1320,7 @@ sub checkauth {
             $uri->query_param_delete('password');
             $uri->query_param_delete('koha_login_context');
             print $query->redirect(-uri => $uri->as_string, -cookie => $cookie, -status=>'303 See other');
-            safe_exit;
+            exit;
         }
 
         return ( $userid, $cookie, $sessionID, $flags );
