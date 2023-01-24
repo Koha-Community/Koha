1d2539b803b2ea13a9548bca3c7140a5a3907aed
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2023-01-24 11:03:40 +0000

    Bug 30979: Redirect to checkout summary
    This is a temporary solution whilst we wait for the biblio api endpoints
    so we may include more biblio details in the checkout modal itself.
    
    Signed-off-by: Silvia Meakins <smeakins@eso.org>
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- koha-tmpl/opac-tmpl/bootstrap/js/modals/checkout.js
+++ koha-tmpl/opac-tmpl/bootstrap/js/modals/checkout.js
@@ -268,6 +268,8 @@ $(document).ready(function() {
         let pageName = $(location).attr("pathname");
         if ( pageName == '/cgi-bin/koha/opac-user.pl' ) {
             location.reload();
+        } else {
+            location.href = "/cgi-bin/koha/opac-user.pl";
         }
     })
 });
