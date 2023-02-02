bywatersolutions/bywater-koha-devel

Bug 20256: Use new methods

catalogue/moredetail.pl
@@ -175,13 +175,8 @@ foreach my $item (@items){
         $item->{status_advisory} = 1;
     }
 
-    if (C4::Context->preference("IndependentBranches")) {
-        #verifying rights
-        my $userenv = C4::Context->userenv();
-        unless (C4::Context->IsSuperLibrarian() or ($userenv->{'branch'} eq $item->{'homebranch'})) {
-                $item->{'nomod'}=1;
-        }
-    }
+    $item->{nomod} = !$patron->can_edit_item( $item );
+
     if ($item->{'datedue'}) {
         $item->{'issue'}= 1;
     } else {

Koha-Community/Koha

Signed-off-by: Bob Bennhoff - CLiC <bbennhoff@clicweb.org>
Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

catalogue/moredetail.pl
@@ -247,6 +247,8 @@ foreach my $item (@items){
         }
     );
 
+    $item_info->{nomod} = !$patron->can_edit_item( $item );
+
     push @item_data, $item_info;
 }
 