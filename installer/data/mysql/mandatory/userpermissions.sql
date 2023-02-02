bywatersolutions/bywater-koha-devel

Bug 20256: Update database
Test Plan:
1) Apply this patch set
2) Run updatedatabase.pl
3) Create two library groups with some libraries in them,
   make sure to enable the new feature to limit editing of items
4) Test the functionality, if an item is owned by a library in
   your group, you should have full editing abilities. If an item
   is from a different group, you should only be able to make the
   same changes you would be able to if IndependentBranches were.

installer/data/mysql/userpermissions.sql
@@ -39,6 +39,7 @@ INSERT INTO permissions (module_bit, code, description) VALUES
    ( 9, 'edit_items', 'Edit items'),
    ( 9, 'edit_items_restricted', 'Limit item modification to subfields defined in the SubfieldsToAllowForRestrictedEditing preference (please note that edit_item is still required)'),
    ( 9, 'delete_all_items', 'Delete all items at once'),
+   ( 9, 'edit_any_item', 'Edit any item reguardless of home library'),
    (10, 'writeoff', 'Write off fines and fees'),
    (10, 'remaining_permissions', 'Remaining permissions for managing fines and fees'),
    (11, 'currencies_manage', 'Manage currencies and exchange rates'),

Koha-Community/Koha

Signed-off-by: Bob Bennhoff - CLiC <bbennhoff@clicweb.org>
Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

installer/data/mysql/mandatory/userpermissions.sql
@@ -56,6 +56,7 @@ INSERT INTO permissions (module_bit, code, description) VALUES
    ( 9, 'edit_items_restricted', 'Limit item modification to subfields defined in the SubfieldsToAllowForRestrictedEditing preference (please note that edit_item is still required)'),
    ( 9, 'delete_all_items', 'Delete all items at once'),
    ( 9, 'manage_item_groups', 'Create, update and delete item groups, add or remove items from a item groups'),
+   ( 9, 'edit_any_item', 'Edit any item regardless of home library'),
    (10, 'payout', 'Perform account payout action'),
    (10, 'refund', 'Perform account refund action'),
    (10, 'discount', 'Perform account discount action'),