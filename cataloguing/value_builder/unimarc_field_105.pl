b7b798549ea1c33a75d035b9d7273312f7136891
Julian Maurice <julian.maurice@biblibre.com>
2023-02-03 11:58:51 +0100

    Bug 32825: Fix cataloguing/value_builder/unimarc_field_105.pl
    Use event parameter
    
    This is part of the refactoring happening in bug 30975
    
    Test plan:
    1. Verify that the plugin continues to work as before
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    (cherry picked from commit 7ec85d23949b97dd0eea3efbd7acdf4abf3a068e)
    Signed-off-by: Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>
    (cherry picked from commit 98262166d911515a2ac38564c499218ab9d2fb84)
    
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

--- cataloguing/value_builder/unimarc_field_105.pl
+++ cataloguing/value_builder/unimarc_field_105.pl
@@ -33,9 +33,10 @@ sub plugin_javascript {
     my ($dbh,$record,$tagslib,$field_number) = @_;
     my $res="
     <script>
-        function Clic$field_number() {
-                defaultvalue=document.getElementById(\"$field_number\").value;
-                window.open(\"../cataloguing/plugin_launcher.pl?plugin_name=unimarc_field_105.pl&index=$field_number&result=\"+defaultvalue,\"unimarc_field_105\",'width=1200,height=400,toolbar=false,scrollbars=yes');
+        function Clic$field_number(ev) {
+                ev.preventDefault();
+                defaultvalue=document.getElementById(ev.data.id).value;
+                window.open(\"../cataloguing/plugin_launcher.pl?plugin_name=unimarc_field_105.pl&index=\" + ev.data.id + \"&result=\"+defaultvalue,\"unimarc_field_105\",'width=1200,height=400,toolbar=false,scrollbars=yes');
 
         }
     </script>
