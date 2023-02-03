634c23f69977976a03043f5fd1b3e8422151f6b3
Julian Maurice <julian.maurice@biblibre.com>
2023-02-03 11:58:55 +0100

    Bug 32875: Fix cataloguing/value_builder/unimarc_leader_authorities.pl
    Use event parameter
    
    This is part of the refactoring happening in bug 30975
    
    Test plan:
    1. Verify that the plugin continues to work as before
    
    Signed-off-by: David Nind <david@davidnind.com>
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- cataloguing/value_builder/unimarc_leader_authorities.pl
+++ cataloguing/value_builder/unimarc_leader_authorities.pl
@@ -30,15 +30,16 @@ sub plugin_javascript {
     my ( $dbh, $record, $tagslib, $field_number ) = @_;
     my $res           = "
         <script type='text/javascript'>
-            function Clic$field_number(i) {
+            function Clic$field_number(event) {
+                event.preventDefault();
                 var defaultvalue;
                 try {
-                    defaultvalue = document.getElementById(i).value;
+                    defaultvalue = document.getElementById(event.data.id).value;
                 } catch(e) {
-                    alert('error when getting '+i);
+                    alert('error when getting '+event.data.id);
                     return;
                 }
-                window.open(\"/cgi-bin/koha/cataloguing/plugin_launcher.pl?plugin_name=unimarc_leader_authorities.pl&index=\"+i+\"&result=\"+defaultvalue,\"unimarc_field_000\",'width=1000,height=600,toolbar=false,scrollbars=yes');
+                window.open(\"/cgi-bin/koha/cataloguing/plugin_launcher.pl?plugin_name=unimarc_leader_authorities.pl&index=\" + event.data.id + \"&result=\"+defaultvalue,\"unimarc_field_000\",'width=1000,height=600,toolbar=false,scrollbars=yes');
             }
         </script>
 ";
