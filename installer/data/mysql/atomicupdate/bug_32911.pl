1e468e18c0d274b03e4ce77f7bc4397786c50632
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2023-02-08 17:06:56 +0000

    Bug 32911: Database update
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

---
+++ installer/data/mysql/atomicupdate/bug_32911.pl
@@ -0,0 +1,20 @@
+use Modern::Perl;
+
+return {
+    bug_number => "32911",
+    description => "Remove ILL partner_code config from koha-conf.xml and turn it into a system preference",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+
+        my $xml_config = C4::Context->config("interlibrary_loans");
+        my $existing_partner_code = $xml_config->{partner_code};
+
+        $dbh->do(qq{
+            INSERT IGNORE INTO systempreferences (variable,value,explanation,options,type)
+            VALUES ('ILLPartnerCode', '$existing_partner_code', NULL, 'Patrons from this patron category will be used as partners to place ILL requests with', 'free');
+        });
+
+        say $out "Moved value of partner_code in koha-conf.xml into new system preference 'ILLPartnerCode'";
+    }
+};
