28b0b435442f0d0d584257c4110b2f14f9a0dbf9
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2023-02-09 15:14:52 +0000

    Bug 32924: (follow-up) Add left margin to 'Show mine only' label
    Signed-off-by: Pedro Amorim <pedro.amorim@ptfs-europe.com>
    Signed-off-by: Victor Grousset/tuxayo <victor@tuxayo.net>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    (cherry picked from commit fafccfd76f263445205febab0cfa5a1c8bffb934)
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- koha-tmpl/intranet-tmpl/prog/js/vue/components/ERM/AgreementsList.vue
+++ koha-tmpl/intranet-tmpl/prog/js/vue/components/ERM/AgreementsList.vue
@@ -494,6 +494,7 @@ export default {
 #agreement_list {
     display: table;
 }
+.filters > label[for="by_mine_filter"],
 .filters > input[type="checkbox"],
 .filters > input[type="button"] {
     margin-left: 1rem;
