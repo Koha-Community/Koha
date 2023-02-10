6a98a8ef837e10ab2303d03af49bf9f6d5f297a3
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2023-02-10 15:39:32 +0000

    Bug 29822: Database and Preference description updates
    Signed-off-by: David Nind <david@davidnind.com>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

---
+++ installer/data/mysql/atomicupdate/bug_29822.pl
@@ -0,0 +1,20 @@
+use Modern::Perl;
+
+return {
+    bug_number  => "29822",
+    description => "Convert DefaultPatronSeachFields from csv to psv",
+    up          => sub {
+        my ($args) = @_;
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
+
+        # Do you stuffs here
+        $dbh->do(q{
+            UPDATE systempreferences
+            SET
+              value = REPLACE( value, ',', '|' )
+            WHERE
+              variable = 'DefaultPatronSearchFields'
+        });
+        say $out "Updated system preference 'DefaultPatronSearchFields";
+    },
+};
