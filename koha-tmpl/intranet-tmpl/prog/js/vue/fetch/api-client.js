e21836135292af633f31920708337185cb34b669
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2023-02-21 10:50:02 +0100

    Bug 32939: Use APIClient to fetch vendors
    Signed-off-by: Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    (cherry picked from commit 2d86a671f2e4ae42bfaba2b69642a5cae3707655)
    Signed-off-by: Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>

--- koha-tmpl/intranet-tmpl/prog/js/vue/fetch/api-client.js
+++ koha-tmpl/intranet-tmpl/prog/js/vue/fetch/api-client.js
@@ -1,7 +1,9 @@
 import ERMAPIClient from "./erm-api-client";
 import PatronAPIClient from "./patron-api-client";
+import AcquisitionAPIClient from "./acquisition-api-client";
 
 export const APIClient = {
     erm: new ERMAPIClient(),
     patron: new PatronAPIClient(),
+    acquisition: new AcquisitionAPIClient(),
 };
