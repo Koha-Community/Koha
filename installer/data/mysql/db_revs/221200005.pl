98eca9807f4f1ae6b5e4f162d399cbe305b848ed
Tomas Cohen Arazi <tomascohen@theke.io>
2023-02-22 09:22:12 -0300

    Bug 31051: DBRev 22.12.00.005
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

---
+++ installer/data/mysql/db_revs/221200005.pl
@@ -0,0 +1,17 @@
+use Modern::Perl;
+
+return {
+    bug_number  => "31051",
+    description => "Add new system preference OPACShowSavings",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+
+        $dbh->do(q{
+            INSERT IGNORE INTO systempreferences (variable,value,options,explanation,type)
+                VALUES ('OPACShowSavings', '', 'checkouthistory|summary|user', 'Show on the OPAC the total amount a patron has saved by using a library instead of purchasing, based on replacement prices', 'multiple')
+        });
+
+        say $out "Added new system preference 'OPACShowSavings'";
+    },
+};
