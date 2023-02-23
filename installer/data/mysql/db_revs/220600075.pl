e7056634a507e35065a16d63e89f77eca6c9d7e7
Tomas Cohen Arazi <tomascohen@theke.io>
2023-02-23 07:56:31 -0300

    Bug 33051: Make 22600075.pl idempotent
    As there is no check, this dbrev could cause trouble if run twice. It
    doesn't look like it is going to get backported to 22.05, but just in
    case...
    
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    (cherry picked from commit 4a28d4295956e077c833b1bc91a12fb4a1c26ad7)
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- installer/data/mysql/db_revs/220600075.pl
+++ installer/data/mysql/db_revs/220600075.pl
@@ -1,12 +1,17 @@
 use Modern::Perl;
 
 return {
-    bug_number => "31948",
+    bug_number  => "31948",
     description => "Add timestamp to tmp_holdsqueue table",
     up => sub {
         my ($args) = @_;
         my ($dbh, $out) = @$args{qw(dbh out)};
 
-        $dbh->do(q{ALTER TABLE `tmp_holdsqueue` ADD `timestamp` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP AFTER item_level_request});
+        unless ( column_exists( 'tmp_holdsqueue', 'timestamp' ) ) {
+            $dbh->do(q{
+                ALTER TABLE `tmp_holdsqueue`
+                    ADD `timestamp` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP AFTER item_level_request
+            });
+        }
     },
 };
