48bc9e54d8c2647fc58cf118665a0988321800d0
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2023-02-23 15:46:48 +0100

    Bug 33032: Remove alternateholdings_count
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    (cherry picked from commit e36e393c7d05fc6e2af6391b908807655f6841e7)
    Signed-off-by: Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>

--- C4/Search.pm
+++ C4/Search.pm
@@ -2068,7 +2068,6 @@ sub searchResults {
             my $holdingsep = C4::Context->preference("AlternateHoldingsSeparator") || ' ';
             my @alternateholdingsinfo = ();
             my @holdingsfields = $marcrecord->field(substr $fieldspec, 0, 3);
-            my $alternateholdingscount = 0;
 
             for my $field (@holdingsfields) {
                 my %holding = ( holding => '' );
@@ -2082,12 +2081,10 @@ sub searchResults {
                 }
                 if ($havesubfield) {
                     push(@alternateholdingsinfo, \%holding);
-                    $alternateholdingscount++;
                 }
             }
 
             $oldbiblio->{'ALTERNATEHOLDINGS'} = \@alternateholdingsinfo;
-            $oldbiblio->{'alternateholdings_count'} = $alternateholdingscount;
         }
 
         push( @newresults, $oldbiblio );
