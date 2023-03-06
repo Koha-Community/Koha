3152fdc14eb37bfa26d1a74f13734edfc9d2dcb7
Agustin Moyano <agustinmoyano@theke.io>
2023-03-06 16:44:44 -0300

    Bug 33169: Build breadcrumbs and left-hand side menu from routes definition
    Signed-off-by: Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    (cherry picked from commit e3f88f0438232ab2c61129466aaa1fb205f4f720)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>
    (cherry picked from commit 31f48a6d19cb6023af7c2fe023b5f39a237d17d0)
    Signed-off-by: Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>

---
+++ koha-tmpl/intranet-tmpl/prog/js/vue/components/Breadcrumbs.vue
@@ -0,0 +1,53 @@
+<template>
+    <nav id="breadcrumbs" aria-label="Breadcrumb" class="breadcrumb">
+        <ol>
+            <template v-for="(item, idx) in breadcrumbs" v-bind:key="idx">
+                <NavigationItem
+                    v-if="idx < breadcrumbs.length - 1"
+                    :item="item"
+                    :params="params"
+                ></NavigationItem>
+                <NavigationItem
+                    v-else
+                    :item="{
+                        ...item,
+                        disabled: true,
+                        path: undefined,
+                        href: undefined,
+                    }"
+                    :params="params"
+                ></NavigationItem>
+            </template>
+        </ol>
+    </nav>
+</template>
+
+<script>
+import { inject } from "vue"
+import { storeToRefs } from "pinia"
+import NavigationItem from "./NavigationItem.vue"
+export default {
+    name: "Breadcrumbs",
+    setup: () => {
+        const navigationStore = inject("navigationStore")
+        const { breadcrumbs } = storeToRefs(navigationStore)
+        const { params } = navigationStore
+        return {
+            breadcrumbs,
+            params,
+        }
+    },
+    components: {
+        NavigationItem,
+    },
+}
+</script>
+
+<style>
+#breadcrumbs .disabled {
+    padding: 0.6em 0.3em;
+    text-decoration: none;
+    color: #000;
+    pointer-events: none;
+}
+</style>
