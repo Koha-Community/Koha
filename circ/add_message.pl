4606ceb05e18265e762761dc306226855586c806
Fridolin Somers <fridolin.somers@biblibre.com>
2023-03-14 10:33:58 -1000

    Bug 29393: (follow-up) Explicite use of methods from C4
    Signed-off-by: Matthias Le Gac <matthias.le-gac@inlibro.com>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- circ/add_message.pl
+++ circ/add_message.pl
@@ -22,8 +22,8 @@ use Modern::Perl;
 use CGI qw ( -utf8 );
 
 use C4::Auth qw( get_template_and_user );
-use C4::Output;
-use C4::Letters;
+use C4::Output qw( output_and_exit );
+use C4::Letters qw( GetPreparedLetter EnqueueLetter );
 use Koha::Patron::Message;
 use Koha::Patrons;
 
