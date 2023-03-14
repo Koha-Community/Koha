3061258f0f41c573e108c31f2b71ec14e4c0233d
Tomas Cohen Arazi <tomascohen@theke.io>
2023-03-14 08:48:06 -0300

    Bug 32057: (QA follow-up) Add missing messages (from skeleton.pl)
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- installer/data/mysql/atomicupdate/bug_32057.pl
+++ installer/data/mysql/atomicupdate/bug_32057.pl
@@ -1,21 +1,24 @@
 use Modern::Perl;
 
 return {
-    bug_number => "32057",
+    bug_number  => "32057",
     description => "Bug 32057 - Add optional stack trace to action logs",
-    up => sub {
+    up          => sub {
         my ($args) = @_;
-        my $dbh = $args->{dbh};
+        my ($dbh, $out) = @$args{qw(dbh out)};
 
-        if( !column_exists( 'action_logs', 'trace' ) ) {
-          $dbh->do(q{
-              ALTER TABLE action_logs ADD COLUMN `trace` TEXT DEFAULT NULL COMMENT 'An optional stack trace enabled by ActionLogsTraceDepth' AFTER `script`
-          });
+        if ( !column_exists( 'action_logs', 'trace' ) ) {
+            $dbh->do(q{
+              ALTER TABLE action_logs
+                ADD COLUMN `trace` TEXT DEFAULT NULL COMMENT 'An optional stack trace enabled by ActionLogsTraceDepth' AFTER `script`
+            });
+            say $out "Added column 'action_logs.trace'";
         }
 
         $dbh->do(q{
             INSERT INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type` ) VALUES
             ('ActionLogsTraceDepth', '0', '', 'Sets the maximum depth of the action logs stack trace', 'Integer')
         });
+        say $out "Added new system preference 'ActionLogsTraceDepth'";
     },
-}
+};
