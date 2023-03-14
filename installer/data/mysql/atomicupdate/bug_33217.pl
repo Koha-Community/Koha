4627f1a8222040b7bf9c22eccdc16f8c145b8603
Nick Clemens <nick@bywatersolutions.com>
2023-03-14 15:12:32 +0000

    Bug 33217: DB update
    Signed-off-by: Andrew Fuerste-Henry <andrewfh@dubcolib.org>
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

---
+++ installer/data/mysql/atomicupdate/bug_33217.pl
@@ -0,0 +1,20 @@
+use Modern::Perl;
+
+return {
+    bug_number  => "33217",
+    description => "Add option to specify sorting for author links",
+    up          => sub {
+        my ($args) = @_;
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
+
+        # Do you stuffs here
+        $dbh->do(
+            q{
+            INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type` ) VALUES
+            ('AuthorLinkSortBy','default','call_number|pubdate|acqdate|title','Specify the default field used for sorting when click author links','Choice'),
+            ('AuthorLinkSortOrder','asc','asc|dsc|az|za','Specify the default sort order for author links','Choice')
+        }
+        );
+        say $out "Added new system preferences 'AuthorLinkSortBy' 'AuthorLinkSortOrder'";
+    },
+};
