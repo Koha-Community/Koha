eec4921552411817acba4588694ed7511c604e0d
Fridolin Somers <fridolin.somers@biblibre.com>
2023-03-21 22:54:17 -1000

    Bug 33300: Wrong system preference name AutomaticWrongTransfer
    Bug 29792 added a DBRev with new system preference 'AutomaticConfirmTransfer'.
    
    But name in installer/data/mysql/mandatory/sysprefs.sql is wrong 'AutomaticWrongTransfer'
    
    Test plan :
    Create a new database and check there is in table systempreferences an
    entry with variable='AutomaticConfirmTransfer'
    
    Signed-off-by: David Nind <david@davidnind.com>
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    
    Bug 33300: (follow-up) DBRev for existing databases
    
    Test plan :
    1.1) Start from a Koha 22.05
    1.2) Upgrade to master
    => Check the upgrade says :
       Wrong system preference 'AutomaticWrongTransfer' renamed 'AutomaticConfirmTransfer'
    2.1) Start from a Koha 22.05
    2.2) Upgrade to 22.11
    2.3) Via interface change system preference 'AutomaticConfirmTransfer' and save
    2.4) Upgrade to master
    => Check the upgrade says :
       Wrong system preference 'AutomaticWrongTransfer' deleted
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- installer/data/mysql/mandatory/sysprefs.sql
+++ installer/data/mysql/mandatory/sysprefs.sql
@@ -84,7 +84,7 @@ INSERT INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `
 ('AutoLocation','0',NULL,'If ON, IP authentication is enabled, blocking access to the staff interface from unauthorized IP addresses','YesNo'),
 ('AutomaticItemReturn','1',NULL,'If ON, Koha will automatically set up a transfer of this item to its homebranch','YesNo'),
 ('AutoLinkBiblios','0',NULL,'If enabled, link biblio to authorities on creation and edit','YesNo'),
-('AutomaticWrongTransfer','0',NULL,'Defines whether transfers should be automatically confirmed at checkin if modal dismissed','YesNo'),
+('AutomaticConfirmTransfer','0',NULL,'Defines whether transfers should be automatically confirmed at checkin if modal dismissed','YesNo'),
 ('autoMemberNum','0','','If ON, patron number is auto-calculated','YesNo'),
 ('AutoRemoveOverduesRestrictions','0',NULL,'Defines whether an OVERDUES debarment should be lifted automatically if all overdue items are returned by the patron.','YesNo'),
 ('AutoRenewalNotices','cron','cron|preferences|never','How should Koha determine whether to end autorenewal notices','Choice'),
