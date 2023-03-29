edd6d7d066167d632573d177285f659711113a9c
Fridolin Somers <fridolin.somers@biblibre.com>
2023-03-28 21:56:51 -1000

    Bug 21330: (follow-up) Fix new preference text and place
    Atomic updated fixed : intranet => OPAC
    Changed to be more like the one adding AuthorityXSLTOpacResultsDisplay
    
    New preference moved to opac.pref
    
    Signed-off-by: Thibault <thibault.keromnes@univ-paris8.fr>
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- installer/data/mysql/atomicupdate/bug-21330.pl
+++ installer/data/mysql/atomicupdate/bug-21330.pl
@@ -2,14 +2,16 @@ use Modern::Perl;
 
 return {
     bug_number => '21330',
-    description => 'Add syspref AuthorityXSLTOpacDetailsDisplay',
+    description => 'Add XSLT for authority details view in OPAC',
     up => sub {
         my ($args) = @_;
         my ($dbh, $out) = @$args{qw(dbh out)};
 
         $dbh->do(q{
             INSERT IGNORE INTO systempreferences (`variable`, `value`, `options`, `explanation`, `type`)
-            VALUES ('AuthorityXSLTOpacDetailsDisplay','','','Enable XSL stylesheet control over authority results page display on intranet','Free')
+            VALUES ('AuthorityXSLTOpacDetailsDisplay','','','Enable XSL stylesheet control over authority details page in the OPAC','Free')
         });
+
+        say $out "Added new system preference 'AuthorityXSLTOpacDetailsDisplay'";
     },
 };
