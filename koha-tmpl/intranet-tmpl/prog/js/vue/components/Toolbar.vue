c7d3cad2860456ef0ce68ce654e29e9f82d952ae
Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>
2023-04-05 12:02:22 +0000

    Bug 33417: Create standard toolbar component
    This commit introduces a standard Toolbar component that can be used
    throughout ERM
    
    Test plan:
    1) Apply patch
    2) Navigate to Agreements and click the "Add agreement" button - this
       should work as normal
    3) Navigate to Licenses and repeat
    4) Navigate to Local packages and repeat
    5) Navigate to local titles - there should be two buttons, one to add and
       one to import. These should work as normal
    
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    (cherry picked from commit 09666ecf4e2eeabfc13ddc4d0b43311e35dbe0b5)
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

---
+++ koha-tmpl/intranet-tmpl/prog/js/vue/components/Toolbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="toolbar" class="btn-toolbar">
+        <router-link
+            v-for="(option, index) in options"
+            :key="index"
+            :to="{ name: option.to }"
+            class="btn btn-default"
+            ><font-awesome-icon icon="plus" />
+            {{ $__(`${option.button_title}`) }}</router-link
+        >
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        options: Array,
+    },
+    name: "Toolbar",
+}
+</script>
