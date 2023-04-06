d5e12d95c25875a28e0f8823ec39a8ca670463d3
Tomas Cohen Arazi <tomascohen@theke.io>
2023-04-06 12:50:40 -0300

    Bug 33368: DBRev 22.12.00.019
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    (cherry picked from commit ba601ca37fe45dacaa1ed34b783a7d239058724e)
    Signed-off-by: Jacob O'Mara <jacobomara901@gmail.com>

--- Koha.pm
+++ Koha.pm
@@ -29,7 +29,7 @@ use vars qw{ $VERSION };
 # - #4 : the developer version. The 4th number is the database subversion.
 #        used by developers when the database changes. updatedatabase take care of the changes itself
 #        and is automatically called by Auth.pm when needed.
-$VERSION = "22.11.04.001";
+$VERSION = "22.11.04.002";
 
 sub version {
     return $VERSION;
