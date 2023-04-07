d08320629190ff8c43a37b2e49122acdd25a107a
Lucas Gass <lucas@bywatersolutions.com>
2023-04-07 16:30:36 +0000

    Bug 16122: Add localuse column to items and deleteditems
    Signed-off-by: Christopher Brannon <cbrannon@cdalibrary.org>
    Signed-off-by: Emily Lamancusa <emily.lamancusa@montgomerycountymd.gov>
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    Signed-off-by: Andrew Fuerste-Henry <andrewfh@dubcolib.org>
    Signed-off-by: Andrew Fuerste Henry <andrewfh@dubcolib.org>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

---
+++ installer/data/mysql/atomicupdate/bug_16122_atomicupdate.pl
@@ -0,0 +1,31 @@
+use Modern::Perl;
+return {
+    bug_number  => "16122",
+    description => "Add localuse column to items table and deleted items table",
+    up          => sub {
+        my ($args) = @_;
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
+        if ( !column_exists( 'items', 'localuse' ) ) {
+            $dbh->do(
+                q{
+                ALTER TABLE items
+                ADD COLUMN localuse smallint(6) NULL DEFAULT NULL
+                COMMENT "item's local use count"
+                AFTER renewals
+            }
+            );
+            say $out "Added column 'items.localuse'";
+        }
+        if ( !column_exists( 'deleteditems', 'localuse' ) ) {
+            $dbh->do(
+                q{
+                ALTER TABLE deleteditems
+                ADD COLUMN localuse smallint(6) NULL DEFAULT NULL
+                COMMENT "deleteditems local use count"
+                AFTER renewals
+            }
+            );
+            say $out "Added column 'deleteditems.localuse'";
+        }
+    },
+    }
