8bbe2eac8ef05b2682ce7ad2f4216734fc6ac2a6
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2023-04-07 14:51:50 +0200

    Bug 30708: Koha controller script and template changes
    Sponsored-by: BULAC - http://www.bulac.fr/
    
    Signed-off-by: BULAC - http://www.bulac.fr/
    Signed-off-by: Heather Hernandez <heather_hernandez@nps.gov>
    Signed-off-by: Laurence Rault <laurence.rault@biblibre.com>
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

---
+++ preservation/home.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/perl
+
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+use CGI qw ( -utf8 );
+use C4::Auth qw( get_template_and_user );
+use C4::Output qw( output_html_with_http_headers );
+
+use Koha::Database::Columns;
+
+my $query = CGI->new;
+my ( $template, $loggedinuser, $cookie, $userflags ) = get_template_and_user(
+    {
+        template_name => 'preservation/home.tt',
+        query         => $query,
+        type          => 'intranet',
+        flagsrequired => { preservation => '*' },
+    }
+);
+
+my $columns = Koha::Database::Columns::columns;
+$template->param(
+    db_columns => {
+        map {
+            my $table = $_;
+            map { ( $table . '.' . $_ => $columns->{$table}->{$_} ) }
+              keys %{ $columns->{$table} }
+        } qw( biblio biblioitems items )
+    },
+    api_mappings => {
+        items => Koha::Item->to_api_mapping,
+        biblioitems => Koha::Biblioitem->to_api_mapping,
+        biblio => Koha::Biblio->to_api_mapping,
+    },
+);
+
+output_html_with_http_headers $query, $cookie, $template->output;
