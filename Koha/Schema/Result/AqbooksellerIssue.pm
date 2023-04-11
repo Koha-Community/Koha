7b68f1afcaeda3c3b99f6e3aab2b018b3639c931
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2023-04-11 22:38:45 +0200

    Bug 33105: DBIC specific
    Signed-off-by: Jonathan Field <jonathan.fieeld@ptfs-europe.com>
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- Koha/Schema/Result/AqbooksellerIssue.pm
+++ Koha/Schema/Result/AqbooksellerIssue.pm
@@ -120,6 +120,11 @@ __PACKAGE__->belongs_to(
 # Created by DBIx::Class::Schema::Loader v0.07049 @ 2023-06-30 09:54:35
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:Ljn21/DFV5QvS5z3kDrBwQ
 
+sub koha_object_class {
+    'Koha::Acquisition::Bookseller::Issue';
+}
+sub koha_objects_class {
+    'Koha::Acquisition::Bookseller::Issues';
+}
 
-# You can replace this text with custom code or comments, and it will be preserved on regeneration
 1;
