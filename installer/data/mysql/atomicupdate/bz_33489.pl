1faf3bcf2a59f8e6a48a2b101d78d35843bf41dd
Nick Clemens <nick@bywatersolutions.com>
2023-04-11 17:33:55 +0000

    Bug 33489: Add indices to default patron search fields
    This patch adds indices to the borrowers table to match the default
    search fields for patrons.
    
    To test:
    1 - Apply patch
    2 - Update database
    3 - Ensure patron searching works as before
    
    Signed-off-by: David Nind <david@davidnind.com>
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

---
+++ installer/data/mysql/atomicupdate/bz_33489.pl
@@ -0,0 +1,23 @@
+use Modern::Perl;
+
+return {
+    bug_number => "33489",
+    description => "Add indices to default patron search fields",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+        # Do you stuffs here
+        unless ( index_exists( 'borrowers', 'cardnumber_idx' ) ) {
+            $dbh->do(q{CREATE INDEX cardnumber_idx ON borrowers ( cardnumber )});
+            say $out "Added new index on borrowers.cardnumber";
+        }
+        unless ( index_exists( 'borrowers', 'userid_idx' ) ) {
+            $dbh->do(q{CREATE INDEX userid_idx ON borrowers ( userid )});
+            say $out "Added new index on borrowers.userid";
+        }
+        unless ( index_exists( 'borrowers', 'middle_name_idx' ) ) {
+            $dbh->do(q{CREATE INDEX middle_name_idx ON borrowers ( middle_name )});
+            say $out "Added new index on borrowers.midddle_name";
+        }
+    },
+};
