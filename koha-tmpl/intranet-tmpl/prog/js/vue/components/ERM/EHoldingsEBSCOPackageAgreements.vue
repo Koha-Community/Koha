2fa604e5339e6f008b406a82c490412038ec3193
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2023-04-11 21:39:02 +0200

    Bug 33491: Fix UI issues in EHoldingsEBSCOPackageAgreements
    Signed-off-by: Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    (cherry picked from commit a0ec93d84f8b00e7b8b583d9fcbe7a564cba2a42)
    Signed-off-by: Jacob O'Mara <jacobomara901@gmail.com>

--- koha-tmpl/intranet-tmpl/prog/js/vue/components/ERM/EHoldingsEBSCOPackageAgreements.vue
+++ koha-tmpl/intranet-tmpl/prog/js/vue/components/ERM/EHoldingsEBSCOPackageAgreements.vue
@@ -126,7 +126,7 @@ export default {
     name: "EHoldingsEBSCOPackageAgreements",
 }
 </script>
-<style scoped>
+<style>
 .modal {
     position: fixed;
     z-index: 9998;
@@ -144,5 +144,17 @@ export default {
     box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
     transition: all 0.3s ease;
     font-family: Helvetica, Arial, sans-serif;
+}
+</style>
+
+<style>
+#agreements_list table {
+    display: table;
+}
+.filters label {
+    float: none !important;
+}
+.filters input[type="checkbox"] {
+    margin-left: 0 !important;
 }
 </style>
