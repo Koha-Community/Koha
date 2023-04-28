f4abf0393a78fab1c3bf124d02a0bf865e39b2b3
Hammat Wele <hammat.wele@inlibro.com>
2023-04-28 22:21:12 +0000

    Bug 33317: Add system preference to set meta robots for the OPAC
    Websites must have a robots meta tag to improve search engine crawling,
    so we could tell search engines to not index OPAC pages for example.
    
    To test:
    
    1) Look at opac-main source and see that meta name=robots is missing.
    2) Apply patch.
    3) Run ./installer/data/mysql/updatedatabase.pl
    4) Go to system preferences > OPAC and add some directives to
       OpacMetaRobots (ex: noindex,nofollow).
    5) Look at opac-main source and confirm that meta name=robots now has
       content equal to the text set in the OpacMetaRobots system
       preference.
    6) This should Prevent search engines from indexing Opac pages (if
       OpacMetaRobots is set to noindex)
    
    Signed-off-by: Owen Leonard <oleonard@myacpl.org>
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

---
+++ installer/data/mysql/atomicupdate/bug_33317.pl
@@ -0,0 +1,14 @@
+use Modern::Perl;
+
+return {
+    bug_number => "33317",
+    description => "Add new system preference OpacMetaRobots",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+
+        $dbh->do(q{INSERT IGNORE INTO systempreferences (variable, value, options, explanation, type) VALUES ('OpacMetaRobots','','','Improve search engine crawling.', 'Textarea') });
+
+        say $out "Added system preference 'OpacMetaRobots'";
+    },
+};
