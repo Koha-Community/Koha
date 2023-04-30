c2ae16ac876cc275a5a6c9e7a5e556370b134e9a
Aleisha Amohia <aleishaamohia@hotmail.com>
2023-04-30 22:20:23 +0000

    Bug 8367: Add holds_pickup_period fallback circulation rule
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    Signed-off-by: Kelly <kelly@bywatersolutions.com>
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

---
+++ installer/data/mysql/atomicupdate/bug_8367-add_holds_pickup_period_circrule.pl
@@ -0,0 +1,18 @@
+use Modern::Perl;
+
+return {
+    bug_number => "8367",
+    description => "Set hold pickup period circulation rule",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+
+        $dbh->do(q{
+            INSERT IGNORE INTO circulation_rules ( branchcode, categorycode, itemtype, rule_name, rule_value )
+            SELECT u.* FROM (SELECT NULL as branchcode, NULL as categorycode, NULL as itemtype, 'holds_pickup_period' as rule_name, '' as rule_value) u
+            WHERE NOT EXISTS ( SELECT rule_name FROM circulation_rules where rule_name = 'holds_pickup_period' )
+        });
+
+        say $out "Added default circulation rule for holds_pickup_period";
+    },
+};
