b6ac9e6b10cfce27eac639c7d5d8776ae8e52639
Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
2023-05-04 06:41:19 +0000

    Bug 33671: Fix for 30472 - Remove null-guarantors
    These guarantors cannot be added. They are of no use.
    
    NOTE FOR QA/RM: I do not think that it is needed to add a new
    db rev for this change since it happens seldom. But if you think
    that it is, just copy it to atomicupdate/change bugno.
    
    Test plan:
    [1] Check your table definition and modify it:
        alter table borrower_relationships modify column `guarantor_id` int(11) NULL;
    [2] Add an empty guarantor:
        insert into borrower_relationships (guarantee_id,relationship) values (SOME_PATRON_ID,'father');
    [3] Run the dbrev and verify that the inserted record has been removed.
    [4] Check table definition with 'show create table borrower_relationships'.
        You should see NOT NULL with guarantor_id.
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    (cherry picked from commit 1dee35d412f9aada8ae58d72a7fa1f4ff88dde7c)
    Signed-off-by: Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>

--- installer/data/mysql/db_revs/220600048.pl
+++ installer/data/mysql/db_revs/220600048.pl
@@ -6,6 +6,11 @@ return {
     up => sub {
         my ($args) = @_;
         my ($dbh, $out) = @$args{qw(dbh out)};
+
+        # Delete 'empty' guarantors. No longer possible to add them via interface. Have no use.
+        $dbh->do(q{
+DELETE FROM borrower_relationships WHERE guarantor_id IS NULL
+        });
         $dbh->do(q{
 ALTER TABLE borrower_relationships CHANGE COLUMN guarantor_id guarantor_id int(11) NOT NULL
         });
