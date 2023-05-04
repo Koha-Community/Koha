753e5a86d9b3498e75b81a22887385cb50423f9a
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2023-05-04 13:34:57 +0000

    Bug 33716: ILL Type Disclaimer - OPAC
    Update the way Availability is handled
    Add the new type disclaimer workflow operation
    after Availability
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- opac/opac-illrequests.pl
+++ opac/opac-illrequests.pl
@@ -25,12 +25,14 @@ use CGI qw ( -utf8 );
 use C4::Auth qw( get_template_and_user );
 use C4::Koha;
 use C4::Output qw( output_html_with_http_headers );
+use POSIX qw( strftime );
 
 use Koha::Illrequest::Config;
 use Koha::Illrequests;
 use Koha::Libraries;
 use Koha::Patrons;
-use Koha::Illrequest::Availability;
+use Koha::Illrequest::Workflow::Availability;
+use Koha::Illrequest::Workflow::TypeDisclaimer;
 
 my $query = CGI->new;
 
@@ -110,43 +112,32 @@ if ( $op eq 'list' ) {
         my $request = Koha::Illrequest->new
             ->load_backend($params->{backend});
 
-        # Does this backend enable us to insert an availability stage and should
-        # we? If not, proceed as normal.
-        if (
-            C4::Context->preference("ILLCheckAvailability") &&
-            $request->_backend_capability(
-                'should_display_availability',
-                $params
-            ) &&
-            # If the user has elected to continue with the request despite
-            # having viewed availability info, this flag will be set
-            !$params->{checked_availability}
-        ) {
-            # Establish which of the installed availability providers
-            # can service our metadata, if so, jump in
-            my $availability = Koha::Illrequest::Availability->new($params);
-            my $services = $availability->get_services({
-                ui_context => 'opac'
-            });
-            if (scalar @{$services} > 0) {
-                # Modify our method so we use the correct part of the
-                # template
-                $op = 'availability';
-                # Prepare the metadata we're sending them
-                my $metadata = $availability->prep_metadata($params);
-                $template->param(
-                    metadata        => $metadata,
-                    services_json   => encode_json($services),
-                    services        => $services,
-                    illrequestsview => 1,
-                    message         => $params->{message},
-                    method          => $op,
-                    whole           => $params
-                );
-                output_html_with_http_headers $query, $cookie,
-                    $template->output, undef, { force_no_caching => 1 };
-                exit;
-            }
+        # Before request creation operations - Preparation
+        my $availability =
+          Koha::Illrequest::Workflow::Availability->new( $params, 'opac' );
+        my $type_disclaimer =
+          Koha::Illrequest::Workflow::TypeDisclaimer->new( $params, 'opac' );
+
+        # ILLCheckAvailability operation
+        if ($availability->show_availability($request)) {
+            $op = 'availability';
+            $template->param(
+                $availability->availability_template_params($params)
+            );
+            output_html_with_http_headers $query, $cookie,
+                $template->output, undef,
+                { force_no_caching => 1 };
+            exit;
+        # ILLModuleDisclaimerByType operation
+        } elsif ( $type_disclaimer->show_type_disclaimer($request)) {
+            $op = 'typedisclaimer';
+            $template->param(
+                $type_disclaimer->type_disclaimer_template_params($params)
+            );
+            output_html_with_http_headers $query, $cookie,
+                $template->output, undef,
+                { force_no_caching => 1 };
+            exit;
         }
 
         $params->{cardnumber} = Koha::Patrons->find({
@@ -154,6 +145,14 @@ if ( $op eq 'list' ) {
         })->cardnumber;
         $params->{opac} = 1;
         my $backend_result = $request->backend_create($params);
+
+        # After creation actions
+        if ( $params->{type_disclaimer_submitted} ) {
+            $type_disclaimer->after_request_created( $params, $request );
+            print $query->redirect('/cgi-bin/koha/opac-illrequests.pl?message=2');
+            exit;
+        }
+
         if ($backend_result->{stage} eq 'copyrightclearance') {
             $template->param(
                 stage       => $backend_result->{stage},
