3c24667a518810aa8c56fd70f06392314e373f58
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2023-05-05 15:08:24 +0000

    Bug 32911: (follow-up) Only move config value from koha-conf.xml if it exists
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- installer/data/mysql/atomicupdate/bug_32911.pl
+++ installer/data/mysql/atomicupdate/bug_32911.pl
@@ -10,11 +10,14 @@ return {
         my $xml_config = C4::Context->config("interlibrary_loans");
         my $existing_partner_code = $xml_config->{partner_code};
 
-        $dbh->do(qq{
-            INSERT IGNORE INTO systempreferences (variable,value,explanation,options,type)
-            VALUES ('ILLPartnerCode', '$existing_partner_code', NULL, 'Patrons from this patron category will be used as partners to place ILL requests with', 'free');
-        });
-
-        say $out "Moved value of partner_code in koha-conf.xml into new system preference 'ILLPartnerCode'";
+        if ( $existing_partner_code ) {
+            $dbh->do(
+                qq{
+                    INSERT IGNORE INTO systempreferences (variable,value,explanation,options,type)
+                    VALUES ('ILLPartnerCode', '$existing_partner_code', NULL, 'Patrons from this patron category will be used as partners to place ILL requests with', 'free');
+                }
+            );
+            say $out "Moved value of partner_code in koha-conf.xml into new system preference 'ILLPartnerCode'";
+        }
     }
 };
