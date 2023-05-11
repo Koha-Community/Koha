8ad444a891de95dba5d0171c35f540157f0f3fa9
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2023-05-11 12:38:53 +0200

    Bug 33406: Handle ES search errors for authorities
    We are dealing ok with ES exceptions for biblio records search,
    catching them and raising them to the end user. But we don't for
    authorities, where we explode with an ugly 500.
    
    Test plan:
    Search for "(term_1*) AND (-) AND (term_2*)" in the authority search and
    notice that you don't get a 500 but an error instead saying that you
    should try again
    
    Signed-off-by: Pedro Amorim <pedro.amorim@ptfs-europe.com>
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    (cherry picked from commit 09b29d06da5fac2bd1315d849f1b9bf924576700)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>
    (cherry picked from commit b4187cf2abb7007d528585f59b723c10dc3b4480)
    Signed-off-by: Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>

--- authorities/authorities-home.pl
+++ authorities/authorities-home.pl
@@ -88,18 +88,6 @@ if ( $op eq "do_search" ) {
     my $resultsperpage = $query->param('resultsperpage') || 20;
     my $offset = ( $startfrom - 1 ) * $resultsperpage + 1;
 
-    my $builder = Koha::SearchEngine::QueryBuilder->new(
-        { index => $Koha::SearchEngine::AUTHORITIES_INDEX } );
-    my $searcher = Koha::SearchEngine::Search->new(
-        { index => $Koha::SearchEngine::AUTHORITIES_INDEX } );
-    my $search_query = $builder->build_authorities_query_compat(
-        [$marclist], [$and_or], [$excluding], [$operator],
-        [$value], $authtypecode, $orderby
-    );
-    my ( $results, $total ) = $searcher->search_auth_compat(
-        $search_query, $offset, $resultsperpage
-    );
-
     ( $template, $loggedinuser, $cookie ) = get_template_and_user(
         {
             template_name   => "authorities/searchresultlist.tt",
@@ -108,6 +96,27 @@ if ( $op eq "do_search" ) {
             flagsrequired   => { catalogue => 1 },
         }
     );
+
+    my $builder = Koha::SearchEngine::QueryBuilder->new(
+        { index => $Koha::SearchEngine::AUTHORITIES_INDEX } );
+    my $searcher = Koha::SearchEngine::Search->new(
+        { index => $Koha::SearchEngine::AUTHORITIES_INDEX } );
+
+    my $search_query = $builder->build_authorities_query_compat(
+        [$marclist], [$and_or], [$excluding], [$operator],
+        [$value], $authtypecode, $orderby
+    );
+    my ( $results, $total );
+    eval {
+        ( $results, $total ) = $searcher->search_auth_compat(
+            $search_query, $offset, $resultsperpage
+        );
+    };
+    if ($@) {
+        my $query_error = q{};
+        $query_error .= $@ if $@;
+        $template->param(query_error => $query_error);
+    }
 
     $template->param( search_query => $search_query ) if C4::Context->preference('DumpSearchQueryTemplate');
     $template->param(
