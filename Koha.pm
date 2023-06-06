9d327f5496234d4fa0d091a0b2d3848d290aabb2
Tomas Cohen Arazi <tomascohen@theke.io>
2023-06-06 15:26:52 -0300

    Bug 21983: DBRev 22.11.00.005
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    (cherry picked from commit aa3218855316923f10da873528dd300b58ed0e3a)

--- Koha.pm
+++ Koha.pm
@@ -30,7 +30,7 @@ use vars qw{ $VERSION };
 #        used by developers when the database changes. updatedatabase take care of the changes itself
 #        and is automatically called by Auth.pm when needed.
 
-$VERSION = "22.11.06.004";
+$VERSION = "22.11.06.005";
 sub version {
     return $VERSION;
 }
