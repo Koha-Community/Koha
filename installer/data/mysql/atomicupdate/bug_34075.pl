6247cd9b5efec71ab61828f77c42a42bfadf25c0
Owen Leonard <oleonard@myacpl.org>
2023-06-23 13:03:40 +0000

    Bug 34075: (follow-up) Fixes for QA tool errors
    - Added missing filter to template variable
    - Correct spelling in atomicupdate
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- installer/data/mysql/atomicupdate/bug_34075.pl
+++ installer/data/mysql/atomicupdate/bug_34075.pl
@@ -8,7 +8,7 @@ return {
         my ($dbh, $out) = @$args{qw(dbh out)};
         $dbh->do(q{
             INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type` ) VALUES
-            ('DefaultAuthorityTab','0','0|1|2|3|4|5|6|7|8|9','Default tab to shwo when displaying authorities','Choice')
+            ('DefaultAuthorityTab','0','0|1|2|3|4|5|6|7|8|9','Default tab to show when displaying authorities','Choice')
         });
         say $out "Added new system preference 'DefaultAuthorityTab'";
     },
