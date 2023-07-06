9603f118837bb8171081da6f892761f2d341726f
Sam Lau <samalau@gmail.com>
2023-07-06 19:59:50 +0000

    Bug 34188: Require Library Branch Selection when Logging in
    This patch adds the ability to force staff to select a library when
    logging into the staff interface. This is done via a new system
    preference: 'ForcedLibrarySelection'
    
    To test:
    1) Apply patch, restart_all, and updatedatabase
    2) Log out of the staff interface. Notice the login form looks the same
       and the "Library:" dropdown has 'My Library" selected as default. Log
       back into the staff interface.
    3) In Administration, search for the system preference
       "ForcedLibrarySelection". Set it to 'Force' and press save.
    4) Log out of the staff interface. Notice that this time, the "Library:"
       dropdown is required and has a blank selection as the default.
    5) Fill in the username and password but do not select a library. Click
       'Log in'
    6) Notice you cannot log in and are asked to 'Please select an item in
       the list'
    7) Select a library from the drop down and click 'Log in'
    8) Notice the login was successful and you were logged in to the library
       you selected.
    9) Try loggin in with some other libraries to verify it works as
       expected
    10) Sign-off :)
    
    Signed-off-by: Owen Leonard <oleonard@myacpl.org>
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

---
+++ installer/data/mysql/atomicupdate/bug_34188.pl
@@ -0,0 +1,17 @@
+use Modern::Perl;
+
+return {
+    bug_number => "34188",
+    description => "Force staff to select a library when logging into the staff interface.",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+        $dbh->do(q{
+                INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type` )
+                VALUES ('ForcedLibrarySelection', '0', NULL,'Force staff to select a library when logging into the staff interface.', 'YesNo')});
+
+        # sysprefs
+        say $out "Added new system preference 'ForcedLibrarySelection'";
+
+    },
+};
