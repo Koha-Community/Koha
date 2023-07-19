c2383c03fdd69e20a7d8071f0a0346847743858f
Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>
2023-07-19 09:02:01 +0000

    Bug 34029: (RMAINT fix) Update DBIC file
    Commit fb202cc3eaa12b5764fd91e2320ab82c7739bc72 was missed when cherry-picking and xt/author/podcorrectness.t was failing. This commit adds the updated DBIC file that fixes the test

--- Koha/Schema/Result/Deletedbiblioitem.pm
+++ Koha/Schema/Result/Deletedbiblioitem.pm
@@ -164,7 +164,7 @@ material size (MARC21 300$c)
   data_type: 'text'
   is_nullable: 1
 
-publication place (MARC21 260$a and 264Â§a)
+publication place (MARC21 260$a and 264$a)
 
 =head2 lccn
 
@@ -314,8 +314,8 @@ __PACKAGE__->add_columns(
 __PACKAGE__->set_primary_key("biblioitemnumber");
 
 
-# Created by DBIx::Class::Schema::Loader v0.07049 @ 2023-06-26 12:47:08
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:GiuL+3o/fRyTkale3lq2xQ
+# Created by DBIx::Class::Schema::Loader v0.07049 @ 2023-07-19 08:58:58
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum://St+Sqs2eNxnSuMIijGeQ
 
 sub koha_objects_class {
     'Koha::Old::Biblioitems';
