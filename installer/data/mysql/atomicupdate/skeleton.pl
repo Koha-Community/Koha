9919d7afd492f844012d8ae12a1ea4155e2edca2
Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
2023-07-20 11:49:37 +0000

    Bug 34321: Tidy skeleton.pl
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- installer/data/mysql/atomicupdate/skeleton.pl
+++ installer/data/mysql/atomicupdate/skeleton.pl
@@ -1,23 +1,28 @@
 use Modern::Perl;
 
 return {
-    bug_number => "BUG_NUMBER",
+    bug_number  => "BUG_NUMBER",
     description => "A single line description",
-    up => sub {
+    up          => sub {
         my ($args) = @_;
-        my ($dbh, $out) = @$args{qw(dbh out)};
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
+
         # Do you stuffs here
         $dbh->do(q{});
+
         # Print useful stuff here
         # tables
         say $out "Added new table 'XXX'";
         say $out "Added column 'XXX.YYY'";
+
         # sysprefs
         say $out "Added new system preference 'XXX'";
         say $out "Updated system preference 'XXX'";
         say $out "Removed system preference 'XXX'";
+
         # permissions
         say $out "Added new permission 'XXX'";
+
         # letters
         say $out "Added new letter 'XXX' (TRANSPORT)";
     },
