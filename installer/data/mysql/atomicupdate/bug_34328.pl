26854910bb2fb50daa74a46fa32004dba564f04e
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2023-07-20 16:31:40 +0100

    Bug 34328: Add Scottish Gaelic to languages
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    (cherry picked from commit 44aa68c3dfa2b067b317cdb1c6578e517e115d46)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

---
+++ installer/data/mysql/atomicupdate/bug_34328.pl
@@ -0,0 +1,46 @@
+use Modern::Perl;
+
+return {
+    bug_number  => "34328",
+    description => "Add Scottish Gaelic to recognised languages",
+    up          => sub {
+        my ($args) = @_;
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
+        $dbh->do(q{
+            INSERT INTO language_rfc4646_to_iso639(rfc4646_subtag,iso639_2_code)
+            VALUES ( 'gd','gla' )
+        });
+
+        $dbh->do(q{
+            INSERT INTO language_subtag_registry( subtag, type, description, added)
+            VALUES ( 'gd', 'language', 'Scottish Gaelic', NOW() )
+        });
+
+        $dbh->do(q{
+            INSERT INTO language_descriptions(subtag, type, lang, description)
+            VALUES ( 'gd', 'language', 'en', 'Scottish Gaelic')
+        });
+
+        $dbh->do(q{
+            INSERT INTO language_descriptions(subtag, type, lang, description)
+            VALUES ( 'gd', 'language', 'gd', 'Gàidhlig')
+        });
+
+        $dbh->do(q{
+            INSERT INTO language_descriptions(subtag, type, lang, description)
+            VALUES ( 'gd', 'language', 'fr', 'Gaélique écossais')
+        });
+
+        $dbh->do(q{
+            INSERT INTO language_descriptions(subtag, type, lang, description)
+            VALUES ( 'gd', 'language', 'de', 'Schottisch-Gälisch')
+        });
+
+        $dbh->do(q{
+            INSERT INTO language_descriptions(subtag, type, lang, description)
+            VALUES ( 'gd', 'language', 'pl', 'Język szkocki gaelicki')
+        });
+
+        say $out "Added new language 'Scottish Gaelic'";
+    },
+};
