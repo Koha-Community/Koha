b5e1b2137fddf6a0ad854ffdb10c103a00c4f7fd
Tomas Cohen Arazi <tomascohen@theke.io>
2023-08-07 15:13:46 -0300

    Bug 33117: Make dbrev idempotent
    QA didn't spot the issue, nor I. So fixing now.
    
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    (cherry picked from commit 951e67a1b3dd1f26eddeaecd2dc63991f9667927)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>
    (cherry picked from commit caa46019d07d89474feee02c7a2f8961e7414283)
    Signed-off-by: Pedro Amorim <pedro.amorim@ptfs-europe.com>

--- installer/data/mysql/db_revs/221107002.pl
+++ installer/data/mysql/db_revs/221107002.pl
@@ -8,7 +8,7 @@ return {
         my ( $dbh, $out ) = @$args{qw(dbh out)};
 
         $dbh->do(q{
-            INSERT INTO systempreferences (`variable`,`value`,`explanation`,`options`,`type`)
+            INSERT IGNORE INTO systempreferences (`variable`,`value`,`explanation`,`options`,`type`)
             VALUES ('DefaultPatronSearchMethod','starts_with','Allows staff to set a default method when searching for patrons with autocomplete','starts_with|contains','Choice');
         });
 
