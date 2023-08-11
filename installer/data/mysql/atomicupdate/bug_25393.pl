6d547f65b3c38549991e70a4b3f59b4ba3e5fa0b
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2023-08-11 14:16:51 +0000

    Bug 25393: atomicupdate file
    Signed-off-by: Andrew Fuerste-Henry <andrewfh@dubcolib.org>
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

---
+++ installer/data/mysql/atomicupdate/bug_25393.pl
@@ -0,0 +1,40 @@
+use Modern::Perl;
+
+return {
+    bug_number  => "25393",
+    description => "Create separate 'no automatic renewal before' rule",
+    up => sub {
+        my ($args) = @_;
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
+
+        my $rules = $dbh->selectall_arrayref(
+            q|SELECT * FROM circulation_rules WHERE rule_name = "noautorenewalbefore"|,
+            { Slice => {} }
+        );
+
+        if(!scalar @{$rules}){
+            my $existing_rules = $dbh->selectall_arrayref(
+                q|SELECT * FROM circulation_rules WHERE rule_name = "norenewalbefore"|,
+                { Slice => {} }
+            );
+
+            my $insert_sth = $dbh->prepare(
+                q{INSERT INTO circulation_rules ( branchcode, categorycode, itemtype, rule_name, rule_value ) VALUES (?, ?, ?, ?, ?)}
+            );
+
+            for my $existing_rule ( @{$existing_rules} ) {
+                $insert_sth->execute(
+                    $existing_rule->{branchcode},
+                    $existing_rule->{categorycode},
+                    $existing_rule->{itemtype},
+                    'noautorenewalbefore',
+                    $existing_rule->{rule_value}
+                );
+            }
+            say $out "Bug 25939: New circulation rule 'noautorenewalbefore' has been added. Defaulting value to 'norenewalbefore'.";
+        }else{
+            say $out "Bug 25939: Circulation rule 'noautorenewalbefore' found. Skipping update.";
+        }
+
+        },
+};
