3e606c4df1d9ab5b8ea609ae2165ef20285f9df8
Victor Grousset/tuxayo <victor@tuxayo.net>
2023-08-15 08:32:27 +0200

    Bug 34494: (QA follow-up) fiks spelin
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- installer/data/mysql/atomicupdate/fix_tmp_holdsqueue.pl
+++ installer/data/mysql/atomicupdate/fix_tmp_holdsqueue.pl
@@ -2,7 +2,7 @@ use Modern::Perl;
 
 return {
     bug_number  => "34494",
-    description => "Fix tmp_holdsqueue for MySQL compatiability",
+    description => "Fix tmp_holdsqueue for MySQL compatibility",
     up          => sub {
         my ($args) = @_;
         my ( $dbh, $out ) = @$args{qw(dbh out)};
