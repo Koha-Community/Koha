cec91b3b270c455ef27e3eec94b947dc06ab4e6f
Kyle M Hall <kyle@bywatersolutions.com>
2023-09-11 13:44:00 -0400

    Bug 8838: Replace the atomic update boilerplate
    Signed-off-by: Emily Lamancusa <emily.lamancusa@montgomerycountymd.gov>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- installer/data/mysql/atomicupdate/bug_8838.pl
+++ installer/data/mysql/atomicupdate/bug_8838.pl
@@ -20,18 +20,6 @@ return {
 
         # Print useful stuff here
         # tables
-        say $out "Added new table 'XXX'";
-        say $out "Added column 'XXX.YYY'";
-
-        # sysprefs
-        say $out "Added new system preference 'XXX'";
-        say $out "Updated system preference 'XXX'";
-        say $out "Removed system preference 'XXX'";
-
-        # permissions
-        say $out "Added new permission 'XXX'";
-
-        # letters
-        say $out "Added new letter 'XXX' (TRANSPORT)";
+        say $out "Added new notice 'HOLDDGST'";
     },
 };
