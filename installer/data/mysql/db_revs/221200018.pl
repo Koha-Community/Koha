d91ea53e901a5bacfb42aa3923e37186cb41c877
Emmi Takkinen <emmi.takkinen@koha-suomi.fi>
2023-09-21 11:49:12 +0300

    Bug 34720: Fix bug 30403 atomic update
    Bug 30403 had wrong syspref name in its
    atomic update file. This patch fixes this in
    db_rev 221200018.pl and adds new atomic
    update file.
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- installer/data/mysql/db_revs/221200018.pl
+++ installer/data/mysql/db_revs/221200018.pl
@@ -2,11 +2,11 @@ use Modern::Perl;
 
 return {
     bug_number => "30403",
-    description => "Add system preference UpdateNotForLoanStatusOnCheckin",
+    description => "Add system preference UpdateNotForLoanStatusOnCheckout",
     up => sub {
         my ($args) = @_;
         my ($dbh, $out) = @$args{qw(dbh out)};
         # Do you stuffs here
-        $dbh->do(q{INSERT IGNORE INTO systempreferences ( variable, value, options, explanation, type ) VALUES ('UpdateNotForLoanStatusOnCheckin', '', 'NULL', 'This is a list of value pairs. When an item is checked in, if the not for loan value on the left matches the items not for loan value it will be updated to the right-hand value. E.g. ''-1: 0'' will cause an item that was set to ''Ordered'' to now be available for loan. Each pair of values should be on a separate line.', 'Free')});
+        $dbh->do(q{INSERT IGNORE INTO systempreferences ( variable, value, options, explanation, type ) VALUES ('UpdateNotForLoanStatusOnCheckout', '', 'NULL', 'This is a list of value pairs. When an item is checked out, if the not for loan value on the left matches the items not for loan value it will be updated to the right-hand value. E.g. ''-1: 0'' will cause an item that was set to ''Ordered'' to now be available for loan. Each pair of values should be on a separate line.', 'Free')});
     },
 };
