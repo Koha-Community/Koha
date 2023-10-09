a0564496b9b4344d95ee1cdd2529b9e49c548209
Tomas Cohen Arazi <tomascohen@theke.io>
2023-10-09 16:30:41 -0300

    Bug 34657: DBRev 23.06.00.029
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- installer/data/mysql/db_revs/230600029.pl
+++ installer/data/mysql/db_revs/230600029.pl
@@ -2,12 +2,11 @@ use Modern::Perl;
 
 return {
     bug_number  => "34657",
-    description => "Update MARC frameworks to use renamed UNIMARC plugin",
+    description => "Update MARC frameworks to use renamed UNIMARC plugin (unimarc_field_123defg.pl)",
     up          => sub {
         my ($args) = @_;
         my ( $dbh, $out ) = @$args{qw(dbh out)};
 
-        # Do you stuffs here
         $dbh->do(q{
             UPDATE `marc_subfield_structure` SET `value_builder` = 'unimarc_field_123defg.pl' WHERE (`value_builder` = 'unimarc_field_123d.pl' OR `value_builder` = 'unimarc_field_123e.pl' OR `value_builder` = 'unimarc_field_123f.pl' OR `value_builder` = 'unimarc_field_123g.pl' );
         });
