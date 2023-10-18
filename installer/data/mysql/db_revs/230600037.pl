cb8228ff0a0956ebac314b001ebbd019f0b520e3
Tomas Cohen Arazi <tomascohen@theke.io>
2023-10-18 11:34:07 -0300

    Bug 31631: DBRev 23.06.00.037
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- installer/data/mysql/db_revs/230600037.pl
+++ installer/data/mysql/db_revs/230600037.pl
@@ -2,7 +2,7 @@ use Modern::Perl;
 
 return {
     bug_number  => "31631",
-    description => "Add new system preference CalculateFundValuesIncludingTax",
+    description => "Allow choosing for tax-exclusive values to be used for calculating fund values (spent, ordered)",
     up          => sub {
         my ($args) = @_;
         my ( $dbh, $out ) = @$args{qw(dbh out)};
@@ -10,5 +10,7 @@ return {
         $dbh->do(
             q{INSERT IGNORE INTO systempreferences (variable, value, options, explanation, type) VALUES ('CalculateFundValuesIncludingTax', '1', NULL, 'Include tax in the calculated fund values (spent, ordered) for all supplier configurations', 'YesNo')}
         );
+
+        say $out "Added new system preference 'CalculateFundValuesIncludingTax'";
     },
 };
