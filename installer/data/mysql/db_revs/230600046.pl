8a099dc4884497ea220a409daf86aed99b5605a0
Tomas Cohen Arazi <tomascohen@theke.io>
2023-10-23 11:28:02 -0300

    Bug 33547: DBRev 23.06.00.046
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- installer/data/mysql/db_revs/230600046.pl
+++ installer/data/mysql/db_revs/230600046.pl
@@ -1,17 +1,19 @@
 use Modern::Perl;
 
 return {
-    bug_number => "33547",
+    bug_number  => "33547",
     description => "Add a new notice template 'PRES_TRAIN_ITEM'",
-    up => sub {
+    up          => sub {
         my ($args) = @_;
-        my ($dbh, $out) = @$args{qw(dbh out)};
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
 
-        unless ( column_exists('preservation_processings', 'letter_code') ) {
-            $dbh->do(q{
+        unless ( column_exists( 'preservation_processings', 'letter_code' ) ) {
+            $dbh->do(
+                q{
                 ALTER TABLE preservation_processings
                 ADD COLUMN `letter_code` varchar(20) DEFAULT NULL COMMENT 'Foreign key to the letters table' AFTER `name`
-            });
+            }
+            );
         }
 
         my $notice_template = q{[%~ USE AuthorisedValues ~%]
@@ -32,11 +34,14 @@ Item number #[% train_item.user_train_item_id %]
     [% item_attribute.processing_attribute.name %]: [% value %]
 [% END %]};
 
-        $dbh->do(q{
+        $dbh->do(
+            q{
             INSERT IGNORE INTO letter
             (module,code,branchcode,name,is_html,title,content,message_transport_type,lang)
             VALUES
-            ('preservation','PRES_TRAIN_ITEM','','Train item slip',0,'Train item slip',?, 'print','default')}, undef, $notice_template);
+            ('preservation','PRES_TRAIN_ITEM','','Train item slip',0,'Train item slip',?, 'print','default')}, undef,
+            $notice_template
+        );
         say $out "Added new letter 'PRES_TRAIN_ITEM' (print)";
     },
 };
