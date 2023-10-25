8382cc9f281e4c12e0e4cf56bfc442506e3f4842
Katrin Fischer <katrin.fischer@bsz-bw.de>
2023-10-25 09:39:30 +0000

    Bug 20755: (QA follow-up) Add descriptions to system preferences
    Updates both the atomic update file and the sysprefs.sql with
    * explanations for each preference
    * type = "Free"
    
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- installer/data/mysql/atomicupdate/bug_20755-add_acquisitionsdefaultemailaddress_syspref.pl
+++ installer/data/mysql/atomicupdate/bug_20755-add_acquisitionsdefaultemailaddress_syspref.pl
@@ -9,28 +9,28 @@ return {
 
         $dbh->do(
             q{
-                INSERT IGNORE INTO systempreferences (variable, value, options, explanation, type) VALUES ('AcquisitionsDefaultEMailAddress', '', NULL, NULL, NULL);
+                INSERT IGNORE INTO systempreferences (variable, value, options, explanation, type) VALUES ('AcquisitionsDefaultEMailAddress', '', NULL, "Default email address  that acquisition notices are sent from.", 'Free');
             }
         );
         say $out "Added new system preference 'AcquisitionsDefaultEMailAddress'";
 
         $dbh->do(
             q{
-                INSERT IGNORE INTO systempreferences (variable, value, options, explanation, type) VALUES ('AcquisitionsDefaultReplyTo', '', NULL, NULL, NULL);
+                INSERT IGNORE INTO systempreferences (variable, value, options, explanation, type) VALUES ('AcquisitionsDefaultReplyTo', '', NULL, "Default email address used as reply-to for notices sent by the acquisitions module.", 'Free');
             }
         );
         say $out "Added new system preference 'AcquisitionsDefaultReplyTo'";
 
         $dbh->do(
             q{
-                INSERT IGNORE INTO systempreferences (variable, value, options, explanation, type) VALUES ('SerialsDefaultEMailAddress', '', NULL, NULL, NULL);
+                INSERT IGNORE INTO systempreferences (variable, value, options, explanation, type) VALUES ('SerialsDefaultEMailAddress', '', NULL, "Default email address that serials notices are sent from.", 'Free');
             }
         );
         say $out "Added new system preference 'SerialsDefaultEMailAddress'";
 
         $dbh->do(
             q{
-                INSERT IGNORE INTO systempreferences (variable, value, options, explanation, type) VALUES ('SerialsDefaultReplyTo', '', NULL, NULL, NULL);
+                INSERT IGNORE INTO systempreferences (variable, value, options, explanation, type) VALUES ('SerialsDefaultReplyTo', '', NULL, "Default email address used as reply-to for notices sent by the serials module.", 'Free');
             }
         );
         say $out "Added new system preference 'SerialsDefaultReplyTo'";
