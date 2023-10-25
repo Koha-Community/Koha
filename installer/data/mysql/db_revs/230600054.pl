4ab24451a337d964edead78cf332073ce1a5e0e3
Tomas Cohen Arazi <tomascohen@theke.io>
2023-10-25 11:43:32 -0300

    Bug 33887: DBRev 23.06.00.054
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- installer/data/mysql/db_revs/230600054.pl
+++ installer/data/mysql/db_revs/230600054.pl
@@ -7,14 +7,11 @@ return {
         my ($args) = @_;
         my ( $dbh, $out ) = @$args{qw(dbh out)};
 
-        # Do you stuffs here
         $dbh->do(
             q{
                 INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type` ) VALUES ('AutomaticCheckinAutoFill', '0', NULL,'Automatically fill the next hold with an automatic checkin cronjob.', 'YesNo') }
         );
 
-        # Print useful stuff here
-        # sysprefs
         say $out "Added new system preference 'AutomaticCheckinAutoFill'";
     },
 };
