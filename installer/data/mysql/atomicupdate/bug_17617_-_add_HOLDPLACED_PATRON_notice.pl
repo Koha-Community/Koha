b62699dff077035d6db225388d446ed569fbcd93
Aleisha Amohia <aleishaamohia@hotmail.com>
2023-10-26 02:37:01 +0000

    Bug 17617: Add HOLDPLACED_PATRON notice
    A HOLDPLACED notice exists and is used by the emailLibrarianWhenHoldIsPlaced function, but libraries may want to use different wording when emailing patrons.
    
    Signed-off-by: Kelly <kelly@bywatersolutions.com>
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

---
+++ installer/data/mysql/atomicupdate/bug_17617_-_add_HOLDPLACED_PATRON_notice.pl
@@ -0,0 +1,19 @@
+use Modern::Perl;
+
+return {
+    bug_number  => "17617",
+    description => "Notify patron when their hold has been placed",
+    up          => sub {
+        my ($args) = @_;
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
+
+        $dbh->do(
+            q{INSERT IGNORE INTO letter (module, code, branchcode, name, is_html, title, message_transport_type, lang, content) VALUES ("reserves", "HOLDPLACED_PATRON", "", "Hold is confirmed", 0, "Your hold on [% hold.biblio.title %] is confirmed", "email", "default", "Hello [% borrower.firstname %] [% borrower.surname %] ([% borrower.cardnumber %]),
+Your hold on [% hold.biblio.title %] ([% hold.biblio.id %]) has been confirmed.
+You will be notified by the library when your item is available for pickup.
+Thank you!")}
+        );
+
+        say $out "Added notice 'HOLDPLACED_PATRON'";
+    },
+};
