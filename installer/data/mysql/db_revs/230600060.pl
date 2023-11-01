66a87766a2974ad7e403441dbd61c500897b5804
Tomas Cohen Arazi <tomascohen@theke.io>
2023-11-01 17:56:34 -0300

    Bug 18203: DBRev 23.06.00.060
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- installer/data/mysql/db_revs/230600060.pl
+++ installer/data/mysql/db_revs/230600060.pl
@@ -1,17 +1,21 @@
 use Modern::Perl;
 
 return {
-    bug_number => "18203",
+    bug_number  => "18203",
     description => "Add per borrower category restrictions on ILL placement",
-    up => sub {
+    up          => sub {
         my ($args) = @_;
-        my ($dbh, $out) = @$args{qw(dbh out)};
-        unless ( column_exists('categories', 'can_place_ill_in_opac') ) {
-            $dbh->do(q{
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
+        unless ( column_exists( 'categories', 'can_place_ill_in_opac' ) ) {
+            $dbh->do(
+                q{
                 ALTER TABLE `categories`
                 ADD COLUMN `can_place_ill_in_opac` TINYINT(1) NOT NULL DEFAULT 1 COMMENT 'can this patron category place interlibrary loan requests'
                 AFTER `checkprevcheckout`;
-            });
+            }
+            );
+
+            say $out "Added column 'categories.can_place_ill_in_opac'";
         }
     },
 };
