cae75b64ddae2ae4450c7dc5e0163cb8bb8a9200
Nick Clemens <nick@bywatersolutions.com>
2023-11-02 19:05:16 +0000

    Bug 17798: Confirm hold when printing slip from another patron's account
    This patch adds a few pieces of information to the print slip button
    and makes the code confirm the hold
    
    As we are printing before the confirm, we also add the ability to pass
    in the itemnumber to 'ReserveSlip'
    
    This is slightly hacky, however, I don't see another way to allow
    printing without an additional page reload.
    
    To test:
     1 - Place a title level hold for patron A, for delivery to library B
     2 - Attempt to checkout an item from the record above to Patron B from
         library A
     3 - You receive an alert about the hold
     4 - Click "Don't check out, confirm hold, and print slip"
     5 - Confirm the slip looks correct and has item info
     6 - Confirm that item is in transit to fill hold
     7 - Revert transit status
     8 - Attempt to checkout the item to Patron B from Library B
     9 - Click "Don't check out, confirm hold, and print slip"
    10 - Confirm slip is correct
    11 - Confirm item is marked waiting
    
    Signed-off-by: Owen Leonard <oleonard@myacpl.org>
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    (cherry picked from commit 3ec73d80e2a88e3a87eefc886865ccfb91f9be09)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>
    (cherry picked from commit 96442d7ac738c3baf3a0e6914362e908bdc7adc3)
    Signed-off-by: Pedro Amorim <pedro.amorim@ptfs-europe.com>

--- C4/Reserves.pm
+++ C4/Reserves.pm
@@ -2178,6 +2178,7 @@ sub ReserveSlip {
     my ($args) = @_;
     my $branchcode     = $args->{branchcode};
     my $reserve_id = $args->{reserve_id};
+    my $itemnumber = $args->{itemnumber};
 
     my $hold = Koha::Holds->find($reserve_id);
     return unless $hold;
@@ -2196,7 +2197,7 @@ sub ReserveSlip {
             'borrowers'   => $reserve->{borrowernumber},
             'biblio'      => $reserve->{biblionumber},
             'biblioitems' => $reserve->{biblionumber},
-            'items'       => $reserve->{itemnumber},
+            'items'       => $reserve->{itemnumber} || $itemnumber,
         },
     );
 }
