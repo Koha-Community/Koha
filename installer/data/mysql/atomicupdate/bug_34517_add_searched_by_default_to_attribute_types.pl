337b18546c0cf6518fa76806200ceca4d652ebcf
Katrin Fischer <katrin.fischer.83@web.de>
2023-11-05 01:14:52 +0000

    Bug 34517: (QA follow-up) Perltidy and fix column check
    Without the fix a second run of the database update would fail.
    
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- installer/data/mysql/atomicupdate/bug_34517_add_searched_by_default_to_attribute_types.pl
+++ installer/data/mysql/atomicupdate/bug_34517_add_searched_by_default_to_attribute_types.pl
@@ -8,17 +8,21 @@ return {
         my ( $dbh, $out ) = @$args{qw(dbh out)};
 
         # Do you stuffs here
-        unless ( column_exists( 'borrower_attribute_type', 'searched_by_default' ) ) {
-            $dbh->do(q{
+        unless ( column_exists( 'borrower_attribute_types', 'searched_by_default' ) ) {
+            $dbh->do(
+                q{
                 ALTER TABLE borrower_attribute_types
                 ADD COLUMN `searched_by_default` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'defines if this field is included in "Standard" patron searches in the staff interface (1 for yes, 0 for no)'
                 AFTER `staff_searchable`
-            });
+            }
+            );
             say $out "Added column 'borrower_attribute_types.searched_by_default'";
-            $dbh->do(q{
+            $dbh->do(
+                q{
                 UPDATE borrower_attribute_types
                 SET searched_by_default = 1 WHERE staff_searchable = 1;
-            });
+            }
+            );
             say $out "Updated values to match staff_searchable columns";
         }
     },
