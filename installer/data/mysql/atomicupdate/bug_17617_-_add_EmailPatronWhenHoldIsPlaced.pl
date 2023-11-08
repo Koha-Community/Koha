8f37894c8362c1bc9907a48acc6eb17c402994a9
Tomas Cohen Arazi <tomascohen@theke.io>
2023-11-08 11:28:32 -0300

    Bug 17617: Unify DBRev
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- installer/data/mysql/atomicupdate/bug_17617_-_add_EmailPatronWhenHoldIsPlaced.pl
+++ installer/data/mysql/atomicupdate/bug_17617_-_add_EmailPatronWhenHoldIsPlaced.pl
@@ -11,6 +11,15 @@ return {
             q{INSERT IGNORE INTO systempreferences (variable,value,options,explanation,type) VALUES ('EmailPatronWhenHoldIsPlaced', '0', NULL, 'Email patron when a hold has been placed for them', 'YesNo') }
         );
 
-        say $out "Added system preference 'EmailPatronWhenHoldIsPlaced'";
+        say $out "Added new system preference 'EmailPatronWhenHoldIsPlaced'";
+
+        $dbh->do(
+            q{INSERT IGNORE INTO letter (module, code, branchcode, name, is_html, title, message_transport_type, lang, content) VALUES ("reserves", "HOLDPLACED_PATRON", "", "Hold is confirmed", 0, "Your hold on [% hold.biblio.title %] is confirmed", "email", "default", "Hello [% borrower.firstname %] [% borrower.surname %] ([% borrower.cardnumber %]),
+Your hold on [% hold.biblio.title %] ([% hold.biblio.id %]) has been confirmed.
+You will be notified by the library when your item is available for pickup.
+Thank you!")}
+        );
+
+        say $out "Added new letter 'HOLDPLACED_PATRON' (email)";
     },
 };
