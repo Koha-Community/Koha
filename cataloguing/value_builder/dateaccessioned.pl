a0458e030050104c1922ea9eb0ab6e43cc1fd3e3
Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
2023-11-09 08:05:34 +0000

    Bug 35298: Remove focus handler from dateaccessioned plugin.
    Test plan:
    Verify that setting focus to date acquired field in items editor
    fills date of today when field is empty but does not replace existing
    date.
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Signed-off-by: Owen Leonard <oleonard@myacpl.org>
    Signed-off-by: Katrin Fischer <katrin.fischer.83@web.de>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    (cherry picked from commit 0583deac95b35f1ad96493262e922d7ac3eef102)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>
    (cherry picked from commit 5ca56bf8b380644ddc35d5f72c8f14707489efdc)
    Signed-off-by: Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>

--- cataloguing/value_builder/dateaccessioned.pl
+++ cataloguing/value_builder/dateaccessioned.pl
@@ -39,10 +39,6 @@ my $builder = sub {
     });
 });
 
-function Focus$function_name(event) {
-    set_to_today(event.data.id);
-}
-
 function Click$function_name(event) {
     set_to_today(event.data.id, 1);
     return false;
