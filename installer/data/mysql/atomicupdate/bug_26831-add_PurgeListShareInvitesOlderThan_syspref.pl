aef2b3f97e379c7a1bc11cd6e7fd425c7bc0ffd9
Kyle M Hall <kyle@bywatersolutions.com>
2023-11-17 17:25:58 +0000

    Bug 26831: (follow-up) Tidy code for qa script
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>
    (cherry picked from commit 2f26de8746576c65fb289f7344dc4507f1e64bca)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- installer/data/mysql/atomicupdate/bug_26831-add_PurgeListShareInvitesOlderThan_syspref.pl
+++ installer/data/mysql/atomicupdate/bug_26831-add_PurgeListShareInvitesOlderThan_syspref.pl
@@ -1,12 +1,14 @@
 use Modern::Perl;
 
 return {
-    bug_number => "26831",
+    bug_number  => "26831",
     description => "Add new system preference PurgeListShareInvitesOlderThan",
-    up => sub {
+    up          => sub {
         my ($args) = @_;
-        my ($dbh, $out) = @$args{qw(dbh out)};
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
 
-        $dbh->do(q{INSERT IGNORE INTO systempreferences (variable,value,options,explanation,type) VALUES ('PurgeListShareInvitesOlderThan', '14', NULL, 'If not empty, number of days used when deleting unaccepted list share invites', 'Integer') });
+        $dbh->do(
+            q{INSERT IGNORE INTO systempreferences (variable,value,options,explanation,type) VALUES ('PurgeListShareInvitesOlderThan', '14', NULL, 'If not empty, number of days used when deleting unaccepted list share invites', 'Integer') }
+        );
     },
 };
