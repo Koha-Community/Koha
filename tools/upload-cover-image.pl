ab5a62e2fc9fea9d750f6bd6ea36416eb116e4cc
Aleisha Amohia <aleishaamohia@hotmail.com>
2023-11-23 21:08:09 +0000

    Bug 35291: (QA follow-up) Tidying script for QA test tools
    Signed-off-by: Aleisha Amohia <aleishaamohia@hotmail.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    (cherry picked from commit b30898c8bf48b828080c557186dcb25b980ffb48)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- tools/upload-cover-image.pl
+++ tools/upload-cover-image.pl
@@ -242,8 +242,8 @@ if ($fileID) {
                             }
                             undef $srcimage;
 
-                            if (!$error && C4::Context->preference("CataloguingLog")) {
-                                logaction('CATALOGUING', 'MODIFY', $biblionumber, "biblio cover image: $filename");
+                            if ( !$error && C4::Context->preference("CataloguingLog") ) {
+                                logaction( 'CATALOGUING', 'MODIFY', $biblionumber, "biblio cover image: $filename" );
                             }
 
                         }
