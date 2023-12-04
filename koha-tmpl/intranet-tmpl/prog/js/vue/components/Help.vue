d20410a1cc8839e269dda091b8e73077aa5bb036
Caroline Cyr La Rose <caroline.cyr-la-rose@inlibro.com>
2023-12-04 17:24:14 -0500

    Bug 35463: Open erm and preservation help in new tab
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Philip Orr <philip.orr@lmscloud.de>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- koha-tmpl/intranet-tmpl/prog/js/vue/components/Help.vue
+++ koha-tmpl/intranet-tmpl/prog/js/vue/components/Help.vue
@@ -1,6 +1,10 @@
 <template>
     <nav id="shortcut" class="shortcut">
-        <a href="/cgi-bin/koha/help.pl" class="toplinks" id="helper"
+        <a
+            href="/cgi-bin/koha/help.pl"
+            class="toplinks"
+            id="helper"
+            target="_blank"
             ><i class="fa fa-question-circle"></i> <span>Help</span></a
         >
     </nav>
