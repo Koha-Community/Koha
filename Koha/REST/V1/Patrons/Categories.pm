0e593e59e2a39819e5384f03445a138aa79a7f34
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2023-12-11 17:30:37 +0000

    Bug 26297: API specs
    Test plan, k-t-d:
    1) Access /api/v1/patron_categories
    2) Verify the patron categories are correctly listed
    
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>
    
    Bug 26297: (QA follow-up) Move to REST::V1::Patrons::Categories
    
    Bug 26297: (QA follow-up) Use search_with_library_limits
    
    JD amended-patch: squashed + tidy
    
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

---
+++ Koha/REST/V1/Patrons/Categories.pm
@@ -0,0 +1,53 @@
+package Koha::REST::V1::Patrons::Categories;
+
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+use Mojo::Base 'Mojolicious::Controller';
+
+use Try::Tiny qw( catch try );
+
+use Koha::Patron::Categories;
+
+=head1 NAME
+
+Koha::REST::V1::Patrons::Categories;
+
+=head1 API
+
+=head2 Methods
+
+=head3 list
+
+Controller function that handles listing Koha::Patron::Category objects
+
+=cut
+
+sub list {
+    my $c = shift->openapi->valid_input or return;
+
+    return try {
+        return $c->render(
+            status  => 200,
+            openapi => $c->objects->search( Koha::Patron::Categories->search_with_library_limits ),
+        );
+    } catch {
+        $c->unhandled_exception($_);
+    };
+}
+
+1;
