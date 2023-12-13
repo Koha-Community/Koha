0179f9a55fe853b26a2da9964a89a7618f1db022
Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
2023-12-13 15:03:39 +0000

    Bug 35536: Add Koha object classes for plugin_data
    Test plan:
    Read the patch.
    The objects will be used in subsequent patches, and tested there.
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>
    (cherry picked from commit 5ef6a3fce70efe372733ac5c0a3ff267cd799838)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

---
+++ Koha/Plugins/Datas.pm
@@ -0,0 +1,52 @@
+package Koha::Plugins::Datas;
+
+# Copyright 2023 Rijksmuseum
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+use base qw(Koha::Objects);
+
+use Koha::Plugins::Data;
+
+=head1 NAME
+
+Koha::Plugin::Datas - Koha plugin data (plural) object class
+
+=head1 API
+
+=head2 Class Methods
+
+=cut
+
+=head3 _type
+
+=cut
+
+sub _type {
+    return 'PluginData';
+}
+
+=head3 object_class
+
+=cut
+
+sub object_class {
+    return 'Koha::Plugins::Data';
+}
+
+1;
