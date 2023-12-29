e70f9979c54b3de9ce9172accbad5ac74dac1e0a
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2023-12-29 11:53:25 +0000

    Bug 35657: Add option to set assignee during update
    This patch exposes the UI to allow setting assignee from the ticket
    update modal.
    
    Test plan
    1) Apply the patches and run the database updates
    2) Enable catalog concerns - `CatalogConcerns` or `OPACCatalogConcerns`
    3) Submit a catalog concern via the OPAC or Staff client biblio page
    4) Navigate to the concerns management page 'Cataloging > Catalog
       concerns'
    5) Note there is no assignee displayed in the status field for your new
       concern.
    6) Click the concern to view it's details
    7) Note that in the modal there is now a new 'Change assignee' option
    8) Use this new option to search for and assign a librarian to the
       concern.
    9) Submit the update
    10) Your assignee should now appear in the 'status' data field in the
        table
    11) Clicking through to details again, you should see when the assignee
        was set and should also be able to re-assign it
    
    Signed-off-by: Paul Derscheid <paulderscheid@gmail.com>
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- koha-tmpl/intranet-tmpl/prog/js/modals/display_ticket.js
+++ koha-tmpl/intranet-tmpl/prog/js/modals/display_ticket.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
         let button = $(event.relatedTarget);
         let ticket_id = button.data("concern");
         let resolved = button.data("resolved");
+        let assignee = button.data("assignee");
         modal.find(".modal-footer input").val(ticket_id);
 
         if (resolved) {
@@ -18,6 +19,14 @@ $(document).ready(function () {
         // Display ticket details
         let display = '<div class="list-group">';
         display += '<div class="list-group-item">';
+        if (assignee) {
+            display +=
+                '<span class="pull-right"><span class="label">' +
+                __("Assignee") +
+                "</span>: " +
+                assignee +
+                "</span>";
+        }
         display += '<span class="wrapfix">' + detail + "</span>";
         display += "</div>";
         display += '<div id="concern-updates" class="list-group-item">';
@@ -33,7 +42,7 @@ $(document).ready(function () {
             url: "/api/v1/tickets/" + ticket_id + "/updates",
             method: "GET",
             headers: {
-                "x-koha-embed": ["user", "+strings"],
+                "x-koha-embed": ["user", "assignee", "+strings"],
             },
         })
             .success(function (data) {
@@ -67,10 +76,18 @@ $(document).ready(function () {
                         $datetime(item.date) +
                         ")";
                     if (item.status) {
-                        updates += '<span class="wrapfix pull-right">';
+                        updates += '<span class="clearfix pull-right">';
                         updates += item._strings.status
                             ? escape_str(item._strings.status.str)
                             : "";
+                        updates += "</span>";
+                    }
+                    if (item.assignee) {
+                        updates += '<span class="clearfix pull-right">';
+                        updates += $patron_to_html(item.assignee, {
+                            display_cardnumber: false,
+                            url: true,
+                        });
                         updates += "</span>";
                     }
                     updates += "</span>";
@@ -83,16 +100,103 @@ $(document).ready(function () {
         // Clear any previously entered update message
         $("#update_message").val("");
         $("#public").prop("checked", false);
+
+        // Patron select2
+        $("#assignee_id").kohaSelect({
+            dropdownParent: $(".modal-content", "#ticketDetailsModal"),
+            width: "50%",
+            dropdownAutoWidth: true,
+            allowClear: true,
+            minimumInputLength: 3,
+            ajax: {
+                url: "/api/v1/patrons",
+                delay: 250,
+                dataType: "json",
+                headers: {
+                    "x-koha-embed": "library",
+                },
+                data: function (params) {
+                    let q = buildPatronSearchQuery(params.term);
+                    let query = {
+                        q: JSON.stringify(q),
+                        _page: params.page,
+                        _order_by: "+me.surname,+me.firstname",
+                    };
+                    return query;
+                },
+                processResults: function (data, params) {
+                    let results = [];
+                    data.results.forEach(function (patron) {
+                        patron.id = patron.patron_id;
+                        results.push(patron);
+                    });
+                    return {
+                        results: results,
+                        pagination: { more: data.pagination.more },
+                    };
+                },
+            },
+            templateResult: function (patron) {
+                if (patron.library_id == loggedInLibrary) {
+                    loggedInClass = "ac-currentlibrary";
+                } else {
+                    loggedInClass = "";
+                }
+
+                let $patron = $("<span></span>")
+                    .append(
+                        "" +
+                            (patron.surname
+                                ? escape_str(patron.surname) + ", "
+                                : "") +
+                            (patron.firstname
+                                ? escape_str(patron.firstname) + " "
+                                : "") +
+                            (patron.cardnumber
+                                ? " (" + escape_str(patron.cardnumber) + ")"
+                                : "") +
+                            "<small>" +
+                            (patron.date_of_birth
+                                ? ' <span class="age_years">' +
+                                  $get_age(patron.date_of_birth) +
+                                  " " +
+                                  __("years") +
+                                  "</span>"
+                                : "") +
+                            (patron.library
+                                ? ' <span class="ac-library">' +
+                                  escape_str(patron.library.name) +
+                                  "</span>"
+                                : "") +
+                            "</small>"
+                    )
+                    .addClass(loggedInClass);
+                return $patron;
+            },
+            templateSelection: function (patron) {
+                if (!patron.surname) {
+                    return patron.text;
+                }
+                return (
+                    escape_str(patron.surname) +
+                    ", " +
+                    escape_str(patron.firstname)
+                );
+            },
+            placeholder: "Search for a patron",
+        });
     });
 
     $("#ticketDetailsModal").on("click", ".updateSubmit", function (e) {
         let clicked = $(this);
         let ticket_id = $("#ticket_id").val();
+        let assignee_id = $("#assignee_id").val();
         let params = {
             public: $("#public").is(":checked"),
             message: $("#update_message").val(),
             user_id: logged_in_user_borrowernumber,
             status: clicked.data("status"),
+            assignee_id: assignee_id,
         };
 
         $("#comment-spinner").show();
