366b9ecabebd98a81a32a642ec27d990d13fc26a
Hammat Wele <hammat.wele@inlibro.com>
2024-01-04 13:28:10 +0000

    Bug 33317: (follow-up) Perltidy
    Signed-off-by: Owen Leonard <oleonard@myacpl.org>
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- installer/data/mysql/atomicupdate/bug_33317.pl
+++ installer/data/mysql/atomicupdate/bug_33317.pl
@@ -1,13 +1,15 @@
 use Modern::Perl;
 
 return {
-    bug_number => "33317",
+    bug_number  => "33317",
     description => "Add new system preference OpacMetaRobots",
-    up => sub {
+    up          => sub {
         my ($args) = @_;
-        my ($dbh, $out) = @$args{qw(dbh out)};
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
 
-        $dbh->do(q{INSERT IGNORE INTO systempreferences (variable, value, options, explanation, type) VALUES ('OpacMetaRobots','','','Improve search engine crawling.', 'Multiple') });
+        $dbh->do(
+            q{INSERT IGNORE INTO systempreferences (variable, value, options, explanation, type) VALUES ('OpacMetaRobots','','','Improve search engine crawling.', 'Multiple') }
+        );
 
         say $out "Added system preference 'OpacMetaRobots'";
     },
