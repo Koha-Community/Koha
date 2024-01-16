8c32b44df5b0500286115eae0d463141ceec1c7b
Thibaud Guillot <thibaud.guillot@biblibre.com>
2024-01-16 17:01:58 +0100

    Bug 30230: (follow-up) Add list permission on home-search include and update atomic update file
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- installer/data/mysql/atomicupdate/bug_30230_add-new-list-borrowers-permission.pl
+++ installer/data/mysql/atomicupdate/bug_30230_add-new-list-borrowers-permission.pl
@@ -1,14 +1,14 @@
 use Modern::Perl;
 
 return {
-    bug_number  => "BUG_30230",
+    bug_number  => "30230",
     description => "Add new list_borrowers permission",
     up          => sub {
         my ($args) = @_;
         my ( $dbh, $out ) = @$args{qw(dbh out)};
 
         $dbh->do(
-            "INSERT IGNORE INTO permissions (module_bit, code, description) VALUES (4, 'list_borrowers', 'Search and list patrons')"
+            "INSERT IGNORE INTO permissions (module_bit, code, description) VALUES (4, 'list_borrowers', 'Search, list and view patrons')"
         );
 
         say $out "Added new permission 'list_borrowers'";
