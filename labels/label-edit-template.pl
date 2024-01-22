8cff9b57dd181bbd572f7be9a7c5476e1ec0104c
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2024-01-22 11:52:58 +0000

    Bug 34478: Manual fix - label-edit-template (cud-save)
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- labels/label-edit-template.pl
+++ labels/label-edit-template.pl
@@ -44,7 +44,7 @@ my $profile_list = undef;
 
 my $units = get_unit_values();
 
-if ($op eq 'cud-edit') {
+if ($op eq 'edit') {
     $label_template = C4::Labels::Template->retrieve(template_id => $template_id);
     $profile_list = get_all_profiles({ fields => [ qw( profile_id printer_name paper_bin ) ], filters => { template_id => [ $template_id, '' ], creator => 'Labels'} } );
     push @$profile_list, {paper_bin => 'N/A', profile_id => 0, printer_name => 'No Profile'};
