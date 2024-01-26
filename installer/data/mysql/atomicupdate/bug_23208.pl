a08362d5ab20a995eb859692bd6bae1e3e4c9118
Lucas Gass <lucas@bywatersolutions.com>
2024-01-26 18:42:14 +0000

    Bug 23208: Add system preference HoldRatioDefault
    Signed-off-by: Andrew Fuerste Henry <andrewfh@dubcolib.org>
    Signed-off-by: Emily Lamancusa <emily.lamancusa@montgomerycountymd.gov>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

---
+++ installer/data/mysql/atomicupdate/bug_23208.pl
@@ -0,0 +1,15 @@
+use Modern::Perl;
+
+return {
+    bug_number => "23208",
+    description => "Add system preference HoldRatioDefault",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+        # Do you stuffs here
+        $dbh->do(q{
+            INSERT IGNORE INTO systempreferences (`variable`,`value`,`options`,`explanation`,`type`)
+            VALUES ('HoldRatioDefault','3','','Default value for the Hold ratio report','Integer')
+        });
+    },
+};
