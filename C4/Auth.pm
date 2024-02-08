3280e5a99de92a3e8d33ccef71d3013bac4add68
Andreas Jonsson <andreas.jonsson@kreablo.se>
2024-02-08 11:57:03 +0100

    Bug 36034: (bug 34893 follow-up) fix capture of return values from checkpw
    Adapt code to the change of return value type of checkpw
    introduced in bug 34893
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

--- C4/Auth.pm
+++ C4/Auth.pm
@@ -1031,7 +1031,8 @@ sub checkauth {
             unless ($shibSuccess) {
                 if ( $cas && $query->param('ticket') ) {
                     my $retuserid;
-                    ( $return, $cardnumber, $retuserid, $cas_ticket ) =
+                    my $patron;
+                    ( $return, $cardnumber, $retuserid, $patron, $cas_ticket ) =
                       checkpw( $userid, $password, $query, $type );
                     $userid = $retuserid;
                     $info{'invalidCasLogin'} = 1 unless ($return);
@@ -1099,8 +1100,9 @@ sub checkauth {
                             && $q_userid eq C4::Context->preference('AutoSelfCheckID') )
                       )
                     {
+                        my $patron;
 
-                        ( $return, $cardnumber, $retuserid, $cas_ticket ) =
+                        ( $return, $cardnumber, $retuserid, $patron, $cas_ticket ) =
                           checkpw( $q_userid, $password, $query, $type );
                         $userid = $retuserid if ($retuserid);
                         $info{'invalid_username_or_password'} = 1 unless ($return);
@@ -1593,7 +1595,8 @@ sub check_api_auth {
                 return ( "failed", undef, undef );
             }
             my $newuserid;
-            ( $return, $cardnumber, $newuserid, $cas_ticket ) = checkpw( $userid, $password, $query );
+            my $patron;
+            ( $return, $cardnumber, $newuserid, $patron, $cas_ticket ) = checkpw( $userid, $password, $query );
         }
 
         if ( $return and haspermission( $userid, $flagsrequired ) ) {
