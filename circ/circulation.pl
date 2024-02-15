d4553637e8586b5d92f81cb7af6e4e26bc2e128d
Michael Hafen <michael.hafen@washk12.org>
2024-02-15 11:21:28 -0700

    Bug 35518: Follow-up for AutoSwitchPatron - clear $patron variable too
    The AutoSwitchPatron clears the $borrowernumber variable to switch patrons.
    With the AuthSwitchPatron block moved, the $patron variable still gets set,
    and the patron doesn't get switched.  The clears the $patron variable too.
    
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

--- circ/circulation.pl
+++ circ/circulation.pl
@@ -131,6 +131,7 @@ if ( C4::Context->preference("AutoSwitchPatron") && $barcode ) {
         $findborrower = $barcode;
         undef $barcode;
         undef $borrowernumber;
+        undef $patron;
         $barcodes = [];
         $autoswitched = 1;
     }
