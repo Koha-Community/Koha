41d24731981cd472b017cb8e867d9708b650d1fb
Matthias Le Gac <matthias.le-gac@inlibro.com>
2024-02-22 15:33:25 -0500

    Bug 35169: add bug_35169.pl for DB
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

---
+++ installer/data/mysql/atomicupdate/bug_35169.pl
@@ -0,0 +1,23 @@
+use Modern::Perl;
+
+return {
+    bug_number  => "35169",
+    description => "Add new system preferences for longoverdue.pl borrowers categories",
+    up          => sub {
+        my ($args) = @_;
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
+
+        # Do you stuffs here
+        $dbh->do(
+            q{
+            INSERT INTO systempreferences (variable, value, options, explanation, type) VALUES
+            ('DefaultLongOverdueBorrowerCategories', '', NULL, NULL, NULL),
+            ('DefaultLongOverdueSkipBorrowerCategories', '', NULL, NULL, NULL);
+        }
+        );
+
+        # sysprefs
+        say $out "Added new system preference 'DefaultLongOverdueBorrowerCategories'";
+        say $out "Added new system preference 'DefaultLongOverdueSkipBorrowerCategories'";
+    },
+};
