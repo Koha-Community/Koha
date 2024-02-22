0f182f84b0bb3df344484182b94f1d25e4ec4146
Owen Leonard <oleonard@myacpl.org>
2024-02-22 12:46:52 +0000

    Bug 34478: Correct check of list op in batch record modification
    The "list" step (previewing records to be modified) is a post operation
    so the op must be cud-list.
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- tools/batch_record_modification.pl
+++ tools/batch_record_modification.pl
@@ -89,7 +89,7 @@ if ( $op eq 'form' ) {
             ]
         )
     );
-} elsif ( $op eq 'list' ) {
+} elsif ( $op eq 'cud-list' ) {
     # List all records to process
     my ( @records, @record_ids );
     if ( my $bib_list = $input->param('bib_list') ) {
