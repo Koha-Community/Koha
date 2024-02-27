e881ace64a99106520e84426439f98e55a0d1c80
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2024-02-27 11:01:42 +0000

    Bug 35724: Sync database update and kohasctructure and set to int(11)
    We use int(11) in the smtp_servers table for port, so I think it makes
    sense to use the same here too.
    
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- installer/data/mysql/atomicupdate/bug_35724-add_upload_port_and_download_port_columns.pl
+++ installer/data/mysql/atomicupdate/bug_35724-add_upload_port_and_download_port_columns.pl
@@ -7,24 +7,24 @@ return {
         my ($args) = @_;
         my ( $dbh, $out ) = @$args{qw(dbh out)};
 
-        if ( !column_exists( 'vendor_edi_accounts', 'upload_port' ) ) {
-            $dbh->do(
-                q{
-              ALTER TABLE vendor_edi_accounts ADD COLUMN `upload_port` varchar(40) DEFAULT '22' AFTER `password`
-          }
-            );
-
-            say $out "Added column 'vendor_edi_accounts.upload_port'";
-        }
-
         if ( !column_exists( 'vendor_edi_accounts', 'download_port' ) ) {
             $dbh->do(
                 q{
-              ALTER TABLE vendor_edi_accounts ADD COLUMN `download_port` varchar(40) DEFAULT '22' AFTER `password`
+              ALTER TABLE vendor_edi_accounts ADD COLUMN `download_port` int(11) DEFAULT NULL AFTER `password`
           }
             );
 
             say $out "Added column 'vendor_edi_accounts.download_port'";
+        }
+
+        if ( !column_exists( 'vendor_edi_accounts', 'upload_port' ) ) {
+            $dbh->do(
+                q{
+              ALTER TABLE vendor_edi_accounts ADD COLUMN `upload_port` int(11) DEFAULT NULL AFTER `download_port`
+          }
+            );
+
+            say $out "Added column 'vendor_edi_accounts.upload_port'";
         }
     },
 };
