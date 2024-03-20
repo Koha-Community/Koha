14b6580e14b956766916c7e1481f8ec29e466aba
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2024-03-20 06:09:24 +0100

    Bug 36351: Pretty the api-client
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- koha-tmpl/intranet-tmpl/prog/js/fetch/cataloguing-api-client.js
+++ koha-tmpl/intranet-tmpl/prog/js/fetch/cataloguing-api-client.js
@@ -11,25 +11,27 @@ export class CataloguingAPIClient extends HttpClient {
         return {
             create: bib_info =>
                 this.post({
-                    endpoint: "new_bib/frameworkcode=%s".format( bib_info.frameworkcode ),
+                    endpoint: "new_bib/frameworkcode=%s".format(
+                        bib_info.frameworkcode
+                    ),
                     body: bib_info.record.toXML(),
                     headers: {
-                        "Content-Type":
-                            "text/xml",
+                        "Content-Type": "text/xml",
                     },
                 }),
             update: bib_info =>
                 this.post({
-                    endpoint: "bib/%s?frameworkcode=%s".format( bib_info.id, bib_info.frameworkcode ),
+                    endpoint: "bib/%s?frameworkcode=%s".format(
+                        bib_info.id,
+                        bib_info.frameworkcode
+                    ),
                     body: bib_info.record.toXML(),
                     headers: {
-                        "Content-Type":
-                            "text/xml",
+                        "Content-Type": "text/xml",
                     },
                 }),
         };
     }
-
 }
 
 export default CataloguingAPIClient;
