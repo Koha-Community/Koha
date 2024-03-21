632e280ea2118c9d74cc6427a1fa10cf7533f978
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2024-03-21 09:31:38 +0000

    Bug 36374: Also tidy ticket and concern modals js
    This patch does the promised follow-up to tidy the two other recently
    added js files for modals. As I'm the main maintainer of those to date
    I'm happy to rebase any patches already submitted atop this bug.
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    Signed-off-by: Pedro Amorim <pedro.amorim@ptfs-europe.com>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- koha-tmpl/intranet-tmpl/prog/js/modals/add_catalog_concern.js
+++ koha-tmpl/intranet-tmpl/prog/js/modals/add_catalog_concern.js
@@ -1,53 +1,62 @@
 /* keep tidy */
-$(document).ready(function() {
-
+$(document).ready(function () {
     // Pre-populate empty message with template
-    $('#addConcernModal').on('show.bs.modal', function (e) {
-        $('#addConfirm').prop('disabled', false);
-        let concern_body = $('#concern_body');
-        if ( concern_body.val() === "" ) {
-            let template = $('#concern_template').text();
+    $("#addConcernModal").on("show.bs.modal", function (e) {
+        $("#addConfirm").prop("disabled", false);
+        let concern_body = $("#concern_body");
+        if (concern_body.val() === "") {
+            let template = $("#concern_template").text();
             concern_body.val(template);
         }
     });
 
-    $('#addConcernModal').on('click', '#addConfirm', function(e) {
-        let concern_title = $('#concern_title').val();
-        let concern_body = $('#concern_body').val();
-        let biblio_id = $('#concern_biblio').val();
-        let reporter_id = $('#concern_reporter').val();
+    $("#addConcernModal").on("click", "#addConfirm", function (e) {
+        let concern_title = $("#concern_title").val();
+        let concern_body = $("#concern_body").val();
+        let biblio_id = $("#concern_biblio").val();
+        let reporter_id = $("#concern_reporter").val();
 
         let params = {
-            source: 'catalog',
+            source: "catalog",
             title: concern_title,
             body: concern_body,
             biblio_id: biblio_id,
             reporter_id: logged_in_user_borrowernumber,
         };
 
-        $('#concern-submit-spinner').show();
-        $('#addConfirm').prop('disabled', true);
+        $("#concern-submit-spinner").show();
+        $("#addConfirm").prop("disabled", true);
 
         $.ajax({
-            url: '/api/v1/tickets',
-            type: 'POST',
+            url: "/api/v1/tickets",
+            type: "POST",
             data: JSON.stringify(params),
-            success: function(data) {
-                $('#concern-submit-spinner').hide();
-                $('#addConcernModal').modal('hide');
-                $('#concern_body').val('');
-                $('#concern_title').val('');
-                $('#toolbar').before('<div class="dialog message">' + __("Your concern was successfully submitted.") + '</div>');
+            success: function (data) {
+                $("#concern-submit-spinner").hide();
+                $("#addConcernModal").modal("hide");
+                $("#concern_body").val("");
+                $("#concern_title").val("");
+                $("#toolbar").before(
+                    '<div class="dialog message">' +
+                        __("Your concern was successfully submitted.") +
+                        "</div>"
+                );
                 if ($.fn.dataTable.isDataTable("#table_concerns")) {
                     $("#table_concerns").DataTable().ajax.reload();
                 }
             },
-            error: function(data) {
-                $('#concern-submit-spinner').hide();
-                $('#addConcernModal').modal('hide');
-                $('#toolbar').before('<div class="dialog alert">' + __("There was an error when submitting your concern, please contact a librarian.") + '</div>');
+            error: function (data) {
+                $("#concern-submit-spinner").hide();
+                $("#addConcernModal").modal("hide");
+                $("#toolbar").before(
+                    '<div class="dialog alert">' +
+                        __(
+                            "There was an error when submitting your concern, please contact a librarian."
+                        ) +
+                        "</div>"
+                );
             },
-            contentType: "json"
+            contentType: "json",
         });
     });
 });
