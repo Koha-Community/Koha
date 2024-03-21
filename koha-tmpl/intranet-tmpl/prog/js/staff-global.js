a1045024372ceb2f149ca2c15f138f0542fc0b8c
Kyle M Hall <kyle@bywatersolutions.com>
2024-03-21 09:30:26 -0400

    Bug 36382: XSS in showLastPatron dropdown
    1) Set borrower surname to:
        <script>alert("here comes trouble");</script>
    2) Save, nothing happens
    3) Enable showLastPatron
    4) Reload patron
    5) Note the alert popup
    6) Apply this patch
    7) Reload patron
    8) No alert!
    
    Signed-off-by: Owen Leonard <oleonard@myacpl.org>
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

--- koha-tmpl/intranet-tmpl/prog/js/staff-global.js
+++ koha-tmpl/intranet-tmpl/prog/js/staff-global.js
@@ -31,6 +31,8 @@ var HtmlCharsToEscape = {
     "&": "&amp;",
     "<": "&lt;",
     ">": "&gt;",
+    "'": '&#39;',
+    '"': '&quot;'
 };
 String.prototype.escapeHtml = function () {
     return this.replace(/[&<>]/g, function (c) {
@@ -204,9 +206,9 @@ $(document).ready(function () {
             });
 
             const previous_patron = {
-                borrowernumber: $("#hiddenborrowernumber").val(),
-                name: $("#hiddenborrowername").val(),
-                card: $("#hiddenborrowercard").val(),
+                borrowernumber: escape_str($("#hiddenborrowernumber").val()),
+                name: escape_str($("#hiddenborrowername").val()),
+                card: escape_str($("#hiddenborrowercard").val())
             };
 
             previous_patrons.unshift(previous_patron);
