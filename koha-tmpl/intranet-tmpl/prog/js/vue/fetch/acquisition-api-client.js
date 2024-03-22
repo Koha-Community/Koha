17b134fcb982768541b66f327ba301e06a727337
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2024-03-22 08:41:33 +0100

    Bug 36392: (bug 34219 follow-up) Fix getAll for vendors - ERM
    In commit f1078daf8f496e1b4234acf766578c511ca8aad1
       Bug 34219: Allow getAll to receive additional URL parameters
    
    We needed to call getAll instead of get to fetch all the vendors
    
    Test plan:
    Have more than 20 vendors and create/edit an agreement.
    Notice that the vendor list contains all the vendors
    
    Signed-off-by: David Nind <david@davidnind.com>
    Signed-off-by: Matthias Le Gac <matthias.le-gac@inlibro.com>
    Signed-off-by: Pedro Amorim <pedro.amorim@ptfs-europe.com>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- koha-tmpl/intranet-tmpl/prog/js/vue/fetch/acquisition-api-client.js
+++ koha-tmpl/intranet-tmpl/prog/js/vue/fetch/acquisition-api-client.js
@@ -10,7 +10,7 @@ export class AcquisitionAPIClient extends HttpClient {
     get vendors() {
         return {
             getAll: (query, params) =>
-                this.get({
+                this.getAll({
                     endpoint: "vendors",
                     query,
                     params,
