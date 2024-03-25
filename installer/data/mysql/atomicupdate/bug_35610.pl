c97c758bd4191d8d74b0b77d5f9d547f1f93a6b2
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2024-03-25 16:51:01 +0100

    Bug 35610: Set branchcode to NULL before adding the FK
    Signed-off-by: Victor Grousset/tuxayo <victor@tuxayo.net>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>
    (cherry picked from commit 495f0d2caa8f75da5f428b8a18e58ce248a9fea1)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- installer/data/mysql/atomicupdate/bug_35610.pl
+++ installer/data/mysql/atomicupdate/bug_35610.pl
@@ -9,6 +9,12 @@ return {
 
         unless ( foreign_key_exists('old_reserves', 'old_reserves_ibfk_branchcode') ) {
             $dbh->do(q{
+                UPDATE old_reserves
+                SET branchcode = NULL
+                WHERE branchcode NOT IN (SELECT branchcode FROM branches)
+            });
+
+            $dbh->do(q{
                 ALTER TABLE old_reserves
                 ADD CONSTRAINT `old_reserves_ibfk_branchcode` FOREIGN KEY (`branchcode`) REFERENCES `branches` (`branchcode`) ON DELETE SET NULL ON UPDATE CASCADE;
             });
