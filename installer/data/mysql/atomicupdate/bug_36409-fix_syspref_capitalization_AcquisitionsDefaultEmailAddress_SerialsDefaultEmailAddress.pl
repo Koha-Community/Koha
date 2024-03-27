f973193830927c5ce000e39c1329e68a64bf6ba3
Emily Lamancusa <emily.lamancusa@montgomerycountymd.gov>
2024-03-27 10:45:55 -0400

    Bug 36409: Dbrev for existing installations
    Dbrev to change EMail to Email for SerialsDefaultEMailAddress and
    AcquisitionsDefaultEMailAddress
    
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>
    (cherry picked from commit 43e480255d06357416c200b41e59c30c6ae70cac)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

---
+++ installer/data/mysql/atomicupdate/bug_36409-fix_syspref_capitalization_AcquisitionsDefaultEmailAddress_SerialsDefaultEmailAddress.pl
@@ -0,0 +1,30 @@
+use Modern::Perl;
+
+return {
+    bug_number  => "36409",
+    description => "Fix capitalization for SerialsDefaultEMailAddress and AcquisitionsDefaultEMailAddress",
+    up          => sub {
+        my ($args) = @_;
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
+
+        $dbh->do(
+            q{
+            UPDATE systempreferences
+            SET variable = 'SerialsDefaultEmailAddress'
+            WHERE variable = 'SerialsDefaultEMailAddress';
+        }
+        );
+
+        say $out "Updated system preference 'SerialsDefaultEMailAddress' -> SerialsDefaultEmailAddress";
+
+        $dbh->do(
+            q{
+            UPDATE systempreferences
+            SET variable = 'AcquisitionsDefaultEmailAddress'
+            WHERE variable = 'AcquisitionsDefaultEMailAddress';
+        }
+        );
+
+        say $out "Updated system preference 'AcquisitionsDefaultEMailAddress' -> AcquisitionsDefaultEmailAddress";
+    },
+};
