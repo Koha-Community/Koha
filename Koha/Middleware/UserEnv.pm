2233effc9a9dc6ce39a1501dd59028ade7851138
Nick Clemens <nick@bywatersolutions.com>
2024-03-29 18:09:30 +0000

    Bug 36149: (follow-up) POD and tidy
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    (cherry picked from commit 31943a5781aaaa9803ca87247eb7a663fb999fc5)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>
    (cherry picked from commit 0534259c2cbdbb9d6e0ad56535d0c000eea3a629)
    Signed-off-by: Frédéric Demians <f.demians@tamil.fr>

--- Koha/Middleware/UserEnv.pm
+++ Koha/Middleware/UserEnv.pm
@@ -5,8 +5,20 @@ use parent qw(Plack::Middleware);
 
 use C4::Context;
 
+=head1 NAME
+
+Koha::Middleware::UserEnv - Middleware to ensure fresh userenv in all requests
+
+=head1 METHODS
+
+=head2 call
+
+This method is called for each request, and will unset the userenv to avoid contamination between requests.
+
+=cut
+
 sub call {
-    my ( $self, $env ) =@_;
+    my ( $self, $env ) = @_;
 
     my $req = Plack::Request->new($env);
 
