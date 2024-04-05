5b66a82afe6d0b743d8223337d47de00e3ad8244
Katrin Fischer <katrin.fischer@bsz-bw.de>
2024-04-05 15:31:24 +0000

    Bug 32707: DBRev 23.11.05.004
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>
    (cherry picked from commit c57e25a4086adf71df8fd3b437d9fa07e34fa4c2)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- installer/data/mysql/db_revs/231105004.pl
+++ installer/data/mysql/db_revs/231105004.pl
@@ -2,7 +2,7 @@ use Modern::Perl;
 
 return {
     bug_number  => "32707",
-    description => "Add `ESPreventAutoTruncate` preference",
+    description => "Add 'ESPreventAutoTruncate' system preference",
     up          => sub {
         my ($args) = @_;
         my ( $dbh, $out ) = @$args{qw(dbh out)};
@@ -12,5 +12,6 @@ return {
             VALUES ('ESPreventAutoTruncate', 'barcode|control-number|control-number-identifier|date-of-acquisition|date-of-publication|date-time-last-modified|identifier-standard|isbn|issn|itype|lc-card-number|number-local-acquisition|other-control-number|record-control-number', NULL, 'List of searchfields (separated by | or ,) that should not be autotruncated by Elasticsearch even if QueryAutoTruncate is set to Yes', 'Free')
         }
         );
+        say $out "Added new system preference 'ESPreventAutoTruncate'";
     },
 };
