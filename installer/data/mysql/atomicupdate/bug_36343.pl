4b5a02bd1d9c4dc14151578d5a950a282add424a
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2024-04-16 13:13:55 +0100

    Bug 36343: (QA follow-up) Add warning in DB update
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

---
+++ installer/data/mysql/atomicupdate/bug_36343.pl
@@ -0,0 +1,13 @@
+use Modern::Perl;
+
+return {
+    bug_number  => "36343",
+    description => "Update plugin hooks for consistency",
+    up          => sub {
+        my ($args) = @_;
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
+
+        say $out "WARNING: Plugin authors should consider changes introduced here";
+        say $out "WARNING: after_biblio_action, after_item_action and patron_generate_userid hooks now pass data inside a 'payload' hash";
+    },
+};
