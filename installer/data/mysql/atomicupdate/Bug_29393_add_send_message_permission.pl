270ff5cd1d399ae59a5f8f9b094a74cb834f1069
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2024-04-25 09:58:19 +0100

    Bug 29393: (QA follow-up) Make database update idempotent
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- installer/data/mysql/atomicupdate/Bug_29393_add_send_message_permission.pl
+++ installer/data/mysql/atomicupdate/Bug_29393_add_send_message_permission.pl
@@ -1,14 +1,16 @@
 use Modern::Perl;
 
 return {
-    bug_number => "29393",
+    bug_number  => "29393",
     description => "Add permission borrowers:send_messages_to_borrowers",
-    up => sub {
+    up          => sub {
         my ($args) = @_;
-        my ($dbh, $out) = @$args{qw(dbh out)};
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
 
-        $dbh->do(q{INSERT INTO permissions (module_bit, code, description)
-            VALUES (4, 'send_messages_to_borrowers', 'Send messages to patrons')});
+        $dbh->do(
+            q{INSERT IGNORE INTO permissions (module_bit, code, description)
+            VALUES (4, 'send_messages_to_borrowers', 'Send messages to patrons')}
+        );
         say $out "Update is going well so far";
     },
-}
+    }
