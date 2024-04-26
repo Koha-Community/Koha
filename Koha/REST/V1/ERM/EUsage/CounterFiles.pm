658161f63741f4b96075f023236c3079bf8932eb
Katrin Fischer <katrin.fischer@bsz-bw.de>
2024-04-26 15:41:02 +0000

    Bug 36495: (follow-up) Terminology: COUNTER is an abbreviation
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- Koha/REST/V1/ERM/EUsage/CounterFiles.pm
+++ Koha/REST/V1/ERM/EUsage/CounterFiles.pm
@@ -59,7 +59,7 @@ sub get {
     return try {
         my $counter_file = Koha::ERM::EUsage::CounterFiles->find( $c->param('erm_counter_files_id') );
 
-        return $c->render_resource_not_found("Counter file")
+        return $c->render_resource_not_found("COUNTER file")
             unless $counter_file;
 
         $c->render_file(
@@ -80,7 +80,7 @@ sub delete {
 
     my $counter_file = Koha::ERM::EUsage::CounterFiles->find( $c->param('erm_counter_files_id') );
 
-    return $c->render_resource_not_found("Counter file")
+    return $c->render_resource_not_found("COUNTER file")
         unless $counter_file;
 
     return try {
