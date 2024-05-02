44a26cbec7d63d873c0b13cbd8097138431480d0
Katariina Pohto <trainee@koha-suomi.fi>
2024-05-02 16:09:39 +0300

    Bug 34263: Suspending holds consecutively populates previously used date falsely
    Suspending a hold doesn't clear the date from the date picker. When another hold is suspended
    the previously used date will show on the date picker but the value is not set on the hold.
    Suspending the hold will suspend it indefinitely. Also the link "Clear date to suspend indefinitely"
    will not clear the date picker. This patch will make both the Suspend button and the Clear date link
    clear the dates from both the date picker and the variable passed on, making the suspending consistent
    with what is seen on the date picker.
    
    Test plan:
    1) Place 3 holds on a patron.
    2) Suspend a hold and set a date for it.
    3) Suspend a second hold and notice the previously used date is shown on the date picker.
       Note that the hold will be suspended indefinitely.
    4) Open the suspending window for the third hold and pick a date. Click "Clear date to suspend indefinitely"
       and note the date picker doesn't get cleared. The hold will be suspended indefinitely.
    5) Apply patch.
    6) Suspend a hold and set a date for it.
    7) Suspend a second hold and notice the date picker does not have a preset date.
    8) Pick a date and clear it with the "Clear date to suspend indefinitely link".
       See that the date picker also gets cleared.
    
    Signed-off-by: Pedro Amorim <pedro.amorim@ptfs-europe.com>
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>
    (cherry picked from commit 0f0479e87aaa324b455fbbf40674f15e2fa862c0)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- koha-tmpl/intranet-tmpl/prog/js/holds.js
+++ koha-tmpl/intranet-tmpl/prog/js/holds.js
@@ -420,7 +420,7 @@ $(document).ready(function() {
         </div>\
     ");
 
-    $("#suspend-modal-clear-date").on( "click", function() { $("#suspend-modal-until").val(""); } );
+    $("#suspend-modal-clear-date").on( "click", function() { $(".suspend-until").val(""); } );
 
     $("#suspend-modal-submit").on( "click", function( e ) {
         e.preventDefault();
@@ -440,7 +440,7 @@ $(document).ready(function() {
             }
             holdsTable.api().ajax.reload();
         }).done(function() {
-            $("#suspend-modal-until").val(""); // clean the input
+            $(".suspend-until").val(""); // clean the input
             $('#suspend-modal').modal('hide');
         });
     });
