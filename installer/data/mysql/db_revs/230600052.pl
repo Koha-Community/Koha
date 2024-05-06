ff183612043b8f37d74fbbd9c306a54c48329d99
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2024-05-06 12:58:33 +0200

    Bug 36790: List table columns for 230600052 queries
    We must list the columns, or the db rev will fail when a new column is
    added. It happened here when 33478 added 'style'
    
    Also remove id and dates
    
    To test:
    1. On current main, run:
       $ ktd --shell
      k$ perl /kohadevbox/misc4dev/run_tests.pl --koha-dir=. --run-db-upgrade-only
    => FAIL: Tests explode for DB query issues
    2. Apply this patch
    3. Repeat 1
    => SUCCESS: Tests pass!
    4. Sign off :-D
    
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- installer/data/mysql/db_revs/230600052.pl
+++ installer/data/mysql/db_revs/230600052.pl
@@ -9,7 +9,8 @@ return {
 
         $dbh->do(
             q{
-            INSERT IGNORE INTO `letter` VALUES (NULL,'reserves','HOLDDGST','','Hold available for pickup (digest)',0,'Hold(s) available for pickup','You have one or more holds available for pickup:\r\n\r\n----\r\nTitle: [% hold.biblio.title %]\r\nAuthor: [% hold.biblio.author %]\r\nCopy: [% hold.item.copynumber %]\r\nLocation: [% hold.branch.branchname %]\r\nWaiting since: [% hold.waitingdate %]\r\nWaiting at: [%hold.branch.branchname%]\r\n[% hold.branch.branchaddress1 %]\r\n[% hold.branch.branchaddress2 %]\r\n[% hold.branch.branchaddress3 %]\r\n[% hold.branch.branchcity %] [% hold.branch.branchzip %]\r\n----','email','default','2023-08-29 18:42:15');
+            INSERT IGNORE INTO `letter` (`module`, `code`, `branchcode`, `name`, `is_html`, `title`, `content`, `message_transport_type`, `lang`)
+            VALUES ('reserves','HOLDDGST','','Hold available for pickup (digest)',0,'Hold(s) available for pickup','You have one or more holds available for pickup:\r\n\r\n----\r\nTitle: [% hold.biblio.title %]\r\nAuthor: [% hold.biblio.author %]\r\nCopy: [% hold.item.copynumber %]\r\nLocation: [% hold.branch.branchname %]\r\nWaiting since: [% hold.waitingdate %]\r\nWaiting at: [%hold.branch.branchname%]\r\n[% hold.branch.branchaddress1 %]\r\n[% hold.branch.branchaddress2 %]\r\n[% hold.branch.branchaddress3 %]\r\n[% hold.branch.branchcity %] [% hold.branch.branchzip %]\r\n----','email','default');
         }
         );
 
@@ -17,7 +18,8 @@ return {
 
         $dbh->do(
             q{
-            INSERT IGNORE INTO `letter` VALUES (NULL,'reserves','HOLDDGST','','Hold available for pickup (digest)',0,'Hold(s) available for pickup','You have one or more holds available for pickup:\r\n----\r\n[% hold.biblio.title %]\r\n----','sms','default','2023-08-29 18:42:15');
+            INSERT IGNORE INTO `letter` (`module`, `code`, `branchcode`, `name`, `is_html`, `title`, `content`, `message_transport_type`, `lang`)
+            VALUES ('reserves','HOLDDGST','','Hold available for pickup (digest)',0,'Hold(s) available for pickup','You have one or more holds available for pickup:\r\n----\r\n[% hold.biblio.title %]\r\n----','sms','default');
         }
         );
 
@@ -25,7 +27,9 @@ return {
 
         $dbh->do(
             q{
-            INSERT IGNORE INTO message_transports VALUES
+            INSERT IGNORE INTO message_transports
+            (`message_attribute_id`, `message_transport_type`, `is_digest`, `letter_module`, `letter_code`, `branchcode`)
+            VALUES
             ( 4, "email", 1, "reserves", "HOLDDGST", "" ),
             ( 4, "sms", 1, "reserves", "HOLDDGST", "" ),
             ( 4, "phone", 1, "reserves", "HOLDDGST", "");
