34b3becef2c38804153f062f4043b47fb23143a4
Tomas Cohen Arazi <tomascohen@theke.io>
2024-05-06 09:35:59 -0300

    Bug 25159: (QA follow-up) Make DBRev idempotent
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- installer/data/mysql/db_revs/231200043.pl
+++ installer/data/mysql/db_revs/231200043.pl
@@ -7,7 +7,10 @@ return {
         my ($args) = @_;
         my ( $dbh, $out ) = @$args{qw(dbh out)};
 
-        $dbh->do(q{ALTER TABLE action_logs ADD COLUMN diff LONGTEXT NULL DEFAULT NULL AFTER trace;});
+        unless ( column_exists( 'action_logs', 'diff' ) ) {
+            $dbh->do(q{ALTER TABLE action_logs ADD COLUMN diff LONGTEXT NULL DEFAULT NULL AFTER trace;});
+        }
+
         say $out "Added column 'action_logs.diff'";
     },
 };
