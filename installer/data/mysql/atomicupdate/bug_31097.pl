9e996fc4667e843cce851c4410f96da0d7ccd3ce
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2024-05-07 11:45:40 +0100

    Bug 31097: Update restriction type display text
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

---
+++ installer/data/mysql/atomicupdate/bug_31097.pl
@@ -0,0 +1,18 @@
+use Modern::Perl;
+use Koha::Installer::Output qw(say_warning say_failure say_success say_info);
+
+return {
+    bug_number  => "31097",
+    description => "Change default 'Manual' restriction test",
+    up          => sub {
+        my ($args) = @_;
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
+
+        $dbh->do(q{ UPDATE restriction_types SET display_text = "Manual restriction" WHERE display_text = "Manual"});
+
+        say_info(
+            $out,
+            "Updated patron restriction types display for MANUAL restrictions from 'Manual' to 'Manual restriction'"
+        );
+    },
+};
