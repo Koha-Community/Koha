79d30205943dd3adeabd267bc729a63e69c0be30
Katrin Fischer <katrin.fischer@bsz-bw.de>
2024-05-07 13:16:07 +0000

    Bug 36396: DBRev 23.12.00.049
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- installer/data/mysql/db_revs/231200049.pl
+++ installer/data/mysql/db_revs/231200049.pl
@@ -2,17 +2,20 @@ use Modern::Perl;
 
 return {
     bug_number  => "36396",
-    description => "Link elastic facets with authorized value categories",
+    description => "Link Elasticsearch facets with authorized value categories",
     up          => sub {
         my ($args) = @_;
         my ( $dbh, $out ) = @$args{qw(dbh out)};
 
-        $dbh->do(
-            q{
+        unless ( column_exists( 'search_field', 'authorised_value_category' ) ) {
+            $dbh->do(
+                q{
             ALTER TABLE search_field
             ADD COLUMN `authorised_value_category` varchar(32) DEFAULT NULL
             AFTER mandatory
         }
-        );
+            );
+        }
+        say $out "Added Elasticsearch facets with authorized value categories";
     },
 };
