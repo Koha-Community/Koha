07d52309c22da89430db344da0a387ce832de2ed
Katrin Fischer <katrin.fischer@bsz-bw.de>
2024-05-08 17:53:44 +0000

    Bug 19768: DBRev 23.12.00.054
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

---
+++ installer/data/mysql/db_revs/231200054.pl
@@ -0,0 +1,20 @@
+use Modern::Perl;
+use Koha::Installer::Output qw(say_warning say_failure say_success say_info);
+
+return {
+    bug_number  => "19768",
+    description => "Add 'Title notes' tab to OpacSerialDefaultTab system preference",
+    up          => sub {
+        my ($args) = @_;
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
+
+        # Do you stuffs here
+        $dbh->do(
+            q{ UPDATE systempreferences SET options = 'holdings|serialcollection|subscriptions|titlenotes' WHERE variable = 'opacSerialDefaultTab' }
+        );
+
+        # Print useful stuff here
+        say $out "Added 'Title notes' option to opacSerialDefaultTab system preference";
+
+    },
+};
