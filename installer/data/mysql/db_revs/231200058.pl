5cf60b53297910bca2e4d6596a33d70ff4c34d05
Katrin Fischer <katrin.fischer@bsz-bw.de>
2024-05-17 07:32:16 +0000

    Bug 28869: DBRev 23.12.00.058
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- installer/data/mysql/db_revs/231200058.pl
+++ installer/data/mysql/db_revs/231200058.pl
@@ -9,7 +9,7 @@ return {
         if ( !column_exists( 'authorised_value_categories', 'is_integer_only' ) ) {
             $dbh->do(
                 q{
-ALTER TABLE authorised_value_categories ADD COLUMN is_integer_only tinyint(1) DEFAULT 0 NOT NULL
+ALTER TABLE authorised_value_categories ADD COLUMN is_integer_only tinyint(1) DEFAULT 0 NOT NULL AFTER `is_system`
             }
             );
         }
