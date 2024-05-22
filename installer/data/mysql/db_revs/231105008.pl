710bd67ae63062b49ea26b05b569aea42ebd91b0
Katrin Fischer <katrin.fischer@bsz-bw.de>
2024-05-22 13:58:35 +0000

    Bug 36665: DBRev 23.11.05.008
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>
    (cherry picked from commit 3a8e38b449ac228f435857bf1e2bf8391470a768)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- installer/data/mysql/db_revs/231105008.pl
+++ installer/data/mysql/db_revs/231105008.pl
@@ -1,9 +1,8 @@
 use Modern::Perl;
-use Koha::Installer::Output qw(say_warning say_failure say_success say_info);
 
 return {
     bug_number  => "36665",
-    description => "Add StaffLoginBranchBasedOnIP",
+    description => "Add system preference StaffLoginBranchBasedOnIP",
     up          => sub {
         my ($args) = @_;
         my ( $dbh, $out ) = @$args{qw(dbh out)};
@@ -12,7 +11,7 @@ return {
         $dbh->do(
             q{
             INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type` ) VALUES
-            ('StaffLoginBranchBasedOnIP', '0','', 'Set the logged in branch for the user based on their current IP','YesNo')
+            ('StaffLoginBranchBasedOnIP', '0','', 'Set the logged in library for the user based on their current IP','YesNo')
         }
         );
 
