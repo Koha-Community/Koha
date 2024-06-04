4dd3df5e1a862eb573dfb4dd5448f357cd42ef7b
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2024-06-04 11:15:59 +0100

    Bug 36986: (QA follow-up) Catch the case where someone already upgraded
    This adds a corresponding atomic update for the case where someone may
    have already upgraded past the version we're correcting in the prior
    patch.
    
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    (cherry picked from commit d0f12989518b32c7bf9cb8093d57e49b038e8d70)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

---
+++ installer/data/mysql/atomicupdate/bug_36986.pl
@@ -0,0 +1,23 @@
+use Modern::Perl;
+use Koha::Installer::Output qw(say_warning say_failure say_success say_info);
+
+return {
+    bug_number  => "36986",
+    description => "Rename StaffLoginLibraryBasedOnIP system preference",
+    up          => sub {
+        my ($args) = @_;
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
+
+        $dbh->do(
+            q{
+            UPDATE systempreferences SET variable = "StaffLoginLibraryBasedOnIP"
+            WHERE variable = "StaffLoginBranchBasedOnIP"
+        }
+            ) == 1
+            && say_success(
+            $out,
+            "Renamed system preference 'StaffLoginBranchBasedOnIP' to 'StaffLoginLibraryBasedOnIP'"
+            );
+
+    },
+};
