3ec24191525e4e7f3022bff40616a8d169bb885b
Lucas Gass <lucas@bywatersolutions.com>
2024-06-05 22:10:34 +0000

    Bug 35635: DB Updates
    Signed-off-by: Brendan Lawlor <blawlor@clamsnet.org>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

---
+++ installer/data/mysql/atomicupdate/bug_35635_add_opac_mandatory.pl
@@ -0,0 +1,32 @@
+use Modern::Perl;
+use Koha::Installer::Output qw(say_warning say_failure say_success say_info);
+
+return {
+    bug_number  => "35635",
+    description => "Add opac_mandatory column to borrower_attribute_type table",
+    up          => sub {
+        my ($args) = @_;
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
+
+        if ( !column_exists( 'borrower_attribute_types', 'opac_mandatory' ) ) {
+            $dbh->do(
+                q{
+                    ALTER TABLE borrower_attribute_types
+                    ADD COLUMN `opac_mandatory` tinyint(1) DEFAULT 0 COMMENT 'defines if the attribute is mandatory or not on the OPAC'
+                    AFTER `mandatory`
+                }
+            );
+
+            say $out "Added column 'borrower_attribute_types.opac_mandatory'";
+
+            $dbh->do(
+                q{
+                UPDATE borrower_attribute_types
+                SET opac_mandatory = 1 WHERE mandatory = 1;
+            }
+            );
+            say $out "Update opac_mandatory to match mandatory column";
+        }
+    },
+
+};
