bfe73050e3351ffe76ef714185c3a456b0c22c51
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2024-06-05 16:57:59 +0100

    Bug 36453: (QA follow-up) Modernise database update
    This patch adds the colored output expected for 24.11.x onward database
    updates.
    
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- installer/data/mysql/atomicupdate/bug_36453.pl
+++ installer/data/mysql/atomicupdate/bug_36453.pl
@@ -1,4 +1,5 @@
 use Modern::Perl;
+use Koha::Installer::Output qw(say_warning say_failure say_success say_info);
 
 return {
     bug_number  => "36453",
@@ -6,6 +7,10 @@ return {
     up          => sub {
         my ($args) = @_;
         my ( $dbh, $out ) = @$args{qw(dbh out)};
+
+        # Update system preference setting to multiple instead of YesNo
+        $dbh->do("UPDATE systempreferences SET type='multiple' WHERE variable='BlockExpiredPatronOpacActions'") == 1
+            and say_success( $out, "BlockExpiredPatronOpacActions system preference type updated to multiple." );
 
         my ($BlockExpiredPatronOpacActions) = $dbh->selectrow_array(
             q{
@@ -18,42 +23,44 @@ return {
             # Update system preference settings if 1
             $dbh->do(
                 "UPDATE systempreferences SET value='hold,renew' WHERE variable='BlockExpiredPatronOpacActions' and value=1"
-            );
-            say $out "BlockExpiredPatronOpacActions system preference value updated to 'hold,renew'.";
+                ) == 1
+                and say_info( $out, "BlockExpiredPatronOpacActions system preference value updated to 'hold,renew'." );
         } elsif ( $BlockExpiredPatronOpacActions eq 0 ) {
 
             # Update system preference settings if 0
-            $dbh->do(
-                "UPDATE systempreferences SET value='' WHERE variable='BlockExpiredPatronOpacActions' and value=0");
-            say $out "BlockExpiredPatronOpacActions system preference value updated to empty string.";
+            $dbh->do("UPDATE systempreferences SET value='' WHERE variable='BlockExpiredPatronOpacActions' and value=0")
+                == 1
+                and
+                say_info( $out, "BlockExpiredPatronOpacActions system preference value updated to an empty string." );
         }
-
-        # Update system preference setting to multiple instead of YesNo
-        $dbh->do("UPDATE systempreferences SET type='multiple' WHERE variable='BlockExpiredPatronOpacActions'");
-        say $out "BlockExpiredPatronOpacActions system preference value updated to multiple.";
 
         # Update categories database table BlockExpiredPatronOpacActions column settings
         $dbh->do(
             "ALTER TABLE categories MODIFY BlockExpiredPatronOpacActions mediumtext NOT NULL DEFAULT 'follow_syspref_BlockExpiredPatronOpacActions' COMMENT 'specific actions expired patrons of this category are blocked from performing or if the BlockExpiredPatronOpacActions system preference is to be followed'"
-        );
-        say $out "categories column BlockExpiredPatronOpacActions updated.";
+        ) and say_success( $out, "Updated categories.BlockExpiredPatronOpacActions to mediumtext" );
 
         # Update patron categories using -1
         $dbh->do(
             "UPDATE categories SET BlockExpiredPatronOpacActions = 'follow_syspref_BlockExpiredPatronOpacActions' WHERE BlockExpiredPatronOpacActions = '-1'"
-        );
-        say $out
-            "Patron categories BlockExpiredPatronOpacActions = -1 have been updated to follow the syspref BlockExpiredPatronOpacActions.";
+            ) >= 1
+            and say_info(
+            $out,
+            "Patron categories BlockExpiredPatronOpacActions = -1 have been updated to follow the syspref BlockExpiredPatronOpacActions."
+            );
 
         # Update patron categories using 0
-        $dbh->do("UPDATE categories SET BlockExpiredPatronOpacActions = '' WHERE BlockExpiredPatronOpacActions = '0'");
-        say $out "Patron categories BlockExpiredPatronOpacActions = 0 have been updated to an empty string ('').";
+        $dbh->do("UPDATE categories SET BlockExpiredPatronOpacActions = '' WHERE BlockExpiredPatronOpacActions = '0'")
+            >= 1
+            and say_info(
+            $out,
+            "Patron categories BlockExpiredPatronOpacActions = 0 have been updated to an empty string ('')."
+            );
 
         # Update patron categories using 1
         $dbh->do(
             "UPDATE categories SET BlockExpiredPatronOpacActions = 'hold,renew' WHERE BlockExpiredPatronOpacActions = '1' "
-        );
-        say $out "Patron categories BlockExpiredPatronOpacActions = 1 have been updated to hold,renew'.";
-
+            ) >= 1
+            and
+            say_info( $out, "Patron categories BlockExpiredPatronOpacActions = 1 have been updated to hold,renew'." );
     },
 };
