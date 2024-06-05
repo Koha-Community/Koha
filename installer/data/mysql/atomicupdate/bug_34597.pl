be240afe3ba659bd1224c2e078dd272e4ea215aa
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2024-06-05 17:18:17 +0100

    Bug 34597: (QA follow-up) Add missing database revision
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

---
+++ installer/data/mysql/atomicupdate/bug_34597.pl
@@ -0,0 +1,16 @@
+use Modern::Perl;
+use Koha::Installer::Output qw(say_warning say_failure say_success say_info);
+
+return {
+    bug_number  => "34597",
+    description => "Update BlockExpiredPatronOpacActions to include ill_request",
+    up          => sub {
+        my ($args) = @_;
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
+
+        $dbh->do(
+            "UPDATE systempreferences SET options='hold,renew,ill_request' WHERE variable='BlockExpiredPatronOpacActions'"
+            ) == 1
+            and say_success( $out, "BlockExpiredPatronOpacActions system updated to include ill_request." );
+    },
+};
