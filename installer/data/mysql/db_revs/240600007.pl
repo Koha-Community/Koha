faac1c973805e287d31b8ab725c8bddd4400ff71
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2024-06-13 14:30:25 +0100

    Bug 36330: DBRev 24.06.00.007
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- installer/data/mysql/db_revs/240600007.pl
+++ installer/data/mysql/db_revs/240600007.pl
@@ -9,14 +9,18 @@ return {
         my ( $dbh, $out ) = @$args{qw(dbh out)};
 
         # Update columns, only changing the COMMENT
-        my $affected1 = $dbh->do(q{ ALTER TABLE course_items MODIFY `location` varchar(80) DEFAULT NULL COMMENT 'new shelving location for the item to have while on reserve (optional)' });
-        if ( $affected1 ) {
+        my $affected1 = $dbh->do(
+            q{ ALTER TABLE course_items MODIFY `location` varchar(80) DEFAULT NULL COMMENT 'new shelving location for the item to have while on reserve (optional)' }
+        );
+        if ($affected1) {
             say_success( $out, "Comment for course_items.location was updated." );
         } else {
             say_failure( $out, "Comment for course_items.location was not updated." );
         }
-        my $affected2 = $dbh->do(q{ ALTER TABLE course_items MODIFY `enabled` enum('yes','no') NOT NULL DEFAULT 'no' COMMENT 'if at least one enabled course has this item on reserve, this field will be ''yes'', otherwise it will be ''no''' });
-        if ( $affected2 ) {
+        my $affected2 = $dbh->do(
+            q{ ALTER TABLE course_items MODIFY `enabled` enum('yes','no') NOT NULL DEFAULT 'no' COMMENT 'if at least one enabled course has this item on reserve, this field will be ''yes'', otherwise it will be ''no''' }
+        );
+        if ($affected2) {
             say_success( $out, "Comment for course_items.enabled was updated." );
         } else {
             say_failure( $out, "Comment for course_items.enabled was not updated." );
