2013ea70ec603989e4765950e5ac00ba884b60c8
Katrin Fischer <katrin.fischer@bsz-bw.de>
2024-06-27 10:10:58 +0000

    Bug 34481: DBRev 24.06.00.010
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- installer/data/mysql/db_revs/240600010.pl
+++ installer/data/mysql/db_revs/240600010.pl
@@ -1,4 +1,5 @@
 use Modern::Perl;
+use Koha::Installer::Output qw(say_warning say_failure say_success say_info);
 
 return {
     bug_number  => "34481",
@@ -7,11 +8,13 @@ return {
         my ($args) = @_;
         my ( $dbh, $out ) = @$args{qw(dbh out)};
 
-        $dbh->do(q{
+        $dbh->do(
+            q{
             INSERT IGNORE INTO systempreferences (`variable`,`value`,`options`,`explanation`,`type`)
             VALUES ('IncludeSeeAlsoFromInSearches','0','','Include see-also-from references in searches.','YesNo')
-        });
+        }
+        );
 
-        say $out "Added new system preference 'IncludeSeeAlsoFromInSearches'";
+        say_success( $out, "Added new system preference 'IncludeSeeAlsoFromInSearches'" );
     },
 };
