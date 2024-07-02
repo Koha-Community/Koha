49ecc5c797cfee7f316dbae01159d16f56a66c6d
Katrin Fischer <katrin.fischer@bsz-bw.de>
2024-07-02 14:32:01 +0000

    Bug 30873: DBRev 24.06.00.012
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- installer/data/mysql/db_revs/240600012.pl
+++ installer/data/mysql/db_revs/240600012.pl
@@ -1,12 +1,12 @@
 use Modern::Perl;
+use Koha::Installer::Output qw(say_warning say_failure say_success say_info);
 
 return {
     bug_number  => "26205",
-    description =>
-        "Add new system preference OPACShowLibraries to control whether the libraries link appears in the OPAC",
-    up => sub {
+    description => "Add new system preference OPACShowLibraries",
+    up          => sub {
         my ($args) = @_;
-        my $dbh = $args->{dbh};
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
 
         $dbh->do(
             q{
@@ -14,5 +14,6 @@ return {
             VALUES ('OPACShowLibraries', '1', 'If enabled, a "Libraries" link appears in the OPAC pointing to a page with library information', '', 'YesNo')
         }
         );
+        say_success( $out, "Added new system preference 'OPACShowLibraries'" );
     },
-    }
+};
