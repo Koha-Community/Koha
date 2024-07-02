377fde1b8f5e0df36aa8b83f8694c431e0dc42f4
Kyle M Hall <kyle@bywatersolutions.com>
2024-07-02 12:09:40 +0000

    Bug 33363: (QA follow-up) Use say_success in atomic update
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- installer/data/mysql/atomicupdate/bug_33363.pl
+++ installer/data/mysql/atomicupdate/bug_33363.pl
@@ -1,4 +1,5 @@
 use Modern::Perl;
+use Koha::Installer::Output qw(say_warning say_failure say_success say_info);
 
 return {
     bug_number  => "33363",
@@ -10,16 +11,17 @@ return {
 
         $dbh->do(
             q{INSERT IGNORE INTO permissions (module_bit, code, description) VALUES (12, 'suggestions_create', 'Create purchase suggestions')}
-        );
+        ) && say_success($out, "Added new permissions suggestions_create");
         $dbh->do(
             q{INSERT IGNORE INTO permissions (module_bit, code, description) VALUES (12, 'suggestions_delete', 'Update purchase suggestions')}
-        );
+        ) && say_success($out, "Added new permissions suggestions_delete");
 
         $dbh->do(
             q{INSERT IGNORE INTO user_permissions (borrowernumber, module_bit, code) SELECT borrowernumber, 12, 'suggestions_create' FROM borrowers WHERE flags & (1 << 2)}
-        );
+        ) && say_success($out, "Added new permissions suggestions_create to patrons with suggestions_manage");
         $dbh->do(
             q{INSERT IGNORE INTO user_permissions (borrowernumber, module_bit, code) SELECT borrowernumber, 12, 'suggestions_delete' FROM borrowers WHERE flags & (1 << 2)}
-        );
+        ) && say_success($out, "Added new permissions suggestions_delete to patrons with suggestions_manage");
+
     },
 };
