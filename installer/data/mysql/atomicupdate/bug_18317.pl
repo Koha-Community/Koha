8477099deeafeb11b09910821e8ee8d206527b06
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2024-07-04 09:40:17 +0100

    Bug 18317: (QA follow-up) Fix missing `,`
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- installer/data/mysql/atomicupdate/bug_18317.pl
+++ installer/data/mysql/atomicupdate/bug_18317.pl
@@ -1,16 +1,18 @@
 use Modern::Perl;
 
 return {
-    bug_number => "18317",
-    description => "Allow check out of already checked out items through SIP"
-    up => sub {
+    bug_number  => "18317",
+    description => "Allow check out of already checked out items through SIP",
+    up          => sub {
         my ($args) = @_;
         my ( $dbh, $out ) = @$args{qw(dbh out)};
 
-        $dbh->do(q{
+        $dbh->do(
+            q{
             INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type` ) VALUES
             ('AllowItemsOnLoanCheckoutSIP','0','','Do not generate ISSUED_TO_ANOTHER warning when checking out items already checked out to someone else via SIP. This allows self checkouts for those items.','YesNo')
-        });
+        }
+        );
 
         say $out "Added new system preference 'AllowItemsOnLoanCheckoutSIP'";
     },
