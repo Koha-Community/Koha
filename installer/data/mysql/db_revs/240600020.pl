49090d9860da6d35747d1ff206f09a03948dff44
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2024-07-23 17:00:14 +0100

    Bug 36996: DBRev 24.06.00.020
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- installer/data/mysql/db_revs/240600020.pl
+++ installer/data/mysql/db_revs/240600020.pl
@@ -14,8 +14,8 @@ return {
             INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type` ) VALUES
             ('z3950Status','','','This syspref allows to define custom YAML based rules for marking items unavailable in z3950 results.','Textarea')
         }
-        );
-
-        say $out "Added new system preference 'Z3950Status'";
+            ) != '0E0'
+            ? say_success( $out, "Added new system preference 'Z3950Status'" )
+            : say_info( $out, "System preference 'Z3950Status' already exists" );
     },
 };
