adae95debee124f42e8ed04e6f315cd294d653ab
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2024-08-02 14:46:43 +0100

    Bug 37256: Add 'PUT' for circulation rule sets
    Sponsored-by: Glasgow Colleges Library Group <https://library.cityofglasgowcollege.ac.uk>
    Signed-off-by: George Harkins <George.Harkins@cityofglasgowcollege.ac.uk>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- Koha/REST/V1/CirculationRules.pm
+++ Koha/REST/V1/CirculationRules.pm
@@ -179,4 +179,90 @@ sub list_rules {
     };
 }
 
+=head3 set_rules
+
+Set rules for the given patron/item/branch combination
+
+=cut
+
+sub set_rules {
+    my $c = shift->openapi->valid_input or return;
+
+    return try {
+        my $body = $c->req->json;
+
+        my $item_type       = $body->{context}->{item_type_id};
+        my $branchcode      = $body->{context}->{library_id};
+        my $patron_category = $body->{context}->{patron_category_id};
+
+        if ( $item_type eq '*' ) {
+            $item_type = undef;
+        } else {
+            my $type = Koha::ItemTypes->find($item_type);
+            return $c->render_invalid_parameter_value(
+                {
+                    path   => '/body/context/item_type_id',
+                    values => {
+                        uri   => '/api/v1/item_types',
+                        field => 'item_type_id'
+                    }
+                }
+            ) unless $type;
+        }
+
+        if ( $branchcode eq '*' ) {
+            $branchcode = undef;
+        } else {
+            my $library = Koha::Libraries->find($branchcode);
+            return $c->render_invalid_parameter_value(
+                {
+                    path   => '/body/context/library_id',
+                    values => {
+                        uri   => '/api/v1/libraries',
+                        field => 'library_id'
+                    }
+                }
+            ) unless $library;
+        }
+
+        if ( $patron_category eq '*' ) {
+            $patron_category = undef;
+        } else {
+            my $category = Koha::Patron::Categories->find($patron_category);
+            return $c->render_invalid_parameter_value(
+                {
+                    path   => '/body/context/patron_category_id',
+                    values => {
+                        uri   => '/api/v1/patron_categories',
+                        field => 'patron_category_id'
+                    }
+                }
+            ) unless $category;
+        }
+
+        my $rules = {%$body};
+        delete $rules->{context};
+
+        my $new_rules = Koha::CirculationRules->set_rules(
+            {
+                categorycode => $patron_category,
+                itemtype     => $item_type,
+                branchcode   => $branchcode,
+                rules        => $rules,
+            }
+        );
+
+        # TODO: Add error handling for rule scope exceptions thrown in Koha::CirculationRules::set_rule
+
+        my $return = { map { $_->rule_name => $_->rule_value } @{$new_rules} };
+        $return->{context} =
+            { library_id => $branchcode, patron_category_id => $patron_category, item_type_id => $item_type };
+
+        return $c->render(
+            status  => 200,
+            openapi => $return
+        );
+    }
+}
+
 1;
