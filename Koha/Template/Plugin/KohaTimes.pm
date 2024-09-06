aecfaa5911e7ca99f73b28d3ab24bfa7f45fb661
CJ Lynce <cj.lynce@westlakelibrary.org>
2024-09-06 20:51:38 +0000

    Bug 36594: Library hours in koha/admin/branches.pl now adhere to selected time and calendar system preferences.
    -Newly created or edited libraries no longer display 'null' for undefined
    	open and close times.
    -Libraries without any defined hours will state such instead of displaying the
    	hours table.
    -The CalendarFirstDayOfWeek system preferences is now respected when viewing
    	a library with defined hours.
    -Time displays and inputs now follow the TimeFormat system preference.
    -Times are no longer displayed with seconds.
    
    As part of this patch, a new Template Filter, KohaTimes, has been added to
    	allow for proper formatting of time strings based on systems preferences.
    
    To Test:
    1. Login to staff interface
    2. Go to Koha administration > Basic parameters > Libraries
    3. Edit any library or create a new library. Do not set hours.
    4. Save the library.
    	*Note that on the library list, in the hours column, all days are displayed
    	with 'null'.
    5. Edit or create a 2nd library.
    6. Try to manually enter a time (not using the flatpickr modal)
    	*Note that the mask wants a date format, not a time format.
    7. Set some hours for the library, leaving at least one
    	day without hours. Save.
    	*Note that in the library list, in the hours column, defined times are in
    	the format HH:MM:SS.
    7. Edit the system preference CalendarFirstDayOfWeek to set a day other
    	than Sunday as the first day of the week.
    8. Return to Koha administration > Basic parameters > Libraries
    9. Click on the name of a specific library with hours to view details.
    	*Note that the order of the calendar weeek still starts with Sunday.
    10. The order of the calendar week still starts with Sunday.
    11. Edit the system preference TimeFormat and set to 12-hours.
    12. Return to Koha administration > Basic parameters > Libraries.
    	*Note that the time format on the branches list is not in 12-hour format.
    13. Click on the name of a specific library with hours to view details.
    	*Note that the time format in the branch details screen is not in 12-hour
    	format.
    
    14. Apply Patch.
    
    15. Return to Koha administration > Basic parameters > Libraries
    16. Verify that your library with all undefined times shows "Library hours
    	not set".
    17. Verify that your library with defined hours is showing times in	HH:MM
    	format, and days without defined hours are blank.
    18. Verify that your library with defined hours is respecting the TimeFormat
    	preferences.
    19. Click on the name of a library with hours to view details.
    20. Verify that the hours respect the CalendarFirstDayOfWeek and TimeFormat
    	preferences.
    21. Click on the name of another library without hours to view details.
    	Verify these libraries show as "No opening hours have been set"
    	in the branch detail view.
    22. Edit a library's hours. When inputing, try to manually enter a time (not
    	using the flatpickr modal). Verify that you can enter time in the proper
    	(12 or 24 hours) format.
    
    Sponsored-by: Westlake Porter Public Library
    Signed-off-by: Olivier V <olivier.vezina@inLibro.com>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

---
+++ Koha/Template/Plugin/KohaTimes.pm
@@ -0,0 +1,41 @@
+package Koha::Template::Plugin::KohaTimes;
+
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
+use Template::Plugin::Filter;
+use base qw( Template::Plugin::Filter );
+
+our $DYNAMIC = 1;
+
+sub filter {
+    my ( $self, $text, $args, $config ) = @_;
+    return "" unless $text;
+
+    my ( $hours, $minutes, $seconds ) = split( ':', $text );
+    if ( C4::Context->preference('TimeFormat') == "12hr" ) {
+        my $ampm = ( $hours >= 12 ) ? 'pm' : 'am';
+        $hours = ( $hours == 0 ) ? "12"            : $hours;
+        $hours = ( $hours > 12 ) ? ( $hours - 12 ) : $hours;
+        $hours = sprintf '%.2u', $hours;
+
+        return "$hours:$minutes $ampm";
+    } else {
+        return "$hours:$minutes";
+    }
+}
+
+1;
