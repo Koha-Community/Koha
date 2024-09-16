d17c640510d0a17dec2928bf1124c9d27872818a
Katrin Fischer <katrin.fischer@bsz-bw.de>
2024-09-16 11:17:49 +0000

    Bug 37592: DBRev 24.06.00.029
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- installer/data/mysql/db_revs/240600029.pl
+++ installer/data/mysql/db_revs/240600029.pl
@@ -25,10 +25,10 @@ return {
         }
 
         my $creation_date_statement = <<~'SQL';
-            ALTER TABLE bookings ADD COLUMN creation_date DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'The datetime for when a bookings was created'
+            ALTER TABLE bookings ADD COLUMN creation_date DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'The datetime for when a bookings was created' AFTER end_date
         SQL
         my $modification_date_statement = <<~'SQL';
-            ALTER TABLE bookings ADD COLUMN modification_date DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'The datetime for when a booking has been updated'
+            ALTER TABLE bookings ADD COLUMN modification_date DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'The datetime for when a booking has been updated' AFTER modification_date
         SQL
         if ( @{$existing_columns} == 0 ) {
             if ( $dbh->do("$creation_date_statement AFTER `end_date`") ) {
