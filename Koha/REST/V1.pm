c692261d4be557a64084b5cf2c9a61087f9286d3
Lari Taskula <lari.taskula@hypernova.fi>
2024-09-18 08:24:47 +0000

    Bug 28907: Add Koha::REST::Plugin::Auth::PublicRoutes
    To test:
    1. prove t/db_dependent/Koha/REST/Plugin/Auth/PublicRoutes.t
    
    Signed-off-by: Victor Grousset/tuxayo <victor@tuxayo.net>
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Signed-off-by: Paul Derscheid <paul.derscheid@lmscloud.de>

--- Koha/REST/V1.pm
+++ Koha/REST/V1.pm
@@ -159,6 +159,7 @@ sub startup {
     $self->plugin('Koha::REST::Plugin::Exceptions');
     $self->plugin('Koha::REST::Plugin::Responses');
     $self->plugin('Koha::REST::Plugin::Auth::IdP');
+    $self->plugin('Koha::REST::Plugin::Auth::PublicRoutes');
     $self->plugin( 'Mojolicious::Plugin::OAuth2' => $oauth_configuration );
 }
 
