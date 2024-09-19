71b7ad60e5cfae77afa5180d1ba5d14525a2d542
Lari Taskula <lari.taskula@hypernova.fi>
2024-09-19 21:07:04 +0000

    Bug 37969: Add missing language code nor
    Signed-off-by: Magnus Enger <magnus@libriotech.no>
    Test plan:
    - Start KTD with Elastic: ktd --es7 up
    - Edit a record, e.g. "Dom Casmurro : a novel" and change the language
      code in 008/35-37 from "eng" to "nor":
      Before: 980914s1997    nyu           00011 eng b
      After:  980914s1997    nyu           00011 nor b
    - Do a search that finds the edited record and some other records,
      so you can see the list of resulots and the facets, e.g. "dom"
    - Verify that "nor" is shown in the languages facet
    - Apply this patch and run koha-upgrade-schema kohadev
    - Do the search again and verify that "Norwegian" is now shown instead
      of "nor"
    
    Signed-off-by: Thomas Klausner <domm@plix.at>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

---
+++ installer/data/mysql/atomicupdate/bug_37969_add_missing_language_code_nor.pl
@@ -0,0 +1,26 @@
+use Modern::Perl;
+use Koha::Installer::Output qw(say_warning say_failure say_success say_info);
+
+return {
+    bug_number  => "37969",
+    description => "Add nor language code",
+    up          => sub {
+        my ($args) = @_;
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
+
+        $dbh->do(q{ INSERT INTO language_descriptions(subtag, type, lang, description) VALUES ( 'nb', 'language', 'no', 'Norsk bokmål'); });
+        $dbh->do(q{ INSERT INTO language_descriptions(subtag, type, lang, description) VALUES ( 'nn', 'language', 'no', 'Norsk nynorsk'); });
+        $dbh->do(q{ INSERT INTO language_subtag_registry( subtag, type, description, added) VALUES ( 'no', 'language', 'Norwegian','2024-09-19' ); });
+        $dbh->do(q{ INSERT INTO language_rfc4646_to_iso639(rfc4646_subtag,iso639_2_code) VALUES ( 'no','nor'); });
+        $dbh->do(q{ INSERT INTO language_descriptions(subtag, type, lang, description) VALUES ( 'no', 'language', 'nb', 'Norsk'); });
+        $dbh->do(q{ INSERT INTO language_descriptions(subtag, type, lang, description) VALUES ( 'no', 'language', 'nn', 'Norsk'); });
+        $dbh->do(q{ INSERT INTO language_descriptions(subtag, type, lang, description) VALUES ( 'no', 'language', 'no', 'Norsk'); });
+        $dbh->do(q{ INSERT INTO language_descriptions(subtag, type, lang, description) VALUES ( 'no', 'language', 'en', 'Norwegian'); });
+        $dbh->do(q{ INSERT INTO language_descriptions(subtag, type, lang, description) VALUES ( 'no', 'language', 'fr', 'Norvégien'); });
+        $dbh->do(q{ INSERT INTO language_descriptions(subtag, type, lang, description) VALUES ( 'no', 'language', 'de', 'Norwegisch'); });
+        $dbh->do(q{ INSERT INTO language_descriptions(subtag, type, lang, description) VALUES ( 'no', 'language', 'pl', 'Norweski'); });
+        $dbh->do(q{ INSERT INTO language_descriptions(subtag, type, lang, description) VALUES ( 'NO', 'region', 'no', 'Noreg'); });
+
+        say $out "Added nor language code";
+    },
+};
