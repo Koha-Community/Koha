08a8ea0f88031df718865394e16f608137c781fb
Katrin Fischer <katrin.fischer@bsz-bw.de>
2024-09-27 15:57:49 +0000

    Bug 37856: DBRev 24.06.00.032
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- installer/data/mysql/db_revs/240600032.pl
+++ installer/data/mysql/db_revs/240600032.pl
@@ -3,7 +3,7 @@ use Koha::Installer::Output qw(say_warning say_failure say_success say_info);
 
 return {
     bug_number  => "37856",
-    description => "Add service_platform column to erm_usage_data_providers",
+    description => "Add column 'erm_usage_data_providers.service_platform'",
     up          => sub {
         my ($args) = @_;
         my ( $dbh, $out ) = @$args{qw(dbh out)};
@@ -14,8 +14,8 @@ return {
               ALTER TABLE erm_usage_data_providers ADD COLUMN `service_platform` varchar(80) DEFAULT NULL COMMENT 'platform if provider requires it' AFTER `report_types`
           }
             );
-            say_info( $out, "Bug 37856 - Added service_platform column to table 'erm_usage_data_providers'" );
+            say_info( $out, "Added column 'service_platformerm_usage_data_providers'" );
         }
-        say_success( $out, "Bug 37856 - Done" );
+        say_success( $out, "Done" );
     },
 };
