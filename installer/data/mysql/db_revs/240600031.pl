18c889c40285c1d90f9c40ec0d8d727fb516307c
Katrin Fischer <katrin.fischer@bsz-bw.de>
2024-09-27 15:07:51 +0000

    Bug 23685: DBRev 24.06.00.031
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- installer/data/mysql/db_revs/240600031.pl
+++ installer/data/mysql/db_revs/240600031.pl
@@ -3,7 +3,7 @@ use Koha::Installer::Output qw(say_warning say_failure say_success say_info);
 
 return {
     bug_number  => "23685",
-    description => "Add prefs ReportsExportFormatODS and ReportsExportLimit",
+    description => "Add system preferences ReportsExportFormatODS and ReportsExportLimit",
     up          => sub {
         my ($args) = @_;
         my ( $dbh, $out ) = @$args{qw(dbh out)};
@@ -16,6 +16,6 @@ return {
             ('ReportsExportLimit',NULL,NULL,'Limit for report downloads','Integer');
         }
         );
-        say $out "Added new system preferences: ReportsExportFormatODS and ReportsExportLimit";
+        say_success( $out, "Added new system preferences 'ReportsExportFormatODS' and 'ReportsExportLimit'" );
     },
 };
