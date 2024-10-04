bfe67da62d939240eed64decab48633284c6c41f
Kyle M Hall <kyle@bywatersolutions.com>
2024-10-04 13:00:26 -0400

    Bug 37070: (QA follow-up) Fix qa script issues
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- C4/Barcodes.pm
+++ C4/Barcodes.pm
@@ -94,11 +94,12 @@ sub max {
 	return $self->db_max;
 }
 sub db_max {
-	my $self = shift;
-	my $query = "SELECT max(abs(CAST(barcode AS UNSIGNED))) FROM items LIMIT 1"; # Possible problem if multiple barcode types populated
-	my $sth = C4::Context->dbh->prepare($query);
-	$sth->execute();
-	return $sth->fetchrow_array || $self->initial;
+    my $self  = shift;
+    my $query = "SELECT max(abs(CAST(barcode AS UNSIGNED))) FROM items LIMIT 1"
+        ;    # Possible problem if multiple barcode types populated
+    my $sth = C4::Context->dbh->prepare($query);
+    $sth->execute();
+    return $sth->fetchrow_array || $self->initial;
 }
 sub next_value {
 	my $self = shift;
