8b5069960c2474fbb8fa811304f553de25090090
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2024-10-16 11:43:48 +0200

    Bug 36640: Adjust pending orders
    Replace fnGetColumnData
    
    No need delay, it's not ajax
    
    Signed-off-by: Jake Deery <jake.deery@ptfs-europe.com>
    Signed-off-by: Lisette Scheer <lisette@bywatersolutions.com>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- koha-tmpl/intranet-tmpl/prog/js/datatables.js
+++ koha-tmpl/intranet-tmpl/prog/js/datatables.js
@@ -81,40 +81,6 @@ function toggledClearFilter(searchText, tableId){
     }
 }
 
-
-// Return an array of string containing the values of a particular column
-$.fn.dataTableExt.oApi.fnGetColumnData = function ( oSettings, iColumn, bUnique, bFiltered, bIgnoreEmpty ) {
-    // check that we have a column id
-    if ( typeof iColumn == "undefined" ) return new Array();
-    // by default we only wany unique data
-    if ( typeof bUnique == "undefined" ) bUnique = true;
-    // by default we do want to only look at filtered data
-    if ( typeof bFiltered == "undefined" ) bFiltered = true;
-    // by default we do not wany to include empty values
-    if ( typeof bIgnoreEmpty == "undefined" ) bIgnoreEmpty = true;
-    // list of rows which we're going to loop through
-    var aiRows;
-    // use only filtered rows
-    if (bFiltered == true) aiRows = oSettings.aiDisplay;
-    // use all rows
-    else aiRows = oSettings.aiDisplayMaster; // all row numbers
-
-    // set up data array
-    var asResultData = new Array();
-    for (var i=0,c=aiRows.length; i<c; i++) {
-        iRow = aiRows[i];
-        var aData = this.fnGetData(iRow);
-        var sValue = aData[iColumn];
-        // ignore empty values?
-        if (bIgnoreEmpty == true && sValue.length == 0) continue;
-        // ignore unique values?
-        else if (bUnique == true && jQuery.inArray(sValue, asResultData) > -1) continue;
-        // else push the value onto the result data array
-        else asResultData.push(sValue);
-    }
-    return asResultData;
-}
-
 // List of unbind keys (Ctrl, Alt, Direction keys, etc.)
 // These keys must not launch filtering
 var blacklist_keys = new Array(0, 16, 17, 18, 37, 38, 39, 40);
