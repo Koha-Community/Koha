80e37da75f2fc0dc2aab1e2740a01495bc1202b2
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2024-10-17 10:00:34 +0200

    Bug 33484: Add 'Copied!' tooltip when the button is clicked
    Signed-off-by: Pedro Amorim <pedro.amorim@ptfs-europe.com>
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- koha-tmpl/intranet-tmpl/prog/js/datatables.js
+++ koha-tmpl/intranet-tmpl/prog/js/datatables.js
@@ -779,7 +779,7 @@ function _dt_buttons(params){
                     let url = window.location.origin + window.location.pathname + '?' + searchParams.toString() + window.location.hash;
                     if( navigator.clipboard && navigator.clipboard.writeText){
                         navigator.clipboard.writeText( url );
-                        // TODO Add tooltip "State copied to the clipboard"
+                        $(node).tooltip({trigger: 'manual', title: _("Copied!")}).tooltip('show');
                     }
                 },
             }
