7cea5b6f348271a5530bd7a6e3097fc2e91db8e2
Katrin Fischer <katrin.fischer@bsz-bw.de>
2024-10-21 10:25:37 +0000

    Bug 36798: DBRev 24.06.00.037
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- installer/data/mysql/db_revs/240600037.pl
+++ installer/data/mysql/db_revs/240600037.pl
@@ -3,7 +3,7 @@ use Koha::Installer::Output qw(say_warning say_failure say_success say_info);
 
 return {
     bug_number  => "36798",
-    description => "Add `SearchCancelledAndInvalidISBNandISSN` preference",
+    description => "Add 'SearchCancelledAndInvalidISBNandISSN' preference",
     up          => sub {
         my ($args) = @_;
         my ( $dbh, $out ) = @$args{qw(dbh out)};
