336ef3a0191329444d28ce3fb36d44ab7cec426b
Katrin Fischer <katrin.fischer@bsz-bw.de>
2024-10-24 13:08:01 +0000

    Bug 30955: DBRev 26.04.00.041
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

---
+++ installer/data/mysql/db_revs/240600041.pl
@@ -0,0 +1,17 @@
+use Modern::Perl;
+use Koha::Installer::Output qw(say_warning say_failure say_success say_info);
+
+return {
+    bug_number  => "30955",
+    description => "Move existing list notices to new lists category",
+    up          => sub {
+        my ($args) = @_;
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
+
+        $dbh->do(q{ UPDATE IGNORE letter SET module = 'lists' WHERE code = 'SHARE_ACCEPT'; });
+        say_success( $out, "Moved SHARE_ACCEPT notice to lists module" );
+
+        $dbh->do(q{ UPDATE IGNORE letter SET module = 'lists' WHERE code = 'SHARE_INVITE'; });
+        say_success( $out, "Moved SHARE_INVITE notice to lists module" );
+    },
+};
