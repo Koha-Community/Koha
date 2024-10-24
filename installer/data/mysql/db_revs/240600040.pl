4a6352376ec41820ecdb1ad777aa7fdebb7ade6e
Katrin Fischer <katrin.fischer@bsz-bw.de>
2024-10-24 08:45:15 +0000

    Bug 23295: DBRev 24.06.00.040
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- installer/data/mysql/db_revs/240600040.pl
+++ installer/data/mysql/db_revs/240600040.pl
@@ -7,13 +7,21 @@ return {
     up          => sub {
         my ($args) = @_;
         my ( $dbh, $out ) = @$args{qw(dbh out)};
+
+        $dbh->do(
+            q{ INSERT IGNORE INTO systempreferences (variable, value, options, explanation, type) VALUES ('RestrictPatronsWithFailedNotices', '0', NULL, 'If enabled then when SMS and email notices fail sending at the Koha level then a debarment will be applied to a patrons account', 'YesNo') }
+        );
+
+        say_success ( $out, "Added new system preference 'RestrictPatronsWithFailedNotices'" );
+
         try {
             $dbh->do(
                 q{INSERT IGNORE INTO restriction_types (code, display_text, is_system, is_default) VALUES ('NOTICE_FAILURE_SUSPENSION', 'Notice failure suspension', 1, 0)}
             );
-            say_success( $out, "Added a new system restriction_types 'NOTICE_FAILURE_SUSPENSION'" );
+            say_success( $out, "Added new system restriction type 'NOTICE_FAILURE_SUSPENSION'" );
         } catch {
             say_failure( $out, "Database modification failed with errors: $_" );
         };
+
     },
 };
