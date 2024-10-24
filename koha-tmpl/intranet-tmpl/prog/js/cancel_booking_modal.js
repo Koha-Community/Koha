c5d0cc271ea6e5da6a11acff3e3e97003a484015
Paul Derscheid <paul.derscheid@lmscloud.de>
2024-10-24 14:57:04 +0000

    Bug 38222: (follow-up) Only replace first numerical match in bookings count containers
    Sponsored-by: Büchereizentrale Schleswig-Holstein <https://www.bz-sh.de/>
    Signed-off-by: LEBSimonsen <simonsen@bz-sh.de>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- koha-tmpl/intranet-tmpl/prog/js/cancel_booking_modal.js
+++ koha-tmpl/intranet-tmpl/prog/js/cancel_booking_modal.js
@@ -60,7 +60,10 @@
             return;
         }
 
-        bookingsCount.innerHTML = parseInt(bookingsCount.innerHTML, 10) - 1;
+        bookingsCount.innerHTML = bookingsCount.innerHTML.replace(
+            /\d+/,
+            match => parseInt(match, 10) - 1
+        );
     }
 
     function handleShowBsModal(e) {
