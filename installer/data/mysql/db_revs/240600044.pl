1f16d490f12da637c26a206749e64c0ac3e72a6a
Katrin Fischer <katrin.fischer@bsz-bw.de>
2024-10-25 09:05:04 +0000

    Bug 28833: DBRev 24.06.00.044
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

---
+++ installer/data/mysql/db_revs/240600044.pl
@@ -0,0 +1,19 @@
+use Modern::Perl;
+use Koha::Installer::Output qw(say_warning say_failure say_success say_info);
+
+return {
+    bug_number  => '28833',
+    description => 'Speed up holds queue builder via parallel processing',
+    up          => sub {
+        my ($args) = @_;
+        my ( $dbh, $out ) = @{$args}{qw(dbh out)};
+
+    $dbh->do(q{
+        INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type` ) VALUES
+        ('HoldsQueueParallelLoopsCount', '1', NULL, 'Number of parallel loops to use when running the holds queue builder', 'Integer');
+    });
+    
+    say_success( $out, "Added new system preference 'HoldsQueueParallelLoopsCount'");
+
+    },
+};
