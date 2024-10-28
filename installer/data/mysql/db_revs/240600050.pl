f2e7fba8c9a2f4c9a05b979ac8548250a30c6245
Katrin Fischer <katrin.fischer@bsz-bw.de>
2024-10-28 18:46:21 +0000

    Bug 30648: DBRev 24.06.00.050
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- installer/data/mysql/db_revs/240600050.pl
+++ installer/data/mysql/db_revs/240600050.pl
@@ -3,7 +3,7 @@ use Koha::Installer::Output qw(say_warning say_failure say_success say_info);
 
 return {
     bug_number  => "30648",
-    description => "Store biblionumber of deleted record in old reserves",
+    description => "Store biblionumber of deleted records in old_reserves",
     up          => sub {
         my ($args) = @_;
         my ( $dbh, $out ) = @$args{qw(dbh out)};
@@ -12,20 +12,20 @@ return {
             $dbh->do(
                 q{
                 ALTER TABLE `reserves`
-                    ADD COLUMN `deleted_biblionumber` int(11) NULL DEFAULT NULL COMMENT 'links the reserve to the deleted bibliographic record (deletedbiblio.biblionumber)' AFTER biblionumber
+                    ADD COLUMN `deleted_biblionumber` int(11) NULL DEFAULT NULL COMMENT 'links the hold to the deleted bibliographic record (deletedbiblio.biblionumber)' AFTER biblionumber
             }
             );
         }
-        say $out "Added column 'reserves.deleted_biblionumber'";
+        say_success( $out, "Added column 'reserves.deleted_biblionumber'");
         if ( !column_exists( 'old_reserves', 'deleted_biblionumber' ) ) {
 
             $dbh->do(
                 q{
                 ALTER TABLE `old_reserves`
-                    ADD COLUMN `deleted_biblionumber` int(11) NULL DEFAULT NULL COMMENT 'links the reserve to the deleted bibliographic record (deletedbiblio.biblionumber)' AFTER biblionumber
+                    ADD COLUMN `deleted_biblionumber` int(11) NULL DEFAULT NULL COMMENT 'links the hold to the deleted bibliographic record (deletedbiblio.biblionumber)' AFTER biblionumber
             }
             );
         }
-        say $out "Added column 'old_reserves.deleted_biblionumber'";
+        say_success( $out, "Added column 'old_reserves.deleted_biblionumber'");
     },
 };
