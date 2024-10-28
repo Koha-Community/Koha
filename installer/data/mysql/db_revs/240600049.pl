3d3f68acf7c4f5ccc28298af029cdde46af5713a
Katrin Fischer <katrin.fischer@bsz-bw.de>
2024-10-28 18:26:41 +0000

    Bug 38274: DBRev 24.06.00.049
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

---
+++ installer/data/mysql/db_revs/240600049.pl
@@ -0,0 +1,16 @@
+use Modern::Perl;
+use Koha::Installer::Output qw(say_warning say_failure say_success say_info);
+
+return {
+    bug_number  => "38274",
+    description => "Fix typo in Arabic language description",
+    up          => sub {
+        my ($args) = @_;
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
+
+        $dbh->do(
+            q{ UPDATE language_descriptions SET description='العربية' WHERE subtag='ar' AND type='language' AND lang='ar' }
+        );
+        say_success( $out, "Updated Arabic language description" );
+    },
+};
