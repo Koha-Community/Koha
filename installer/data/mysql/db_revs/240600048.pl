167a498ea290dae60eda2050c6434070e88498a1
Katrin Fischer <katrin.fischer@bsz-bw.de>
2024-10-28 13:16:19 +0000

    Bug 33484: DBRev 24.06.00.48
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- installer/data/mysql/db_revs/240600048.pl
+++ installer/data/mysql/db_revs/240600048.pl
@@ -1,24 +1,29 @@
 use Modern::Perl;
+use Koha::Installer::Output qw(say_warning say_failure say_success say_info);
 
 return {
-    bug_number => "33484",
+    bug_number  => "33484",
     description => "Add state save as an option to datatables",
-    up => sub {
+    up          => sub {
         my ($args) = @_;
-        my ($dbh, $out) = @$args{qw(dbh out)};
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
         if ( !column_exists( 'tables_settings', 'default_save_state' ) ) {
-            $dbh->do(q{
+            $dbh->do(
+                q{
                 ALTER TABLE tables_settings
                   ADD COLUMN default_save_state tinyint(1) DEFAULT 1 AFTER default_sort_order
-            });
-            say $out "Added column 'tables_settings.default_save_state'";
+            }
+            );
+            say_success( $out, "Added column 'tables_settings.default_save_state'" );
         }
         if ( !column_exists( 'tables_settings', 'default_save_state_search' ) ) {
-            $dbh->do(q{
+            $dbh->do(
+                q{
                 ALTER TABLE tables_settings
                   ADD COLUMN default_save_state_search tinyint(1) DEFAULT 0 AFTER default_save_state
-            });
-            say $out "Added column 'tables_settings.default_save_state_search'";
+            }
+            );
+            say_success( $out, "Added column 'tables_settings.default_save_state_search'" );
         }
     },
 };
