4c15fb996db0cecc8b161ba82f9aafafaaed3e43
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2024-10-29 13:14:54 +0000

    Bug 36822: atomicupdate
    Fix any existing updated_on issues
    
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>
    (cherry picked from commit 473f918ba876b7343ced24b20b21d80d7a33abaf)
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

---
+++ installer/data/mysql/atomicupdate/bug_36822.pl
@@ -0,0 +1,28 @@
+use Modern::Perl;
+use Koha::Installer::Output qw(say_warning say_failure say_success say_info);
+
+return {
+    bug_number  => "36822",
+    description => "Sanitize borrowers.updated_on",
+    up          => sub {
+        my ($args) = @_;
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
+
+        my $sth = $dbh->prepare(
+            q{
+            SELECT borrowernumber
+            FROM borrowers
+            WHERE updated_on = '0000-00-00 00:00:00'
+        }
+        );
+        $sth->execute;
+        my $results = $sth->fetchall_arrayref( {} );
+
+        if (@$results) {
+            sanitize_zero_date( 'borrowers', 'updated_on' );
+            say_info( $out, "The following borrowers' updated_on has been sanitized: "
+                    . join( ', ', map { $_->{borrowernumber} } @$results ) );
+        }
+
+    },
+};
