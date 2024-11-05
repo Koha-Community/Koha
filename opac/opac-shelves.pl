5cf526588a4732ecb239467325e42dd2c3607b85
Lucas Gass <lucas@bywatersolutions.com>
2024-11-05 22:13:32 +0000

    Bug 38362: Fix printing lists on opac/opac-shelves.pl
    To test from the OPAC:
    1 - create a list with more than 10 items
    2 - print the list -> there is a pagination and that only 10 items
      are printed
    3 - Apply patch
    4 - print the list again -> every items are being printed
    
    Signed-off-by: Sam Sowanick <sam.sowanick@corvallisoregon.gov>
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    [EDIT] Tidied: added a few spaces.
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>
    (cherry picked from commit 8d0e7ff906ae4e6b1367307e7dfdead909676bcb)
    Signed-off-by: wainuiwitikapark <wainuiwitikapark@catalyst.net.nz>
    (cherry picked from commit 29a6f938e4a59a79c5e55bccfbb32c1401e7f211)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- opac/opac-shelves.pl
+++ opac/opac-shelves.pl
@@ -317,7 +317,9 @@ if ( $op eq 'view' ) {
             $sortfield = 'title' if !$sortfield or !grep { $_ eq $sortfield } qw( title author copyrightdate itemcallnumber dateadded );
 
             my $rows;
-            unless ( $query->param('print') or $query->param('rss') ) {
+            if ( $query->param('print') or $query->param('rss') ) {
+                $page = "";
+            } else {
                 $rows = C4::Context->preference('OPACnumSearchResults') || 20;
             }
             my $order_by = $sortfield eq 'itemcallnumber' ? 'items.cn_sort' : $sortfield;
