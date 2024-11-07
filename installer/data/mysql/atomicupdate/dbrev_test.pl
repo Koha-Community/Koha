0be29ed976a682bc85f669860850ac331fcca633
Emily Lamancusa <emily.lamancusa@montgomerycountymd.gov>
2024-11-07 10:20:01 -0500

    Bug 38299: [DO NOT PUSH] Test dbrev
    Signed-off-by: David Nind <david@davidnind.com>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

---
+++ installer/data/mysql/atomicupdate/dbrev_test.pl
@@ -0,0 +1,44 @@
+use Modern::Perl;
+use Koha::Installer::Output qw(say_warning say_failure say_success say_info);
+
+return {
+    bug_number  => "BUG_NUMBER",
+    description => "Test dbrev",
+    up          => sub {
+        my ($args) = @_;
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
+
+        say_success($out, "Success!");
+        say_info($out, "Info...");
+        say_warning($out, "Warning!!");
+        say_failure($out, "Failure :(");
+
+        # Do you stuffs here
+        $dbh->do(q{CREATE TABLE borrowers});
+
+        # Print useful stuff here
+        # tables
+        say $out "Added new table 'XXX'";
+        say $out "Added column 'XXX.YYY'";
+
+        # sysprefs
+        say $out "Added new system preference 'XXX'";
+        say $out "Updated system preference 'XXX'";
+        say $out "Removed system preference 'XXX'";
+
+        # permissions
+        say $out "Added new permission 'XXX'";
+
+        # letters
+        say $out "Added new letter 'XXX' (TRANSPORT)";
+
+        # HTML customizations
+        say $out "Added 'XXX' HTML customization";
+
+        # Other information
+        say_failure( $out, "Use red for danger/failure" );
+        say_success( $out, "Use green for success" );
+        say_warning( $out, "Use yellow for warning/a call to action" );
+        say_info( $out, "Use blue for further information" );
+    },
+};
