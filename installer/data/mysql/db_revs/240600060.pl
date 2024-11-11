84962355961fc4e558d28764e7abbbb0453018d9
Katrin Fischer <katrin.fischer@bsz-bw.de>
2024-11-11 13:46:50 +0000

    Bug 33766: DBRev 24.06.00.060
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

---
+++ installer/data/mysql/db_revs/240600060.pl
@@ -0,0 +1,19 @@
+use Modern::Perl;
+use Koha::Installer::Output qw(say_warning say_failure say_success say_info);
+
+return {
+    bug_number  => "33766",
+    description => "Add system preference to determine label of userid input on login form",
+    up          => sub {
+        my ($args) = @_;
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
+
+        # Do you stuffs here
+        $dbh->do(
+            q{INSERT IGNORE INTO systempreferences (variable,value) VALUES ('OPACLoginLabelTextContent','cardnumberorusername')  }
+        );
+
+        # sysprefs
+        say_success( $out, "Added new system preference 'OPACLoginLabelTextContent'" );
+    },
+};
