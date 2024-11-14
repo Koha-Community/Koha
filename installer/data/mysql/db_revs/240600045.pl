9a316e042b68496aef9aa07c48b1366c41736806
Thibaud Guillot <thibaud.guillot@biblibre.com>
2024-11-14 10:15:08 +0000

    Bug 38434: Fix to define the same structure as kohastructure.sql
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- installer/data/mysql/db_revs/240600045.pl
+++ installer/data/mysql/db_revs/240600045.pl
@@ -11,7 +11,7 @@ return {
         if ( !column_exists( 'itemtypes', 'bookable' ) ) {
             $dbh->do(
                 q{
-                ALTER TABLE itemtypes ADD COLUMN bookable INT(1) DEFAULT 0 AFTER automatic_checkin
+                ALTER TABLE itemtypes ADD COLUMN bookable tinyint(1) NOT NULL DEFAULT 0 AFTER automatic_checkin
             }
             );
 
