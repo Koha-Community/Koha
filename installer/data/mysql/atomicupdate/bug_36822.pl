3b2019442823eda3a719649cb2ff643a37ab0089
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2024-11-14 13:08:16 +0000

    Bug 36822: (follow-up) Catch MySQL 8 failure in update
    We add a CAST to the fetch of 0000-00-00 dates in the database. This
    prevents an error in MySQL 8.0 throws htat aborts the update.
    
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>
    (cherry picked from commit 36a387db22b43f0e8302eea42e3d279f28d7c076)
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

--- installer/data/mysql/atomicupdate/bug_36822.pl
+++ installer/data/mysql/atomicupdate/bug_36822.pl
@@ -12,17 +12,15 @@ return {
             q{
             SELECT borrowernumber
             FROM borrowers
-            WHERE updated_on = '0000-00-00 00:00:00'
+            WHERE CAST(updated_on AS CHAR(19)) = '0000-00-00 00:00:00'
         }
         );
         $sth->execute;
         my $results = $sth->fetchall_arrayref( {} );
-
         if (@$results) {
             sanitize_zero_date( 'borrowers', 'updated_on' );
             say_info( $out, "The following borrowers' updated_on has been sanitized: "
                     . join( ', ', map { $_->{borrowernumber} } @$results ) );
         }
-
     },
 };
