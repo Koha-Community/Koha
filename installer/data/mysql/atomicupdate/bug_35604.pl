87a020be6392c16d0633f90c2389568ca9d0f2f3
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2024-11-14 12:46:11 +0000

    Bug 35604: atomicupdate
    Sponsored-by: NHS England
    Sponsored-by: PTFS Europe Ltd
    Signed-off-by: David Nind <david@davidnind.com>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

---
+++ installer/data/mysql/atomicupdate/bug_35604.pl
@@ -0,0 +1,16 @@
+use Modern::Perl;
+use Koha::Installer::Output qw(say_warning say_success say_info);
+
+return {
+    bug_number  => "35604",
+    description => "Add new AutoILLBackendPriority system preferences",
+    up          => sub {
+        my ($args) = @_;
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
+
+        $dbh->do(
+            q{ INSERT IGNORE INTO systempreferences (variable,value,options,explanation,type) VALUES ('AutoILLBackendPriority','',NULL,'Set the automatic backend selection priority','ILLBackends'); }
+        );
+        say_success( $out, "Added new system preference 'AutoILLBackendPriority'" );
+    },
+};
