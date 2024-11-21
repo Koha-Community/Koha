00fe3f8b557af3e134b9f8dcccfe4153448ed77d
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2024-11-21 14:07:31 +0100

    Bug 38503: Have a generic buildSampleObject[s]
    Signed-off-by: Victor Grousset/tuxayo <victor@tuxayo.net>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>
    (cherry picked from commit 3f880c5978bff78965f4bb2cb907c73347ca32f8)
    Signed-off-by: Paul Derscheid <paul.derscheid@lmscloud.de>

--- t/cypress/integration/t/mockData.ts
+++ t/cypress/integration/t/mockData.ts
@@ -2,7 +2,7 @@ import { mount } from "@cypress/vue";
 
 describe("Generate Random Patron", () => {
     it("should generate a random patron from the schema", () => {
-        cy.task("buildSamplePatron").then(mockPatron => {
+        cy.task("buildSampleObject", { object: "patron" }).then(mockPatron => {
             expect(mockPatron).to.have.property("patron_id");
         });
     });
@@ -10,26 +10,32 @@ describe("Generate Random Patron", () => {
 
 describe("Generate Random Patrons", () => {
     it("should generate 42 random patron from the schema", () => {
-        cy.task("buildSamplePatrons", 42).then(mockPatrons => {
-            expect(mockPatrons.length).to.equal(42);
-            expect(mockPatrons[0]).to.have.property("patron_id");
-        });
+        cy.task("buildSampleObjects", { object: "patron", count: 42 }).then(
+            mockPatrons => {
+                expect(mockPatrons.length).to.equal(42);
+                expect(mockPatrons[0]).to.have.property("patron_id");
+            }
+        );
     });
 });
 
 describe("Generate Random Library", () => {
     it("should generate a random library from the schema", () => {
-        cy.task("buildSampleLibrary").then(mockLibrary => {
-            expect(mockLibrary).to.have.property("library_id");
-        });
+        cy.task("buildSampleObject", { object: "library" }).then(
+            mockLibrary => {
+                expect(mockLibrary).to.have.property("library_id");
+            }
+        );
     });
 });
 
 describe("Generate Random Libraries", () => {
     it("should generate 42 random library from the schema", () => {
-        cy.task("buildSampleLibraries", 42).then(mockLibraries => {
-            expect(mockLibraries.length).to.equal(42);
-            expect(mockLibraries[0]).to.have.property("library_id");
-        });
+        cy.task("buildSampleObjects", { object: "library", count: 42 }).then(
+            mockLibraries => {
+                expect(mockLibraries.length).to.equal(42);
+                expect(mockLibraries[0]).to.have.property("library_id");
+            }
+        );
     });
 });
