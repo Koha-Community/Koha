b615e56388e2e26a712e856aea66993f8d58ed74
Lucas Gass <lucas@bywatersolutions.com>
2024-12-05 19:05:46 +0000

    Bug 37856: DBRev 23.11.10.004
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>
    (cherry picked from commit 1fb5bc4d8374ed149134bb8f945bf0c56c12ea5a)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- installer/data/mysql/db_revs/231110004.pl
+++ installer/data/mysql/db_revs/231110004.pl
@@ -1,5 +1,4 @@
 use Modern::Perl;
-use Koha::Installer::Output qw(say_warning say_failure say_success say_info);
 
 return {
     bug_number  => "37856",
@@ -14,8 +13,8 @@ return {
               ALTER TABLE erm_usage_data_providers ADD COLUMN `service_platform` varchar(80) DEFAULT NULL COMMENT 'platform if provider requires it' AFTER `report_types`
           }
             );
-            say_info( $out, "Bug 37856 - Added service_platform column to table 'erm_usage_data_providers'" );
+            say $out "Bug 37856 - Added service_platform column to table 'erm_usage_data_providers'";
         }
-        say_success( $out, "Bug 37856 - Done" );
+        say $out "Bug 37856 - Done";
     },
 };
