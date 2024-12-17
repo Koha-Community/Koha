2283abc42b95d25de09fc724a833eebc60e5dec5
wainuiwitikapark <wainuiwitikapark@catalyst.net.nz>
2024-12-17 23:36:21 +0000

    Bug 38522: DBRev 23.11.10.005
    Signed-off-by: wainuiwitikapark <wainuiwitikapark@catalyst.net.nz>
    (cherry picked from commit 279d05ceae36da5f474c0cd9dc69480cce899c1d)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- installer/data/mysql/db_revs/231110005.pl
+++ installer/data/mysql/db_revs/231110005.pl
@@ -1,5 +1,4 @@
 use Modern::Perl;
-use Koha::Installer::Output qw(say_warning say_success say_info);
 
 return {
     bug_number  => "38522",
@@ -9,6 +8,6 @@ return {
         my ( $dbh, $out ) = @$args{qw(dbh out)};
 
         $dbh->do(q{ALTER TABLE erm_agreements MODIFY COLUMN license_info mediumtext DEFAULT NULL COMMENT 'info about the license'});
-        say_success( $out, "Updated erm_agreements.license_info to mediumtext." );
+        say $out "Updated erm_agreements.license_info to mediumtext.";
     },
 };
