4dbe67f6f263c81081cfe68320857153d5dd76b2
Victor Grousset/tuxayo <victor@tuxayo.net>
2024-12-22 09:33:45 +0100

    Bug 38770: Remove @vue/cli-service and babel
    And the webpack related dead config.
    
    Test plan:
    1. Do not apply
    2. Switch to node 22
         sudo sed 's/18.x/22.x/g' -i /etc/apt/sources.list.d/nodesource.list
         sudo apt update ; sudo apt install nodejs
    3. Try to install node deps
         sudo yarn install --modules-folder /kohadevbox/node_modules
    4. It fails
         error @achrinza/node-ipc@9.2.5: The engine "node" is incompatible with this module.
         Expected version "8 || 9 || 10 || 11 || 12 || 13 || 14 || 15 || 16 || 17 || 18". Got "22.12.0"
    5. Apply patches
    6. Try to install node deps
         sudo yarn install --modules-folder /kohadevbox/node_modules
    7. It works! :)
    
    Signed-off-by: David Nind <david@davidnind.com>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- t/cypress/plugins/index.js
+++ t/cypress/plugins/index.js
@@ -1,13 +1,11 @@
-const { startDevServer } = require("@cypress/webpack-dev-server");
-const webpackConfig = require("@vue/cli-service/webpack.config.js");
+const { startDevServer } = require('@cypress/webpack-dev-server')
 
 module.exports = (on, config) => {
-    on("dev-server:start", options =>
-        startDevServer({
-            options,
-            webpackConfig,
-        })
-    );
+    on('dev-server:start', options =>
+      startDevServer({
+        options,
+      })
+    )
 
     return config;
 };
