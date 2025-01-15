57bd44ae0fe2b71bd1ae93519d1229fa6b09fda0
Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>
2025-01-15 10:42:19 +0000

    Bug 38899: Add a cypress test
    Signed-off-by: David Nind <david@davidnind.com>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

--- t/cypress/integration/Toolbar_spec.ts
+++ t/cypress/integration/Toolbar_spec.ts
@@ -33,4 +33,16 @@ describe("Sticky toolbar", () => {
         cy.get("#toolbar a").contains("Receive shipments").click();
         cy.get("h1").contains("Receive shipment from vendor " + vendor.name);
     });
+
+    it("Should stick on scroll", () => {
+        cy.visit("/cgi-bin/koha/acqui/acqui-home.pl");
+
+        cy.get("#toolbar").contains("New vendor").click();
+        cy.scrollTo("bottom");
+        cy.get("#toolbar").should("be.visible");
+        cy.get("#toolbar").should("have.class", "floating");
+
+        cy.scrollTo("top");
+        cy.get("#toolbar").should("not.have.class", "floating");
+    });
 });
