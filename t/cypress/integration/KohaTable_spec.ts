a3406f10a7351331716a4b3d9f96d12de1582464
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2025-01-20 13:38:46 +0100

    Bug 38461: Do not rely on biblio_id=1
    It might not exist!
    
    This code is not trivial and should ideally be moved to somewhere it
    could be reused easily. But good enough for now to fix the failing tests.
    
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>
    (cherry picked from commit bb2e88a1e3ebbed3d291cdf7450933aca0ff88ec)
    Signed-off-by: Paul Derscheid <paul.derscheid@lmscloud.de>

--- t/cypress/integration/KohaTable_spec.ts
+++ t/cypress/integration/KohaTable_spec.ts
@@ -480,8 +480,124 @@ describe("Hit all tables", () => {
 
     describe("catalogue/detail/holdings_table", () => {
         const table_id = "holdings_table";
-        it("correctly init", () => {
-            const biblio_id = 1;
+        beforeEach(() => {
+            // FIXME All the following code should not be reused as it
+            // It must be moved to a Cypress command or task "buildSampleBiblio" or even "insertSampleBiblio"
+            let generated_objects = {};
+            const objects = [{ object: "library" }, { object: "item_type" }];
+            cy.wrap(Promise.resolve())
+                .then(() => {
+                    return objects.reduce((chain, { object }) => {
+                        return chain.then(() => {
+                            return cy
+                                .task("buildSampleObject", { object })
+                                .then(attributes => {
+                                    generated_objects[object] = attributes;
+                                });
+                        });
+                    }, Promise.resolve());
+                })
+                .then(() => {
+                    const library = generated_objects["library"];
+                    const item_type = generated_objects["item_type"];
+                    const queries = [
+                        {
+                            query: "INSERT INTO branches(branchcode, branchname) VALUES (?, ?)",
+                            values: [library.library_id, library.name],
+                        },
+                        {
+                            query: "INSERT INTO itemtypes(itemtype, description) VALUES (?, ?)",
+                            values: [
+                                item_type.item_type_id,
+                                item_type.description,
+                            ],
+                        },
+                    ];
+                    cy.wrap(Promise.resolve())
+                        .then(() => {
+                            return queries.reduce(
+                                (chain, { query, values }) => {
+                                    return chain.then(() =>
+                                        cy.query(query, values)
+                                    );
+                                },
+                                Promise.resolve()
+                            );
+                        })
+                        .then(() => {
+                            let biblio = {
+                                leader: "     nam a22     7a 4500",
+                                fields: [
+                                    { "005": "20250120101920.0" },
+                                    {
+                                        "245": {
+                                            ind1: "",
+                                            ind2: "",
+                                            subfields: [
+                                                { a: "Some boring read" },
+                                            ],
+                                        },
+                                    },
+                                    {
+                                        "100": {
+                                            ind1: "",
+                                            ind2: "",
+                                            subfields: [
+                                                { c: "Some boring author" },
+                                            ],
+                                        },
+                                    },
+                                    {
+                                        "942": {
+                                            ind1: "",
+                                            ind2: "",
+                                            subfields: [
+                                                { c: item_type.item_type_id },
+                                            ],
+                                        },
+                                    },
+                                ],
+                            };
+                            cy.request({
+                                method: "POST",
+                                url: "/api/v1/biblios",
+                                auth: {
+                                    username: "koha",
+                                    password: "koha",
+                                },
+                                headers: {
+                                    "Content-Type": "application/marc-in-json",
+                                    "x-confirm-not-duplicate": 1,
+                                },
+                                body: biblio,
+                            }).then(response => {
+                                const biblio_id = response.body.id;
+                                cy.wrap(biblio_id).as("biblio_id");
+                                cy.request({
+                                    method: "POST",
+                                    url: `/api/v1/biblios/${biblio_id}/items`,
+                                    auth: {
+                                        username: "koha",
+                                        password: "koha",
+                                    },
+                                    headers: {
+                                        "Content-Type": "application/json",
+                                    },
+                                    body: {
+                                        home_library_id: library.library_id,
+                                        holding_library_id: library.library_id,
+                                    },
+                                });
+                            });
+                        });
+                });
+        });
+
+        afterEach(() => {});
+
+        it("Correctly init the table", function () {
+            // Do not use `() => {` or this.biblio_id won't be retrieved
+            const biblio_id = this.biblio_id;
             cy.task("buildSampleObjects", {
                 object: "item",
                 count: RESTdefaultPageSize,
@@ -526,6 +642,82 @@ describe("Hit all tables", () => {
                 cy.get(`#${table_id}_wrapper .dt-info`).contains(
                     `Showing 1 to ${RESTdefaultPageSize} of ${baseTotalCount} entries`
                 );
+            });
+        });
+
+        it("Show filters", function () {
+            // Do not use `() => {` or this.biblio_id won't be retrieved
+            const biblio_id = this.biblio_id;
+            cy.task("buildSampleObjects", {
+                object: "item",
+                count: RESTdefaultPageSize,
+                values: {
+                    biblio_id,
+                    checkout: null,
+                    transfer: null,
+                    lost_status: 0,
+                    withdrawn: 0,
+                    damaged_status: 0,
+                    not_for_loan_status: 0,
+                    course_item: null,
+                },
+            }).then(items => {
+                cy.intercept("get", `/api/v1/biblios/${biblio_id}/items*`, {
+                    statuscode: 200,
+                    body: items,
+                    headers: {
+                        "X-Base-Total-Count": baseTotalCount,
+                        "X-Total-Count": baseTotalCount,
+                    },
+                });
+
+                cy.visit(
+                    "/cgi-bin/koha/catalogue/detail.pl?biblionumber=" +
+                        biblio_id
+                );
+
+                // Hide the 'URL' column
+                mock_table_settings(
+                    {
+                        columns: { uri: { is_hidden: 1 } },
+                    },
+                    "items_table_settings.holdings"
+                );
+
+                cy.window().then(win => {
+                    win.libraries_map = items.reduce((map, i) => {
+                        map[i.library_id] = i.library_id;
+                        return map;
+                    }, {});
+                });
+
+                cy.get("@columns").then(columns => {
+                    cy.get(`#${table_id}_wrapper tbody tr`).should(
+                        "have.length",
+                        RESTdefaultPageSize
+                    );
+
+                    cy.get(`#${table_id} th`).contains("Status");
+                    cy.get(`#${table_id} th`)
+                        .contains("URL")
+                        .should("not.exist");
+                    cy.get(`#${table_id} th`)
+                        .contains("Course reserves")
+                        .should("not.exist");
+
+                    cy.get(".show_filters").click();
+                    cy.get(`#${table_id}_wrapper .dt-info`).contains(
+                        `Showing 1 to ${RESTdefaultPageSize} of ${baseTotalCount} entries`
+                    );
+
+                    cy.get(`#${table_id} th`).contains("Status");
+                    cy.get(`#${table_id} th`)
+                        .contains("URL")
+                        .should("not.exist");
+                    cy.get(`#${table_id} th`)
+                        .contains("Course reserves")
+                        .should("not.exist");
+                });
             });
         });
     });
