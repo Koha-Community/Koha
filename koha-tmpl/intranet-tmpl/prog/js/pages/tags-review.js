6e267dc90f2598c9bcc6cc22b9787abac11befa3
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2025-01-27 09:56:10 +0100

    Bug 38969: Prevent XSS in tags/review.pl and Prevent JS error when adding tags with quote at the OPAC
    Signed-off-by: Baptiste Wojtkowski <baptiste.wojtkowski@biblibre.com>
    Signed-off-by: David Cook <dcook@prosentient.com.au>
    Signed-off-by: wainuiwitikapark <wainuiwitikapark@catalyst.net.nz>

--- koha-tmpl/intranet-tmpl/prog/js/pages/tags-review.js
+++ koha-tmpl/intranet-tmpl/prog/js/pages/tags-review.js
@@ -151,7 +151,7 @@ $(document).ready(function() {
             $(event.target).text( __("Testing...") ).prop('disabled', true);
             $.ajax(Object.assign({}, {...review_ajax_params, type: "GET"}, {
                 data: {
-                    tag: $('#test').val(),
+                    tag: escape_str($("#test").val()),
                     op: 'test',
                 },
                 success: success_test_call // success_reject
