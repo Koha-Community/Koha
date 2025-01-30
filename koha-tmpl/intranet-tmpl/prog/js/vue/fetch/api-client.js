72c45bd209a5faed91fca6095e5c146fbaa6511d
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2025-01-30 09:15:12 +0100

    Bug 38993: Use the @fetch alias
    Signed-off-by: Paul Derscheid <paul.derscheid@lmscloud.de>
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- koha-tmpl/intranet-tmpl/prog/js/vue/fetch/api-client.js
+++ koha-tmpl/intranet-tmpl/prog/js/vue/fetch/api-client.js
@@ -1,14 +1,14 @@
 import HttpClient from "./http-client";
 
-import ERMAPIClient from "./../../fetch/erm-api-client";
-import PatronAPIClient from "./../../fetch/patron-api-client";
-import AcquisitionAPIClient from "./../../fetch/acquisition-api-client";
-import AdditionalFieldsAPIClient from "./../../fetch/additional-fields-api-client";
-import AVAPIClient from "./../../fetch/authorised-values-api-client";
-import ItemAPIClient from "./../../fetch/item-api-client";
-import RecordSourcesAPIClient from "./../../fetch/record-sources-api-client";
-import SysprefAPIClient from "./../../fetch/system-preferences-api-client";
-import PreservationAPIClient from "./../../fetch/preservation-api-client";
+import ERMAPIClient from "@fetch/erm-api-client";
+import PatronAPIClient from "@fetch/patron-api-client";
+import AcquisitionAPIClient from "@fetch/acquisition-api-client";
+import AdditionalFieldsAPIClient from "@fetch/additional-fields-api-client";
+import AVAPIClient from "@fetch/authorised-values-api-client";
+import ItemAPIClient from "@fetch/item-api-client";
+import RecordSourcesAPIClient from "@fetch/record-sources-api-client";
+import SysprefAPIClient from "@fetch/system-preferences-api-client";
+import PreservationAPIClient from "@fetch/preservation-api-client";
 
 export const APIClient = {
     erm: new ERMAPIClient(HttpClient),
