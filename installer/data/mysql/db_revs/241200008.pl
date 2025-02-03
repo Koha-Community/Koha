0cf04c11db8226d5868532e16671c1f1c45080cd
Katrin Fischer <katrin.fischer@bsz-bw.de>
2025-02-03 13:16:23 +0000

    Bug 35028: DBRev 24.12.00.008
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- installer/data/mysql/db_revs/241200008.pl
+++ installer/data/mysql/db_revs/241200008.pl
@@ -1,8 +1,9 @@
 use Modern::Perl;
+use Koha::Installer::Output qw(say_warning say_success say_info);
 
 return {
     bug_number  => '35028',
-    description => 'Add system preference PatronSelfRegistrationAlert',
+    description => "Add system preference 'PatronSelfRegistrationAlert'",
     up          => sub {
         my ($args) = @_;
         my ( $dbh, $out ) = @$args{qw(dbh out)};
@@ -11,5 +12,6 @@ return {
             INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type` ) VALUES
             ('PatronSelfRegistrationAlert','0',NULL,'If enabled, an alter will be shown on staff interface home page when there are self-registered patrons.','YesNo')
         " );
+        say_success( $out, "Added new system preference 'PatronSelfRegistrationAlert'" );
     },
 };
