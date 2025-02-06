4f2ee1640d4f9f916d4d4e30b20000309c874779
Andrew Fuerste Henry <andrew@bywatersolutions.com>
2025-02-06 21:51:09 +0000

    Bug 39062: Increase stocknumber length in items and deleteditems
    Signed-off-by: David Cook <dcook@prosentient.com.au>
    Signed-off-by: Baptiste Wojtkowski <baptiste.wojtkowski@biblibre.com>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

---
+++ installer/data/mysql/atomicupdate/bug_39062_increase_stocknumber_length.pl
@@ -0,0 +1,24 @@
+use Modern::Perl;
+use Koha::Installer::Output qw(say_warning say_success say_info);
+
+return {
+    bug_number  => "39062",
+    description => "Increase the size of items.stocknumber and deleteditems.stocknumber",
+    up          => sub {
+        my ($args) = @_;
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
+
+        # Do you stuffs here
+        $dbh->do(
+            q{ALTER TABLE items MODIFY COLUMN stocknumber varchar(80) DEFAULT NULL COMMENT 'inventory number (MARC21 952$i)'}
+        );
+        $dbh->do(
+            q{ALTER TABLE deleteditems MODIFY COLUMN stocknumber varchar(80) DEFAULT NULL COMMENT 'inventory number (MARC21 952$i)'}
+        );
+
+        # Print useful stuff here
+        # tables
+        say $out "Increased length of items.stocknumber";
+        say $out "Increased length of deleteditems.stocknumber";
+    },
+};
