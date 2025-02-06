6945fa70b82fa4aa0fe56610f600bf351109ab9d
Caroline Cyr La Rose <caroline.cyr-la-rose@inlibro.com>
2025-02-06 16:11:55 -0500

    Bug 39061: Allow translation context in vue files
    This patch adds the possibility to add translation context clues in vue
    files.
    
    Syntax is
    
    $__p("context","term")
    
    To test, see bug 39045.
    
    Signed-off-by: Martin Renvoize <martin.renvoize@openfifth.co.uk>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- koha-tmpl/intranet-tmpl/prog/js/vue/i18n/index.js
+++ koha-tmpl/intranet-tmpl/prog/js/vue/i18n/index.js
@@ -2,8 +2,13 @@ export const $__ = key => {
     return window["__"](key);
 };
 
+export const $__p = (ctx, key) => {
+    return window["__p"](ctx, key);
+};
+
 export default {
     install: (app, options) => {
         app.config.globalProperties.$__ = $__;
+        app.config.globalProperties.$__p = $__p;
     },
 };
