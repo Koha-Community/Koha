7752b0e7cfea38561b12ae44e155a039197a2894
Victor Grousset/tuxayo <victor@tuxayo.net>
2025-02-12 19:02:43 +0100

    Bug 39132: Fix dbic: Revert changes to Schema.pm
    It's autogenerated and checksum protected so should be touch only by
    dbic.
    
    JD amended patch: just the revert to Koha/Schema.pm here
    Koha/Schema.pm will be skipped on bug 39115 (otherwise I will have to
    rebase)
    original title: Bug 39132: Fix dbic: Revert changes to Schema.pm and add it to denylist
    
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- Koha/Schema.pm
+++ Koha/Schema.pm
@@ -1,5 +1,4 @@
 use utf8;
-
 package Koha::Schema;
 
 # Created by DBIx::Class::Schema::Loader
@@ -12,8 +11,10 @@ use base 'DBIx::Class::Schema';
 
 __PACKAGE__->load_namespaces;
 
+
 # Created by DBIx::Class::Schema::Loader v0.07025 @ 2013-10-14 20:56:21
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:oDUxXckmfk6H9YCjW8PZTw
+
 
 # You can replace this text with custom content, and it will be preserved on regeneration
 1;
