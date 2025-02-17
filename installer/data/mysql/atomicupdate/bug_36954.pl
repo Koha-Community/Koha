754fef4bac8136343a3209e14898fbd4c8ffc7b2
Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
2025-02-17 09:15:59 +0000

    Bug 36954: (QA follow-up) Add a dbrev print message about sip.log
    With reference to BZ comment9 and comment10.
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

---
+++ installer/data/mysql/atomicupdate/bug_36954.pl
@@ -0,0 +1,14 @@
+use Modern::Perl;
+use Koha::Installer::Output qw(say_warning say_success say_info);
+
+return {
+    bug_number  => "36954",
+    description => "Adjustment for logfiles in koha-sip",
+    up          => sub {
+        my ($args) = @_;
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
+
+        say_warning( $out, "IMPORTANT: If you use SIP, please edit your /etc/koha/sites/[CLONE]/log4perl.conf at upgrade time." );
+        say_warning( $out, "Replace sip.log there by sip-output.log." );
+    },
+};
