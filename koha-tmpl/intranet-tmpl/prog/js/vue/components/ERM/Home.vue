4308261d62b51c36da5237bfc14373fa6212b009
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2025-02-21 15:25:17 -0100

    Bug 39320: Add dashboard to ERM home
    Sponsored-by: UKHSA (UK Health Security Agency)
    
    Signed-off-by: Jeremy Evans <Jeremy.Evans@ukhsa.gov.uk>
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

--- koha-tmpl/intranet-tmpl/prog/js/vue/components/ERM/Home.vue
+++ koha-tmpl/intranet-tmpl/prog/js/vue/components/ERM/Home.vue
@@ -1,5 +1,24 @@
-<template></template>
-
 <script>
-export default {};
+import ModuleDashboard from "../ModuleDashboard/ModuleDashboard.vue";
+import ERMCounts from "../ModuleDashboard/Widgets/ERMCounts.vue";
+import ERMLicensesNeedingAction from "../ModuleDashboard/Widgets/ERMLicensesNeedingAction.vue";
+import ERMRunUsageReport from "../ModuleDashboard/Widgets/ERMRunUsageReport.vue";
+import ERMLatestSUSHIJobs from "../ModuleDashboard/Widgets/ERMLatestSUSHIJobs.vue";
+import { markRaw } from "vue";
+
+export default {
+    extends: ModuleDashboard,
+    setup() {
+        return {
+            ...ModuleDashboard.setup({
+                availableWidgets: [
+                    markRaw(ERMRunUsageReport),
+                    markRaw(ERMLatestSUSHIJobs),
+                    markRaw(ERMCounts),
+                    markRaw(ERMLicensesNeedingAction),
+                ],
+            }),
+        };
+    },
+};
 </script>
