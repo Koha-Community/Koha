80da7d7809e53af48b451690e29fee0e3bfe6c0a
Katrin Fischer <katrin.fischer@bsz-bw.de>
2025-03-07 17:00:13 +0000

    Bug 32630: DBIC schema update
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- Koha/Schema/Result/Illrequest.pm
+++ Koha/Schema/Result/Illrequest.pm
@@ -320,7 +320,7 @@ __PACKAGE__->belongs_to(
   {
     is_deferrable => 1,
     join_type     => "LEFT",
-    on_delete     => "CASCADE",
+    on_delete     => "SET NULL",
     on_update     => "CASCADE",
   },
 );
@@ -391,8 +391,8 @@ __PACKAGE__->belongs_to(
 );
 
 
-# Created by DBIx::Class::Schema::Loader v0.07049 @ 2023-10-10 14:49:40
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:DQl+7uwLCz5GeqU2hBFmMA
+# Created by DBIx::Class::Schema::Loader v0.07051 @ 2025-03-07 16:59:58
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:+LJD6O0ZLhsml2/43CSgBg
 
 __PACKAGE__->has_many(
   "comments",
