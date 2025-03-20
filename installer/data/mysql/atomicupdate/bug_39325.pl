a82f7d19cb4551b45448bb37acf72760f3a0162e
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2025-03-20 09:54:46 +0100

    Bug 39325: Adjust DB
    We need to adjust kohastructure.sql, sysprefs.sql and the DB itself.
    
    Signed-off-by: Owen Leonard <oleonard@myacpl.org>
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

---
+++ installer/data/mysql/atomicupdate/bug_39325.pl
@@ -0,0 +1,109 @@
+use Modern::Perl;
+use Koha::Installer::Output qw(say_warning say_success say_info);
+
+return {
+    bug_number  => "39325",
+    description => "Fix some spelling mistakes",
+    up          => sub {
+        my ($args) = @_;
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
+
+        # avaiable ==> available
+        $dbh->do(
+            q{
+            UPDATE systempreferences
+            SET explanation="If enabled, item fields in the MARC record will be made available to XSLT sheets. Otherwise they will be removed."
+            WHERE variable="PassItemMarcToXSLT"
+        }
+        );
+
+        # foriegn ==> foreign
+        $dbh->do(
+            q{
+            ALTER TABLE deletedbiblio
+            MODIFY `frameworkcode` varchar(4) NOT NULL DEFAULT '' COMMENT 'foreign key from the biblio_framework table to identify which framework was used in cataloging this record'
+        }
+        );
+
+        # addres ==> address
+        # attemps ==> attempts
+        # profesionals ==> professionals
+        $dbh->do(
+            q{
+            ALTER TABLE borrowers
+            MODIFY `emailpro` mediumtext DEFAULT NULL COMMENT 'the secondary email address for your patron/borrower''s primary address',
+            MODIFY `login_attempts` int(4) NOT NULL DEFAULT 0 COMMENT 'number of failed login attempts',
+            MODIFY `contactname` longtext DEFAULT NULL COMMENT 'used for children and professionals to include surname or last name of guarantor or organization name'
+        }
+        );
+
+        # addres ==> address
+        # attemps ==> attempts
+        # profesionals ==> professionals
+        $dbh->do(
+            q{
+            ALTER TABLE deletedborrowers
+            MODIFY `emailpro` mediumtext DEFAULT NULL COMMENT 'the secondary email address for your patron/borrower''s primary address',
+            MODIFY `login_attempts` int(4) NOT NULL DEFAULT 0 COMMENT 'number of failed login attempts',
+            MODIFY `contactname` longtext DEFAULT NULL COMMENT 'used for children and professionals to include surname or last name of guarantor or organization name'
+        }
+        );
+
+        # agains ==> against
+        $dbh->do(
+            q{
+            ALTER TABLE club_holds
+            MODIFY `item_id` int(11) DEFAULT NULL COMMENT 'If item-level, the id for the item the hold has been placed against'
+        }
+        );
+
+        # intented ==> intended
+        $dbh->do(
+            q{
+            ALTER TABLE items
+            MODIFY `new_status` varchar(32) DEFAULT NULL COMMENT '''new'' value, you can put whatever free-text information. This field is intended to be managed by the automatic_item_modification_by_age cronjob.'
+        }
+        );
+
+        # intented ==> intended
+        $dbh->do(
+            q{
+            ALTER TABLE deleteditems
+            MODIFY `new_status` varchar(32) DEFAULT NULL COMMENT '''new'' value, you can put whatever free-text information. This field is intended to be managed by the automatic_item_modification_by_age cronjob.'
+        }
+        );
+
+        # definining ==> defining
+        $dbh->do(
+            q{
+            ALTER TABLE housebound_profile
+            MODIFY `frequency` mediumtext NOT NULL COMMENT 'The Authorised_Value defining the pattern for delivery.'
+        }
+        );
+
+        # goup ==> group
+        $dbh->do(
+            q{
+            ALTER TABLE library_groups
+            MODIFY `title` varchar(100) DEFAULT NULL COMMENT 'Short description of the group'
+        }
+        );
+
+        # shoud ==> should
+        $dbh->do(
+            q{
+            ALTER TABLE stockrotationstages
+            MODIFY `duration` int(11) NOT NULL DEFAULT 4 COMMENT 'The number of days items should occupy this stage'
+        }
+        );
+
+        # thist ==> this
+        $dbh->do(
+            q{
+            ALTER TABLE virtualshelfshares
+            MODIFY `invitekey` varchar(10) DEFAULT NULL COMMENT 'temporary string used in accepting the invitation to access this list; not-empty means that the invitation has not been accepted yet'
+        }
+        );
+
+    },
+};
--- installer/data/mysql/db_revs/220600059.pl
+++ installer/data/mysql/db_revs/220600059.pl
@@ -8,11 +8,11 @@ return {
         my ( $dbh, $out ) = @$args{qw(dbh out)};
 
         # Add PASSWORD_CHANGE notice
+        my $message = "Dear [% borrower.firstname %] [% borrower.surname %],\r\n\r\n"
+            . "We want to notify you that your password has been changed. If you did not change it yourself (or requested that change), please contact library staff.\r\n\r\nYour library.";
         $dbh->do(
-            q{
-            INSERT IGNORE INTO letter (module, code, name, title, content, message_transport_type) VALUES ('members', 'PASSWORD_CHANGE', 'Notification of password change', 'Library account password change notification',
-            "Dear [% borrower.firstname %] [% borrower.surname %],\r\n\r\new want to notify you that your password has been changed. If you did not change it yourself (or requested that change), please contact library staff.\r\n\r\nYour library.", 'email');
-        }
+            q{INSERT IGNORE INTO letter (module, code, name, title, content, message_transport_type) VALUES ('members', 'PASSWORD_CHANGE', 'Notification of password change', 'Library account password change notification',?, 'email');},
+            $message
         );
 
         say $out "Added new letter 'PASSWORD_CHANGE' (email)";
