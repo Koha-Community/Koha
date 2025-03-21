3e506d58ab3259af2a49b653802a1c5448eb24c7
Nick Clemens <nick@bywatersolutions.com>
2025-03-21 13:58:24 +0000

    Bug 39408: Don't attempt to send a message if one is not generated
    The routine to create the message can return nothing if there is no content,
    we should handle this case and not attempt to send.
    
    To test:
    1 - Test plan from last patch
    2 - Apply this patch
    3 - POST to patron API again
    4 - Success!
    
    Signed-off-by: Claire Hernandez <claire.hernandez@biblibre.com>
    Signed-off-by: Martin Renvoize <martin.renvoize@openfifth.co.uk>
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>
    (cherry picked from commit c1fa513be9ad460402cece1726cf9df8126e8379)
    Signed-off-by: Paul Derscheid <paul.derscheid@lmscloud.de>
    (cherry picked from commit 91da4b4b3837abf6019fe0be6b00ace7cb4afe94)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

--- Koha/REST/V1/Patrons.pm
+++ Koha/REST/V1/Patrons.pm
@@ -123,7 +123,6 @@ sub add {
                             module      => 'members',
                             letter_code => 'WELCOME',
                             branchcode  => $patron->branchcode,
-                            ,
                             lang   => $patron->lang || 'default',
                             tables => {
                                 'branches'  => $patron->branchcode,
@@ -141,7 +140,9 @@ sub add {
                                     message_transport_type => 'email'
                                 }
                             );
-                            SendQueuedMessages( { message_id => $message_id } );
+
+                            # Don't send a message if we didn't generate one for some reason
+                            SendQueuedMessages( { message_id => $message_id } ) if $message_id;
                         }
                     }
                 }
