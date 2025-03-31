519df374729a032823cc39d852d4571ee5f67b76
Lucas Gass <lucas@bywatersolutions.com>
2025-03-31 09:58:15 +0000

    Bug 37211: Update permission name to edit_accountline_notes
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- installer/data/mysql/atomicupdate/bug_37211.pl
+++ installer/data/mysql/atomicupdate/bug_37211.pl
@@ -3,7 +3,7 @@ use Koha::Installer::Output qw(say_warning say_failure say_success say_info);
 
 return {
     bug_number  => "37211",
-    description => "Add permission updatecharges:edit_notes",
+    description => "Add permission updatecharges:edit_accountline_notes",
     up          => sub {
         my ($args) = @_;
         my ( $dbh, $out ) = @$args{qw(dbh out)};
@@ -12,12 +12,12 @@ return {
         $dbh->do(
             q{
             INSERT IGNORE INTO permissions (module_bit, code, description)
-            VALUES (10, 'edit_notes', 'Edit accountline notes')
+            VALUES (10, 'edit_accountline_notes', 'Edit accountline notes')
         }
         );
 
         # permissions
-        say_success( $out, "Added new permission 'updatecharges:edit_notes'" );
+        say_success( $out, "Added new permission 'updatecharges:edit_accountline_notes'" );
 
     },
 };
