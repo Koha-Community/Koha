9f44cb8a88a8f1e2b6a92e68cbdafda6317114ae
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2025-04-07 10:44:21 +0200

    Bug 39567: Include form-submit code from js_includes.inc (staff interface)
    Import the form-submit.js script in the global includes file for the
    staff interface, so that all pages have access to it without needing to
    import it directly.
    
    Signed-off-by: David Nind <david@davidnind.com>
    Signed-off-by: Martin Renvoize <martin.renvoize@openfifth.co.uk>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- koha-tmpl/intranet-tmpl/prog/js/form-submit.js
+++ koha-tmpl/intranet-tmpl/prog/js/form-submit.js
@@ -1,38 +1,35 @@
-$(document).ready(function () {
-    $("body").on("click", ".submit-form-link", function (e) {
-        e.preventDefault();
-        let form_data = $(this).data();
+export function submit_form(form) {
+    let form_data = $(form).data();
 
-        let confirm_msg = form_data.confirmationMsg;
-        if (confirm_msg) {
-            let confirmation = confirm(confirm_msg);
-            if (!confirmation) {
-                return false;
-            }
-            delete form_data.confirmationMsg;
+    let confirm_msg = form_data.confirmationMsg;
+    if (confirm_msg) {
+        let confirmation = confirm(confirm_msg);
+        if (!confirmation) {
+            return false;
         }
+        delete form_data.confirmationMsg;
+    }
 
-        let the_form = $("<form/>");
-        if (form_data.method === "post") {
-            form_data.csrf_token = $('meta[name="csrf-token"]').attr("content");
-        }
-        the_form.attr("method", form_data.method);
-        the_form.attr("action", form_data.action);
-        delete form_data.method;
-        delete form_data.action;
-        $.each(form_data, function (key, value) {
-            the_form.append(
-                $("<input/>", {
-                    type: "hidden",
-                    name: key,
-                    value: value,
-                })
-            );
-        });
-        if (form_data.new_tab) {
-            the_form.attr("target", "_blank");
-        }
-        $("body").append(the_form);
-        the_form.submit();
+    let the_form = $("<form/>");
+    if (form_data.method === "post") {
+        form_data.csrf_token = $('meta[name="csrf-token"]').attr("content");
+    }
+    the_form.attr("method", form_data.method);
+    the_form.attr("action", form_data.action);
+    delete form_data.method;
+    delete form_data.action;
+    $.each(form_data, function (key, value) {
+        the_form.append(
+            $("<input/>", {
+                type: "hidden",
+                name: key,
+                value: value,
+            })
+        );
     });
-});
+    if (form_data.new_tab) {
+        the_form.attr("target", "_blank");
+    }
+    $("body").append(the_form);
+    the_form.submit();
+}
