1db6734bfdddd1f90dd700a1f802d8e188eaca68
Lucas Gass <lucas@bywatersolutions.com>
2025-04-11 20:41:17 +0000

    Bug 39618: Add index to borrowers.preferred_name column
    To test:
    1. APPLY PATCH
    2. updatedatabase
    
    Signed-off-by: Roman Dolny <roman.dolny@jezuici.pl>
    Signed-off-by: Martin Renvoize <martin.renvoize@openfifth.co.uk>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>
    (cherry picked from commit 9276f33d4b93d44b3e44386053cd32a27af29fb3)
    Signed-off-by: Fridolin Somers <fridolin.somers@biblibre.com>

---
+++ installer/data/mysql/atomicupdate/bug_39618_add_preferred_name_key.pl
@@ -0,0 +1,21 @@
+use Modern::Perl;
+use Koha::Installer::Output qw(say_warning say_success say_info);
+
+return {
+    bug_number  => "39618",
+    description => "Add index to borrowers.preferred_name",
+    up          => sub {
+        my ($args) = @_;
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
+
+        unless ( index_exists( 'borrowers', 'preferred_name_idx' ) ) {
+            $dbh->do(
+                q{
+                ALTER TABLE `borrowers`
+                ADD KEY `preferred_name_idx` (`preferred_name`)
+            }
+            );
+            say_success( $out, "Added index to borrowers.preferred_name" );
+        }
+    },
+};
