5986c6649c7958565b221268f8e72c791a01189a
Katrin Fischer <katrin.fischer@bsz-bw.de>
2025-04-14 06:50:40 +0000

    Bug 37211: DBRev 24.12.00.024
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- installer/data/mysql/db_revs/241200024.pl
+++ installer/data/mysql/db_revs/241200024.pl
@@ -3,7 +3,7 @@ use Koha::Installer::Output qw(say_warning say_failure say_success say_info);
 
 return {
     bug_number  => "37211",
-    description => "Add permission updatecharges:edit_accountline_notes",
+    description => "Add new permission for editing accountline notes",
     up          => sub {
         my ($args) = @_;
         my ( $dbh, $out ) = @$args{qw(dbh out)};
