2ae1f8ba35a75542af18cf7bb9078e61a78ded0f
Tomas Cohen Arazi <tomascohen@theke.io>
2025-04-17 09:44:32 -0300

    Bug 36197: Atomic update style update
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- installer/data/mysql/atomicupdate/bug_36197.pl
+++ installer/data/mysql/atomicupdate/bug_36197.pl
@@ -1,4 +1,6 @@
 use Modern::Perl;
+use Koha::Installer::Output qw(say_warning say_success say_info);
+
 return {
     bug_number  => "36197",
     description => "Add new ILLOpacUnauthenticatedRequest system preferences",
@@ -9,6 +11,6 @@ return {
         $dbh->do(
             q{ INSERT IGNORE INTO systempreferences (variable,value,options,explanation,type) VALUES ('ILLOpacUnauthenticatedRequest','',NULL,'Can OPAC users place ILL requests without having to be logged in','YesNo'); }
         );
-        say $out "Added new system preference 'ILLOpacUnauthenticatedRequest'";
+        say_success( $out, "Added new system preference 'ILLOpacUnauthenticatedRequest'" );
     },
 };
