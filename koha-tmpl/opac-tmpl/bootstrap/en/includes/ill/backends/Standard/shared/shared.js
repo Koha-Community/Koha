b559cd37eaf1f75c402186980d84f0dc8910e3b5
Tomas Cohen Arazi <tomascohen@theke.io>
2025-04-17 16:40:26 -0300

    Bug 38340: (follow-up) Refactor and tidy template files
    Signed-off-by: Martin Renvoize <martin.renvoize@openfifth.co.uk>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

---
+++ koha-tmpl/opac-tmpl/bootstrap/en/includes/ill/backends/Standard/shared/shared.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", function () {
+    $("#add-new-fields").click(function (e) {
+        e.preventDefault();
+        var row =
+            '<li class="form-horizontal">' +
+            '<input type="text" class="custom-name ' +
+            "form-control input-fluid custom-field-input" +
+            '" name="custom_key" placeholder="' +
+            __("key") +
+            '">' +
+            " " +
+            '<input type="text" id="custom-value" name="custom_value" class="' +
+            "form-control input-fluid custom-field-input" +
+            '" placeholder="' +
+            __("value") +
+            '"> ' +
+            '<button type="button" class="btn btn-danger btn-sm ' +
+            'delete-new-field">' +
+            '<span class="fa fa-trash-can"></span> ' +
+            __("Delete") +
+            "</button></li>";
+        $("#standard-fields").append(row);
+    });
+    $("#standard-fields").on("click", ".delete-new-field", function (event) {
+        event.preventDefault();
+        $(event.target).parent().remove();
+    });
+    $("#type").change(function () {
+        $("#create_form").prepend(
+            '<input type="hidden" name="change_type" value="1" />'
+        );
+        $("#create_form").submit();
+    });
+    $("#standard-fields").on("keyup", ".custom-name", function () {
+        var val = $(this).val();
+        if (core.indexOf(val.toLowerCase()) > -1) {
+            $("#custom-warning")
+                .text(__("The name '%s' is not permitted").format(val))
+                .show();
+            $("#ill-submit").attr("disabled", true);
+            $("#add-new-fields").attr("disabled", true);
+        } else {
+            $("#custom-warning").hide();
+            $("#ill-submit").attr("disabled", false);
+            $("#add-new-fields").attr("disabled", false);
+        }
+    });
+});
