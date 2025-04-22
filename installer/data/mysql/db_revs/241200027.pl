7ee729af4dd28f49e47b9bbc2c40b6d9cd498ff8
Katrin Fischer <katrin.fischer@bsz-bw.de>
2025-04-22 06:45:45 +0000

    Bug 39171: DBRev 24.12.00.027
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- installer/data/mysql/db_revs/241200027.pl
+++ installer/data/mysql/db_revs/241200027.pl
@@ -1,8 +1,9 @@
 use Modern::Perl;
+use Koha::Installer::Output qw(say_warning say_success say_info);
 
 return {
     bug_number  => "39171",
-    description => "Rename ElasticsearchMARCFormat syspref options",
+    description => "Rename ElasticsearchMARCFormat system preference options",
     up          => sub {
         my ($args) = @_;
         my ( $dbh, $out ) = @$args{qw(dbh out)};
@@ -13,6 +14,6 @@ return {
         $dbh->do(
             q{ UPDATE systempreferences SET value = 'base64ISO2709' WHERE variable = 'ElasticsearchMARCFormat' AND value = 'ISO2709' }
         );
-        say $out "Renamed options for 'ElasticsearchMARCFormat' system preference";
+        say_success( $out, "Renamed options for 'ElasticsearchMARCFormat' system preference" );
     },
     }
