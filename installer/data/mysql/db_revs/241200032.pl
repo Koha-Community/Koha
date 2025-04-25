054793eac57f536235bd86099a5da24e7766fa6a
Katrin Fischer <katrin.fischer@bsz-bw.de>
2025-04-25 08:45:16 +0200

    Bug 39518: DBRev 24.12.00.032
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

--- installer/data/mysql/db_revs/241200032.pl
+++ installer/data/mysql/db_revs/241200032.pl
@@ -12,7 +12,7 @@ return {
             $dbh->do(
                 q{ ALTER TABLE marc_order_accounts ADD COLUMN `basket_name_field` varchar(10) DEFAULT NULL COMMENT 'the field that a vendor can use to include a basket name that will be used to create the basket for the file' AFTER match_value}
             );
-            say_success( $out, "Basket name field added successfully" );
+            say_success( $out, "Added column 'marc_order_accounts.basket_name_field'" );
         }
     },
 };
