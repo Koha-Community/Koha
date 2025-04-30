c7cf6952792b2b87c76901201e2d9cfc3fdf4113
Tadeusz „tadzik” Sośnierz <tadeusz@sosnierz.com>
2025-04-30 12:09:46 +0200

    Bug 39772: Extend background_tasks API to allow for human-readable job names
    Signed-off-by: David Nind <david@davidnind.com>
    Signed-off-by: Martin Renvoize <martin.renvoize@openfifth.co.uk>
    Signed-off-by: Jesse Maseto <jesse@bywatersolutions.com>

--- t/lib/plugins/Koha/Plugin/Test.pm
+++ t/lib/plugins/Koha/Plugin/Test.pm
@@ -365,7 +365,10 @@ sub intranet_catalog_biblio_tab {
 sub background_tasks {
     return {
         foo => 'MyPlugin::Class::Foo',
-        bar => 'MyPlugin::Class::Bar',
+        bar => {
+            class => 'MyPlugin::Class::Bar',
+            name  => "Bar task",
+        },
     };
 }
 
