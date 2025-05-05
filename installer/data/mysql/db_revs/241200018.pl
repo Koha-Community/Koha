ccc84b1a3273e68d13751b287fd32a96bddf18f7
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2025-05-05 12:53:10 +0200

    Bug 38663: Fix columns order in db_rev
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>

--- installer/data/mysql/db_revs/241200018.pl
+++ installer/data/mysql/db_revs/241200018.pl
@@ -18,7 +18,8 @@ return {
             say_info( $out, "Converting record_id from int(11) to VARCHAR(11)...\n" );
 
             $dbh->do(
-                q{ ALTER TABLE additional_field_values ADD COLUMN new_record_id VARCHAR(11) NOT NULL DEFAULT ''; });
+                q{ ALTER TABLE additional_field_values ADD COLUMN new_record_id VARCHAR(11) NOT NULL DEFAULT '' COMMENT "record_id" AFTER field_id; }
+            );
             $dbh->do(q{ UPDATE additional_field_values SET new_record_id = CAST(record_id AS CHAR(11)); });
             $dbh->do(q{ ALTER TABLE additional_field_values DROP COLUMN record_id; });
             $dbh->do(q{ ALTER TABLE additional_field_values RENAME COLUMN new_record_id TO record_id; });
