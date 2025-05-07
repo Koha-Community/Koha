b7227e873866ad96e36379688e52a8a07c1a4792
Nick Clemens <nick@bywatersolutions.com>
2025-05-07 14:45:23 +0000

    Bug 39452: (follow-up) Use INSERT IGNORE for system preference
    Signed-off-by: Martin Renvoize <martin.renvoize@openfifth.co.uk>
    Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

---
+++ installer/data/mysql/atomicupdate/bug_39452_add_cardnumberlog.pl
@@ -0,0 +1,21 @@
+use Modern::Perl;
+use Koha::Installer::Output qw(say_warning say_success say_info);
+
+return {
+    bug_number  => "39452",
+    description => "Add CardnumberLog Preference",
+    up          => sub {
+        my ($args) = @_;
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
+
+        # Do you stuffs here
+        $dbh->do(
+            q{
+            INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type` ) VALUES
+            ('CardnumberLog','1',NULL,'If ON, log edit actions on patron cardnumbers','YesNo')
+        }
+        );
+
+        say $out "Added new system preference 'CardnumberLog'";
+    },
+};
