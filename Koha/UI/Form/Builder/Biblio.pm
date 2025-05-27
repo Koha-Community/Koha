0ba11f98f99dee947c087cdd9b92838b772a7918
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2025-05-27 10:40:33 +0200

    Bug 40003: Remove warning from log when creating a new bib record
    Test plan:
    Hit cataloguing/addbiblio.pl?frameworkcode=FA
    
    Without this patch you get warnings in the Koha log file:
    Use of uninitialized value in string eq at /kohadevbox/koha/cataloguing/addbiblio.pl line 572.
    Use of uninitialized value in substitution (s///) at /kohadevbox/koha/Koha/UI/Form/Builder/Biblio.pm line 55.
    
    with this patch applied: no warning
    
    Signed-off-by: Owen Leonard <oleonard@myacpl.org>
    
    Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

--- Koha/UI/Form/Builder/Biblio.pm
+++ Koha/UI/Form/Builder/Biblio.pm
@@ -51,7 +51,7 @@ sub new {
 
     my $self = {};
 
-    if ( exists $params->{biblionumber} ) {
+    if ( defined $params->{biblionumber} ) {
         $self->{biblionumber} = $params->{biblionumber} =~ s/\D//gr;
 
         # just in case biblionumber obtained from CGI and passed directly here contains weird characters like spaces
