523fa34d6939a1ad82ab60b848503a33b2868f61
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2025-06-16 11:10:07 +0200

    Bug 40173: Add test for the api-client Cypress plugin
    Signed-off-by: Owen Leonard <oleonard@myacpl.org>
    Signed-off-by: Matt Blenkinsop <matt.blenkinsop@openfifth.co.uk>
    Signed-off-by: Martin Renvoize <martin.renvoize@openfifth.co.uk>
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>
    (cherry picked from commit 892b0f4d53b6f06cb6ee8bf61e557bc926cd46d9)
    Signed-off-by: Paul Derscheid <paul.derscheid@lmscloud.de>

--- t/cypress/plugins/index.js
+++ t/cypress/plugins/index.js
@@ -4,6 +4,8 @@ const { buildSampleObject, buildSampleObjects } = require("./mockData.js");
 
 const { query } = require("./db.js");
 
+const { apiGet, apiPost, apiPut, apiDelete } = require("./api-client.js");
+
 module.exports = (on, config) => {
     on("dev-server:start", options =>
         startDevServer({
@@ -15,6 +17,19 @@ module.exports = (on, config) => {
         buildSampleObject,
         buildSampleObjects,
         query,
+
+        apiGet(args) {
+            return apiGet({ ...args, baseUrl, authHeader });
+        },
+        apiPost(args) {
+            return apiPost({ ...args, baseUrl, authHeader });
+        },
+        apiPut(args) {
+            return apiPut({ ...args, baseUrl, authHeader });
+        },
+        apiDelete(args) {
+            return apiDelete({ ...args, baseUrl, authHeader });
+        },
     });
     return config;
 };
