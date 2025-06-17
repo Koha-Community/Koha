3706351ac1523eee07ae6aff59fe7868ec8b07de
Lucas Gass <lucas@bywatersolutions.com>
2025-06-17 13:57:49 +0000

    Bug 36135: Add ability to batch modify holds
    TO TEST:
    
    0. APPLY PATCHES, updatedatabase, restart_all
    1. Create a user with limited permissions, making sure you give them the "batch_modify_holds" permission
    2. Login as chosen patron.
    3. Navigate to Tools page.
    4. Note there's a link "Batch modify holds".
    5. Remove permission from user.
    6. Link to new tool is not displayed.
    
    7. Navigate to new tool Tools->Batch modify holds.
    8. Fill in some criteria to search holds with.
    9. Confirm that holds found match your search criteria.
    10. Set e.g. new expiration date for all or just selected holds.
    11. Confirm that holds were modified as they should have.
    12. Confirm this also from database.
    
    13. Find holds with a found/waiting status.
    14. Attempt to modify their suspend status.
    15. Alert box with text "One or more holds have found status and can't be suspended." should be displayed.
    16. Play around, test different modifications.
    
    17. Create SQL report which contains column for reserve_id.
    18. e.g "SELECT reserve_id FROM reserves limit 10".
    19.. Run report.
    20. Over report select "Batch operations" should be displayed and under that there should be option "Batch modify holds".
    21. Click "Batch modify holds" option.
    22. Confirm that holds displayed in "Batch mofidy holds" tool are same as found with your report.
    
    23. Do a holds search that will bring up multiple pages of results
    24. Try selecting some holds from different pages to modify
    25. Ensure all selected holds are being modified
    26. Play around, test different modifications.
    
    Sponsored-by: Koha-Suomi Oy
    Signed-off-by: Jason Robb <jrobb@sekls.org>
    Signed-off-by: Pedro Amorim <pedro.amorim@openfifth.co.uk>
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>
    (cherry picked from commit f769e3c1e643070ce4a79da4e1583d169d965b5c)
    Signed-off-by: Paul Derscheid <paul.derscheid@lmscloud.de>

---
+++ tools/batch_modify_holds.pl
@@ -0,0 +1,107 @@
+#!/usr/bin/perl
+
+# This file is part of Koha.
+#
+# Copyright 2024 Koha Development Team
+#
+# Koha is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General
+# Public License along with Koha; if not, see
+# <http://www.gnu.org/licenses>
+
+use Modern::Perl;
+use CGI;
+
+use JSON qw( to_json );
+
+use C4::Auth   qw( get_template_and_user );
+use C4::Output qw( output_html_with_http_headers );
+
+use Koha::DateUtils qw( dt_from_string );
+use Koha::Holds;
+
+my $input = CGI->new;
+my $op    = $input->param('op') // q|cud-form|;
+
+my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
+    {
+        template_name => 'tools/batch_modify_holds.tt',
+        query         => $input,
+        type          => "intranet",
+        flagsrequired => { tools => 'batch_modify_holds' },
+    }
+);
+
+my @hold_ids;
+
+if ( $op eq 'cud-form' ) {
+    my $reserve_ids_list = $input->param('reserve_ids_list') || undef;
+    if ($reserve_ids_list) {
+        my @reserve_ids = split /\n/, $reserve_ids_list;
+        $template->param( reserve_ids_list => to_json( \@reserve_ids ), );
+    }
+    $template->param( view => 'cud-form', );
+} elsif ( $op eq 'cud-modify' ) {
+    my $new_expiration_date = $input->param('new_expiration_date');
+    my $new_pickup_loc      = $input->param('new_pickup_loc');
+    my $new_suspend_status  = $input->param('new_suspend_status');
+    my $new_suspend_date    = $input->param('new_suspend_date');
+    my $new_hold_note       = $input->param('new_hold_note');
+    my $clear_hold_notes    = $input->param('clear_hold_notes');
+
+    @hold_ids = $input->multi_param('hold_id');
+
+    my $holds_to_update =
+        Koha::Holds->search( { reserve_id => { -in => \@hold_ids } }, { join => [ "item", "biblio" ] } );
+
+    while ( my $hold = $holds_to_update->next ) {
+
+        if ($new_expiration_date) {
+            $hold->expirationdate($new_expiration_date)->store;
+        }
+
+        if ( $new_pickup_loc && ( $hold->branchcode ne $new_pickup_loc ) ) {
+            $hold->branchcode($new_pickup_loc)->store;
+        }
+
+        if ( $new_suspend_status && $new_suspend_status ne "" && !$hold->is_found ) {
+            $hold->suspend(1)->store;
+            if ($new_suspend_date) {
+                $hold->suspend_until($new_suspend_date)->store;
+            } else {
+                $hold->suspend_until(undef)->store;
+            }
+        } elsif ( !$new_suspend_status && $new_suspend_date ) {
+            $hold->suspend(1)->store;
+            $hold->suspend_until($new_suspend_date)->store;
+        } else {
+            $hold->suspend(0)->store;
+            $hold->suspend_until(undef)->store;
+        }
+
+        if ($new_hold_note) {
+            $hold->reservenotes($new_hold_note)->store;
+        }
+
+        if ($clear_hold_notes) {
+            $hold->reservenotes(undef)->store;
+        }
+    }
+
+    $template->param(
+        updated_holds => to_json( \@hold_ids ),
+        view          => 'report',
+    );
+
+}
+
+output_html_with_http_headers $input, $cookie, $template->output;
