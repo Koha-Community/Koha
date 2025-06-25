c1a1b85c874e95ed9839fff206a3609f3d0fd858
Lisette Scheer <lisette@bywatersolutions.com>
2025-06-25 15:11:00 +0000

    Bug 34561: (QA follow-up) Rename to StaffReportsHome to match othe html customizations
    I noticed the rest of the html customizations don't have html in the option name, so I renamed for consistency.
    
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

--- installer/data/mysql/atomicupdate/bug-34561-move-StaffReportsHomeHTML-to-additional-contents.pl
+++ installer/data/mysql/atomicupdate/bug-34561-move-StaffReportsHomeHTML-to-additional-contents.pl
@@ -8,28 +8,28 @@ return {
         my ( $dbh, $out ) = @$args{qw(dbh out)};
 
         # Get any existing value from the IntranetReportsHomeHTML system preference
-        my ($staffreportshomehtml) = $dbh->selectrow_array(
+        my ($staffreportshome) = $dbh->selectrow_array(
             q|
             SELECT value FROM systempreferences WHERE variable='IntranetReportsHomeHTML';
         |
         );
-        if ($staffreportshomehtml) {
+        if ($staffreportshome) {
 
             $dbh->do(
-                "INSERT INTO additional_contents ( category, code, location, branchcode, published_on ) VALUES ('html_customizations', 'StaffReportsHomeHTML', 'StaffReportsHomeHTML', NULL, CAST(NOW() AS date) )"
+                "INSERT INTO additional_contents ( category, code, location, branchcode, published_on ) VALUES ('html_customizations', 'StaffReportsHome', 'StaffReportsHome', NULL, CAST(NOW() AS date) )"
             );
 
             my ($insert_id) = $dbh->selectrow_array(
-                "SELECT id FROM additional_contents WHERE category = 'html_customizations' AND code = 'StaffReportsHomeHTML' AND location = 'StaffReportsHomeHTML' LIMIT 1",
+                "SELECT id FROM additional_contents WHERE category = 'html_customizations' AND code = 'StaffReportsHome' AND location = 'StaffReportsHome' LIMIT 1",
                 {}
             );
 
             $dbh->do(
-                "INSERT INTO additional_contents_localizations ( additional_content_id, title, content, lang ) VALUES ( ?, 'StaffReportsHomeHTML default', ?, 'default' )",
-                undef, $insert_id, $staffreportshomehtml
+                "INSERT INTO additional_contents_localizations ( additional_content_id, title, content, lang ) VALUES ( ?, 'StaffReportsHome default', ?, 'default' )",
+                undef, $insert_id, $staffreportshome
             );
 
-            say $out "Added 'StaffReportsHomeHTML' HTML customization";
+            say $out "Added 'StaffReportsHome' HTML customization";
         }
 
         # Remove old system preference
