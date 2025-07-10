3c3cc6622c87326eba2fa33b07ac0e51b34da325
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2025-07-10 09:10:01 +0200

    Bug 40337: Define checkprevcheckout as ENUM
    On bug 40178 and bug 40179 we defined checkprevcheckout attributes as
    ENUM at the REST API level, so the specs prevent us from inserting
    values that are no yes, no, inherit in DB.
    However the DB schema didn't have that constraint.
    This has been caught by some tests that were failing:
    eg. t/db_dependent/selenium/patrons_search.t with "Not in enum list: yes, no, inherit."
    
    Test plan:
    Apply this patch, run `updatedatabase`, `dbic` and `restart_all`
    `prove t/db_dependent/selenium/patrons_search.t` should now pass.
    
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

---
+++ installer/data/mysql/atomicupdate/bug_40337.pl
@@ -0,0 +1,32 @@
+use Modern::Perl;
+use Koha::Installer::Output qw(say_warning say_success say_info);
+
+return {
+    bug_number  => "40337",
+    description => "Define checkprevcheckout as ENUM",
+    up          => sub {
+        my ($args) = @_;
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
+
+        $dbh->do(
+            q{
+            ALTER TABLE borrowers
+            MODIFY COLUMN `checkprevcheckout` enum('yes', 'no', 'inherit') NOT NULL DEFAULT 'inherit' COMMENT 'produce a warning for this patron if this item has previously been checked out to this patron if ''yes'', not if ''no'', defer to category setting if ''inherit''.'
+        }
+        );
+
+        $dbh->do(
+            q{
+            ALTER TABLE categories
+            MODIFY COLUMN `checkprevcheckout` enum('yes', 'no', 'inherit') NOT NULL DEFAULT 'inherit' COMMENT 'produce a warning for this patron category if this item has previously been checked out to this patron if ''yes'', not if ''no'', defer to syspref setting if ''inherit''.'
+        }
+        );
+
+        $dbh->do(
+            q{
+            ALTER TABLE deletedborrowers
+            MODIFY COLUMN `checkprevcheckout` enum('yes', 'no', 'inherit') NOT NULL DEFAULT 'inherit' COMMENT 'produce a warning for this patron if this item has previously been checked out to this patron if ''yes'', not if ''no'', defer to category setting if ''inherit''.'
+        }
+        );
+    },
+};
