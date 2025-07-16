6f1bd24e874f655e430ba341e74cfc6c0a2dd156
Tomas Cohen Arazi <tomascohen@theke.io>
2025-07-16 15:12:02 -0300

    Bug 40419: Fix wrong license
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

--- reports/orders_by_fund.pl
+++ reports/orders_by_fund.pl
@@ -1,22 +1,22 @@
 #!/usr/bin/perl
 
+# Copyright Frédérick Capovilla, 2011 - SYS-TECH
+# Copyright Élyse Morin, 2012 - Libéo
+#
 # This file is part of Koha.
 #
-# Author : Frédérick Capovilla, 2011 - SYS-TECH
-# Modified by : Élyse Morin, 2012 - Libéo
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
 #
-# Koha is free software; you can redistribute it and/or modify it under the
-# terms of the GNU General Public License as published by the Free Software
-# Foundation; either version 3 of the License, or (at your option) any later
-# version.
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
 #
-# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along with
-# Koha; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
-# Suite 330, Boston, MA  02111-1307 USA
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
 
 =head1 orders_by_budget
 
