a42a263cbfa8febb37b60591d32213718eb579d2
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2025-09-02 12:31:13 +0200

    Bug 40680: Fix admin/columns_settings.pl
    Attempt to call undefined import method with arguments (get_modules) via package C4::Utils::DataTables::TablesSettings (Perhaps you forgot to load the package?) at admin/columns_settings.pl line 8.
    
    Signed-off-by: Victor Grousset/tuxayo <victor@tuxayo.net>
    Signed-off-by: Martin Renvoize <martin.renvoize@openfifth.co.uk>
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

--- admin/columns_settings.pl
+++ admin/columns_settings.pl
@@ -4,8 +4,8 @@ use Modern::Perl;
 use CGI;
 use C4::Auth qw( get_template_and_user );
 use C4::Context;
-use C4::Output                            qw( output_html_with_http_headers );
-use C4::Utils::DataTables::TablesSettings qw( get_modules );
+use C4::Output qw( output_html_with_http_headers );
+use C4::Utils::DataTables::TablesSettings;
 my $input = CGI->new;
 
 my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
