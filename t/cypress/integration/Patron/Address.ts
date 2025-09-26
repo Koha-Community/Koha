77a78121f2414aa4a6914aec51d146d056dfbb3f
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2025-09-26 21:41:22 +0200

    Bug 40320: Use insertSamplePatron
    On
      commit 959b7e8f05059522151fb24992ed7250ac436dc1
      Bug 40345: Add insertSamplePatron
    
    We remove the ability to insert a patron using insertObject, we need to
    use insertSamplePatron instead.
    
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>
    (cherry picked from commit be0dd91c4197b4209adfc716db3a65517f2891fb)
    Signed-off-by: Paul Derscheid <paul.derscheid@lmscloud.de>

--- t/cypress/integration/Patron/Address.ts
+++ t/cypress/integration/Patron/Address.ts
@@ -2,37 +2,20 @@ describe("Display address", () => {
     beforeEach(() => {
         cy.login();
         cy.title().should("eq", "Koha staff interface");
-        cy.task("buildSampleObject", {
-            object: "patron",
-            values: {
-                street_number: 12,
-                street_type: "Rd",
-                address: "Awesome",
-                address2: "Library",
-                city: "Portland",
-                state: "OR",
-                postal_code: "44240",
-                country: "USA",
-            },
-        }).then(generatedPatron => {
-            cy.task("insertObject", {
-                type: "patron",
-                object: generatedPatron,
-            }).then(objects_patron => {
-                cy.wrap(objects_patron).as("objects_patron");
-            });
+        cy.task("insertSamplePatron").then(objects_patron => {
+            cy.wrap(objects_patron).as("objects_patron");
         });
     });
 
     afterEach(function () {
-        cy.task("deleteSampleObjects", [this.objects_patron]);
+        cy.task("deleteSampleObjects", this.objects_patron);
     });
 
     it("should have correct spacing", function () {
+        const patron = this.objects_patron.patron;
         cy.visit(
-            `/cgi-bin/koha/members/moremember.pl?borrowernumber=${this.objects_patron.patron_id}`
+            `/cgi-bin/koha/members/moremember.pl?borrowernumber=${patron.patron_id}`
         );
-        const patron = this.objects_patron;
         cy.get(".patronbriefinfo").should($el => {
             const re = new RegExp(
                 `${patron.street_number} ${patron.address} ${patron.street_type}\\n\\s*${patron.address2}\\n\\s*${patron.city}, ${patron.state} ${patron.postal_code}\\n\\s*${patron.country}`
