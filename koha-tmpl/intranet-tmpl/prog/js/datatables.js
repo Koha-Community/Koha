Koha-Community/Koha

commit e9b8004d197876dea8c38374c08ec00451e37020

Bug 38255: Replace dataTable constructor with kohaTable - Staff
Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>
Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
Signed-off-by: Katrin Fischer <katrin.fischer@bsz-bw.de>

koha-tmpl/intranet-tmpl/prog/js/datatables.js
@@ -1332,22 +1332,29 @@ function update_search_description(
                 };
             }
 
+            if (options.ajax) {
+                options.ajax = Object.assign(
+                    {},
+                    options.ajax,
+                    _dt_default_ajax({ default_filters, options })
+                );
+                options.serverSide = true;
+                options.processing = true;
+                options.pagingType = "full_numbers";
+            }
+
             settings = $.extend(
                 true,
                 {},
                 dataTablesDefaults,
                 {
                     paging: true,
-                    serverSide: true,
                     searching: true,
-                    pagingType: "full_numbers",
-                    processing: true,
                     language: {
                         emptyTable: options.emptyTable
                             ? options.emptyTable
                             : __("No data available in table"),
                     },
-                    ajax: _dt_default_ajax({ default_filters, options }),
                 },
                 options
             );

commit 1b57d75e966a6a38738a556dfe622a576ae86d60

Bug 40865: Redirect if only one patron is found
Regression from
  commit e9b8004d197876dea8c38374c08ec00451e37020
  Bug 38255: Replace dataTable constructor with kohaTable - Staff

We should keep the params passed and not override them

Test plan:
1 - Search for 'edna acosta' in check out search on koha home page
2 - See the one result
3 - You are expected to be redirected to the patron's page

Signed-off-by: Brendan Lawlor <blawlor@clamsnet.org>

Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>
Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

koha-tmpl/intranet-tmpl/prog/js/datatables.js
@@ -1373,8 +1373,8 @@ function update_search_description(
             if (options.ajax && !options.bKohaAjaxSVC) {
                 options.ajax = Object.assign(
                     {},
-                    options.ajax,
-                    _dt_default_ajax({ default_filters, options })
+                    _dt_default_ajax({ default_filters, options }),
+                    options.ajax
                 );
                 options.serverSide = true;
                 options.processing = true;