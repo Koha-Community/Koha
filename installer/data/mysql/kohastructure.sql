d90780504fcd9110e832fdb570f8df6c938ea953
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2025-10-09 16:25:46 +0200

    Bug 39190: Fix DB struct diff
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

--- installer/data/mysql/kohastructure.sql
+++ installer/data/mysql/kohastructure.sql
@@ -3476,6 +3476,33 @@ CREATE TABLE `export_format` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Table structure for table `file_transports`
+--
+
+DROP TABLE IF EXISTS `file_transports`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `file_transports` (
+  `file_transport_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(80) NOT NULL,
+  `host` varchar(80) NOT NULL DEFAULT 'localhost',
+  `port` int(11) NOT NULL DEFAULT 22,
+  `transport` enum('ftp','sftp') NOT NULL DEFAULT 'sftp',
+  `passive` tinyint(1) NOT NULL DEFAULT 1,
+  `user_name` varchar(80) DEFAULT NULL,
+  `password` mediumtext DEFAULT NULL,
+  `key_file` mediumtext DEFAULT NULL,
+  `auth_mode` enum('password','key_file','noauth') NOT NULL DEFAULT 'password',
+  `download_directory` mediumtext DEFAULT NULL,
+  `upload_directory` mediumtext DEFAULT NULL,
+  `status` longtext DEFAULT NULL,
+  `debug` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`file_transport_id`),
+  KEY `host_idx` (`host`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `hold_cancellation_requests`
 --
 
@@ -5990,33 +6017,6 @@ CREATE TABLE `sessions` (
   `id` varchar(32) NOT NULL,
   `a_session` longblob NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `file_transports`
---
-
-DROP TABLE IF EXISTS `file_transports`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `file_transports` (
-  `file_transport_id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(80) NOT NULL,
-  `host` varchar(80) NOT NULL DEFAULT 'localhost',
-  `port` int(11) NOT NULL DEFAULT 22,
-  `transport` enum('ftp','sftp') NOT NULL DEFAULT 'sftp',
-  `passive` tinyint(1) NOT NULL DEFAULT 1,
-  `user_name` varchar(80) DEFAULT NULL,
-  `password` mediumtext DEFAULT NULL,
-  `key_file` mediumtext DEFAULT NULL,
-  `auth_mode` enum('password','key_file','noauth') NOT NULL DEFAULT 'password',
-  `download_directory` mediumtext DEFAULT NULL,
-  `upload_directory` mediumtext DEFAULT NULL,
-  `status` longtext DEFAULT NULL,
-  `debug` tinyint(1) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`file_transport_id`),
-  KEY `host_idx` (`host`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
