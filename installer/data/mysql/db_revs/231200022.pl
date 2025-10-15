eb5c38a5ab48b60d310115728e0c377fae6eb95f
Martin Renvoize <martin.renvoize@openfifth.co.uk>
2025-10-15 16:41:38 +0100

    Bug 38489: (follow-up) Make old db_rev more idempotent 2
    In the unit test system we reset the database version to 21.11 after
    having upgraded a long way past that. The 231200022 update appears not
    to be fully idempotent, so we fail running through the upgrade.
    
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

--- installer/data/mysql/db_revs/231200022.pl
+++ installer/data/mysql/db_revs/231200022.pl
@@ -7,24 +7,26 @@ return {
         my ($args) = @_;
         my ( $dbh, $out ) = @$args{qw(dbh out)};
 
-        if ( !column_exists( 'vendor_edi_accounts', 'download_port' ) ) {
-            $dbh->do(
-                q{
-              ALTER TABLE vendor_edi_accounts ADD COLUMN `download_port` int(11) DEFAULT NULL AFTER `password`
-          }
-            );
+        if ( column_exists( 'vendor_edi_accounts', 'password' ) ) {
+            if ( !column_exists( 'vendor_edi_accounts', 'download_port' ) ) {
+                $dbh->do(
+                    q{
+                      ALTER TABLE vendor_edi_accounts ADD COLUMN `download_port` int(11) DEFAULT NULL AFTER `password`
+                      }
+                );
 
-            say $out "Added column 'vendor_edi_accounts.download_port'";
-        }
+                say $out "Added column 'vendor_edi_accounts.download_port'";
+            }
 
-        if ( !column_exists( 'vendor_edi_accounts', 'upload_port' ) ) {
-            $dbh->do(
-                q{
-              ALTER TABLE vendor_edi_accounts ADD COLUMN `upload_port` int(11) DEFAULT NULL AFTER `download_port`
-          }
-            );
+            if ( !column_exists( 'vendor_edi_accounts', 'upload_port' ) ) {
+                $dbh->do(
+                    q{
+                          ALTER TABLE vendor_edi_accounts ADD COLUMN `upload_port` int(11) DEFAULT NULL AFTER `download_port`
+                    }
+                );
 
-            say $out "Added column 'vendor_edi_accounts.upload_port'";
+                say $out "Added column 'vendor_edi_accounts.upload_port'";
+            }
         }
     },
 };
