c24a5b25c56a464c3a25d687c2a8ce0069545e7e
Pedro Amorim <pedro.amorim@openfifth.co.uk>
2025-11-06 15:53:05 +0000

    Bug 37893: (QA follow-up): Remove file_transports database table
    This was clearly a mistake due a botched rebase.
    
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

--- installer/data/mysql/kohastructure.sql
+++ installer/data/mysql/kohastructure.sql
@@ -6048,33 +6048,6 @@ CREATE TABLE `sessions` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `file_transports`
---
-
-DROP TABLE IF EXISTS `file_transports`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `file_transports` (
-  `file_transport_id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(80) NOT NULL,
-  `host` varchar(80) NOT NULL DEFAULT 'localhost',
-  `port` int(11) NOT NULL DEFAULT 22,
-  `transport` enum('ftp','sftp') NOT NULL DEFAULT 'sftp',
-  `passive` tinyint(1) NOT NULL DEFAULT 1,
-  `user_name` varchar(80) DEFAULT NULL,
-  `password` mediumtext DEFAULT NULL,
-  `key_file` mediumtext DEFAULT NULL,
-  `auth_mode` enum('password','key_file','noauth') NOT NULL DEFAULT 'password',
-  `download_directory` mediumtext DEFAULT NULL,
-  `upload_directory` mediumtext DEFAULT NULL,
-  `status` longtext DEFAULT NULL,
-  `debug` tinyint(1) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`file_transport_id`),
-  KEY `host_idx` (`host`)
-  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Table structure for table `sip_accounts`
 --
 
