30c3b6e00114c01b1df8bb6345da9716a7aec9a1
Lucas Gass <lucas@bywatersolutions.com>
2025-12-05 10:28:39 -0700

    25.11.00: Update kohastructure.sql
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

--- installer/data/mysql/kohastructure.sql
+++ installer/data/mysql/kohastructure.sql
@@ -1,8 +1,9 @@
--- MariaDB dump 10.19  Distrib 10.11.11-MariaDB, for debian-linux-gnu (x86_64)
+/*M!999999\- enable the sandbox mode */ 
+-- MariaDB dump 10.19  Distrib 10.11.14-MariaDB, for debian-linux-gnu (x86_64)
 --
 -- Host: db    Database: koha_kohadev
 -- ------------------------------------------------------
--- Server version	11.7.2-MariaDB-ubu2404
+-- Server version	12.1.2-MariaDB-ubu2404
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -7102,4 +7103,4 @@ CREATE TABLE `zebraqueue` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-05-26 13:26:31
+-- Dump completed on 2025-12-05 17:27:25
