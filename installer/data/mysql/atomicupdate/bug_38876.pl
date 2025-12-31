287abb41df66f80c0c1184af466aa5f348ff355f
Jessica Zairo <jzairo@bywatersolutions.com>
2025-12-31 16:50:22 +0000

    Bug 38876: (follow-up) fix db rev
    syntax was wrong for dbrev
    To test apply patches
    dbic
    updatedatatbase
    koha-mysql kohadev
    select * from systempreferences where variable='UpdateNotForLoanStatusOnCheckout';
    confirm the updated description
    
    Signed-off-by: Owen Leonard <oleonard@myacpl.org>
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>
    (cherry picked from commit e8dca2a8421fb0881d669e7dc23756f9262eafa4)
    Signed-off-by: Chloé Zermatten <chloe.zermatten@openfifth.co.uk>

--- installer/data/mysql/atomicupdate/bug_38876.pl
+++ installer/data/mysql/atomicupdate/bug_38876.pl
@@ -10,7 +10,7 @@ return {
 
         # Do you stuffs here
         $dbh->do(
-            q{UPDATE systempreferences (explanation) VALUES ('This is a list of value pairs. When an item is checked out, if its not for loan value matches the value on the left, then the items not for loan value will be updated to the value on the right. \nE.g. ''-1: 0'' will cause an item that was set to ''Ordered'' to now be available for loan. Each pair of values should be on a separate line.') WHERE variable='UpdateNotForLoanStatusOnCheckout'}
+            q{UPDATE systempreferences set explanation="This is a list of value pairs. When an item is checked out, if its not for loan value matches the value on the left, then the items not for loan value will be updated to the value on the right. \nE.g. ''-1: 0'' will cause an item that was set to ''Ordered'' to now be available for loan. Each pair of values should be on a separate line." WHERE variable='UpdateNotForLoanStatusOnCheckout'}
         );
 
         # sysprefs
