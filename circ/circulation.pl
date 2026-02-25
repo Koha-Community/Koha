171f727734adc288477357097649f38b9dfdcdb6
Roman Dolny <roman.dolny@jezuici.pl>
2026-02-25 19:39:13 +0000

    Bug 41940: Remove uninitialized value warnings in circulation.pl
    The warnings:
    
    [WARN] Use of uninitialized value $patron_session_confirmation in split
       at /kohadevbox/koha/circ/circulation.pl line 493.
    [WARN] Use of uninitialized value $patron_for_session in numeric eq (==)
       at /kohadevbox/koha/circ/circulation.pl line 494.
    [WARN] Use of uninitialized value $session_confirmations in split
       at /kohadevbox/koha/circ/circulation.pl line 495.
    
    appears in the plack-intranet-error.log.
    
    Test plan:
    ==========
    1. Use a fresh KTD, observe plack-intranet-error.log.
    2. Have a barcode of existing item.
    3. Find any patron and go to "Check out" tab.
    4. Check out item.
    ==> Warnings should appear in plack-intranet-error.log
    5. Apply the patch; restart_all.
    6. Repeat 2-4 with another barcode.
    ==> No warnings in plack-intranet-error.log.
    
    Sponsored-by: Ignatianum University in Cracow
    Signed-off-by: David Nind <david@davidnind.com>
    
    Bug 41940: (follow-up) Syntax corrections
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    Signed-off-by: Baptiste Wojtkowski <baptiste.wojtkowski@biblibre.com>
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

--- circ/circulation.pl
+++ circ/circulation.pl
@@ -528,10 +528,10 @@ if ( @$barcodes && $op eq 'cud-checkout' ) {
                 $template_params->{ADDITIONAL_MATERIALS} = $materials;
                 $template_params->{itemhomebranch}       = $item->homebranch;
 
-                my $patron_session_confirmation = $query->cookie('patronSessionConfirmation') || undef;
+                my $patron_session_confirmation = $query->cookie('patronSessionConfirmation') || '';
                 my ( $patron_for_session, $session_confirmations ) = split( /:/, $patron_session_confirmation, 2 );
-                my $patron_match = $borrowernumber == $patron_for_session;
-                my @conf_keys    = split( /\|/, $session_confirmations );
+                my $patron_match = $patron_for_session && ( $borrowernumber == $patron_for_session );
+                my @conf_keys    = $session_confirmations ? split( /\|/, $session_confirmations ) : ();
                 if ( $patron_match && grep { $_ eq 'cancelreserve' } @conf_keys ) {
                     $cancelreserve = 1;
                 }
