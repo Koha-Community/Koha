637139d782d4cb3932bd66abcbe692ee342aa9dc
Kyle M Hall <kyle@bywatersolutions.com>
2026-03-26 11:55:17 -0400

    Bug 29016 [25.11.x]: Add REST API for action logs
    Signed-off-by: David Nind <david@davidnind.com>
    Signed-off-by: Martin Renvoize <martin.renvoize@openfifth.co.uk>
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>
    Signed-off-by: Jacob O'Mara <Jacob.omara@openfifth.co.uk>

---
+++ Koha/REST/V1/ActionLogs.pm
@@ -0,0 +1,45 @@
+package Koha::REST::V1::ActionLogs;
+
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <https://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+use Mojo::Base 'Mojolicious::Controller';
+
+use Koha::ActionLogs;
+
+use Try::Tiny qw( catch try );
+
+=head1 API
+
+=head2 Methods
+
+=head3 list
+
+=cut
+
+sub list {
+    my $c = shift->openapi->valid_input or return;
+
+    return try {
+        my $logs = $c->objects->search( Koha::ActionLogs->new );
+        return $c->render( status => 200, openapi => $logs );
+    } catch {
+        $c->unhandled_exception($_);
+    };
+}
+
+1;
