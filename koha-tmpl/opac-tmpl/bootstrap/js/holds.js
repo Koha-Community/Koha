5134a34d5846dc06b585f4257641d8b8b4b45658
Pedro Amorim <pedro.amorim@openfifth.co.uk>
2026-04-02 10:58:01 +0000

    Bug 41953: Make it obvious that '(part of a hold group)' is expandable
    Test plan:
    1) Enable DisplayAddHoldGroups
    2) Go to 'koha' patron and click the 'Search to hold' button:
    <staff_url>/cgi-bin/koha/members/moremember.pl?borrowernumber=51
    3) Search for keyword 'music'. Pick 2 results with available items by ticking the checkbox next to each record.
    4) Click the downward arrow in the 'Place hold' toolbar button.
    5) Pick 'Place hold for koha'
    6) Tick the 'Treat as hold group' checkbox.
    7) Set a pickup location for each of the holds. Click 'Place holds'.
    8) Repeat steps 3 to 7, but pick 2 other records (creating a second hold group).
    9) Log in to the OPAC as the 'koha' user:
    <opac_url>/cgi-bin/koha/opac-user.pl
    10) On the summary, click the 'Holds' tab.
    11) Notice a new 'hold group' column is shown, similar to Staff UI
    12) Notice the 'Part of a hold group' bit is now visually an expandable link, reflecting its behaviour
    
    Signed-off-by: Bob Bennhoff <bbennhoff@clicweb.org>
    Signed-off-by: Andrew Fuerste Henry <andrew@bywatersolutions.com>
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>
    (cherry picked from commit acc2d6aa99f3d99825bb0a77bb173c17eb77f966)
    Signed-off-by: Jacob O'Mara <Jacob.omara@openfifth.co.uk>

--- koha-tmpl/opac-tmpl/bootstrap/js/holds.js
+++ koha-tmpl/opac-tmpl/bootstrap/js/holds.js
@@ -1,5 +1,19 @@
 $(document).ready(function () {
-    $("div.group_hold").click(function () {
-        $(this).find("div.group_hold_list").toggle();
+    $(".group_hold_trigger").on("click", function (e) {
+        e.preventDefault();
+
+        var $icon = $(this).find("i");
+        var $list = $(this).siblings(".group_hold_list");
+
+        $list.toggle();
+
+        // Swap the caret icon direction
+        if ($list.is(":visible")) {
+            $icon.removeClass("fa-caret-right").addClass("fa-caret-down");
+            $(this).attr("aria-expanded", "true");
+        } else {
+            $icon.removeClass("fa-caret-down").addClass("fa-caret-right");
+            $(this).attr("aria-expanded", "false");
+        }
     });
 });
