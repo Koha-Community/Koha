ab7602b0f360d327149937c80aeb24c57d58d9ab
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2026-04-09 11:27:23 +0200

    Bug 42322: Add a 'collapse sidebar' button
    On bug 42190 we discuss the possibility to collapse the sidebar in order to enlarge the screen and specifically the table in the main div.
    
    This patch adds the button to main-container.inc that is supposed to be
    used from everywhere.
    
    Signed-off-by: Clemens Tubach <clemens.tubach@kit.edu>
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>
    Signed-off-by: Pedro Amorim <pedro.amorim@openfifth.co.uk>

--- koha-tmpl/intranet-tmpl/prog/js/staff-global.js
+++ koha-tmpl/intranet-tmpl/prog/js/staff-global.js
@@ -1158,3 +1158,23 @@ function validatePatronSearch(form) {
     }
     return true;
 }
+
+// Sidebar toggle functionality
+$("#toggle-sidebar").click(function () {
+    var sidebar = $("#sidebar-container");
+    var icon = $("#toggle-sidebar i");
+    var toggleWrapper = $(".sidebar-toggle-wrapper");
+    var mainContainer = $("main").parent();
+
+    if (sidebar.hasClass("collapsed")) {
+        sidebar.removeClass("collapsed");
+        icon.removeClass("fa-chevron-right").addClass("fa-chevron-left");
+        toggleWrapper.css("left", "14%");
+        mainContainer.removeClass("col-md-12").addClass("col-md-10");
+    } else {
+        sidebar.addClass("collapsed");
+        icon.removeClass("fa-chevron-left").addClass("fa-chevron-right");
+        toggleWrapper.css("left", "-15px");
+        mainContainer.removeClass("col-md-10").addClass("col-md-12");
+    }
+});
