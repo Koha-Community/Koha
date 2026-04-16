d867e26002d53d9b0991936a08d0c774150a0375
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2026-04-16 09:15:56 +0200

    Bug 42374: Restore require Koha::Plugins (instead of use)
    See comment 4 for a detail explanation of the problem.
    
    Test plan:
    hit /cgi-bin/koha/installer/install.pl
    login with the DB credentials
    upgrade
    => Without this patch you get
    ```
    ISBN_RANGE_MESSAGE is set to [] but that file does not exist!
    Trying to use the default locations
    ```
    => With this patch applied: "Everything went okay. Update done."
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    Signed-off-by: Emily Lamancusa <emily.lamancusa@montgomerycountymd.gov>
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>
    (cherry picked from commit d0bd802da61818a15bb8adfcc720bb693d587544)
    Signed-off-by: Jacob O'Mara <Jacob.omara@openfifth.co.uk>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -49,7 +49,6 @@ use Koha;
 use Koha::DateUtils qw( dt_from_string output_pref );
 use Koha::Caches;
 use Koha::Installer::Output qw ( say_failure );
-use Koha::Plugins;
 
 use MARC::Record;
 use MARC::File::XML ( BinaryEncoding => 'utf8' );
@@ -23041,6 +23040,7 @@ if ( CheckVersion($DBversion) ) {
         );
     }
 
+    require Koha::Plugins;
     Koha::Plugins->new( { enable_plugins => 1 } )->InstallPlugins;
 
     SetVersion($DBversion);
