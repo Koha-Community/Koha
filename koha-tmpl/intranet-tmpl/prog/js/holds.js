ac31d8ae257d4e809bc9c2d510bc69b89f457fa4
Slava Shishkin <slavashishkin@gmail.com>
2026-04-28 18:58:48 +0300

    Bug 41227: Respect SuspendHoldsIntranet on patron holds page
    Test plan:
    1. Set SuspendHoldsIntranet to "Don't allow"
    2. Place a hold for a patron
    3. Open the patron holds table
    4. Confirm the Suspend button is visible in the Suspend column
    5. Apply the patch
    6. Reload the patron holds table
    7. Confirm the Suspend button is no longer visible
    8. Set SuspendHoldsIntranet to "Allow"
    9. Reload the patron holds table
    10. Confirm the Suspend button is visible again
    
    Signed-off-by: Anneli Österman <anneli.osterman@koha-suomi.fi>
    Signed-off-by: Laura_Escamilla <laura.escamilla@bywatersolutions.com>
    Signed-off-by: Pedro Amorim <pedro.amorim@openfifth.co.uk>

--- koha-tmpl/intranet-tmpl/prog/js/holds.js
+++ koha-tmpl/intranet-tmpl/prog/js/holds.js
@@ -415,6 +415,10 @@ $(document).ready(function () {
                             data: function (oObj) {
                                 holds[oObj.reserve_id] = oObj; //Store holds for later use
 
+                                if (!SuspendHoldsIntranet) {
+                                    return "";
+                                }
+
                                 if (oObj.found) {
                                     return "";
                                 } else if (oObj.suspend == 1) {
