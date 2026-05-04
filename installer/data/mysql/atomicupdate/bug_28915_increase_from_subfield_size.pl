694227943527015fa3ff27f69147894d42a77619
Andrew Fuerste Henry <andrew@bywatersolutions.com>
2026-05-04 15:44:32 +0000

    Bug 28915: (follow-up) atomicupdate to increase from_subfield size
    Signed-off-by: Andrew Fuerste Henry <andrew@bywatersolutions.com>
    Signed-off-by: Anneli Österman <anneli.osterman@koha-suomi.fi>
    Signed-off-by: Andrew Fuerste Henry <andrew@bywatersolutions.com>
    Signed-off-by: Pedro Amorim <pedro.amorim@openfifth.co.uk>

---
+++ installer/data/mysql/atomicupdate/bug_28915_increase_from_subfield_size.pl
@@ -0,0 +1,22 @@
+use Modern::Perl;
+use Koha::Installer::Output qw(say_warning say_success say_info);
+
+return {
+    bug_number  => "28915",
+    description => "Increase from_subfield to allow two characters",
+    up          => sub {
+        my ($args) = @_;
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
+
+        # Do you stuffs here
+        $dbh->do(q{ALTER TABLE marc_modification_template_actions MODIFY from_subfield VARCHAR(2)});
+
+        # Print useful stuff here
+        # tables
+        say $out "Modifying marc_modification_template_actions.from_subfield to VARCHR(2)";
+
+        # Other information
+        say_success( $out, "Modified marc_modification_template_actions.from_subfield to VARCHR(2)" );
+
+    },
+};
