b1834a75fdefdf57ba14a02b6a38ba7f2d645311
Pedro Amorim <pedro.amorim@openfifth.co.uk>
2026-06-24 08:38:35 +0000

    Bug 42666: (RM follow-up): Tidy datatables.js
    prove xt/js_tidy.t
    
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

--- koha-tmpl/intranet-tmpl/prog/js/datatables.js
+++ koha-tmpl/intranet-tmpl/prog/js/datatables.js
@@ -1496,8 +1496,10 @@ function update_search_description(
                 table_settings.hasOwnProperty("default_display_length") &&
                 table_settings["default_display_length"] != null
             ) {
-                settings["pageLength"] =
-                    parseInt(table_settings["default_display_length"], 10);
+                settings["pageLength"] = parseInt(
+                    table_settings["default_display_length"],
+                    10
+                );
             }
             if (
                 table_settings.hasOwnProperty("default_sort_order") &&
