d9b06c79e358470729c106bbb1e164388f540d48
Kyle M Hall <kyle@bywatersolutions.com>
2026-06-29 10:02:09 -0400

    Bug 41996: (QA follow-up) Fix up atomic update
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>
    Signed-off-by: Pedro Amorim <pedro.amorim@openfifth.co.uk>

--- installer/data/mysql/atomicupdate/bug_41996.pl
+++ installer/data/mysql/atomicupdate/bug_41996.pl
@@ -1,5 +1,5 @@
 use Modern::Perl;
-use Koha::Installer::Output qw(say_warning say_success say_info);
+use Koha::Installer::Output qw(say_success say_failure say_info);
 
 return {
     bug_number  => "41996",
@@ -9,24 +9,36 @@ return {
         my ( $dbh, $out ) = @$args{qw(dbh out)};
 
         if ( !column_exists( 'edifact_errors', 'invoicenumber' ) ) {
-            $dbh->do(
+            my $ok = $dbh->do(
                 q{
-                ALTER TABLE edifact_errors
-                ADD COLUMN invoicenumber varchar(48) DEFAULT NULL
-                AFTER message_id
-            }
+                    ALTER TABLE edifact_errors
+                    ADD COLUMN invoicenumber varchar(48) DEFAULT NULL
+                    AFTER message_id
+                }
             );
-            say_success( $out, "Added invoicenumber column to edifact_errors" );
-
-            $dbh->do(
-                q{
-                ALTER TABLE edifact_errors
-                ADD INDEX message_id_invoicenumber (message_id, invoicenumber)
+            if ($ok) {
+                say_success( $out, "Added invoicenumber column to edifact_errors" );
+            } else {
+                say_failure( $out, "Failed to add invoicenumber column to edifact_errors: " . $dbh->errstr );
             }
-            );
-            say_success( $out, "Added index message_id_invoicenumber to edifact_errors" );
         } else {
             say_info( $out, "Column invoicenumber already exists in edifact_errors" );
+        }
+
+        unless ( index_exists( 'edifact_errors', 'message_id_invoicenumber' ) ) {
+            my $ok = $dbh->do(
+                q{
+                    ALTER TABLE edifact_errors
+                    ADD INDEX message_id_invoicenumber (message_id, invoicenumber)
+                }
+            );
+            if ($ok) {
+                say_success( $out, "Added index message_id_invoicenumber to edifact_errors" );
+            } else {
+                say_failure( $out, "Failed to add index message_id_invoicenumber to edifact_errors: " . $dbh->errstr );
+            }
+        } else {
+            say_info( $out, "Index message_id_invoicenumber already exists on edifact_errors" );
         }
     },
 };
