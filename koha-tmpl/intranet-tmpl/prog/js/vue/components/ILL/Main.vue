fc8f56b88f8d76198bf8d3743d9ac01c59d971d3
Owen Leonard <oleonard@myacpl.org>
2026-08-07 07:53:16 -0400

    Bug 42322: (follow-up) Correct markup of ILL sidebar menu
    Signed-off-by: Pedro Amorim <pedro.amorim@openfifth.co.uk>

--- koha-tmpl/intranet-tmpl/prog/js/vue/components/ILL/Main.vue
+++ koha-tmpl/intranet-tmpl/prog/js/vue/components/ILL/Main.vue
@@ -13,9 +13,7 @@
                     </main>
                 </div>
 
-                <div class="col-md-2 order-sm-2 order-md-1">
-                    <LeftMenu :title="$__('Interlibrary loans')"></LeftMenu>
-                </div>
+                <LeftMenu :title="$__('Interlibrary loans')"></LeftMenu>
             </div>
         </div>
     </div>
