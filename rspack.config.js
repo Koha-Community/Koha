2a8adb8b63a2fdd87027acc90c2cad69ef425167
David Cook <dcook@prosentient.com.au>
2026-09-11 00:52:38 +0000

    Bug 43401: (QA follow-up) tweak rspack.config.js comment and fix Makefile.PL
    Signed-off-by: David Cook <dcook@prosentient.com.au>
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>

--- rspack.config.js
+++ rspack.config.js
@@ -6,7 +6,7 @@ const rspack = require("@rspack/core");
 // NOTE: Every output.path under koha-tmpl/ that ships must also be handled in
 // Makefile.PL: the 'NONE' entries in the target-directory map and the cp -r
 // lines in the compile step. A new dist dir added here without both changes is
-// silently omitted from packaged builds (see Bug 43401).
+// silently omitted from builds from source (see Bug 43401).
 
 const islandsExport = application => {
     const vueDir =
